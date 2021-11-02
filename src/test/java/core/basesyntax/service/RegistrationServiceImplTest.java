ff35408f9b37cae190223f017b479563de386e9c
ASelivonchyk <aselivonchyk@gmail.com>
2021-11-02 11:36:41 +0200

    Made setUp() method, that create valid user before each test; Made tearDown() method, that cleat storage after each test; Deleted sout() and assertNotNull() in userRegistration_Ok() test.

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,37 +1,42 @@
 package core.basesyntax.service;
 
-import core.basesyntax.dao.StorageDao;
-import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegistrationServiceImplTest {
     private static RegistrationService registrationService;
-    private static StorageDao storageDao;
     private User actual;
 
     @BeforeAll
     static void beforeAll() {
         registrationService = new RegistrationServiceImpl();
-        storageDao = new StorageDaoImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        actual = new User("Alex", "qwerty", 18);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
     }
 
     @Test
     void userDuplicate_notOk() {
-        User duplicate = registrationService.register(new User("Alex", "qwerty", 18));
+        User duplicate = registrationService.register(actual);
         assertThrows(RuntimeException.class, () -> registrationService.register(duplicate));
     }
 
     @Test
     void userRegistration_Ok() {
-        User expected = storageDao.get("Alex");
-        assertNotNull(expected);
-        System.out.println("User added to List");
-        User current = new User("Bob", "123456", 20);
-        assertEquals(current,registrationService.register(current));
-        System.out.println("method return current user");
+        assertEquals(actual,registrationService.register(actual));
     }
 
     @Test
@@ -42,32 +47,32 @@ class RegistrationServiceImplTest {
 
     @Test
     void nullUserLogin_notOk() {
-        actual = new User(null, "qwerty", 18);
+        actual.setLogin(null);
         assertThrows(RuntimeException.class, () -> registrationService.register(actual));
     }
 
     @Test
     void nullUserAge_notOk() {
-        actual = new User("Cris", "qwerty", null);
+        actual.setAge(null);
         assertThrows(RuntimeException.class, () -> registrationService.register(actual));
     }
 
     @Test
     void nullUserPass_notOk() {
-        actual = new User("Dan", null, 18);
+        actual.setPassword(null);
         assertThrows(RuntimeException.class, () -> registrationService.register(actual));
     }
 
 
     @Test
     void userAge_notOk() {
-        actual = new User("Eric", "qwerty", 15);
+        actual.setAge(15);
         assertThrows(RuntimeException.class, () -> registrationService.register(actual));
     }
 
     @Test
     void userPassword_notOk() {
-        actual = new User("Felix", "12345", 18);
+        actual.setPassword("12345");
         assertThrows(RuntimeException.class, () -> registrationService.register(actual));
     }
 }
