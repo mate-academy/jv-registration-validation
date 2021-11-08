11b14885bc1fd1fa216a5ff252ac0b85d05f4df3
Alexander <aleksandr_pogreb@ukr.net>
2021-11-08 13:35:04 +0200

    fixed

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -9,70 +9,66 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RegistrationServiceImplTest {
 
-    private static final User VALID_USER = new User("Ivanovich", "3455667",18);
-        private static final User TWO_VALID_USER = new User("Sidorovich", "34jkl667",33);
-        private static final User SAME_LOGIN_VALID_USER = new User("Ivanovich", "pp477777",85);
+        private static final User validUser = new User("Ivanovich", "3455667",18);
+        private static final User twoValidUser = new User("Sidorovich", "34jkl667",33);
 
-        private static final User NULL_USER = new User(null, null,null);
-        private static final User NULL_LOGIN_USER = new User(null, "dgfhjjgh",45);
-        private static final User NULL_PASSWORD_USER = new User("Petrov", null,21);
-        private static final User NULL_AGE_USER = new User("Petrovko", "121212",null);
+        private static final User nullUser = new User(null, null,null);
+        private static final User nullLoginUser = new User(null, "dgfhjjgh",45);
+        private static final User nullPasswordUser = new User("Petrov", null,21);
+        private static final User nullAgeUser = new User("Petrovko", "121212",null);
 
-        private static final User AGE_USER_NOT_OK = new User("Petrovich", "1211111",10);
-        private static final User NEGATIVE_AGE_USER_NOT_OK = new User("Petrovichev", "121ll11",-100);
-        private static final User PASSWORD_USER_NOT_OK = new User("Petro", "1211w",77);
+        private static final User ageUser_NotOk = new User("Petrovich", "1211111",10);
+        private static final User negativeAgeUser_NotOk = new User("Petrovichev", "121ll11",-100);
+        private static final User passwordUser_NotOk = new User("Petro", "1211w",77);
 
-        RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
-
+        private RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
 
         @Test
         public void registered_sameUser_NotOk() {
-            registrationService.register(VALID_USER);
-            registrationService.register(TWO_VALID_USER);
-            assertThrows(RuntimeException.class, () -> registrationService.register(SAME_LOGIN_VALID_USER));
+            registrationService.register(validUser);
+            registrationService.register(twoValidUser);
+            assertThrows(RuntimeException.class, () -> registrationService.register(validUser));
         }
 
         @Test
         public void registered_Ok() {
-
-            User actual = registrationService.register(VALID_USER);
-            assertEquals(VALID_USER, actual);
+            User actual = registrationService.register(validUser);
+            assertEquals(validUser, actual);
         }
 
         @Test
         public void registered_nullUser_NotOk() {
-            assertThrows(RuntimeException.class, () -> registrationService.register(NULL_USER));
+            assertThrows(RuntimeException.class, () -> registrationService.register(nullUser));
         }
 
         @Test
         public void registered_nullLogin_NotOk() {
-            assertThrows(RuntimeException.class, () -> registrationService.register(NULL_LOGIN_USER));
+            assertThrows(RuntimeException.class, () -> registrationService.register(nullLoginUser));
         }
 
         @Test
         public void registered_nullPassword_NotOk () {
-            assertThrows(RuntimeException.class, () -> registrationService.register(NULL_PASSWORD_USER));
+            assertThrows(RuntimeException.class, () -> registrationService.register(nullPasswordUser));
         }
 
         @Test
         public void registered_nullAge_NotOk() {
-            assertThrows(RuntimeException.class, () -> registrationService.register(NULL_AGE_USER));
+            assertThrows(RuntimeException.class, () -> registrationService.register(nullAgeUser));
         }
 
         @Test
         public void registered_age_NotOk() {
-            assertThrows(RuntimeException.class, () -> registrationService.register(AGE_USER_NOT_OK));
+            assertThrows(RuntimeException.class, () -> registrationService.register(ageUser_NotOk));
         }
 
         @Test
         public void registered_negative_age_NotOk() {
-            assertThrows(RuntimeException.class, () -> registrationService.register(NEGATIVE_AGE_USER_NOT_OK));
+            assertThrows(RuntimeException.class, () -> registrationService.register(negativeAgeUser_NotOk));
         }
 
         @Test
         public void registered_password_NotOk() {
-            assertThrows(RuntimeException.class, () -> registrationService.register(PASSWORD_USER_NOT_OK));
-
+            assertThrows(RuntimeException.class, () -> registrationService.register(passwordUser_NotOk));
         }
 
         @AfterEach
