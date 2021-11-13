3a71abe63811418f411b4059565361c091910a0e
Vladyslav Chyver <lione2009lione@gmail.com>
2021-11-14 00:30:35 +0200

    logic was added, tests not work

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,30 +1,39 @@
 package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDaoImpl;
-import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private StorageDaoImpl dao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
+        checkUserDataForTrueValues(user);
+        if (user.getAge() < 18) {
+            throw new RuntimeException("User is not adult!");
+        }
+        if (dao.get(user.getLogin()) != null) {
+            throw new RuntimeException("Login is exist!");
+        }
+        return dao.add(user);
+    }
+
+    private void checkUserDataForTrueValues (User user) {
         if (user == null) {
             throw new RuntimeException("Incorrect entry about user");
         }
-        if (user.getPassword() == null || user.getLogin() == null) {
-            throw new RuntimeException("User's password or login is empty");
-        }
-        if ( user.getAge() < 18) {
-            throw new RuntimeException(" User is not adult! Registration canceled.");
+        if (user.getLogin() == null || user.getPassword() == null) {
+            throw new RuntimeException("User's login or password is absent");
         }
         if ( user.getPassword().length() < 6) {
-            throw new RuntimeException("User's password is very short. Registration canceled");
+            throw new RuntimeException("User's password is very short");
         }
-        StorageDaoImpl dao = new StorageDaoImpl();
-        if (dao.get(user.getLogin()) != null) {
-            throw new RuntimeException("Login is exist");
+        Integer age = user.getAge();
+        if (age == null) {
+            throw new RuntimeException("User's age is empty");
         }
-        dao.add(user);
-        return user;
+        if (age < 0 || age > 130) {
+            throw new RuntimeException("Incorrect user's age. Age =" + user.getAge());
+        }
     }
 }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,10 +1,92 @@
 package core.basesyntax.service;
 
-import junit.framework.TestCase;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.model.User;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
-public class RegistrationServiceImplTest extends TestCase {
+public class RegistrationServiceImplTest {
+  private RegistrationService registrationService = new RegistrationServiceImpl();
+  private User user;
+  private int nubmber = 1;
+
   @BeforeAll
+  static void storageInitialization() {
+    User marry = new User();
+    marry.setId(1L);
+    marry.setLogin("marry123");
+    marry.setPassword("qwertyqwerty");
+    marry.setAge(33);
+    User bob = new User();
+    bob.setId(12L);
+    bob.setLogin("Bobchich");
+    bob.setPassword("qqqaaazzz");
+    bob.setAge(18);
+    User john = new User();
+    john.setId(16L);
+    john.setLogin("J");
+    john.setPassword("John2005");
+    john.setAge(22);
+    new StorageDaoImpl().add(marry);
+    new StorageDaoImpl().add(bob);
+    new StorageDaoImpl().add(john);
+  }
 
+  @BeforeEach
+  void setupNewUser() {
+    System.out.println("BeforeEach");
+    user = new User();
+    user.setId((long) nubmber);
+    user.setLogin("userLogin" + nubmber);
+    user.setPassword("abcdef" + nubmber);
+    user.setAge(18 + nubmber % 50);
+    nubmber++;
+  }
 
+  @Test
+  public void userIsNullThrowMessage() {
+    Exception exception = generateException(null);
+    Assert.assertEquals("Incorrect entry about user", exception.getMessage());
+  }
+
+  @Test
+  public void loginIsNullThrowMessage() {
+    user.setLogin(null);
+    Exception exception = generateException(user);
+    Assert.assertEquals("User's login or password is absent", exception.getMessage());
+  }
+
+  @Test
+  public void passwordIsNullThrowMessage() {
+    user.setPassword(null);
+    Exception exception = generateException(user);
+    Assert.assertEquals("User's login or password is absent", exception.getMessage());
+  }
+
+  @Test
+  public void passwordIsShortThrowMessage() {
+    user.setPassword("3");
+    Exception exception = generateException(user);
+    Assert.assertEquals("User's password is very short", exception.getMessage());
+  }
+
+  @Test
+  public void ageIsEmptyThrowMassege(){
+    user.setAge(null);
+    Exception exception = generateException(user);
+    Assert.assertEquals("User's age is empty", exception.getMessage());
+
+  }
+
+  private Exception generateException(User user) {
+    Exception exception = new Exception();
+    try {
+      registrationService.register(user);
+    } catch (Exception exc) {
+      exception = exc;
+    }
+    return exception;
+  }
 }
