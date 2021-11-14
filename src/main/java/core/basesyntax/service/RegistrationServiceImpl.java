26600d685d38d5828e49fc228bb4f2661f97cb14
Sergii Svystun <sergiisvystun@gmail.com>
2021-11-14 11:54:20 +0200

    initial commit

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,11 +1,26 @@
 package core.basesyntax.service;
 
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
+import java.util.Objects;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MINIMUM_ALLOWED_AGE = 18;
+    private static final int PASSWORD_MINIMUM_LENGTH = 6;
+    private StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null
+                || user.getAge() == null
+                || user.getPassword() == null
+                || user.getAge() < MINIMUM_ALLOWED_AGE
+                || user.getPassword().length() < PASSWORD_MINIMUM_LENGTH
+                || !Objects.equals(storageDao.get(user.getLogin()), null)) {
+            throw new RuntimeException("Input data is not valid");
+        }
+        storageDao.add(user);
+        return storageDao.get(user.getLogin());
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,79 @@
+package core.basesyntax.service;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+    private static StorageDao storageDao;
+    User user = new User();
+
+    @BeforeAll
+    public static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        storageDao = new StorageDaoImpl();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        Storage.people.clear();
+        user.setLogin("qwertyu");
+        user.setPassword("asdfghj");
+        user.setAge(25);
+    }
+
+    @Test
+    void register_nullAge_notOk() {
+        user.setAge(null);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_nullLogin_notOk() {
+        user.setLogin(null);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_nullPassword_notOk() {
+        user.setPassword(null);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_shortPassword_notOk() {
+        user.setPassword("zxcv");
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_smallAge_notOk() {
+        user.setAge(15);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_nullUser_notOk() {
+        user = null;
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_firstUserRegistration_Ok(){
+        User firstActualUser = registrationService.register(user);
+        int expected = 1;
+        assertEquals(expected, storageDao.get(firstActualUser.getLogin()).getId());
+    }
+
+    @Test
+    void register_userAlreadyExist_notOk() {
+        User firstActualUser = registrationService.register(user);
+        assertThrows(RuntimeException.class, () -> registrationService.register(firstActualUser));
+    }
+}
