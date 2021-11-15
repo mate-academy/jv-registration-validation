cf8f70738878aa4f39c54698d93003943ba212dd
Yaroslav Kavun <yka40znp@gmail.com>
2021-11-15 15:10:33 +0200

    incapsulated people

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,7 +1,5 @@
 package core.basesyntax.service;
 
-import static core.basesyntax.db.Storage.people;
-
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
@@ -16,14 +14,14 @@ public class RegistrationServiceImpl implements RegistrationService {
                 || user.getAge() == null) {
             throw new RuntimeException("The information are not valid, can't register user.");
         }
-        if (people.contains(storageDao.get(user.getLogin()))) {
-            throw new RuntimeException("User with that login already exists.");
-        }
         if (user.getAge() < 18) {
             throw new RuntimeException("You need to be more than 18 years of age.");
         }
         if (user.getPassword().length() < 6) {
             throw new RuntimeException("Your password need to be at least 6 symbols long.");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User with that login already exists.");
         }
         return storageDao.add(user);
     }
