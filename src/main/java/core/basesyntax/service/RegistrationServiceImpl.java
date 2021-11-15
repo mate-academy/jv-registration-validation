6aaca8c1d41bdacc83022b8407958342ba2c1607
Natalia Vidzivashets <nata.vidzivashets@gmail.com>
2021-11-15 20:50:48 +0200

    implemented task jv-registration-validation

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,11 +1,25 @@
 package core.basesyntax.service;
 
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    static final int MIN_AGE = 18;
+    static final int MIN_COUNT_CHARACTER = 6;
+    static final String EMPTY_LINE = "";
+
+    private StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null || user.getAge() < MIN_AGE || user.getAge() == null
+                || user.getPassword() == null || user.getPassword().length() < MIN_COUNT_CHARACTER
+                || user.getPassword().equals(EMPTY_LINE) || user.getLogin().equals(EMPTY_LINE)
+                || user.getLogin() == null || storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("Date is wrong!");
+        }
+        storageDao.add(user);
+        return user;
     }
 }
