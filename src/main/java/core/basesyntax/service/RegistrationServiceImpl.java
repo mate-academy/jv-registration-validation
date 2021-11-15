812f3199970d052217724fd2050a79da655df4d5
Anatolii Huryk <tolik.mario96@gmail.com>
2021-11-15 20:11:06 +0200

    created junit

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,11 +1,23 @@
 package core.basesyntax.service;
 
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MINIMUM_AGE = 18;
+    private static final int MINIMUM_PASSWORD = 6;
+    private StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getAge() < MINIMUM_AGE || user.getAge() == null
+                || user.getPassword().length() < MINIMUM_PASSWORD
+                || user.getPassword() == null
+                || user.getLogin() == null
+                || storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("");
+        }
+        return storageDao.add(user);
     }
 }
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,8 +1,66 @@
 package core.basesyntax;
 
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 /**
  * Feel free to remove this class and create your own.
  */
 public class HelloWorldTest {
+    private User user = new User();
+    private RegistrationService registration = new RegistrationServiceImpl();
 
+    @BeforeEach
+    public void setUp() throws Exception {
+        user.setAge(19);
+        user.setLogin("Login");
+        user.setPassword("Password");
+    }
+
+    @Test
+    public void userNull_NotOk() {
+        assertThrows(RuntimeException.class, () -> registration.register(null));
+    }
+
+    @Test
+    public void passwordNull_NotOk() {
+        user.setPassword(null);
+        assertThrows(RuntimeException.class, () -> registration.register(user));
+    }
+
+    @Test
+    public void ageNull_NotOk() {
+        user.setAge(null);
+        assertThrows(RuntimeException.class, () -> registration.register(user));
+    }
+
+    @Test
+    public void loginNull_NotOk() {
+        user.setLogin(null);
+        assertThrows(RuntimeException.class, () -> registration.register(user));
+    }
+
+    @Test
+    public void invalidAge_NotOk() {
+        user.setAge(17);
+        assertThrows(RuntimeException.class, () -> registration.register(user));
+    }
+
+    @Test
+    public void invalidPassword_NotOk() {
+        user.setPassword("notOk");
+        assertThrows(RuntimeException.class, () -> registration.register(user));
+    }
+
+    @Test
+    public void validUser_Ok() {
+        User expected = user;
+        User actual = registration.register(user);
+        assertEquals(expected, actual);
+    }
 }
