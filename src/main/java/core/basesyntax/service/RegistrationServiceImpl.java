b5154019ace0a133417ec2d2116badba37b7a3d0
VitaliiMarchenko <vitalbka.01@gmail.com>
2021-11-15 20:05:40 +0200

    StorageDao moved to class fields

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -8,6 +8,7 @@ public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_AGE = 18;
     private static final int MAX_AGE = 120;
     private static final int MIN_PASSWORD_LENGTH = 6;
+    private StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
@@ -22,20 +23,16 @@ public class RegistrationServiceImpl implements RegistrationService {
             throw new RuntimeException("Incorrect data");
         }
 
-        StorageDao storageDao = new StorageDaoImpl();
-
         if (user.getAge() >= MIN_AGE
                 && user.getAge() <= MAX_AGE
                 && user.getPassword().length() >= MIN_PASSWORD_LENGTH
                 && userWithSameLoginExists(user) == false) {
-            storageDao.add(user);
-            return user;
+            return storageDao.add(user);
         }
         return null;
     }
 
     public boolean userWithSameLoginExists(User user) {
-        StorageDao storageDao = new StorageDaoImpl();
         if (storageDao.get(user.getLogin()) != null) {
             return true;
         }
