64e69f3c02848f38d04fe3c65b28058b849dfd75
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2021-11-15 13:22:46 +0200

    fixed naming

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -4,9 +4,9 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private static final int VALID_PASSWORD_LENGTH = 6;
-    private static final int VALID_LOGIN_LENGTH = 1;
-    private static final int VALID_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_LOGIN_LENGTH = 1;
+    private static final int MIN_AGE = 18;
     private final StorageDaoImpl storageDao = new StorageDaoImpl();
 
     @Override
@@ -17,9 +17,9 @@ public class RegistrationServiceImpl implements RegistrationService {
             throw new RuntimeException("Invalid data");
         }
         if (storageDao.get(user.getLogin()) == null
-                && user.getPassword().length() >= VALID_PASSWORD_LENGTH
-                && user.getAge() >= VALID_AGE
-                && user.getLogin().length() >= VALID_LOGIN_LENGTH) {
+                && user.getPassword().length() >= MIN_PASSWORD_LENGTH
+                && user.getAge() >= MIN_AGE
+                && user.getLogin().length() >= MIN_LOGIN_LENGTH) {
             return storageDao.add(user);
         }
         throw new RuntimeException("Can't register user with these parameters");
