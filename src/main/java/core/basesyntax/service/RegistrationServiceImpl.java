0071a505cb7cf1dbd34a37faa745ec0b86b823ad
Kate Deshchinska <katerinka9636@gmail.com>
2021-11-15 18:58:58 +0200

    first commit

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,11 +1,21 @@
 package core.basesyntax.service;
 
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
+        StorageDao storageDao = new StorageDaoImpl();
+        if (user.getLogin() == null || user.getAge() == null ){
+            throw new RuntimeException("Login or Age are not correct")
+        }
+        if (storageDao.get(user.getLogin()) == null && user.getAge() >=18 && user.getPassword().length() >= 6) {
+            return storageDao.add(user);
+        }
         return null;
     }
 }
