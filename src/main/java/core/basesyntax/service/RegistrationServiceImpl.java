2a077be6917446c671f71cc824f2bd91b7ffbd0e
Oleg Shiriaev <mystidreamer@gmail.com>
2021-11-15 15:44:18 +0200

    more conditions in Register Server was added

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -7,14 +7,14 @@ import core.basesyntax.model.User;
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_AGE = 18;
     private static final int MIN_CHARS = 6;
-    private static final String ERROR_MESSAGE = "Invalid data, user not registered!";
+    private static final String ERROR_MESSAGE = "Invalid data, user was not registered!";
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        if (user.getLogin() == null
-                || user.getAge() < MIN_AGE
-                || user.getPassword().length() < MIN_CHARS
+        if (user == null || user.getLogin() == null
+                || user.getAge() == null || user.getAge() < MIN_AGE
+                || user.getPassword() == null || user.getPassword().length() < MIN_CHARS
                 || storageDao.get(user.getLogin()) != null) {
             throw new RuntimeException(ERROR_MESSAGE);
         }
