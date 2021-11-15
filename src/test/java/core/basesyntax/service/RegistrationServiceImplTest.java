4d6e24efc6fd99742c99b927ead4eb44acb92c24
Zachar Chorny <ufumoveldar@gmail.com>
2021-11-15 19:45:23 +0200

    edits made(1)

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 class RegistrationServiceImplTest {
     private static RegistrationServiceImpl service;
