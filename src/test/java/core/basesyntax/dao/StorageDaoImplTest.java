733157f3126cc85483ab19a8dd9903fd68ba6b33
Panchenko Volodymyr <volodymyr.panchenko147@gmail.com>
2021-11-16 16:58:39 +0200

    don't use *

--- src/test/java/core/basesyntax/dao/StorageDaoImplTest.java
+++ src/test/java/core/basesyntax/dao/StorageDaoImplTest.java
@@ -4,9 +4,12 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationService;
 import core.basesyntax.service.RegistrationServiceImpl;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class StorageDaoImplTest {
     private static RegistrationService registrationServise;
