b4ebf2428fbf968367f40a218128a5fb2d6ab3c5
Kate Deshchinska <katerinka9636@gmail.com>
2021-11-16 14:00:18 +0200

    fixed

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,18 +6,18 @@ import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_VALID_AGE = 18;
-    private static final int MIN_SYMBOLS_AT_PASSWORD = 6;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private static final int MIN_LOGIN_LENGTH = 1;
 
     @Override
-    public User register(User user) throws RuntimeException {
+    public User register(User user) {
         StorageDao storageDao = new StorageDaoImpl();
         if (user.getAge() != null
                 && user.getPassword() != null
                 && user.getLogin() != null
                 && storageDao.get(user.getLogin()) == null
                 && user.getAge() >= MIN_VALID_AGE
-                && user.getPassword().length() >= MIN_SYMBOLS_AT_PASSWORD
+                && user.getPassword().length() >= MIN_PASSWORD_LENGTH
                 && user.getLogin().length() >= MIN_LOGIN_LENGTH) {
             return storageDao.add(user);
         }
