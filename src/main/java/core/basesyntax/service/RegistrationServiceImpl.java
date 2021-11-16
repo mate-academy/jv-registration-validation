ae154a2736b0385ae3974c77a01261288d579340
Artik <Artik25942@gmail.com>
2021-11-16 13:56:23 +0200

    mvn clean BUILD SUCCESS

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -10,14 +10,14 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        if(user ==null || user.getLogin() == null || user.getPassword() == null
-                || user.getAge() ==null ){
+        if (user == null || user.getLogin() == null || user.getPassword() == null
+                || user.getAge() == null) {
             throw new RuntimeException("incorrect data");
         }
-        if (storageDao.get(user.getLogin()) != null){
+        if (storageDao.get(user.getLogin()) != null) {
             throw new RuntimeException("This user already exists");
         }
-        if (user.getAge() < MIN_AGE){
+        if (user.getAge() < MIN_AGE) {
             throw new RuntimeException("User age < " + MIN_AGE);
         }
         if (user.getPassword().length() < MIN_PASS_LENGTH) {
