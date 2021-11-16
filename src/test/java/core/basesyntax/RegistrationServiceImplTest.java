51393ecc14394239affee6173663876b40f775e7
Volodimir <vova.stepanenko2020@gmail.com>
2021-11-16 10:36:55 +0200

    delete check NPE, breaking logic testing

--- src/test/java/core/basesyntax/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -14,83 +14,100 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RegistrationServiceImplTest {
   private static RegistrationService registrationService;
-  private static User user1;
-  private static User user2;
+  private static User firstUser;
+  private static User secondUser;
 
   @BeforeAll
   static void beforeAll() {
     registrationService = new RegistrationServiceImpl();
-    user1 = new User();
-    user2 = new User();
+    firstUser = new User();
+    secondUser = new User();
   }
 
   @BeforeEach
   void setUp() {
     Storage.people.clear();
-    user1.setPassword("goodPassword");
-    user1.setAge(25);
-    user1.setLogin("userLogin");
-    user2.setPassword("passwordUser2");
-    user2.setLogin("loginUser2");
-    user2.setAge(27);
+    firstUser.setPassword("goodPassword");
+    firstUser.setAge(25);
+    firstUser.setLogin("userLogin");
+    secondUser.setPassword("passwordUser2");
+    secondUser.setLogin("loginUser2");
+    secondUser.setAge(27);
   }
 
   @Test
   void register_validUser_ok() {
-    User expected = user1;
-    User actual = registrationService.register(user1);
+    User expected = firstUser;
+    User actual = registrationService.register(firstUser);
     assertEquals(expected, actual);
   }
 
   @Test
   void register_manyUser_ok(){
-    assertEquals(registrationService.register(user1), user1);
-    assertEquals(registrationService.register(user2), user2);
+    User expectedFirst = firstUser;
+    User actualFirst = registrationService.register(firstUser);
+    User expectedSecond = secondUser;
+    User actualSecond = registrationService.register(secondUser);
+    assertEquals(expectedFirst, actualFirst);
+    assertEquals(expectedSecond, actualSecond);
   }
 
   @Test
   void register_samePassword_ok(){
-    user2.setPassword(user1.getPassword());
-    assertEquals(registrationService.register(user1), user1);
-    assertEquals(registrationService.register(user2), user2);
+    secondUser.setPassword(firstUser.getPassword());
+    User expectedFirst = firstUser;
+    User actualFirst = registrationService.register(firstUser);
+    User expectedSecond = secondUser;
+    User actualSecond = registrationService.register(secondUser);
+    assertEquals(actualFirst, expectedFirst);
+    assertEquals(actualSecond, expectedSecond);
   }
 
   @Test
   public void register_null_notOk() {
-    assertThrows(NullPointerException.class, () -> registrationService.register(null), "must be NullPointerException when user1==null");
+    assertThrows(RegistrationUserException.class, () -> registrationService.register(null));
   }
 
   @Test
   void register_invalidPassword_notOK() {
-    user1.setPassword("1");
-    assertThrows(RegistrationUserException.class, () -> registrationService.register(user1));
-    user1.setPassword(null);
-    assertThrows(NullPointerException.class, () -> registrationService.register(user1));
-    user1.setPassword("");
-    assertThrows(RegistrationUserException.class, () -> registrationService.register(user1));
+    firstUser.setPassword("12345");
+    assertThrows(RegistrationUserException.class, () -> registrationService.register(firstUser));
+  }
+
+  @Test
+  void register_nullPassword_notOk() {
+    firstUser.setPassword(null);
+    assertThrows(RegistrationUserException.class, () -> registrationService.register(firstUser));
   }
 
   @Test
   void register_invalidLogin_notOk() {
-    user1.setLogin("");
-    assertThrows(RegistrationUserException.class, () -> registrationService.register(user1));
-    user1.setPassword(null);
-    assertThrows(RegistrationUserException.class, () -> registrationService.register(user1));
-    user1.setPassword("");
-    assertThrows(RegistrationUserException.class, () -> registrationService.register(user1));
+    firstUser.setLogin("");
+    assertThrows(RegistrationUserException.class, () -> registrationService.register(firstUser));
   }
 
   @Test
-  void register_invalidAge() {
-    user1.setAge(null);
-    assertThrows(NullPointerException.class, () -> registrationService.register(user1));
-    user1.setAge(0);
-    assertThrows(RegistrationUserException.class, () -> registrationService.register(user1));
+  void register_nullLogin_notOk() {
+    firstUser.setPassword(null);
+    assertThrows(RegistrationUserException.class, () -> registrationService.register(firstUser));
+  }
+
+  @Test
+  void register_nullAge_notOk() {
+    firstUser.setAge(null);
+    assertThrows(NullPointerException.class, () -> registrationService.register(firstUser));
+
+  }
+
+  @Test
+  void register_lessMinAge_NotOk() {
+    firstUser.setAge(17);
+    assertThrows(RegistrationUserException.class, () -> registrationService.register(firstUser));
   }
 
   @Test
   void register_sameUser_notOk() {
-    registrationService.register(user1);
-    assertThrows(RegistrationUserException.class, () -> registrationService.register(user1));
+    registrationService.register(firstUser);
+    assertThrows(RegistrationUserException.class, () -> registrationService.register(firstUser));
   }
 }
