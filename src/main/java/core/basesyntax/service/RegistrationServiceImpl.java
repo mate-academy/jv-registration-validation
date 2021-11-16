0416f10a9137452ff9ff2e23c806b73025fc1b67
Eugene-exe <zhekacauselove@gmail.com>
2021-11-16 15:01:03 +0200

    Fix fixed solution

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,18 +5,19 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private static final int VALID_AGE = 18;
-    private static final int VALID_PASSWORD_LENGTH = 6;
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
         if (user == null
                 || user.getAge() == null
-                || user.getAge() < VALID_AGE
+                || user.getAge() < MIN_AGE
                 || user.getLogin() == null
                 || user.getPassword() == null
-                || user.getPassword().length() < VALID_PASSWORD_LENGTH) {
+                || user.getPassword().length() < MIN_PASSWORD_LENGTH
+                || storageDao.get(user.getLogin()) != null) {
             throw new RuntimeException("Invalid data entered. :(");
         }
         return storageDao.add(user);
