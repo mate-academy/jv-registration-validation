a7dada008821d895c4197d14b2ebd9e6b209b1f8
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-11-16 13:22:48 +0200

    some checkstyle changes

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,26 +5,30 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private final StorageDao storage = new StorageDaoImpl();
     private static final int MIN_PASSWORD_LENGTH = 6;
     private static final int MIN_AGE = 18;
+    private final StorageDao storage = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
         if (user == null) {
             throw new RuntimeException("User is null");
         }
-        if (user.getLogin() == null || user.getPassword() == null || user.getAge() == null || user.getId() == null) {
+        if (user.getLogin() == null || user.getPassword() == null
+                || user.getAge() == null || user.getId() == null) {
             throw new RuntimeException("User cannot have null states");
         }
         if (storage.get(user.getLogin()) != null) {
-            throw new RuntimeException("User with login " + user.getLogin() + " is already registered");
+            throw new RuntimeException("User with login " + user.getLogin()
+                    + " is already registered");
         }
         if (user.getAge() < MIN_AGE) {
-            throw new RuntimeException("User is too young, " + user.getAge() + " y.o.");
+            throw new RuntimeException("User is too young, " + user.getAge()
+                    + " y.o.");
         }
         if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
-            throw new RuntimeException("Password should be biggest or equals to 6 symbols");
+            throw new RuntimeException("Password should be biggest or"
+                    + " equals to 6 symbols");
         }
         return storage.add(user);
     }
