db054345d20084f95a752f532ba29e6f690fbde4
Yevheniy Savenkov <yevheniy.savenkov@gmail.com>
2021-11-16 17:20:08 +0200

    quickfix, added missing empty line

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class RegistrationServiceImplTest {
     static RegistrationService registrationService;
     static User firstUser;
