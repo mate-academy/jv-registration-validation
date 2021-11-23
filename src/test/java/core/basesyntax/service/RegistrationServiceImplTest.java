dcfb62f61819cae60082b3f276898e80fce7a33e
resci <vlad.evdockimov.062@gmail.com>
2021-11-23 08:38:44 +0200

    Update CI config

---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,15 @@
+package core.basesyntax.service;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    @Test
+    void containsRegistrationServiceImpl_Ok() {
+        Long id = ;
+
+    }
+
+    }
