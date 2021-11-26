2258874e98b08d0f433f6cf960272307a95c9db2
Oleh Mazurkov <o.mazurkov@gmail.com>
2021-11-26 20:00:01 +0200

    solving hw-10v2 "Registration validation"

--- src/test/java/core/basesyntax/dao/StorageDaoImplTest.java
+++ src/test/java/core/basesyntax/dao/StorageDaoImplTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StorageDaoImplTest {
-    private StorageDao storageDao = new StorageDaoImpl();
+    private final StorageDao storageDao = new StorageDaoImpl();
 
     @BeforeEach
     void setUp() {
