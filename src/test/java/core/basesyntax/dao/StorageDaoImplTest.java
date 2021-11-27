561d0b33d33843422f47a229614d61fe9f2dae7d
Oleh Mazurkov <o.mazurkov@gmail.com>
2021-11-27 02:24:22 +0200

    solving hw10

---
+++ src/test/java/core/basesyntax/dao/StorageDaoImplTest.java
@@ -0,0 +1,44 @@
+package core.basesyntax.dao;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class StorageDaoImplTest {
+    private final StorageDao storageDao = new StorageDaoImpl();
+
+    @BeforeEach
+    void setUp() {
+        for (User user : Storage.people) {
+            storageDao.add(new User(1L,"log000","ffffff",46));
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void userAgeLow18_NotOk() {
+        assertThrows(RuntimeException.class, () -> storageDao
+                .add(new User(1L,"gfgf", "ffffff",1)));
+    }
+
+    @Test
+    void suchLoginAlreadyExist_NotOk() {
+        storageDao.add(new User(1L,"user1","ffffff",56));
+        assertThrows(RuntimeException.class, () -> storageDao
+                .add(new User(1L,"user1","ffffff",26)));
+    }
+
+    @Test
+    void passwordLess6Charicters_NotOk() {
+        assertThrows(RuntimeException.class, () -> storageDao
+                .add(new User(1L,"user1","fff",26)));
+    }
+}
