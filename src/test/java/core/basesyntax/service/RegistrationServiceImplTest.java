78b39b40c8beabccf730c2859c0d2281b7a0833d
Oleh Mazurkov <o.mazurkov@gmail.com>
2021-11-27 16:35:18 +0200

    solving hw-10

---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,37 @@
+package core.basesyntax.service;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
+
+    @Test
+    void noRepeatedUserLogin() {
+        registrationService.register(new User(0L,"goodLogin_A","myPassword_A",56));
+        registrationService.register(new User(0L,"goodLogin_B","myPassword_A",56));
+        registrationService.register(new User(0L,"goodLogin_A","myPassword_B",57));
+        assertTrue(Storage.people.contains(new User(0L,"goodLogin_A","myPassword_A",56)));
+        assertTrue(Storage.people.contains(new User(0L,"goodLogin_B","myPassword_A",56)));
+        assertFalse(Storage.people.contains(new User(0L,"goodLogin_A","myPassword_B",57)));
+        System.out.println(Storage.people.size());
+    }
+
+    @Test
+    void userAgeHaveToMoreOrEqual18() {
+        registrationService.register(new User(0L,"goodLogin_C","myPassword_A",18));
+        registrationService.register(new User(0L,"goodLogin_D","myPassword_A",17));
+        assertTrue(Storage.people.contains(new User(0L,"goodLogin_C","myPassword_A",18)));
+        assertFalse(Storage.people.contains(new User(0L,"goodLogin_D","myPassword_A",17)));
+
+    }
+
+    @Test
+    void userPasswordHaveToAtLeast6Charecters() {
+        registrationService.register(new User(0L,"goodLoginF","myPas",18));
+        assertFalse(Storage.people.contains(new User(0L,"goodLoginF","myPas",18)));
+    }
+}
