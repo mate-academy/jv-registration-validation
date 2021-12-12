c0e01951baebdee4e74b51db3eb6e3030c6e197a
Serhii Pedchenko <djmolot@gmail.com>
2021-12-12 23:06:28 +0200

    added class RegistrationServiceImplTest.java with test methods

---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,54 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class RegistrationServiceImplTest {
+
+    private RegistrationService registrationService = new RegistrationServiceImpl();
+
+    @Test
+    void register_addExistingUser_notOk() {
+        User user1 = new User();
+        user1.setAge(23);
+        user1.setLogin("Chandra");
+        user1.setPassword("fjvn84n(nfn");
+        User registredUser = registrationService.register(user1);
+        assertThrows(RuntimeException.class, () -> {registrationService.register(registredUser);});
+    }
+
+    @Test
+    void register_nullValue_notOk() {
+        assertThrows(RuntimeException.class, () -> {registrationService.register(null);});
+    }
+
+    @Test
+    void register_ageUnder18_notOk() {
+        User user1 = new User();
+        user1.setAge(17);
+        user1.setLogin("Chandra");
+        user1.setPassword("fjvn84n(nfn");
+        assertThrows(RuntimeException.class, () -> {registrationService.register(user1);});
+    }
+
+    @Test
+    void register_passwordUnder6_notOk() {
+        User user1 = new User();
+        user1.setAge(19);
+        user1.setLogin("Sandra");
+        user1.setPassword("f(R4n");
+        assertThrows(RuntimeException.class, () -> {registrationService.register(user1);});
+    }
+
+    @Test
+    void register_validUser_ok() {
+        User user1 = new User();
+        user1.setAge(23);
+        user1.setLogin("Sandra");
+        user1.setPassword("fjvn84n(nfn");
+        assertNotNull(registrationService.register(user1));
+    }
+}
