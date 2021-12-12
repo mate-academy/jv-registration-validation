fdbf4ac564b98d1fb2d365f5fe5d23d4b7b9d188
Serhii Pedchenko <djmolot@gmail.com>
2021-12-12 23:02:46 +0200

    in method register() added condition user != null

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -8,7 +8,7 @@ public class RegistrationServiceImpl implements RegistrationService {
     @Override
     public User register(User user) {
         StorageDaoImpl storageDao = new StorageDaoImpl();
-        if (storageDao.get(user.getLogin()) == null
+        if (user != null && storageDao.get(user.getLogin()) == null
                 && user.getAge() >= 18 && user.getPassword().length() >= 6) {
             return storageDao.add(user);
         } else {
