8b844f2cb305cbe49d855d7aca289125a28f25b2
Serhii Pedchenko <djmolot@gmail.com>
2021-12-13 11:53:03 +0200

    removed redundant else{} block and throw placed after if{}

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -11,8 +11,7 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user != null && storageDao.get(user.getLogin()) == null
                 && user.getAge() >= 18 && user.getPassword().length() >= 6) {
             return storageDao.add(user);
-        } else {
-            throw new RuntimeException();
         }
+        throw new RuntimeException();
     }
 }
