68d9daa9bcc859264ce74cc1c78964b9ca03cdfb
Stanislav <stassonich@icloud.com>
2021-12-18 23:12:13 +0200

    Update CI config

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,15 +1,91 @@
 package core.basesyntax.service;
 
 
-import org.junit.jupiter.api.Test;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class RegistrationServiceImplTest {
+
+    private static RegistrationService registrationService;
+    private User bob;
+
+    @BeforeClass
+    public static void beforeClass() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @Before
+    public void setUp() {
+        Storage.people.clear();
+        bob = new User();
+        bob.setLogin("123");
+        bob.setAge(19);
+        bob.setPassword("1234567");
+    }
     @Test
-    void containsRegistrationServiceImpl_Ok() {
-        Long id = ;
-
+    public void register_userWithExistingLogin_notOk() {
+        registrationService.register(bob);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(bob);
+        });
     }
 
+    @Test
+    public void register_emptyLogin_notOk() {
+        bob.setLogin("");
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(bob);
+        });
     }
+    @Test
+    public void register_nullLogin_notOk() {
+        bob.setLogin(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(bob);
+        });
+    }
+
+    @Test
+    public void register_ageUnder18_notOk() {
+        bob.setAge(17);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(bob);
+        });
+    }
+    @Test
+    public void register_nullAge_notOk() {
+        bob.setAge(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(bob);
+        });
+    }
+
+    @Test
+    public void register_passwordUnder6Characters_notOk() {
+        bob.setPassword("12345");
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(bob);
+        });
+    }
+
+    @Test
+    public void register_nullPassword_notOk() {
+        bob.setPassword(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(bob);
+        });
+    }
+
+    @Test
+    public void register_validUser_ok() {
+        User expected = bob;
+        User actual = registrationService.register(bob);
+        assertEquals(expected, actual);
+    }
+}
