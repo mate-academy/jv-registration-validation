ef17a8a270f4c7bd26bd7c3023a77904c6bc55ad
Maksym Pokras <mxpokras@gmail.com>
2022-01-10 12:40:57 +0200

    login test methods completed

--- src/test/java/core/basesyntax/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -5,26 +5,30 @@ import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationService;
 import core.basesyntax.service.RegistrationServiceImpl;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Feel free to remove this class and create your own.
  */
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestMethodOrder(OrderAnnotation.class)
 public class RegistrationServiceTest {
     private static RegistrationService registrationService;
-    private String loginNull;
-    private String loginEmpty = "";
-    private String loginDefault = "login";
-    private String passNull;
-    private String passShort = "short";
-    private String passDefault = "password";
-    private Integer ageNull;
-    private Integer ageSmall = 17;
-    private Integer ageBarely = 18;
-    private Integer ageMax = Integer.MAX_VALUE;
-    private Integer ageDefault = 25;
+    private final String loginNull = null;
+    private final String loginEmpty = "";
+    private final String loginDefault = "login";
+    private final String passNull = null;
+    private final String passShort = "short";
+    private final String passDefault = "password";
+    private final Integer ageNull = null;
+    private final Integer ageSmall = 17;
+    private final Integer ageBarely = 18;
+    private final Integer ageMax = Integer.MAX_VALUE;
+    private final Integer ageMin = Integer.MIN_VALUE;
+    private final Integer ageDefault = 25;
     long id = 0L; // there is no restrictions for id field
 
 
@@ -42,8 +46,11 @@ public class RegistrationServiceTest {
     @DisplayName("Correct login handling")
     @Order(1)
     void login_Ok() {
-        User user = getUser(id, loginNull, passDefault, ageDefault);
-        assertThrows(RuntimeException.class, ()->registrationService.register(user), "login = null");
+        User expected = getUser(id, loginDefault, passDefault, ageDefault);
+        User actual = registrationService.register(expected);
+        assertEquals(expected, actual, "register method return value");
+        assertEquals(1, Storage.people.size(), "Storage size");
+        assertEquals(expected, Storage.people.get(0), "added to storage user");
     }
 
     @Test
@@ -51,7 +58,8 @@ public class RegistrationServiceTest {
     @Order(2)
     void loginNull_NotOk() {
         User user = getUser(id, loginNull, passDefault, ageDefault);
-        assertThrows(RuntimeException.class, ()->registrationService.register(user), "login = null");
+        assertThrows(RuntimeException.class, ()->registrationService.register(user),
+                "login couldn't be null");
     }
 
     @Test
@@ -73,16 +81,17 @@ public class RegistrationServiceTest {
     @DisplayName("Different users handling")
     @Order(4)
     void loginDiff_Ok() {
-        User expected = getUser(id, loginDefault, passDefault, ageDefault);
-        registrationService.register(expected);
-        User sameLoginUser = getUser(1L, loginDefault, passDefault, ageDefault);
-        assertAll(
-                ()->assertThrows(RuntimeException.class,
-                        ()->registrationService.register(sameLoginUser), "login = null"),
-
-                ()->assertEquals(1, Storage.people.size(), "Storage size"),
-                ()->assertEquals(expected, Storage.people.get(0), "Added to Storage user"));
+        User expected_0 = getUser(id, loginDefault, passDefault, ageDefault);
+        registrationService.register(expected_0);
+        User expected_1 = getUser(1L, loginDefault, passDefault, ageDefault);
+        User actual_1 = registrationService.register(expected_1);
+        assertEquals(expected_1, actual_1, "register method return value");
+        assertEquals(2, Storage.people.size(), "Storage size");
+        assertEquals(expected_0, Storage.people.get(0), "Storage user 0");
+        assertEquals(expected_1, Storage.people.get(1), "Storage user 1");
     }
+
+
 
 
     private static User getUser(Long id, String login, String password, Integer age) {
