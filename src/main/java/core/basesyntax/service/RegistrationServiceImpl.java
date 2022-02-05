33c85af7612d7d12b0ec000d027a51ed755d3460
LinkClink <shekaqwe@gmail.com>
2022-02-05 09:03:23 +0200

    fix (constants)

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,19 +5,21 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int REQUIRE_PASSWORD_MIN_LENGTH = 6;
+    private static final int REQUIRE_MIN_AGE = 18;
     private StorageDaoImpl storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        for (User person : Storage.people) { /* Check login already exits */
+        for (User person : Storage.people) {
             if (person.getLogin().equals(user.getLogin())) {
                 throw new RuntimeException("Sorry, this login already exit");
             }
         }
-        if (user.getAge() < 18) { /* Check age */
+        if (user.getAge() < REQUIRE_MIN_AGE) {
             throw new RuntimeException("Sorry, age should be the same 18 y. old.");
         }
-        if (user.getPassword().length() < 6) { /* Check password length */
+        if (user.getPassword().length() < REQUIRE_PASSWORD_MIN_LENGTH) {
             throw new RuntimeException(
                     "Sorry, your password is too small." + user.getPassword().length());
         }
