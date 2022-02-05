67cf92669feb10a4f75cc1366a7b5a7518729c21
Sasha Bachynskyi <allfred12@gmail.com>
2022-02-05 09:46:34 +0200

    fixed checkstyle error

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,7 +1,6 @@
 package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDaoImpl;
-import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
