fb3f19866d0668ff3217fd4767cd4f55069acefc
LinkClink <shekaqwe@gmail.com>
2022-02-07 11:35:53 +0200

    fix

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,20 +1,17 @@
 package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDaoImpl;
-import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int REQUIRE_PASSWORD_MIN_LENGTH = 6;
     private static final int REQUIRE_MIN_AGE = 18;
-    private StorageDaoImpl storageDao = new StorageDaoImpl();
+    private final StorageDaoImpl storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        for (User person : Storage.people) {
-            if (person.getLogin().equals(user.getLogin())) {
-                throw new RuntimeException("Sorry, this login already exit");
-            }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("Sorry, this login already exit");
         }
         if (user.getAge() < REQUIRE_MIN_AGE) {
             throw new RuntimeException("Sorry, age should be the same 18 y. old.");
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,72 +1,56 @@
 package core.basesyntax.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class RegistrationServiceImplTest {
 
     private RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
-    private User userWithBadAge = new User();
-    private User userWithBadPassword = new User();
     private User goodUser = new User();
-    private User sameUser = new User();
 
     @BeforeEach
     public void setUp() {
         goodUser.setAge(18);
         goodUser.setLogin("login1245");
         goodUser.setPassword("123456");
-        sameUser.setAge(22);
-        sameUser.setLogin("login4626");
-        sameUser.setPassword("12463578");
-        userWithBadAge.setAge(17);
-        userWithBadAge.setLogin("login12453");
-        userWithBadAge.setPassword("123456");
-        userWithBadPassword.setAge(22);
-        userWithBadPassword.setLogin("login12356");
-        userWithBadPassword.setPassword("123");
     }
 
     @Test
-    public void registerGoodUserOK() {
+    public void register_goodUser_Ok() {
         User excepted = goodUser;
         User actual = registrationService.register(excepted);
         assertEquals(excepted, actual, "Sorry user is not equals");
     }
 
     @Test
-    public void registerUserWithBadAge() {
-        User excepted = userWithBadAge;
+    public void register_userWithBadAge_notOk() {
+        User excepted = goodUser;
+        excepted.setAge(17);
         assertThrows(RuntimeException.class,
-                () -> {
-                    registrationService.register(excepted);
-                });
+                () -> registrationService.register(excepted));
     }
 
     @Test
-    public void registerUserWithBadPassword() {
-        User actual = userWithBadPassword;
+    public void register_userWithBadPassword_notOk() {
+        User actual = goodUser;
+        actual.setPassword("12345");
         assertThrows(RuntimeException.class,
-                () -> {
-                    registrationService.register(actual);
-                });
+                () -> registrationService.register(actual));
     }
 
     @Test
-    public void userLoginAlreadyExit() {
+    public void register_userLoginAlreadyExit_notOk() {
         Storage.people.clear();
-        User one = sameUser;
-        User two = sameUser;
+        User one = goodUser;
+        User two = goodUser;
         registrationService.register(one);
         assertThrows(RuntimeException.class,
-                () -> {
-                    registrationService.register(two);
-                });
+                () -> registrationService.register(two));
         int excepted = 1;
         int actual = Storage.people.size();
         assertEquals(excepted, actual, "Sorry storage size after adding is bad" + actual);
