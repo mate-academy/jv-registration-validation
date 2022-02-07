04ebef4437147b0b9d04401e7c68134695b9e745
Eugene <shekaqwe@gmail.com>
2022-02-07 11:39:51 +0200

    Update RegistrationServiceImplTest.java

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-
     private RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
     private User goodUser = new User();
 
