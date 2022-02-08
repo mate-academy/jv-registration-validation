c26c7cbc25cc3134de5056e677d67635b23a43d6
Ostap Danyliv <danyliv.ostap@gmail.com>
2022-02-08 14:55:36 +0200

    First commit JUnit

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,11 +1,35 @@
 package core.basesyntax.service;
 
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    public static final int MINIMUM_PASSWORD_LENGTH = 6;
+    public static final int MINIMUM_USER_AGE = 18;
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RuntimeException("You tried to register user with NULL");
+        }
+        if (user.getAge() < MINIMUM_USER_AGE || user.getAge() == null) {
+            throw new RuntimeException("You can not register user who is less than "
+                + MINIMUM_USER_AGE);
+        }
+        if (user.getPassword().length() < MINIMUM_PASSWORD_LENGTH) {
+            throw new RuntimeException("Your password is too short. It should be at least "
+                + MINIMUM_PASSWORD_LENGTH + "symbols.");
+        }
+        if (user.getLogin() != null) {
+            for (User person : Storage.people) {
+                if (user.getLogin().toLowerCase().equals(person.getLogin().toLowerCase())) {
+                    throw new RuntimeException("The login you want to use is already taken");
+                }
+            }
+        } else {
+            throw new RuntimeException("Login is null");
+        }
+        Storage.people.add(user);
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,143 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+
+    private static RegistrationService registrationService;
+    private User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        User bob = new User();
+        bob.setPassword("123456");
+        bob.setId(1L);
+        bob.setAge(24);
+        bob.setLogin("Bob");
+    }
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setPassword("123456");
+        user.setId(1L);
+        user.setAge(24);
+        user.setLogin("Alice");
+    }
+
+    @Test
+    void register_UserIsNull_NotOk() {
+        try {
+            registrationService.register(null);
+        } catch (RuntimeException e) {
+            return;
+        }
+        fail();
+    }
+
+    @Test
+        void register_UserIsLess18_NotOk() {
+        user.setAge(15);
+        try {
+            registrationService.register(user);
+        } catch (RuntimeException e) {
+            return;
+        }
+        fail();
+    }
+
+    @Test
+  void register_PasswordIsLess6Letters_NotOk() {
+        user.setPassword("12345");
+        try {
+            registrationService.register(user);
+        } catch (RuntimeException e) {
+            return;
+        }
+        fail();
+    }
+
+    @Test
+  void register_AddExistingLoginUser_NotOk() {
+        Storage.people.add(user);
+        user.setLogin("Bob");
+        try {
+            registrationService.register(user);
+        } catch (RuntimeException e) {
+            return;
+        }
+        fail();
+    }
+
+    @Test
+  void register_AddExistingLoginUserUpperCase_NotOk() {
+        Storage.people.add(user);
+        user.setLogin("bOb");
+        try {
+            registrationService.register(user);
+        } catch (RuntimeException e) {
+            return;
+        }
+        fail();
+    }
+
+    @Test
+  void registerAdd_CorrectInput_Ok() {
+        registrationService.register(user);
+        assertTrue(Storage.people.contains(user));
+    }
+
+    @Test
+  void register_LoginValueIsNull_NotOK() {
+        user.setLogin(null);
+        try {
+            registrationService.register(user);
+        } catch (RuntimeException e) {
+            return;
+        }
+        fail();
+    }
+
+    @Test
+  void register_AgeIsNull_NotOk() {
+        user.setAge(null);
+        try {
+            registrationService.register(user);
+        } catch (RuntimeException e) {
+            return;
+        }
+        fail();
+    }
+
+    @Test
+  void register_UserNegativeAge_NotOk() {
+        user.setAge(-26);
+        try {
+            registrationService.register(user);
+        } catch (RuntimeException e) {
+            return;
+        }
+        fail("User age can not be negative");
+    }
+
+    @Test
+  void register_UserAgeOver18_Ok() {
+        user.setAge(39);
+        registrationService.register(user);
+        assertTrue(Storage.people.contains(user));
+    }
+
+    @AfterEach
+  void tearDown() {
+        Storage.people.clear();
+    }
+}
