81229f14716788ebefe2aee0cb970c4ef0213c7b
Anton Ignatenko <87337009+antonignatenko@users.noreply.github.com>
2022-02-09 16:07:01 +0200

    implemented all tests and methods passed checkstyle tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -7,9 +7,8 @@ import core.basesyntax.model.User;
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_AGE = 18;
     private static final int MAX_AGE = 100;
+    private static final int PASS_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
-
-    private int passwordLength = 6;
 
     @Override
     public User register(User user) {
@@ -22,46 +21,40 @@ public class RegistrationServiceImpl implements RegistrationService {
         return storageDao.add(user);
     }
 
-    private boolean checksUserForNull(User user) {
+    private void checksUserForNull(User user) {
         if (user == null) {
             throw new RuntimeException("user can't be null");
         }
-        return true;
     }
 
-    private boolean checksIfAgeTooYoung(User user) {
+    private void checksIfAgeTooYoung(User user) {
         if (user.getAge() < MIN_AGE) {
             throw new RuntimeException("Sorry age must be above 18 and below 100");
         }
-        return true;
     }
 
-    private boolean checkcIfAgeTooOld(User user) {
+    private void checkcIfAgeTooOld(User user) {
         if (user.getAge() > MAX_AGE) {
             throw new RuntimeException("Sorry age must be above 18 and below 100");
         }
-        return true;
     }
 
-    private boolean checksPasswordLength(User user) {
-        if (user.getPassword().length() < passwordLength) {
+    private void checksPasswordLength(User user) {
+        if (user.getPassword().length() < PASS_LENGTH) {
             throw new RuntimeException("Password is too short");
         }
-        return true;
     }
 
-    private boolean checksUserNullFields(User user) {
+    private void checksUserNullFields(User user) {
         if (user.getLogin() == null || user.getPassword() == null || user.getAge() == null) {
             throw new RuntimeException("User hasn't got any information");
         }
-        return true;
     }
 
-    private boolean checksValidLogin(User user) {
+    private void checksValidLogin(User user) {
         if (storageDao.get(user.getLogin()) != null) {
             throw new RuntimeException("Sorry such login already exists");
         }
-        return true;
     }
 
 }
