f0fba69e92cd2e278835b17905f65163b9546dcc
Oleksandr Sydorskyi <oleksandrsydorskyi@gmail.com>
2022-02-12 20:04:08 +0200

    Oleksandr Sydorskyi # jv-registration-validation 12.02.2022

---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,77 @@
+package core.basesyntax.service;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+    private static User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        user = new User();
+    }
+
+    @BeforeEach
+    void setUp() {
+        Storage.people.clear();
+        user.setId(1L);
+        user.setAge(21);
+        user.setLogin("validLogin");
+        user.setPassword("validPassword");
+    }
+
+    @Test
+    void register_validUser_ok() {
+        User expected = user;
+        User actual = registrationService.register(user);
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void register_userExist_notOk() {
+        registrationService.register(user);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_usersAgeLessThan18_notOk() {
+        user.setAge(17);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_usersPasswordLessThan6symbols_notOk() {
+        user.setPassword("12345");
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_emptyLogin_notOk() {
+        user.setLogin("");
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_emptyPassword_notOk() {
+        user.setPassword("");
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_nullLogin_notOk() {
+        user.setLogin(null);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_nullPassword_notOk() {
+        user.setPassword(null);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+}
