e5920eadbd6f9fa20d29445243ef23a5e48431f8
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-03-18 08:04:03 +0100

    added conditions to register User

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,11 +1,56 @@
 package core.basesyntax.service;
 
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MAX_AGE = 60;
+    private static final int MIN_SYMBOL = 7;
+    private StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        validUser(user);
+        validAge(user);
+        validPassword(user);
+        validLogin(user);
+        validNull(user);
+        return storageDao.add(user);
     }
+
+    private void validNull(User user) {
+        if (user == null) {
+            throw new RuntimeException("Empty data is not allowed.");
+        }
+    }
+
+    private void validUser(User user) {
+        if (user == null) {
+            throw new RuntimeException("User does not exist.");
+        }
+    }
+
+    private void validAge(User user) {
+        if (user.getAge() < MIN_AGE || user.getAge() > MAX_AGE) {
+            throw new RuntimeException("User can not be younger 18 and older 60!");
+        }
+    }
+
+    private void validPassword(User user) {
+        if (user.getPassword() == null || user.getPassword().length() < MIN_SYMBOL) {
+            throw new RuntimeException("Password must has 7 characters or more.");
+        }
+    }
+
+    private void validLogin(User user) {
+        if (user.getLogin() == null) {
+            throw new RuntimeException("Enter login. :)");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("Such login already exist");
+        }
+    }
+
 }
