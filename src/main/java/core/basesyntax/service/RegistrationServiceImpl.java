4ef02cdb082e4974c19de73fa2b5b660fe810256
Mykhailo Nikolov <mikenikolov.work@gmail.com>
2022-04-04 22:33:16 +0300

    feat: add registration

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,11 +1,39 @@
 package core.basesyntax.service;
 
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getAge() == null
+                || user.getLogin() == null
+                || user.getPassword() == null
+                || user.getId() == null) {
+            throw new NullPointerException();
+        }
+        if (!checkUserExists(user.getLogin())
+                && checkAge(user.getAge())
+                && checkPassword(user.getPassword())) {
+            Storage.people.add(user);
+            return user;
+        }
+        throw new RuntimeException("Invalid input data");
+    }
+
+    private boolean checkUserExists(String login) {
+        return storageDao.get(login) != null;
+    }
+
+    private boolean checkAge(int age) {
+        return age >= 18;
+    }
+
+    private boolean checkPassword(String password) {
+        return password.length() >= 6;
     }
 }
