ec91cc26b23401acff858e65363fef55f3d60445
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-04-07 19:05:14 +0200

    RegistrationServiceImpl-was-created-and-tested

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,11 +1,25 @@
 package core.basesyntax.service;
 
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private StorageDaoImpl storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() != null && user.getLogin().length() > 0) {
+            for (User person : Storage.people) {
+                if (person.getLogin().equals(user.getLogin())) {
+                    throw new RuntimeException("User login is already exist");
+                }
+            }
+            if (user.getAge() >= 18 && user.getPassword().length() >= 6) {
+                storageDao.add(user);
+                return user;
+            }
+        }
+        throw new RuntimeException("User's data is wrong");
     }
 }
