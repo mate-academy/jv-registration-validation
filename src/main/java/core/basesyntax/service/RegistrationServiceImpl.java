ea6b875ea51483725f8c44ae5247049959a0bd52
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-04-11 12:32:19 +0200

    register-method-was-changed

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,23 +1,24 @@
 package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDaoImpl;
-import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private StorageDaoImpl storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        if (user.getLogin() != null && user.getLogin().length() > 0) {
-            for (User person : Storage.people) {
-                if (person.getLogin().equals(user.getLogin())) {
-                    throw new RuntimeException("User login is already exist");
+        if (user != null) {
+            if (user.getLogin() != null && user.getLogin().length() > 0
+                    && user.getAge() != null && user.getPassword() != null) {
+                User findUser = storageDao.get(user.getLogin());
+                if (findUser == null && user.getAge() >= MIN_AGE
+                        && user.getPassword().length() >= MIN_PASSWORD_LENGTH) {
+                    storageDao.add(user);
+                    return user;
                 }
-            }
-            if (user.getAge() >= 18 && user.getPassword().length() >= 6) {
-                storageDao.add(user);
-                return user;
             }
         }
         throw new RuntimeException("User's data is wrong");
