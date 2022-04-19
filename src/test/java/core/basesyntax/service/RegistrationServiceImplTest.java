459b360e003f3b46a9a46842dc1108ea16e807bb
GerasimovTA-92 <olexandr.gerasimov.02@gmail.com>
2022-04-19 21:45:14 +0300

    added StorageTest

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,20 +1,23 @@
 package core.basesyntax.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
+    private static RegistrationService registration;
     private User user;
-    private RegistrationService registration = new RegistrationServiceImpl();
+
+    @BeforeAll
+    static void setUpFirst() {
+        registration = new RegistrationServiceImpl();
+    }
 
     @BeforeEach
-    void setup() {
+    void setUpEach() {
         user = new User("gerasimov", "wevrewvre", 18);
     }
 
@@ -35,7 +38,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void register_nullValue_NotOk() {
+    void register_nullUser_NotOk() {
         assertThrows(RuntimeException.class, () -> {
             registration.register(null);
         });
@@ -47,29 +50,5 @@ class RegistrationServiceImplTest {
         assertThrows(RuntimeException.class, () -> {
             registration.register(user);
         });
-    }
-
-    @Test
-    void register_NewValue_Ok() {
-        user.setLogin("ivanov");
-        registration.register(user);
-        assertEquals(1, Storage.people.size());
-    }
-
-    @Test
-    void sizeOfStorage_Ok() {
-        assertEquals(0, Storage.people.size());
-    }
-
-    @Test
-    void get_UserId_Ok() {
-        registration.register(user);
-        assertEquals(user.getLogin(), Storage.people.get(Math.toIntExact(user.getId()) - 1)
-                .getLogin());
-    }
-
-    @AfterEach
-    void clearStorage() {
-        Storage.people.clear();
     }
 }
