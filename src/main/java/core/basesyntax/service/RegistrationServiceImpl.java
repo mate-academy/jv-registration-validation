3e5df2afb8b74ba67247dea03e1a919d5e9c3b2f
Oleksander <ghjwtyrj25@gmail.com>
2022-04-20 15:51:46 +0300

    Made an registration class and tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,11 +1,28 @@
 package core.basesyntax.service;
 
+import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD = 6;
 
     @Override
     public User register(User user) {
-        return null;
+        StorageDaoImpl storage = new StorageDaoImpl();
+        if (user.getPassword() == null || user.getPassword().length() < MIN_PASSWORD) {
+            throw new RuntimeException("Password isn't correct!");
+        }
+        if (storage.get(user.getLogin()) != null) {
+            throw new RuntimeException("Login already exist!");
+        }
+        if (user == null) {
+            throw new RuntimeException("User is null!");
+        }
+        if (user.getAge() == null || user.getAge() < MIN_AGE) {
+            throw new RuntimeException("Age isn't correct!");
+        }
+        storage.add(user);
+        return user;
     }
 }
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,8 +1,65 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorldTest {
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
 
+public class HelloWorldTest {
+    private static RegistrationServiceImpl registration;
+    private static StorageDaoImpl storage;
+
+    @BeforeAll
+    static void beforeAll() {
+        registration = new RegistrationServiceImpl();
+        storage = new StorageDaoImpl();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        Storage.people.clear();
+    }
+
+    @Test
+    public void addingNormalUser() {
+        User user1 = new User("Login", "123456", 18);
+        User user2 = new User("Login2", "123456", 18);
+        registration.register(user1);
+        registration.register(user2);
+
+        Assert.assertEquals(user2, storage.get(user2.getLogin()));
+    }
+
+    @Test
+    public void checkIfPassword_NotOK() {
+        User user = new User("Login", "12345", 18);
+        Assert.assertThrows(RuntimeException.class, ()
+                -> registration.register(user));
+    }
+
+    @Test
+    public void checkIfAge_NotOK() {
+        User user = new User("Login", "123457", 17);
+        Assert.assertThrows(RuntimeException.class, ()
+                -> registration.register(user));
+    }
+
+    @Test
+    public void checkIfLoginAlreadyExist() {
+        User user1 = new User("Login", "123457", 18);
+        User user2 = new User("Login", "7354787", 19);
+        registration.register(user1);
+        Assert.assertThrows(RuntimeException.class, ()
+                -> registration.register(user2));
+    }
+
+    @Test
+    public void addingNullUser() {
+        Assert.assertThrows(RuntimeException.class, ()
+                -> registration.register(null));
+    }
 }
