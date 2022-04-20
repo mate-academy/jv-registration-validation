0702a99868adbad0df05fec7151ca63f293bc74f
Dmytro Koliada <dmitrikstar@gmail.com>
2022-04-20 13:33:23 +0300

    fix logic method and delete garbage

--- src/test/java/core/basesyntax/service/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceTest.java
@@ -6,12 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceTest {
-    private static final int MIN_AGE = 18;
-    private static final int MIN_LENGTH = 6;
     private static RegistrationService registrationService;
     private User userWithNotValidPassword;
     private User userValid;
@@ -25,28 +22,28 @@ class RegistrationServiceTest {
         registrationService = new RegistrationServiceImpl();
     }
 
-    @BeforeEach
-    void setUp() {
-        userWithNullLogin = new User(null, "q3ff5e", 23);
-        userWithNullPassword = new User("jack", null, 43);
-        userWithNotValidPassword = new User("john", "49Gк5", 18);
-        userValid = new User("alice", "fOre8dfg", 35);
-        userWithInvalidAge = new User("rey", "dUrmd8sI", 17);
-        userWithEqualLogin = new User("alice", "425gfh89", 110);
-        Storage.people.clear();
-    }
-
     @Test
-    void loginIsNull_NotOk() {
-        assertThrows(NullPointerException.class, () ->
-                registrationService.register(userWithNullLogin));
+    void userIsValid_Ok() {
+        userValid = new User("alice", "fOre8dfg", 35);
+        User registeredUser = registrationService.register(userValid);
+        assertTrue(Storage.people.contains(registeredUser));
     }
 
     @Test
     void loginIsUsed_NotOk() {
-        Storage.people.add(userValid);
+        userWithEqualLogin = new User("alice", "425gfh89", 110);
+        userValid = new User("alice", "fOre8dfg", 35);
+        Storage.people.clear();
+        registrationService.register(userValid);
         assertThrows(RuntimeException.class, () ->
                 registrationService.register(userWithEqualLogin));
+    }
+
+    @Test
+    void loginIsNull_NotOk() {
+        userWithNullLogin = new User(null, "q3ff5e", 23);
+        assertThrows(NullPointerException.class, () ->
+                registrationService.register(userWithNullLogin));
     }
 
     @Test
@@ -57,27 +54,21 @@ class RegistrationServiceTest {
 
     @Test
     void passwordIsNotValid_NotOk() {
+        userWithNotValidPassword = new User("john", "49Gк5", 18);
         assertThrows(RuntimeException.class, () ->
                 registrationService.register(userWithNotValidPassword));
     }
 
     @Test
     void invalidAge_notOk() {
+        userWithInvalidAge = new User("rey", "dUrmd8sI", 17);
         assertThrows(RuntimeException.class, () ->
                 registrationService.register(userWithInvalidAge));
     }
 
     @Test
-    void userIsValid_Ok() {
-        User registeredUser = registrationService.register(userValid);
-        assertTrue(Storage.people.contains(registeredUser));
-        assertTrue(registeredUser.getAge() >= MIN_AGE);
-        assertTrue(registeredUser.getPassword().length()
-                >= MIN_LENGTH);
-    }
-
-    @Test
     void passwordIsNull_notOk() {
+        userWithNullPassword = new User("jack", null, 43);
         assertThrows(NullPointerException.class, () ->
                 registrationService.register(userWithNullPassword));
     }
