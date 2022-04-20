51a7c19ba3190301519d4bcd8214847a1805c71e
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-04-20 16:06:36 +0300

    writted first test and implemented remove() method

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,11 +1,32 @@
 package core.basesyntax.service;
 
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASS_LENGTH = 6;
 
     @Override
     public User register(User user) {
-        return null;
+        StorageDao storageDao = new StorageDaoImpl();
+        if (user.getLogin() == null || user.getPassword() == null) {
+            throw new RuntimeException();
+        }
+        if (storageDao.get(user.getLogin()) == null
+                || storageDao.get(user.getLogin()).equals(user.getLogin())) {
+            if (user.getAge() >= MIN_AGE) {
+                if (user.getPassword().length() >= MIN_PASS_LENGTH) {
+                    storageDao.add(user);
+                    return user;
+                }
+                throw new RuntimeException("Min pass length " + MIN_PASS_LENGTH
+                        + ", but was "
+                        + user.getPassword().length());
+            }
+            throw new RuntimeException("The age must be at least 18 but was " + user.getAge());
+        }
+        throw new RuntimeException("This login is already exist");
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,72 @@
+package core.basesyntax.service;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+    private static User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        user = new User();
+    }
+
+    @BeforeEach
+    void setUp() {
+        user.setAge(18);
+        user.setLogin("Loki");
+        user.setPassword("123456");
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void register_CorrectUser_Ok() {
+        User registeredUser = registrationService.register(user);
+        Assertions.assertTrue(Storage.people.contains(registeredUser));
+        Assertions.assertTrue(registeredUser.getAge() >= 18);
+        Assertions.assertTrue(registeredUser.getPassword().length() >= 6);
+    }
+
+    @Test
+    void register_LowAge_NotOk() {
+        user.setAge(12);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_SameLogin_NotOk() {
+        User firstUser = user;
+        firstUser.setLogin("Loki");
+        registrationService.register(firstUser);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_TooShortPass_NotOk() {
+        user.setPassword("12345");
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_NullLogin_NotOk() {
+        user.setLogin(null);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_NullPass_NotOk() {
+        user.setPassword(null);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+}
