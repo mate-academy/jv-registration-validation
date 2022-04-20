cacdf534c1803ccaf049f6b36ba30e3fd457ce59
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-04-20 16:48:45 +0300

    junit-testing-homework-solution-added

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -11,26 +11,17 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        if (user.getLogin() == null) {
-            throw new RuntimeException("Your login can not be null. Please, enter a valid login");
-        } else if (user.getLogin().isEmpty()) {
-            throw new RuntimeException("Your login can not be empty. Please, enter a valid login");
-        } else if (user.getLogin().isBlank()) {
-            throw new RuntimeException("Your login can not contain blank spaces only."
-                    + " Please, enter a valid login");
+        if (user.getLogin() == null || user.getLogin().isEmpty() || user.getLogin().isBlank()
+                || user.getAge() == null) {
+            throw new RuntimeException("Please, enter a valid login");
         } else if (storageDao.get(user.getLogin()) != null) {
             throw new RuntimeException("This user is already exist");
-        } else if (user.getAge() == null) {
-            throw new RuntimeException("The age can not be null. Please, enter your age");
         } else if (user.getAge() < MINIMAL_AGE) {
             throw new RuntimeException("You should be at least " + MINIMAL_AGE
                    + " years old to register");
-        } else if (user.getPassword() == null) {
-            throw new RuntimeException("Your password can not be mull."
-                    + " Please, enter a valid password");
-        } else if (user.getPassword().isBlank()) {
-            throw new RuntimeException("Your password can not contain blank spaces only."
-                    + " Please, enter a valid password");
+        } else if (user.getPassword() == null || user.getPassword().isBlank()
+                || user.getPassword().isEmpty()) {
+            throw new RuntimeException("Please, enter a valid password");
         } else if (user.getPassword().length() < MINIMAL_PASSWORD_LENGTH) {
             throw new RuntimeException("Your password should have at least 6 characters."
                     + " Please, enter a valid password");
