fe69b880cfa6becff4143b52bdcbe2c6b33f6c4b
Yaroslav Lapovets <lapovetsyaroslav@gmail.com>
2022-04-20 16:07:02 +0300

    Added homework solution

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,11 +1,28 @@
 package core.basesyntax.service;
 
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MINIMUM_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private StorageDao storage = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null || user.getLogin() == null || user.getPassword() == null || user.getAge() == null) {
+            throw new RuntimeException("Incorrect data");
+        }
+        if (user.getAge() < MINIMUM_AGE) {
+            throw new RuntimeException("The user must be" + MINIMUM_AGE + " years of age or older");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password length must be at least " + MIN_PASSWORD_LENGTH);
+        }
+        if (storage.get(user.getLogin()) != null) {
+            throw new RuntimeException("Login already exists");
+        }
+        return storage.add(user);
     }
 }
