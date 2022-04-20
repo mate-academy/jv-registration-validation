6bde4454302995351e8b85d8c5d287b7da16bb6d
Gigawatt94 <101740223+Gigawatt94@users.noreply.github.com>
2022-04-20 16:09:50 +0300

    change number of tests

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -25,12 +25,12 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void register_userGetAgeIsOK() {
+    void register_userIsOK() {
         user.setAge(19);
         user.setPassword("123456");
         user.setLogin("pasis");
         User actual = registrationService.register(user);
-        assertEquals(user.getAge(), actual.getAge());
+        assertEquals(user, actual);
     }
 
     @Test
@@ -43,15 +43,6 @@ class RegistrationServiceImplTest {
     void register_userGetAgeIsNull() {
         user.setAge(null);
         assertThrows(RuntimeException.class, () -> registrationService.register(user));
-    }
-
-    @Test
-    void register_userGetPasswordIsOK() {
-        user.setPassword("1234567");
-        user.setAge(18);
-        user.setLogin("pasis");
-        User actual = registrationService.register(user);
-        assertEquals(user.getPassword(), actual.getPassword());
     }
 
     @Test
@@ -81,13 +72,5 @@ class RegistrationServiceImplTest {
         user2.setAge(19);
         user2.setPassword("123456");
         assertThrows(RuntimeException.class, () -> registrationService.register(user2));
-    }
-
-    @Test
-    void register_usergetloginIsOk() {
-        user.setLogin("ghjkl");
-        user.setAge(19);
-        user.setPassword("123456");
-        assertEquals(user, registrationService.register(user));
     }
 }
