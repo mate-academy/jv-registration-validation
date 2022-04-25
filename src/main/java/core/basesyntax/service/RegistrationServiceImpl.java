5e0a8ac1e658bca698c76a7dec9b162bb42f40b2
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-04-25 13:27:37 +0300

    added local constant variables

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -3,21 +3,22 @@ package core.basesyntax.service;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
-import org.jetbrains.annotations.NotNull;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
-    public User register(@NotNull User user) {
-        if (user.getAge() == null || user.getLogin() == null || user.getPassword() == null) {
-            // throw new RuntimeException("You must not leave any fields empty");
-            throw new NullPointerException();
+    public User register(User user) {
+        int ageLimit = 18;
+        int minLengthPassword = 6;
+        if (user == null || user.getAge() == null || user.getLogin() == null
+                || user.getPassword() == null) {
+            throw new RuntimeException("You must not leave any fields empty");
         }
-        if (user.getAge() < 18) {
+        if (user.getAge() < ageLimit) {
             throw new RuntimeException("Your age must be greater than eighteen");
         }
-        if (user.getPassword().length() < 6) {
+        if (user.getPassword().length() < minLengthPassword) {
             throw new RuntimeException("Your password must have more than 6 characters");
         }
         return storageDao.add(user);
