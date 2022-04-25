426c4b0c8540cbc84be9da03d57eb28d1a59036a
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-04-25 21:05:04 +0300

    local variables where moved to clas fields and condition user == null moved to separate if

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,13 +5,16 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int ageLimit = 18;
+    private static final int minLengthPassword = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        int ageLimit = 18;
-        int minLengthPassword = 6;
-        if (user == null || user.getAge() == null || user.getLogin() == null
+        if (user == null) {
+            throw new NullPointerException();
+        }
+        if (user.getAge() == null || user.getLogin() == null
                 || user.getPassword() == null) {
             throw new RuntimeException("You must not leave any fields empty");
         }
