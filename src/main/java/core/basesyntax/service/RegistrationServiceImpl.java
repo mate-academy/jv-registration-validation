96670be317f4232b76a500128df1228e24fe3021
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-04-26 14:08:16 +0300

    changed class as required

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,23 +6,23 @@ import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_AGE = 18;
-    private static final int PASSWORD_LENGTH = 6;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
         if (user == null) {
-            throw new RuntimeException();
+            throw new RuntimeException("This user doesn't exist");
         }
         if (user.getAge() == null || user.getLogin() == null
                 || user.getPassword() == null) {
             throw new RuntimeException("You must not leave any fields empty");
         }
         if (user.getAge() < MIN_AGE) {
-            throw new RuntimeException("Your age must be greater than eighteen");
+            throw new RuntimeException("Your age must be greater than " + MIN_AGE);
         }
-        if (user.getPassword().length() < PASSWORD_LENGTH) {
-            throw new RuntimeException("Your password must have more than 6 characters");
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Your password must have more than " + MIN_PASSWORD_LENGTH + " characters");
         }
         return storageDao.add(user);
     }
