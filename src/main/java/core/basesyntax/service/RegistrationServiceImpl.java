de3b72451a53074840d68e8a9d1bb1956c8be031
Anastasia <anastasian817@gmail.com>
2022-05-08 10:23:05 +0300

    added fixes after review

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,6 +5,8 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int PASSWORD_LENGTH = 6;
+    private static final int ALLOWED_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -12,17 +14,15 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user.getLogin() == null || user.getPassword() == null || user.getAge() == null) {
             throw new RuntimeException("Data can't be null");
         }
-
         if (storageDao.get(user.getLogin()) != null) {
             throw new RuntimeException("This login already exists");
         }
-
-        if (user.getPassword().length() < 6) {
-            throw new RuntimeException("Password can't be less than 6 characters");
+        if (user.getPassword().length() < PASSWORD_LENGTH) {
+            throw new RuntimeException("Password can't be less than " + PASSWORD_LENGTH
+                    + " characters");
         }
-
         if (user.getAge() < 18) {
-            throw new RuntimeException("Age under 18 is not allowed");
+            throw new RuntimeException("Age under " + ALLOWED_AGE + " is not allowed");
         }
         storageDao.add(user);
         return user;
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -14,31 +14,33 @@ class RegistrationServiceImplTest {
 
     @BeforeAll
     static void beforeAll() {
-        registrationService = new RegistrationServiceImpl() {
-        };
+        registrationService = new RegistrationServiceImpl();
     }
 
     @BeforeEach
     void setUp() {
-        user = new User("user", "111111", 22);
+        user = new User();
+        user.setLogin("user");
+        user.setAge(18);
+        user.setPassword("111111");
     }
 
     @Test
-    void inputValidData_Ok() {
+    void register_inputValidData_Ok() {
         assertEquals(user,registrationService.register(user));
     }
 
     @Test
-    void userWithThisLoginAlreadyExists_notOk() {
+    void register_userWithThisLoginAlreadyExists_notOk() {
         user.setLogin("firstUser");
         registrationService.register(user);
         Throwable exception = assertThrows(RuntimeException.class, () ->
-                registrationService.register(new User(user.getLogin(), "password", 22)));
+                registrationService.register(user));
         assertEquals("This login already exists", exception.getLocalizedMessage());
     }
 
     @Test
-    void inputPasswordIsLessThanSixCharacters_notOk() {
+    void register_inputPasswordIsLessThanSixCharacters_notOk() {
         user.setLogin("secondUser");
         user.setPassword("111");
         Throwable exception = assertThrows(RuntimeException.class, () ->
@@ -47,7 +49,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void inputAgeLessThanEighteen_notOk() {
+    void register_inputAgeLessThanEighteen_notOk() {
         user.setLogin("thirdUser");
         user.setAge(12);
         Throwable exception = assertThrows(RuntimeException.class, () ->
@@ -56,7 +58,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void inputPasswordIsNull_notOk() {
+    void register_inputPasswordIsNull_notOk() {
         user.setPassword(null);
         Throwable exception = assertThrows(RuntimeException.class, () ->
                 registrationService.register(user));
@@ -64,7 +66,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void inputAgeIsNull_notOk() {
+    void register_inputAgeIsNull_notOk() {
         user.setAge(null);
         Throwable exception = assertThrows(RuntimeException.class, () ->
                 registrationService.register(user));
@@ -72,7 +74,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void inputLoginIsNull_notOk() {
+    void register_inputLoginIsNull_notOk() {
         user.setLogin(null);
         Throwable exception = assertThrows(RuntimeException.class, () ->
                 registrationService.register(user));
