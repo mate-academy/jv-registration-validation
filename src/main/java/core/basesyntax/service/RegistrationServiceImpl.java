b1f4082d839414c8e4fc26ccddf41da741c44fd3
Anastasia <anastasian817@gmail.com>
2022-05-08 18:18:03 +0300

    added fixes after review

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,8 +5,8 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private static final int PASSWORD_LENGTH = 6;
-    private static final int ALLOWED_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_ALLOWED_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -17,12 +17,12 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (storageDao.get(user.getLogin()) != null) {
             throw new RuntimeException("This login already exists");
         }
-        if (user.getPassword().length() < PASSWORD_LENGTH) {
-            throw new RuntimeException("Password can't be less than " + PASSWORD_LENGTH
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password can't be less than " + MIN_PASSWORD_LENGTH
                     + " characters");
         }
-        if (user.getAge() < 18) {
-            throw new RuntimeException("Age under " + ALLOWED_AGE + " is not allowed");
+        if (user.getAge() < MIN_ALLOWED_AGE) {
+            throw new RuntimeException("Age under " + MIN_ALLOWED_AGE + " is not allowed");
         }
         storageDao.add(user);
         return user;
