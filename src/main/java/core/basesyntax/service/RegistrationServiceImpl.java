05b3cdc7b9362421cc3c7a65ab62ae4f4820a6c5
Ivan Kharchenko <pro100detki@gmail.com>
2022-05-15 23:44:33 +0300

    Changed access modifiers

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,8 +5,8 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    public static final int MIN_USER_VALID_AGE = 18;
-    public static final int MIN_USER_PASSWORD_LENGTH = 6;
+    private static final int MIN_USER_VALID_AGE = 18;
+    private static final int MIN_USER_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
