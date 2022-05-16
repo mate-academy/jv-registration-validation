2f3a5503a38c238ed1c4d3745489e90dc16b908a
Ivan Kharchenko <pro100detki@gmail.com>
2022-05-16 17:00:12 +0300

    1. Changed hardcore user age and password length values in thrown errors. 2. Changed the names of the methods to be more informative and conform to the conventions. 3. Updated the assignment to avoid conflict with GitHub.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,32 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_USER_AGE = 18;
+    private static final int MIN_USER_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null
+                || user.getLogin() == null
+                || user.getPassword() == null
+                || user.getAge() == null) {
+            throw new RuntimeException("Field can't be NULL");
+        }
+        if (user.getLogin().isEmpty()
+                || user.getPassword().isEmpty()) {
+            throw new RuntimeException("Field can't be EMPTY");
+        }
+        if (user.getPassword().length() < MIN_USER_PASSWORD_LENGTH) {
+            throw new RuntimeException("Min user password must be "
+                    + MIN_USER_PASSWORD_LENGTH + " characters");
+        }
+        if (user.getAge() < MIN_USER_AGE) {
+            throw new RuntimeException("Minimum user age must be " + MIN_USER_AGE + " years old");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User's login is already exist");
+        }
+        return storageDao.add(user);
     }
 }
