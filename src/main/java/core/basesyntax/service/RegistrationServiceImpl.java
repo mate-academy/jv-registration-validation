c0fd548e79e92261c60a41483fae0368560b8325
Vitalii Smahlenko <smaglenko@gmail.com>
2022-05-18 11:19:14 +0300

    Thank you a lot!

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -15,15 +15,19 @@ public class RegistrationServiceImpl implements RegistrationService {
                 || user.getPassword() == null) {
             throw new RuntimeException("The user or one of the fields "
                     + "can't be null");
-        } else if (user.getAge() < MIN_AGE) {
+        }
+        if (user.getAge() < MIN_AGE) {
             throw new RuntimeException("The user must be at least "
                     + "18 years old!");
-        } else if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
             throw new RuntimeException("The user must have a password "
                     + "of at least 6 characters");
-        } else if (user.equals(storageDao.get(user.getLogin()))) {
+        }
+        if (user.equals(storageDao.get(user.getLogin()))) {
             throw new RuntimeException("A user with this login already exists");
-        } else if (user.getLogin().length() < 1) {
+        }
+        if (user.getLogin().length() < 1) {
             throw new RuntimeException("Username can't be empty");
         }
         storageDao.add(user);
