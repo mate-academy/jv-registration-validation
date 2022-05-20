50faf90f1de97d0628c95a6cec475e7f59fd1bc0
Vladimir Chuvashov <vova5179@gmail.com>
2022-05-20 19:05:12 +0300

    added jv-registration-validation implementation

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,35 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private final StorageDao storageDao = new StorageDaoImpl();
+    private static final StorageDao storageDao = new StorageDaoImpl();
+    private static final int ADULT_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RuntimeException("User's object is null");
+        }
+        if (user.getAge() == null || user.getPassword() == null
+                                  || user.getLogin() == null) {
+            throw new RuntimeException("User's age, login or password cannot be null!");
+        }
+        if (user.getAge() < ADULT_AGE) {
+            throw new RuntimeException("Age must be more than " + ADULT_AGE);
+        }
+        if (user.getPassword().isBlank()) {
+            throw new RuntimeException("Password field is blank");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password to short. Must be more "
+                    + MIN_PASSWORD_LENGTH + " characters");
+        }
+        if (user.getLogin().isBlank()) {
+            throw new RuntimeException("Login field is blank");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("This login already exist");
+        }
+        return storageDao.add(user);
     }
 }
