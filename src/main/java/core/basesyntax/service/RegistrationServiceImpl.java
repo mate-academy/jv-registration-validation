0121467f6a3836a41c49fd8c8e04e86b9eda1ec3
agash <alexgash8@gmail.com>
2022-05-22 09:52:33 +0300

    beautiful tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,26 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_AGE = 18;
+    private static final int UNBELIEVABLE_AGE = 110;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null || user.getLogin() == null || user.getLogin() == "") {
+            throw new RuntimeException("user is null or login is null/empty");
+        }
+        if (user.getAge() == null || user.getAge() < MIN_AGE || user.getAge() > UNBELIEVABLE_AGE) {
+            throw new RuntimeException("or age null or user is too young or age is unbelievable");
+        }
+        if (user.getPassword() == null || user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("user's password is too short");
+        }
+        if (storageDao.get(user.getLogin()) == null) {
+            return storageDao.add(user);
+        } else {
+            throw new RuntimeException("Such a user already exists with this login");
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,97 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static StorageDao testStorage;
+    private static User testUser;
+    private static RegistrationService testRegistration;
+
+    @BeforeAll
+    static void atStartOnce() {
+        testStorage = new StorageDaoImpl();
+        testRegistration = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        testUser = new User();
+        testUser.setAge(100);
+        testUser.setLogin("abcdef");
+        testUser.setPassword("1234567");
+    }
+
+    @AfterEach
+    void clearStorage() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void register_gotValidNotExistingUser_Ok() {
+        assertEquals(testUser, testRegistration.register(testUser));
+    }
+
+    @Test
+    void register_gotExistingUsername_notOk() {
+        testRegistration.register(testUser);
+        assertThrows(RuntimeException.class, () -> testRegistration.register(testUser));
+    }
+
+    @Test
+    void register_gotNullUser_notOk() {
+        testUser = null;
+        assertThrows(RuntimeException.class, () -> testRegistration.register(testUser));
+    }
+
+    @Test
+    void register_gotNullLogin_notOk() {
+        testUser.setLogin(null);
+        assertThrows(RuntimeException.class, () -> testRegistration.register(testUser));
+    }
+
+    @Test
+    void register_gotEmptyLogin_notOk() {
+        testUser.setLogin("");
+        assertThrows(RuntimeException.class, () -> testRegistration.register(testUser));
+    }
+
+    @Test
+    void register_gotSmallAge_notOk() {
+        testUser.setAge(-100);
+        assertThrows(RuntimeException.class, () -> testRegistration.register(testUser));
+    }
+
+    @Test
+    void register_gotNullAge_notOk() {
+        testUser.setAge(null);
+        assertThrows(RuntimeException.class, () -> testRegistration.register(testUser));
+    }
+
+    @Test
+    void register_gotTooOldUser_notOk() {
+        testUser.setAge(111);
+        assertThrows(RuntimeException.class, () -> testRegistration.register(testUser));
+    }
+
+    @Test
+    void register_gotEmptyPassword_notOk() {
+        testUser.setPassword("");
+        assertThrows(RuntimeException.class, () -> testRegistration.register(testUser));
+    }
+
+    @Test
+    void register_gotTooShortPassword_notOk() {
+        testUser.setPassword("1234");
+        assertThrows(RuntimeException.class, () -> testRegistration.register(testUser));
+    }
+}
