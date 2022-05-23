3a81adb1559ce54c9503346f93c9fe145715f802
agash <alexgash8@gmail.com>
2022-05-23 11:14:10 +0300

    one more iteration

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,6 +5,8 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -12,10 +14,10 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user == null || user.getLogin() == null || user.getLogin().isEmpty()) {
             throw new RuntimeException("user is null or login is null/empty");
         }
-        if (user.getAge() == null || user.getAge() < 18) {
+        if (user.getAge() == null || user.getAge() < MIN_AGE) {
             throw new RuntimeException("or age null or user is too young or age is unbelievable");
         }
-        if (user.getPassword() == null || user.getPassword().length() < 6) {
+        if (user.getPassword() == null || user.getPassword().length() < MIN_PASSWORD_LENGTH) {
             throw new RuntimeException("user's password is too short");
         }
         if (storageDao.get(user.getLogin()) != null) {
