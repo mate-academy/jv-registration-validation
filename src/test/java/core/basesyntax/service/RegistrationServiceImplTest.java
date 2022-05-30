02fee0065897e33f806867210c7e6c7bc95acda9
Mykola Dubich <nikolay19888@hotmail.com>
2022-05-30 18:06:38 +0300

    Fixed after review

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -36,81 +36,71 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_nullUser_NotOk() {
-        assertThrows(NullPointerException.class, () -> {
-            registrationService.register(null);
-        });
+        assertThrows(NullPointerException.class, () ->
+            registrationService.register(null));
     }
 
     @Test
     void register_nullUserLogin_NotOK() {
         firstUser.setLogin(null);
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(firstUser);
-        });
+        assertThrows(RuntimeException.class, () ->
+            registrationService.register(firstUser));
     }
 
     @Test
     void register_onlyWhitespaceInLogin_NotOk() {
         firstUser.setLogin("      ");
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(firstUser);
-        });
+        assertThrows(RuntimeException.class, () ->
+            registrationService.register(firstUser));
     }
 
     @Test
     void register_nullUserPassword_NotOK() {
         firstUser.setPassword(null);
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(firstUser);
-        });
+        assertThrows(RuntimeException.class, () ->
+            registrationService.register(firstUser));
     }
 
     @Test
     void register_notValidPassword_NotOK() {
         firstUser.setPassword("12345");
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(firstUser);
-        });
+        assertThrows(RuntimeException.class, () ->
+            registrationService.register(firstUser));
     }
 
     @Test
     void register_onlyWhitespaceInPassword_NotOk() {
         firstUser.setPassword("      ");
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(firstUser);
-        });
+        assertThrows(RuntimeException.class, () ->
+            registrationService.register(firstUser));
     }
 
     @Test
     void register_nullUserAge_NotOK() {
         firstUser.setAge(null);
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(firstUser);
-        });
+        assertThrows(RuntimeException.class, () ->
+            registrationService.register(firstUser));
     }
 
     @Test
     void register_notValidAge_NotOK() {
         firstUser.setAge(NOT_VALID_AGE);
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(firstUser);
-        });
+        assertThrows(RuntimeException.class, () ->
+            registrationService.register(firstUser));
     }
 
     @Test
     void register_negativeAge_NotOk() {
         firstUser.setAge(NEGATIVE_NUMBER);
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(firstUser);
-        });
+        assertThrows(RuntimeException.class, () ->
+            registrationService.register(firstUser));
     }
 
     @Test
     void register_registerSameUser_NotOk() {
         registrationService.register(firstUser);
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(firstUser);
-        });
+        assertThrows(RuntimeException.class, () ->
+            registrationService.register(firstUser));
     }
 
     @Test
@@ -129,15 +119,14 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_multipleSameRegistrationWithSame_NotOk() {
-        Storage.people.add(firstUser);
+        registrationService.register(firstUser);
         User secondUser = new User();
         secondUser.setLogin(DEFAULT_LOGIN + "_" + 1);
         secondUser.setPassword(DEFAULT_PASSWORD + "_" + 1);
         secondUser.setAge(DEFAULT_AGE + 1);
-        Storage.people.add(secondUser);
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(firstUser);
-        });
+        registrationService.register(secondUser);
+        assertThrows(RuntimeException.class, () ->
+            registrationService.register(firstUser));
     }
 
     @AfterEach
