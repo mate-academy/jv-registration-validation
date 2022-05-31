85170215897bebf255a008ccdb0c2f9ad2b6ac75
gdp <gdp@zigzag.ints.net>
2022-05-31 11:27:45 +0300

    refactoring after mentor review (1)
    - rename variable register to registrationService in class RegistrationServiceImplTest;
    - change "_Ok" to "_ok" in method's names in class RegistrationServiceImplTest.

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -11,12 +11,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    private static RegistrationServiceImpl register;
+    private static RegistrationServiceImpl registrationService;
     private User user;
 
     @BeforeAll
     static void beforeAll() {
-        register = new RegistrationServiceImpl();
+        registrationService = new RegistrationServiceImpl();
     }
 
     @BeforeEach
@@ -30,40 +30,40 @@ class RegistrationServiceImplTest {
     @Test
     void register_loginNull_notOk() {
         user.setLogin(null);
-        assertThrows(NullPointerException.class, () -> register.register(user));
+        assertThrows(NullPointerException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_loginAlreadyExist_notOk() {
-        register.register(user);
+        registrationService.register(user);
         User newUser = new User();
         newUser.setLogin(user.getLogin());
         newUser.setPassword(user.getPassword());
         newUser.setAge(user.getAge());
-        assertThrows(RuntimeException.class, () -> register.register(newUser));
+        assertThrows(RuntimeException.class, () -> registrationService.register(newUser));
     }
 
     @Test
     void register_loginEmpty_notOk() {
         user.setLogin("");
-        assertThrows(RuntimeException.class, () -> register.register(user));
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_login_Ok() {
-        User actual = register.register(user);
+    void register_login_ok() {
+        User actual = registrationService.register(user);
         assertEquals(user, actual);
     }
 
     @Test
-    void register_loginMultiplyRegistration_Ok() {
+    void register_loginMultiplyRegistration_ok() {
         int numberUsers = 5;
         for (int i = 0; i < numberUsers; i++) {
             User newUser = new User();
             newUser.setLogin("new-user-" + i);
             newUser.setPassword("test-password" + i);
             newUser.setAge(RegistrationServiceImpl.AGE_MINIMUM_FOR_REGISTRATION + i);
-            User actual = register.register(newUser);
+            registrationService.register(newUser);
         }
         assertEquals(numberUsers, Storage.people.size());
     }
@@ -71,57 +71,57 @@ class RegistrationServiceImplTest {
     @Test
     void register_ageNull_notOk() {
         user.setAge(null);
-        assertThrows(NullPointerException.class, () -> register.register(user));
+        assertThrows(NullPointerException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_ageUnderRestriction_notOk() {
         user.setAge(RegistrationServiceImpl.AGE_MINIMUM_FOR_REGISTRATION - 1);
-        assertThrows(RuntimeException.class, () -> register.register(user));
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_ageNegative_notOk() {
         user.setAge(-1);
-        assertThrows(RuntimeException.class, () -> register.register(user));
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_ageRestrictionEqual_Ok() {
+    void register_ageRestrictionEqual_ok() {
         user.setAge(RegistrationServiceImpl.AGE_MINIMUM_FOR_REGISTRATION);
-        User actual = register.register(user);
+        User actual = registrationService.register(user);
         assertEquals(user, actual);
     }
 
     @Test
-    void register_ageAboveRestriction_Ok() {
+    void register_ageAboveRestriction_ok() {
         user.setAge(RegistrationServiceImpl.AGE_MINIMUM_FOR_REGISTRATION + 1);
-        User actual = register.register(user);
+        User actual = registrationService.register(user);
         assertEquals(user, actual);
     }
 
     @Test
     void register_passwordNull_notOk() {
         user.setPassword(null);
-        assertThrows(NullPointerException.class, () -> register.register(user));
+        assertThrows(NullPointerException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_passwordEmpty_notOk() {
         user.setPassword("");
-        assertThrows(RuntimeException.class, () -> register.register(user));
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_passwordShort_notOk() {
         user.setPassword("*".repeat(RegistrationServiceImpl.PASSWORD_MINIMUM_LENGTH - 1));
-        assertThrows(RuntimeException.class, () -> register.register(user));
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_password_Ok() {
+    void register_password_ok() {
         user.setPassword("*".repeat(RegistrationServiceImpl.PASSWORD_MINIMUM_LENGTH));
-        User actual = register.register(user);
+        User actual = registrationService.register(user);
         assertEquals(user, actual);
     }
 
