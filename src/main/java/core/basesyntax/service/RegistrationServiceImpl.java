0932163caa6d2cabad798bc739f9360df32624a8
Konstantin Miroshnichenko <bigfishnotbad@gmail.com>
2022-06-01 18:44:00 +0300

    added seven tests and implemented method register

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,30 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final String LOGIN_EXIST = "user with such login already exists";
+    private static final String LOGIN_IS_NULL = "user's login is null";
+    private static final String TOO_YOUNG = "the user must first grow up a little, age less than 18";
+    private static final String LOGIN_HAS_INVALID_SYMBOL = "login has at least one invalid symbols";
+    private static final String LOGIN_HAS_LESS_THAT_SIX_SYMBOLS = "login too short, must be at least six symbols";
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null) {
+            throw new RuntimeException(LOGIN_IS_NULL);
+        }
+        if (user.getAge() < 18) {
+            throw new RuntimeException(TOO_YOUNG);
+        }
+        if (user.getLogin().length() < 6) {
+            throw new RuntimeException(LOGIN_HAS_LESS_THAT_SIX_SYMBOLS);
+        }
+        if (user.getLogin().replaceAll("\\W+", "").length() - user.getLogin().length() != 0) {
+            throw new RuntimeException(LOGIN_HAS_INVALID_SYMBOL);
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException(LOGIN_EXIST);
+        }
+        return storageDao.add(user);
     }
 }
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,8 +1,79 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorldTest {
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloWorldTest {
+    private StorageDao storage;
+    private RegistrationService registrationService;
+    private User user;
+
+    @BeforeEach
+     void setUp() {
+        storage = new StorageDaoImpl();
+        registrationService = new RegistrationServiceImpl();
+        user = new User();
+        user.setAge(42);
+        user.setLogin("labudabudapta2022");
+        user.setPassword("motorama");
+    }
+
+    @Test
+    void userAtLeast18_Ok() {
+        User result = registrationService.register(user);
+        assertEquals(result, user);
+    }
+
+    @Test
+    void userIsYoungerThan18_notOk() {
+        user.setAge(12);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void userLoginIsValid_Ok() {
+        assertEquals(user, registrationService.register(user));
+    }
+
+    @Test
+    void userLoginLessThanSixCharacters_notOk() {
+        user.setLogin("Robin");
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void userLoginIsNull_notOk() {
+        user.setLogin(null);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void userLoginHasInvalidSymbols_notOk() {
+        user.setLogin("caramba#98");
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void userWithSuchLoginExistsInDatabase_notOk() {
+        User current;
+        String login = user.getLogin();
+        do {
+            current = storage.get(login);
+            login = login + "test";
+            user.setLogin(login);
+        } while (current != null);
+        login = login.substring(0, login.length() - 4);
+        user.setLogin(login);
+        assertEquals(user, registrationService.register(user), "method register had to add user to the storage, but it didn't");
+        assertThrows(RuntimeException.class, () -> registrationService.register(user),
+                "you added user to the storage with duplicate login \n");
+    }
 }
