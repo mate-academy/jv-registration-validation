8055009f1ca31af7435d6b06b20107d8f48fa14d
Konstantin Miroshnichenko <bigfishnotbad@gmail.com>
2022-06-01 23:28:48 +0300

    removed redundant metod where age was more than 18

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -7,13 +7,18 @@ import core.basesyntax.model.User;
 public class RegistrationServiceImpl implements RegistrationService {
     private static final String LOGIN_EXIST = "user with such login already exists";
     private static final String LOGIN_IS_NULL = "user's login is null";
+    private static final String USER_IS_NULL = "user is null";
     private static final String TOO_YOUNG = "the user must first grow up a little, age less than 18";
     private static final String LOGIN_HAS_INVALID_SYMBOL = "login has at least one invalid symbols";
     private static final String LOGIN_HAS_LESS_THAT_SIX_SYMBOLS = "login too short, must be at least six symbols";
+    private static final String SPECIAL_SYMBOL = "\\W+";
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
+        if (user == null) {
+            throw new NullPointerException(USER_IS_NULL);
+        }
         if (user.getLogin() == null) {
             throw new RuntimeException(LOGIN_IS_NULL);
         }
@@ -23,7 +28,7 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user.getLogin().length() < 6) {
             throw new RuntimeException(LOGIN_HAS_LESS_THAT_SIX_SYMBOLS);
         }
-        if (user.getLogin().replaceAll("\\W+", "").length() - user.getLogin().length() != 0) {
+        if (user.getLogin().replaceAll(SPECIAL_SYMBOL, "").length() - user.getLogin().length() != 0) {
             throw new RuntimeException(LOGIN_HAS_INVALID_SYMBOL);
         }
         if (storageDao.get(user.getLogin()) != null) {
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -6,74 +6,80 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationService;
 import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HelloWorldTest {
-    private StorageDao storage;
-    private RegistrationService registrationService;
+    private static final String PLUG = "test";
+    private static StorageDao storage;
+    private static RegistrationService registrationService;
     private User user;
+
+    @BeforeAll
+    static void initial() {
+        storage = new StorageDaoImpl();
+        registrationService = new RegistrationServiceImpl();
+    }
 
     @BeforeEach
      void setUp() {
-        storage = new StorageDaoImpl();
-        registrationService = new RegistrationServiceImpl();
         user = new User();
         user.setAge(42);
-        user.setLogin("labudabudapta2022");
+        user.setLogin("labudabudapta");
         user.setPassword("motorama");
     }
 
     @Test
-    void userAtLeast18_Ok() {
-        User result = registrationService.register(user);
-        assertEquals(result, user);
+    void userIsNull_NotOk() {
+        User userNull = null;
+        assertThrows(RuntimeException.class, () -> registrationService.register(userNull));
     }
 
     @Test
-    void userIsYoungerThan18_notOk() {
+    void userIsYoungerThan18_NotOk() {
         user.setAge(12);
-        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+        assertThrows(RuntimeException.class, () -> registrationService.register(user), "condition break: user with age up to 18 was added\n");
     }
 
     @Test
-    void userLoginIsValid_Ok() {
-        assertEquals(user, registrationService.register(user));
-    }
-
-    @Test
-    void userLoginLessThanSixCharacters_notOk() {
+    void userLoginLesserThanSixCharacters_NotOk() {
         user.setLogin("Robin");
-        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+        assertThrows(RuntimeException.class, () -> registrationService.register(user), "too short login\n");
     }
 
     @Test
-    void userLoginIsNull_notOk() {
+    void userLoginIsNull_NotOk() {
         user.setLogin(null);
         assertThrows(RuntimeException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void userLoginHasInvalidSymbols_notOk() {
+    void userLoginHasInvalidSymbols_NotOk() {
         user.setLogin("caramba#98");
-        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+        assertThrows(RuntimeException.class, () -> registrationService.register(user), "login shouldn't have any special symbol\n");
     }
 
     @Test
-    void userWithSuchLoginExistsInDatabase_notOk() {
-        User current;
+    void userWithDuplicateLoginMayBeAddToDatabase_NotOk() {
         String login = user.getLogin();
-        do {
-            current = storage.get(login);
-            login = login + "test";
+        User uniqueUser = storage.get(login);
+        while (uniqueUser != null) {
+            login = login + PLUG;
             user.setLogin(login);
-        } while (current != null);
-        login = login.substring(0, login.length() - 4);
-        user.setLogin(login);
-        assertEquals(user, registrationService.register(user), "method register had to add user to the storage, but it didn't");
+            uniqueUser = storage.get(login);
+        }
+        assertEquals(user, registrationService.register(user), "method register had to add user to the storage, but it didn't\n");
         assertThrows(RuntimeException.class, () -> registrationService.register(user),
-                "you added user to the storage with duplicate login \n");
+                "you added user to the storage with duplicate login\n");
+    }
+
+    @AfterAll
+    static void clearingStorageWithTestingUserObjects() {
+        Storage.people.clear();
     }
 }
