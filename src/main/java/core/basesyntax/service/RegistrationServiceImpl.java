13ceea03168ab42c3cf9dd999193c54244a18f8b
Vasylyi Bevz <bevzvasya@gmail.com>
2022-06-07 21:42:33 +0300

    Registration-validation test class was done

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,6 +5,11 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int VALID_AGE = 18;
+    private static final int ZERO_AGE = 0;
+    private static final int UNBELIEVEBLE_AGE = 130;
+    private static final int EMPTY_LINE = 0;
+    private static final int MIN_LENGTH_PASSWORD = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -18,25 +23,25 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user.getPassword() == null) {
             throw new RuntimeException("Password is null");
         }
-        if (user.getLogin().length() == 0 || user.getLogin().matches("^[ ]*$")) {
+        if (user.getLogin().length() == EMPTY_LINE || user.getLogin().matches("^[ ]*$")) {
             throw new RuntimeException("Login is empty");
         }
-        if (user.getPassword().length() == 0 || user.getPassword().matches("^[ ]*$")) {
+        if (user.getPassword().length() == EMPTY_LINE || user.getPassword().matches("^[ ]*$")) {
             throw new RuntimeException("Password is empty");
         }
         if (!Character.isLetter(user.getLogin().charAt(0))) {
             throw new RuntimeException("First letter of login mast be letter");
         }
-        if (user.getPassword().length() < 6) {
+        if (user.getPassword().length() < MIN_LENGTH_PASSWORD) {
             throw new RuntimeException("Password is too short");
         }
-        if (user.getAge() < 18 && user.getAge() > 0) {
+        if (user.getAge() < VALID_AGE && user.getAge() > ZERO_AGE) {
             throw new RuntimeException("Age for access must be 18 and older");
         }
-        if (user.getAge() < 0) {
+        if (user.getAge() < ZERO_AGE) {
             throw new RuntimeException("Age can't be less then zero");
         }
-        if (user.getAge() > 130) {
+        if (user.getAge() > UNBELIEVEBLE_AGE) {
             throw new RuntimeException("We don't believe in this age");
         }
         if (storageDao.get(user.getLogin()) != null) {
