10cf08135a13a304b0a8f8bad0ba7fa8ae5c047a
Nikita Semisoshenko <jaderussell197@gmail.com>
2022-06-09 11:28:40 +0300

    all comments was treated. Build success

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -12,30 +12,38 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        checkUser(user);
-        storageDao.add(user);
-        return user;
+        if (checkAge(user) && checkLogin(user) && checkPassword(user)) {
+            storageDao.add(user);
+            return user;
+        }
+        return null;
     }
 
-    private boolean checkUser(User user) {
+    private boolean checkAge(User user) {
         if (user.getAge() == null) {
             throw new RuntimeException("Age can't be null");
-        } else if (user.getLogin() == null) {
-            throw new RuntimeException("Login can't be null");
-        } else if (user.getId() == null) {
-            throw new RuntimeException("ID can't be null");
-        } else if (user.getPassword() == null) {
-            throw new RuntimeException("Password can't be null");
-        } else if (storageDao.get(user.getLogin()) != null) {
-            throw new RuntimeException("User with same login is already exists");
         } else if (user.getAge() < MINIMAL_AGE) {
             throw new RuntimeException("Age must be 18 or greater for this action.");
-        } else if (user.getPassword().length() < MINIMAL_PASSWORD_LENGTH) {
-            throw new RuntimeException("Password must be greater than 6 characters");
         } else if (user.getAge() < FINAL_STATIC_ZERO) {
             throw new RuntimeException("Age can't be negative");
-        } else if (user.getId() < FINAL_STATIC_ZERO) {
-            throw new RuntimeException("ID can't be negative");
+        }
+        return true;
+    }
+
+    private boolean checkLogin(User user) {
+        if (user.getLogin() == null) {
+            throw new RuntimeException("Login can't be null");
+        } else if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User with same login is already exists");
+        }
+        return true;
+    }
+
+    private boolean checkPassword(User user) {
+        if (user.getPassword() == null) {
+            throw new RuntimeException("Password can't be null");
+        } else if (user.getPassword().length() < MINIMAL_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password must be greater than 6 characters");
         }
         return true;
     }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,12 +1,13 @@
 package core.basesyntax.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
@@ -17,31 +18,35 @@ class RegistrationServiceImplTest {
     static void beforeAll() {
         storage = new StorageDaoImpl();
         storage.add(new User(1535854439L, "bobTheOne", "iAmHereAlice", 23));
-    }
-
-    @BeforeEach
-    void setUp() {
         service = new RegistrationServiceImpl();
     }
 
     @Test
-    void register_ExistingUser_NotOk() {
+    void register_normalUser_Ok() {
+        User normalUser = new User(1535855239L, "MisterSandman", "BringMeADreams", 20);
+        User user = service.register(normalUser);
+        assertNotNull(user);
+        assertEquals(normalUser, user);
+    }
+
+    @Test
+    void register_existingUser_NotOk() {
         assertThrows(RuntimeException.class, () -> {
             service.register(new User(1535854439L, "bobTheOne", "iAmHereAlice", 23));
         });
     }
 
     @Test
-    void register_UserAgeLessThan18_NotOk() {
+    void register_userAgeLessNotValid_NotOk() {
         assertThrows(RuntimeException.class, () -> {
-            service.register(new User(15358545039L, "Shazam", "SHAZAM!!!", 12));
+            service.register(new User(15358545039L, "Shazam", "SHAZAM!!!", 17));
         });
     }
 
     @Test
-    void register_ShortPassword_NotOk() {
+    void register_shortPassword_NotOk() {
         assertThrows(RuntimeException.class, () -> {
-            service.register(new User(1535855239L, "John", "hi", 19));
+            service.register(new User(1535855239L, "John", "passw", 19));
         });
     }
 
@@ -66,30 +71,8 @@ class RegistrationServiceImplTest {
         });
     }
 
-    @Test
-    void register_nullID_NotOk() {
-        assertThrows(RuntimeException.class, () -> {
-            service.register(new User(null, "NeoTheChosen", "followTheWhiteRabbit", 19));
-        });
-    }
-
-    @Test
-    void register_negativeAge_NotOk() {
-        assertThrows(RuntimeException.class, () -> {
-            service.register(new User(1535829039L, "BendjaminButton", "amILive?", -12));
-        });
-    }
-
-    @Test
-    void register_negativeID_NotOk() {
-        assertThrows(RuntimeException.class, () -> {
-            service.register(new User(-1535854439L, "Penguin", "whereIsRobin?", 29));
-        });
-    }
-
-    @Test
-    void register_normalUser_Ok() {
-        User user = service.register(new User(1535855239L, "MisterSandman", "BringMeADreams", 20));
-        assertNotNull(user);
+    @AfterEach
+    void tearDown() {
+        storage = new StorageDaoImpl();
     }
 }
