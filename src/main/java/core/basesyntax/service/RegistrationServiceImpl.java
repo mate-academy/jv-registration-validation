7c1b72f1fffd854162662dd4bdfdc2b11eb4e4e1
Tymur Tsvietkov <tymur.tsvietkov@gmail.com>
2022-06-14 12:05:36 +0300

    constants and infomative messages  added

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,18 +5,21 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
         if (user.getLogin() == null || storageDao.get(user.getLogin()) != null) {
-            throw new InvalidDataException("Invalid data");
+            throw new InvalidDataException(
+                    "Invalid login or such login already contains in storage");
         }
-        if (user.getAge() == null || user.getAge() < 18) {
-            throw new InvalidDataException("Invalid data");
+        if (user.getAge() == null || user.getAge() < MIN_AGE) {
+            throw new InvalidDataException("Invalid age");
         }
-        if (user.getPassword() == null || user.getPassword().length() < 6) {
-            throw new InvalidDataException("Invalid data");
+        if (user.getPassword() == null || user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new InvalidDataException("Invalid password");
         }
         return user;
     }
