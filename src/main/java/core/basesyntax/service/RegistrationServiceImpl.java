d2a646e26c7ff62923222da3f3cddf140044bf50
svyatoslav <sviatoslav55@icloud.com>
2022-06-16 15:03:39 +0200

    I create realisation test for method validation RegistrationServiceImpl.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,27 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    public static final int MIN_VALID_AGE = 18;
+    public static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User is already exist");
+        }
+        if (user.getAge() <= MIN_VALID_AGE) {
+            throw new RuntimeException("Users age is less than 18 years old");
+        }
+        if (user.getPassword().length() <= MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Users password is less than 6 symbols");
+        }
+        if (user.getLogin() == null
+                || user.getPassword() == null
+                || user.getLogin().isEmpty()
+                || user.getPassword().isEmpty()) {
+            throw new RuntimeException("This field should not be empty");
+        }
+        return storageDao.add(user);
     }
 }
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,8 +1,79 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class HelloWorldTest {
 
+    private static RegistrationService registrationService;
+    private static User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new core.basesyntax.service.RegistrationServiceImpl();
+        user = new User();
+    }
+
+    @BeforeEach
+    void setUp() {
+        Storage.people.clear();
+        user.setId(1L);
+        user.setAge(21);
+        user.setLogin("validLogin");
+        user.setPassword("validPassword");
+    }
+
+    @Test
+    void register_validUser_ok() {
+        User expected = user;
+        User actual = registrationService.register(user);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void register_userExist_notOk() {
+        registrationService.register(user);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_usersAgeLessThan18_notOk() {
+        user.setAge(17);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_usersPasswordLessThan6symbols_notOk() {
+        user.setPassword("12345");
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_emptyLogin_notOk() {
+        user.setLogin("");
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_emptyPassword_notOk() {
+        user.setPassword("");
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_nullLogin_notOk() {
+        user.setLogin(null);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_nullPassword_notOk() {
+        user.setPassword(null);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
 }
