ed1dd905b5db5ccb651f0d46a7c7400d636ffadf
Vasiliy Sheretko <vasil.sheretko@yahoo.com>
2022-06-28 18:14:40 +0300

    Using TDD principle implemented registration service

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,26 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE_ALLOWED = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RuntimeException("User can't be null");
+        }
+        if (user.getLogin() == null) {
+            throw new RuntimeException("Login can't be null");
+        }
+
+        if (user.getPassword().length() >= MIN_PASSWORD_LENGTH
+                && !user.equals(storageDao.get(user.getLogin()))
+                && user.getAge() != null
+                && user.getAge() >= MIN_AGE_ALLOWED
+                && user.getAge() < Integer.MAX_VALUE) {
+            storageDao.add(user);
+        }
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,100 @@
+package core.basesyntax.service;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_AGE_ALLOWED = 18;
+    private static final String PASSWORD = "123456";
+    private RegistrationService service;
+    private StorageDao storageDao;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        service = new RegistrationServiceImpl();
+        storageDao = new StorageDaoImpl();
+        user = new User();
+    }
+
+    @AfterEach
+    void tearDown() {
+        service = null;
+        storageDao = null;
+        user = null;
+    }
+
+    @Test
+    void register_nullUser_notOk() {
+        assertThrows(RuntimeException.class, () -> service.register(null));
+    }
+
+    @Test
+    void register_nullUserLogin_NotOk() {
+        user.setLogin(null);
+        assertThrows(RuntimeException.class, () -> service.register(user));
+    }
+
+    @Test
+    void register_nullUserAge_NotOk() {
+        user.setAge(null);
+        assertThrows(RuntimeException.class, () -> service.register(user));
+    }
+
+    @Test
+    void register_nullUserPassword_NotOk() {
+        user.setPassword(null);
+        assertThrows(RuntimeException.class, () -> service.register(user));
+    }
+
+    @Test
+    void register_userPasswordLengthIsCorrect_Ok() {
+        user.setPassword(PASSWORD);
+        user.setLogin("user");
+        user.setAge(18);
+        service.register(user);
+        boolean actual = user.getPassword().length() >= MIN_PASSWORD_LENGTH;
+        assertTrue(actual);
+    }
+
+    @Test
+    void register_userIsInStorage_NotOk() {
+        user.setAge(18);
+        user.setLogin("user");
+        user.setPassword(PASSWORD);
+        storageDao.add(user);
+        service.register(user);
+        boolean actual = !user.equals(storageDao.get(user.getLogin()));
+        assertFalse(actual);
+    }
+
+    @Test
+    void register_userAgeIsZeroOrLess_NotOk() {
+        user.setAge(-5);
+        assertThrows(RuntimeException.class, () -> service.register(user));
+    }
+
+    @Test
+    void register_userAgeEqualsIntMaxValue_NotOk() {
+        user.setAge(Integer.MAX_VALUE);
+        boolean actual = user.getAge() == Integer.MAX_VALUE;
+        assertTrue(actual);
+    }
+
+    @Test
+    void register_userHasRightAge_Ok() {
+        user.setAge(18);
+        user.setLogin("user");
+        user.setPassword(PASSWORD);
+        service.register(user);
+        boolean actual = user.getAge() >= MIN_AGE_ALLOWED;
+        assertTrue(actual);
+    }
+}
