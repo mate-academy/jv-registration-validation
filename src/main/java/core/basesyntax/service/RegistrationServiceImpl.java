00e379aa3733e0c66e6be5931b2fab68a40f7afb
Roman Pylypchuk <roman.pylypchuk@yahoo.com>
2022-07-25 12:46:25 +0300

    implemented a registration service

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,32 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE_VALUE = 18;
+    private static final int MIN_PASSWORD_SIZE = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getAge() < MIN_AGE_VALUE) {
+            throw new RuntimeException("It's unable to register user with age less than "
+                    + MIN_AGE_VALUE + ".");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_SIZE) {
+            throw new RuntimeException("Error. Your password should contain at least "
+                    + MIN_PASSWORD_SIZE + " characters.");
+        }
+        if (Storage.people.contains(user)) {
+            throw new RuntimeException("Error. User with such login already exists.");
+        }
+        if (user == null || user.getAge() == null
+                || user.getLogin() == null
+                || user.getPassword() == null) {
+            throw new RuntimeException("Error. Invalid data type.");
+        }
+        return storageDao.add(user);
     }
 }
