76ca60cf090d048b792c4bef4f5ef4fbbe3fe469
ssplot56 <ssplot56@gmail.com>
2022-07-26 13:18:58 +0300

    Implemented RegistrationServiceImpl and tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,26 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_REQUIRED_AGE = 18;
+    private static final int MIN_REQUIRED_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null || user.getAge() == null
+                || user.getLogin() == null
+                || user.getPassword() == null) {
+            throw new RuntimeException("Нou have not entered all the required information");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("This user is already in the storage");
+        }
+        if (user.getAge() < MIN_REQUIRED_AGE) {
+            throw new RuntimeException("User must be at least 18 years old");
+        }
+        if (user.getPassword().length() < MIN_REQUIRED_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password too short, change it");
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,101 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+    private User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        user = new User("bestUser", "qwerty123", 18);
+    }
+
+    @Test
+    void register_user_ok() {
+        registrationService.register(user);
+        int actual = Storage.people.size();
+        assertEquals(1, actual);
+    }
+
+    @Test
+    void register_usersWithTheSameLogin_notOk() {
+        registrationService.register(user);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(new User("bestUser", "123456", 20));
+        });
+    }
+
+    @Test
+    void register_nullAge_notOk() {
+        user.setAge(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_nullUser_notOk() {
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(null);
+        });
+    }
+
+    @Test
+    void register_nullLogin_notOk() {
+        user.setLogin(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_nullPassword_notOk() {
+        user.setLogin(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_shortPassword_notOk() {
+        user.setPassword("123");
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_ageUnder18_notOk() {
+        user.setAge(14);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_negativeAge_notOk() {
+        user.setAge(-14);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
+}
