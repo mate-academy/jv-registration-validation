a9766be457d53f8311ebaeda4097b7689f0c7154
dmitriy-tverdokhlibov <dmitriytverdokhlibov@yahoo.com>
2022-07-27 18:35:21 +0200

    removed magical values and created constants, changed the registration method and added messages to errors

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,20 +5,32 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    public final static int ALLOWED_AGE = 18;
+    public final static int ALLOWED_PASSWORD_LENGTH = 6;
+
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        if (user == null || user.getPassword().length() < 6
-                || user.getAge() < 18 || user.getLogin() == null) {
+        if (user == null) {
             throw new RuntimeException();
         }
 
-        if (storageDao.get(user.getLogin()) == null) {
-            storageDao.add(user);
-            return user;
-        } else {
-            throw new RuntimeException();
+        if (user.getPassword().length() < ALLOWED_PASSWORD_LENGTH) {
+            throw new RuntimeException("Error: invalid password length, user password must be at least 6 characters");
         }
+
+        if (user.getAge() < ALLOWED_AGE) {
+            throw new RuntimeException("Error: invalid age, age must be at least 18 years old");
+        }
+
+        if (user.getLogin() == null) {
+            throw new RuntimeException("Error: login error, login cannot be empty");
+        }
+
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("Error: user with such login allready exist.");
+        }
+        return storageDao.add(user);
     }
 }
