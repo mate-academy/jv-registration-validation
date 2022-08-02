416979dfe6c2c78c857499af43a2199fdf7bef75
Nata <natserbo@gmail.com>
2022-08-02 17:22:16 +0300

    mvn corrections

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,118 +1,107 @@
 package core.basesyntax.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.security.Provider;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class RegistrationServiceImplTest {
-    private RegistrationServiceImpl registrationService;
-    private User DEFAULT_USER;
-    private static User USER2;
-    private static User USER2_COPY;
-    private static User USER3;
-    public static User INVALID_USER;
     private static final String LOGIN_DEFAULT = "Kate";
     private static final String LOGIN_BRUCE = "Bruce";
     private static final String LOGIN_AMY = "Amy";
     private static final int MINUS_AGE = -5;
-    public static final int HUNDRED_AGE = 100;
+    private static final int HUNDRED_AGE = 100;
     private static final int ZERO_AGE = 0;
     private static final int EIGHTEEN_AGE = 18;
     private static final String INVALID_PASSWORD = "H";
     private static final String EMPTY_PASSWORD = "";
     private static final String GOOD_PASSWORD = "123456";
     private static final String GOOD_PASSWORD2 = "parol2";
-    public static final String REVERSE_GOOD_PASSWORD = "654321";
-
+    private static final String REVERSE_GOOD_PASSWORD = "654321";
     private static final User NULL_USER = new User();
+    private static User user2;
+    private static User user2Copy;
+    private static User user3;
+    private User defaultUser;
+    private RegistrationServiceImpl registrationService;
 
     @BeforeAll
     static void beforeAll() {
-        USER2 = new User();
-        USER2.setLogin(LOGIN_BRUCE);
-        USER2.setAge(HUNDRED_AGE);
-        USER2.setPassword(REVERSE_GOOD_PASSWORD);
-        INVALID_USER = new User();
-        INVALID_USER.setLogin(LOGIN_AMY);
-        USER3 = new User();
-        USER3.setLogin(LOGIN_AMY);
-        USER3.setAge(EIGHTEEN_AGE);
-        USER3.setPassword(GOOD_PASSWORD2);
-        USER2_COPY = new User();
-        USER2_COPY.setLogin(LOGIN_BRUCE);
-        USER2_COPY.setAge(HUNDRED_AGE);
-        USER2_COPY.setPassword(REVERSE_GOOD_PASSWORD);
-
+        user2 = new User();
+        user2.setLogin(LOGIN_BRUCE);
+        user2.setAge(HUNDRED_AGE);
+        user2.setPassword(REVERSE_GOOD_PASSWORD);
+        user3 = new User();
+        user3.setLogin(LOGIN_AMY);
+        user3.setAge(EIGHTEEN_AGE);
+        user3.setPassword(GOOD_PASSWORD2);
+        user2Copy = user2;
     }
 
     @BeforeEach
      void beforeEach() {
         registrationService = new RegistrationServiceImpl();
-        DEFAULT_USER = new User();
-        DEFAULT_USER.setLogin(LOGIN_DEFAULT);
-        DEFAULT_USER.setAge(EIGHTEEN_AGE);
-        DEFAULT_USER.setPassword(GOOD_PASSWORD);
+
+        defaultUser = new User();
+        defaultUser.setLogin(LOGIN_DEFAULT);
+        defaultUser.setAge(EIGHTEEN_AGE);
+        defaultUser.setPassword(GOOD_PASSWORD);
     }
-
-
-
 
     @Test
     void addSameLoginUser_NotOk() {
-        registrationService.register(DEFAULT_USER);
-        assertThrows(RuntimeException.class,() -> registrationService.register(DEFAULT_USER));
+        registrationService.register(defaultUser);
+        assertThrows(RuntimeException.class,() -> registrationService.register(defaultUser));
     }
 
     @Test
-    void addUserWithNullLogin_NotOk() {
-        DEFAULT_USER.setLogin(null);
-        assertThrows(RuntimeException.class,() -> registrationService.register(DEFAULT_USER));
+    void addUser_NullLogin_NotOk() {
+        defaultUser.setLogin(null);
+        assertThrows(RuntimeException.class,() -> registrationService.register(defaultUser));
     }
 
     @Test
     void addFewUsers_Ok() {
-        registrationService.register(DEFAULT_USER);
-        registrationService.register(USER2);
-        registrationService.register(USER3);
+        registrationService.register(defaultUser);
+        registrationService.register(user2);
+        registrationService.register(user3);
+        assertThrows(RuntimeException.class,() -> registrationService.register(user2Copy));
         assertEquals(3, Storage.people.size());
     }
 
     @Test
-    void addUserYounger18_NotOk() {
-        DEFAULT_USER.setAge(MINUS_AGE);
-        assertThrows(RuntimeException.class,() -> registrationService.register(DEFAULT_USER));
-        DEFAULT_USER.setLogin(LOGIN_BRUCE);
-        DEFAULT_USER.setAge(ZERO_AGE);
-        assertThrows(RuntimeException.class,() -> registrationService.register(DEFAULT_USER));
+    void addUser_Younger18_NotOk() {
+        defaultUser.setAge(MINUS_AGE);
+        assertThrows(RuntimeException.class,() -> registrationService.register(defaultUser));
+        defaultUser.setLogin(LOGIN_BRUCE);
+        defaultUser.setAge(ZERO_AGE);
+        assertThrows(RuntimeException.class,() -> registrationService.register(defaultUser));
     }
 
     @Test
-    void addUserWithNullAge_NotOk() {
-        DEFAULT_USER.setAge(null);
-        assertThrows(RuntimeException.class,() -> registrationService.register(DEFAULT_USER));
+    void addUser_NullAge_NotOk() {
+        defaultUser.setAge(null);
+        assertThrows(RuntimeException.class,() -> registrationService.register(defaultUser));
     }
 
     @Test
-    void addInvalidUserPassword_NotOk() {
-        DEFAULT_USER.setPassword(INVALID_PASSWORD);
-        assertThrows(RuntimeException.class,() -> registrationService.register(DEFAULT_USER));
-        DEFAULT_USER.setLogin(LOGIN_BRUCE);
-        DEFAULT_USER.setPassword(EMPTY_PASSWORD);
-        assertThrows(RuntimeException.class,() -> registrationService.register(DEFAULT_USER));
+    void addUser_InvalidPassword_NotOk() {
+        defaultUser.setPassword(INVALID_PASSWORD);
+        assertThrows(RuntimeException.class,() -> registrationService.register(defaultUser));
+        defaultUser.setLogin(LOGIN_BRUCE);
+        defaultUser.setPassword(EMPTY_PASSWORD);
+        assertThrows(RuntimeException.class,() -> registrationService.register(defaultUser));
     }
 
     @Test
-    void addUserWithNullPassword_NotOk() {
-        DEFAULT_USER.setPassword(null);
-        assertThrows(RuntimeException.class,() -> registrationService.register(DEFAULT_USER));
+    void addUser_NullPassword_NotOk() {
+        defaultUser.setPassword(null);
+        assertThrows(RuntimeException.class,() -> registrationService.register(defaultUser));
     }
 
     @Test
