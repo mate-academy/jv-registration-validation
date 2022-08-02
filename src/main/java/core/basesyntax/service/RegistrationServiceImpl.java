587dc119df8f169c978d4cce4683da0cb0883f32
Benatti <ssunica0@gmail.com>
2022-08-02 18:59:36 +0300

    completed task 'jv-registration-validation'

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,36 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
+import java.util.ArrayList;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LENGTH_PASSWORD = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
+    public RegistrationServiceImpl() {
+        Storage.setPeople(new ArrayList<>());
+    }
+
     @Override
-    public User register(User user) {
-        return null;
+    public User register(User user) throws RuntimeException {
+        if (user == null || user.getLogin() == null
+                || user.getAge() == null || user.getPassword() == null || user.getAge() < 0) {
+            throw new RegistrationServiceException("Invalid data!");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationServiceException("Login already exist!");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationServiceException("User age must be not less "
+                    + MIN_LENGTH_PASSWORD);
+        }
+        if (user.getPassword().length() < MIN_LENGTH_PASSWORD) {
+            throw new RegistrationServiceException("Minimal length of the password must be not less"
+                    + MIN_LENGTH_PASSWORD);
+        }
+        return storageDao.add(user);
     }
 }
