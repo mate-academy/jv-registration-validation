791b98a16234d57eb4b1736e03b48f116dce998d
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-08-08 13:49:22 +0300

    add throw in method register

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,16 +6,27 @@ import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
+    private static final int MIN_AGE = 18;
+    private static final int MIN_SIZE_OF_PASSWORD = 6;
 
     @Override
     public User register(User user) {
         if (user.getLogin() != null  && user.getLogin() != "") {
             User findUser = storageDao.get(user.getLogin());
-            if (findUser == null && user.getAge() >= 18 && user.getPassword().length() >= 6 ) {
-                storageDao.add(user);
-                return user;
+            if (findUser != null) {
+                throw new RuntimeException("The user with such login already exists. "
+                        + "Please, enter another login.");
             }
-        }
-        return null;
+            if (user.getAge() < MIN_AGE) {
+                throw new RuntimeException("The user should be at least "
+                        + MIN_AGE + " years old.");
+            }
+            if (user.getPassword().length() < MIN_SIZE_OF_PASSWORD ) {
+                throw new RuntimeException("Password should be at least "
+                        + MIN_SIZE_OF_PASSWORD + " characters.");
+            }
+            storageDao.add(user);
+            return user;
+        } else { throw new NullPointerException("Login can't be null or empty!");}
     }
 }
