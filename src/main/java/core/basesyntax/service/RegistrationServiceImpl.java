24b37f70f14fb68b67ed760f44b3ec242802e7bd
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-08-15 19:09:35 +0300

    created @Test void registerUserIsNull_NotOk()

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,18 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.ValidationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
-    public User register(User user) {
-        return null;
+    public User register(User user) throws ValidationException {
+        if (user == null) {
+            throw new ValidationException("User cannot be null");
+        }
+        storageDao.add(user);
+        return user;
     }
 }
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,8 +1,31 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorldTest {
+import core.basesyntax.exception.ValidationException;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class HelloWorldTest {
+    RegistrationService registrationService = new RegistrationServiceImpl();
+    @BeforeAll
+    static void beforeAll() {
+        User user = new User();
+        user.setLogin("login");
+        user.setPassword("password");
+        user.setAge(25);
+    }
+
+    @Test
+    void registerUserIsNull_NotOk() {
+        try {
+            registrationService.register(null);
+        } catch (ValidationException e) {
+            return;
+        }
+        fail("User cannot be null");
+    }
 }
