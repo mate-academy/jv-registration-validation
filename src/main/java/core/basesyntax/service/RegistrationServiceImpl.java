891fd03976cfae97261f1b966ae9c1c950dd93c4
Andrii Pylypenko <pilipenko1975@gmail.com>
2022-08-16 22:10:44 +0300

    created Test register_userRegistered_Ok() mentor's comments are taken into account

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,8 +5,8 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private final short MINIMUM_PASSWORD_LENGTH = 6;
-    private final short MINIMUM_USER_AGE = 18;
+    private static final short MINIMUM_PASSWORD_LENGTH = 6;
+    private static final short MINIMUM_USER_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
