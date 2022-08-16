0dbb9aaf237d48de95bcbaec4bbb91b9040b777c
Vasyl Shevtsiv <vasyl.shevtsiv@gmail.com>
2022-08-16 13:24:29 +0300

    cleaning with maven

--- src/test/java/core/basesyntax/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -1,38 +1,35 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-import core.basesyntax.dao.StorageDao;
-import core.basesyntax.dao.StorageDaoImpl;
-import core.basesyntax.db.Storage;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationService;
 import core.basesyntax.service.RegistrationServiceImpl;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class RegistrationServiceTest {
     private RegistrationService service;
     private User user1;
     private User user2;
 
-     @BeforeEach
-     void setUp() {
-         service = new RegistrationServiceImpl();
-         user1 = new User();
-         user2 = new User();
-     }
+    @BeforeEach
+    void setUp() {
+        service = new RegistrationServiceImpl();
+        user1 = new User();
+        user2 = new User();
+    }
 
     @Test
     public void registerUserUnderagedUNotOkey() {
         user1.setAge(16);
         user1.setLogin("Bob");
         user1.setPassword("123456");
-        RuntimeException exception = assertThrows(RuntimeException.class,() -> {service.register(user1);});
+        RuntimeException exception =
+                assertThrows(RuntimeException.class,() -> {
+                    service.register(user1);
+                });
         assertEquals("User must be 18 years old or older", exception.getMessage());
     }
 
@@ -41,8 +38,12 @@ public class RegistrationServiceTest {
         user1.setAge(26);
         user1.setLogin("Tom");
         user1.setPassword("1236");
-        RuntimeException exception = assertThrows(RuntimeException.class,() -> {service.register(user1);});
-        assertEquals("Password must be at least 6 characters long", exception.getMessage());
+        RuntimeException exception =
+                assertThrows(RuntimeException.class,() -> {
+                    service.register(user1);
+                });
+        assertEquals("Password must be at least 6 characters long",
+                exception.getMessage());
     }
 
     @Test
@@ -53,7 +54,11 @@ public class RegistrationServiceTest {
         user2.setAge(100);
         user2.setLogin("Bob");
         user2.setPassword("123456");
-        RuntimeException exception = assertThrows(RuntimeException.class,() -> {service.register(user1);service.register(user2);});
+        RuntimeException exception =
+                assertThrows(RuntimeException.class,() -> {
+                    service.register(user1);
+                    service.register(user2);
+                });
         assertEquals("There is already user with such login", exception.getMessage());
     }
 
@@ -72,6 +77,4 @@ public class RegistrationServiceTest {
         user1.setPassword("1234567");
         assertEquals(user1, service.register(user1));
     }
-
-
 }
