d43c20cab553427090aabd8a9bf298469c584d09
asha <ashajanebrown@gmail.com>
2022-08-18 12:03:57 +0300

    fixed register() method

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,6 +5,8 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -12,35 +14,28 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user == null) {
             throw new RuntimeException("User is null");
         }
-
         if (user.getLogin() == null) {
             throw new RuntimeException("Login is null");
         }
-
         if (user.getPassword() == null) {
             throw new RuntimeException("Password is null");
         }
-
         if (user.getAge() == null) {
             throw new RuntimeException("Age is null");
         }
-
         if (user.getLogin().equals("")) {
             throw new RuntimeException("Login is empty");
         }
-
         if (storageDao.get(user.getLogin()) != null) {
             throw new RuntimeException("User already exists");
         }
-
-        if (user.getAge() <= 18) {
-            throw new RuntimeException("Age is under 18");
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("Age is under " + MIN_AGE);
         }
-
-        if (user.getPassword().length() < 6) {
-            throw new RuntimeException("Password is less than 6 characters");
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password is less than "
+                    + MIN_PASSWORD_LENGTH + " characters");
         }
-
         storageDao.add(user);
         return user;
     }
