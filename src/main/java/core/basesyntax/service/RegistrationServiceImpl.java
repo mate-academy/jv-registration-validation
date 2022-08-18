c879819befccb97b39e0583db0b173b07d216bb6
Anna Popova <anitapopova1996@gmail.com>
2022-08-18 21:56:59 +0300

    Implemented register() in RegistrationServiceImpl. RegistrationServiceImplTest created, 97% coverage.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,52 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MAX_PASSWORD_LENGTH = 20;
+    private static final String EMPTY_SPACE_REGEX = ".*\\s+.*";
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        validateUser(user);
+        return storageDao.add(user);
+    }
+
+    private void validateUser(User user) {
+        if (user == null) {
+            throw new RuntimeException("User can't be null!");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User with this login: " + user.getLogin()
+                    + " already exists!");
+        }
+        if (user.getLogin() == null) {
+            throw new RuntimeException("Login can't be null!");
+        }
+        if (user.getLogin().matches(EMPTY_SPACE_REGEX)) {
+            throw new RuntimeException("Login can't contain empty space!");
+        }
+        if (!Character.isLetter(user.getLogin().charAt(0))) {
+            throw new RuntimeException("Login need to begin with letter: " + user.getLogin() + "!");
+        }
+        if (user.getPassword() == null) {
+            throw new RuntimeException("Password can't be null");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH
+                || user.getPassword().length() > MAX_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password must be 6 - 20 characters. "
+                    + "Your password is invalid: " + user.getPassword());
+        }
+        if (user.getAge() == null) {
+            throw new RuntimeException("Age can't be null.");
+        }
+        if (user.getAge() < 0) {
+            throw new RuntimeException("Can't enter age less than 0." + user.getAge());
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("You are under age! Forbidden to register."
+                    + " Come back when you are 18.");
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,122 @@
+package core.basesyntax.service;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        StorageDao storageDao = new StorageDaoImpl();
+        storageDao.add(new User("popova", "justpopova", 26));
+        storageDao.add(new User("sterniuk", "1123313", 30));
+        storageDao.add(new User("pony", "dfyrg6", 17));
+    }
+
+    @Test
+    public void userNull_isNotValid() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            registrationService.register(null);
+        });
+    }
+
+    @Test
+    public void userSameLogin_isNotValid() {
+        User newUser = new User("popova", "justpopova", 26);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            registrationService.register(newUser);
+        });
+    }
+
+    @Test
+    public void userLogin_isValid() {
+        User newUser = new User("gabriella", "gabi2000", 22);
+        Assertions.assertEquals(newUser, registrationService.register(newUser));
+    }
+
+    @Test
+    public void userLoginNull_isNotValid() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            registrationService.register(new User(null, "hellokitty", 19));
+        });
+    }
+
+    @Test
+    public void userLoginContainsEmptySpace_isNotValid() {
+        String userLogin = "S     ";
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            registrationService.register(new User(userLogin, "emptySpaces", 24));
+        });
+    }
+
+    @Test
+    public void userLoginStartsWithCharacter_isNotValid() {
+        String userLogin = "1anita";
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            registrationService.register(new User(userLogin, "dreamsaway", 130));
+        });
+    }
+
+    @Test
+    public void userLoginStartsWithCharacter_isValid() {
+        String userLogin = "mamasita";
+        Assertions.assertEquals(new User(userLogin, "mamasita1", 20),
+                registrationService.register(new User(userLogin, "mamasita1", 20)));
+    }
+
+    @Test
+    public void userPasswordNull_isNotValid() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            registrationService.register(new User("defaultUser", null, 17));
+        });
+    }
+
+    @Test
+    public void userPasswordLength_isNotValid() {
+        User newUser = new User("defaultUser", "12345", 17);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            registrationService.register(newUser);
+        });
+    }
+
+    @Test
+    public void userPasswordLength_isValid() {
+        User newUser = new User("florenta", "flora15", 22);
+        Assertions.assertEquals(newUser, registrationService.register(newUser));
+    }
+
+    @Test
+    public void userAgeNull_isNotValid() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            registrationService.register(new User("kristine", "password1", null));
+        });
+    }
+
+    @Test
+    public void userAgeLessThanAccepted_isNotValid() {
+        User newUser = new User("pinguin", "blackAndWhite", 15);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            registrationService.register(newUser);
+        });
+    }
+
+    @Test
+    public void userAgeIsLessThanZero_isNotValid() {
+        User newUser = new User("pinguin", "blackAndWhite", -10);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            registrationService.register(newUser);
+        });
+    }
+
+    @Test
+    public void userAge_isValid() {
+        User newUser = new User("hellboy", "hellOfTime", 35);
+        Assertions.assertEquals(newUser, registrationService.register(newUser));
+    }
+}
