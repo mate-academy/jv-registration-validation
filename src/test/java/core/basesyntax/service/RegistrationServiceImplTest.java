37412416868e62acbbb366ea62a3d081ec51d0c0
asha <ashajanebrown@gmail.com>
2022-08-18 22:39:58 +0300

    fixed tests after review, added new tests

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,12 +1,14 @@
 package core.basesyntax.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,69 +17,99 @@ class RegistrationServiceImplTest {
     public static final String DEFAULT_PASSWORD = "password";
     public static final int DEFAULT_AGE = 30;
     private static RegistrationService registrationService;
-    private User user;
+    private User validUser;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+    }
 
     @BeforeEach
     void setUp() {
-        registrationService = new RegistrationServiceImpl();
-        user = new User();
-        user.setLogin(DEFAULT_LOGIN);
-        user.setPassword(DEFAULT_PASSWORD);
-        user.setAge(DEFAULT_AGE);
+        validUser = new User();
+        validUser.setLogin(DEFAULT_LOGIN);
+        validUser.setPassword(DEFAULT_PASSWORD);
+        validUser.setAge(DEFAULT_AGE);
     }
 
     @Test
     void register_validData_Ok() {
-        User actual = registrationService.register(user);
-        assertEquals(user, actual);
-    }
-
-    @Test
-    void register_TwoUsers_Ok() {
-        User user2 = new User();
-        user2.setLogin("user2");
-        user2.setPassword(DEFAULT_PASSWORD);
-        user2.setAge(DEFAULT_AGE);
-        registrationService.register(user);
-        registrationService.register(user2);
-        assertTrue(Storage.people.contains(user));
-        assertTrue(Storage.people.contains(user2));
+        User actualUser = registrationService.register(validUser);
+        assertEquals(validUser, actualUser);
+        assertNotNull(validUser.getId());
+        assertTrue(Storage.people.contains(validUser));
     }
 
     @Test
     void register_emptyLogin_NotOk() {
-        user.setLogin("");
+        validUser.setLogin("");
         assertThrows(RuntimeException.class, () -> {
-            registrationService.register(user);
+            registrationService.register(validUser);
         });
     }
 
     @Test
-    void register_invalidLogin_NotOk() {
-        User user2 = new User();
-        user2.setLogin(DEFAULT_LOGIN);
-        user2.setPassword(DEFAULT_PASSWORD);
-        user2.setAge(DEFAULT_AGE);
-        registrationService.register(user2);
+    void register_usersWithSameLogins_NotOk() {
+        registrationService.register(validUser);
         assertThrows(RuntimeException.class, () -> {
-            registrationService.register(user);
+            registrationService.register(validUser);
         });
     }
 
     @Test
-    void register_invalidAge_NotOk() {
-        user.setAge(16);
+    void register_lessThenMinAge_NotOk() {
+        validUser.setAge(17);
         assertThrows(RuntimeException.class, () -> {
-            registrationService.register(user);
+            registrationService.register(validUser);
         });
     }
 
     @Test
-    void register_invalidPassword_NotOk() {
-        user.setPassword("pass");
+    void register_moreThenMinAge_Ok() {
+        validUser.setAge(19);
+        User actualUser = registrationService.register(validUser);
+        assertEquals(validUser, actualUser);
+        assertEquals(validUser.getAge(), actualUser.getAge());
+    }
+
+    @Test
+    void register_equalToMinAge_Ok() {
+        validUser.setAge(18);
+        User actualUser = registrationService.register(validUser);
+        assertEquals(validUser, actualUser);
+        assertEquals(validUser.getAge(), actualUser.getAge());
+    }
+
+    @Test
+    void register_negativeAge_NotOk() {
+        validUser.setAge(-1);
         assertThrows(RuntimeException.class, () -> {
-            registrationService.register(user);
+            registrationService.register(validUser);
         });
+    }
+
+    @Test
+    void register_lessThanMinLengthPassword_NotOk() {
+        validUser.setPassword("pass");
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(validUser);
+        });
+    }
+
+    @Test
+    void register_equalToMinLengthPassword_Ok() {
+        validUser.setPassword("pass12");
+        User actualUser = registrationService.register(validUser);
+        assertEquals(validUser, actualUser);
+        assertEquals(validUser.getPassword(), actualUser.getPassword());
+    }
+
+    @Test
+    void register_moreThanMinLengthPassword_Ok() {
+        validUser.setPassword("pass123");
+        User actualUser = registrationService.register(validUser);
+        assertEquals(validUser, actualUser);
+        assertEquals(validUser.getPassword(), actualUser.getPassword());
     }
 
     @Test
@@ -89,25 +121,25 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_nullLogin_NotOk() {
-        user.setLogin(null);
+        validUser.setLogin(null);
         assertThrows(RuntimeException.class, () -> {
-            registrationService.register(user);
+            registrationService.register(validUser);
         });
     }
 
     @Test
     void register_nullAge_NotOk() {
-        user.setAge(null);
+        validUser.setAge(null);
         assertThrows(RuntimeException.class, () -> {
-            registrationService.register(user);
+            registrationService.register(validUser);
         });
     }
 
     @Test
     void register_nullPassword_NotOk() {
-        user.setPassword(null);
+        validUser.setPassword(null);
         assertThrows(RuntimeException.class, () -> {
-            registrationService.register(user);
+            registrationService.register(validUser);
         });
     }
 
