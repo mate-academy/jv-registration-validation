b84393a5aa9cddc1e59f8af9906ccb420069703a
Valentyn Chepiha <valentyn.chepiha@gmail.com>
2022-08-20 17:01:07 +0300

    Refactored test. Added magic values.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,6 +6,9 @@ import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_LENGTH_LOGIN = 0;
+    private static final int MIN_LENGTH_PASSWORD = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -16,12 +19,15 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user.getLogin() == null || user.getPassword() == null || user.getAge() == null) {
             throw new RegistrationException("All fields User must be not empty!");
         }
-        if (user.getAge() < 18) {
+        if (user.getAge() < MIN_AGE) {
             throw new RegistrationException("You are very young for this ;)");
         }
-        if (user.getPassword().trim().length() < 6) {
-            throw new RegistrationException("Your password is small. "
-                    + " The password must be at least 6 characters long.");
+        if (user.getPassword().trim().length() < MIN_LENGTH_PASSWORD) {
+            throw new RegistrationException("Your password is small. The password must be at least "
+                    + MIN_LENGTH_PASSWORD + " characters long.");
+        }
+        if (user.getLogin().trim().length() == MIN_LENGTH_LOGIN) {
+            throw new RegistrationException("Login cannot be empty.");
         }
         if (storageDao.get(user.getLogin()) != null) {
             throw new RegistrationException("A user with the same login is already registered.");
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import core.basesyntax.db.Storage;
 import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,16 +14,26 @@ class RegistrationServiceImplTest {
     private static final String DEFAULT_LOGIN = "userLogin";
     private static final String DEFAULT_PASSWORD = "userPassword";
     private static final int DEFAULT_AGE = 99;
-    private static final int WRONG_AGE = 17;
+    private static final String WRONG_LOGIN = "";
     private static final String WRONG_PASSWORD = "small";
+    private static final int WRONG_AGE = 17;
 
     private RegistrationService registrationService;
     private User user;
+    private User emptyUser;
 
     @BeforeEach
     void setUp() {
         registrationService = new RegistrationServiceImpl();
+        emptyUser = new User();
         user = new User();
+        user.setAge(DEFAULT_AGE);
+        user.setPassword(DEFAULT_PASSWORD);
+        user.setLogin(DEFAULT_LOGIN);
+    }
+
+    @AfterEach
+    void clearOldData() {
         Storage.people.clear();
     }
 
@@ -36,14 +47,13 @@ class RegistrationServiceImplTest {
     @Test
     void registration_emptyUser_notOk() {
         assertThrows(RegistrationException.class, () -> {
-            registrationService.register(user);
+            registrationService.register(emptyUser);
         });
     }
 
     @Test
     void registration_emptyUserLogin_notOk() {
-        user.setAge(DEFAULT_AGE);
-        user.setPassword(DEFAULT_PASSWORD);
+        user.setLogin(null);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         });
@@ -51,8 +61,7 @@ class RegistrationServiceImplTest {
 
     @Test
     void registration_emptyUserPassword_notOk() {
-        user.setAge(DEFAULT_AGE);
-        user.setLogin(DEFAULT_LOGIN);
+        user.setPassword(null);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         });
@@ -60,8 +69,7 @@ class RegistrationServiceImplTest {
 
     @Test
     void registration_emptyUserAge_notOk() {
-        user.setPassword(DEFAULT_PASSWORD);
-        user.setLogin(DEFAULT_LOGIN);
+        user.setAge(null);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         });
@@ -69,17 +77,11 @@ class RegistrationServiceImplTest {
 
     @Test
     void registration_User_Ok() {
-        user.setAge(DEFAULT_AGE);
-        user.setPassword(DEFAULT_PASSWORD);
-        user.setLogin(DEFAULT_LOGIN);
         assertEquals(user, registrationService.register(user));
     }
 
     @Test
     void registration_reRegistrationUser_notOk() {
-        user.setAge(DEFAULT_AGE);
-        user.setPassword(DEFAULT_PASSWORD);
-        user.setLogin(DEFAULT_LOGIN);
         registrationService.register(user);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
@@ -89,8 +91,6 @@ class RegistrationServiceImplTest {
     @Test
     void registration_youngUser_notOk() {
         user.setAge(WRONG_AGE);
-        user.setPassword(DEFAULT_PASSWORD);
-        user.setLogin(DEFAULT_LOGIN);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         });
@@ -98,9 +98,15 @@ class RegistrationServiceImplTest {
 
     @Test
     void registration_smallPassword_notOk() {
-        user.setAge(DEFAULT_AGE);
         user.setPassword(WRONG_PASSWORD);
-        user.setLogin(DEFAULT_LOGIN);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void registration_loginLengthZero_notOk() {
+        user.setLogin(WRONG_LOGIN);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         });
