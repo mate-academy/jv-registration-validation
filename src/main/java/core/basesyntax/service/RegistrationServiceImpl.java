75bed30dfe0542f0a89ca18a6c015479fe42b310
Volodymyr Kachan <vladimirkachan1996@gmail.com>
2022-08-21 19:13:14 +0300

    My home work jv-registration-validarion v01

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,38 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RuntimeException("User is null");
+        }
+        if (user.getLogin() == null) {
+            throw new RuntimeException("User login is null");
+        }
+        if (user.getLogin().length() == 0) {
+            throw new RuntimeException("User login length = 0");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User already exists");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("User age under " + MIN_AGE);
+        }
+        if (user.getAge() == null) {
+            throw new RuntimeException("User age is null");
+        }
+        if (user.getPassword() == null) {
+            throw new RuntimeException("User password is null");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password is less than "
+                    + MIN_PASSWORD_LENGTH + " characters");
+        }
+        storageDao.add(user);
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/dao/StorageDaoImplTest.java
@@ -0,0 +1,164 @@
+package core.basesyntax.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StorageDaoImplTest {
+    private static final String DEFAULT_LOGIN = "UserLogin";
+    private static final String DEFAULT_PASSWORD = "123456";
+    private static final int DEFAULT_AGE = 20;
+    private static RegistrationService registrationService;
+    private User validUser;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        validUser = new User();
+        validUser.setPassword(DEFAULT_PASSWORD);
+        validUser.setAge(DEFAULT_AGE);
+        validUser.setLogin(DEFAULT_LOGIN);
+    }
+
+    @Test
+    void register_ValidUser_OK() {
+        User actualUser = registrationService.register(validUser);
+        assertEquals(validUser, actualUser);
+        assertNotNull(actualUser.getId());
+        assertTrue(Storage.people.contains(actualUser));
+    }
+
+    @Test
+    void age_UnderMinAge_NotOK() {
+        validUser.setAge(17);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(validUser);
+        });
+    }
+
+    @Test
+    void age_UpperMinAge_OK() {
+        validUser.setAge(19);
+        User actualUser = registrationService.register(validUser);
+        assertEquals(validUser, actualUser);
+        assertNotNull(actualUser.getId());
+        assertTrue(Storage.people.contains(actualUser));
+    }
+
+    @Test
+    void age_EqualsMinAge_OK() {
+        validUser.setAge(18);
+        User actualUser = registrationService.register(validUser);
+        assertEquals(validUser, actualUser);
+        assertNotNull(actualUser.getId());
+        assertTrue(Storage.people.contains(actualUser));
+    }
+
+    @Test
+    void age_isNull_NotOK() {
+        validUser.setAge(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(validUser);
+        });
+    }
+
+    @Test
+    void age_isNegative_NotOK() {
+        validUser.setAge(-18);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(validUser);
+        });
+    }
+
+    @Test
+    void login_IsEmpty_NotOK() {
+        validUser.setLogin("");
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(validUser);
+        });
+    }
+
+    @Test
+    void login_IsNull_NotOK() {
+        validUser.setLogin(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(validUser);
+        });
+    }
+
+    @Test
+    void register_usersWithSameLogins_NotOK() {
+        registrationService.register(validUser);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(validUser);
+        });
+    }
+
+    @Test
+    void login_Valid_OK() {
+        validUser.setLogin("User2");
+        User actualUser = registrationService.register(validUser);
+        assertEquals(validUser, actualUser);
+        assertNotNull(actualUser.getId());
+        assertTrue(Storage.people.contains(actualUser));
+    }
+
+    @Test
+    void password_twoCharacters_NotOK() {
+        validUser.setPassword("ab");
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(validUser);
+        });
+    }
+
+    @Test
+    void password_Valid_OK() {
+        validUser.setPassword("1234567");
+        User actualUser = registrationService.register(validUser);
+        assertEquals(validUser, actualUser);
+        assertNotNull(actualUser.getId());
+        assertTrue(Storage.people.contains(actualUser));
+    }
+
+    @Test
+    void password_isEmpty_NotOK() {
+        validUser.setPassword("");
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(validUser);
+        });
+    }
+
+    @Test
+    void password_isNull_NotOK() {
+        validUser.setPassword(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(validUser);
+        });
+    }
+
+    @Test
+    void user_isNull_NotOK() {
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(null);
+        });
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
+}
