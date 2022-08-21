1042c4b43db868a9176cedbe2e553f496b318804
GolDenik <denik.golibroda@gmail.com>
2022-08-21 22:29:58 +0300

    fixed some mistakes after 1st student`s review

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,8 +5,11 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private static final int MIN_AGE = 18;
-    private static final int MAX_AGE = 100;
+    public static final int MIN_REGISTRATION_AGE = 18;
+    public static final int MAX_REGISTRATION_AGE = 100;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MAX_LOGIN_LENGTH = 20;
+    private static final int MAX_PASSWORD_LENGTH = 20;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -14,24 +17,24 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user == null) {
             throw new RuntimeException("User cannot be null");
         }
-        if (user.getAge() < MIN_AGE || user.getAge() > MAX_AGE) {
-            throw new RuntimeException("User`s age cannot be less than 18 and more than 100");
+        if (user.getAge() < MIN_REGISTRATION_AGE || user.getAge() > MAX_REGISTRATION_AGE
+                || user.getAge() == null) {
+            throw new RuntimeException("User`s age cannot be less than "
+                    + MIN_REGISTRATION_AGE + " and more than " + MAX_REGISTRATION_AGE);
         }
-        if (user.getId() < 0 || user.getId() == null) {
-            throw new RuntimeException("User`s id cannot be negative or empty");
-        }
-        if (user.getLogin().length() == 0 || user.getLogin() == null) {
+        if (user.getLogin().length() == 0 || user.getLogin().length() > MAX_LOGIN_LENGTH
+                || user.getLogin() == null) {
             throw new RuntimeException("User`s login cannot be empty");
         }
-        if (user.getPassword().length() < 6 || user.getPassword() == null) {
-            throw new RuntimeException("Password length cannot be less than 6 "
-                    + "and password cannot be null");
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH
+                || user.getPassword().length() > MAX_PASSWORD_LENGTH
+                || user.getPassword() == null) {
+            throw new RuntimeException("Password length cannot be less than " + MIN_PASSWORD_LENGTH
+                    + "or more than " + MAX_PASSWORD_LENGTH + " and password cannot be null");
         }
-        if (storageDao.get(user.getLogin()) == null) {
-            storageDao.add(user);
-            return user;
-        } else {
+        if (storageDao.get(user.getLogin()) != null) {
             throw new RuntimeException("User by login: " + user.getLogin() + " already exists");
         }
+        return storageDao.add(user);
     }
 }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -3,49 +3,44 @@ package core.basesyntax.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
 
     private static RegistrationService registrationService;
     private static List<User> validUsers;
-    private static List<User> unValidUsers;
     private static List<User> nullFieldUsers;
 
     @BeforeAll
     static void beforeAll() {
-        registrationService = new RegistrationServiceImpl();
-
         validUsers = new ArrayList<>();
-        validUsers.add(new User(24L, "Sofia", "sofasofa", 25));
-        validUsers.add(new User(735L, "Bogdan", "bogdanchik", 19));
-        validUsers.add(new User(65449L, "Oleg", "olejik", 27));
-        validUsers.add(new User(1649L, "Ksenia", "kseniyaa", 26));
-        validUsers.add(new User(975L, "Dima", "demon228", 27));
-
-        unValidUsers = new ArrayList<>();
-        unValidUsers.add(new User(37L, "Valid", "validpassword", 25));
-        unValidUsers.add(new User(7305L, "Valid", "bogdanchik", 19));
-        unValidUsers.add(new User(679L, "Oleg", "olejik", 17));
-        unValidUsers.add(new User(169L, "Ksenia", "ksyu", 26));
-        unValidUsers.add(new User(856L, "", "demon228", 27));
-        unValidUsers.add(new User(-234L, "Peter", "peterparker", 25));
+        validUsers.add(new User("Sofia", "sofasofa", 24));
+        validUsers.add(new User("Bogdan", "bogdanchik", 19));
+        validUsers.add(new User("Oleg", "olejik", 27));
+        validUsers.add(new User("Ksenia", "kseniyaa", 24));
+        validUsers.add(new User("Dima", "demon228", 27));
 
         nullFieldUsers = new ArrayList<>();
-        nullFieldUsers.add(new User(null, "Bony", "bonybony", 27));
-        nullFieldUsers.add(new User(62345L, null, "davidbekham", 23));
-        nullFieldUsers.add(new User(73L,"Anna", null, 30));
-        nullFieldUsers.add(new User(623L, "Greg", "greg342", null));
+        nullFieldUsers.add(new User(null, "davidbekham", 23));
+        nullFieldUsers.add(new User("Anna", null, 30));
+        nullFieldUsers.add(new User("Greg", "greg342", null));
     }
 
-    @Test
-    void register_negativeAge_NotOk() {
-        User bob = new User(51323L, "Bob", "bobobob", -19);
-        assertThrows(RuntimeException.class, () -> registrationService.register(bob));
+    @BeforeEach
+    void setUp() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
     }
 
     @Test
@@ -56,23 +51,13 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void register_unValidUsers_NotOk() {
-        assertEquals(unValidUsers.get(0), registrationService.register(unValidUsers.get(0)));
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(unValidUsers.get(1));
-        });
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(unValidUsers.get(2));
-        });
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(unValidUsers.get(3));
-        });
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(unValidUsers.get(4));
-        });
-        assertThrows(RuntimeException.class, () -> {
-            registrationService.register(unValidUsers.get(5));
-        });
+    void register_unValidLogin_NotOk() {
+        User valid = new User("Valid", "validpassword", 25);
+        assertEquals(valid, registrationService.register(valid));
+        User sameLoginUser = new User("Valid", "bogdanchik", 19);
+        assertThrows(RuntimeException.class, () -> registrationService.register(sameLoginUser));
+        User emptyLoginUser = new User("", "demon228", 27);
+        assertThrows(RuntimeException.class, () -> registrationService.register(emptyLoginUser));
     }
 
     @Test
@@ -84,9 +69,11 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_userIsAlreadyExist_NotOk() {
-        User bob = new User(5436L, "Bob", "bobikbobik", 20);
+        User bob = new User("Bob", "bobikbobik", 20);
         registrationService.register(bob);
         assertThrows(RuntimeException.class, () -> registrationService.register(bob));
+        User secondBob = new User("Bob", "bobobo123", 19);
+        assertThrows(RuntimeException.class, () -> registrationService.register(secondBob));
     }
 
     @Test
@@ -95,29 +82,35 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void register_userAgeIsIllegal_NotOk() {
-        User alice = new User(1124L, "Alice", "alice321", 16);
-        User andriy = new User(5452L, "Andriy", "andriy2000", 185);
+    void register_userAgeIsUnValid_NotOk() {
+        User kevin = new User("Kevin", "kevinz12", -23);
+        User alice = new User("Alice", "alice321", 16);
+        User andriy = new User("Andriy", "andriy2000", 185);
+        assertThrows(RuntimeException.class, () -> registrationService.register(kevin));
         assertThrows(RuntimeException.class, () -> registrationService.register(andriy));
         assertThrows(RuntimeException.class, () -> registrationService.register(alice));
     }
 
     @Test
     void register_userPasswordIsUnValid_NotOk() {
-        User john = new User(8678L, "John", "hello", 35);
-        User jeremy = new User(73745L, "Jeremy", "", 23);
+        User john = new User("John", "hello", 35);
+        User jeremy = new User("Jeremy", "", 23);
+        User johny = new User("Johny", "johnydeppjohnydepp2211", 20);
         assertThrows(RuntimeException.class, () -> registrationService.register(john));
         assertThrows(RuntimeException.class, () -> registrationService.register(jeremy));
+        assertThrows(RuntimeException.class, () -> registrationService.register(johny));
     }
 
     @Test
-    void register_fewNullFields() {
-        User becky = new User(970L, "Becky", null, null);
-        User simon = new User(null, "Simon", "simon2000", null);
-        User frida = new User(null, null, "fridayfrida", null);
-        assertThrows(RuntimeException.class, () -> registrationService.register(becky));
-        assertThrows(RuntimeException.class, () -> registrationService.register(simon));
-        assertThrows(RuntimeException.class, () -> registrationService.register(frida));
-
+    void register_LegalAgeUsers_Ok() {
+        int minAge = RegistrationServiceImpl.MIN_REGISTRATION_AGE;
+        int maxAge = RegistrationServiceImpl.MAX_REGISTRATION_AGE;
+        User expected;
+        User actual;
+        for (int i = minAge; i <= maxAge; i++) {
+            expected = new User("User" + i, "validpassword", i);
+            actual = registrationService.register(expected);
+            assertEquals(expected, actual);
+        }
     }
 }
