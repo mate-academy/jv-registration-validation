5edad7fb4022a66f346bf322529b2346373f6e96
Alina Nikiforova <0500302717a@gmail.com>
2022-08-22 17:37:40 +0200

    implements RegistrationServiceImpl

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,24 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_LENGTH_PASSWORD = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null) {
+            throw new NullPointerException("Login is null");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User already registered");
+        }
+        if (user.getAge() < MIN_AGE || user.getAge() == null) {
+            throw new RuntimeException("Age is less than " + MIN_AGE);
+        }
+        if (user.getPassword().length() < MIN_LENGTH_PASSWORD || user.getPassword() == null) {
+            throw new RuntimeException("Length password less than " + MIN_LENGTH_PASSWORD);
+        }
+        return storageDao.add(user);
     }
 }
