74c354350f6ac7570c051ad74658533152b9c6ae
Vasyl Pankiv <wasilius2@gmail.com>
2022-08-22 12:41:44 +0300

    add validateUserPassword method

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,19 +5,34 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_USER_AGE = 18;
+
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
+        if (user == null) {
+            throw new NullPointerException("User can not be Null!");
+        }
         validateUserLogin(user.getLogin());
         validateUserAge(user.getAge());
+        validateUserPassword(user.getPassword());
         return user;
+    }
+
+    private void validateUserPassword(String password) {
+        if (password == null) {
+            throw new NullPointerException("Password field can't be null");
+        } else if (password.length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password length must be at least 6");
+        }
     }
 
     private void validateUserAge(Integer age) {
         if (age == null) {
             throw new NullPointerException("User age can not be Null!");
-        } else if (age < 18) {
+        } else if (age < MIN_USER_AGE) {
             throw new RuntimeException("User is under 18 years old");
         }
     }
