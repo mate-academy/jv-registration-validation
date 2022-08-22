b30065baaba93e5e2f787a7e79f95766a908b9ea
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-08-22 14:04:09 +0200

    implemented task

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,26 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_SIZE = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null || user.getAge() == null || user.getPassword() == null
+                || user.getLogin() == null || user.getAge() < 0) {
+            throw new RegistrationServiceException("Invalid data? how? why? eh-hhh....");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationServiceException("A few years missing! Come next year!");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationServiceException("We already have your namesake!");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_SIZE) {
+            throw new RegistrationServiceException(
+                    "A good password should be longer than 6 characters!");
+        }
+        return storageDao.add(user);
     }
 }
