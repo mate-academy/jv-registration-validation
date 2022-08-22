4ce8f54774a0f3bfce8f84fdedfe4e0644eb4926
Vasyl Pankiv <wasilius2@gmail.com>
2022-08-22 12:35:54 +0300

    add validateUserAge method

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -9,18 +9,27 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        validateLogin(user);
+        validateUserLogin(user.getLogin());
+        validateUserAge(user.getAge());
         return user;
     }
 
-    private void validateLogin(User user) {
-        if (user.getLogin() == null) {
+    private void validateUserAge(Integer age) {
+        if (age == null) {
+            throw new NullPointerException("User age can not be Null!");
+        } else if (age < 18) {
+            throw new RuntimeException("User is under 18 years old");
+        }
+    }
+
+    private void validateUserLogin(String login) {
+        if (login == null) {
             throw new NullPointerException("User login can not be Null!");
-        } else if (user.getLogin().isEmpty()) {
+        } else if (login.isEmpty()) {
             throw new RuntimeException("User login can not be empty!");
-        } else if (user.getLogin().contains(" ")) {
+        } else if (login.contains(" ")) {
             throw new RuntimeException("User login can not contain white spaces!");
-        } else if (storageDao.get(user.getLogin()) != null) {
+        } else if (storageDao.get(login) != null) {
             throw new RuntimeException("Such user has already exist!!!");
         }
     }
