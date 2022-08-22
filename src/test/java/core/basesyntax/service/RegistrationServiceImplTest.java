b744b172cf9921d1c1a76e4ea9c9cdf01e61f8ae
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-08-22 16:05:01 +0200

    improve checkstyle

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,13 +1,11 @@
 package core.basesyntax.service;
 
-import core.basesyntax.model.User;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
     private static final String CORRECT_LOGIN = "login";
@@ -19,71 +17,58 @@ class RegistrationServiceImplTest {
         registrationService = new RegistrationServiceImpl();
     }
 
-@Test
-void register_nullAge_NotOk() {
+    @Test
+    void register_nullAge_NotOk() {
         User actual = new User(CORRECT_LOGIN, CORRECT_PASSWORD, null);
         checkException(actual);
-}
+    }
 
-@Test
-void register_nullPassword_NotOk() {
-    User actual = new User(CORRECT_LOGIN, null, 18);
-    checkException(actual);
-}
+    @Test
+    void register_nullPassword_NotOk() {
+        User actual = new User(CORRECT_LOGIN, null, 18);
+        checkException(actual);
+    }
 
-private void checkException(User actual) {
+    private void checkException(User actual) {
         assertThrows(RegistrationServiceException.class, () -> {
             registrationService.register(actual);
         });
-}
+    }
 
-@Test
-void register_nullLogin_NotOk() {
+    @Test
+    void register_nullLogin_NotOk() {
         User actual = new User(null, CORRECT_PASSWORD, 18);
         checkException(actual);
-}
+    }
 
-@Test
-void register_ageLess18_NotOk() {
+    @Test
+    void register_ageLess18_NotOk() {
         User actual = new User(CORRECT_LOGIN, CORRECT_PASSWORD, 17);
         checkException(actual);
-}
+    }
 
-@Test
-void register_ageLess0_NotOk() {
+    @Test
+    void register_ageLess0_NotOk() {
         User actual = new User(CORRECT_LOGIN, CORRECT_PASSWORD, -1);
         checkException(actual);
+    }
+
+    @Test
+    void register_age18_Ok() {
+        User expected = new User(CORRECT_LOGIN, CORRECT_PASSWORD, 18);
+        User actual = registrationService.register(expected);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void register_loginLengthLess6_NotOk() {
+        User expected = new User(CORRECT_LOGIN, CORRECT_PASSWORD, 18);
+        User actual = new User(CORRECT_LOGIN, CORRECT_PASSWORD, 18);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void register_addNullUser_NotOk() {
+        checkException(null);
+    }
 }
-
-@Test
-void register_age18_Ok() {
-    User expected = new User(CORRECT_LOGIN, CORRECT_PASSWORD, 18);
-    User actual = registrationService.register(expected);
-    assertEquals(expected, actual);
-}
-
-@Test
-void register_loginLengthLess6_NotOk() {
-    User expected = new User(CORRECT_LOGIN, CORRECT_PASSWORD, 18);
-    User actual = new User(CORRECT_LOGIN, CORRECT_PASSWORD, 18);
-    assertEquals(expected, actual);
-}
-
-@Test
-void register_addNullUser_NotOk() {
-    checkException(null);
-}
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
