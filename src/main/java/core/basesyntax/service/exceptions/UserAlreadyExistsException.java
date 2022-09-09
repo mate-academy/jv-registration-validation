61f841c0ac3056b884ef3c0dcbd9deb367a6e1b4
Vladyslav Boholii <vlad.boholii@gmail.com>
2022-09-09 14:44:42 +0300

    added RegistrationService implementation and tests

---
+++ src/main/java/core/basesyntax/service/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.service.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
