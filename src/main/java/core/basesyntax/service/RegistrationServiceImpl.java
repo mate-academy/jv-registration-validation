301b30d1799927f5c99815c715718b5817935a89
Oleg Pruh <olegpruh1@gmail.com>
2022-09-16 17:47:40 +0100

    Task completed

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,35 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RuntimeException("User can't be null");
+        }
+        if (user.getLogin() == null || user.getPassword() == null
+                || user.getAge() == null) {
+            throw new RuntimeException("User has null values");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User already exists");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("User must not be under " + MIN_AGE);
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password must be longer than "
+                    + MIN_PASSWORD_LENGTH + " characters");
+        }
+        if (!user.getLogin().contains("@")) {
+            throw new RuntimeException("'@' must be present");
+        }
+        if (!user.getLogin().endsWith("@gmail.com")) {
+            throw new RuntimeException("User login is not correct");
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,69 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+
+    @BeforeEach
+    void beforeEach() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @Test
+    void userCannotBeNull_NotOK() {
+        assertThrows(RuntimeException.class, () ->
+                registrationService.register(null));
+    }
+
+    @Test
+    void isUserHasNullParameter_NotOK() {
+        assertThrows(RuntimeException.class, () ->
+                registrationService.register(
+                        new User("without_age@gmail.com", "password", null)
+                ));
+    }
+
+    @Test
+    void userIsAlreadyExists_NotOK() {
+        registrationService.register(new User("bob@gmail.com", "password", 23));
+        assertThrows(RuntimeException.class, () ->
+                registrationService.register(new User("bob@gmail.com", "bob2003", 19)));
+    }
+
+    @Test
+    void userIsTooYoung_NotOK() {
+        assertThrows(RuntimeException.class, () ->
+                registrationService.register(new User("too_young@gmail.com", "password", 15)));
+    }
+
+    @Test
+    void passwordIsTooShort_NotOK() {
+        assertThrows(RuntimeException.class, () ->
+                registrationService.register(new User("alice@gmail.com", "123", 19)));
+    }
+
+    @Test
+    void emailSymbolMustBePresent_NotOK() {
+        assertThrows(RuntimeException.class, () ->
+                registrationService.register(new User("incorrect_login2", "password", 24)));
+    }
+
+    @Test
+    void emailIsNotCorrect_NotOK() {
+        assertThrows(RuntimeException.class, () ->
+                registrationService.register(new User("incorrect_login1@", "password", 28)));
+    }
+
+    @Test
+    void correctUserValues_OK() {
+        User user = new User("correct@gmail.com", "password", 28);
+        assertEquals(registrationService.register(user), user);
+    }
+}
