e6c2ea7a93c4cc92e0fde930598f980a5917b699
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-09-19 19:04:50 +0300

    Implement class methods

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,35 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    protected static final int PASSWORD_MIN_LENGTH = 6;
+    protected static final int AGE_BOUND = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
-
     @Override
     public User register(User user) {
-        return null;
+        if(user == null) {
+            throw new RuntimeException("User is null.");
+        }
+        if(user.getLogin() == null){
+            throw new RuntimeException("User login is null.");
+        }
+        if(user.getPassword() == null){
+            throw new RuntimeException("Password login is null.");
+        }
+        if(user.getAge() == null){
+            throw new RuntimeException("Age login is null.");
+        }
+        if(user.getAge() < 0){
+            throw new RuntimeException("Age must be positive");
+        }
+        if(user.getAge() < AGE_BOUND){
+            throw new RuntimeException("Age must be equal or more than " + AGE_BOUND + ".");
+        }
+        if(user.getPassword().length() < PASSWORD_MIN_LENGTH){
+            throw new RuntimeException("Password length must be equal or more than " + PASSWORD_MIN_LENGTH + ".");
+        }
+        if(storageDao.get(user.getLogin()) != null ){
+            throw new RuntimeException("User with login " + user.getLogin() + " is already exists in the database.");
+        }
+        return storageDao.add(user);
     }
 }
