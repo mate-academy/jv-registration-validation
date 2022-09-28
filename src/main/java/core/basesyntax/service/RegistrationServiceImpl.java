8ee79a97a54d48e60318bd3a7ec56d16eb18bfb7
Ivan Litvynets <Litvyvanya@gmail.com>
2022-09-29 00:09:50 +0300

    RegistrationService_implementation

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,44 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MINIUM_PASSWORD_LENGTH = 8;
+    private static final int MIN_AGE = 18;
+    private static final int MAX_AGE = 120;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new NullPointerException("user is null");
+        }
+        if (user.getLogin() == null
+                || user.getLogin() == ""
+                || !Character.isLetter(user.getLogin().charAt(0))
+                || user.getLogin().contains("\\W")) {
+            throw new RuntimeException("incorrect login");
+        } else {
+            if (storageDao.get(user.getLogin()) != null) {
+                throw new RuntimeException("login already exists");
+            }
+        }
+        if (user.getPassword() == null
+                || user.getPassword() == "") {
+            throw new RuntimeException("password cannot be empty");
+        } else {
+            if (user.getPassword().length() < MINIUM_PASSWORD_LENGTH) {
+                throw new RuntimeException("password must contain at least 8 characters");
+            }
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("user must be over 18 years old");
+        }
+        if (user.getAge() > MAX_AGE) {
+            throw new RuntimeException("Incorrect age");
+        }
+        User checkedUser = new User();
+        checkedUser.setAge(user.getAge());
+        checkedUser.setLogin(user.getLogin().trim());
+        checkedUser.setPassword(user.getPassword());
+        return storageDao.add(checkedUser);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,110 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private RegistrationServiceImpl regService = new RegistrationServiceImpl();
+    private User user = new User();
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setLogin("Ivan_Litvynets");
+        user.setPassword("qwerty123456");
+        user.setAge(23);
+        Storage.people.clear();
+    }
+
+    @Test
+    void register_User_Ok() {
+        assertEquals(user, regService.register(user));
+        assertTrue(Storage.people.contains(user));
+    }
+
+    @Test
+    void register_NullLogin_NotOk() {
+        user.setLogin(null);
+        assertThrows(RuntimeException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void register_EmptyLogin_NotOk() {
+        user.setLogin("");
+        assertThrows(RuntimeException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void register_LoginWithSpecialSymbols_NotOk() {
+        user.setLogin("&Ivan_Litvynets");
+        assertThrows(RuntimeException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void register_PasswordEmpty_notOk() {
+        user.setPassword("");
+        assertThrows(RuntimeException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void register_NullPassword_NotOk() {
+        user.setPassword(null);
+        assertThrows(RuntimeException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void register_PasswordLength_NotOk() {
+        user.setPassword("qwerty");
+        assertThrows(RuntimeException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void register_UserAlreadyExist() {
+        regService.register(user);
+        assertThrows(RuntimeException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void register_YoungAge_notOk() {
+        user.setAge(12);
+        assertThrows(RuntimeException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void register_VeryBigAge_NotOk() {
+        user.setAge(Integer.MAX_VALUE);
+        assertThrows(RuntimeException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void register_IncorrectAge_NotOk() {
+        user.setAge(Integer.MIN_VALUE);
+        assertThrows(RuntimeException.class, () -> {
+            regService.register(user);
+        });
+    }
+}
