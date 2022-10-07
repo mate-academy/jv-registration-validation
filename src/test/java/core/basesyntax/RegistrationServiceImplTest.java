00c9829668962b0343a2bf04daaf7627a9594abb
Anatolii Bespalko <tolyan9901@gmail.com>
2022-10-07 14:32:35 +0300

    Fix test, common fixe

--- src/test/java/core/basesyntax/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -3,8 +3,6 @@ package core.basesyntax;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import core.basesyntax.dao.StorageDao;
-import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationService;
@@ -15,12 +13,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    private static RegistrationService testRegistration;
+    private static RegistrationService testRegistrationService;
     private User testUser;
 
     @BeforeAll
-    static void atStartOnce() {
-        testRegistration = new RegistrationServiceImpl();
+    static void atStartOnce() {        
+        testRegistrationService = new RegistrationServiceImpl();
     }
 
     @BeforeEach
@@ -38,62 +36,61 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_nonExistentUser_ok() {
-        assertEquals(testUser, testRegistration.register(testUser));
+        assertEquals(testUser, testRegistrationService.register(testUser));
     }
 
     @Test
     void register_nullUser_notOk() {
         testUser = null;
         assertThrows(RuntimeException.class, ()
-                -> testRegistration.register(testUser));
+                -> testRegistrationService.register(testUser));
     }
 
     @Test
-    void register_gotExistingUsername_notOk() {
-        StorageDao storage = new StorageDaoImpl();
-        User newUser1 = testRegistration.register(testUser);
-        User newUser2 = storage.get(testUser.getLogin());
-        assertEquals(newUser1, newUser2);
+    void register_existingUsername_notOk() {
+        Storage.people.add(testUser);
+        assertThrows(RuntimeException.class, ()
+                -> testRegistrationService.register(testUser));
     }
 
     @Test
-    void register_gotNullLogin_notOk() {
+    void register_nullLogin_notOk() {
         testUser.setLogin(null);
         assertThrows(RuntimeException.class, ()
-                -> testRegistration.register(testUser));
+                -> testRegistrationService.register(testUser));
     }
 
     @Test
-    void register_gotEmptyLogin_notOk() {
+    void register_emptyLogin_notOk() {
         testUser.setLogin("");
         assertThrows(RuntimeException.class, ()
-                -> testRegistration.register(testUser));
+                -> testRegistrationService.register(testUser));
     }
 
     @Test
-    void register_gotShortPassword_notOk() {
+    void register_shortPassword_notOk() {
         testUser.setPassword("55555");
         assertThrows(RuntimeException.class, ()
-                -> testRegistration.register(testUser));
+                -> testRegistrationService.register(testUser));
     }
 
     @Test
-    void register_gotEmptyPassword_notOk() {
+    void register_emptyPassword_notOk() {
         testUser.setPassword("");
         assertThrows(RuntimeException.class, ()
-                -> testRegistration.register(testUser));
+                -> testRegistrationService.register(testUser));
     }
 
     @Test
-    void register_gotNullAge_notOk() {
+    void register_nullAge_notOk() {
         assertThrows(RuntimeException.class, ()
-                -> testRegistration.register(null));
+                -> testRegistrationService.register(null));
     }
 
     @Test
-    void register_gotLowAge_notOk() {
+    void register_lowAge_notOk() {
         testUser.setAge(17);
         assertThrows(RuntimeException.class, ()
-                -> testRegistration.register(testUser));
+                -> testRegistrationService.register(testUser));
     }
 }
