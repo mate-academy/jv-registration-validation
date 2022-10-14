7605e85a6bb56fb79603ae63aab2a14bdfe9feeb
Anton Pavliuk <anton.pavliuk20@gmail.com>
2022-10-14 14:54:58 +0300

    Implement RegistrationService and create test class

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -9,6 +9,16 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User already registered");
+        }
+        if (user.getAge() < 18) {
+            throw new RuntimeException("User must be at least 18 years old");
+        }
+        if (user.getPassword().length() < 6) {
+            throw new RuntimeException("Password must be at least 6 characters");
+        }
+        storageDao.add(user);
+        return user;
     }
 }
