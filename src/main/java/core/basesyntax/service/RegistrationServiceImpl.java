b31eb3b7fd3ad16414bf5d4b114e492ddeb7e546
galenkoigor <galenkoigor@gmail.com>
2022-10-16 22:30:33 +0200

    implemented method register(User user) in RegistrationServiceImpl class

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -4,11 +4,28 @@ import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
+import java.util.Comparator;
+import java.util.Objects;
+
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final String USER_EXISTS_MSG = "User already exists in DB";
+    private static final String USER_ADULT_MSG = "User must be adult (18+)";
+    private static final String PASSWORD_LENGTH_MSG = "Password must be 6 symbols at least";
+    private static final int MINIMUM_USER_AGE = 18;
+    private static final int MINIMUM_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (Objects.nonNull(storageDao.get(user.getLogin()))) {
+            throw new RuntimeException(USER_EXISTS_MSG);
+        }
+        if (Objects.compare(user.getAge(), MINIMUM_USER_AGE, Comparator.naturalOrder()) < 0) {
+            throw new RuntimeException(USER_ADULT_MSG);
+        }
+        if (Objects.compare(user.getPassword().length(), MINIMUM_PASSWORD_LENGTH, Comparator.naturalOrder()) < 0) {
+            throw new RuntimeException(PASSWORD_LENGTH_MSG);
+        }
+        return storageDao.add(user);
     }
 }
