af17de9784b8c3e4cd68a0729352e3a2ef88fdd4
Serhii <romanchuk552@gmail.com>
2022-10-18 15:31:02 +0300

    Registration-Service completed

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,29 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int EIGHTEEN_YEARS_OLD = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new NullPointerException("User can't be Null");
+        }
+        if (Storage.people.contains(user)) {
+            throw new RuntimeException("User already exist");
+        }
+        if (user.getAge() < EIGHTEEN_YEARS_OLD) {
+            throw new RuntimeException("User to young");
+        }
+        if (user.getPassword().length() < 6) {
+            throw new RuntimeException("User's password to short, it mast consist of 6 character");
+        }
+        storageDao.add(user);
+
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -0,0 +1,55 @@
+package core.basesyntax;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationServiceImplTest {
+    private static RegistrationServiceImpl registrationService;
+    private User user1 = new User("spider@gmail.com", "qwerty12345", 25);
+
+    @BeforeAll
+    public static void setUp() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @Test
+    public void register_AlreadyExistedUser_notOk() {
+        registrationService.register(user1);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user1));
+    }
+
+    @Test
+    public void register_nullUser_notOk() {
+        user1 = null;
+        assertThrows(NullPointerException.class, () -> registrationService.register(user1));
+    }
+
+    @Test
+    public void register_adult_notOk() {
+        user1.setAge(17);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user1));
+    }
+
+    @Test
+    public void register_adult_isOk() {
+        user1.setAge(19);
+        assertEquals(user1, registrationService.register(user1));
+    }
+
+    @Test
+    public void register_lengthOfPassword_notOk() {
+        user1.setPassword("sdfsd");
+        assertThrows(RuntimeException.class, () -> registrationService.register(user1));
+    }
+
+    @Test
+    public void register_lengthOfPassword_isOk() {
+        user1.setPassword("sdfsddd");
+        assertEquals(user1, registrationService.register(user1));
+    }
+}
