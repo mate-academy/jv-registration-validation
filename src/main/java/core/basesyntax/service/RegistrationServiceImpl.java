fb441f73d140660e96415f09b2bbe1e42d860a89
Volodymyr Oliinyk <volodymyr.oliinyk89@gmail.com>
2022-10-19 16:36:20 +0300

    added fixes after review

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -11,25 +11,37 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
+        validateLogin(user);
+        validatePassword(user);
+        validateAge(user);
+        return storageDao.add(user);
+    }
+
+    private void validateLogin(User user) {
         if (user.getLogin() == null) {
             throw new NullPointerException("Login can't be null");
-        }
-        if (user.getPassword() == null) {
-            throw new NullPointerException("Password can't be null");
-        }
-        if (user.getAge() == null) {
-            throw new NullPointerException("Age can't be null");
         }
         if (storageDao.get(user.getLogin()) != null) {
             throw new RuntimeException("It's already exists user by login " + user.getLogin());
         }
-        if (user.getAge() <= ADULT_AGE) {
-            throw new RuntimeException("Can't register user with age " + user.getAge());
+    }
+
+    private void validatePassword(User user) {
+        if (user.getPassword() == null) {
+            throw new NullPointerException("Password can't be null");
         }
         if (user.getPassword().length() < MIN_PASS_LENGTH) {
             throw new RuntimeException("Password must contains at least "
                     + MIN_PASS_LENGTH + " characters");
         }
-        return storageDao.add(user);
+    }
+
+    private void validateAge(User user) {
+        if (user.getAge() == null) {
+            throw new NullPointerException("Age can't be null");
+        }
+        if (user.getAge() <= ADULT_AGE) {
+            throw new RuntimeException("Can't register user with age " + user.getAge());
+        }
     }
 }
