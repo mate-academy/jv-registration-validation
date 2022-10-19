f7cb9dbbc7230f943c8d1e6d5ab7d9d4f6348b54
galenkoigor <galenkoigor@gmail.com>
2022-10-19 13:56:13 +0200

    refactored code with mentor's recommendations

--- src/test/java/core/basesyntax/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -13,6 +13,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
+    private static final String VALID_PASS = "more_than_six";
+    private static final String INVALID_PASS = "four";
+    private static final int VALID_AGE = 25;
+    private static final int INVALID_AGE = 5;
     private static User defaultUser;
     private static User anotherUser;
     private static RegistrationService service;
@@ -20,32 +24,32 @@ class RegistrationServiceImplTest {
     @BeforeAll
     static void beforeAll() {
         service = new RegistrationServiceImpl();
-        defaultUser = new User();
-        anotherUser = new User();
+        defaultUser = new User("Mate", VALID_PASS, VALID_AGE);
+        anotherUser = new User("Academic", VALID_PASS, VALID_AGE);
     }
 
     @BeforeEach
     void setUp() {
         defaultUser.setLogin("Mate");
-        defaultUser.setPassword("more_than_six");
-        defaultUser.setAge(25);
+        defaultUser.setAge(VALID_AGE);
+        defaultUser.setPassword(VALID_PASS);
         anotherUser.setLogin("Academic");
-        anotherUser.setPassword("six_or_more");
-        anotherUser.setAge(50);
+        anotherUser.setAge(VALID_AGE);
+        anotherUser.setPassword(VALID_PASS);
         Storage.people.clear();
     }
 
     @Test
     void register_nullAge_notOk() {
         defaultUser.setAge(null);
-        assertThrows(RuntimeException.class, () -> service.register(defaultUser));
+        assertThrows(RuntimeException.class, () -> service.register(defaultUser), "");
     }
 
     @Test
     void register_lowAge_notOk() {
-        defaultUser.setAge(0);
+        defaultUser.setAge(INVALID_AGE);
         assertThrows(RuntimeException.class, () -> service.register(defaultUser));
-        defaultUser.setAge(17);
+        defaultUser.setAge(Integer.MIN_VALUE);
         assertThrows(RuntimeException.class, () -> service.register(defaultUser));
     }
 
@@ -71,7 +75,7 @@ class RegistrationServiceImplTest {
     void register_shortPassword_notOk() {
         defaultUser.setPassword("");
         assertThrows(RuntimeException.class, () -> service.register(defaultUser));
-        defaultUser.setPassword("12345");
+        defaultUser.setPassword(INVALID_PASS);
         assertThrows(RuntimeException.class, () -> service.register(defaultUser));
     }
 
@@ -101,7 +105,6 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_notNullId_ok() {
-
         assertNotEquals(null, service.register(defaultUser).getId());
     }
 
