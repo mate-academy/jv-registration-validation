3d2e344fe2c81a399616c937993fcca66f37e6ac
Andrew Miroshnikov <giggle2594@gmail.com>
2022-10-21 23:39:13 +0300

    Create my first tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,30 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private final StorageDao storageDao = new StorageDaoImpl();
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASS_LENGTH = 6;
+    private StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RuntimeException("User can't be null");
+        }
+        if (user.getLogin().equals(null)) {
+            throw new RuntimeException("Login can't be null");
+        }
+        if (user.getPassword().equals(null)) {
+            throw new RuntimeException("Password can't be null");
+        }
+        if (user.getPassword().length() < MIN_PASS_LENGTH) {
+            throw new RuntimeException("Minimum " + MIN_PASS_LENGTH + " characters");
+        }
+        if (user.getAge() == null) {
+            throw new RuntimeException("Age can't be null");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("Age less than " + MIN_AGE);
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,101 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+    private User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setLogin("UserMate");
+        user.setPassword("Mate2022");
+        user.setAge(33);
+    }
+
+    @Test
+    void userLogin_notOk() {
+        user.setLogin(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void userPasswordNull_notOk() {
+        user.setPassword(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void userAgeNull_notOk() {
+        user.setAge(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void userAge_notOk() {
+        user.setAge(17);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void userAge_Ok() {
+        user.setAge(18);
+        registrationService.register(user);
+        Long actual = user.getId();
+        Long expected = 2L;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void userNegativeAge_notOk() {
+        user.setAge(-18);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void userPassword_notOk() {
+        user.setPassword("55555");
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void userNull_notOk() {
+        user = null;
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void user_ok() {
+        user.setLogin("Login");
+        registrationService.register(user);
+        Long actual = user.getId();
+        Long expected = 1L;
+        assertEquals(expected, actual);
+    }
+}
