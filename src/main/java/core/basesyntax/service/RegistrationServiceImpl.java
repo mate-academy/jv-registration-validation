1a1fd42edc29d8385bdc0658e80d84176e7f4242
Yulia Tsariv <purplesquirrel68@gmail.com>
2022-10-23 15:31:59 +0300

    Realize tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,43 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private final StorageDao storageDao = new StorageDaoImpl();
+    private static final StorageDao storageDao = new StorageDaoImpl();
+    private static final int MIN_AGE_OF_USERS=18;
+    private static final int MIN_LENGTH_OF_PASSWORD=6;
 
     @Override
     public User register(User user) {
-        return null;
+        checkAge(user);
+        checkPassword(user);
+        checkLogin(user);
+        storageDao.add(user);
+        return user;
+    }
+
+    public static void checkPassword(User user){
+        if(user.getPassword()==null){
+            throw new NullPointerException("Password cannot be null");
+        }
+        if(user.getPassword().length()<MIN_LENGTH_OF_PASSWORD){
+            throw new RuntimeException("Minimal length of password is "+MIN_LENGTH_OF_PASSWORD);
+        }
+    }
+
+    public static void checkAge(User user){
+        if(user.getAge()==null){
+            throw  new NullPointerException("Age connot be null");
+        }
+        if(user.getAge()<MIN_AGE_OF_USERS){
+            throw new RuntimeException("Minimum user age for registration:"+ MIN_AGE_OF_USERS+" . Your age is not suitable");
+        }
+    }
+
+    public static void checkLogin(User user){
+        if(user.getLogin()==null){
+            throw new NullPointerException("Login cannot be null");
+        }
+        if(user.equals(storageDao.get(user.getLogin()))) {
+            throw new RuntimeException("User " + user.getLogin() + " alreasy exist");
+        }
     }
 }
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,8 +1,71 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorldTest {
 
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class HelloWorldTest {
+   private static RegistrationServiceImpl registrationService;
+    User rightUser;
+    User usWithWrongPass;
+    User usWithWrongAge;
+
+    @BeforeAll
+           static void beforeAll(){
+         registrationService=new RegistrationServiceImpl();
+    }
+    @BeforeEach
+void setUp(){
+rightUser=new User();
+rightUser.setId(2L);
+rightUser.setAge(22);
+rightUser.setPassword("Mypassss12");
+rightUser.setLogin("YuraBorly");
+        usWithWrongPass=new User();
+        usWithWrongPass.setId(2L);
+        usWithWrongPass.setAge(25);
+        usWithWrongPass.setPassword("Kata");
+        usWithWrongPass.setLogin("Katherine12");
+        usWithWrongAge=new User();
+        usWithWrongAge.setId(2L);
+        usWithWrongAge.setAge(15);
+        usWithWrongAge.setPassword("MaryMeds1");
+        usWithWrongAge.setLogin("MariaMedsia");
+    }
+    @Test
+    void rightUser(){
+        User user= registrationService.register(rightUser);
+        assertEquals(user,rightUser);
+    }
+    @Test
+    void wrongPassword(){
+        assertThrows(RuntimeException.class,()->{
+            registrationService.register(usWithWrongPass);
+        });
+    }
+    @Test
+    void wrongAge(){
+        assertThrows(RuntimeException.class,()->{
+            registrationService.register(usWithWrongAge);
+        });
+    }
+    @Test
+    void userAlreadyExist(){
+        assertThrows(RuntimeException.class,()->{
+            registrationService.register(rightUser);
+        });
+    }
+
+    @Test
+void register_nullAge_notOk(){
+        assertThrows(NullPointerException.class,()->{
+            registrationService.register(null);
+        });
+    }
 }
+
