9f186215891775082650092a5138780ce39623e4
Bohdan Vyshnevskyy <yksvenhsiv@gmail.com>
2022-10-25 20:22:24 +0300

    solution 3.0

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -3,6 +3,8 @@ package core.basesyntax.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.AfterEach;
@@ -11,62 +13,57 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    public static final String CORRECT_LOGIN = "login@gmail.com";
-    public static final String CORRECT_PASSWORD = "password";
-    public static final String INCORRECT_PASSWORD = "pass";
-    public static final int CORRECT_AGE = 77;
-    public static final int INCORRECT_AGE = 17;
-    public static final long CORRECT_ID = 777777L;
+    private static final String CORRECT_LOGIN = "login@gmail.com";
+    private static final String SHORT_PASSWORD = "password";
+    private static final String INCORRECT_PASSWORD = "pass";
+    private static final int CORRECT_AGE = 77;
+    private static final int INCORRECT_AGE = 17;
     private static RegistrationService registrationService;
+    private static StorageDao storageDao;
     private User user;
 
     @BeforeAll
     static void beforeAll() {
         registrationService = new RegistrationServiceImpl();
+        storageDao = new StorageDaoImpl();
     }
 
     @BeforeEach
     void setUp() {
         user = new User();
         user.setLogin(CORRECT_LOGIN);
-        user.setPassword(CORRECT_PASSWORD);
+        user.setPassword(SHORT_PASSWORD);
         user.setAge(CORRECT_AGE);
-        user.setId(CORRECT_ID);
     }
 
     @Test
     void register_nullAge_notOk() {
         user.setAge(null);
-        assertThrows(NullPointerException.class, () -> registrationService.register(user));
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_nullLogin_notOk() {
         user.setLogin(null);
-        assertThrows(NullPointerException.class, () -> registrationService.register(user));
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_nullPassword_notOk() {
         user.setPassword(null);
-        assertThrows(NullPointerException.class, () -> registrationService.register(user));
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_nonExistsUser_ok() {
-        assertEquals(user, registrationService.register(user));
+        registrationService.register(user);
+        assertEquals(user, storageDao.get(user.getLogin()));
     }
 
     @Test
     void register_alreadyExistsUser_notOk() {
         Storage.people.add(user);
         assertThrows(RuntimeException.class, () -> registrationService.register(user));
-    }
-
-    @Test
-    void register_age_ok() {
-        user.setAge(CORRECT_AGE);
-        assertEquals(user, registrationService.register(user));
     }
 
     @Test
@@ -77,12 +74,12 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_passLength_ok() {
-        user.setPassword(CORRECT_PASSWORD);
+        user.setPassword(SHORT_PASSWORD);
         assertEquals(user, registrationService.register(user));
     }
 
     @Test
-    void register_passLength_notOk() {
+    void register_shortPassword_notOk() {
         user.setPassword(INCORRECT_PASSWORD);
         assertThrows(RuntimeException.class, () -> registrationService.register(user));
     }
