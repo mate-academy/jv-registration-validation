281db87942190375a200de8e3217601b81314319
Denys Domashevskyi <denis.domashevskiy28@gmail.com>
2022-10-25 23:26:38 +0300

    Implemented RegistrationServiceImpl class and made tests for it

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,42 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int AGE_ALLOW = 18;
+    private static final int LENGTH_ALLOW = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RuntimeException("User can't be: null!");
+        }
+        loginCheck(user.getLogin());
+        ageCheck(user.getAge());
+        passwordLengthCheck(user.getPassword().length());
+        return storageDao.add(user);
+    }
+
+    private void loginCheck(String login) {
+        if (login == null) {
+            throw new RuntimeException("Login can't be null!");
+        }
+        if (storageDao.get(login) != null) {
+            throw new RuntimeException("There is already exist user with such login!");
+        }
+    }
+
+    private void ageCheck(Integer age) {
+        if (age == null) {
+            throw new RuntimeException("Age can't be null!");
+        }
+        if (age < AGE_ALLOW) {
+            throw new RuntimeException("User age is less than expected!");
+        }
+    }
+
+    private void passwordLengthCheck(Integer passwordLength) {
+        if (passwordLength < LENGTH_ALLOW) {
+            throw new RuntimeException("Your password has less than 6 characters");
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,163 @@
+package core.basesyntax.service;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private static StorageDao storageDao;
+    private static RegistrationService registrationService;
+    private User mark;
+    private User sasha;
+    private User bob;
+    private User alice;
+    private User denis;
+
+    @BeforeAll
+    static void beforeAll() {
+        storageDao = new StorageDaoImpl();
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        mark = new User("Mark","123456", 18);
+        sasha = new User("Sasha","ValidPassword", 20);
+        bob = new User("Bob","short", 21);
+        denis = new User("Denis","isValid", 17);
+        alice = new User("Alice","PASSWORD", 19);
+    }
+
+    @Test
+    void setNullUser_NotOk() {
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(null);
+        });
+    }
+
+    @Test
+    void setNullLogin_NotOk() {
+        mark.setLogin(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(mark);
+        });
+    }
+
+    @Test
+    void setNullAge_NotOk() {
+        mark.setAge(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(mark);
+        });
+    }
+
+    @Test
+    void setNullPassword_NotOk() {
+        mark.setPassword(null);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(mark);
+        });
+    }
+
+    @Test
+    void userAlreadyExistWithSuchLogin_NotOk() {
+        storageDao.add(mark);
+        storageDao.add(sasha);
+        storageDao.add(bob);
+        storageDao.add(alice);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(new User("Mark", "654321", 20));
+        });
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(new User("Sasha","qwerty", 22));
+        });
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(new User("Bob", "asdfGH", 19));
+        });
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(new User("Alice", "LARGE_LETTERS", 18));
+        });
+    }
+
+    @Test
+    void userNotExistWithSuchLogin_Ok() {
+        User expected = new User("Mark", "asdq123", 20);
+        User actual = registrationService.register(mark);
+        assertNotEquals(expected, actual);
+    }
+
+    @Test
+    void setNotValidAge_NotOk() {
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(denis);
+        });
+    }
+
+    @Test
+    void setNegativeAge_NotOk() {
+        denis.setAge(-18);
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(denis);
+        });
+    }
+
+    @Test
+    void setAgeEqualsAllowAge_Ok() {
+        denis.setAge(18);
+        storageDao.add(denis);
+        assertEquals(denis, storageDao.get(denis.getLogin()));
+    }
+
+    @Test
+    void setAgeBiggerAgeThanAllow_Ok() {
+        denis.setAge(20);
+        storageDao.add(denis);
+        assertEquals(denis, storageDao.get(denis.getLogin()));
+    }
+
+    @Test
+    void setPasswordWithLengthLessThanAllow_NotOk() {
+        assertThrows(RuntimeException.class, () -> {
+            registrationService.register(bob);
+        });
+    }
+
+    @Test
+    void setPasswordWithLengthMoreThanAllow_Ok() {
+        bob.setPassword("tooLong123435132412346");
+        assertEquals(bob, registrationService.register(bob));
+    }
+
+    @Test
+    void setPasswordWithLengthThatEqualsAllow_Ok() {
+        bob.setPassword("123456");
+        assertEquals(bob, registrationService.register(bob));
+    }
+
+    @Test
+    void setPasswordWithSymbols_Ok() {
+        bob.setPassword("!@#@$#$!&*^*_-");
+        assertEquals(bob, registrationService.register(bob));
+    }
+
+    @Test
+    void setUserWithValidParameters_Ok() {
+        storageDao.add(sasha);
+        storageDao.add(alice);
+        assertEquals(sasha, storageDao.get(sasha.getLogin()));
+        assertEquals(alice, storageDao.get(alice.getLogin()));
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
+}
