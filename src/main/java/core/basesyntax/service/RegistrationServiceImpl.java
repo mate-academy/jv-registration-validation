0dd96a4547c9c743496217d94c18762a56ec09fb
yehbreus <yeh.breus@gmail.com>
2022-10-25 15:08:05 +0300

    fixes after mvncp

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,11 +5,11 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private final StorageDao storageDao = new StorageDaoImpl();
-    private static final int PASSWORD_MIN_LENGTH = 6;
     private static final String DEFAULT_EXCEPTION_TEXT = "Can't register this user. ";
+    private static final int PASSWORD_MIN_LENGTH = 6;
     private static final String EMPTY_STRING = "";
     private static final int MIN_USER_AGE = 18;
+    private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
@@ -25,39 +25,45 @@ public class RegistrationServiceImpl implements RegistrationService {
         }
     }
 
-    private void validateUserFields (User user) {
+    private void validateUserFields(User user) {
         if (user.getLogin() == null || user.getAge() == null || user.getPassword() == null) {
-            throw new RuntimeException(DEFAULT_EXCEPTION_TEXT + "User login/password/age can't be null");
+            throw new RuntimeException(DEFAULT_EXCEPTION_TEXT
+                    + "User login/password/age can't be null");
         }
         validateLogin(user);
         validatePassword(user);
         validateUserAge(user);
     }
 
-    private void validateLogin (User user) {
+    private void validateLogin(User user) {
         if (user.getLogin().equals(EMPTY_STRING)) {
-            throw new RuntimeException(DEFAULT_EXCEPTION_TEXT + "Login can't be empty");
+            throw new RuntimeException(DEFAULT_EXCEPTION_TEXT
+                    + "Login can't be empty");
         }
     }
 
-    private void validatePassword (User user) {
+    private void validatePassword(User user) {
         if (user.getPassword().equals(EMPTY_STRING)) {
-            throw new RuntimeException(DEFAULT_EXCEPTION_TEXT + "Password can't be empty");
+            throw new RuntimeException(DEFAULT_EXCEPTION_TEXT
+                    + "Password can't be empty");
         }
         if (user.getPassword().length() < PASSWORD_MIN_LENGTH) {
-            throw new RuntimeException(DEFAULT_EXCEPTION_TEXT + "Password must contain more than 6 symbols");
+            throw new RuntimeException(DEFAULT_EXCEPTION_TEXT
+                    + "Password must contain more than 6 symbols");
         }
     }
 
-    private void validateUserAge (User user) {
+    private void validateUserAge(User user) {
         if (user.getAge() < MIN_USER_AGE) {
-            throw new RuntimeException(DEFAULT_EXCEPTION_TEXT + "User must be of legal age");
+            throw new RuntimeException(DEFAULT_EXCEPTION_TEXT
+                    + "User must be of legal age");
         }
     }
 
-    private void validateUserNonDuplicates (User user) {
+    private void validateUserNonDuplicates(User user) {
         if (storageDao.get(user.getLogin()) != null) {
-            throw new RuntimeException(DEFAULT_EXCEPTION_TEXT + "User already exist");
+            throw new RuntimeException(DEFAULT_EXCEPTION_TEXT
+                    + "User already exist");
         }
     }
 }
