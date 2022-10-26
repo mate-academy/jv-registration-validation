c6026d76076aa66e16c1faa827d366e6a16b726c
Oleksandr <oleksandrkorniychuk98@gmail.com>
2022-10-26 09:47:44 +0300

    Implemented method User register. Created tests for register (92%)

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,24 +6,24 @@ import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
-    private static final int MIN_AGE = 18;
-    private static final int MIN_VALUE_OF_PASSWORD = 6;
 
     @Override
     public User register(User user) {
+        final int minValueOfPassword = 6;
+        final int minAge = 18;
+
         if (user.getLogin() == null) {
             throw new RuntimeException("Login can't be null");
         }
         if (user.getAge() == null) {
             throw new RuntimeException("No valid age");
         }
-        if (user.getPassword() == null || user.getPassword().length() < MIN_VALUE_OF_PASSWORD) {
+        if (user.getPassword() == null || user.getPassword().length() < minValueOfPassword) {
             throw new RuntimeException("No valid password");
         }
-        if (user.getAge() < MIN_AGE) {
+        if (user.getAge() < minAge) {
             throw new RuntimeException("Age must be greater than 18");
         }
-
         if (storageDao.get(user.getLogin()).getLogin() == null) {
             storageDao.add(user);
         }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,15 +1,15 @@
 package core.basesyntax.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RegistrationServiceImplTest {
-
-    RegistrationService registrationService  = new RegistrationServiceImpl();
+    private final RegistrationService registrationService = new RegistrationServiceImpl();
     private final StorageDao storageDao = new StorageDaoImpl();
     private final User defaultUser = new User();
     private final String correctLogin = "Johnson";
