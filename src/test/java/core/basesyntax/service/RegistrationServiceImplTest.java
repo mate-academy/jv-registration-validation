3ceb413c78b1ff376e55e773d0a705c83bdea2bd
thebitler <yurchyknikita@gmail.com>
2022-10-26 16:45:14 +0200

    Added constants

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -11,15 +11,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
+    private static final String USER_NAME_1 = "Bob";
+    private static final String USER_PASSWORD_1 = "123456";
+    private static final int USER_AGE_1 = 18;
+    private static final String EMPTY_STRING = "";
+    private static final String WHITESPACE_CHARACTERS_STRING = "  \t\n";
+    private static final String INVALID_PASSWORD_ABCDE = "abcde";
+    private static final int INVALID_AGE = 17;
+    private static final int INVALID_AGE_2 = Integer.MIN_VALUE;
+    private static final int INVALID_AGE_3 = 121;
+    private static final int INVALID_AGE_4 = Integer.MAX_VALUE;
+    private static final String USER_LOGIN_2 = "Rick";
+    private static final String USER_PASSWORD_2 = "pickle";
+    private static final int USER_AGE_2 = 65;
+    private static final String USER_LOGIN_3 = "Rudeus";
+    private static final String USER_PASSWORD_3 = "quagmire";
     private final RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
     private User actualUser;
 
     @BeforeEach
     void setUp() {
         actualUser = new User();
-        actualUser.setLogin("Bob");
-        actualUser.setPassword("123456");
-        actualUser.setAge(18);
+        actualUser.setLogin(USER_NAME_1);
+        actualUser.setPassword(USER_PASSWORD_1);
+        actualUser.setAge(USER_AGE_1);
         Storage.people.clear();
     }
 
@@ -37,13 +52,13 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_emptyUserLoginString_notOk() {
-        actualUser.setLogin("");
+        actualUser.setLogin(EMPTY_STRING);
         runAssertThrows();
     }
 
     @Test
     void register_whitespaceOnlyUserLoginString_notOk() {
-        actualUser.setLogin("  \t\n");
+        actualUser.setLogin(WHITESPACE_CHARACTERS_STRING);
         runAssertThrows();
     }
 
@@ -61,23 +76,23 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_passwordMinLength_notOk() {
-        actualUser.setPassword("abcde");
+        actualUser.setPassword(INVALID_PASSWORD_ABCDE);
         runAssertThrows();
     }
 
     @Test
     void register_minAge_notOk() {
-        actualUser.setAge(17);
+        actualUser.setAge(INVALID_AGE);
         runAssertThrows();
-        actualUser.setAge(Integer.MIN_VALUE);
+        actualUser.setAge(INVALID_AGE_2);
         runAssertThrows();
     }
 
     @Test
     void register_maxAge_notOk() {
-        actualUser.setAge(121);
+        actualUser.setAge(INVALID_AGE_3);
         runAssertThrows();
-        actualUser.setAge(Integer.MIN_VALUE);
+        actualUser.setAge(INVALID_AGE_4);
         runAssertThrows();
     }
 
@@ -89,9 +104,9 @@ class RegistrationServiceImplTest {
     @Test
     void register_coupleOfRegisteredUsers_areReturnedSameAsPassed_ok() {
         User validUser = new User();
-        validUser.setLogin("Rick");
-        validUser.setPassword("pickle");
-        validUser.setAge(65);
+        validUser.setLogin(USER_LOGIN_2);
+        validUser.setPassword(USER_PASSWORD_2);
+        validUser.setAge(USER_AGE_2);
         Storage.people.add(validUser);
         assertEquals(actualUser, registrationService.register(actualUser));
     }
@@ -106,14 +121,14 @@ class RegistrationServiceImplTest {
     @Test
     void register_coupleOfUsersStoredCorrectly_ok() {
         User validUser1 = new User();
-        validUser1.setLogin("Rick");
-        validUser1.setPassword("pickle");
-        validUser1.setAge(65);
+        validUser1.setLogin(USER_LOGIN_2);
+        validUser1.setPassword(USER_PASSWORD_2);
+        validUser1.setAge(USER_AGE_2);
         Storage.people.add(validUser1);
         Storage.people.add(actualUser);
         User validUser2 = new User();
-        validUser1.setLogin("Rudeus");
-        validUser1.setPassword("quagmire");
+        validUser1.setLogin(USER_LOGIN_3);
+        validUser1.setPassword(USER_PASSWORD_3);
         Storage.people.add(validUser2);
         assertTrue(Storage.people.contains(actualUser));
         assertEquals(actualUser, Storage.people.get(1));
