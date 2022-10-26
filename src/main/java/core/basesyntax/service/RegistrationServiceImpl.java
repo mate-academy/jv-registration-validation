b525609af5d6b8abce52b71141dad456c6758b96
AlinaOryshak <alinaoryshak@gmail.com>
2022-10-26 13:04:57 +0300

    Registration service was implemented and tested

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,43 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RuntimeException("User can't be null");
+        }
+        loginChecking(user);
+        passwordChecking(user);
+        ageChecking(user);
+        storageDao.add(user);
+        return user;
+    }
+
+    private void loginChecking(User user) {
+        if (user.getLogin() == null) {
+            throw new RuntimeException("Invalid login");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User with such login has already existed");
+        }
+    }
+
+    private void passwordChecking(User user) {
+        if (user.getPassword() == null) {
+            throw new RuntimeException("Invalid password");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password must be at least 6 characters");
+        }
+    }
+
+    private void ageChecking(User user) {
+        if (user.getAge() == null || user.getAge() < MIN_AGE) {
+            throw new RuntimeException("Invalid age");
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -0,0 +1,77 @@
+package core.basesyntax;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+    private static User user;
+
+    @BeforeAll
+    public static void setUp() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    public void userInit() {
+        user = new User(12121212L, "testUser", "123user", 20);
+    }
+
+    @Test
+    public void register_validUser_Ok() {
+        User actual = registrationService.register(user);
+        assertEquals(user, actual);
+    }
+
+    @Test
+    public void register_nullUser_notOk() {
+        assertThrows(RuntimeException.class, () -> registrationService.register(null));
+    }
+
+    @Test
+    public void register_loginExists_notOk() {
+        User user2 = new User(12958212L, "testUser", "123user123", 47);
+        registrationService.register(user);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user2));
+    }
+
+    @Test
+    public void register_nullLogin_notOk() {
+        user.setLogin(null);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_invalidPasswordLength_notOk() {
+        user.setPassword("1234");
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_nullPassword_notOk() {
+        user.setPassword(null);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_invalidAge_notOk() {
+        user.setAge(15);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+        user.setAge(null);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @AfterEach
+    public void clearStorage() {
+        Storage.people.clear();
+    }
+}
