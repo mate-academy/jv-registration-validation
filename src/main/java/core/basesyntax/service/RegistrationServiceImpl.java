6d030c8eaef2faabf0585e15b986ffc127c813b1
Andrii Malenko <andmalenko@gmail.com>
2022-10-28 11:14:29 +0300

    Here is my solution for hw-registration-validation

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,44 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    static final int MIN_AGE = 18;
+    static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null) {
+            throw new RuntimeException("Login is null");
+        }
+
+        if (user.getLogin().equals("")) {
+            throw new RuntimeException("Login is empty");
+        }
+
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("Login are not unique");
+        }
+        if (user.getAge() == null) {
+            throw new RuntimeException("Age is null");
+        }
+
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("Age is lower than 18");
+        }
+
+        if (user.getPassword() == null) {
+            throw new RuntimeException("Password is null");
+        }
+
+        if (user.getPassword().equals("")) {
+            throw new RuntimeException("Password is empty");
+        }
+
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password is poor. Minimal size is 6 character");
+        }
+
+        storageDao.add(user);
+        return user;
     }
 }
