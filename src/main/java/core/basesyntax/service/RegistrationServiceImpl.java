f7ef75f8d9f4b76e0b8ed1cf92d304eea4c05c85
Vasyl Piznak <piznak3145@gmail.com>
2022-11-14 22:23:27 +0200

    First time commit

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,30 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int DEFAULT_AGE = 18;
+    private static final int DEFAULT_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RuntimeException("User is null");
+        }
+        if (user.getId() == null) {
+            throw new RuntimeException("User is not found");
+        }
+        if (user.getLogin() == null) {
+            throw new RuntimeException("user with this login does not exist");
+        }
+        if (user.getAge() < DEFAULT_AGE) {
+            throw new RuntimeException("Age is lower than 18");
+        }
+        if (user.getPassword().length() < DEFAULT_PASSWORD_LENGTH) {
+            throw new RuntimeException("User password must be at least 6 characters");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User with this login is already in the register");
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,105 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static final int DEFAULT_AGE = 18;
+    private static final int DEFAULT_PASSWORD_LENGTH = 6;
+    private static final int STORAGE_SIZE_FOR_TEST = 2;
+    private final RegistrationService registrationService = new RegistrationServiceImpl();
+    private User user;
+
+    @BeforeEach
+    public void defaultUser() {
+        user = new User();
+        user.setId(123L);
+        user.setLogin("vasyl");
+        user.setPassword("vasyl55555");
+        user.setAge(24);
+    }
+
+    @Test
+    void check18_years_old_NotOk() {
+        user.setAge(15);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void check18_years_old_Ok() {
+        User actualUser = registrationService.register(user);
+        assertTrue(actualUser.getAge() > DEFAULT_AGE,"Fine");
+    }
+
+    @Test
+    void userAge_is_null() {
+        user.setAge(null);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void userPassword_is_null() {
+        user.setPassword(null);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void userLogin_is_null() {
+        user.setLogin(null);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void userId_is_null() {
+        user.setId(null);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void user_is_null() {
+        user = null;
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void correct_password() {
+        User actualUser = registrationService.register(user);
+        assertTrue(actualUser.getPassword().length() >= DEFAULT_PASSWORD_LENGTH,"Fine");
+    }
+
+    @Test
+    void password_NotOk() {
+        user.setPassword("228");
+        Assertions.assertThrows(RuntimeException.class,() -> registrationService.register(user));
+    }
+
+    @Test
+    void already_HadUser() {
+        Storage.people.add(user);
+        Assertions.assertThrows(RuntimeException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void newRegister_User() {
+        User user2 = new User();
+        user2.setId(354L);
+        user2.setLogin("Roman");
+        user2.setPassword("roman2281488");
+        user2.setAge(31);
+        Storage.people.add(user);
+        Storage.people.add(user2);
+        assertEquals(STORAGE_SIZE_FOR_TEST,Storage.people.size());
+    }
+
+    @AfterEach
+    public void clear() {
+        Storage.people.clear();
+    }
+}
