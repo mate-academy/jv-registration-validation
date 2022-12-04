43160d6807fed7c5739658baff6cc53c6efceb81
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2022-12-04 16:24:07 +0000

    Task added with changes

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,7 +2,6 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
-import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
@@ -12,30 +11,28 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        for (User u : Storage.people) {
-            if (user.getLogin() == u.getLogin()) {
-                throw new RuntimeException("User with such login exist in the Storage");
-            }
-        }
         if (user.getLogin() == null) {
             throw new RuntimeException("Login can't be null");
         }
-        if (user.getLogin().equals("")) {
-            throw new RuntimeException("Login can't be empty. Please enter at "
-                  + "least one character.");
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User with such login exist in the Storage");
         }
-        if (user.getAge() < MIN_AGE) {
-            throw new RuntimeException("User age must be over 18");
+        if (user.getLogin().isEmpty()) {
+            throw new RuntimeException("Login can't be empty. Please enter at "
+                    + "least one character.");
         }
         if (user.getAge() == null) {
             throw new RuntimeException("Age can't be null");
         }
-
-        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
-            throw new RuntimeException("Password must be at least 6 characters");
-        }
         if (user.getPassword() == null) {
             throw new RuntimeException("Password can't be null");
+        }
+
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("User age must be over 18");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password must be at least 6 characters");
         }
         storageDao.add(user);
         return user;
