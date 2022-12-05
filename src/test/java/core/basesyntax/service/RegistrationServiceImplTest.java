2998e2fd9af669a18574405fc9118cd48e4ad8a6
Victoria Kharlanchuk <kaplabetty@gmail.com>
2022-12-05 20:51:24 +0100

    solutions_test

---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,84 @@
+package core.basesyntax.service;
+
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+
+    private static final User user = new User();
+    private static final User actual = new User();
+    private static RegistrationServiceImpl registrationService;
+    private static StorageDaoImpl storageDao;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        storageDao = new StorageDaoImpl();
+        user.setLogin("FirstPerson");
+        user.setPassword("thebestpassword");
+        user.setAge(20);
+        storageDao.add(user);
+    }
+
+    @BeforeEach
+    void setup() {
+        actual.setLogin("SecondPerson");
+        actual.setLogin("anotherpass");
+        actual.setAge(36);
+    }
+
+    @Test
+    void register_null_pas_NotOk() {
+        actual.setPassword(null);
+        assertThrows(RuntimeException.class, () -> registrationService.register(actual));
+    }
+
+    @Test
+    void register_null_log_NotOk() {
+        actual.setLogin(null);
+        assertThrows(RuntimeException.class, () -> registrationService.register(actual));
+    }
+
+    @Test
+    void register_null_age_NotOk() {
+        actual.setAge(null);
+        assertThrows(RuntimeException.class, () -> registrationService.register(actual));
+    }
+
+    @Test
+    void register_negativ_age_NotOk() {
+        actual.setAge(-1);
+        assertThrows(RuntimeException.class, () -> registrationService.register(actual));
+    }
+
+    @Test
+    void register_small_age_NotOk() {
+        actual.setAge(12);
+        assertThrows(RuntimeException.class, () -> registrationService.register(actual));
+    }
+
+    @Test
+    void register_theSame_login_NotOk() {
+        actual.setLogin("FirstPerson");
+        assertThrows(RuntimeException.class, () -> registrationService.register(actual));
+    }
+
+    @Test
+    void register_short_pass_NotOk() {
+        actual.setPassword("01234");
+        assertThrows(RuntimeException.class, () -> registrationService.register(actual));
+    }
+
+
+}
