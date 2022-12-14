213b220bbfba7452e881168317cb7c9c76b9bed1
Dmytro M <dmytro.malinnikov@gmail.com>
2022-12-14 16:29:26 +0200

    minor refactor

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,6 +6,8 @@ import core.basesyntax.exception.InvalidInputDataException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int VALID_AGE = 18;
+    private static final int VALID_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -16,13 +18,13 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user.getAge() == null) {
             throw new InvalidInputDataException("Age can't be null");
         }
-        if (user.getAge() < 18) {
+        if (user.getAge() < VALID_AGE) {
             throw new InvalidInputDataException("Age should be 18 or higher");
         }
         if (user.getPassword() == null) {
             throw new InvalidInputDataException("Password can't be null");
         }
-        if (user.getPassword().length() < 6) {
+        if (user.getPassword().length() < VALID_PASSWORD_LENGTH) {
             throw new InvalidInputDataException(
                     "Password length should be at least 6 characters long");
         }
