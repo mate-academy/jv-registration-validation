007bb93310048f5500f20da728cfd54ef3051aa3
alexeyukrainets <qaaq.com.ua@gmail.com>
2022-12-17 13:02:42 +0100

    Final

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,28 +2,46 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
-import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    public static final int MIN_AGE_FOR_USER = 18;
+    public static final int MIN_LENGTH_FOR_PASSWORD = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
+
+    public void validateAge(User user) {
+        if (user.getAge() == null) {
+            throw new InvalidDataException("The field for age is equal null");
+        }
+        if (user.getAge() < MIN_AGE_FOR_USER) {
+            throw new InvalidDataException("Incorrect user age");
+        }
+    }
+
+    public void validatePassword(User user) {
+        if (user.getPassword() == null) {
+            throw new InvalidDataException("The field for password is equal null");
+        }
+        if (user.getPassword().length() < MIN_LENGTH_FOR_PASSWORD) {
+            throw new InvalidDataException("Incorrect password, min length 6 characters");
+        }
+    }
+
+    public void validateLogin(User user) {
+        if (user.getLogin() == null) {
+            throw new InvalidDataException("The field for login is equal null");
+        }
+        User user1 = storageDao.get(user.getLogin());
+        if (user1 != null) {
+            throw new InvalidDataException("This login is not available");
+        }
+    }
 
     @Override
     public User register(User user) {
-        if (user.getAge() == null || user.getPassword() == null) {
-            throw new NullPointerException("Incorrect password or user age");
-        }
-        if (user.getAge() < 18 || user.getPassword().length() < 6) {
-            throw new InvalidDataException("Incorrect password or user age");
-        }
-        if (user.getLogin() == null) {
-            throw new NullPointerException("Incorrect password or user age");
-        }
-        for (User person : Storage.people) {
-            if (user.getLogin().equals(person.getLogin())) {
-                throw new InvalidDataException("This login is not available");
-            }
-        }
+        validateAge(user);
+        validatePassword(user);
+        validateLogin(user);
         return storageDao.add(user);
     }
 }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -12,48 +12,49 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_incorrectAge_NotOk() {
-        User user = new User(null, "qwertyd", "1234567", 17);
+        User user = new User("qwertyd", "1234567", 17);
         assertThrows(InvalidDataException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_incorrectPassword_NotOk() {
-        User user = new User(null, "qwertyd", "12345", 20);
+    void register_shortPassword_NotOk() {
+        User user = new User("qwertyd", "12345", 20);
         assertThrows(InvalidDataException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_incorrectLogin_NotOk() {
-        User user = new User(null, "Ukraients_Oleksii", "123456", 20);
+        User user = new User("Ukraients_Oleksii", "123456", 20);
         Storage.people.add(user);
-        User userFromRegister = new User(null, "Ukraients_Oleksii", "123456", 20);
+        User userFromRegister = new User("Ukraients_Oleksii", "123456", 20);
         assertThrows(InvalidDataException.class, () ->
                 registrationService.register(userFromRegister));
     }
 
     @Test
     void register_nullLogin_NotOk() {
-        User user = new User(null, null, "1234567", 20);
-        assertThrows(NullPointerException.class, () -> registrationService.register(user));
+        User user = new User(null, "1234567", 20);
+        assertThrows(InvalidDataException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_nullAge_NotOk() {
-        User user = new User(null, "qwertyd", "1234567", null);
-        assertThrows(NullPointerException.class, () -> registrationService.register(user));
+        User user = new User("qwertyd", "1234567", null);
+        assertThrows(InvalidDataException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_nullPassword_NotOk() {
-        User user = new User(null, "qwertyd", null, 20);
-        assertThrows(NullPointerException.class, () -> registrationService.register(user));
+        User user = new User("qwertyd", null, 20);
+        assertThrows(InvalidDataException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_verificationOfAddingAUserToTheDatabase_Ok() {
-        int sizeListPeople = Storage.people.size();
-        User user = new User(null, "qwertyd", "1234567", 20);
+    void register_validData_Ok() {
+        int expectedStorageSize = Storage.people.size() + 1;
+        User user = new User("qwertyd", "1234567", 20);
         registrationService.register(user);
-        assertEquals(sizeListPeople + 1, Storage.people.size());
+        assertEquals(expectedStorageSize, Storage.people.size());
+        assertEquals(user, Storage.people.get(Storage.people.size() - 1));
     }
 }
