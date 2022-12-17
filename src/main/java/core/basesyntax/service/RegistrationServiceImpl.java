6660e3e2e6ce1029cf5a3baa9f202a59ac2e349e
Oksana Rudenko <0ksaha_rudenko@ukr.net>
2022-12-17 19:56:51 +0200

    jv-registration-validation fixed issues

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,35 +6,35 @@ import core.basesyntax.exception.InvalidDataException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int ZERO_AGE = 0;
+    private static final int REQUIRED_AGE = 18;
+    private static final int REQUIRED_PASSWORD_LENGTH = 6;
     private StorageDao storageDao = new StorageDaoImpl();
-    private final int zeroAge = 0;
-    private final int requiredAge = 18;
-    private final int requiredPasswordLength = 6;
 
     @Override
     public User register(User user) throws InvalidDataException {
         if (user == null) {
             throw new InvalidDataException("Can't register null user. Please, enter your data");
         }
-        if (storageDao.get(user.getLogin()) != null) {
-            throw new InvalidDataException("User with such login is already exist");
-        }
         if (user.getLogin() == null) {
             throw new InvalidDataException("Login can't be null");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new InvalidDataException("User with such login is already exist");
         }
         if (user.getPassword() == null) {
             throw new InvalidDataException("Password can't be null");
         }
-        if (user.getPassword().length() < requiredPasswordLength) {
+        if (user.getPassword().length() < REQUIRED_PASSWORD_LENGTH) {
             throw new InvalidDataException("Your password should consist at least 6 characters");
         }
         if (user.getAge() == null) {
             throw new InvalidDataException("Age can't be null");
         }
-        if (user.getAge() < zeroAge) {
+        if (user.getAge() < ZERO_AGE) {
             throw new InvalidDataException("Not valid age!");
         }
-        if (user.getAge() < requiredAge) {
+        if (user.getAge() < REQUIRED_AGE) {
             throw new InvalidDataException("Your age is less then require");
         }
         return storageDao.add(user);
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -14,20 +14,20 @@ import org.junit.jupiter.api.Test;
 class RegistrationServiceImplTest {
     private static RegistrationService registrationService;
     private static StorageDao storageDao;
-    private static User user1 = new User();
-    private static User user2 = new User();
-    private static User user3 = new User();
 
     @BeforeAll
     static void beforeAll() {
         registrationService = new RegistrationServiceImpl();
         storageDao = new StorageDaoImpl();
+        User user1 = new User();
         user1.setLogin("FirstUser");
         user1.setAge(34);
         user1.setPassword("45jisf3");
+        User user2 = new User();
         user2.setLogin("SecondUser");
         user2.setAge(18);
         user2.setPassword("newark");
+        User user3 = new User();
         user3.setLogin("ThirdUser");
         user3.setAge(28);
         user3.setPassword("vo*edekmybr");
@@ -37,7 +37,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void addUserWithUniqueLogin_Ok() {
+    void register_uniqueLogin_ok() {
         User user = new User();
         user.setLogin("FourthUser");
         user.setAge(26);
@@ -53,7 +53,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void addNullUser_NotOk() {
+    void register_nullUser_notOk() {
         User user = null;
         assertThrows(InvalidDataException.class, () -> {
             registrationService.register(user);
@@ -61,7 +61,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void addUserWithExistingLogin() {
+    void register_existingLogin_notOk() {
         User user = new User();
         user.setLogin("ThirdUser");
         user.setAge(19);
@@ -75,7 +75,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void addUserWithNullLogin_NotOk() {
+    void register_nullLogin_notOk() {
         User user = new User();
         user.setAge(65);
         user.setPassword("biscuit");
@@ -85,7 +85,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void addUserWithNullPassword_NotOk() {
+    void register_nullPassword_notOk() {
         User user = new User();
         user.setLogin("FifthUser");
         user.setAge(45);
@@ -95,7 +95,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void addUserWithInvalidPassword_NotOk() {
+    void register_invalidPassword_notOk() {
         User user = new User();
         user.setLogin("SixthUser");
         user.setAge(65);
@@ -109,7 +109,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void addUserWithValidPasswordWithRequiredLength_OK() {
+    void register_validPasswordWithRequiredLength_ok() {
         User user = new User();
         user.setLogin("SeventhUser");
         user.setAge(36);
@@ -125,7 +125,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void addUserWithNullAge_NotOk() {
+    void register_nullAge_notOk() {
         User user = new User();
         user.setLogin("EighthUser");
         user.setPassword("poiuyt8");
@@ -135,7 +135,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void addUserWithNegativeValueOfAge_NotOk() {
+    void register_negativeValueOfAge_notOk() {
         User user = new User();
         user.setLogin("NinthUser");
         user.setAge(-18);
@@ -147,7 +147,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void addUserWithAgeLessThenRequire_NotOk() {
+    void register_ageLessThenRequire_notOk() {
         User user = new User();
         user.setLogin("TenthUser");
         user.setAge(15);
@@ -158,7 +158,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void addUserWithAgeEqualRequiredValue_Ok() {
+    void register_ageEqualsRequiredValue_ok() {
         User user = new User();
         user.setLogin("EleventhUser");
         user.setAge(18);
