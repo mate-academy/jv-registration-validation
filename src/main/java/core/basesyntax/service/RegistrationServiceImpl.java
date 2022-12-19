57ed1229be482d9b86e285d63cdfd6ec05aa79c3
Andrii Varlanovych <andrew_exp@ukr.net>
2022-12-19 15:40:39 +0200

    was solved

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,34 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    public static final int MIN_AGE = 18;
+    public static final int VALID_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getPassword() == null) {
+            throw new RegisterServiceImplException("Password can't be null !");
+        }
+        if (user.getLogin() == null) {
+            throw new RegisterServiceImplException("Login can't be null !");
+        }
+        if (user.getAge() < MIN_AGE && user.getAge() > 0) {
+            throw new RegisterServiceImplException("Not valid age !");
+        }
+        if (user.getAge() == 0) {
+            throw new RegisterServiceImplException("Age can't be zero !");
+        }
+        if (user.getAge() < 0) {
+            throw new RegisterServiceImplException("Age can't be nagative !");
+        }
+        if (user.getPassword().length() < VALID_PASSWORD_LENGTH) {
+            throw new RegisterServiceImplException("Password must be at least 6 charaters !");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegisterServiceImplException("User already exist !");
+        } else {
+            return storageDao.add(user);
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,137 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static StorageDao storageDao;
+    private static Storage storage;
+    private RegistrationService registrationService = new RegistrationServiceImpl();
+
+    @BeforeAll
+    static void beforeAll() {
+        storageDao = new StorageDaoImpl();
+        storage = new Storage();
+    }
+
+    @BeforeEach
+    void setUp() {
+        User firstUser = new User();
+        firstUser.setPassword("firstUserPassword");
+        firstUser.setAge(25);
+        firstUser.setLogin("firstUserLogin");
+        storageDao.add(firstUser);
+
+        User secondUser = new User();
+        secondUser.setPassword("secondUserPassword");
+        secondUser.setAge(35);
+        secondUser.setLogin("secondUserLogin");
+        storageDao.add(secondUser);
+
+        User thirddUser = new User();
+        thirddUser.setPassword("thirddUserPassword");
+        thirddUser.setAge(35);
+        thirddUser.setLogin("thirddUserLogin");
+        storageDao.add(thirddUser);
+    }
+
+    @Test
+    void register_nullPassword_notOk() {
+        User testUser = new User();
+        testUser.setPassword(null);
+        testUser.setAge(45);
+        testUser.setLogin("testUserLogin");
+        assertThrows(RegisterServiceImplException.class, () -> {
+            registrationService.register(testUser);
+        });
+    }
+
+    @Test
+    void register_nullLogin_notOk() {
+        User testUser = new User();
+        testUser.setPassword("testUserPassword");
+        testUser.setAge(45);
+        testUser.setLogin(null);
+        assertThrows(RegisterServiceImplException.class, () -> {
+            registrationService.register(testUser);
+        });
+    }
+
+    @Test
+    void register_notValidAge_notOk() {
+        User testUser = new User();
+        testUser.setPassword("testUserPassword");
+        testUser.setAge(17);
+        testUser.setLogin("testUserLogin");
+        assertThrows(RegisterServiceImplException.class, () -> {
+            registrationService.register(testUser);
+        });
+    }
+
+    @Test
+    void register_zeroAge_notOk() {
+        User testUser = new User();
+        testUser.setPassword("testUserPassword");
+        testUser.setAge(0);
+        testUser.setLogin("testUserLogin");
+        assertThrows(RegisterServiceImplException.class, () -> {
+            registrationService.register(testUser);
+        });
+    }
+
+    @Test
+    void register_nagativeAge_notOk() {
+        User testUser = new User();
+        testUser.setPassword("testUserPassword");
+        testUser.setAge(-10);
+        testUser.setLogin("testUserLogin");
+        assertThrows(RegisterServiceImplException.class, () -> {
+            registrationService.register(testUser);
+        });
+    }
+
+    @Test
+    void register_notValidPassword_notOk() {
+        User testUser = new User();
+        testUser.setPassword("test");
+        testUser.setAge(55);
+        testUser.setLogin("testUserLogin");
+        assertThrows(RegisterServiceImplException.class, () -> {
+            registrationService.register(testUser);
+        });
+    }
+
+    @Test
+    void register_addingExistingUser_notOk() {
+        User testUser = storage.people.get(1);
+        assertThrows(RegisterServiceImplException.class, () -> {
+            registrationService.register(testUser);
+        });
+    }
+
+    @Test
+    void register_addingValidUser_Ok() {
+        User testUser = new User();
+        testUser.setPassword("testUserPassword");
+        testUser.setAge(30);
+        testUser.setLogin("testUserLogin");
+        User actual = registrationService.register(testUser);
+        assertEquals("testUserPassword", testUser.getPassword());
+        assertEquals(30, testUser.getAge());
+        assertEquals("testUserLogin", testUser.getLogin());
+    }
+
+    @AfterEach
+    void tearDown() {
+        storage.people.clear();
+    }
+}
