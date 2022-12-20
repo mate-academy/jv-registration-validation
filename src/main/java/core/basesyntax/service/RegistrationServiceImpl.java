a1793328d63c9c979cf9105035e4b99ec58aa566
Sander <alex.kapusniak@gmail.com>
2022-12-20 22:54:33 +0200

    add new scenario tests and make some refactoring

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -8,14 +8,18 @@ import core.basesyntax.model.User;
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_USERS_AGE = 18;
     private static final int MIN_PASSWORD_LENGTH = 6;
-    private final StorageDao storageDao = new StorageDaoImpl();
+    private final StorageDao storageDao;
+
+    public RegistrationServiceImpl() {
+        this.storageDao = new StorageDaoImpl();
+    }
 
     @Override
     public User register(User user) {
         if (validation(user)) {
             return storageDao.add(user);
         }
-        throw new ValidationException("This user can`t be registered");
+        return null;
     }
 
     private boolean validation(User user) {
@@ -23,16 +27,21 @@ public class RegistrationServiceImpl implements RegistrationService {
             throw new ValidationException("User can`t be null");
         }
 
+        if (user.getPassword() == null || user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new ValidationException("User`s password length can`t be null or less than "
+                    + MIN_PASSWORD_LENGTH);
+        }
+
+        if (user.getAge() == null || user.getAge() < MIN_USERS_AGE) {
+            throw new ValidationException("User`s age can`t be null or less than " + MIN_USERS_AGE);
+        }
+
+        if (user.getLogin() == null) {
+            throw new ValidationException("User`s login can`t be null");
+        }
+
         if (storageDao.get(user.getLogin()) != null) {
             throw new ValidationException("User with this login exist in storage");
-        }
-
-        if (user.getAge() < MIN_USERS_AGE) {
-            throw new ValidationException("User`s age can`t be less than 18");
-        }
-
-        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
-            throw new ValidationException("User`s password length can`t be less than 6");
         }
         return true;
     }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -12,40 +12,10 @@ import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
     private static RegistrationServiceImpl registrationService;
-    private static User userOne;
-    private static User userTwo;
-    private static User userThree;
-    private static User nullUser;
-    private static User userFour;
 
     @BeforeAll
-    public static void createEntities() {
+    public static void createRegistrationService() {
         registrationService = new RegistrationServiceImpl();
-        userOne = new User();
-        userTwo = new User();
-        userThree = new User();
-        userFour = new User();
-        nullUser = null;
-
-        //create first User with valid credentials
-        userOne.setLogin("John");
-        userOne.setAge(18);
-        userOne.setPassword("qwerty");
-
-        //create second User with invalid password
-        userTwo.setLogin("Corey");
-        userTwo.setAge(42);
-        userTwo.setPassword("asdf");
-
-        //create third User with invalid age
-        userThree.setLogin("John Deere");
-        userThree.setAge(12);
-        userThree.setPassword("qwerty");
-
-        //create fourth User with valid credentials
-        userFour.setLogin("James");
-        userFour.setAge(58);
-        userFour.setPassword("metallica");
     }
 
     @AfterEach
@@ -55,24 +25,83 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_nullUser_notOk() {
+        User nullUser = null;
         assertThrows(ValidationException.class, () -> registrationService.register(nullUser));
     }
 
     @Test
     void register_UsersWithInvalidCredentials_notOk() {
-        assertThrows(ValidationException.class, () -> registrationService.register(userTwo));
-        assertThrows(ValidationException.class, () -> registrationService.register(userThree));
+        User userInvalidPassword = new User();
+        userInvalidPassword.setLogin("Corey");
+        userInvalidPassword.setAge(42);
+        userInvalidPassword.setPassword("asdf");
+
+        User userInvalidAge = new User();
+        userInvalidAge.setLogin("John Deere");
+        userInvalidAge.setAge(12);
+        userInvalidAge.setPassword("qwerty");
+
+        assertThrows(ValidationException.class,
+                () -> registrationService.register(userInvalidPassword));
+        assertThrows(ValidationException.class,
+                () -> registrationService.register(userInvalidAge));
     }
 
     @Test
-    void register_withFewSameUser_notOk() {
-        assertEquals(userOne, registrationService.register(userOne));
-        assertThrows(ValidationException.class, () -> registrationService.register(userOne));
+    void register_UserWithNullPassword_notOk() {
+        User userNullPassword = new User();
+        userNullPassword.setLogin("Corey");
+        userNullPassword.setAge(42);
+        userNullPassword.setPassword(null);
+
+        assertThrows(ValidationException.class,
+                () -> registrationService.register(userNullPassword));
+    }
+
+    @Test
+    void register_UserWithNullLogin_notOk() {
+        User userNullLogin = new User();
+        userNullLogin.setLogin(null);
+        userNullLogin.setAge(42);
+        userNullLogin.setPassword("asdfqwer");
+
+        assertThrows(ValidationException.class, () -> registrationService.register(userNullLogin));
+    }
+
+    @Test
+    void register_UserWithNegativeAge_notOk() {
+        User userNullLogin = new User();
+        userNullLogin.setLogin("Abrams");
+        userNullLogin.setAge(-1);
+        userNullLogin.setPassword("asdfqwer");
+
+        assertThrows(ValidationException.class, () -> registrationService.register(userNullLogin));
+    }
+
+    @Test
+    void register_existingUser_notOk() {
+        User userValid = new User();
+        userValid.setLogin("John");
+        userValid.setAge(18);
+        userValid.setPassword("qwerty");
+
+        assertEquals(userValid, registrationService.register(userValid));
+        assertThrows(ValidationException.class, () -> registrationService.register(userValid));
     }
 
     @Test
     void register_userWithValidCredentials_Ok() {
-        assertEquals(userOne, registrationService.register(userOne));
-        assertEquals(userFour, registrationService.register(userFour));
+        User userValid = new User();
+        userValid.setLogin("John");
+        userValid.setAge(18);
+        userValid.setPassword("qwerty");
+
+        User userValidOther = new User();
+        userValidOther.setLogin("James");
+        userValidOther.setAge(58);
+        userValidOther.setPassword("metallica");
+
+        assertEquals(userValid, registrationService.register(userValid));
+        assertEquals(userValidOther, registrationService.register(userValidOther));
     }
 }
