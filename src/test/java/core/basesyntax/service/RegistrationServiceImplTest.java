839aaa8218e3a4cc3bd06d3828f53a1a2d5e2ade
Vyacheslav Sukhov <makasin_12@hotmail.com>
2022-12-21 20:49:07 +0100

    2nd try commit 21/12

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,5 +1,6 @@
 package core.basesyntax.service;
 
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,16 +10,34 @@ class RegistrationServiceImplTest {
     private static final int MIN_AGE = 18;
     private static final int MIN_PASS_LENGTH = 6;
     private final RegistrationService registrationService = new RegistrationServiceImpl();
-    private final User user = new User();
-    private final String login = "slavik";
-    private final int age = 18;
+    private User user;
+    private String login = "slavik";
+    private int age = 18;
     private final String password = "Sukhov28";
 
     @BeforeEach
     void setUp() {
-        user.setAge(age);
+        user = new User();
+        user.setAge(age++);
         user.setLogin(login);
         user.setPassword(password);
+    }
+
+    @Test
+    void register_UserExistsAlready_Ok() {
+        User user1 = registrationService.register(user);
+        Assertions.assertThrows(CustomException.class, () -> {
+            registrationService.register(user1);
+        });
+        Storage.people.remove(user1);
+    }
+
+    @Test
+    void register_UserAge_Ok() {
+        user.setAge(19);
+        User user1 = registrationService.register(user);
+        Assertions.assertTrue(user1.getAge() > MIN_AGE);
+        Storage.people.remove(user1);
     }
 
     @Test
@@ -27,12 +46,7 @@ class RegistrationServiceImplTest {
         Assertions.assertThrows(CustomException.class, () -> {
             registrationService.register(user);
         });
-    }
-
-    @Test
-    void register_UserAge_Ok() {
-        registrationService.register(user);
-        Assertions.assertTrue(user.getAge() >= MIN_AGE);
+        Storage.people.remove(user);
     }
 
     @Test
@@ -41,19 +55,21 @@ class RegistrationServiceImplTest {
         Assertions.assertThrows(CustomException.class, () -> {
             registrationService.register(user);
         });
+        Storage.people.remove(user);
     }
 
     @Test
     void register_UserLogin_NotNull() {
-        String actualLogin = user.getLogin();
-        Assertions.assertNotNull(actualLogin);
-        registrationService.register(user);
+        User user1 = registrationService.register(user);
+        Assertions.assertNotNull(user1.getLogin());
+        Storage.people.remove(user1);
     }
 
     @Test
     void register_UserPasswordLength_Ok() {
-        registrationService.register(user);
-        Assertions.assertTrue(user.getPassword().length() > MIN_PASS_LENGTH);
+        User user1 = registrationService.register(user);
+        Assertions.assertTrue(user1.getPassword().length() > MIN_PASS_LENGTH);
+        Storage.people.remove(user1);
     }
 
     @Test
@@ -62,6 +78,7 @@ class RegistrationServiceImplTest {
         Assertions.assertThrows(CustomException.class, () -> {
             registrationService.register(user);
         });
+        Storage.people.remove(user);
     }
 
     @Test
@@ -70,5 +87,6 @@ class RegistrationServiceImplTest {
         Assertions.assertThrows(CustomException.class, () -> {
             registrationService.register(user);
         });
+        Storage.people.remove(user);
     }
 }
