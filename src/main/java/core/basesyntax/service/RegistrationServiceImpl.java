c85ed5a60f5a34b2d8cb3ecf52da9769f058afcd
Inna Diachkina <innadyachkina@gmail.com>
2023-01-03 08:20:16 +0200

    implement test class

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,33 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MAX_AGE = 118;
+    private static final int MIN_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null || user.getLogin().length() == 0
+                || user.getLogin().indexOf(' ') != -1) {
+            throw new UserNotFoundException("This login is incorrect");
+        }
+        if (user.getPassword() == null || user.getPassword().length() == 0
+                || user.getPassword().indexOf(' ') != -1) {
+            throw new UserNotFoundException("This password is incorrect");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new UserNotFoundException("This user is already registered");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new UserNotFoundException("User age is less than allowed");
+        }
+        if (user.getAge() > MAX_AGE) {
+            throw new UserNotFoundException("User age is more than allowed");
+        }
+        if (user.getPassword().length() < MIN_LENGTH) {
+            throw new UserNotFoundException("User password is less than expected");
+        }
+        return storageDao.add(user);
     }
 }
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,8 +1,138 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorldTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
+import core.basesyntax.service.RegistrationServiceImpl;
+import core.basesyntax.service.UserNotFoundException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+    private static final int DEFAULT_AGE = 18;
+    private static final String DEFAULT_PASSWORD = "user_password";
+    private static final String DEFAULT_LOGIN = "user_login";
+    private static final String NULL_STRING = null;
+    private static final String EMPTY_STRING = "";
+    private static final char EMPTY_PLACE = ' ';
+    private static final String SHORT_PASSWORD = "short";
+    private static final int SMALL_AGE = 17;
+    private static final int NEGATIVE_AGE = -68;
+    private static final int BIG_AGE = 119;
+    private static final int MAX_AGE = 118;
+    private static final int NULL_AGE = 0;
+    private static RegistrationService registrationService;
+    private static StorageDao storageDao;
+    private User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        storageDao = new StorageDaoImpl();
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+     void setUp() {
+        user = new User();
+        user.setLogin(DEFAULT_LOGIN);
+        user.setAge(DEFAULT_AGE);
+        user.setPassword(DEFAULT_PASSWORD);
+    }
+
+    @Test
+    void register_correct_ok() {
+        user.setAge(MAX_AGE);
+        assertEquals(user, storageDao.add(user));
+    }
+
+    @Test
+    void register_maxAge_ok() {
+        assertEquals(user, storageDao.add(user));
+    }
+
+    @Test
+    void register_nullLogin_notOk() {
+        user.setLogin(NULL_STRING);
+        assertThrows(UserNotFoundException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_nullPassword_notOk() {
+        user.setPassword(NULL_STRING);
+        assertThrows(UserNotFoundException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_emptyPassword_notOk() {
+        user.setPassword(EMPTY_STRING);
+        assertThrows(UserNotFoundException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_emptyPlacePassword_notOk() {
+        user.setPassword(DEFAULT_PASSWORD + EMPTY_PLACE);
+        assertThrows(UserNotFoundException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_emptyPlaceLogin_notOk() {
+        user.setPassword(DEFAULT_LOGIN + EMPTY_PLACE);
+        assertThrows(UserNotFoundException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_emptyLogin_notOk() {
+        user.setLogin(EMPTY_STRING);
+        assertThrows(UserNotFoundException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_shortPassword_notOk() {
+        user.setPassword(SHORT_PASSWORD);
+        assertThrows(UserNotFoundException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_smallAge_notOk() {
+        user.setAge(SMALL_AGE);
+        assertThrows(UserNotFoundException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_negativeAge_notOk() {
+        user.setAge(NEGATIVE_AGE);
+        assertThrows(UserNotFoundException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_noAge_notOk() {
+        user.setAge(NULL_AGE);
+        assertThrows(UserNotFoundException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_bigAge_notOk() {
+        user.setAge(BIG_AGE);
+        assertThrows(UserNotFoundException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_userRegistered_notOk() {
+        User userRegistered = user;
+        registrationService.register(userRegistered);
+        assertThrows(UserNotFoundException.class, () -> registrationService.register(user));
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
 }
