8e2d9715a435756298ae44c6f5d29c9b5b1fa0c7
Your NameMykola <mykola.osolinskyi@gmail.com>
2023-01-05 17:54:34 +0100

    solution with 83 percent coverage

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,55 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.InvalidInputDataException;
 import core.basesyntax.model.User;
+import org.jetbrains.annotations.NotNull;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_USER_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        checkUser(user);
+        checkLogin(user);
+        checkPassword(user);
+        checkAge(user);
+        return storageDao.add(user);
+    }
+
+    private void checkUser(User user) {
+        if (user == null) {
+            throw new InvalidInputDataException("User can't be null");
+        }
+    }
+
+    private void checkLogin(User user) {
+        if (user.getLogin() == null) {
+            throw new InvalidInputDataException("Login can't be null");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new InvalidInputDataException("User with this login is already exists!");
+        }
+    }
+
+    private void checkPassword(User user) {
+        if (user.getPassword() == null) {
+            throw new InvalidInputDataException("Password can't be null");
+        }
+        if (user.getPassword().length() <= MIN_PASSWORD_LENGTH) {
+            throw new InvalidInputDataException("Password length must be more or equals " + MIN_PASSWORD_LENGTH);
+        }
+    }
+
+    private void checkAge(User user) {
+        if (user.getAge() == null) {
+            throw new InvalidInputDataException("Age can't be null");
+        }
+        if (user.getAge() < MIN_USER_AGE) {
+            throw new InvalidInputDataException("Age must be more or equals " + MIN_USER_AGE);
+        }
     }
 }
+
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,111 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.InvalidInputDataException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+    private static StorageDao storageDao;
+    private static final int VALID_AGE = 18;
+    private static final String VALID_PASSWORD = "userPassword";
+    private static final String VALID_LOGIN = "validLogin";
+    private static final String SECOND_VALID_LOGIN = "qwerty";
+
+    @Test
+    void register_userIsNull_NotOk() {
+        User newUser = null;
+        assertThrows(InvalidInputDataException.class, () ->
+                registrationService.register(newUser)
+        );
+    }
+
+    @Test
+    void register_loginIsNull_NotOk() {
+        User newUser = new User(null, VALID_PASSWORD, VALID_AGE);
+        Assertions.assertThrows(InvalidInputDataException.class,
+                () -> registrationService.register(newUser), "Login can't be null"
+        );
+    }
+
+    @Test
+    void register_userWithSuchLoginAlreadyExist_NotOk() {
+        User newUser = new User(VALID_LOGIN, VALID_PASSWORD, VALID_AGE);
+        assertThrows(InvalidInputDataException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userAgeLessThanMin_NotOk() {
+        User newUser = new User("thisIsLogin", VALID_PASSWORD, VALID_AGE - 5);
+        Assertions.assertThrows(RuntimeException.class,
+                () -> registrationService.register(newUser), "Not valid age"
+        );
+    }
+
+    @Test
+    void register_userAgeEnough_Ok() {
+        User user = new User("newLogin", VALID_PASSWORD, VALID_AGE + 5);
+        assertEquals(user, registrationService.register(user));
+    }
+
+    @Test
+    public void register_ageFloorEdge_NotOk() {
+        User newUser = new User(VALID_LOGIN, VALID_PASSWORD, VALID_AGE - 1);
+        assertThrows(InvalidInputDataException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    public void register_userAgeIsNull_NotOk() {
+        User user = new User("myLogin", VALID_PASSWORD, null);
+        Assertions.assertThrows(RuntimeException.class, () ->
+                registrationService.register(user), "Age cant be null");
+    }
+
+    @Test
+    void register_passwordIsNull_NotOk() {
+        User newUser = new User("someLogin", null, VALID_AGE);
+        Assertions.assertThrows(RuntimeException.class,
+                () -> registrationService.register(newUser)
+        );
+    }
+
+    @Test
+    void register_checkPasswordLength_NotOk() {
+        User newUser = new User("another", "abc", VALID_AGE);
+        Assertions.assertThrows(RuntimeException.class,
+                () -> registrationService.register(newUser)
+        );
+    }
+
+    @Test
+    void register_passwordLengthOk_Ok() {
+        User newUser = new User("anotherLogin","enoughLengthPassword",VALID_AGE);
+        assertEquals(newUser, registrationService.register(newUser));
+    }
+
+    @BeforeAll
+    public static void init() {
+        registrationService = new RegistrationServiceImpl();
+        storageDao = new StorageDaoImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        Storage.people.clear();
+        User firstUser = new User(VALID_LOGIN, VALID_PASSWORD, VALID_AGE);
+        User secondUser = new User(SECOND_VALID_LOGIN, VALID_PASSWORD, VALID_AGE);
+        storageDao.add(firstUser);
+        storageDao.add(secondUser);
+    }
+}
