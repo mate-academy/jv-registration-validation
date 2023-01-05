61a2b27194a9692c6d6398a4f4111e28e3a805cd
Дмитро <dima.smyk99.19@gmail.com>
2023-01-05 16:10:41 +0200

    created tests for valid registration data

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,49 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exceptions.InvalidUserDataException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_SIZE = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        checkLogin(user.getLogin());
+        checkAge(user.getAge());
+        checkPassword(user.getPassword());
+        return storageDao.add(user);
+    }
+
+    private void checkLogin(String login) {
+        if (login == null || login.isEmpty()) {
+            throw new InvalidUserDataException("Login can't be empty!");
+        }
+        if (storageDao.get(login) != null) {
+            throw new InvalidUserDataException("User with this login "
+                    + login + " is already registered!");
+        }
+    }
+
+    private void checkPassword(String password) {
+        if (password == null || password.isEmpty()) {
+            throw new InvalidUserDataException("Password can't be empty!");
+        }
+        if (password.length() <= MIN_PASSWORD_SIZE) {
+            throw new InvalidUserDataException("Password length can't be"
+                     + "less then " + MIN_PASSWORD_SIZE + "!");
+        }
+    }
+
+    private void checkAge(Integer age) {
+        if (age == null) {
+            throw new InvalidUserDataException("Your age can't be null");
+        }
+        if (age < MIN_AGE) {
+            throw new InvalidUserDataException("Your age " + age + " less"
+                    + " then allowed: " + MIN_AGE);
+        }
     }
 }
