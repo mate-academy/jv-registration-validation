47abbfc779b946a14499b6033e55c5b880755a0f
Maksim Surzhik <kabaneri15@gmail.com>
2023-01-05 16:39:03 +0200

    fix methods + rename

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -12,24 +12,14 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) throws InvalidUserException {
-        if (user.getLogin() == null) {
-            throw new InvalidUserException("Current user doesn't exist! This is NULL value e-mail");
-        }
-        if (storageDao.get(user.getLogin()) != null) {
-            throw new InvalidUserException("Current user with login: " + user.getLogin()
-                    + " already exists! Create new user with different e-mail");
-        }
-        if (user.getAge() == null) {
-            throw new InvalidUserException("The age is null!");
-        }
-        if (user.getAge() <= 0) {
-            throw new InvalidUserException("The age is 0 or above! Uncorrected value");
-        }
-        if (user.getAge() < MIN_AGE_LINE && user.getAge() > 0) {
-            throw new InvalidUserException("The age: " + user.getAge()
-                    + " is above minimum value!");
+        checkLogin(user);
+        checkAge(user);
+        checkPassword(user);
+        storageDao.add(user);
+        return user;
+    }
 
-        }
+    private static void checkPassword(User user) {
         if (user.getPassword() == null) {
             throw new InvalidUserException("Your password is NULL");
         }
@@ -40,7 +30,28 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user.getPassword().length() == 0) {
             throw new InvalidUserException("Your password length is empty! Enter new password");
         }
-        storageDao.add(user);
-        return user;
+    }
+
+    private static void checkAge(User user) {
+        if (user.getAge() == null) {
+            throw new InvalidUserException("The age is null!");
+        }
+        if (user.getAge() <= 0) {
+            throw new InvalidUserException("The age is 0 or above! Uncorrected value");
+        }
+        if (user.getAge() < MIN_AGE_LINE && user.getAge() > 0) {
+            throw new InvalidUserException("The age: " + user.getAge()
+                    + " is above minimum value!");
+        }
+    }
+
+    private void checkLogin(User user) {
+        if (user.getLogin() == null) {
+            throw new InvalidUserException("Current user doesn't exist! This is NULL value e-mail");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new InvalidUserException("Current user with login: " + user.getLogin()
+                    + " already exists! Create new user with different e-mail");
+        }
     }
 }
