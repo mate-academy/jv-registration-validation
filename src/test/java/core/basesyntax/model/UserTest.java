89a82d82669b03b35097bb61f34a3c0ddb693a45
Maksim Surzhik <kabaneri15@gmail.com>
2023-01-05 15:52:52 +0200

    added solution4

--- src/test/java/core/basesyntax/model/UserTest.java
+++ src/test/java/core/basesyntax/model/UserTest.java
@@ -1,11 +1,12 @@
 package core.basesyntax.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import core.basesyntax.exeption.InvalidEmailException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
     private static final String DEFAULT_EMAIL = "test_email@gmail.com";
