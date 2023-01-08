f80444db625564ee9ffd7df2f1b720e98e7e0f54
Leonid <leonvil093@gmail.com>
2023-01-08 10:31:23 +0200

    changes

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -7,7 +7,7 @@ import core.basesyntax.model.User;
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int MINIMUM_PASSWORD_LENGTH = 6;
     private static final int MIN_AGE = 18;
-    private static final StorageDao storageDao = new StorageDaoImpl();
+    private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
