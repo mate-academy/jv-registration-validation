55d6c8ee4c9438b1eeb6e4ea49d211aab24c850e
Your NameMykola <mykola.osolinskyi@gmail.com>
2023-01-09 13:32:30 +0100

    solution after first mentor's review

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,6 +1,5 @@
 package core.basesyntax.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.db.Storage;
@@ -20,78 +19,6 @@ class RegistrationServiceImplTest {
     private static final String VALID_LOGIN = "validLogin";
     private static final String SHORT_PASSWORD = "abc";
 
-    @Test
-    void register_userIsNull_NotOk() {
-        User newUser = null;
-        assertThrows(InvalidInputDataException.class, () ->
-                registrationService.register(newUser)
-        );
-    }
-
-    @Test
-    void register_loginIsNull_NotOk() {
-        user.setLogin(null);
-        Assertions.assertThrows(InvalidInputDataException.class,
-                () -> registrationService.register(user), "Login can't be null"
-        );
-    }
-
-    @Test
-    void register_userWithSuchLoginAlreadyExist_NotOk() {
-        registrationService.register(user);
-        assertThrows(InvalidInputDataException.class, () ->
-                registrationService.register(user));
-    }
-
-    @Test
-    void register_userAgeLessThanMin_NotOk() {
-        user.setAge(VALID_AGE - 5);
-        Assertions.assertThrows(RuntimeException.class,
-                () -> registrationService.register(user), "Not valid age"
-        );
-    }
-
-    @Test
-    void register_userAgeEnough_Ok() {
-        user.setAge(VALID_AGE + 5);
-        assertEquals(user, registrationService.register(user));
-    }
-
-    @Test
-    public void register_ageFloorEdge_NotOk() {
-        user.setAge(VALID_AGE - 1);
-        assertThrows(InvalidInputDataException.class, () ->
-                registrationService.register(user));
-    }
-
-    @Test
-    public void register_userAgeIsNull_NotOk() {
-        user.setAge(null);
-        Assertions.assertThrows(RuntimeException.class, () ->
-                registrationService.register(user), "Age cant be null");
-    }
-
-    @Test
-    void register_passwordIsNull_NotOk() {
-        user.setPassword(null);
-        Assertions.assertThrows(RuntimeException.class,
-                () -> registrationService.register(user)
-        );
-    }
-
-    @Test
-    void register_shortPasswordLength_NotOk() {
-        user.setPassword(SHORT_PASSWORD);
-        Assertions.assertThrows(RuntimeException.class,
-                () -> registrationService.register(user)
-        );
-    }
-
-    @Test
-    void register_passwordLengthOk_Ok() {
-        assertEquals(user, registrationService.register(user));
-    }
-
     @BeforeAll
     public static void init() {
         registrationService = new RegistrationServiceImpl();
@@ -103,10 +30,71 @@ class RegistrationServiceImplTest {
         user.setLogin(VALID_LOGIN);
         user.setPassword(VALID_PASSWORD);
         user.setAge(VALID_AGE);
+        registrationService.register(user);
     }
 
     @AfterEach
     void tearDown() {
         Storage.people.clear();
+    }
+
+    @Test
+    void register_userIsNull_notOk() {
+        User newUser = null;
+        assertThrows(InvalidInputDataException.class, () ->
+                registrationService.register(newUser)
+        );
+    }
+
+    @Test
+    void register_loginIsNull_notOk() {
+        user.setLogin(null);
+        Assertions.assertThrows(InvalidInputDataException.class,
+                () -> registrationService.register(user), "Login can't be null"
+        );
+    }
+
+    @Test
+    void register_userWithSuchLoginAlreadyExist_notOk() {
+        assertThrows(InvalidInputDataException.class, () ->
+                registrationService.register(user));
+    }
+
+    @Test
+    void register_userAgeLessThanMin_notOk() {
+        user.setAge(VALID_AGE - 5);
+        Assertions.assertThrows(RuntimeException.class,
+                () -> registrationService.register(user), "Not valid age"
+        );
+    }
+
+    @Test
+    public void register_ageFloorEdge_notOk() {
+        user.setAge(VALID_AGE - 1);
+        assertThrows(InvalidInputDataException.class, () ->
+                registrationService.register(user));
+    }
+
+    @Test
+    public void register_userAgeIsNull_notOk() {
+        user.setAge(null);
+        Assertions.assertThrows(RuntimeException.class, () ->
+                registrationService.register(user), "Age cant be null");
+    }
+
+    @Test
+    void register_passwordIsNull_notOk() {
+        user.setPassword(null);
+        Assertions.assertThrows(RuntimeException.class,
+                () -> registrationService.register(user)
+        );
+    }
+
+    @Test
+    void register_shortPasswordLength_notOk() {
+        user.setPassword(SHORT_PASSWORD);
+        Assertions.assertThrows(RuntimeException.class,
+                () -> registrationService.register(user)
+        );
     }
 }
