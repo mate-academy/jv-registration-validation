70a16db5b133cc886fedae2c28927961c8de21b7
Leonid <leonvil093@gmail.com>
2023-01-10 11:25:27 +0200

    fixed mistakes

--- src/test/java/core/basesyntax/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    private RegistrationServiceImpl regService = new RegistrationServiceImpl();
+    private final RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
     private User user = new User();
 
     @BeforeEach
@@ -24,64 +24,64 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_addAdultUser_ok() {
-        assertEquals(user, regService.register(user),
+        assertEquals(user, registrationService.register(user),
                 "User: " + user + " should be created");
     }
 
     @Test
     void register_addNullLogin_NotOk() {
         user.setLogin(null);
-        assertThrows(RuntimeException.class, () -> regService.register(user),
+        assertThrows(RuntimeException.class, () -> registrationService.register(user),
                 "Login can't be null");
     }
 
     @Test
     void register_addEmptyLogin_NotOk() {
         user.setLogin("");
-        assertThrows(RuntimeException.class, () -> regService.register(user),
+        assertThrows(RuntimeException.class, () -> registrationService.register(user),
                 "Login can't be empty");
     }
 
     @Test
     void register_addPasswordEmpty_NotOk() {
         user.setPassword("");
-        assertThrows(RuntimeException.class, () -> regService.register(user),
+        assertThrows(RuntimeException.class, () -> registrationService.register(user),
                 "Password can't be empty");
     }
 
     @Test
     void register_addNullPassword_NotOk() {
         user.setPassword(null);
-        assertThrows(RuntimeException.class, () -> regService.register(user),
+        assertThrows(RuntimeException.class, () -> registrationService.register(user),
                 "Password cant be null");
     }
 
     @Test
     void register_addShortPasswordLength_NotOk() {
         user.setPassword("abcde");
-        assertThrows(RuntimeException.class, () -> regService.register(user),
+        assertThrows(RuntimeException.class, () -> registrationService.register(user),
                 "This password to short, must be 6 symbols");
     }
 
     @Test
     void register_addUserAlreadyExist_NotOk() {
-        User user = new User();
-        user.setLogin("Ivan_Ivanov");
-        assertThrows(RuntimeException.class, () -> regService.register(user),
+        User newUser = new User();
+        newUser.setLogin("Ivan_Ivanov");
+        assertThrows(RuntimeException.class, () -> registrationService.register(newUser),
                 " This user can't created, it already exist");
     }
 
     @Test
     void register_addYoungAge_NotOk() {
-        user.setAge(12);
-        assertThrows(RuntimeException.class, () -> regService.register(user),
+        user.setAge(17);
+        assertThrows(RuntimeException.class, () -> registrationService.register(user),
                 "User age should be greater than 18");
     }
 
     @Test
     void register_addIncorrectAge_NotOk() {
         user.setAge(Integer.MIN_VALUE);
-        assertThrows(RuntimeException.class, () -> regService.register(user),
+        assertThrows(RuntimeException.class, () -> registrationService.register(user),
                 "User age should be greater than 18");
     }
 
@@ -89,7 +89,7 @@ class RegistrationServiceImplTest {
     void register_addYoungAge_ShortPassword_NotOk() {
         user.setPassword("12345");
         user.setAge(17);
-        assertThrows(RuntimeException.class, () -> regService.register(user),
+        assertThrows(RuntimeException.class, () -> registrationService.register(user),
                 "This password: " + user.getPassword() + " to short, must be 6 symbols."
                 + " User age should be greater than 18");
     }
