773be111cb85263ad576ab222f88c8c584efbbde
iskamele <iskamele@gmail.com>
2023-01-24 22:47:49 +0200

    Implemented methods that check if the fields of the incoming object meet the requirements.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -12,27 +12,46 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
+        checkUser(user);
+        checkLogin(user);
+        checkPassword(user);
+        checkAge(user);
+        return storageDao.add(user);
+    }
+
+    private void checkUser(User user) {
         if (user == null) {
             throw new RegistrationException("User can't be null!");
         }
+    }
+
+    private void checkLogin(User user) {
         if (user.getLogin() == null) {
             throw new RegistrationException("Login can't be null!");
+        }
+        if (user.getLogin().isEmpty()) {
+            throw new RegistrationException("Login can't be empty!");
         }
         if (storageDao.get(user.getLogin()) != null) {
             throw new RegistrationException("Login " + user.getLogin() + " are exist!");
         }
-        if (user.getAge() == null) {
-            throw new RegistrationException("Age can't be null!");
-        }
-        if (user.getAge() < MIN_AGE) {
-            throw new RegistrationException("Age must be 18+");
-        }
+    }
+
+    private void checkPassword(User user) {
         if (user.getPassword() == null) {
             throw new RegistrationException("Password can't be null!");
         }
         if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
-            throw new RegistrationException("Minimum password length must be 6 characters!");
+            throw new RegistrationException("Password must be at least " + MIN_PASSWORD_LENGTH + " characters length!");
         }
-        return storageDao.add(user);
+    }
+
+    private void checkAge(User user) {
+        if (user.getAge() == null) {
+            throw new RegistrationException("Age can't be null!");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("Age must be at least " + MIN_AGE + "!");
+        }
     }
 }
