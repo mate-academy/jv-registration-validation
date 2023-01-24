f60fa9c7f8408d8a15e2f87451c16bea2bbb4066
iskamele <iskamele@gmail.com>
2023-01-24 19:56:13 +0200

    Implemented registration method.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,37 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exceptions.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RegistrationException("User can't be null!");
+        }
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Login can't be null!");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("Login " + user.getLogin() + " are exist!");
+        }
+        if (user.getAge() == null) {
+            throw new RegistrationException("Age can't be null!");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("Age must be 18+");
+        }
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Password can't be null!");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RegistrationException("Minimum password length must be 6 characters!");
+        }
+        return storageDao.add(user);
     }
 }
