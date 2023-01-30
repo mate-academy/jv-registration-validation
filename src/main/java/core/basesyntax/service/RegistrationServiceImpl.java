ed69b13007d1f944177e8e94de51117917c1db74
Tymofii Skrypko <skrypko270901@gmail.com>
2023-01-30 22:31:17 +0100

    implemented all requested changes

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,6 +6,9 @@ import core.basesyntax.exceptions.ValidationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int REQUIRED_MINIMUM_AGE = 18;
+    private static final int MINIMUM_POSSIBLE_AGE = 1;
+    private static final int REQUIRED_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -18,16 +21,16 @@ public class RegistrationServiceImpl implements RegistrationService {
     }
 
     private void passwordCheck(User user) throws ValidationException {
-        if (user.getPassword().length() < 6) {
+        if (user.getPassword() == null || user.getPassword().length() < REQUIRED_PASSWORD_LENGTH) {
             throw new ValidationException("Password is too short");
         }
     }
 
     private void ageCheck(User user) throws ValidationException {
-        if (user.getAge() < 1) {
+        if (user.getAge() < MINIMUM_POSSIBLE_AGE) {
             throw new ValidationException("Impossible age");
         }
-        if (user.getAge() < 18) {
+        if (user.getAge() < REQUIRED_MINIMUM_AGE) {
             throw new ValidationException("User is too young to be registered");
         }
     }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -3,6 +3,7 @@ package core.basesyntax.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import core.basesyntax.exceptions.ValidationException;
 import core.basesyntax.model.User;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,12 +16,12 @@ class RegistrationServiceImplTest {
     private static final String DEFAULT_LOGIN = "Login";
     private static final String DEFAULT_PASSWORD = "123456";
     private static final int DEFAULT_AGE = 18;
-    private static final String LOGIN_1 = "login1";
-    private static final String LOGIN_2 = "login2";
-    private static final String LOGIN_3 = "login3";
-    private static final int AGE_1 = 100000;
-    private static final int AGE_2 = 25;
-    private static final int AGE_3 = 999999999;
+    private static final String LOGIN_FIRST = "login1";
+    private static final String LOGIN_SECOND = "login2";
+    private static final String LOGIN_THIRD = "login3";
+    private static final int AGE_FIRST = 100000;
+    private static final int AGE_SECOND = 25;
+    private static final int AGE_THIRD = 999999999;
     private static final int INCORRECT_AGE = 17;
     private static final int NEGATIVE_AGE = -1;
     private static final String INCORRECT_PASSWORD = "12345";
@@ -29,9 +30,9 @@ class RegistrationServiceImplTest {
     static void beforeAll() {
         registrationService = new RegistrationServiceImpl();
         users = new ArrayList<>();
-        users.add(new User(LOGIN_1, DEFAULT_PASSWORD, AGE_1));
-        users.add(new User(LOGIN_2, DEFAULT_PASSWORD, AGE_2));
-        users.add(new User(LOGIN_3, DEFAULT_PASSWORD, AGE_3));
+        users.add(new User(LOGIN_FIRST, DEFAULT_PASSWORD, AGE_FIRST));
+        users.add(new User(LOGIN_SECOND, DEFAULT_PASSWORD, AGE_SECOND));
+        users.add(new User(LOGIN_THIRD, DEFAULT_PASSWORD, AGE_THIRD));
         users.add(new User(DEFAULT_LOGIN, DEFAULT_PASSWORD, DEFAULT_AGE));
     }
 
@@ -46,7 +47,7 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_existingLogin_notOk() {
-        assertThrows(RuntimeException.class, () -> {
+        assertThrows(ValidationException.class, () -> {
             registrationService.register(users.get(0));
             registrationService.register(users.get(0));
         });
@@ -54,42 +55,42 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_ageUnderRequired_notOk() {
-        assertThrows(RuntimeException.class, () -> {
+        assertThrows(ValidationException.class, () -> {
             registrationService.register(new User(DEFAULT_LOGIN, DEFAULT_PASSWORD, INCORRECT_AGE));
         });
     }
 
     @Test
     void register_shortPassword_notOk() {
-        assertThrows(RuntimeException.class, () -> {
+        assertThrows(ValidationException.class, () -> {
             registrationService.register(new User(DEFAULT_LOGIN, INCORRECT_PASSWORD, DEFAULT_AGE));
         });
     }
 
     @Test
     void register_nullLogin_notOk() {
-        assertThrows(RuntimeException.class, () -> {
+        assertThrows(ValidationException.class, () -> {
             registrationService.register(new User(null, DEFAULT_PASSWORD, DEFAULT_AGE));
         });
     }
 
     @Test
     void register_nullPassword_notOk() {
-        assertThrows(RuntimeException.class, () -> {
+        assertThrows(ValidationException.class, () -> {
             registrationService.register(new User(DEFAULT_LOGIN, null, DEFAULT_AGE));
         });
     }
 
     @Test
     void register_nullAge_notOk() {
-        assertThrows(RuntimeException.class, () -> {
+        assertThrows(ValidationException.class, () -> {
             registrationService.register(new User(DEFAULT_LOGIN, DEFAULT_PASSWORD, null));
         });
     }
 
     @Test
     void register_negativeAge_notOk() {
-        assertThrows(RuntimeException.class, () -> {
+        assertThrows(ValidationException.class, () -> {
             registrationService.register(new User(DEFAULT_LOGIN, DEFAULT_PASSWORD, NEGATIVE_AGE));
         });
     }
