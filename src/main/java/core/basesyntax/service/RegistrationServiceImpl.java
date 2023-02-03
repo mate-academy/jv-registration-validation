7c2e48151ce993adabcb4b7340c07bf17d902f70
Nikita Kolesnikov <kit1datasvit@gmail.com>
2023-02-03 12:35:45 +0200

    hw-solution-reg-service fix after review fix mistakes

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -29,7 +29,7 @@ public class RegistrationServiceImpl implements RegistrationService {
             throw new RegistrationExeption("User already exist !");
         }
 
-            return storageDao.add(user);
+        return storageDao.add(user);
     }
 
 }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -3,8 +3,6 @@ package core.basesyntax.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import core.basesyntax.dao.StorageDao;
-import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.exeption.RegistrationExeption;
 import core.basesyntax.model.User;
