201dd9b73f16bf41404b341e0bfa1a72eb301964
Oleksii Bilko <lucheus@gmail.com>
2023-02-13 13:33:18 +0200

    implemented reg_service with unit tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,34 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exceptions.UserRegisterException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null || user.getLogin().length() == 0) {
+            throw new UserRegisterException("Login can't be empty");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new UserRegisterException("This login is already registered");
+        }
+        if (user.getPassword() == null || user.getPassword().length() == 0) {
+            throw new UserRegisterException("Password can't be empty");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new UserRegisterException("Password can't be less than six characters");
+        }
+        if (user.getAge() == null) {
+            throw new UserRegisterException("The age can`t be null");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new UserRegisterException("The age can`t be less than eighteen years");
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceTest.java
@@ -0,0 +1,127 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.exceptions.UserRegisterException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceTest {
+    private static StorageDao storageDao;
+    private static RegistrationServiceImpl registrationService;
+    private static User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        storageDao = new StorageDaoImpl();
+        registrationService = new RegistrationServiceImpl();
+        user = new User();
+    }
+
+    @BeforeEach
+    void setUp() {
+        user.setLogin("bob2020");
+        user.setAge(20);
+        user.setPassword("123456");
+    }
+
+    @Test
+    void register_nullLogin_notOk() {
+        user.setLogin(null);
+        assertThrows(UserRegisterException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_emptyLogin_notOk() {
+        user.setLogin("");
+        assertThrows(UserRegisterException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_sameLogin_notOk() {
+        storageDao.add(user);
+        assertThrows(UserRegisterException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_nullPass_notOk() {
+        user.setPassword(null);
+        assertThrows(UserRegisterException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_emptyPass_notOk() {
+        user.setPassword("");
+        assertThrows(UserRegisterException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_shortPass_notOk() {
+        user.setPassword("123");
+        assertThrows(UserRegisterException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_nullAge_notOk() {
+        user.setAge(null);
+        assertThrows(UserRegisterException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_negativeAge_notOk() {
+        user.setAge(-1);
+        assertThrows(UserRegisterException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_underAge_notOk() {
+        user.setAge(17);
+        assertThrows(UserRegisterException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_18Age_ok() {
+        user.setAge(18);
+        registrationService.register(user);
+        boolean actual = Storage.people.contains(user);
+        assertTrue(actual);
+    }
+
+    @Test
+    void register_overAge_ok() {
+        user.setAge(19);
+        registrationService.register(user);
+        boolean actual = Storage.people.contains(user);
+        assertTrue(actual);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
+}
