923d7ba3c4971fa0d9fdeed32dd15dd9c0410223
Yaroslav Ometiukh <yaroslav.ometiukh@outlook.com>
2023-02-15 15:41:29 +0200

    -- refactored register(User user) method, each check is handled in private method.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -12,47 +12,51 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        String msg = checkForNullValues(user);
-        if (!msg.equals("OK")) {
-            throw new UserValidationException(msg);
-        } else if (storageDao.get(user.getLogin()) != null) {
-            throw new UserValidationException("Login is already registered");
-        } else if (!isLoginValid(user)) {
-            throw new UserValidationException(
-                    "Can't be empty or contain whitespace");
-        } else if (!isAgeValid(user)) {
-            throw new UserValidationException(
-                    "Must meet the minimum age requirement");
-        } else if (!isPasswordValid(user)) {
-            throw new UserValidationException(
-                    "Must be at least 6 characters without whitespace");
-        }
+        checkForNullValues(user);
+        loginValid(user);
+        userNotExists(user);
+        ageValid(user);
+        passwordValid(user);
         return storageDao.add(user);
     }
 
-    private boolean isLoginValid(User user) {
-        return !user.getLogin().isEmpty() && !user.getLogin().contains(" ");
-    }
-
-    private boolean isAgeValid(User user) {
-        return user.getAge() >= USER_MIN_AGE;
-    }
-
-    private boolean isPasswordValid(User user) {
-        return !user.getPassword().isEmpty() && !user.getPassword().contains(" ")
-                && user.getPassword().length() >= USER_PASSWORD_MIN_LENGTH;
-    }
-
-    private String checkForNullValues(User user) {
+    private void checkForNullValues(User user) {
         if (user == null) {
-            return "User can't be null";
+            throw new UserValidationException("User can't be null");
         } else if (user.getLogin() == null) {
-            return "User can't be null";
+            throw new UserValidationException("Login can't be null");
         } else if (user.getPassword() == null) {
-            return "Password can't be null";
+            throw new UserValidationException("Password can't be null");
         } else if (user.getAge() == null) {
-            return "Age can't be null";
+            throw new UserValidationException("Age can't be null");
         }
-        return "OK";
+    }
+
+    private void loginValid(User user) {
+        if (user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
+            throw new UserValidationException(
+                    "Can't be empty or contain whitespace");
+        }
+    }
+
+    private void userNotExists(User user) {
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new UserValidationException("Login is already registered");
+        }
+    }
+
+    private void ageValid(User user) {
+        if (user.getAge() < USER_MIN_AGE) {
+            throw new UserValidationException(
+                    "Must meet the minimum age requirement");
+        }
+    }
+
+    private void passwordValid(User user) {
+        if (user.getPassword().isEmpty() || user.getPassword().contains(" ")
+                || user.getPassword().length() < USER_PASSWORD_MIN_LENGTH) {
+            throw new UserValidationException(
+                    "Must be at least 6 characters without whitespace");
+        }
     }
 }
