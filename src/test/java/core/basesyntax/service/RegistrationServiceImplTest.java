5fc9d7f1a7fced946a2e9089881dee8533e9d456
Igor Puhach <igorpuhach21@gmail.com>
2023-02-21 23:04:24 +0200

    fixed bugs after review № 5 I don't found out a few fixes

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -10,56 +10,57 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
+    private static final String VALID_PASSWORD = "12345";
+    private static final String INVALID_PASSWORD = "1234567";
     private static RegistrationService registrationService = new RegistrationServiceImpl();
 
     @Before
-    void before() {
+    void init() {
         registrationService = new RegistrationServiceImpl();
     }
 
     @Test
     void register_nullAge_notOk() {
-        User user = new User(1L,"Bob","12345",0);
+        User user = new User(1L, "Bob", VALID_PASSWORD, 0);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_nullLogin_notOk() {
-        User user = new User(1L,null,"1234567",20);
+        User user = new User(1L, null, INVALID_PASSWORD, 20);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_userAlreadyExists_notOk() {
-        User user = new User(1L,"Ivan ivanov","1234567",21);
+        User user = new User(1L, "Ivan ivanov", INVALID_PASSWORD, 21);
         Storage.people.add(user);
-        User userReg = new User(1L,"Ivan ivanov","1234567",21);
+        User userReg = new User(1L, "Ivan ivanov", INVALID_PASSWORD, 21);
         assertThrows(RegistrationException.class, () -> registrationService.register(userReg));
     }
 
     @Test
     void register_shortPassword_notOk() {
-        User user = new User(1L,"Jane","12345",22);
+        User user = new User(1L, "Jane", VALID_PASSWORD, 22);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_lessAge_notOk() {
-        User user = new User(1L,"Nike","12345",15);
+        User user = new User(1L, "Nike", VALID_PASSWORD, 15);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_validData() {
         int size = Storage.people.size();
-        User user = new User(1L,"Nike","1234567",22);
+        User user = new User(1L, "Nike", INVALID_PASSWORD, 22);
         registrationService.register(user);
-        assertEquals(size + 1,Storage.people.size());
+        assertEquals(size + 1, Storage.people.size());
     }
 
     @AfterEach
     public void afterEach() {
-
         Storage.people.clear();
     }
 }
