874c5615b84814f49280938dfd9f8219638b3894
Sergiy Golubchenko <galuba1@gmail.com>
2023-02-22 22:58:09 +0200

    Fix issue and add validateUser method

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -8,28 +8,38 @@ import core.basesyntax.model.User;
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int AGE_LOW_LIMIT = 18;
     private static final int AGE_UP_LIMIT = 80;
-    private final StorageDao storageDao = new StorageDaoImpl();
+    private static final String SPACE = " ";
+    private final StorageDao storageDao;
+
+    public RegistrationServiceImpl() {
+        storageDao = new StorageDaoImpl();
+    }
 
     @Override
     public User register(User user) {
         if (user == null) {
             throw new RegisterValidationException("User must be Not null");
         }
+        validateUser(user);
+        storageDao.add(user);
+        return user;
+    }
+
+    private void validateUser(User user) {
         if (user.getAge() == null
                 || user.getAge() < AGE_LOW_LIMIT
                 || user.getAge() > AGE_UP_LIMIT) {
-            throw new RegisterValidationException("You enter wrong age");
+            throw new RegisterValidationException("You enter wrong age. Expected between "
+                    + AGE_LOW_LIMIT + " and " + AGE_UP_LIMIT + " but was " + user.getAge());
         }
-        if (user.getLogin() == null || user.getLogin().contains(" ")) {
-            throw new RegisterValidationException("You enter wrong login");
+        if (user.getLogin() == null || user.getLogin().contains(SPACE)) {
+            throw new RegisterValidationException("Your login must not contain a space");
         }
         if (user.getPassword() == null || user.getPassword().length() < 8) {
-            throw new RegisterValidationException("You enter wrong password");
+            throw new RegisterValidationException("You password must be more then 8 character");
         }
         if (storageDao.get(user.getLogin()) != null) {
             throw new RegisterValidationException("Login already exist!");
         }
-        storageDao.add(user);
-        return user;
     }
 }
