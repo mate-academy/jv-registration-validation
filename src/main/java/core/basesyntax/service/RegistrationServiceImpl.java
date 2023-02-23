642605e45b71cfd89af4f6c49fb56b5b5abd15b3
user <graban2253@gmail.com>
2023-02-23 22:17:17 +0200

    Finished

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,36 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_LEN_PAS = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+
+        if (user == null) {
+            throw new RegistrationException("No user!");
+        }
+
+        if (user.getLogin() == null) {
+            throw new RegistrationException("No Login!");
+        }
+
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("You are too young");
+        }
+
+        if (user.getPassword() == null || user.getPassword().length() < MIN_LEN_PAS) {
+            throw new RegistrationException("The password is week");
+        }
+
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("User already exist");
+        }
+        return storageDao.add(user);
     }
 }
