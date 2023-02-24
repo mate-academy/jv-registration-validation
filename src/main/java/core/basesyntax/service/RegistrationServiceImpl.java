8a3f05a7418cf9a16b4be2606101cb25a51d2774
Yehor-Cherkashyn <gooooodvin1995@gmail.com>
2023-02-24 21:14:14 +0200

    Created tests for register method

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,14 +1,35 @@
 package core.basesyntax.service;
 
+import core.basesyntax.CustomException;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int VALID_AGE = 18;
+    private static final int MAX_VALID_AGE = 115;
+    private static final int VALID_COUNT_OF_CHARACTER = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
-    public User register(User user) {
-        return null;
+    public User register(User user) throws CustomException {
+        if (user == null) {
+            throw new CustomException("User is null");
+        }
+        if (user.getAge() < VALID_AGE || user.getAge() > MAX_VALID_AGE || user.getAge() == null) {
+            throw new CustomException("Age should be above 18 or equal and less than 115!");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new CustomException("User with the same Login already exist!");
+        }
+        for (char chars : user.getPassword().toCharArray()) {
+            if (!Character.isLetter(chars) || !Character.isDigit(chars)
+                    || user.getPassword().length() < VALID_COUNT_OF_CHARACTER
+                    || user.getPassword() == null) {
+                throw new CustomException("Your Password should include 6 "
+                        + "and more characters! Your Password cannot contains any special symbols");
+            }
+        }
+        return storageDao.add(user);
     }
 }
