7d5e4266fc76d42db0294f0c7a0d08e09eef0c3b
Mykyta <nikita.nikonets@gmail.com>
2023-02-28 22:21:36 +0200

    added messages to tests

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -30,74 +30,90 @@ class RegistrationServiceImplTest {
     @Test
     void register_AgeUnderMin_NotOk() {
         defaultUser.setAge(MIN_AGE - 1);
-        assertThrows(RegistrationException.class, () -> regService.register(defaultUser));
+        assertThrows(RegistrationException.class, () -> regService.register(defaultUser),
+                "Registered a user with age less than minimum without throwing an exception");
     }
 
     @Test
     void register_NegativeAge_NotOk() {
         defaultUser.setAge(-1);
-        assertThrows(RegistrationException.class, () -> regService.register(defaultUser));
+        assertThrows(RegistrationException.class, () -> regService.register(defaultUser),
+                "Registered a user with negative age value without throwing an exception");
     }
 
     @Test
     void register_ZeroAge_NotOk() {
         defaultUser.setAge(0);
-        assertThrows(RegistrationException.class, () -> regService.register(defaultUser));
+        assertThrows(RegistrationException.class, () -> regService.register(defaultUser),
+                "Registered a user with zero age without throwing an exception");
     }
 
     @Test
     void register_NullAge_NotOk() {
         defaultUser.setAge(null);
-        assertThrows(RegistrationException.class, () -> regService.register(defaultUser));
+        assertThrows(RegistrationException.class, () -> regService.register(defaultUser),
+                "Registered a user with age null without throwing an exception");
     }
 
     @Test
     void register_AgeMin_Ok() {
         defaultUser.setAge(MIN_AGE);
-        assertEquals(defaultUser, regService.register(defaultUser));
-        assertEquals(defaultUser, storageDao.get(defaultUser.getLogin()));
+        assertEquals(defaultUser, regService.register(defaultUser),
+                "User with minimum age wasn't registered");
+        assertEquals(defaultUser, storageDao.get(defaultUser.getLogin()),
+                "User with minimum age wasn't added to database");
     }
 
     @Test
     void register_AgeOverMin_Ok() {
         defaultUser.setAge(MIN_AGE + 1);
-        assertEquals(defaultUser, regService.register(defaultUser));
-        assertEquals(defaultUser, storageDao.get(defaultUser.getLogin()));
+        assertEquals(defaultUser, regService.register(defaultUser),
+                "User with age above minimum wasn't registered");
+        assertEquals(defaultUser, storageDao.get(defaultUser.getLogin()),
+                "User with age above minimum wasn't added to database");
     }
 
     @Test
     void register_NullId_NotOk() {
         defaultUser.setId(null);
-        assertThrows(RegistrationException.class, () -> regService.register(defaultUser));
+        assertThrows(RegistrationException.class, () -> regService.register(defaultUser),
+                "Registered a user with Id null without throwing an exception");
     }
 
     @Test
     void register_NullLogin_NotOk() {
         defaultUser.setLogin(null);
-        assertThrows(RegistrationException.class, () -> regService.register(defaultUser));
+        assertThrows(RegistrationException.class, () -> regService.register(defaultUser),
+                "Registered a user with Login null without throwing an exception");
     }
 
     @Test
     void register_ZeroLengthLogin_NotOk() {
         defaultUser.setLogin("");
-        assertThrows(RegistrationException.class, () -> regService.register(defaultUser));
+        assertThrows(RegistrationException.class, () -> regService.register(defaultUser),
+                "Registered a user with empty Login without throwing an exception");
     }
 
     @Test
     void register_NullPassword_NotOk() {
         defaultUser.setPassword(null);
-        assertThrows(RegistrationException.class, () -> regService.register(defaultUser));
+        assertThrows(RegistrationException.class, () -> regService.register(defaultUser),
+                "Registered a user with null password without throwing an exception");
     }
 
     @Test
     void register_ShortPassword_NotOk() {
         defaultUser.setPassword("12345");
-        assertThrows(RegistrationException.class, () -> regService.register(defaultUser));
+        assertThrows(RegistrationException.class, () -> regService.register(defaultUser),
+                "Registered a user with password shorter than minimum length "
+                        + "without throwing an exception");
     }
 
     @Test
     void register_UserAlreadyAdded_NotOk() {
         storageDao.add(defaultUser);
-        assertThrows(RegistrationException.class, () -> regService.register(defaultUser));
+        assertThrows(RegistrationException.class, () -> regService.register(defaultUser),
+                "Registered a user that is already added "
+                        + "to database without throwing an exception");
     }
 }
