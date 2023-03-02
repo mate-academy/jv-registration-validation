a00ee8f921f8cc325290f967790be42d3771dbd2
Melnychuk <melnychukmyhailo@gmail.com>
2023-03-02 11:15:11 +0200

    Create own exception.

---
+++ src/main/java/core/basesyntax/exceptions/RegistrationException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exceptions;
+
+public class RegistrationException extends RuntimeException{
+    public RegistrationException(String message) {
+        super(message);
+    }
+}
