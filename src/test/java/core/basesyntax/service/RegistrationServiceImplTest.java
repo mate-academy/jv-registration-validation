82d463b1c18e286d7cf2f6c78fa115869937b3fa
Artur Muliukov <arthur.muliukov@gmail.com>
2023-03-02 11:33:16 +0100

    Solved 1.6

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,39 +1,43 @@
 package core.basesyntax.service;
 
 import core.basesyntax.RegistrationException;
+import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegistrationServiceImplTest {
-    private User userOne;
-    private User userTwo;
-    private User userThree;
-    private User userFour;
+    private static RegistrationService registrationService;
+    private static StorageDao storageDao;
     private User user;
-    private RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        storageDao = new StorageDaoImpl();
+    }
 
     @BeforeEach
     void setUp() {
-        userOne = new User(1238383L, "world12", "userworld123", 20);
-        userTwo = new User(2323233L, "world13", "userworld1543", 25);
-        userThree = new User(2323233L, "world14", "userworld95884", 19);
-        userFour = new User(12333L, "world1266", "userw23023", 21);
         Storage.people.clear();
     }
 
     @Test
     void addUser_Ok() {
-        User userTestOne = registrationService.register(userOne);
-        User userTestTwo = registrationService.register(userTwo);
-        User userTestThree = registrationService.register(userThree);
-        assertEquals(userOne, userTestOne);
-        assertEquals(userTwo, userTestTwo);
-        assertEquals(userThree, userTestThree);
+        user = new User(1238383L, "world12", "userworld123", 20);
+        User userTestOne = registrationService.register(user);
+        user = new User(2323233L, "world13", "userworld1543", 25);
+        User userTestTwo = registrationService.register(user);
+        user = new User(2323233L, "world14", "userworld95884", 19);
+        User userTestThree = registrationService.register(user);
+        assertEquals(storageDao.get("world12"), userTestOne);
+        assertEquals(storageDao.get("world13"), userTestTwo);
+        assertEquals(storageDao.get("world14"), userTestThree);
         assertEquals(3, Storage.people.size());
     }
 
@@ -41,30 +45,31 @@ class RegistrationServiceImplTest {
     void addExistUser_NotOk() {
         assertThrows(RegistrationException.class, () -> {
             StorageDaoImpl storageDao = new StorageDaoImpl();
-            storageDao.add(userFour);
-            registrationService.register(userFour);
+            user = new User(12333L, "world1266", "userw23023", 21);
+            storageDao.add(user);
+            registrationService.register(user);
         }, "You can not add users with same login");
     }
 
     @Test
     void addUserWithNullPassword_NotOk() {
-        User userWithNullPassword = new User(12333L, "world126333", null, 21);
+        user = new User(12333L, "world126333", null, 21);
         assertThrows(RegistrationException.class, () -> {
-            registrationService.register(userWithNullPassword);
+            registrationService.register(user);
         }, "We can not add user with null password");
     }
 
     @Test
     void addUserWithNullLogin_NotOk() {
-        User userWithNullLogin = new User(12333L, null, "jdhfhd83", 21);
+        user = new User(12333L, null, "jdhfhd83", 21);
         assertThrows(RegistrationException.class, () -> {
-            registrationService.register(userWithNullLogin);
+            registrationService.register(user);
         }, "We can not add user with null login");
     }
 
     @Test
     void addUserWithForbiddenAge_NotOk() {
-        User user = new User(12333L, "newLogin1029", "jdhfhd83", 17);
+        user = new User(12333L, "newLogin1029", "jdhfhd83", 17);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         }, "You can not add use with age under 18");
@@ -72,7 +77,7 @@ class RegistrationServiceImplTest {
 
     @Test
     void addUserWithForbiddenPassword_NotOk() {
-        User user = new User(12333L, "newLogin10329", "2828", 18);
+        user = new User(12333L, "newLogin10329", "2828", 18);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         }, "You can not add user with password length less then 8");
@@ -80,7 +85,7 @@ class RegistrationServiceImplTest {
 
     @Test
     void addUserWithNullAge_NotOk() {
-        User user = new User(12333L, "newLogin10329", "2828333333", null);
+        user = new User(12333L, "newLogin10329", "2828333333", null);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         }, " You can not add user with null age");
