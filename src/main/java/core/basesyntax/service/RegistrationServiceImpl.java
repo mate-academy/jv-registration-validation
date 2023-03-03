a63b16f539b3b444abd245403c1fafa2bed076a7
IvanZuravlev228 <courses.zhuravlev.ivan@gmail.com>
2023-03-03 17:41:25 +0200

    fied checkstyle mistake

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,15 +6,23 @@ import core.basesyntax.model.User;
 public class RegistrationServiceImpl implements RegistrationService {
     @Override
     public User register(User user) {
-
-        if (user != null && user.getAge() != null
-                && user.getLogin() != null && user.getPassword() != null) {
-            StorageDaoImpl storage = new StorageDaoImpl();
-            if (storage.get(user.getLogin()) == null) {
-                if (user.getAge() >= 18 && user.getPassword().length() >= 6) {
-                    storage.add(user);
-                    return user;
-                }
+        if (user == null) {
+            throw new RuntimeException("User object can't be null");
+        }
+        if (user.getLogin() == null) {
+            throw new RuntimeException("User login can't be null");
+        }
+        if (user.getAge() == null) {
+            throw new RuntimeException("User age can't be null");
+        }
+        if (user.getPassword() == null) {
+            throw new RuntimeException("User password can't be null");
+        }
+        StorageDaoImpl storageDao = new StorageDaoImpl();
+        if (storageDao.get(user.getLogin()) == null) {
+            if (user.getAge() >= 18 && user.getPassword().length() >= 6) {
+                storageDao.add(user);
+                return user;
             }
         }
         return null;
--- src/test/java/core/basesyntax/UserTest.java
+++ src/test/java/core/basesyntax/UserTest.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
@@ -11,77 +12,81 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UserTest {
-    private RegistrationServiceImpl register;
+    private RegistrationServiceImpl registration;
     private User user;
     private StorageDao storageDaoImpl;
 
     @BeforeEach
     void setUp() {
-        register = new RegistrationServiceImpl();
+        registration = new RegistrationServiceImpl();
         storageDaoImpl = new StorageDaoImpl();
         user = new User();
         user.setLogin("Ivan Zhuravlev");
-        user.setAge(22);
+        user.setAge(18);
         user.setPassword("my password 123");
     }
 
     @Test
     void register_Ok() {
-        User actual = register.register(user);
+        User actual = registration.register(user);
         assertEquals(user, actual);
     }
 
     @Test
     void addedToStorage_Ok() {
-        register.register(user);
+        registration.register(user);
         assertEquals(user, storageDaoImpl.get("Ivan Zhuravlev"));
     }
 
     @Test
     void objectUserIsNull_NotOk() {
-        User actual = register.register(null);
-        assertNull(actual);
+        assertThrows(RuntimeException.class, () -> {
+            registration.register(null);
+        });
     }
 
     @Test
     void loginIsNull_NotOk() {
         user.setLogin(null);
-        User actual = register.register(user);
-        assertNull(actual);
+        assertThrows(RuntimeException.class, () -> {
+            registration.register(user);
+        });
     }
 
     @Test
     void passwordIsNull_NotOk() {
         user.setPassword(null);
-        User actual = register.register(user);
-        assertNull(actual);
+        assertThrows(RuntimeException.class, () -> {
+            registration.register(user);
+        });
     }
 
     @Test
-    void agedIsNull_NotOk() {
+    void ageIsNull_NotOk() {
         user.setAge(null);
-        User actual = register.register(user);
-        assertNull(actual);
+        assertThrows(RuntimeException.class, () -> {
+            registration.register(user);
+        });
     }
 
     @Test
     void notValidAge_NotOk() {
         user.setAge(-10);
-        User actual = register.register(user);
+        User actual = registration.register(user);
         assertNull(actual);
     }
-    
+
     @Test
     void littleAge_NotOk() {
         user.setAge(17);
-        User actual = register.register(user);
+        User actual = registration.register(user);
         assertNull(actual);
     }
 
     @Test
     void smallPassword_NotOk() {
         user.setPassword("12345");
-        User actual = register.register(user);
+        User actual = registration.register(user);
         assertNull(actual);
     }
 }
