a14a643c4c6938ba9aa348f2eac98ada924cd9df
Budkevych Nikita <jatfun58@gmail.com>
2023-03-04 20:07:36 +0100

    Improvement considering suggestions

--- src/main/java/core/basesyntax/service/InvalidInputExcepton.java
+++ src/main/java/core/basesyntax/service/InvalidInputExcepton.java
@@ -1,12 +1,7 @@
 package core.basesyntax.service;
 
 public class InvalidInputExcepton extends RuntimeException {
-    public InvalidInputExcepton() {
-        super();
-    }
-
     public InvalidInputExcepton(String message) {
-        super();
-        System.out.println(message);
+        super(message);
     }
 }
