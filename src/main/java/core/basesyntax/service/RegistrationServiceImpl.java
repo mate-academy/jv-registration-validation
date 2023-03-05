97893e526f063477d8e05235cc50efd7d443dadb
Alona Klishchenko <alyonaklischenko@gmail.com>
2023-03-06 01:45:05 +0200

    created tests for RegistrationServiceImpl class

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,31 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null) {
+            throw new RuntimeException("Login can't be null");
+        }
+        if (user.getPassword() == null) {
+            throw new RuntimeException("Password can't be null");
+        }
+        if (user.getAge() == null) {
+            throw new RuntimeException("Age can't be null");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("User need to have age - 18 or more");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("User's password should to contain 6 symbols or more");
+        }
+        if (storageDao.get(user.getLogin()) != null && storageDao.get(user.getLogin())
+                .getLogin().equals(user.getLogin())) {
+            throw new RuntimeException("User with such login already exist");
+        }
+        return storageDao.add(user);
     }
 }
