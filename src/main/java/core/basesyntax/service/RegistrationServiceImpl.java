b135d65892fc8d17382e681091bfa4b0d4aefde9
Maksym <maxim.yashyn@gmail.com>
2023-03-05 23:09:14 +0200

    changes in exception message

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -12,7 +12,7 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user.getAge() <= 18) {
             throw new RegistrationFailedException("Your age must be over 18!");
         } else if (user.getPassword().length() <= 6) {
-            throw new RegistrationFailedException("Your password length must be more than 6 characters!");
+            throw new RegistrationFailedException("Your password must be more than 6 characters!");
         } else if (storageDao.get(user.getLogin()) != null) {
             throw new RegistrationFailedException("Your login already exist!");
         }
