d2278defa32d7979a9ca1c3ae56ef51bb04eec7b
kyrylh <kyryl.huliuk@gmail.com>
2023-03-05 23:08:23 +0200

    Added hw solution

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,45 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MAX_AGE = 90;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new InvalidDataException("User should be not null");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new InvalidDataException("User with same login exist");
+        }
+        if (user.getLogin() == null || user.getLogin().length() == 0) {
+            throw new InvalidDataException("Login should be not null");
+        }
+        if (user.getAge() == null) {
+            throw new InvalidDataException("Age must be not null");
+        }
+        if (user.getPassword() == null) {
+            throw new InvalidDataException("Password should be not null");
+        }
+        if (user.getAge() < MIN_AGE || user.getAge() > MAX_AGE) {
+            throw new InvalidDataException("Not valid age");
+        }
+        if (user.getPassword().length() < 6) {
+            throw new InvalidDataException("Password should be more than 6");
+        }
+        return storageDao.add(user);
     }
 }
+//        1. Аргумент нулл +
+//        2. Логін нулл +
+//        3. Age нулл +
+//        4. Пароль нулл +
+//        5. Кейс коли юзер з таким же логіном вже є в сторейдж +
+//        6. Логін пуста строка +
+//        7. Дефолтний кейс +
+//        8. Пароль меньше 6 символів +
+//        9. Age меньше 18 +
+//        10. Age == 18 +
+//        11. Age від'ємний +
+//        12. Age завеликий +
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,120 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static final int VALID_AGE = 20;
+    private static final int NOT_VALID_AGE = 15;
+    private static final int NEGATIVE_AGE = -1;
+    private static final int LARGE_AGE = 234534255;
+    private static final Integer NULL_AGE = null;
+    private static final String VALID_LOGIN = "Antony";
+    private static final String ANOTHER_VALID_LOGIN = "Vlad";
+    private static final String NOT_VALID_NAME = "";
+    private static final String VALID_PASSWORD = "654321";
+    private static final String NOT_VALID_PASSWORD = "111";
+
+    private RegistrationService registrationService = new RegistrationServiceImpl();
+    private User user = new User();
+
+    @BeforeEach
+    void setUp() {
+        registrationService = new RegistrationServiceImpl();
+        user = new User();
+    }
+
+    @Test
+    void register_defaultUser_NotOk() {
+        assertThrows(InvalidDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_LoginNull_NotOk() {
+        user.setLogin(null);
+        user.setPassword(VALID_PASSWORD);
+        user.setAge(VALID_AGE);
+        assertThrows(InvalidDataException.class,() -> registrationService.register(user));
+    }
+
+    @Test
+    void register_ageLessThanEighteen_NotOk() {
+        user.setLogin(VALID_LOGIN);
+        user.setPassword(VALID_PASSWORD);
+        user.setAge(NOT_VALID_AGE);
+        assertThrows(InvalidDataException.class,() -> registrationService.register(user));
+    }
+
+    @Test
+    void register_ageNull_NotOk() {
+        user.setLogin(VALID_LOGIN);
+        user.setPassword(VALID_PASSWORD);
+        user.setAge(NULL_AGE);
+        assertThrows(InvalidDataException.class,() -> registrationService.register(user));
+    }
+
+    @Test
+    void register_passwordNull_NotOk() {
+        user.setLogin(VALID_LOGIN);
+        user.setPassword(null);
+        user.setAge(VALID_AGE);
+        assertThrows(InvalidDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_notValidPassword_NotOk() {
+        user.setLogin(VALID_LOGIN);
+        user.setPassword(NOT_VALID_PASSWORD);
+        user.setAge(VALID_AGE);
+        assertThrows(InvalidDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_emptyLogin_NotOk() {
+        user.setLogin(NOT_VALID_NAME);
+        user.setPassword(null);
+        user.setAge(VALID_AGE);
+        assertThrows(InvalidDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_sameUserInStorage_NotOk() {
+        user.setLogin(VALID_LOGIN);
+        user.setPassword(VALID_PASSWORD);
+        user.setAge(VALID_AGE);
+        registrationService.register(user);
+    }
+
+    @Test
+    void register_nullArgument_NotOk() {
+        assertThrows(InvalidDataException.class, () -> registrationService.register(null));
+    }
+
+    @Test
+    void register_negativeAge_NotOk() {
+        user.setLogin(VALID_LOGIN);
+        user.setPassword(VALID_PASSWORD);
+        user.setAge(NEGATIVE_AGE);
+        assertThrows(InvalidDataException.class,() -> registrationService.register(user));
+    }
+
+    @Test
+    void register_veryLargeAge_NotOk() {
+        user.setLogin(VALID_LOGIN);
+        user.setPassword(VALID_PASSWORD);
+        user.setAge(LARGE_AGE);
+        assertThrows(InvalidDataException.class,() -> registrationService.register(user));
+    }
+
+    @Test
+    void register_eighteenAge_Ok() {
+        user.setLogin(ANOTHER_VALID_LOGIN);
+        user.setPassword(VALID_PASSWORD);
+        user.setAge(18);
+        registrationService.register(user);
+    }
+}
+
