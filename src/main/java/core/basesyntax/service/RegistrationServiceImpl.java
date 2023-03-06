3a9b1d04971a618fd66cfff5fca1c30f7cb86e6b
kyrylh <kyryl.huliuk@gmail.com>
2023-03-06 18:31:30 +0200

    refactored register method

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -18,7 +18,7 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (storageDao.get(user.getLogin()) != null) {
             throw new InvalidDataException("User with same login exist");
         }
-        if (user.getLogin() == null || user.getLogin().length() == 0) {
+        if (user.getLogin() == null || user.getLogin().isEmpty()) {
             throw new InvalidDataException("Login should be not null or not empty");
         }
         if (user.getAge() == null) {
@@ -33,7 +33,7 @@ public class RegistrationServiceImpl implements RegistrationService {
                     + " and less than "
                     + MAX_AGE);
         }
-        if (user.getPassword().length() < 6) {
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
             throw new InvalidDataException("Password should be more than " + MIN_PASSWORD_LENGTH);
         }
         return storageDao.add(user);
