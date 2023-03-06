7da4212d5c825b585f0fd0cb69f604ba1edf67a4
Kostia Vershynin <vershynin.kostia@gmail.com>
2023-03-07 00:48:47 +0200

    realization method register and tests

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,9 +1,10 @@
 package core.basesyntax.service;
 
-import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
