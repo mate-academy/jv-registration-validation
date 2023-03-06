47010c1fc5f6ea334c67abff88c4ac793a8a38a6
Alona Klishchenko <alyonaklischenko@gmail.com>
2023-03-06 02:04:25 +0200

    add exception class

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,5 +1,6 @@
 package core.basesyntax.service;
 
+import core.basesyntax.RegistrationException;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
@@ -12,23 +13,26 @@ public class RegistrationServiceImpl implements RegistrationService {
     @Override
     public User register(User user) {
         if (user.getLogin() == null) {
-            throw new RuntimeException("Login can't be null");
+            throw new RegistrationException("Login can't be null");
         }
         if (user.getPassword() == null) {
-            throw new RuntimeException("Password can't be null");
+            throw new RegistrationException("Password can't be null");
         }
         if (user.getAge() == null) {
-            throw new RuntimeException("Age can't be null");
+            throw new RegistrationException("Age can't be null");
         }
         if (user.getAge() < MIN_AGE) {
-            throw new RuntimeException("User need to have age - 18 or more");
+            throw new RegistrationException("User need to have age - 18 or more. The age -"
+                    + user.getAge() + "is invalid");
         }
         if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
-            throw new RuntimeException("User's password should to contain 6 symbols or more");
+            throw new RegistrationException("User's password" + user.getPassword()
+                    + "should to contain 6 symbols or more");
         }
         if (storageDao.get(user.getLogin()) != null && storageDao.get(user.getLogin())
                 .getLogin().equals(user.getLogin())) {
-            throw new RuntimeException("User with such login already exist");
+            throw new RegistrationException("User with such login" + user.getLogin()
+                    + "already exist");
         }
         return storageDao.add(user);
     }
