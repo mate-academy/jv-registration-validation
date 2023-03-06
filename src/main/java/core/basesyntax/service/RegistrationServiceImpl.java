d54b5466382aabf53d9c61d648eb90f94f371422
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-03-06 14:44:19 +0200

    Implemented solution for hw.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,49 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.RegistrationServiceException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    public static final int MINIMAL_AGE = 18;
+    public static final int MINIMAL_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        nullCheck(user);
+        minAgeCheck(user);
+        passwordLengthCheck(user);
+        loginCheck(user);
+        storageDao.add(user);
+        return user;
+    }
+
+    void nullCheck(User user) {
+        if (user == null) {
+            throw new RegistrationServiceException("Register field is null!");
+        }
+    }
+
+    void minAgeCheck(User user) {
+        if (user.getAge() == null || user.getAge() < MINIMAL_AGE) {
+            throw new RegistrationServiceException("Age must be more than 18!");
+        }
+    }
+
+    void passwordLengthCheck(User user) {
+        if (user.getPassword() == null || user.getPassword().length() < MINIMAL_PASSWORD_LENGTH) {
+            throw new RegistrationServiceException("Password length must be more than 6 symbols!");
+        }
+    }
+
+    void loginCheck(User user) {
+        if (user.getLogin() == null) {
+           throw new RegistrationServiceException("Login field is empty!");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+           throw new RegistrationServiceException("User with login " + user.getLogin()
+                    + " already exist");
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -0,0 +1,114 @@
+package core.basesyntax;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.RegistrationServiceException;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class RegistrationServiceImplTest {
+    public static final String DEFAULT_LOGIN = "SomeLoginName";
+    public static final String DEFAULT_PASSWORD = "Word123Pass";
+    public static final String LESS_THAN_6_PASSWORD = "rat";
+    public static final int DEFAULT_AGE = 75;
+    public static final int AGE_LESS_THAN_18 = 2;
+    public static final int AGE_IS_18 = 18;
+    private static RegistrationService registrationService;
+    private User testUser;
+
+    @BeforeAll
+    static void registrationServiceCreation() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        testUser = new User();
+        testUser.setLogin(DEFAULT_LOGIN);
+        testUser.setPassword(DEFAULT_PASSWORD);
+        testUser.setAge(DEFAULT_AGE);
+    }
+
+    @AfterEach
+    void afterEach() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void register_loginExist_notOk() {
+        User tempUser = new User();
+        tempUser.setLogin(DEFAULT_LOGIN);
+        tempUser.setPassword(DEFAULT_PASSWORD);
+        tempUser.setAge(AGE_IS_18);
+        registrationService.register(tempUser);
+        testUser.setLogin(DEFAULT_LOGIN);
+        assertThrows(RegistrationServiceException.class, () -> {
+            registrationService.register(testUser);
+        }, "RegistrationServiceException expected for login " + DEFAULT_LOGIN);
+    }
+
+    @Test
+    void register_nullLogin_notOk() {
+        testUser.setLogin(null);
+        assertThrows(RegistrationServiceException.class, () -> {
+            registrationService.register(testUser);
+        }, "RegistrationServiceException expected for null age");
+    }
+
+    @Test
+    void register_passwordLessSymbols_notOk() {
+        testUser.setPassword(LESS_THAN_6_PASSWORD);
+        assertThrows(RegistrationServiceException.class, () -> {
+            registrationService.register(testUser);
+        }, "RegistrationServiceException expected for password " + LESS_THAN_6_PASSWORD);
+    }
+
+    @Test
+    void register_nullPassword_notOk() {
+        testUser.setPassword(null);
+        assertThrows(RegistrationServiceException.class, () -> {
+            registrationService.register(testUser);
+        }, "RegistrationServiceException expected for null age");
+    }
+
+    @Test
+    void register_ageEquals18_Ok() {
+        testUser.setAge(AGE_IS_18);
+        registrationService.register(testUser);
+        assertEquals(AGE_IS_18, testUser.getAge(), "Age accepted");
+    }
+
+    @Test
+    void register_ageLessThan18_notOk() {
+        testUser.setAge(AGE_LESS_THAN_18);
+        assertThrows(RegistrationServiceException.class, () -> {
+            registrationService.register(testUser);
+        }, "RegistrationServiceException expected for age " + AGE_LESS_THAN_18);
+    }
+
+    @Test
+    void register_nullAge_notOk() {
+        testUser.setAge(null);
+        assertThrows(RegistrationServiceException.class, () -> {
+            registrationService.register(testUser);
+        }, "RegistrationServiceException expected for null age");
+    }
+
+    @Test
+    void register_correctData_Ok() {
+        registrationService.register(testUser);
+        assertEquals(DEFAULT_LOGIN, testUser.getLogin());
+        assertEquals(DEFAULT_PASSWORD, testUser.getPassword());
+        assertEquals(DEFAULT_AGE, testUser.getAge());
+        assertNotNull(testUser.getId());
+    }
+}
