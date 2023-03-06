f773ce1457582dedb36b15d99c260bcf9e007e76
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2023-03-06 15:04:58 +0200

    realized and added unitTest for task

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,14 +1,60 @@
 package core.basesyntax.service;
 
+import core.basesyntax.RegistrationValidationException;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MAX_PASSWORD_LENGTH = 26;
+    private static final int MIN_AGE = 18;
+    private static final int MAX_AGE = 90;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RegistrationValidationException("User field can't be empty!");
+        }
+        checkLogin(user.getLogin());
+        checkPassword(user.getPassword());
+        checkAge(user.getAge());
+        return storageDao.add(user);
+    }
+
+    private void checkLogin(String login) {
+        if (login == null) {
+            throw new RegistrationValidationException("Login field is empty");
+        }
+        if (storageDao.get(login) != null) {
+            throw new RegistrationValidationException("This login " + login + " already exists");
+        }
+    }
+
+    private void checkPassword(String password) {
+        if (password == null) {
+            throw new RegistrationValidationException("Password field is empty");
+        }
+        if (password.length() < MIN_PASSWORD_LENGTH) {
+            throw new RegistrationValidationException(
+                    "Password must be at least " + MIN_PASSWORD_LENGTH + " characters");
+        }
+        if (password.length() > MAX_PASSWORD_LENGTH) {
+            throw new RegistrationValidationException(
+                    "Password can't be more " + MIN_PASSWORD_LENGTH + " characters");
+        }
+    }
+
+    private void checkAge(Integer age) {
+        if (age == null) {
+            throw new RegistrationValidationException("Age field is empty");
+        }
+        if (age < MIN_AGE) {
+            throw new RegistrationValidationException("Your age is less than allowable");
+        }
+        if (age > MAX_AGE) {
+            throw new RegistrationValidationException("Your age is over than maximum allowable");
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,127 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.RegistrationValidationException;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static final String EXCEPTION_MESSAGE
+            = RegistrationValidationException.class.toString();
+    private static final String DEFAULT_LOGIN = "abc";
+    private static final String MIN_LIMIT_OF_CHARACTERS_IN_PASSWORD = "abcdef";
+    private static final String MAX_LIMIT_OF_CHARACTERS_IN__PASSWORD = "abcdefghijklmnopqrstuvwxyz";
+    private static final Integer MIN_AGE = 18;
+    private static final Integer MAX_AGE = 90;
+    private static RegistrationService registrationService;
+
+    private User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setLogin(DEFAULT_LOGIN);
+        user.setPassword(MIN_LIMIT_OF_CHARACTERS_IN_PASSWORD);
+        user.setPassword(MAX_LIMIT_OF_CHARACTERS_IN__PASSWORD);
+        user.setAge(MIN_AGE);
+        user.setAge(MAX_AGE);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void register_nullUser_notOk() {
+        assertThrows(RegistrationValidationException.class,
+                () -> registrationService.register(null),
+                String.format("%s should be thrown for user is null", EXCEPTION_MESSAGE));
+    }
+
+    @Test
+    void register_nullLogin_notOk() {
+        user.setLogin(null);
+        assertThrows(RegistrationValidationException.class,
+                () -> registrationService.register(user),
+                String.format("%s should be thrown for login is null", EXCEPTION_MESSAGE));
+    }
+
+    @Test
+    void register_existingLogin_notOk() {
+        registrationService.register(user);
+        assertThrows(RegistrationValidationException.class,
+                () -> registrationService.register(user),
+                String.format("%s should be thrown for already existing login: "
+                        + DEFAULT_LOGIN, EXCEPTION_MESSAGE));
+    }
+
+    @Test
+    void register_nullPassword_notOk() {
+        user.setPassword(null);
+        assertThrows(RegistrationValidationException.class,
+                () -> registrationService.register(user),
+                String.format("%s should be thrown for password is null", EXCEPTION_MESSAGE));
+    }
+
+    @Test
+    void register_invalidMinPasswordLength_notOk() {
+        user.setPassword("abcd");
+        assertThrows(RegistrationValidationException.class,
+                () -> registrationService.register(user),
+                String.format("%s should be thrown for invalid password: "
+                        + user.getPassword(), EXCEPTION_MESSAGE));
+    }
+
+    @Test
+    void register_invalidMaxPasswordLength_notOk() {
+        user.setPassword("abcdefghijklmnopqrstuvwxyzabc");
+        assertThrows(RegistrationValidationException.class,
+                () -> registrationService.register(user),
+                String.format("%s should be thrown for invalid password: "
+                        + user.getPassword(), EXCEPTION_MESSAGE));
+    }
+
+    @Test
+    void register_nullAge_notOk() {
+        user.setAge(null);
+        assertThrows(RegistrationValidationException.class,
+                () -> registrationService.register(user),
+                String.format("%s should be thrown for age is null", EXCEPTION_MESSAGE));
+    }
+
+    @Test
+    void register_invalidMinAge_notOk() {
+        user.setAge(9);
+        assertThrows(RegistrationValidationException.class,
+                () -> registrationService.register(user),
+                String.format("%s should be thrown for a invalid age: "
+                        + user.getAge(), EXCEPTION_MESSAGE));
+    }
+
+    @Test
+    void register_invalidMaxAge_notOK() {
+        user.setAge(91);
+        assertThrows(RegistrationValidationException.class,
+                () -> registrationService.register(user),
+                String.format("%s should be thrown for a invalid age: "
+                + user.getAge(), EXCEPTION_MESSAGE));
+    }
+
+    @Test
+    void register_validUser_Ok() {
+        assertEquals(user, registrationService.register(user),
+                "User must be registered");
+    }
+}
