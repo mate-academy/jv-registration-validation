950f2a487c804d0f4e74db909c21b2a75a9ae35f
Nikolaichuk Artem <tompsoq@gmail.com>
2023-03-06 18:14:11 +0200

    implemented RegistrationServiceImplTest class created tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,14 +1,46 @@
 package core.basesyntax.service;
 
+import core.basesyntax.InvalidDataException;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_ALLOWED_AGE = 18;
+    private static final int MAX_ALLOWED_AGE = 125;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new InvalidDataException("User is null");
+        }
+        if (user.getLogin() == null) {
+            throw new InvalidDataException("Login can`t be null");
+        }
+        if (user.getLogin().isEmpty()) {
+            throw new InvalidDataException("Login can`t be empty");
+        }
+        if (user.getAge() == null) {
+            throw new InvalidDataException("Age can`t be null");
+        }
+        if (user.getAge() < MIN_ALLOWED_AGE) {
+            throw new InvalidDataException("Age is less than " + "MIN_ALLOWED_AGE");
+        }
+        if (user.getAge() > MAX_ALLOWED_AGE) {
+            throw new InvalidDataException("Age is bigeer than " + "MAX_ALLOWED_AGE");
+        }
+        if (user.getPassword() == null) {
+            throw new InvalidDataException("Password can`t be null");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new InvalidDataException("Your password is shorter than "
+                    + "MIN_PASSWORD_LENGTH");
+        }
+        if (user.getPassword().isEmpty()) {
+            throw new InvalidDataException("Password can`t be empty");
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,110 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.InvalidDataException;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static final String EXAMPLE_OF_LOGIN = "Thompson";
+    private static final int EXAMPLE_OF_AGE = 23;
+    private static final int MIN_ALLOWED_AGE = 18;
+
+    private static final String EXAMPLE_OF_PASSWORD = "123456artem";
+    private static final String EXAMPLE_OF_INVALID_PASS = "adqd";
+    private static final int MAX_ALLOWED_AGE = 125;
+    private static final String EMPTY_LINE = "";
+    private static final int NEGATIVE_AGE = -33;
+    private static RegistrationService registrationService;
+    private User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setAge(EXAMPLE_OF_AGE);
+        user.setLogin(EXAMPLE_OF_LOGIN);
+        user.setPassword(EXAMPLE_OF_PASSWORD);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void register_LoginIsNull_notOk() {
+        user.setLogin(null);
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_AgeIsNull_NotOk() {
+        user.setAge(null);
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_AgeIsLessThanAllowed_NotOk() {
+        user.setAge(MIN_ALLOWED_AGE - 1);
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_negativeAgeUser_notOk() {
+
+        user.setAge(NEGATIVE_AGE);
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_AgeIsBiggerThanAllowed_NotOk() {
+        user.setAge(MAX_ALLOWED_AGE + 1);
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_PassNull_NotOk() {
+        user.setPassword(null);
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_EmptyPassword_NotOk() {
+        user.setPassword(EMPTY_LINE);
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+
+    void register_PassIsLessThanAllowed_NotOk() {
+        user.setPassword(EXAMPLE_OF_INVALID_PASS);
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(user);
+        });
+
+    }
+}
