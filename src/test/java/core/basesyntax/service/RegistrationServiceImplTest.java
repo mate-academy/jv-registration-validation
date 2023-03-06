445e2a5d086e70afb325625ad4ac0c899df0674c
Maksym <maxim.yashyn@gmail.com>
2023-03-06 13:37:09 +0200

    added more tests and fixed mistakes

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -3,92 +3,163 @@ package core.basesyntax.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    private static User userOne = new User();
-    private static User userTwo = new User();
-    private static User userThree = new User();
-    private static User userFour = new User();
-    private static User userFive = new User();
-    private RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
+    private static RegistrationServiceImpl registrationService;
+    private static User user;
+    private static final String LOGIN = "qwerty";
+    private static final String PASSWORD = "qwerty12";
+    private static final String EMPTY_ENTRY = "";
+    private static final int AGE = 18;
 
     @BeforeAll
     static void beforeAll() {
-        userOne.setLogin("Sakura");
-        userOne.setPassword("12345678");
-        userOne.setAge(16);
+        registrationService = new RegistrationServiceImpl();
+    }
 
-        userTwo.setLogin("CherryTree");
-        userTwo.setPassword("12345");
-        userTwo.setAge(19);
+    @BeforeEach
+    void setUp() {
+        user = new User(LOGIN, PASSWORD, AGE);
+    }
 
-        userThree.setLogin("Sakura");
-        userThree.setPassword("123456");
-        userThree.setAge(18);
-
-        userFour.setLogin("ILoveCherry");
-        userFour.setPassword("ireallylovecherry");
-        userFour.setAge(21);
-
-        userFive.setLogin(null);
-        userFive.setPassword(null);
-        userFive.setAge(null);
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
     }
 
     @Test
-    void userAge_Ok() {
-        try {
-            assertEquals(userFour, registrationService.register(userFour));
-        } catch (RegistrationFailedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    void userAge_NotOk() {
+    void register_nullLogin_notOk() {
+        user.setLogin(null);
         assertThrows(RegistrationFailedException.class, () -> {
-            registrationService.register(userOne);
-        }, "Your age must be over 18!");
+            registrationService.register(user);
+        }, " expected " + RegistrationFailedException.class.getName()
+                + " to be thrown for null login "
+                + user.getLogin()
+                + " but it wasn't");
+        user.setLogin(EMPTY_ENTRY);
         assertThrows(RegistrationFailedException.class, () -> {
-            registrationService.register(userFive);
-        }, "Your age must be over 18!");
+            registrationService.register(user);
+        });
     }
 
     @Test
-    void userLogin_Ok() {
-        try {
-            assertEquals(userFour, registrationService.register(userFour));
-        } catch (RegistrationFailedException e) {
-            throw new RuntimeException(e);
-        }
+    void register_existLogin_notOk() {
+        User anotherUser = new User(LOGIN, PASSWORD, AGE);
+        Storage.people.add(user);
+        assertThrows(RegistrationFailedException.class, () -> {
+            registrationService.register(anotherUser);
+        }, " expected " + RegistrationFailedException.class.getName()
+                + " to be thrown for exist login "
+                + user.getLogin()
+                + " but it wasn't");
     }
 
     @Test
-    void userLogin_NotOk() {
+    void register_lessLengthLogin_notOk() {
+        user.setLogin("qwer");
         assertThrows(RegistrationFailedException.class, () -> {
-            registrationService.register(userThree);
-        }, "Your login already exist!");
-        assertThrows(RegistrationFailedException.class, () -> {
-            registrationService.register(userFive);
-        }, "You wrote an empty login!");
+            registrationService.register(user);
+        }, " expected " + RegistrationFailedException.class.getName()
+                + " to be thrown for less length login "
+                + user.getLogin()
+                + " but it wasn't");
     }
 
     @Test
-    void userPassword_Ok() {
-        try {
-            assertEquals(userFour, registrationService.register(userFour));
-        } catch (RegistrationFailedException e) {
-            throw new RuntimeException(e);
-        }
+    void register_overLengthLogin_notOk() {
+        user.setLogin("qwerty12345");
+        assertThrows(RegistrationFailedException.class, () -> {
+            registrationService.register(user);
+        }, " expected " + RegistrationFailedException.class.getName()
+                + " to be thrown for over length login "
+                + user.getLogin()
+                + " but it wasn't");
     }
 
     @Test
-    void userPassword_NotOk() {
+    void register_nullPassword_notOk() {
+        user.setPassword(null);
         assertThrows(RegistrationFailedException.class, () -> {
-            registrationService.register(userTwo);
-        }, "Your password must be at least 6 characters!");
+            registrationService.register(user);
+        }, " expected " + RegistrationFailedException.class.getName()
+                + " to be thrown for null password "
+                + user.getPassword()
+                + " but it wasn't");
+        user.setPassword(EMPTY_ENTRY);
+        assertThrows(RegistrationFailedException.class, () -> {
+            registrationService.register(user);
+        }, " expected " + RegistrationFailedException.class.getName()
+                + " to be thrown for empty entry password "
+                + user.getPassword()
+                + " but it wasn't");
     }
+
+    @Test
+    void register_lessLengthPassword_notOk() {
+        user.setPassword("qwer");
+        assertThrows(RegistrationFailedException.class, () -> {
+            registrationService.register(user);
+        }, " expected " + RegistrationFailedException.class.getName()
+                + " to be thrown for less length password "
+                + user.getPassword()
+                + " but it wasn't");
+    }
+
+    @Test
+    void register_overLengthPassword_notOk() {
+        user.setPassword("qwerty123456789012345678901234567890");
+        assertThrows(RegistrationFailedException.class, () -> {
+            registrationService.register(user);
+        }, " expected " + RegistrationFailedException.class.getName()
+                + " to be thrown for over password length "
+                + user.getPassword()
+                + " but it wasn't");
+    }
+
+    @Test
+    void register_nullAge_notOk() {
+        user.setAge(null);
+        assertThrows(RegistrationFailedException.class, () -> {
+            registrationService.register(user);
+        }, " expected " + RegistrationFailedException.class.getName()
+                + " to be thrown for null age "
+                + user.getAge()
+                + " but it wasn't");
+    }
+
+    @Test
+    void register_lessAge_notOk() {
+        user.setAge(17);
+        assertThrows(RegistrationFailedException.class, () -> {
+            registrationService.register(user);
+        }, " expected " + RegistrationFailedException.class.getName()
+                + " to be thrown for less age "
+                + user.getAge()
+                + " but it wasn't");
+    }
+
+    @Test
+    void register_overAge_notOk() {
+        user.setAge(101);
+        assertThrows(RegistrationFailedException.class, () -> {
+            registrationService.register(user);
+        }, " expected " + RegistrationFailedException.class.getName()
+                + " to be thrown for over age "
+                + user.getAge()
+                + " but it wasn't");
+    }
+
+    @Test
+    void register_successful_ok() {
+        registrationService.register(user);
+        Storage.people.add(user);
+        assertEquals(Storage.people.get(0).getId(), user.getId());
+    }
+
 }
