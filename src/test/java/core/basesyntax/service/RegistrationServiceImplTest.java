18221587ff14f97632f269b6091fca7f1b3067fa
Artem Ponomarenko <artemponomar98@gmail.com>
2023-03-06 15:37:03 +0200

    Improved code coverage to 100%

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -8,19 +8,26 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
+    private static final int AGE_BARELY_OK = 18;
+    private static final int AGE_OK = 25;
+    private static final int AGE_NEGATIVE = -10;
+    private static final int AGE_NOT_OK = 13;
+    private static final String SHORT_PASSWORD = "123";
     private static StorageDao storageDao;
     private static User validUser1;
     private static User validUser2;
     private static User validUser3;
+    private static User validUser4;
     private static RegistrationServiceImpl registrationServiceImpl;
 
     @BeforeAll
     static void setUp() {
         storageDao = new StorageDaoImpl();
         registrationServiceImpl = new RegistrationServiceImpl();
-        validUser1 = new User("login1", "1234567", 25);
+        validUser1 = new User("login1", "1234567", AGE_BARELY_OK);
         validUser2 = new User("login2", "12345678", 26);
         validUser3 = new User("login3", "123456789", 27);
+        validUser4 = new User("login4", "1234567890", 28);
     }
 
     @Test
@@ -32,15 +39,31 @@ class RegistrationServiceImplTest {
     @Test
     void registerAge_NotOk() {
         Assertions.assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(
-                new User(validUser3.getLogin(), validUser3.getPassword(), -10)));
+                new User(validUser3.getLogin(), validUser3.getPassword(), AGE_NEGATIVE)));
         Assertions.assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(
-                new User(validUser3.getLogin(), validUser3.getPassword(), 13)));
+                new User(validUser3.getLogin(), validUser3.getPassword(), AGE_NOT_OK)));
+    }
+
+    @Test
+    void registerAge_Ok() {
+        Assertions.assertEquals(registrationServiceImpl.register(
+                        new User(validUser2.getLogin(), validUser2.getPassword(), AGE_BARELY_OK)),
+                storageDao.get(validUser2.getLogin()));
+        Assertions.assertEquals(registrationServiceImpl.register(
+                        new User(validUser3.getLogin(), validUser3.getPassword(), AGE_OK)),
+                storageDao.get(validUser3.getLogin()));
     }
 
     @Test
     void registerPasswordTooShort_NotOk() {
         Assertions.assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(
-                new User(validUser1.getLogin(), "123", validUser1.getAge())));
+                new User(validUser1.getLogin(), SHORT_PASSWORD, validUser1.getAge())));
+    }
+
+    @Test
+    void registerNullUser_NotOk() {
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationServiceImpl.register(null));
     }
 
     @Test
@@ -63,8 +86,8 @@ class RegistrationServiceImplTest {
 
     @Test
     void registerExistingUser_NotOk() {
-        registrationServiceImpl.register(validUser2);
+        registrationServiceImpl.register(validUser4);
         Assertions.assertThrows(RegistrationException.class,
-                () -> registrationServiceImpl.register(validUser2));
+                () -> registrationServiceImpl.register(validUser4));
     }
 }
