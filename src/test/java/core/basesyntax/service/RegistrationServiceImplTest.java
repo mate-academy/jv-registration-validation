e10e752aef34e25760d40e579daca2014db83fd1
IhorPetrytskyi333 <mygit333@gmail.com>
2023-03-06 20:08:56 -0800

    solution_15

---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,79 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.exception.InvalidDataException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+    private static final String LOGIN = "login";
+    private static final String PASSWORD = "password";
+    private static final int AGE = 30;
+
+    private User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setLogin(LOGIN);
+        user.setPassword(PASSWORD);
+        user.setAge(AGE);
+    }
+
+    @Test
+    void register_NullForLogin_NotOk() {
+        user.setLogin(null);
+        assertThrows(NullPointerException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_NullForAge_Not_Ok() {
+        user.setAge(null);
+        assertThrows(NullPointerException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_NulForPassword_Not_Ok() {
+        user.setPassword(null);
+        assertThrows(NullPointerException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_EmptyLineForLogin_NotOk() {
+        user.setLogin("");
+        assertThrows(InvalidDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_PasswordLengthLimit_Not_Ok() {
+        user.setPassword("not");
+        assertThrows(InvalidDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_AgeLimit_Not_Ok() {
+        user.setAge(3);
+        assertThrows(InvalidDataException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_SameUserLogin_NotOK() {
+        assertEquals(user, registrationService.register(user));
+    }
+
+    @Test
+    void register_User_ValidData_Ok() {
+        assertSame(user, registrationService.register(user));
+    }
+}
