6652ae988b6c731177f00ca64ed2fa1f724d8d03
Artem Zaiarnyi <artem_zaiarnyi@epam.com>
2023-03-15 12:57:20 +0200

    Test hw

---
+++ src/main/java/core/basesyntax/exception/InvalidInputDataException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exception;
+
+public class InvalidInputDataException extends RuntimeException{
+    public InvalidInputDataException (String message) {
+        super(message);
+    }
+}
