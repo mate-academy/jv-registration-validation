7c7c10c1b2f7f0e46176cd7b7ee43b7b6b582b2e
Artem Zaiarnyi <artem_zaiarnyi@epam.com>
2023-03-18 11:51:03 +0200

    Test hw

--- src/main/java/core/basesyntax/exception/InvalidInputDataException.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax.exception;
-
-public class RegistrationException extends RuntimeException {
-    public InvalidInputDataException(String message) {
-        super(message);
-    }
-}
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -3,89 +3,78 @@ package core.basesyntax.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    private static final String login = "Spider_Man";
-    private static final String password = "qwerty123";
-    private static final int age = 21;
-    private static RegistrationService registrationService;
-    private User user;
-
-    @BeforeAll
-    static void beforeAll() {
-        registrationService = new RegistrationServiceImpl();
-    }
+    private RegistrationServiceImpl service;
+    private StorageDaoImpl storageDao;
+    private User userDefault;
+    private User userFailLogin;
+    private User userFailPassword;
+    private User userFailAge;
 
     @BeforeEach
     void setUp() {
-        user = new User();
-        user.setLogin(login);
-        user.setPassword(password);
-        user.setAge(age);
-    }
+        service = new RegistrationServiceImpl();
+        storageDao = new StorageDaoImpl();
 
-    @Test
-    void register_newUser_ok() {
-        User actual = registrationService.register(user);
-        assertEquals(user, actual);
-    }
-
-    @Test
-    void register_UserNull_notOk() {
-        assertThrows(RuntimeException.class, () -> registrationService.register(null));
-    }
-
-    @Test
-    void register_userLoginNull_notOk() {
-        user.setLogin(null);
-        assertThrows(RuntimeException.class, () -> registrationService.register(user));
-    }
-
-    @Test
-    void register_userPassword_notOk() {
-        user.setPassword("1234");
-        assertThrows(RuntimeException.class, () -> registrationService.register(user));
-    }
-
-    @Test
-    void register_userPasswordNull_notOk() {
-        user.setPassword(null);
-        assertThrows(RuntimeException.class, () -> registrationService.register(user));
-    }
-
-    @Test
-    void register_userAgeNull_notOk() {
-        user.setAge(null);
-        assertThrows(RuntimeException.class, () -> registrationService.register(user));
-    }
-
-    @Test
-    void register_userYoungAge_notOk() {
-        user.setAge(14);
-        assertThrows(RuntimeException.class, () -> registrationService.register(user));
-    }
-
-    @Test
-    void register_userNegativeAge_notOk() {
-        user.setAge(-2);
-        assertThrows(RuntimeException.class, () -> registrationService.register(user));
-    }
-
-    @Test
-    void register_userAgeOver18_ok() {
-        user.setAge(40);
-        User actual = registrationService.register(user);
-        assertEquals(user, actual);
+        userDefault = createUser("Denys","12345678",23);
+        userFailLogin = createUser("Ma","asdasdd34",21);
+        userFailPassword = createUser("Mak","45",21);
+        userFailAge = createUser("Mak","asdqwert6",17);
     }
 
     @AfterEach
-    void tearDown() {
+    void clearStorage() {
         Storage.people.clear();
+    }
+
+    @Test
+    void register_NullUser_notOk() {
+        assertThrows(RuntimeException.class, () -> {
+            service.register(null);
+        });
+    }
+
+    @Test
+    void register_AddUser_Ok() {
+        service.register(userDefault);
+        int actual = Storage.people.size();
+        assertEquals(5, actual);
+    }
+
+    @Test
+    void register_ChecksLogin_notOk() {
+        assertThrows(RuntimeException.class, () -> {
+            service.register(userFailLogin);
+        });
+    }
+
+    @Test
+    void register_ChecksPassword_notOk() {
+        assertThrows(RuntimeException.class, () -> {
+            service.register(userFailPassword);
+        });
+    }
+
+    @Test
+    void register_ChecksAge_notOk() {
+        assertThrows(RuntimeException.class, () -> {
+            service.register(userFailAge);
+        });
+    }
+
+    private User createUser(String login, String password, int age) {
+        User user = new User();
+        user.setLogin(login);
+        user.setPassword(password);
+        user.setAge(age);
+        storageDao.add(user);
+        return user;
     }
 }
