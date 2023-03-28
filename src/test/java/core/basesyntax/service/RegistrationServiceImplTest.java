870dc0e7b08fdb0a4f7babc873fbcb0bcdc1eeb0
Bohdan Shevtsov <shevtsov.b@gmail.com>
2023-03-28 17:49:21 +0300

    new user created in each test

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -6,77 +6,46 @@ import static org.junit.jupiter.api.Assertions.fail;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
-import java.util.HashMap;
-import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
 
-    private RegistrationService registrationService = new RegistrationServiceImpl();
-    private StorageDao storageDao = new StorageDaoImpl();
-    private Map<String,User> users;
+    private static RegistrationService registrationService;
+    private static StorageDao storageDao;
 
-    @BeforeEach
-    void setUp() {
-        users = new HashMap<>();
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        storageDao = new StorageDaoImpl();
+    }
+
+    @Test
+    void registerUser_Ok() throws AuthenticationException {
         User testUserOk = new User(); //testUserOk
         testUserOk.setId(12345L);
         testUserOk.setAge(18);
         testUserOk.setLogin("testUserOk");
         testUserOk.setPassword("123456");
-        users.put("testUserOk",testUserOk);
+        registrationService.register(testUserOk);
+        assertNotNull(storageDao.get(testUserOk.getLogin()));
+    }
+
+    @Test
+    void userAlreadyExist_NotOk() throws AuthenticationException {
+        User testUserOk = new User(); //testUserOk
+        testUserOk.setId(12345L);
+        testUserOk.setAge(18);
+        testUserOk.setLogin("testUserOk");
+        testUserOk.setPassword("123456");
         User testUserNamesake = new User(); //testUserNamesake
         testUserNamesake.setId(12347L);
         testUserNamesake.setAge(22);
         testUserNamesake.setLogin("testUserOk");
         testUserNamesake.setPassword("1234568");
-        users.put("testUserNamesake", testUserNamesake);
-        User testUserJuvenile = new User(); //testUserJuvenile
-        testUserJuvenile.setId(12347L);
-        testUserJuvenile.setAge(16);
-        testUserJuvenile.setLogin("userJuvenile");
-        testUserJuvenile.setPassword("1234568");
-        users.put("testUserJuvenile", testUserJuvenile);
-        User testUserShortPassword = new User(); //testUserShortPassword
-        testUserShortPassword.setId(12348L);
-        testUserShortPassword.setAge(20);
-        testUserShortPassword.setLogin("userShortPassword");
-        testUserShortPassword.setPassword("12345");
-        users.put("testUserShortPassword", testUserShortPassword);
-        User testUserNoId = new User(); //testUserNoId
-        testUserNoId.setAge(20);
-        testUserNoId.setLogin("userNoId");
-        testUserNoId.setPassword("123456");
-        users.put("testUserNoId", testUserNoId);
-        User testUserNoAge = new User(); //testUserNoAge
-        testUserNoAge.setId(12348L);
-        testUserNoAge.setLogin("userNoAge");
-        testUserNoAge.setPassword("123456");
-        users.put("testUserNoAge", testUserNoAge);
-        User testUserNoLogin = new User(); //testUserNoLogin
-        testUserNoLogin.setId(12348L);
-        testUserNoLogin.setAge(20);
-        testUserNoLogin.setPassword("123456");
-        users.put("testUserNoLogin", testUserNoLogin);
-        User testUserNoPassword = new User(); //testUserNoPassword
-        testUserNoPassword.setId(12348L);
-        testUserNoPassword.setLogin("userNoPassword");
-        testUserNoPassword.setAge(20);
-        users.put("testUserNoPassword", testUserNoPassword);
-    }
-
-    @Test
-    void registerUser_Ok() throws AuthenticationException {
-        registrationService.register(users.get("testUserOk"));
-        assertNotNull(storageDao.get(users.get("testUserOk").getLogin()));
-    }
-
-    @Test
-    void userAlreadyExist_NotOk() throws AuthenticationException {
         try {
-            registrationService.register(users.get("testUserOk"));
-            registrationService.register(users.get("testUserNamesake"));
+            registrationService.register(testUserOk);
+            registrationService.register(testUserNamesake);
         } catch (AuthenticationException e) {
             return;
         }
@@ -84,9 +53,14 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void userYoungerThanLimit_NotOk() {
+    void userAgeLessThanLimit_NotOk() {
+        User testUserJuvenile = new User(); //testUserJuvenile
+        testUserJuvenile.setId(12347L);
+        testUserJuvenile.setAge(16);
+        testUserJuvenile.setLogin("userJuvenile");
+        testUserJuvenile.setPassword("1234568");
         try {
-            registrationService.register(users.get("testUserJuvenile"));
+            registrationService.register(testUserJuvenile);
         } catch (AuthenticationException e) {
             return;
         }
@@ -95,8 +69,13 @@ class RegistrationServiceImplTest {
 
     @Test
     void passwordLessThanLimit_NotOk() {
+        User testUserShortPassword = new User(); //testUserShortPassword
+        testUserShortPassword.setId(12348L);
+        testUserShortPassword.setAge(20);
+        testUserShortPassword.setLogin("userShortPassword");
+        testUserShortPassword.setPassword("12345");
         try {
-            registrationService.register(users.get("testUserShortPassword"));
+            registrationService.register(testUserShortPassword);
         } catch (AuthenticationException e) {
             return;
         }
@@ -104,30 +83,60 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void registerNull_NotOk() {
+    void registerNoId_NotOk() {
+        User testUserNoId = new User(); //testUserNoId
+        testUserNoId.setAge(20);
+        testUserNoId.setLogin("userNoId");
+        testUserNoId.setPassword("123456");
         try {
-            registrationService.register(users.get("testUserNoId"));
+            registrationService.register(testUserNoId);
         } catch (AuthenticationException e) {
             return;
         }
         fail("AuthenticationException should be thrown because ID can't be null");
+    }
+
+    @Test
+    void registerNoAge_NotOk() {
+        User testUserNoAge = new User(); //testUserNoAge
+        testUserNoAge.setId(12348L);
+        testUserNoAge.setLogin("userNoAge");
+        testUserNoAge.setPassword("123456");
         try {
-            registrationService.register(users.get("testUserNoAge"));
+            registrationService.register(testUserNoAge);
         } catch (AuthenticationException e) {
             return;
         }
         fail("AuthenticationException should be thrown because Age can't be null");
+    }
+
+    @Test
+    void registerNoLogin_NotOk() {
+        User testUserNoLogin = new User(); //testUserNoLogin
+        testUserNoLogin.setId(12348L);
+        testUserNoLogin.setAge(20);
+        testUserNoLogin.setPassword("123456");
         try {
-            registrationService.register(users.get("testUserNoLogin"));
-        } catch (AuthenticationException e) {
-            return;
-        }
-        fail("AuthenticationException should be thrown because Login can't be null");
-        try {
-            registrationService.register(users.get("testUserNoPassword"));
+            registrationService.register(testUserNoLogin);
         } catch (AuthenticationException e) {
             return;
         }
         fail("AuthenticationException should be thrown because Login can't be null");
     }
+
+    @Test
+    void registerNoPassword_NotOk() {
+        User testUserNoPassword = new User(); //testUserNoPassword
+        testUserNoPassword.setId(12348L);
+        testUserNoPassword.setLogin("userNoPassword");
+        testUserNoPassword.setAge(20);
+        try {
+            registrationService.register(testUserNoPassword);
+        } catch (AuthenticationException e) {
+            return;
+        }
+        fail("AuthenticationException should be thrown because Password can't be null");
+    }
 }
+
+
