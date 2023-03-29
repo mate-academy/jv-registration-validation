5fbfd0663d0459db3b8ac2d7410c1d59f07c8587
antonsablev <live.incntr@gmail.com>
2023-03-29 11:59:22 +0200

    Create my own tests and implement registration service class.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,14 +1,31 @@
 package core.basesyntax.service;
 
+import core.basesyntax.RegistrationException;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int USER_MIN_AGE = 18;
+    private static final int PASSWORD_MIN_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getAge() < USER_MIN_AGE) {
+            throw new RegistrationException("User age should be "
+                    + USER_MIN_AGE + " or more!");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("User with this username is already exist");
+        }
+        if (user.getPassword() == null || user.getPassword().length() < PASSWORD_MIN_LENGTH) {
+            throw new RegistrationException("Password is too short. Minimum length is "
+                    + PASSWORD_MIN_LENGTH);
+        }
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Login can't be null");
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,101 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.RegistrationException;
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static final String DEFAULT_USER_LOGIN = "tonyPix";
+    private static final int DEFAULT_USER_AGE = 25;
+    private static final int USER_AGE_EIGHTEEN = 18;
+    private static final int LOW_USER_AGE = 15;
+    private static final String DEFAULT_USER_PASS = "qwerty12345";
+    private static final String SHORT_PASSWORD = "pass";
+    private static RegistrationServiceImpl regService;
+    private final StorageDao storageDao = new StorageDaoImpl();
+    private User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        regService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setAge(DEFAULT_USER_AGE);
+        user.setLogin(DEFAULT_USER_LOGIN);
+        user.setPassword(DEFAULT_USER_PASS);
+    }
+
+    @Test
+    void userNotAdult_NotOk() {
+        user.setAge(LOW_USER_AGE);
+        assertThrows(RegistrationException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void userIsEighteen_Ok() {
+        user.setAge(USER_AGE_EIGHTEEN);
+        regService.register(user);
+        User actual = storageDao.get(user.getLogin());
+        assertEquals(actual, user);
+    }
+
+    @Test
+    void userIsAdult_Ok() {
+        regService.register(user);
+        User actual = storageDao.get(user.getLogin());
+        assertEquals(user, actual);
+
+    }
+
+    @Test
+    void passwordIsNull_NotOk() {
+        user.setPassword(null);
+        assertThrows(RegistrationException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void userLoginNull_NotOk() {
+        user.setLogin(null);
+        assertThrows(RegistrationException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void userWithThisUserNameIsExist_NotOk() {
+        regService.register(user);
+        assertThrows(RegistrationException.class, () -> {
+            regService.register(user);
+        });
+    }
+
+    @Test
+    void shortPass_NotOk() {
+        user.setPassword(SHORT_PASSWORD);
+        assertThrows(RegistrationException.class, () -> {
+            regService.register(user);
+        });
+
+    }
+
+    @AfterEach
+    void clearStorage() {
+        Storage.people.clear();
+    }
+}
