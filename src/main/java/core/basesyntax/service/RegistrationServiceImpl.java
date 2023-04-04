6e66cf77f864ddd6ac8ef90aea02d077527f950c
Andrei_Bondarenko <bondorol007@gmail.com>
2023-04-04 23:25:48 +0300

    Task is registration-validation. I did 6 test methods, but i don't know, how should i make the comparison so that the age is more than 18.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,5 +1,6 @@
 package core.basesyntax.service;
 
+import core.basesyntax.RegistrationUserException;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
@@ -7,8 +8,16 @@ import core.basesyntax.model.User;
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
 
+
     @Override
     public User register(User user) {
-        return null;
+        if (user.getPassword().length() < 6) {
+            throw new RegistrationUserException("Can't registration user, because password is less than 6 letters");
+        } else if (user.getAge() < 18) {
+            throw new RegistrationUserException("Can't registration user, because user isn't 18 years ago");
+        } else if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationUserException("Can't registration user, because this login already uses in storage");
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,66 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private RegistrationService registrationService = new RegistrationServiceImpl();
+
+    @Test
+    void userNotNull_Ok() {
+        User userTest = new User();
+        userTest.setId(31654L);
+        userTest.setLogin("Bond-1234");
+        userTest.setPassword("1234567");
+        userTest.setAge(43);
+        User register = registrationService.register(userTest);
+        assertNotNull(register.getLogin());
+    }
+
+    @Test
+    void PasswordNotNull_Ok() {
+        User userTest = new User();
+        userTest.setId(4135141L);
+        userTest.setLogin("bondorol");
+        userTest.setPassword("1234567");
+        userTest.setAge(43);
+        assertNotNull(userTest.getPassword());
+    }
+
+    @Test
+    void idNotNull_Ok() {
+        User userTest = new User();
+        userTest.setId(3264827L);
+        assertNotNull(userTest.getId());
+    }
+
+    @Test
+    void ageIsNotNull_Ok() {
+        User userTest = new User();
+        userTest.setAge(20);
+        assertNotNull(userTest.getAge());
+    }
+
+    @Test
+    void loginIsNotNull_Ok() {
+        User userTest = new User();
+        userTest.setLogin("ewruyeu");
+        assertNotNull(userTest.getLogin());
+    }
+
+    @Test
+    void theLoginIsEquals_notOk() {
+        User userFirstTest = new User();
+        User userSecondTest = new User();
+        userFirstTest.setLogin("bondorol");
+        userSecondTest.setLogin("fsagah");
+
+        String expected = userFirstTest.getLogin();
+        String actual = userSecondTest.getLogin();
+
+        assertNotEquals(expected, actual);
+    }
+
+}
