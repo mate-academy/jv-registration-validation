ce44c0217e2ba057f4e2195401c09c48da3def82
Yuriy Kononchuk <y_kononchuk@ukr.net>
2023-04-08 16:06:03 +0300

    Revert "added changes after review in Tests and RegistrationServiceImpl"
    This reverts commit 0319fea428c44ea9c504193375addebfa53ccf64.

--- src/main/java/core/basesyntax/service/ParameterNotNullUncheckedException.java
+++ src/main/java/core/basesyntax/service/ParameterNotNullUncheckedException.java
@@ -1,7 +1,7 @@
 package core.basesyntax.service;
 
-public class ParameterNotNullUncheckedException extends RuntimeException {
-    public ParameterNotNullUncheckedException(String message) {
+public class ParameterNotNullUncheckedException extends RuntimeException{
+    public ParameterNotNullUncheckedException(String message){
         super(message);
     }
 }
