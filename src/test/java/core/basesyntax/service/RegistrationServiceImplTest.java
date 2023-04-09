086c3834c90019a59c17f31782d1a095bb17c2a8
Sergey Zavalniuk <sergzavalnyuk@gmail.com>
2023-04-09 20:22:33 +0300

    Renamed test method names in one style; Used beforeAll instead of beforeEach;

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -5,67 +5,67 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.model.User;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    private RegistrationService registrationService;
+    private static RegistrationService registrationService;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void beforeAll() {
         registrationService = new RegistrationServiceImpl();
     }
 
     @Test
-    void register_nullUser() {
+    void register_nullUser_NotOk() {
         assertThrows(RegistrationUserException.class, () -> registrationService.register(null));
     }
 
     @Test
-    void nullLogin_NotOk() {
+    void register_nullLogin_NotOk() {
         User user = new User(null, "wqr32r23r", 40);
         assertThrows(RegistrationUserException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void checkLogin_Ok() {
+    void register_checkLogin_Ok() {
         User user = new User("qwe", "qewrqt23", 20);
         assertThrows(RegistrationUserException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_loginAlreadyExists() {
+    void register_loginAlreadyExists_NotOk() {
         User user = new User("user345", "password", 20);
         registrationService.register(user);
         assertThrows(RegistrationUserException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void checkPassword_Ok() {
+    void register_checkPassword_Ok() {
         User user = new User("user135", "qwe", 25);
         assertThrows(RegistrationUserException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void nullPassword_NotOk() {
+    void register_nullPassword_NotOk() {
         User user = new User("user567", null, 40);
         assertThrows(RegistrationUserException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void checkAge_NotOk() {
+    void register_checkAge_NotOk() {
         User user = new User("user246", "password", 12);
         assertThrows(RegistrationUserException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void nullAge_NotOk() {
+    void register_nullAge_NotOk() {
         User user = new User("user678", "password", null);
         assertThrows(RegistrationUserException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_successfulRegistration() {
+    void register_successfulRegistration_Ok() {
         User user = new User("user456", "password", 20);
         User registeredUser = registrationService.register(user);
         assertNotNull(registeredUser.getId());
@@ -75,19 +75,19 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void register_invalidAge() {
+    void register_invalidAge_Ok() {
         User user = new User("user234", "password", 16);
         assertThrows(RegistrationUserException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_invalidLogin() {
+    void register_invalidLogin_Ok() {
         User user = new User("a", "password", 20);
         assertThrows(RegistrationUserException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_invalidPassword() {
+    void register_invalidPassword_Ok() {
         User user = new User("user123", "pass", 20);
         assertThrows(RegistrationUserException.class, () -> registrationService.register(user));
     }
