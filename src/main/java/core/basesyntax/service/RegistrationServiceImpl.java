8770055dff0c41a23518e376268b9e5da540a869
Artem Fedoseev <afedoseev359@gmail.com>
2023-04-13 20:41:49 +0300

    Initial solution

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -9,6 +9,28 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
+        boolean isUserValid = validateUser(user);
+        if (isUserValid) {
+            return storageDao.add(user);
+        }
         return null;
+    }
+
+    private boolean validateUser(User user) {
+        if (user == null) {
+            return false;
+        }
+        String login = user.getLogin();
+        String password = user.getPassword();
+        Integer age = user.getAge();
+
+        if (login == null || password == null || age == null) {
+            return false;
+        }
+        if (login.length() < 6 || password.length() < 6 || age < 18) {
+            return false;
+        }
+        User existingUser = storageDao.get(login);
+        return existingUser == null;
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,122 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import core.basesyntax.model.User;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static final int DEFAULT_USER_AMOUNT = 20;
+    private static final int USERS_TO_ADD = 3;
+    private static final String SHORT_STRING = "abcde";
+    private static final int YOUNG_AGE = 10;
+    private static RegistrationService registrationService;
+    private static Stack<User> users;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        users = new Stack<>();
+        for (int i = 0; i < DEFAULT_USER_AMOUNT; i++) {
+            User user = new User();
+            user.setLogin("user-login-" + i);
+            user.setPassword("user-password-" + i);
+            user.setAge(18 + i);
+            users.add(user);
+        }
+    }
+
+    @Test
+    void register_multipleUsers_Ok() {
+        List<User> registeredUsers = new ArrayList<>();
+        for (int i = 0; i < USERS_TO_ADD; i++) {
+            User registeredUser = registrationService.register(users.pop());
+            if (registeredUser == null) {
+                break;
+            }
+            registeredUsers.add(registeredUser);
+        }
+        assertEquals(USERS_TO_ADD, registeredUsers.size());
+    }
+
+    @Test
+    void register_nullUser_notOk() {
+        User actual = registrationService.register(null);
+        assertNull(actual);
+    }
+
+    @Test
+    void register_returnsUserOnSuccess_Ok() {
+        User actual = registrationService.register(users.pop());
+        assertEquals(User.class, actual.getClass());
+    }
+
+    @Test
+    void register_returnedUserHasId_Ok() {
+        User actual = registrationService.register(users.pop());
+        assertNotNull(actual.getId());
+    }
+
+    @Test
+    void register_existingUser_notOk() {
+        User user = users.pop();
+        User actual = registrationService.register(user);
+        assertNotNull(actual);
+        actual = registrationService.register(user);
+        assertNull(actual);
+    }
+
+    @Test
+    void register_shortLogin_notOk() {
+        User user = users.pop();
+        user.setLogin(SHORT_STRING);
+        User actual = registrationService.register(user);
+        assertNull(actual);
+    }
+
+    @Test
+    void register_nullLogin_notOk() {
+        User user = users.pop();
+        user.setLogin(null);
+        User actual = registrationService.register(user);
+        assertNull(actual);
+    }
+
+    @Test
+    void register_shortPassword_notOk() {
+        User user = users.pop();
+        user.setPassword(SHORT_STRING);
+        User actual = registrationService.register(user);
+        assertNull(actual);
+    }
+
+    @Test
+    void register_nullPassword_notOk() {
+        User user = users.pop();
+        user.setPassword(null);
+        User actual = registrationService.register(user);
+        assertNull(actual);
+    }
+
+    @Test
+    void register_youngUser_notOk() {
+        User user = users.pop();
+        user.setAge(YOUNG_AGE);
+        User actual = registrationService.register(user);
+        assertNull(actual);
+    }
+
+    @Test
+    void register_nullAge_notOk() {
+        User user = users.pop();
+        user.setAge(null);
+        User actual = registrationService.register(user);
+        assertNull(actual);
+    }
+}
