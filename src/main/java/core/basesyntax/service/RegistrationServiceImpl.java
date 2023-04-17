27f023138d85da3be3b8561b5bb89b3741348512
Volodymyr Davidov <volodymyrdavidov1@gmail.com>
2023-04-17 21:08:48 +0300

    hw-jv-registration-validation

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -53,3 +53,4 @@ public class RegistrationServiceImpl implements RegistrationService {
         }
     }
 }
+
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,13 +1,15 @@
 package core.basesyntax.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class RegistrationServiceImplTest {
     private static RegistrationService registrationService;
@@ -48,8 +50,6 @@ class RegistrationServiceImplTest {
         testUser.setLogin(null);
         assertThrows(NullPointerException.class, () -> registrationService.register(testUser));
     }
-
-
 
     @Test
     void register_userAge_ok() {
