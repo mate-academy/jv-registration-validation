42e8d64d7df836d3de42c4bfc83b5488d1b25cd9
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-04-20 02:45:44 +0300

    added registration method and created custom exception

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,39 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final StorageDaoImpl STORAGE = new StorageDaoImpl();
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
-    public User register(User user) {
-        return null;
+    public User register(User user) throws RuntimeException {
+        boolean correctUser = checkUserForAge(user)
+                && checkUserLoginForIdentity(user)
+                && checkUserLoginForLength(user)
+                && checkUserPasswordForLength(user);
+        if (correctUser) {
+            STORAGE.add(user);
+            return user;
+        }
+        throw new InvalidUserException();
+    }
+
+    private boolean checkUserLoginForIdentity(User user) {
+        return STORAGE.get(user.getLogin()) == null;
+    }
+
+    private boolean checkUserLoginForLength(User user) {
+        return user.getLogin().length() >= 6;
+    }
+
+    private boolean checkUserPasswordForLength(User user) {
+        return user.getPassword().length() >= 6;
+    }
+
+    private boolean checkUserForAge(User user) {
+        return user.getAge() >= 18;
     }
 }
