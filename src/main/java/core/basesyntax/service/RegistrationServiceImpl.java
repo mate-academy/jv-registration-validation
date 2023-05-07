c2f5e8aef336c093e2afb469c459eb864bf04434
Tim <tymurprokhorov@gmail.com>
2023-05-07 20:11:35 +0300

    register() implemented, tests provided.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,6 +2,8 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.ValidationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
@@ -9,6 +11,20 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        for (User eachUser : Storage.people) {
+            if (eachUser.getLogin().equals(user.getLogin())) {
+                throw new ValidationException("This user is already present in the storage.");
+            }
+        }
+        if (user.getLogin().length() < 6) {
+            throw new ValidationException("User`s login must have more than 6 symbols");
+        }
+        if (user.getPassword().length() < 6) {
+            throw new ValidationException("User`s password must have more than 6 symbols");
+        }
+        if (user.getAge() < 18) {
+            throw new ValidationException("User`s age must be more than 18 years");
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,116 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.ValidationException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private RegistrationService registrationService;
+
+    @BeforeEach
+    void setUp() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @Test
+    void containsValidUser_Ok() {
+        User kate = new User();
+        kate.setAge(40);
+        kate.setLogin("1234566666");
+        kate.setPassword("43434343");
+        User expected = kate;
+        User actual = registrationService.register(kate);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void userIsAlreadyInStorage_NotOk() {
+        User bob = new User();
+        bob.setAge(22);
+        bob.setLogin("1234567");
+        bob.setPassword("21212121");
+
+        Storage.people.add(bob);
+        assertThrows(ValidationException.class, () -> {
+            registrationService.register(bob);
+        });
+    }
+
+    @Test
+    void nullValue_NotOk() {
+        assertThrows(NullPointerException.class, () -> {
+            registrationService.register(null);
+        });
+    }
+
+    @Test
+    void loginIsMoreThan6Letters_NotOk() {
+        User john = new User();
+        john.setAge(30);
+        john.setLogin("1");
+        john.setPassword("4343");
+        assertThrows(ValidationException.class, () -> {
+            registrationService.register(john);
+        });
+    }
+
+    @Test
+    void loginIsMoreThan6Letters_Ok() {
+        User alice = new User();
+        alice.setAge(32);
+        alice.setLogin("1234567890");
+        alice.setPassword("32323232");
+        int expected = 10;
+        int actual = alice.getLogin().length();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void passwordIsMoreThan6Letters_NotOk() {
+        User jane = new User();
+        jane.setAge(15);
+        jane.setLogin("1234567890");
+        jane.setPassword("1");
+        assertThrows(ValidationException.class, () -> {
+            registrationService.register(jane);
+        });
+    }
+
+    @Test
+    void passwordIsMoreThan6Letters_Ok() {
+        User tim = new User();
+        tim.setAge(25);
+        tim.setLogin("1234567890");
+        tim.setPassword("11111111");
+        int expected = 8;
+        int actual = registrationService.register(tim).getPassword().length();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void userAge_Ok() {
+        User ann = new User();
+        ann.setAge(18);
+        ann.setLogin("1234567890");
+        ann.setPassword("11111111111");
+        int expected = 18;
+        int actual = ann.getAge();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void userAge_NotOk() {
+        User tanya = new User();
+        tanya.setAge(10);
+        tanya.setLogin("123456789");
+        tanya.setPassword("111111111");
+        assertThrows(ValidationException.class, () -> {
+            registrationService.register(tanya);
+        });
+    }
+}
