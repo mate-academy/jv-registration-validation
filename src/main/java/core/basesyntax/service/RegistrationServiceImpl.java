51c71bd77516f5383aba9f6d2608cb9a1406225d
Gamemod13 <igorfetov1997@gmail.com>
2023-05-09 16:05:00 +0300

    Remove the comments

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -10,17 +10,6 @@ public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_PASSWORD_SIZE = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
-    /*
-     * Registration service has one method register(User user),
-     * that accepts some user (the User.class has already been given to you).
-     * This method should register a user (by adding it to Storage) only
-     * if the user meets the following criteria:
-     *
-     * <li>there is no user with such login in the Storage yet</li>
-     * <li>user's login is at least 6 characters</li>
-     * <li>user's password is at least 6 characters</li>
-     * <li>user's age is at least 18 years old</li>
-     */
     @Override
     public User register(User user) {
         validateUserData(user);
