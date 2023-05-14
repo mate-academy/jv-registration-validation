7f61b16773e6c41e0df4b0c3b5ebba72e4966e4b
Oleh Pryshchepa <poleja122@gmail.com>
2023-05-14 14:28:46 +0300

    fixed another mistake

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -8,10 +8,7 @@ import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int VALID_PASSWORD_LENGTH = 6;
-<<<<<<< HEAD
     private static final int VALID_LOGIN_LENGTH = 6;
-=======
->>>>>>> 27b63ef850ab35eb31dad3148cf25dedc66b1940
     private static final int VALID_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
@@ -32,7 +29,6 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user == null) {
             throw new UserRegistrationException("User can`t be null");
         }
-<<<<<<< HEAD
     }
 
     private void isValidUserLogin(User user) {
@@ -60,19 +56,7 @@ public class RegistrationServiceImpl implements RegistrationService {
             throw new UserRegistrationException("User`s age can`t be null");
         }
         if (user.getAge() < VALID_AGE) {
-            throw new UserRegistrationException("Age have to be 18 or older, but was"
-=======
-        if (user.getLogin() == null || user.getLogin().length() < VALID_PASSWORD_LENGTH) {
-            throw new NotValidDataException("Login have to be longer then 6 character but was: "
-                    + user.getLogin().length());
-        }
-        if (user.getPassword() == null || user.getPassword().length() < VALID_PASSWORD_LENGTH) {
-            throw new NotValidDataException("Password have to be longer then 6 character but was: "
-                    + user.getPassword().length());
-        }
-        if (user.getAge() < VALID_AGE) {
-            throw new NotValidDataException("Age have to be 18 or older, but was"
->>>>>>> 27b63ef850ab35eb31dad3148cf25dedc66b1940
+            throw new UserRegistrationException("Age have to be 18 or older, but was: "
                     + user.getAge());
         }
     }
