d07e1964de344086587082b1880c8c2f0b364704
Vladyslav Krutyus <kijdron@gmail.com>
2023-05-16 12:50:25 +0300

    code improved after mentor review

--- src/main/java/core/basesyntax/middleware/user/stringparams/login/LoginExistsMiddleware.java
+++ src/main/java/core/basesyntax/middleware/user/stringparams/login/LoginExistsMiddleware.java
@@ -1,17 +1,18 @@
 package core.basesyntax.middleware.user.stringparams.login;
 
-import core.basesyntax.db.Storage;
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.exceptions.RegistrationException;
 import core.basesyntax.middleware.Middleware;
 import core.basesyntax.model.User;
 
 public class LoginExistsMiddleware extends Middleware {
+    private final StorageDao storageDao = new StorageDaoImpl();
+
     @Override
     public void check(User user) {
-        for (User inStorage : Storage.people) {
-            if (user.getLogin().equals(inStorage.getLogin())) {
-                throw new RegistrationException("User with such email already exists");
-            }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("User with such email already exists");
         }
         checkNext(user);
     }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -17,7 +17,6 @@ class RegistrationServiceImplTest {
     private static final String PASSWORD_VALID = "123456";
     private static final String LOGIN_INVALID = "aboba";
     private static final String PASSWORD_INVALID = "12345";
-
     private static final int AGE_VALID = 18;
     private static final int AGE_INVALID = 17;
 
@@ -38,63 +37,55 @@ class RegistrationServiceImplTest {
     @Test
     void register_nullUser_notOk() {
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(null),
-                "User shouldn't be null");
+                () -> registrationService.register(null));
     }
 
     @Test
     void register_nullAge_notOk() {
         user.setAge(null);
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(user),
-                "Age shouldn't be null");
+                () -> registrationService.register(user));
     }
 
     @Test
     void register_nullLogin_notOk() {
         user.setLogin(null);
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(user),
-                "Login shouldn't be null");
+                () -> registrationService.register(user));
     }
 
     @Test
     void register_nullPassword_notOk() {
         user.setPassword(null);
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(user),
-                "Password shouldn't be null");
+                () -> registrationService.register(user));
     }
 
     @Test
     void register_invalidAge_notOk() {
         user.setAge(AGE_INVALID);
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(user),
-                "Age must be at least 18 years old");
+                () -> registrationService.register(user));
     }
 
     @Test
     void register_invalidLogin_notOk() {
         user.setLogin(LOGIN_INVALID);
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(user),
-                "Login must be at least 6 characters");
+                () -> registrationService.register(user));
     }
 
     @Test
     void register_invalidPassword_notOk() {
         user.setPassword(PASSWORD_INVALID);
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(user),
-                "Password must be at least 6 characters");
+                () -> registrationService.register(user));
     }
 
     @Test
     void register_loginAlreadyExists_notOk() {
         Storage.people.add(user);
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(user),
-                "This login has already been registered");
+                () -> registrationService.register(user));
     }
 }
