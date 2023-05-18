65910c9c454948681882c1b19e6dc0e1749abbd1
ASnazar <36624661+ASnazar@users.noreply.github.com>
2023-05-18 13:48:09 +0300

    Split checks by methods

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -19,24 +19,40 @@ public class RegistrationServiceImpl implements RegistrationService {
     }
 
     private void validateUserData(User user) {
-        if (user.getAge() == null) {
-            throw new InvalidUserDataException("Age can't be null");
-        } else if (user.getAge() <= MIN_AGE) {
-            throw new InvalidUserDataException("Not valid age: " + user.getAge()
-                    + ". Min allowed age is " + MIN_AGE);
-        }
-        if (user.getLogin() == null) {
-            throw new InvalidUserDataException("Login can't be null");
-        } else if (user.getLogin().length() < MIN_LENGTH_LOGIN) {
-            throw new InvalidUserDataException("Not valid login: " + user.getLogin()
-                    + ". Min length login is " + MIN_LENGTH_LOGIN);
-        }
+        validatePassword(user);
+        validateLogin(user);
+        validateAge(user);
+        addExistUser(user);
+    }
+
+    private void validatePassword(User user) {
         if (user.getPassword() == null) {
             throw new InvalidUserDataException("Not valid password: " + user.getPassword()
                     + ". Min length password is " + MIN_LENGTH_PASSWORD);
         } else if (user.getPassword().length() < MIN_LENGTH_PASSWORD) {
             throw new InvalidUserDataException("Password can't be null");
         }
+    }
+
+    private void validateLogin(User user) {
+        if (user.getLogin() == null) {
+            throw new InvalidUserDataException("Login can't be null");
+        } else if (user.getLogin().length() < MIN_LENGTH_LOGIN) {
+            throw new InvalidUserDataException("Not valid login: " + user.getLogin()
+                    + ". Min length login is " + MIN_LENGTH_LOGIN);
+        }
+    }
+
+    private void validateAge(User user) {
+        if (user.getAge() == null) {
+            throw new InvalidUserDataException("Age can't be null");
+        } else if (user.getAge() <= MIN_AGE) {
+            throw new InvalidUserDataException("Not valid age: " + user.getAge()
+                    + ". Min allowed age is " + MIN_AGE);
+        }
+    }
+
+    private void addExistUser(User user) {
         if (storageDao.get(user.getLogin()) != null) {
             throw new InvalidUserDataException("A user with this login already exists");
         }
