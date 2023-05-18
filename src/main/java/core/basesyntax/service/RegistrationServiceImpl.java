e6072688ea1e03b54f4091f3d8d23e1741e60372
Mykyta Naumov <nneed4use@gmail.com>
2023-05-18 16:36:14 +0300

    Third version. Student reviewed 1 time. Mentor reviewed 1 time. Tests created and done ! Checkstyle done

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -12,58 +12,48 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        isValid(user);
+        validateUser(user);
+        validateLogin(user);
+        validatePassword(user);
+        validateAge(user);
         return storageDao.add(user);
     }
 
-    public boolean isValid(User user) {
-        userCheck(user);
-        idCheck(user);
-        loginCheck(user);
-        passwordCheck(user);
-        ageCheck(user);
-        return true;
-    }
-
-    public void userCheck(User user) {
+    public void validateUser(User user) {
         if (user == null) {
             throw new ValidationException("You should fill all necessary fields");
         }
     }
 
-    public void idCheck(User user) {
-        if (user.getId() == null) {
-            throw new ValidationException("ID number shouldn't be null");
-        }
-    }
-
-    public void loginCheck(User user) {
+    public void validateLogin(User user) {
         if (user.getLogin() == null) {
             throw new ValidationException("Login shouldn't be null");
         }
         if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
-            throw new ValidationException("Your login should have length 6 or more");
+            throw new ValidationException(
+                    "Your login ${login} should have length ${MIN_LOGIN_LENGTH} or more");
         }
         if (storageDao.get(user.getLogin()) != null) {
-            throw new ValidationException("Your login is already taken by another user");
+            throw new ValidationException("Your login ${login} is already taken by another user");
         }
     }
 
-    public void passwordCheck(User user) {
+    public void validatePassword(User user) {
         if (user.getPassword() == null) {
             throw new ValidationException("Password shouldn't be null");
         }
         if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
-            throw new ValidationException("Your password should have length 6 or more");
+            throw new ValidationException(
+                    "Your password ${password} should have length ${MIN_PASSWORD_LENGTH} or more");
         }
     }
 
-    public void ageCheck(User user) {
+    public void validateAge(User user) {
         if (user.getAge() == null) {
             throw new ValidationException("Age shouldn't be null");
         }
         if (user.getAge() < MIN_AGE) {
-            throw new ValidationException("User can't be under age 18");
+            throw new ValidationException("User can't be under age ${MIN_AGE}");
         }
     }
 }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -2,7 +2,6 @@ package core.basesyntax.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
@@ -21,6 +20,12 @@ class RegistrationServiceImplTest {
     private static final String DEFAULT_PASSWORD = "qwerty12345";
     private static final int DEFAULT_AGE = 28;
 
+    private void userInit(User user) {
+        user.setLogin(DEFAULT_LOGIN);
+        user.setPassword(DEFAULT_PASSWORD);
+        user.setAge(DEFAULT_AGE);
+    }
+
     @BeforeAll
     static void beforeAll() {
         service = new RegistrationServiceImpl();
@@ -29,107 +34,88 @@ class RegistrationServiceImplTest {
     @BeforeEach
     void setUp() {
         user1 = new User();
-        user1.setId(DEFAULT_ID);
-        user1.setLogin(DEFAULT_LOGIN);
-        user1.setPassword(DEFAULT_PASSWORD);
-        user1.setAge(DEFAULT_AGE);
+        userInit(user1);
         user2 = new User();
-        user2.setId(DEFAULT_ID);
-        user2.setLogin(DEFAULT_LOGIN);
-        user2.setPassword(DEFAULT_PASSWORD);
-        user2.setAge(DEFAULT_AGE);
+        userInit(user2);
         user3 = new User();
-        user3.setId(DEFAULT_ID);
-        user3.setLogin(DEFAULT_LOGIN);
-        user3.setPassword(DEFAULT_PASSWORD);
-        user3.setAge(DEFAULT_AGE);
+        userInit(user3);
     }
 
     @Test
-    void registration_validData_ok() {
-        User expected = user1;
-        service.register(expected);
+    void register_validData_ok() {
+        service.register(user1);
         User actual = Storage.people.get(0);
-        assertEquals(expected, actual);
+        assertEquals(user1, actual);
     }
 
     @Test
-    void isValid_hasSixChar_ok() {
+    void register_hasMinimumLength_ok() {
         user2.setLogin("loginloginloginloginloginloginloginloginloginloginlogin");
         user3.setLogin("login1");
         user2.setPassword("qwertyqwertyqwertyqwertyqwertyqwertyqwertyqwerty");
         user3.setPassword("qwerty");
-        assertTrue(service.isValid(user1));
-        assertTrue(service.isValid(user2));
-        assertTrue(service.isValid(user3));
+        assertEquals(user1, service.register(user1));
+        assertEquals(user2, service.register(user2));
+        assertEquals(user3, service.register(user3));
     }
 
     @Test
-    void isValid_minAge_ok() {
+    void register_ageAboveMinimum_ok() {
         user2.setAge(1234567890);
+        user2.setLogin("loginn");
         user3.setAge(18);
-        assertTrue(service.isValid(user1));
-        assertTrue(service.isValid(user2));
-        assertTrue(service.isValid(user3));
+        user3.setLogin("llogin");
+        assertEquals(user1, service.register(user1));
+        assertEquals(user2, service.register(user2));
+        assertEquals(user3, service.register(user3));
     }
 
     @Test
-    void isValid_sameLogin_ok() {
-        assertTrue(service.isValid(user1));
+    void register_uniqueLogin_ok() {
+        assertEquals(user1, service.register(user1));
     }
 
     @Test
-    void isValid_userNull_ok() {
-        assertTrue(service.isValid(user1));
+    void register_userNotNull_ok() {
+        assertEquals(user1, service.register(user1));
     }
 
     @Test
-    void isValid_idNull_ok() {
-        assertTrue(service.isValid(user1));
-    }
-
-    @Test
-    void isValid_hasSixChar_NotOk() {
+    void register_hasMinimumLength_NotOk() {
         user1.setLogin("login");
         user2.setLogin("");
         user3.setLogin(null);
         user1.setPassword("");
         user2.setPassword("12345");
         user3.setPassword(null);
-        assertThrows(ValidationException.class, () -> service.isValid(user1));
-        assertThrows(ValidationException.class, () -> service.isValid(user2));
-        assertThrows(ValidationException.class, () -> service.isValid(user3));
+        assertThrows(ValidationException.class, () -> service.register(user1));
+        assertThrows(ValidationException.class, () -> service.register(user2));
+        assertThrows(ValidationException.class, () -> service.register(user3));
     }
 
     @Test
-    void isValid_minAge_NotOk() {
+    void register_ageAboveMinimum_NotOk() {
         user1.setAge(-23);
         user2.setAge(17);
         user3.setAge(null);
-        assertThrows(ValidationException.class, () -> service.isValid(user1));
-        assertThrows(ValidationException.class, () -> service.isValid(user2));
-        assertThrows(ValidationException.class, () -> service.isValid(user3));
+        assertThrows(ValidationException.class, () -> service.register(user1));
+        assertThrows(ValidationException.class, () -> service.register(user2));
+        assertThrows(ValidationException.class, () -> service.register(user3));
     }
 
     @Test
-    void isValid_sameLogin_NotOk() {
+    void register_uniqueLogin_NotOk() {
         Storage.people.add(user1);
         user2 = user1;
         user3.setLogin(user1.getLogin());
-        assertThrows(ValidationException.class, () -> service.isValid(user2));
-        assertThrows(ValidationException.class, () -> service.isValid(user3));
+        assertThrows(ValidationException.class, () -> service.register(user2));
+        assertThrows(ValidationException.class, () -> service.register(user3));
     }
 
     @Test
-    void isValid_userNull_NotOk() {
+    void register_userNotNull_NotOk() {
         user1 = null;
-        assertThrows(ValidationException.class, () -> service.isValid(user1));
-    }
-
-    @Test
-    void isValid_idNull_NotOk() {
-        user1.setId(null);
-        assertThrows(ValidationException.class, () -> service.isValid(user1));
+        assertThrows(ValidationException.class, () -> service.register(user1));
     }
 
     @AfterEach
