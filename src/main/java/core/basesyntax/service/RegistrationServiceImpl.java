b68fc75ca246eefa3463ad752bc120308d027eee
mpankrad <kolia.pankrad@gmail.com>
2023-05-18 15:24:30 +0300

    improved solution

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,9 +5,9 @@ import core.basesyntax.exception.InvalidUserException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private static final int MIN_AGE = 18;
     private static final int MIN_LOGIN_LENGTH = 6;
     private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_AGE = 18;
 
     private final StorageDao storageDao;
 
@@ -18,24 +18,45 @@ public class RegistrationServiceImpl implements RegistrationService {
     @Override
     public User register(User user) {
         validateUser(user);
-        if (storageDao.get(user.getLogin()) != null) {
-            throw new InvalidUserException("User with such login already exists");
-        }
         return storageDao.add(user);
     }
 
     private void validateUser(User user) {
+        validateUserNotNull(user);
+        validateLogin(user.getLogin());
+        validatePassword(user.getPassword());
+        validateAge(user.getAge());
+        validateUniqueLogin(user.getLogin());
+    }
+
+    private void validateUserNotNull(User user) {
         if (user == null) {
-            throw new InvalidUserException("User can't be null");
+            throw new InvalidUserException("Object user can not be null");
         }
-        if (user.getLogin() == null || user.getLogin().length() < MIN_LOGIN_LENGTH) {
+    }
+
+    private void validateLogin(String login) {
+        if (login == null || login.length() < MIN_LOGIN_LENGTH) {
             throw new InvalidUserException("Invalid login");
         }
-        if (user.getPassword() == null || user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+    }
+
+    private void validatePassword(String password) {
+        if (password == null || password.length() < MIN_PASSWORD_LENGTH) {
             throw new InvalidUserException("Invalid password");
         }
-        if (user.getAge() == null || user.getAge() < MIN_AGE) {
+    }
+
+    private void validateAge(Integer age) {
+        if (age == null || age < MIN_AGE) {
             throw new InvalidUserException("Invalid age");
         }
     }
+
+    private void validateUniqueLogin(String login) {
+        if (storageDao.get(login) != null) {
+            throw new InvalidUserException("User with such login already exists");
+        }
+    }
 }
+
--- src/test/java/core/basesyntax/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -11,6 +11,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RegistrationServiceTest {
+
+    private static final String VALID_LOGIN = "user12345";
+    private static final String VALID_PASSWORD = "qwerty123";
+    private static final int VALID_AGE = 20;
+
+    private static final String NULL_LOGIN = null;
+    private static final String SHORT_LOGIN = "user";
+    private static final String NULL_PASSWORD = null;
+    private static final String SHORT_PASSWORD = "pass";
+    private static final int NEGATIVE_AGE = -25;
+    private static final Integer NULL_AGE = null;
+    private static final int UNDERAGE = 17;
+
     private RegistrationService registrationService;
     private StorageDao storageDao;
 
@@ -22,74 +35,84 @@ public class RegistrationServiceTest {
 
     @Test
     public void register_validUser_registersSuccessfully() {
-        User user = createUser("user12345", "qwerty123", 20);
+        User user = createUser(VALID_LOGIN, VALID_PASSWORD, VALID_AGE);
+        String failureMessage = "User registration failed";
 
         User registeredUser = registrationService.register(user);
 
-        Assertions.assertEquals(user, registeredUser);
-        Assertions.assertEquals(registeredUser, storageDao.get(registeredUser.getLogin()));
+        Assertions.assertEquals(user, registeredUser, failureMessage);
+        Assertions.assertEquals(registeredUser, storageDao.get(registeredUser.getLogin()),
+                "User not found in storage after registration");
     }
 
     @Test
     public void register_nullUser_throwsInvalidUserException() {
+        String failureMessage = "Expected to throw InvalidUserException for null user";
         Assertions.assertThrows(InvalidUserException.class, () ->
-                registrationService.register(null));
+                registrationService.register(null), failureMessage);
     }
 
     @Test
     public void register_nullLogin_throwsInvalidUserException() {
-        User user = createUser(null, "qwerty123", 20);
+        User user = createUser(null, VALID_PASSWORD, VALID_AGE);
+        String failureMessage = "Expected to throw InvalidUserException for null login";
 
         Assertions.assertThrows(InvalidUserException.class, () ->
-                registrationService.register(user));
+                registrationService.register(user), failureMessage);
     }
 
     @Test
     public void register_shortLogin_throwsInvalidUserException() {
-        User user = createUser("user", "qwerty123", 20);
+        User user = createUser(SHORT_LOGIN, VALID_PASSWORD, VALID_AGE);
+        String failureMessage = "Expected to throw InvalidUserException for short login";
 
         Assertions.assertThrows(InvalidUserException.class, () ->
-                registrationService.register(user));
+                registrationService.register(user), failureMessage);
     }
 
     @Test
     public void register_nullPassword_throwsInvalidUserException() {
-        User user = createUser("user12345", null, 20);
+        User user = createUser(VALID_LOGIN, null, VALID_AGE);
+        String failureMessage = "Expected to throw InvalidUserException for null password";
 
         Assertions.assertThrows(InvalidUserException.class, () ->
-                registrationService.register(user));
+                registrationService.register(user), failureMessage);
     }
 
     @Test
     public void register_shortPassword_throwsInvalidUserException() {
-        User user = createUser("user12345", "pass", 20);
+        User user = createUser(VALID_LOGIN, SHORT_PASSWORD, VALID_AGE);
+        String failureMessage = "Expected to throw InvalidUserException for short password";
 
         Assertions.assertThrows(InvalidUserException.class, () ->
-                registrationService.register(user));
+                registrationService.register(user), failureMessage);
     }
 
     @Test
     public void register_negativeAge_throwsInvalidUserException() {
-        User user = createUser("user12345", "password123", -25);
+        User user = createUser(VALID_LOGIN, VALID_PASSWORD, NEGATIVE_AGE);
+        String failureMessage = "Expected to throw InvalidUserException for negative age";
 
         Assertions.assertThrows(InvalidUserException.class, () ->
-                registrationService.register(user));
+                registrationService.register(user), failureMessage);
     }
 
     @Test
     public void register_nullAge_throwsInvalidUserException() {
-        User user = createUser("user12345", "qwerty123", null);
+        User user = createUser(VALID_LOGIN, VALID_PASSWORD, null);
+        String failureMessage = "Expected to throw InvalidUserException for null age";
 
         Assertions.assertThrows(InvalidUserException.class, () ->
-                registrationService.register(user));
+                registrationService.register(user), failureMessage);
     }
 
     @Test
     public void register_under18Age_throwsInvalidUserException() {
-        User user = createUser("user12345", "password123", 17);
+        User user = createUser(VALID_LOGIN, VALID_PASSWORD, UNDERAGE);
+        String failureMessage = "Expected to throw InvalidUserException for under 18 age";
 
         Assertions.assertThrows(InvalidUserException.class, () ->
-                registrationService.register(user));
+                registrationService.register(user), failureMessage);
     }
 
     private User createUser(String login, String password, Integer age) {
