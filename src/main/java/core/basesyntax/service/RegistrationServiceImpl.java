babcf717393829ac91a95f464a1e0cdf58e7a459
Mykyta Udovycehnko <nikitaudav123@gmail.com>
2023-05-18 11:41:24 +0300

    improve solution jv-registration-validation

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,13 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
-import core.basesyntax.exception.InvalidDataException;
+import core.basesyntax.exception.UserRegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_AGE = 18;
     private static final int MAX_AGE = 100;
-    private static final int PASSWORD_LENGTH = 6;
+    private static final int MIN_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -22,44 +22,48 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     private void checkUser(User user) {
         if (user == null) {
-            throw new InvalidDataException("User shouldn't be null!");
+            throw new UserRegistrationException("User shouldn't be null!");
         }
         if (storageDao.get(user.getLogin()) != null) {
-            throw new InvalidDataException("User: " + user.getLogin() + " already exists!");
+            throw new UserRegistrationException("User: " + user.getLogin() + " already exists!");
         }
     }
 
     private void checkLogin(User user) {
         if (user.getLogin() == null) {
-            throw new InvalidDataException("Login shouldn't be null!");
+            throw new UserRegistrationException("Login shouldn't be null!");
         }
         if (user.getLogin().isEmpty()) {
-            throw new InvalidDataException("Login shouldn't be empty!");
+            throw new UserRegistrationException("Login shouldn't be empty!");
+        }
+        if (user.getLogin().length() < MIN_LENGTH) {
+            throw new UserRegistrationException("Login must contain at least "
+                    + MIN_LENGTH + " character");
         }
     }
 
     private void checkAge(User user) {
         if (user.getAge() == null) {
-            throw new InvalidDataException("Age shouldn't be null!");
+            throw new UserRegistrationException("Age shouldn't be null!");
         }
         if (user.getAge() < MIN_AGE) {
-            throw new InvalidDataException("Age shouldn't be less than " + MIN_AGE + " years!");
+            throw new UserRegistrationException("Age shouldn't be less than " + MIN_AGE + " years!");
         }
         if (user.getAge() > MAX_AGE) {
-            throw new InvalidDataException("Age shouldn't be more than " + MAX_AGE + " years!");
+            throw new UserRegistrationException("Age shouldn't be more than " + MAX_AGE + " years!");
         }
     }
 
     private void checkPassword(User user) {
         if (user.getPassword() == null) {
-            throw new InvalidDataException("Password shouldn't be null!");
+            throw new UserRegistrationException("Password shouldn't be null!");
         }
         if (user.getPassword().isEmpty()) {
-            throw new InvalidDataException("Password shouldn't be empty!");
+            throw new UserRegistrationException("Password shouldn't be empty!");
         }
-        if (user.getPassword().length() < PASSWORD_LENGTH) {
-            throw new InvalidDataException("Password shouldn't be less than "
-                    + PASSWORD_LENGTH + " characters!");
+        if (user.getPassword().length() < MIN_LENGTH) {
+            throw new UserRegistrationException("Password shouldn't be less than "
+                    + MIN_LENGTH + " characters!");
         }
     }
 }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.db.Storage;
-import core.basesyntax.exception.InvalidDataException;
+import core.basesyntax.exception.UserRegistrationException;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -66,69 +66,76 @@ class RegistrationServiceImplTest {
         nameExistsUser.setLogin(DEFAULT_LOGIN);
         nameExistsUser.setAge(24);
         nameExistsUser.setPassword("newPass");
-        assertThrows((InvalidDataException.class), ()
+        assertThrows((UserRegistrationException.class), ()
                 -> registrationService.register(nameExistsUser));
     }
 
     @Test
     void register_nullUser_notOk() {
-        assertThrows(InvalidDataException.class, ()
+        assertThrows(UserRegistrationException.class, ()
                 -> registrationService.register(null));
     }
 
     @Test
     void register_nullLogin_notOk() {
         user.setLogin(null);
-        assertThrows(InvalidDataException.class, ()
+        assertThrows(UserRegistrationException.class, ()
                 -> registrationService.register(user));
     }
 
     @Test
     void register_emptyLogin_notOk() {
         user.setLogin("");
-        assertThrows(InvalidDataException.class, ()
+        assertThrows(UserRegistrationException.class, ()
                 -> registrationService.register(user));
     }
 
     @Test
     void register_nullAge_notOk() {
         user.setAge(null);
-        assertThrows(InvalidDataException.class, ()
+        assertThrows(UserRegistrationException.class, ()
                 -> registrationService.register(user));
     }
 
     @Test
     void register_nullPassword_notOk() {
         user.setPassword(null);
-        assertThrows(InvalidDataException.class, ()
+        assertThrows(UserRegistrationException.class, ()
                 -> registrationService.register(user));
     }
 
     @Test
     void register_emptyPassword_notOk() {
         user.setPassword("");
-        assertThrows(InvalidDataException.class, ()
+        assertThrows(UserRegistrationException.class, ()
                 -> registrationService.register(user));
     }
 
     @Test
     void register_ageLessMinAge_notOk() {
         user.setAge(17);
-        assertThrows(InvalidDataException.class, ()
+        assertThrows(UserRegistrationException.class, ()
                 -> registrationService.register(user));
     }
 
     @Test
     void register_ageMoreMaxAge_notOk() {
         user.setAge(120);
-        assertThrows(InvalidDataException.class, ()
+        assertThrows(UserRegistrationException.class, ()
                 -> registrationService.register(user));
     }
 
     @Test
     void register_passwordLessMinLength_notOk() {
         user.setPassword("12345");
-        assertThrows(InvalidDataException.class, ()
+        assertThrows(UserRegistrationException.class, ()
+                -> registrationService.register(user));
+    }
+
+    @Test
+    void register_loginLessMinLength_notOk() {
+        user.setLogin("Nik");
+        assertThrows(UserRegistrationException.class, ()
                 -> registrationService.register(user));
     }
 
