303819dc564edbed04a9b1cfe760d0add7cc90f2
Artem <cr7realartem@gmail.com>
2023-05-24 22:08:31 +0300

    add 6 test methods with 100% coverage

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,44 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Please enter your login");
+        }
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Please enter your password");
+        }
+        if (user.getAge() == null) {
+            throw new RegistrationException("Please enter your age");
+        }
+        for (User eachUser : Storage.people) {
+            if (eachUser.getLogin().equals(user.getLogin())) {
+                throw new RegistrationException("This login is already use: " + user.getLogin());
+            }
+        }
+        if (user.getLogin().length() < MIN_LENGTH) {
+            throw new RegistrationException("User's login must be at least " + MIN_LENGTH
+                    + " character long, but was " + user.getLogin().length());
+        }
+        if (user.getPassword().length() < MIN_LENGTH) {
+            throw new RegistrationException("User's password must be at least " + MIN_LENGTH
+                    + " character long, but was " + user.getLogin().length());
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("User's age must be minimum " + MIN_AGE
+                    + " but was " + user.getAge());
+        }
+        storageDao.add(user);
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,125 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.RegistrationException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService service;
+    private User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        service = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        Storage.people.clear();
+    }
+
+    @Test
+    void register_IsOk() {
+        user.setAge(18);
+        user.setLogin("login1");
+        user.setPassword("123456");
+        service.register(user);
+        int expected = 1;
+        int actual = Storage.people.size();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void register_LargeDataInput_IsOk() {
+        user.setAge(99);
+        user.setPassword("1112284623846822");
+        user.setLogin("longExampleLogin_qwertyuiopasdfghjkl");
+        service.register(user);
+        int expected = 1;
+        int actual = Storage.people.size();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void register_NullDataInput() {
+        user.setLogin(null);
+        user.setAge(20);
+        user.setPassword("123456");
+        assertThrows(RegistrationException.class, () -> service.register(user));
+        user.setLogin("login1");
+        user.setPassword(null);
+        assertThrows(RegistrationException.class, () -> service.register(user));
+        user.setAge(null);
+        user.setPassword("654321");
+        assertThrows(RegistrationException.class, () -> service.register(user));
+    }
+
+    @Test
+    void register_UnderMinValue() {
+        user.setAge(6);
+        user.setPassword("123456");
+        user.setLogin("login1");
+        assertThrows(RegistrationException.class, () -> service.register(user));
+        user.setAge(20);
+        user.setPassword("1");
+        assertThrows(RegistrationException.class, () -> service.register(user));
+        user.setPassword("123456");
+        user.setLogin("1");
+        assertThrows(RegistrationException.class, () -> service.register(user));
+    }
+
+    @Test
+    void register_sameLogin() {
+        user.setAge(22);
+        user.setPassword("123654");
+        user.setLogin("same_login");
+        service.register(user);
+        int expected = 1;
+        int actual = Storage.people.size();
+        assertEquals(expected, actual);
+        user = new User();
+        user.setAge(21);
+        user.setPassword("333666");
+        user.setLogin("same_login");
+        assertThrows(RegistrationException.class, () -> service.register(user));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void register_Add_Five_Users_isOk() {
+        user.setAge(22);
+        user.setPassword("123654");
+        user.setLogin("same_login");
+        service.register(user);
+        user = new User();
+        user.setAge(56);
+        user.setPassword("1234579796");
+        user.setLogin("login1");
+        service.register(user);
+        user = new User();
+        user.setAge(25);
+        user.setPassword("123645354");
+        user.setLogin("login_example");
+        service.register(user);
+        user = new User();
+        user.setAge(29);
+        user.setPassword("123786654");
+        user.setLogin("same_login111222n");
+        service.register(user);
+        user = new User();
+        user.setAge(72);
+        user.setPassword("12987983654");
+        user.setLogin("smile_login");
+        service.register(user);
+        int expected = 5;
+        int actual = Storage.people.size();
+        assertEquals(expected, actual);
+    }
+}
