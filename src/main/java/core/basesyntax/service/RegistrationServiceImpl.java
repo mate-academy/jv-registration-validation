645501248ea71cd97ed6c5624c9a706256385def
Ros <rospsix@gmail.com>
2023-05-27 12:43:17 +0100

    In the RegistrationServiceImpl class, constant variables have been created to be used in user validation methods.
     The first method checks if the user is not null, while the second method validates the ID to ensure it falls within the allowed range of minimum and maximum lengths.
    
      Then, the user login is validated for null, maximum and minimum length, and the presence of spaces.
    
      The password validation mechanism is similar to the login validation but includes checking for special symbols in the password.
    
      Lastly, the user's age is checked for valid values.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,96 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_ID_LENGTH = 1;
+    private static final int MAX_ID_LENGTH = 200;
+    private static final int MIN_LOGIN_LENGTH = 6;
+    private static final int MAX_LOGIN_LENGTH = 100;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MAX_PASSWORD_LENGTH = 24;
+    private static final int MIN_AGE = 18;
+    private static final int MAX_AGE = 110;
+    private static final String SPACE = " ";
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        validateUser(user);
+        storageDao.add(user);
+        return user;
+    }
+
+    User validateUser(User user) {
+        if (user == null) {
+            throw new RegistrationException("User cannot be null!");
+        }
+        return user;
+    }
+
+    Long validateUserId(Long id) {
+        if (id == null) {
+            throw new RegistrationException("You entered an incorrect ID");
+        }
+        if (id < MIN_ID_LENGTH) {
+            throw new RegistrationException("Invalid User ID length (minimum)!");
+        }
+        if (id > MAX_ID_LENGTH) {
+            throw new RegistrationException("Invalid User ID length (maximum)!");
+        }
+        return id;
+    }
+
+    public String validateUserLogin(String login) {
+        if (login == null) {
+            throw new RegistrationException("Incorrect Login: Login cannot be null");
+        }
+        if (login.length() < MIN_LOGIN_LENGTH) {
+            throw new RegistrationException("Invalid User Login length (min): " + login.length());
+        }
+        if (login.length() > MAX_LOGIN_LENGTH) {
+            throw new RegistrationException("Invalid User Login length (max): " + login.length());
+        }
+        if (login.contains(SPACE)) {
+            throw new RegistrationException("User login cannot contain spaces.  " + login);
+        }
+
+        return login;
+    }
+
+    String validateUserPassword(String password) {
+        if (password == null) {
+            throw new RegistrationException("User password cannot be null");
+        }
+        if (password.length() < MIN_PASSWORD_LENGTH) {
+            throw new RegistrationException("Invalid User password length."
+                    + " Your password is shorter than the minimum length : "
+                    + password.length());
+        }
+        if (password.length() > MAX_PASSWORD_LENGTH) {
+            throw new RegistrationException("Invalid User password length."
+                    + " Your password exceeds the maximum length : " + password.length());
+        }
+        if (!password.matches(".*[!@#$%^&*()_+=|<>?{}\\[\\]~-].*")) {
+            throw new RegistrationException("User password must contain at least one special symbol"
+                    + password);
+        }
+        return password;
+    }
+
+    void validateUserAge(Integer age) {
+        if (age == null) {
+            throw new RegistrationException("User age cannot be null");
+        }
+        if (age < MIN_AGE) {
+            throw new RegistrationException("Invalid User age range :" + age);
+        }
+
+        if (age > MAX_AGE) {
+            throw new RegistrationException("Invalid User age range : " + age);
+        }
     }
 }
+
