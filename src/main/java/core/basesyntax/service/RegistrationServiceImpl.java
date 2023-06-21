5f88d9ebb66ddecde005f225f3780ded764199f6
Zhuk Sergei <svzhuk@ukr.net>
2023-06-21 12:08:49 +0300

    Solution

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,40 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LOGIN_LENGTH = 6;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.equals(null)) {
+            throw new RuntimeException("Mandatory data must be filled");
+        }
+        if (user.getLogin() == null) {
+            throw new RuntimeException("Login must be filled");
+        }
+        if (user.getPassword() == null) {
+            throw new RuntimeException("Password must be filled");
+        }
+        if (user.getAge() == null) {
+            throw new RuntimeException("Age must be filled");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User with login already exists"
+                    + "\n"
+                    + "Try another login");
+        }
+        if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
+            throw new RuntimeException("Login should contain at least 6 characters");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password should contain at least 6 characters");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("Age must be more than " + MIN_AGE + " years.");
+        }
+        return storageDao.add(user);
     }
 }
+
