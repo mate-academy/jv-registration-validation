f5f7da51d36b18b4f5258790144f50e2e13f1a21
ANDREY <andrey.kiyas@gmail.com>
2023-07-02 12:16:39 +0300

    hw-jv-registration-validation-solution-andrey-kiyas

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,48 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    static final int MIN_AGE = 18;
+    static final int MIN_LOGIN = 6;
+    static final int MIN_PASSWORD = 8;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RegistrationException("User can't be null");
+        }
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Login can't be null");
+        }
+        if (user.getLogin().length() <= MIN_LOGIN) {
+            throw new RegistrationException("Login error. Minimum login length must be greater than " + MIN_LOGIN);
+        }
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Password can't be null");
+        }
+        if (user.getPassword().length() <= MIN_PASSWORD) {
+            throw new RegistrationException("Login error. Minimum login length must be greater than " + MIN_PASSWORD);
+        }
+        if (user.getAge() == null) {
+            throw new RegistrationException("Age can't be null");
+        }
+        if (user.getAge() <= 0) {
+            throw new RegistrationException("Not valid age: " + user.getAge() + ". Age can't be less than zero");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("Not valid age: " + user.getAge() + ". Min allowed age is " + MIN_AGE);
+        }
+        if (user.getLogin() != null) {
+            User existedUser = storageDao.get(user.getLogin());
+            if (existedUser != null) {
+                throw new RegistrationException("User login \"" + user.getLogin()
+                        + "\" already exists");
+            }
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,78 @@
+package core.basesyntax.service;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.RegistrationException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+    private User expectedUser;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        expectedUser = new User();
+        expectedUser.setLogin("UserLogin");
+        expectedUser.setPassword("UserPassword");
+        expectedUser.setAge(18);
+    }
+
+    @Test
+    void register_validUser_Ok() {
+        User actualUser = registrationService.register(expectedUser);
+        assertEquals(expectedUser, actualUser);
+        assertNotNull(actualUser.getId());
+    }
+
+    @Test
+    void register_nullUser_notOk() {
+        assertThrows(RegistrationException.class, () -> registrationService.register(null));
+    }
+
+    @Test
+    void register_dublicatedUser_notOk() {
+        User userDouble = new User();
+        userDouble.setLogin(expectedUser.getLogin());
+        Storage.people.add(userDouble);
+        assertThrows(RegistrationException.class, () -> registrationService.register(expectedUser));
+    }
+
+    @Test
+    void register_invalidLogin_notOk() {
+        expectedUser.setLogin(null);
+        assertThrows(RegistrationException.class, () -> registrationService.register(expectedUser));
+        expectedUser.setLogin("");
+        assertThrows(RegistrationException.class, () -> registrationService.register(expectedUser));
+        expectedUser.setLogin("lll");
+        assertThrows(RegistrationException.class, () -> registrationService.register(expectedUser));
+    }
+
+    @Test
+    void register_invalidPassword_notOk() {
+        expectedUser.setPassword(null);
+        assertThrows(RegistrationException.class, () -> registrationService.register(expectedUser));
+        expectedUser.setPassword("");
+        assertThrows(RegistrationException.class, () -> registrationService.register(expectedUser));
+        expectedUser.setPassword("ppp");
+        assertThrows(RegistrationException.class, () -> registrationService.register(expectedUser));
+    }
+
+    @Test
+    void register_invalidAge_notOk() {
+        expectedUser.setAge(null);
+        assertThrows(RegistrationException.class, () -> registrationService.register(expectedUser));
+        expectedUser.setAge(15);
+        assertThrows(RegistrationException.class, () -> registrationService.register(expectedUser));
+        expectedUser.setAge(-15);
+        assertThrows(RegistrationException.class, () -> registrationService.register(expectedUser));
+    }
+}
