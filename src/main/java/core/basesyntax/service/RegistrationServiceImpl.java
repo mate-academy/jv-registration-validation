6ab1c2e6fa01a070a810c870c90561dc05eb9fe9
Nikita <kaptetos@gmail.com>
2023-07-10 16:22:24 +0300

    Implement the RegistrationService interface in the RegistrationServiceImpl class

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,5 +1,6 @@
 package core.basesyntax.service;
 
+import core.basesyntax.InvalidUserException;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
@@ -9,6 +10,22 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        valiidateUser(user);
+        return storageDao.add(user);
+    }
+
+    private void valiidateUser(User user) {
+        if (user.getLogin() == null || user.getLogin().length() < 6) {
+            throw new InvalidUserException("Invalid login: " + user.getLogin());
+        }
+        if (user.getPassword() == null || user.getPassword().length() < 6) {
+            throw new InvalidUserException("Invalid password: " + user.getPassword());
+        }
+        if (user.getAge() == null || user.getAge() < 18) {
+            throw new InvalidUserException("Invalid age: " + user.getAge());
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new InvalidUserException("User with login already exists" + user.getLogin());
+        }
     }
 }
