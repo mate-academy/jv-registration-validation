d2a062c43bff5cee60dbc188743a8855e12270bc
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-07-19 15:21:31 +0300

    changed test where is less than allowed

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.RegistrationException;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
+import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,7 +26,6 @@ class RegistrationServiceImplTest {
     @BeforeEach
     void setUp() {
         user = new User();
-        user.setId(1L);
         user.setAge(VALID_AGE);
         user.setLogin(VALID_LOGIN);
         user.setPassword(VALID_PASSWORD);
@@ -33,11 +34,17 @@ class RegistrationServiceImplTest {
     @Test
     void register_validUser_Ok() {
         assertEquals(user, registrationService.register(user));
+        List<User> expected = List.of(user);
+        assertEquals(expected, Storage.people);
     }
 
     @Test
     void register_ageLessThanAllowed_NotOk() {
         user.setAge(10);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+        user.setAge(3);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+        user.setAge(0);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
@@ -55,7 +62,11 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_loginLessThanAllowed_NotOk() {
-        user.setLogin("qwer");
+        user.setLogin("qwe");
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+        user.setLogin(" ");
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+        user.setLogin("qwert");
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
@@ -68,6 +79,10 @@ class RegistrationServiceImplTest {
     @Test
     void register_passwordLessThanAllowed_NotOk() {
         user.setPassword("qwert");
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+        user.setPassword(" ");
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+        user.setPassword("qwe");
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 }
