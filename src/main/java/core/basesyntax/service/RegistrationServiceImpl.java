18154127b095a4d4d5a4957c7771e70d6be42b84
satori1111 <romakuchmii@gmail.com>
2023-07-21 14:04:13 +0300

    added solution

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,43 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exceptions.UserInvalidDataException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    public static final int MINIMUM_AGE = 18;
+    public static final int MINIMUM_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new UserInvalidDataException("User can't be null");
+        }
+        if (user.getLogin() == null) {
+            throw new UserInvalidDataException("Login can't be null");
+        }
+        if (user.getPassword() == null) {
+            throw new UserInvalidDataException("Password can't be null");
+        }
+        if (user.getAge() == null) {
+            throw new UserInvalidDataException("Age can't be null");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new UserInvalidDataException("User " + user.getLogin() + " already registered");
+        }
+        if (user.getAge() < MINIMUM_AGE) {
+            throw new UserInvalidDataException("Not valid age: " + user.getAge()
+                    + ". Min allowed age is " + MINIMUM_AGE);
+        }
+        if (user.getLogin().length() < MINIMUM_LENGTH) {
+            throw new UserInvalidDataException("Not valid Login: " + user.getLogin()
+                    + ". Min allowed Login is " + MINIMUM_LENGTH);
+        }
+        if (user.getPassword().length() < MINIMUM_LENGTH) {
+            throw new UserInvalidDataException("Not valid password: " + user.getPassword()
+                    + ". Min allowed password is " + MINIMUM_LENGTH);
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,145 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.exceptions.UserInvalidDataException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+    private static StorageDao storageDao;
+    private static final int VALID_AGE = 21;
+    private static final String VALID_LOGIN = "validLogin";
+    private static final String VALID_PASSWORD = "validPassword";
+
+    @BeforeAll
+    static void beforeAll() {
+        storageDao = new StorageDaoImpl();
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void testRegisterUserWhoIsRegistered() {
+        User user = new User();
+        user.setAge(19);
+        user.setLogin("login1234");
+        user.setPassword("password1234");
+        storageDao.add(user);
+        assertThrows(UserInvalidDataException.class, () -> registrationService.register(user),
+                "method need throw UserInvalidDataException for user that contains in storage");
+    }
+
+    @Test
+    void testRegisterNull() {
+        assertThrows(UserInvalidDataException.class, () -> registrationService.register(null),
+                "method need to throw UserInvalidDataException for user: null");
+    }
+
+    @Test
+    void testRegisterNullFields() {
+        User userNullLogin = new User();
+        userNullLogin.setPassword(VALID_PASSWORD);
+        User userNullPassword = new User();
+        userNullPassword.setLogin(VALID_LOGIN);
+        assertThrows(UserInvalidDataException.class,
+                () -> registrationService.register(userNullLogin),
+                "method need throw UserInvalidDataException for login: null");
+        assertThrows(UserInvalidDataException.class,
+                () -> registrationService.register(userNullPassword),
+                "method need throw UserInvalidDataException for password: null");
+    }
+
+    @Test
+    void testRegisterAgeThrowsException() {
+        User userInvalidAge = new User();
+        userInvalidAge.setLogin(VALID_LOGIN);
+        userInvalidAge.setPassword(VALID_PASSWORD);
+        userInvalidAge.setAge(10);
+        assertThrows(UserInvalidDataException.class,
+                () -> registrationService.register(userInvalidAge),
+                "method need throw UserInvalidDataException for age: "
+                        + userInvalidAge.getAge());
+    }
+
+    @Test
+    void testRegister_ok() {
+        User validUser = new User();
+        validUser.setAge(VALID_AGE);
+        validUser.setPassword(VALID_PASSWORD);
+        validUser.setLogin(VALID_LOGIN);
+        assertEquals(validUser, registrationService.register(validUser),
+                "method need return user: " + validUser);
+        User anotherValidUser = new User();
+        anotherValidUser.setPassword(VALID_PASSWORD);
+        anotherValidUser.setLogin("valid login number two");
+        anotherValidUser.setAge(18);
+        assertEquals(anotherValidUser, registrationService.register(anotherValidUser),
+                "method need return user: " + anotherValidUser);
+    }
+
+    @Test
+    void testRegisterNonValidLogin() {
+        User nonValidUser = new User();
+        nonValidUser.setPassword(VALID_PASSWORD);
+        nonValidUser.setLogin("");
+        nonValidUser.setAge(VALID_AGE);
+        assertThrows(UserInvalidDataException.class,
+                () -> registrationService.register(nonValidUser),
+                "method need throw UserInvalidDataException for login:"
+                        + nonValidUser.getLogin());
+        User anotherNonValidUser = new User();
+        anotherNonValidUser.setPassword(VALID_PASSWORD);
+        anotherNonValidUser.setLogin("short");
+        anotherNonValidUser.setAge(VALID_AGE);
+        assertThrows(UserInvalidDataException.class,
+                () -> registrationService.register(anotherNonValidUser),
+                "method need throw UserInvalidDataException for login: "
+                        + anotherNonValidUser.getLogin());
+
+    }
+
+    @Test
+    void testRegisterNonValidPassword() {
+        User nonValidUser = new User();
+        nonValidUser.setPassword("");
+        nonValidUser.setLogin(VALID_LOGIN);
+        nonValidUser.setAge(VALID_AGE);
+        assertThrows(UserInvalidDataException.class,
+                () -> registrationService.register(nonValidUser),
+                "method need throw UserInvalidDataException for password: "
+                        + nonValidUser.getPassword());
+        User anotherNonValidUser = new User();
+        anotherNonValidUser.setPassword("short");
+        anotherNonValidUser.setLogin(VALID_LOGIN);
+        anotherNonValidUser.setAge(VALID_AGE);
+        assertThrows(UserInvalidDataException.class,
+                () -> registrationService.register(anotherNonValidUser),
+                "method need throw UserInvalidDataException for password: "
+                        + anotherNonValidUser.getPassword());
+    }
+
+    @Test
+    void testRegisterAgeNull_notOk() {
+        User nonValidUser = new User();
+        nonValidUser.setPassword(VALID_PASSWORD);
+        nonValidUser.setAge(null);
+        nonValidUser.setLogin(VALID_LOGIN);
+        assertThrows(UserInvalidDataException.class,
+                () -> registrationService.register(nonValidUser),
+                "method need throw UserInvalidDataException for age: null");
+
+    }
+
+}
