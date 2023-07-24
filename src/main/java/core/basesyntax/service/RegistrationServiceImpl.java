fab59a9f9b450b4683784ceb05ada89609c07014
Olexander Parkhomovskyi <sanekparkhomovsky@gmail.com>
2023-07-24 14:16:32 +0300

    changed return type to void in registrationServiceImpl checks

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -24,7 +24,7 @@ public class RegistrationServiceImpl implements RegistrationService {
         return storageDao.add(user);
     }
 
-    private boolean checkNulls(User user) throws ValidDataException {
+    private void checkNulls(User user) throws ValidDataException {
         // Null login check
         if (user.getLogin() == null) {
             throw new ValidDataException("You did`t fill login field!");
@@ -37,69 +37,61 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user.getAge() == null) {
             throw new ValidDataException("You did`t fill age field!");
         }
-        return true;
     }
 
-    private boolean isLoginNotRegistered(User user) throws AlreadyRegisteredException {
+    private void isLoginNotRegistered(User user) throws AlreadyRegisteredException {
         // Same login check
         if (storageDao.get(user.getLogin()) != null) {
             throw new AlreadyRegisteredException("Same login already registered");
         }
-        return true;
     }
 
-    private boolean isIdNotRegistered(User user) throws AlreadyRegisteredException {
+    private void isIdNotRegistered(User user) throws AlreadyRegisteredException {
         // Same id check
         if (storageDao.get(user.getLogin()) != null
                 && storageDao.get(user.getLogin()).getId() != null
                 && storageDao.get(user.getLogin()).getId().equals(user.getId())) {
             throw new AlreadyRegisteredException("Same id already registered");
         }
-        return true;
     }
 
-    private boolean isPasswordLengthValid(User user) throws ValidDataException {
+    private void isPasswordLengthValid(User user) throws ValidDataException {
         // Additional validation for password
         if (user.getPassword().length() < MIN_AUTHORIZATION_LENGTH) {
             throw new ValidDataException("Password must be at least 6 characters long.");
         }
-        return true;
     }
 
-    private boolean isLoginLengthValid(User user) throws ValidDataException {
+    private void isLoginLengthValid(User user) throws ValidDataException {
         // Additional validation for login
         if (user.getLogin().length() < MIN_AUTHORIZATION_LENGTH) {
             throw new ValidDataException("Login must be at least 6 characters long.");
         }
-        return true;
     }
 
-    private boolean isPasswordValid(User user) throws ValidDataException {
+    private void isPasswordValid(User user) throws ValidDataException {
         for (Character character : user.getPassword().toCharArray()) {
             if (Character.isLetterOrDigit(character)) {
                 continue;
             }
             throw new ValidDataException("Password is not meets A-Z-a-z-1-9");
         }
-        return true;
     }
 
-    private boolean isLoginValid(User user) throws ValidDataException {
+    private void isLoginValid(User user) throws ValidDataException {
         for (Character character : user.getLogin().toCharArray()) {
             if (Character.isLetter(character) || Character.isDigit(character)) {
                 continue;
             }
             throw new ValidDataException("Login is not meets A-Z-a-z-1-9");
         }
-        return true;
     }
 
-    private boolean isAgeValid(User user) throws ValidDataException {
+    private void isAgeValid(User user) throws ValidDataException {
         if (user.getAge() < MIN_AGE) {
             throw new ValidDataException("Your age is not acceptable. "
                     + "Come here again after " + (MIN_AGE - user.getAge())
                     + " year/s \n We will wait for you ;)");
         }
-        return true;
     }
 }
