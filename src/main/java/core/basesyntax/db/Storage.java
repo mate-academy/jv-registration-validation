bd7219b6d6b98f93ee19f7271fc3029b04804ddd
Mykyta Zuber <nikitazuber@gmail.com>
2023-07-24 15:01:27 +0300

    Validation Tests

--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -7,3 +7,6 @@ import java.util.List;
 public class Storage {
     public static final List<User> people = new ArrayList<>();
 }
+
+
+
--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,62 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.ValidatorException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int DEFAULT_LOGIN_PASSWORD_LENGTH = 6;
+    private static final int MINIMUM_AGE_REGISTRATION = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
+
+    public User getUserByLogin(String login) {
+        User findedUser = storageDao.get(login);
+        if (findedUser == null) {
+            throw new ValidatorException("User is not register yet");
+        }
+        return findedUser;
+    }
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new NullPointerException("User cant be null!!!");
+        }
+        if (user.getLogin() == null) {
+            throw new ValidatorException("User login can not be null");
+        }
+        if (user.getPassword() == null) {
+            throw new ValidatorException("User password can not be null");
+        }
+        if (user.getAge() == null) {
+            throw new ValidatorException("User age can not be null");
+        }
+        if (user.getLogin().isEmpty()) {
+            throw new ValidatorException("User login can not be empty");
+        }
+        if (user.getPassword().isEmpty()) {
+            throw new ValidatorException("User password can not be empty");
+        }
+        if (user.getLogin().length() < DEFAULT_LOGIN_PASSWORD_LENGTH) {
+            throw new ValidatorException("User login length cant be less 6 characters");
+        }
+        if (user.getPassword().length() < DEFAULT_LOGIN_PASSWORD_LENGTH) {
+            throw new ValidatorException("User password length cant be less 6 characters");
+        }
+        if (user.getAge() < MINIMUM_AGE_REGISTRATION) {
+            throw new ValidatorException("User age cant be less 18");
+        }
+        if (user.getAge() < 0) {
+            throw new ValidatorException("User age cant be negative");
+        }
+        if (Character.isDigit(user.getLogin().charAt(0))) {
+            throw new ValidatorException("User login cant start with number!");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new ValidatorException("User is already exist");
+        }
+
+        storageDao.add(user);
+        return user;
     }
 }
