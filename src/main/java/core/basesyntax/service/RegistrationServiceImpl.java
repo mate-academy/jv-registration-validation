a4c8c052aeef1d89546acbae361c07f223fe38d2
Влад <dakadofordisluv@gmail.com>
2023-07-24 17:46:28 +0300

    fixed after review

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -9,14 +9,12 @@ public class RegistrationServiceImpl implements RegistrationService {
     private static final int LOGIN_MIN_LENGTH = 6;
     private static final int PASSWORD_MIN_LENGTH = 6;
     private static final int MIN_AGE = 18;
-
+    private static final String NULL_MESSAGE = " can`t be null";
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        if (user == null) {
-            throw new UserRegistrationException("User can`t be null");
-        }
+        nullCheck(user);
         checkLogin(user.getLogin());
         checkPassword(user.getPassword());
         checkAge(user.getAge());
@@ -24,10 +22,19 @@ public class RegistrationServiceImpl implements RegistrationService {
         return user;
     }
 
-    private void checkLogin(String login) {
-        if (login == null) {
-            throw new UserRegistrationException("Login can`t be null");
+    private void nullCheck(User user) {
+        if (user == null) {
+            throw new UserRegistrationException("User" + NULL_MESSAGE);
+        } else if (user.getLogin() == null) {
+            throw new UserRegistrationException("Login" + NULL_MESSAGE);
+        } else if (user.getAge() == null) {
+            throw new UserRegistrationException("Age" + NULL_MESSAGE);
+        } else if (user.getPassword() == null) {
+            throw new UserRegistrationException("Password" + NULL_MESSAGE);
         }
+    }
+
+    private void checkLogin(String login) {
         if (login.length() < LOGIN_MIN_LENGTH) {
             throw new UserRegistrationException("Login " + login + " with length " + login.length()
                     + " is invalid by min length " + LOGIN_MIN_LENGTH);
@@ -38,9 +45,6 @@ public class RegistrationServiceImpl implements RegistrationService {
     }
 
     private void checkPassword(String password) {
-        if (password == null) {
-            throw new UserRegistrationException("Login can`t be null");
-        }
         if (password.length() < PASSWORD_MIN_LENGTH) {
             throw new UserRegistrationException(password.length()
                     + " password length is invalid for min length " + PASSWORD_MIN_LENGTH);
@@ -48,9 +52,6 @@ public class RegistrationServiceImpl implements RegistrationService {
     }
 
     private void checkAge(Integer age) {
-        if (age == null) {
-            throw new UserRegistrationException("Age can`t be null");
-        }
         if (age < MIN_AGE) {
             throw new UserRegistrationException(age + " years is less then min age " + MIN_AGE);
         }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -14,13 +14,21 @@ class RegistrationServiceImplTest {
     private static final int LOGIN_MIN_LENGTH = 6;
     private static final int PASSWORD_MIN_LENGTH = 6;
     private static final int MIN_AGE = 18;
-    private static RegistrationService registrationServiceImpl;
-    private static User validUser;
+    private static final String VALID_LOGIN = "Valid_login";
+    private static final String VALID_PASSWORD = "Valid_password";
+    private static RegistrationService registrationService;
 
     @BeforeAll
     static void beforeAll() {
-        registrationServiceImpl = new RegistrationServiceImpl();
-        validUser = User.of("valid_login", "valid_password", 32);
+        if (VALID_LOGIN.length() < LOGIN_MIN_LENGTH) {
+            throw new RuntimeException("Valid login should has length at least "
+                    + LOGIN_MIN_LENGTH);
+        }
+        if (VALID_PASSWORD.length() < PASSWORD_MIN_LENGTH) {
+            throw new RuntimeException("Valid password should has length at least "
+                    + PASSWORD_MIN_LENGTH);
+        }
+        registrationService = new RegistrationServiceImpl();
     }
 
     @BeforeEach
@@ -30,103 +38,111 @@ class RegistrationServiceImplTest {
 
     @Test
     void validUserCase() {
-        User expected = validUser;
-        User actual = registrationServiceImpl.register(validUser);
+        User expected = User.of(VALID_LOGIN, VALID_PASSWORD, 37);
+        User actual = registrationService.register(expected);
         assertEquals(expected, actual);
     }
 
     @Test
-    void validUserCase_withMinLengthLogin() {
-        User newValidUser = User.of(validUser.getLogin().substring(0, LOGIN_MIN_LENGTH),
-                validUser.getPassword(), validUser.getAge());
-        User expected = newValidUser;
-        User actual = registrationServiceImpl.register(newValidUser);
+    void validUserCase_minLengthLogin() {
+        String minLengthLogin = VALID_LOGIN.substring(0, LOGIN_MIN_LENGTH);
+        User expected = User.of(minLengthLogin, VALID_PASSWORD, 56);
+        User actual = registrationService.register(expected);
         assertEquals(expected, actual);
     }
 
     @Test
-    void validUserCase_withMinAge() {
-        User newValidUser = User.of(validUser.getLogin(), validUser.getPassword(), MIN_AGE);
-        User expected = newValidUser;
-        User actual = registrationServiceImpl.register(newValidUser);
+    void validUserCase_minAge() {
+        User expected = User.of(VALID_LOGIN, VALID_PASSWORD, MIN_AGE);
+        User actual = registrationService.register(expected);
         assertEquals(expected, actual);
     }
 
     @Test
-    void validUserCase_withMinLengthPassword() {
-        User newValidUser = User.of(validUser.getLogin(),
-                validUser.getPassword().substring(0, PASSWORD_MIN_LENGTH), validUser.getAge());
-        User expected = newValidUser;
-        User actual = registrationServiceImpl.register(newValidUser);
+    void validUserCase_minLengthPassword() {
+        String minLengthPassword = VALID_PASSWORD.substring(0, PASSWORD_MIN_LENGTH);
+        User expected = User.of(VALID_LOGIN, minLengthPassword, 75);
+        User actual = registrationService.register(expected);
         assertEquals(expected, actual);
     }
 
     @Test
-    void userWithExistingLogin() {
-        registrationServiceImpl.register(validUser);
-        User existingLoginUser = User.of(validUser.getLogin(), "else_password", 19);
+    void invalidUserCase_existingLogin() {
+        User validUser = User.of(VALID_LOGIN, VALID_PASSWORD, MIN_AGE);
+        registrationService.register(validUser);
+        User existingLoginUser = User.of(VALID_LOGIN, "else_password", 19);
         assertThrows(UserRegistrationException.class, () -> {
-            registrationServiceImpl.register(existingLoginUser);
+            registrationService.register(existingLoginUser);
         }, "This login already exist - " + validUser.getLogin());
     }
 
     @Test
-    void nullUser() {
+    void invalidUserCase_nullUser() {
         assertThrows(UserRegistrationException.class, () -> {
-            registrationServiceImpl.register(null);
+            registrationService.register(null);
         }, "User can`t be null");
     }
 
     @Test
-    void userWithInvalidLogin() {
-        String invalidLogin = "12345";
-        User invalidLoginUser = User.of(invalidLogin, validUser.getPassword(),
-                validUser.getAge());
+    void invalidUserCase_invalidLoginByLength() {
+        String invalidLogin = VALID_LOGIN.substring(0, LOGIN_MIN_LENGTH - 1);
+        User invalidLoginUser = User.of(invalidLogin, VALID_PASSWORD, 46);
         assertThrows(UserRegistrationException.class, () -> {
-            registrationServiceImpl.register(invalidLoginUser);
+            registrationService.register(invalidLoginUser);
         }, "Login " + invalidLogin + " with length " + invalidLogin.length()
                 + " is invalid by min length " + LOGIN_MIN_LENGTH);
-        String nullLogin = null;
-        User nullLoginUser = User.of(nullLogin, validUser.getPassword(), validUser.getAge());
+    }
+
+    @Test
+    void invalidUserCase_nullLogin() {
+        User nullLoginUser = User.of(null, VALID_PASSWORD, MIN_AGE);
         assertThrows(UserRegistrationException.class, () -> {
-            registrationServiceImpl.register(nullLoginUser);
+            registrationService.register(nullLoginUser);
         }, "Login can`t be null");
     }
 
     @Test
-    void userWithInvalidPassword() {
-        String invalidPassword = "12345";
-        User invalidPasswordUser = User.of(validUser.getLogin(), invalidPassword,
-                validUser.getAge());
+    void invalidUserCase_invalidPasswordByLength() {
+        String invalidPassword = VALID_PASSWORD.substring(0, PASSWORD_MIN_LENGTH - 1);
+        User invalidPasswordUser = User.of(VALID_LOGIN, invalidPassword, 89);
         assertThrows(UserRegistrationException.class, () -> {
-            registrationServiceImpl.register(invalidPasswordUser);
-        }, invalidPassword.length()
-                + " password length is invalid for min length " + PASSWORD_MIN_LENGTH);
-        String nullPassword = null;
-        User nullPasswordUser = User.of(validUser.getLogin(), nullPassword, validUser.getAge());
-        assertThrows(UserRegistrationException.class, () -> {
-            registrationServiceImpl.register(nullPasswordUser);
-        }, "Login can`t be null");
+            registrationService.register(invalidPasswordUser);
+        }, invalidPassword.length() + " password length is invalid for min length "
+                + PASSWORD_MIN_LENGTH);
     }
 
     @Test
-    void userWithInvalidAge() {
-        Integer invalidLoverLimit = 17;
-        User invalidAgeUserLover = User.of(validUser.getLogin(), validUser.getPassword(),
-                invalidLoverLimit);
+    void invalidUserCase_nullPassword() {
+        User nullPasswordUser = User.of(VALID_LOGIN, null, 76);
         assertThrows(UserRegistrationException.class, () -> {
-            registrationServiceImpl.register(invalidAgeUserLover);
-        }, invalidLoverLimit + " years is less then min age " + MIN_AGE);
+            registrationService.register(nullPasswordUser);
+        }, "Password can`t be null");
+    }
+
+    @Test
+    void invalidUserCase_ageLessThenMinAge() {
+        Integer invalidAge = MIN_AGE - 1;
+        User invalidAgeUserLover = User.of(VALID_LOGIN, VALID_PASSWORD, invalidAge);
+        assertThrows(UserRegistrationException.class, () -> {
+            registrationService.register(invalidAgeUserLover);
+        }, invalidAge + " years is less then min age " + MIN_AGE);
+    }
+
+    @Test
+    void invalidUserCase_negativeAge() {
         Integer negativeAge = -100;
-        User negativeAgeUser = User.of(validUser.getLogin(), validUser.getPassword(),
+        User negativeAgeUser = User.of(VALID_LOGIN, VALID_PASSWORD,
                 negativeAge);
         assertThrows(UserRegistrationException.class, () -> {
-            registrationServiceImpl.register(negativeAgeUser);
+            registrationService.register(negativeAgeUser);
         }, negativeAge + " years is less then min age " + MIN_AGE);
-        Integer nullAge = null;
-        User nullAgeUser = User.of(validUser.getLogin(), validUser.getPassword(), nullAge);
+    }
+
+    @Test
+    void invalidUserCase_nullAge() {
+        User nullAgeUser = User.of(VALID_LOGIN, VALID_PASSWORD, null);
         assertThrows(UserRegistrationException.class, () -> {
-            registrationServiceImpl.register(nullAgeUser);
+            registrationService.register(nullAgeUser);
         }, "Age can`t be null");
     }
 }
