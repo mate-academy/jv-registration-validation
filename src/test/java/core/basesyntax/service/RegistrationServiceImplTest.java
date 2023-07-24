9431e251d08bedee32203aa2c1db39fc01e1c298
Volodymyr Lazarchuk <lazarchuk.v.u@gmail.com>
2023-07-24 17:44:39 +0300

    implemented RegistrationService and covered by tests

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -17,42 +17,42 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void register_nullUser_notOk() {
+    void register_AddNullUser_notOk() {
         assertThrows(RegistrationException.class, () -> registrationService.register(null));
     }
 
     @Test
-    void register_nullLogin_notOk() {
+    void register_UseNullLogin_notOk() {
         User user = new User(null, "123456", 20);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_shortLogin_notOk() {
+    void register_UseShortLogin_notOk() {
         User user = new User("short", "123456", 20);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_nullPassword_notOk() {
+    void register_UseNullPassword_notOk() {
         User user = new User("logiin", null, 20);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_shortPassword_notOk() {
+    void register_UseShortPassword_notOk() {
         User user = new User("logiin", "short", 20);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_underageUser_notOk() {
+    void register_AddUnderageUser_notOk() {
         User user = new User("logiin", "123456", 17);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void register_validUser_ok() {
+    void register_AddValidUser_ok() {
         User user = new User("logiin", "123456", 20);
         assertEquals(user, registrationService.register(user));
     }
