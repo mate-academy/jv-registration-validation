3d88c8fe359a3c7d5fd77fe20e3d8e418645f7e4
Tetiana <tetiana.kolomiiets.fop@gmail.com>
2023-07-24 16:58:13 +0300

    added new edge tests

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -8,17 +8,26 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
     private static final Long VALID_ID = 1L;
-    private static final int VALID_AGE = 18;
-    private static final String VALID_LOGIN = "newUser";
-    private static final String VALID_PASSWORD = "newUser";
-
-    private static final int INVALID_AGE = 15;
-    private static final String INVALID_LOGIN = "mate";
-    private static final String INVALID_PASSWORD = "mate";
+    private static final int VALID_AGE_MIN = 18;
+    private static final String VALID_LOGIN_MIN = "new111";
+    private static final String VALID_PASSWORD_MIN = "new111";
+    private static final int VALID_AGE_AVG = 50;
+    private static final String VALID_LOGIN_AVG = "new111new111new111new111";
+    private static final String VALID_PASSWORD_AVG = "new111new111new111new111";
+    private static final int INVALID_AGE_MAX = 17;
+    private static final String INVALID_LOGIN_MAX = "mate1";
+    private static final String INVALID_PASSWORD_MAX = "mate1";
+    private static final int INVALID_AGE_AVG = 13;
+    private static final String INVALID_LOGIN_AVG = "mate";
+    private static final String INVALID_PASSWORD_AVG = "mate";
+    private static final int INVALID_AGE_MIN = 0;
+    private static final String INVALID_LOGIN_MIN = "";
+    private static final String INVALID_PASSWORD_MIN = "";
 
     private static RegistrationService registrationService;
     private static User user;
@@ -27,27 +36,40 @@ class RegistrationServiceImplTest {
     static void beforeAll() {
         registrationService = new RegistrationServiceImpl();
         user = new User();
+    }
+
+    @BeforeEach
+    void setUp() {
         user.setId(VALID_ID);
-        user.setLogin(VALID_LOGIN);
-        user.setPassword(VALID_PASSWORD);
-        user.setAge(VALID_AGE);
+        user.setLogin(VALID_LOGIN_MIN);
+        user.setPassword(VALID_PASSWORD_MIN);
+        user.setAge(VALID_AGE_MIN);
     }
 
     @Test
-    void validUserIsCreated_Ok() {
+    void register_validUserEdgeCaseIsCreated_Ok() {
         User actual = registrationService.register(user);
         assertTrue(actual.equals(user));
     }
 
     @Test
-    void userIsNull_NotOk() {
+    void register_validUserNonEdgeCaseIsCreated_Ok() {
+        user.setAge(VALID_AGE_AVG);
+        user.setLogin(VALID_LOGIN_AVG);
+        user.setPassword(VALID_PASSWORD_AVG);
+        User actual = registrationService.register(user);
+        assertTrue(actual.equals(user));
+    }
+
+    @Test
+    void register_nullUser_notOk() {
         assertThrows(InvalidUserException.class, () -> {
             registrationService.register(null);
         });
     }
 
     @Test
-    void ageIsNull_NotOk() {
+    void register_nullAge_notOk() {
         user.setAge(null);
         assertThrows(InvalidUserException.class, () -> {
             registrationService.register(user);
@@ -55,7 +77,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void loginIsNull_NotOk() {
+    void register_nullLogin_notOk() {
         user.setLogin(null);
         assertThrows(InvalidUserException.class, () -> {
             registrationService.register(user);
@@ -63,7 +85,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void passwordIsNull_NotOk() {
+    void register_nullPassword_notOk() {
         user.setPassword(null);
         assertThrows(InvalidUserException.class, () -> {
             registrationService.register(user);
@@ -71,7 +93,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void userAlreadyExists_NotOk() {
+    void register_userAlreadyExists_NotOk() {
         Storage.people.add(user);
         assertThrows(InvalidUserException.class, () -> {
             registrationService.register(user);
@@ -79,24 +101,72 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void tooShortLogin_NotOk() {
-        user.setLogin(INVALID_LOGIN);
+    void register_tooShortLoginMaxEdgeCase_NotOk() {
+        user.setLogin(INVALID_LOGIN_MAX);
         assertThrows(InvalidUserException.class, () -> {
             registrationService.register(user);
         });
     }
 
     @Test
-    void tooShortPassword_NotOk() {
-        user.setPassword(INVALID_PASSWORD);
+    void register_tooShortPasswordMaxEdgeCase_NotOk() {
+        user.setPassword(INVALID_PASSWORD_MAX);
         assertThrows(InvalidUserException.class, () -> {
             registrationService.register(user);
         });
     }
 
     @Test
-    void tooYoungUser_NotOk() {
-        user.setAge(INVALID_AGE);
+    void register_tooYoungUserMaxEdgeCase_NotOk() {
+        user.setAge(INVALID_AGE_MAX);
+        assertThrows(InvalidUserException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_tooShortLoginAvgEdgeCase_NotOk() {
+        user.setLogin(INVALID_LOGIN_AVG);
+        assertThrows(InvalidUserException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_tooShortPasswordAvgEdgeCase_NotOk() {
+        user.setPassword(INVALID_PASSWORD_AVG);
+        assertThrows(InvalidUserException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_tooYoungUserAvgEdgeCase_NotOk() {
+        user.setAge(INVALID_AGE_AVG);
+        assertThrows(InvalidUserException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_tooShortLoginMinEdgeCase_NotOk() {
+        user.setLogin(INVALID_LOGIN_MIN);
+        assertThrows(InvalidUserException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_tooShortPasswordMinEdgeCase_NotOk() {
+        user.setPassword(INVALID_PASSWORD_MIN);
+        assertThrows(InvalidUserException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_tooYoungUserMinEdgeCase_NotOk() {
+        user.setAge(INVALID_AGE_MIN);
         assertThrows(InvalidUserException.class, () -> {
             registrationService.register(user);
         });
