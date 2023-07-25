ef7d2b6d27dbf3be4bc41aa1fb9ca4a1a4f5bf23
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-07-25 12:01:24 +0300

    Add registrating existing user and changed to lambdas

--- src/test/java/core/basesyntax/service/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceTest.java
@@ -3,8 +3,10 @@ package core.basesyntax.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import core.basesyntax.db.Storage;
 import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -28,86 +30,74 @@ class RegistrationServiceTest {
     @Test
     void register_nullUser_notOk() {
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(NULL_USER); },
+                () -> registrationService.register(NULL_USER),
                 "It should throw RegistrationException when user is null");
     }
 
     @Test
     void register_nullPassword_notOk() {
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(NULL_PASSWORD_USER); },
+                () -> registrationService.register(NULL_PASSWORD_USER),
                 "It should throw RegistrationException when password is null");
     }
 
     @Test
     void register_nullLogin_notOk() {
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(NULL_LOGIN_USER); },
+                () -> registrationService.register(NULL_LOGIN_USER),
                 "It should throw RegistrationException when login is null");
     }
 
     @Test
     void register_nulAge_notOk() {
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(NULL_AGE_USER); },
+                () -> registrationService.register(NULL_AGE_USER),
                 "It should throw RegistrationException when age is null");
     }
 
     @Test
     void register_shortPassword_notOk() {
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(SHORT_PASSWORD_USER); },
+                () -> registrationService.register(SHORT_PASSWORD_USER),
                 "Password length must be more than "
                         + SHORT_PASSWORD_USER.getPassword().length() + "letters");
         String passwordOneLetter = "a";
         SHORT_PASSWORD_USER.setPassword(passwordOneLetter);
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(SHORT_PASSWORD_USER); },
+                () -> registrationService.register(SHORT_PASSWORD_USER),
                 "Password length must be more than " + passwordOneLetter.length() + "letters");
         String passwordThreeLetters = "abc";
         SHORT_PASSWORD_USER.setPassword(passwordThreeLetters);
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(SHORT_PASSWORD_USER); },
+                () -> registrationService.register(SHORT_PASSWORD_USER),
                 "Password length must be more than " + passwordThreeLetters.length() + "letters");
         String passwordFiveLetters = "abcde";
         SHORT_PASSWORD_USER.setPassword(passwordFiveLetters);
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(SHORT_PASSWORD_USER); },
+                () -> registrationService.register(SHORT_PASSWORD_USER),
                 "Password length must be more than " + passwordFiveLetters.length() + "letters");
     }
 
     @Test
     void register_shortLogin_notOk() {
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(SHORT_LOGIN_USER); },
+                () -> registrationService.register(SHORT_LOGIN_USER),
                 "Login length must be more than "
                         + SHORT_LOGIN_USER.getLogin().length() + "letters");
         String loginOneLetter = "a";
         SHORT_LOGIN_USER.setLogin(loginOneLetter);
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(SHORT_LOGIN_USER); },
+                () -> registrationService.register(SHORT_LOGIN_USER),
                 "Login length must be more than " + loginOneLetter.length() + "letters");
         String loginThreeLetters = "abc";
         SHORT_LOGIN_USER.setLogin(loginThreeLetters);
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(SHORT_LOGIN_USER); },
+                () -> registrationService.register(SHORT_LOGIN_USER),
                 "Login length must be more than " + loginThreeLetters.length() + "letters");
         String loginFiveLetters = "abcde";
         SHORT_LOGIN_USER.setLogin(loginFiveLetters);
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(SHORT_LOGIN_USER); },
+                () -> registrationService.register(SHORT_LOGIN_USER),
                 "Login length must be more than " + loginFiveLetters.length() + "letters");
     }
 
@@ -118,28 +108,37 @@ class RegistrationServiceTest {
     }
 
     @Test
+    void register_existingUser_notOk() {
+        User existing = new User("AnnaLisa", "12345678", 78);
+        registrationService.register(existing);
+        assertThrows(RegistrationException.class, () -> registrationService.register(existing),
+                "Must be thrown RegistrationException while registrating existing user");
+    }
+
+    @Test
     void register_smallAge_notOk() {
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(SMALL_AGE_USER); },
+                () -> registrationService.register(SMALL_AGE_USER),
                 "Your age should be at least 18");
         Integer negativeAge = -1;
         SMALL_AGE_USER.setAge(negativeAge);
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(SMALL_AGE_USER); },
+                () -> registrationService.register(SMALL_AGE_USER),
                 "Your age must be non negative");
         Integer tenAge = 10;
         SMALL_AGE_USER.setAge(tenAge);
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(SMALL_AGE_USER); },
+                () -> registrationService.register(SMALL_AGE_USER),
                 "Your age should be at least 18");
         Integer edgeAge = 17;
         SMALL_AGE_USER.setAge(edgeAge);
         assertThrows(RegistrationException.class,
-                () -> {
-                registrationService.register(SMALL_AGE_USER); },
+                () -> registrationService.register(SMALL_AGE_USER),
                 "Your age should be at least 18");
+    }
+
+    @AfterEach
+    void clearStorage() {
+        Storage.people.clear();
     }
 }
