e542f83db4f70b5cb5372b84acaf20b5011417b5
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-07-25 10:36:07 +0300

    test malpractice variable

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -16,12 +16,12 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user == null) {
             throw new RegistrationException("User can`t be a null");
         }
-        if (storageDao.get(user.getLogin()) != null) {
-            throw new RegistrationException("This login already used: " + user.getLogin());
-        }
         if (user.getLogin() == null || user.getLogin().length() < MIN_LENGTH_LOGIN) {
             throw new RegistrationException("Login can't be null or must be more than "
                     + MIN_LENGTH_LOGIN + " characters");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("This login already used: " + user.getLogin());
         }
         if (user.getPassword() == null || user.getPassword().length() < MIN_LENGTH_PASSWORD) {
             throw new RegistrationException("Password can't be null or must be more than "
