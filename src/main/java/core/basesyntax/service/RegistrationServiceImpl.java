a7741eaeee8b7e4f8de2712df6da4137d3b41864
Oleg Turyk <oleh.turyk.ki.2019@lpnu.ua>
2023-07-27 10:19:03 +0300

    fixed errors

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -13,34 +13,48 @@ public class RegistrationServiceImpl implements RegistrationService {
     @Override
     public User register(User user) {
         if (user == null) {
-            throw new RegistrationServiceException("Can't register user, user is null");
+            throw new RegistrationServiceException("User registration failed."
+                    + " User object is null."
+                    + " Please provide valid user information.");
         }
         if (user.getLogin() == null) {
-            throw new RegistrationServiceException("Can't register user, login is null");
+            throw new RegistrationServiceException("User registration failed."
+                    + " The login field is empty or null."
+                    + " Please provide a valid login.");
         }
         if (user.getLogin().length() < MIN_PASSWORD_AND_EMAIL_LENGTH) {
-            throw new RegistrationServiceException("Can't register user, login "
-                    + user.getLogin() + " is to small");
+            throw new RegistrationServiceException("User registration failed."
+                    + " The login provided is too short."
+                    + " Please use a login that contains at least "
+                    + MIN_PASSWORD_AND_EMAIL_LENGTH + " characters.");
         }
         if (storageDao.get(user.getLogin()) != null) {
-            throw new RegistrationServiceException("Can't register user, there is "
-                    + user.getLogin() + " login in Storage");
+            throw new RegistrationServiceException("User registration failed."
+                    + " The login '"
+                    + user.getLogin() + "' is already in use."
+                    + " Please choose a different login.");
         }
         if (user.getPassword() == null) {
-            throw new RegistrationServiceException("Can't register user, password is null");
+            throw new RegistrationServiceException("User registration failed."
+                    + " The password field cannot be empty."
+                    + " Please provide a valid password.");
         }
         if (user.getPassword().length() < MIN_PASSWORD_AND_EMAIL_LENGTH) {
-            throw new RegistrationServiceException("Can't register user, password "
-                    + user.getPassword() + " is to small");
+            throw new RegistrationServiceException("User registration failed."
+                    + " The password provided does not meet the minimum length requirement."
+                    + " Please use a password of at least 8 characters.");
         }
         if (user.getAge() == null) {
-            throw new RegistrationServiceException("Can't register user, age is null");
+            throw new RegistrationServiceException("User registration failed."
+                    + " The age field is missing or null."
+                    + " Please provide a valid age to complete the registration.");
         }
         if (user.getAge() < MIN_USER_AGE) {
-            throw new RegistrationServiceException("Can't register user, age "
-                    + user.getAge() + " is to small");
+            throw new RegistrationServiceException("User registration failed."
+                    + " The minimum age requirement is "
+                    + MIN_USER_AGE + " years. Please make sure you are at least "
+                    + MIN_USER_AGE + " years old to register.");
         }
-        storageDao.add(user);
-        return user;
+        return storageDao.add(user);
     }
 }
--- src/test/java/core/basesyntax/service/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceTest.java
@@ -12,14 +12,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceTest {
-    private static final String INVALID_LOGIN = "LOGIN";
-    private static final String VALID_LOGIN = "LOGIN@";
+    private static final String EMPTY_ROW = "";
+    private static final String INVALID_LOGIN_SHORTER_BY_FEW_SYMBOLS = "LOG";
+    private static final String INVALID_LOGIN_SHORTER_BY_ONE = "LOGIN";
+    private static final String MIN_VALID_LOGIN = "LOGIN@";
+    private static final String VALID_LOGIN_LONGER_BY_ONE = "LOGINS@";
     private static final String EXISTING_LOGIN = "LOGIN_OLEG";
-    private static final String INVALID_PASSWORD = "PASS";
-    private static final String VALID_PASSWORD = "PASSWO";
-    private static final int INVALID_AGE = 15;
-    private static final int VALID_AGE = 20;
-    private static final User EXISTING = new User(EXISTING_LOGIN, VALID_PASSWORD, VALID_AGE);
+    private static final String INVALID_PASS_SHORTER_BY_FEW_SYMBOLS = "PAS";
+    private static final String INVALID_PASS_SHORTER_BY_ONE = "PASS@";
+    private static final String MIN_VALID_PASSWORD = "PASSWO";
+    private static final String VALID_PASS_LONGER_BY_ONE = "PASSWOR";
+    private static final int INVALID_AGE_LESS_BY_FEW_SYMBOLS = 15;
+    private static final int INVALID_AGE_LESS_BY_ONE = 17;
+    private static final int MIN_VALID_AGE = 18;
+    private static final int VALID_AGE_GREATER_BY_ONE = 19;
+    private static final User EXISTING
+            = new User(EXISTING_LOGIN, MIN_VALID_PASSWORD, MIN_VALID_AGE);
     private RegistrationService registrationService;
     private StorageDao storageDao;
 
@@ -31,59 +39,99 @@ class RegistrationServiceTest {
     }
 
     @Test
-    void validRegistration() {
-        User user = new User(VALID_LOGIN, VALID_PASSWORD, VALID_AGE);
+    void register_ValidUser_Success() {
+        User user = new User(MIN_VALID_LOGIN, MIN_VALID_PASSWORD, MIN_VALID_AGE);
         User registeredUser = registrationService.register(user);
+
         assertNotNull(registeredUser.getId());
         assertEquals(user.getLogin(), registeredUser.getLogin());
         assertEquals(user.getPassword(), registeredUser.getPassword());
         assertEquals(user.getAge(), registeredUser.getAge());
+
+        User userValidLongerByOne
+                = new User(VALID_LOGIN_LONGER_BY_ONE,
+                VALID_PASS_LONGER_BY_ONE, VALID_AGE_GREATER_BY_ONE);
+        registeredUser = registrationService.register(userValidLongerByOne);
+
+        assertNotNull(registeredUser.getId());
+        assertEquals(userValidLongerByOne.getLogin(), registeredUser.getLogin());
+        assertEquals(userValidLongerByOne.getPassword(), registeredUser.getPassword());
+        assertEquals(userValidLongerByOne.getAge(), registeredUser.getAge());
     }
 
     @Test
-    void nullUserRegistration() {
+    void register_NullUser_ThrowsRegistrationServiceException() {
         assertThrows(RegistrationServiceException.class, () -> registrationService.register(null));
     }
 
     @Test
-    void duplicateLoginRegistration() {
-        User user = new User(EXISTING.getLogin(), VALID_PASSWORD, VALID_AGE);
+    void register_DuplicateLogin_ThrowsRegistrationServiceException() {
+        User user = new User(EXISTING.getLogin(), MIN_VALID_PASSWORD, MIN_VALID_AGE);
         assertThrows(RegistrationServiceException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void nullLoginRegistration() {
-        User user = new User(null, VALID_PASSWORD, VALID_AGE);
+    void register_NullLogin_ThrowsRegistrationServiceException() {
+        User user = new User(null, MIN_VALID_PASSWORD, MIN_VALID_AGE);
         assertThrows(RegistrationServiceException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void invalidLoginLengthRegistration() {
-        User user = new User(INVALID_LOGIN, VALID_PASSWORD, VALID_AGE);
+    void register_InvalidLoginLength_ThrowsRegistrationServiceException() {
+        User userEmptyLogin = new User(EMPTY_ROW, MIN_VALID_PASSWORD, MIN_VALID_AGE);
+        assertThrows(RegistrationServiceException.class,
+                () -> registrationService.register(userEmptyLogin));
+
+        User userInvalidShorterByFew
+                = new User(INVALID_LOGIN_SHORTER_BY_FEW_SYMBOLS, MIN_VALID_PASSWORD, MIN_VALID_AGE);
+        assertThrows(RegistrationServiceException.class,
+                () -> registrationService.register(userInvalidShorterByFew));
+
+        User userInvalidShorterByOne
+                = new User(INVALID_LOGIN_SHORTER_BY_ONE, MIN_VALID_PASSWORD, MIN_VALID_AGE);
+        assertThrows(RegistrationServiceException.class,
+                () -> registrationService.register(userInvalidShorterByOne));
+    }
+
+    @Test
+    void register_NullPassword_ThrowsRegistrationServiceException() {
+        User user = new User(MIN_VALID_LOGIN, null, MIN_VALID_AGE);
         assertThrows(RegistrationServiceException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void nullPasswordRegistration() {
-        User user = new User(VALID_LOGIN, null, VALID_AGE);
+    void register_InvalidPasswordLength_ThrowsRegistrationServiceException() {
+        User userEmptyPassword = new User(MIN_VALID_LOGIN, EMPTY_ROW, MIN_VALID_AGE);
+        assertThrows(RegistrationServiceException.class,
+                () -> registrationService.register(userEmptyPassword));
+
+        User userInvalidShorterByFew = new User(MIN_VALID_LOGIN,
+                INVALID_PASS_SHORTER_BY_FEW_SYMBOLS, MIN_VALID_AGE);
+        assertThrows(RegistrationServiceException.class,
+                () -> registrationService.register(userInvalidShorterByFew));
+
+        User userInvalidShorterByOne = new User(MIN_VALID_LOGIN,
+                INVALID_PASS_SHORTER_BY_ONE, MIN_VALID_AGE);
+        assertThrows(RegistrationServiceException.class,
+                () -> registrationService.register(userInvalidShorterByOne));
+    }
+
+    @Test
+    void register_NullAge_ThrowsRegistrationServiceException() {
+        User user = new User(MIN_VALID_LOGIN, MIN_VALID_PASSWORD, null);
         assertThrows(RegistrationServiceException.class, () -> registrationService.register(user));
     }
 
     @Test
-    void invalidPasswordLengthRegistration() {
-        User user = new User(VALID_LOGIN, INVALID_PASSWORD, VALID_AGE);
-        assertThrows(RegistrationServiceException.class, () -> registrationService.register(user));
-    }
+    void register_InvalidAge_ThrowsRegistrationServiceException() {
+        User userInvalidShorterByFew
+                = new User(MIN_VALID_LOGIN, MIN_VALID_PASSWORD, INVALID_AGE_LESS_BY_FEW_SYMBOLS);
+        assertThrows(RegistrationServiceException.class,
+                () -> registrationService.register(userInvalidShorterByFew));
 
-    @Test
-    void nullAgeRegistration() {
-        User user = new User(VALID_LOGIN, VALID_PASSWORD, null);
-        assertThrows(RegistrationServiceException.class, () -> registrationService.register(user));
-    }
-
-    @Test
-    void invalidAgeRegistration() {
-        User user = new User(VALID_LOGIN, VALID_PASSWORD, INVALID_AGE);
-        assertThrows(RegistrationServiceException.class, () -> registrationService.register(user));
+        User userInvalidShorterByOne
+                = new User(MIN_VALID_LOGIN, MIN_VALID_PASSWORD, INVALID_AGE_LESS_BY_ONE);
+        assertThrows(RegistrationServiceException.class,
+                () -> registrationService.register(userInvalidShorterByOne));
     }
 }
