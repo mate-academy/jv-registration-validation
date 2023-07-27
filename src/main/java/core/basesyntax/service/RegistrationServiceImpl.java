b4867b6711560c209fbfca506dd631762c8af297
literandltx <romanskovronn@gmail.com>
2023-07-27 12:12:09 +0300

    refactored: exception, added new var, renaming

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -4,28 +4,30 @@ import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.exception.RegistrationException;
-import core.basesyntax.exception.UserNullPointerException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int DEFAULT_PASSWORD_LENGTH = 6;
+    private static final int DEFAULT_LOGIN_LENGTH = 6;
+    private static final int DEFAULT_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
         if (user == null || user.getLogin() == null || user.getPassword() == null) {
-            throw new UserNullPointerException("Cannot apply null value");
+            throw new RegistrationException("Cannot apply null value");
         }
         if (Storage.people.contains(user)) {
             throw new RegistrationException("User is already registered");
         }
-        if (user.getAge() < 18) {
+        if (user.getAge() < DEFAULT_AGE) {
             throw new RegistrationException("Cannot register, user age is less than 18");
         }
-        if (user.getLogin().length() < 6) {
+        if (user.getLogin().length() < DEFAULT_LOGIN_LENGTH) {
             throw new RegistrationException("Cannot register, user login: \""
                     + user.getLogin() + "\" is less than 6 length");
         }
-        if (user.getPassword().length() < 6) {
+        if (user.getPassword().length() < DEFAULT_PASSWORD_LENGTH) {
             throw new RegistrationException("Cannot register, user password: \""
                     + user.getPassword() + "\" is less than 6 length");
         }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -2,7 +2,6 @@ package core.basesyntax.service;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.exception.RegistrationException;
-import core.basesyntax.exception.UserNullPointerException;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -26,7 +25,102 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void name() {
+    void register_existedUser_NotOk() {
+        User user = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, DEFAULT_PASSWORD);
+
+        registrationService.register(user);
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_loginLength_ok() {
+        User userPasswordMinimumLength = setUpUser(DEFAULT_AGE, "login6", DEFAULT_PASSWORD);
+        User userPasswordEnoughLength = setUpUser(DEFAULT_AGE, "longlogin", DEFAULT_PASSWORD);
+
+        Assertions.assertEquals(userPasswordMinimumLength,
+                registrationService.register(userPasswordMinimumLength));
+        Assertions.assertEquals(userPasswordEnoughLength,
+                registrationService.register(userPasswordEnoughLength));
+    }
+
+    @Test
+    void register_loginLength_NotOk() {
+        User userNullLoginValue = setUpUser(DEFAULT_AGE, null, DEFAULT_PASSWORD);
+        User userSmallLengthLogin = setUpUser(DEFAULT_AGE, "login", DEFAULT_PASSWORD);
+        User userBlankLoginSmallLength = setUpUser(DEFAULT_AGE, "     ", DEFAULT_PASSWORD);
+        User userBlankLoginMinimumLength = setUpUser(DEFAULT_AGE, "      ", DEFAULT_PASSWORD);
+        User userBlankLoginEnoughLength = setUpUser(DEFAULT_AGE, "             ", DEFAULT_PASSWORD);
+
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userNullLoginValue));
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userSmallLengthLogin));
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userBlankLoginSmallLength));
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userBlankLoginMinimumLength));
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userBlankLoginEnoughLength));
+    }
+
+    @Test
+    void register_passwordLength_Ok() {
+        User userMinimumLength = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, "passwr");
+        User userPasswordEnoughLength = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, "password");
+
+        Assertions.assertEquals(userMinimumLength, registrationService.register(userMinimumLength));
+        Assertions.assertEquals(userPasswordEnoughLength,
+                registrationService.register(userPasswordEnoughLength));
+    }
+
+    @Test
+    void register_passwordLength_NotOk() {
+        User userNullPasswordValue = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, null);
+        User userSmallLengthPassword = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, "pass");
+        User userBlankPasswordSmallLength = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, "    ");
+        User userBlankPasswordMinimumLength = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, "      ");
+        User userBlankPasswordEnoughLength = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, "             ");
+
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userNullPasswordValue));
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userSmallLengthPassword));
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userBlankPasswordSmallLength));
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userBlankPasswordMinimumLength));
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userBlankPasswordEnoughLength));
+    }
+
+    @Test
+    void register_userAge_Ok() {
+        User userMinimumFitAge = setUpUser(18, DEFAULT_LOGIN, DEFAULT_PASSWORD);
+        User userEnoughAge = setUpUser(48, DEFAULT_LOGIN, DEFAULT_PASSWORD);
+
+        Assertions.assertEquals(userMinimumFitAge, registrationService.register(userMinimumFitAge));
+        Assertions.assertEquals(userEnoughAge, registrationService.register(userEnoughAge));
+    }
+
+    @Test
+    void register_userAge_NotOk() {
+        User userAlmostEnoughAge = setUpUser(17, DEFAULT_LOGIN, DEFAULT_PASSWORD);
+        User userSmallAge = setUpUser(2, DEFAULT_LOGIN, DEFAULT_PASSWORD);
+        User userNegativeAge = setUpUser(-20, DEFAULT_LOGIN, DEFAULT_PASSWORD);
+
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userAlmostEnoughAge));
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userSmallAge));
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(userNegativeAge));
+    }
+
+    @Test
+    void register_userNull_Ok() {
+        Assertions.assertThrows(RegistrationException.class,
+                () -> registrationService.register(null));
     }
 
     private User setUpUser(int age, String login, String password) {
@@ -37,102 +131,6 @@ class RegistrationServiceImplTest {
         user.setPassword(password);
 
         return user;
-    }
-
-    @Test
-    void register_existedUser_NotOk() {
-        User user1 = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, DEFAULT_PASSWORD);
-
-        registrationService.register(user1);
-        Assertions.assertThrows(RegistrationException.class,
-                () -> registrationService.register(user1));
-    }
-
-    @Test
-    void register_loginLength_ok() {
-        User user1 = setUpUser(DEFAULT_AGE, "longlogin", DEFAULT_PASSWORD);
-        User user2 = setUpUser(DEFAULT_AGE, "login6", DEFAULT_PASSWORD);
-
-        Assertions.assertEquals(user1, registrationService.register(user1));
-        Assertions.assertEquals(user2, registrationService.register(user2));
-    }
-
-    @Test
-    void register_loginLength_NotOk() {
-        User user0 = setUpUser(DEFAULT_AGE, null, DEFAULT_PASSWORD);
-        User user1 = setUpUser(DEFAULT_AGE, "login", DEFAULT_PASSWORD);
-        User user2 = setUpUser(DEFAULT_AGE, "     ", DEFAULT_PASSWORD);
-        User user3 = setUpUser(DEFAULT_AGE, "      ", DEFAULT_PASSWORD);
-        User user4 = setUpUser(DEFAULT_AGE, "                 ", DEFAULT_PASSWORD);
-
-        Assertions.assertThrows(UserNullPointerException.class,
-                () -> registrationService.register(user0));
-        Assertions.assertThrows(RegistrationException.class,
-                () -> registrationService.register(user1));
-        Assertions.assertThrows(RegistrationException.class,
-                () -> registrationService.register(user2));
-        Assertions.assertThrows(RegistrationException.class,
-                () -> registrationService.register(user3));
-        Assertions.assertThrows(RegistrationException.class,
-                () -> registrationService.register(user4));
-    }
-
-    @Test
-    void register_passwordLength_Ok() {
-        User user1 = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, "passwr");
-        User user2 = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, "password");
-
-        Assertions.assertEquals(user1, registrationService.register(user1));
-        Assertions.assertEquals(user2, registrationService.register(user2));
-    }
-
-    @Test
-    void register_passwordLength_NotOk() {
-        User user0 = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, null);
-        User user1 = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, "pass");
-        User user2 = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, "    ");
-        User user3 = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, "      ");
-        User user4 = setUpUser(DEFAULT_AGE, DEFAULT_LOGIN, "                 ");
-
-        Assertions.assertThrows(UserNullPointerException.class,
-                () -> registrationService.register(user0));
-        Assertions.assertThrows(RegistrationException.class,
-                () -> registrationService.register(user1));
-        Assertions.assertThrows(RegistrationException.class,
-                () -> registrationService.register(user2));
-        Assertions.assertThrows(RegistrationException.class,
-                () -> registrationService.register(user3));
-        Assertions.assertThrows(RegistrationException.class,
-                () -> registrationService.register(user4));
-    }
-
-    @Test
-    void register_userAge_Ok() {
-        User user1 = setUpUser(18, DEFAULT_LOGIN, DEFAULT_PASSWORD);
-        User user2 = setUpUser(124, DEFAULT_LOGIN, DEFAULT_PASSWORD);
-
-        Assertions.assertEquals(user1, registrationService.register(user1));
-        Assertions.assertEquals(user2, registrationService.register(user2));
-    }
-
-    @Test
-    void register_userAge_NotOk() {
-        User user1 = setUpUser(17, DEFAULT_LOGIN, DEFAULT_PASSWORD);
-        User user2 = setUpUser(2, DEFAULT_LOGIN, DEFAULT_PASSWORD);
-        User user3 = setUpUser(-20, DEFAULT_LOGIN, DEFAULT_PASSWORD);
-
-        Assertions.assertThrows(RegistrationException.class,
-                () -> registrationService.register(user1));
-        Assertions.assertThrows(RegistrationException.class,
-                () -> registrationService.register(user2));
-        Assertions.assertThrows(RegistrationException.class,
-                () -> registrationService.register(user3));
-    }
-
-    @Test
-    void register_userNull_Ok() {
-        Assertions.assertThrows(UserNullPointerException.class,
-                () -> registrationService.register(null));
     }
 
 }
