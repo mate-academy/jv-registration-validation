b6d7e7f6a5415bba56f5efe30bdceebbbd7e585f
Artem Hlukhoman <giantking1231@gmail.com>
2023-08-02 12:56:58 +0300

    registration validation with bug

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,87 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int AGE_CHECK = 18;
+    private static final int LOGIN_LENGTH = 6;
+    private static final int PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
+    private int count = 0;
+    private boolean canContinue = false;
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RegistrationException("User information is empty");
+        }
+
+        if (user.getLogin() == null || user.getLogin().length() < LOGIN_LENGTH) {
+            throw new RegistrationException("User`s login is less then 6 characters");
+        }
+
+        if (user.getPassword() == null || user.getPassword().length() < PASSWORD_LENGTH) {
+            throw new RegistrationException("User`s password is less then 6 characters");
+        }
+
+        if (user.getAge() == null || user.getAge() < AGE_CHECK) {
+            throw new RegistrationException("User is too young - min age is 18!");
+        }
+
+        if (user.getAge() >= 100) {
+            throw new RegistrationException("User`s age is not real");
+        }
+
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("User with that login already exists");
+        }
+
+        if (user.getPassword().contains(" ")) {
+            throw new RegistrationException("Password should not contain WhiteSpaces");
+        }
+
+        if (user.getLogin().contains(" ")) {
+            throw new RegistrationException("Login should not contain WhiteSpaces");
+        }
+
+        for (Character chars: user.getLogin().toCharArray()) {
+            if (Character.isLetter(chars)) {
+                count++;
+            }
+            if (count == LOGIN_LENGTH) {
+                count = 0;
+                canContinue = true;
+                break;
+            }
+        }
+
+        if (!canContinue) {
+            throw new RegistrationException("Wrong login, you need at least 6 characters");
+        }
+
+        canContinue = false;
+
+        for (Character character: user.getPassword().toCharArray()) {
+            if (Character.isLetter(character)) {
+                count++;
+            }
+            if (count == PASSWORD_LENGTH) {
+                count = 0;
+                canContinue = true;
+                break;
+            }
+        }
+
+        if (!canContinue) {
+            throw new RegistrationException("Wrong password, you need at least 6 characters");
+        }
+
+        canContinue = false;
+
+        storageDao.add(user);
+
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,172 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.RegistrationException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static final String DEFAULT_LOGIN = "GiantKIng";
+    private static final String DEFAULT_PASSWORD = "GiantKIng";
+    private static final int DEFAULT_AGE = 18;
+    private static RegistrationService registrationService;
+    private static StorageDao storageDao;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        storageDao = new StorageDaoImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void nullValue_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(null);
+        });
+    }
+
+    @Test
+    void loginAlreadyExist_notOk() {
+        User user = createUser(DEFAULT_LOGIN, DEFAULT_PASSWORD, DEFAULT_AGE);
+        registrationService.register(user);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void loginIsTooShort_notOk() {
+        User user = createUser("DEFAU1234512", DEFAULT_PASSWORD, DEFAULT_AGE);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+        user.setLogin("Def");
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+        user.setLogin("");
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void passwordIsTooShort_notOk() {
+        User user = createUser(DEFAULT_LOGIN, "qwe", DEFAULT_AGE);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+        user.setPassword("");
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void belowAge_notOk() {
+        User user = createUser(DEFAULT_LOGIN, DEFAULT_PASSWORD, 10);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void registrationComplete_ok() {
+        User user = createUser(DEFAULT_LOGIN, DEFAULT_PASSWORD, DEFAULT_AGE);
+        registrationService.register(user);
+        assertEquals(storageDao.get(user.getLogin()), user);
+    }
+
+    @Test
+    void passwordIsNull_notOk() {
+        User user = createUser(DEFAULT_LOGIN, null, DEFAULT_AGE);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void loginIsNull_notOk() {
+        User user = createUser(null, DEFAULT_PASSWORD, DEFAULT_AGE);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void ageIsNull_notOk() {
+        User user = createUser(DEFAULT_LOGIN, DEFAULT_PASSWORD, DEFAULT_AGE);
+        user.setAge(null);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void ageIsTooBig_notOk() {
+        User user = createUser(DEFAULT_LOGIN, DEFAULT_PASSWORD, Integer.MAX_VALUE);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void ageIsNegative_notOk() {
+        User user = createUser(DEFAULT_LOGIN, DEFAULT_PASSWORD, -10);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void wrongUserLogin_NotOk() {
+        User userNotEnoughChars = createUser("giant1234223412531", DEFAULT_PASSWORD, DEFAULT_AGE);
+        User userWithSpaces = createUser("                ", DEFAULT_PASSWORD, DEFAULT_AGE);
+        User userOkButSpaces = createUser("giantking   123", DEFAULT_PASSWORD, DEFAULT_AGE);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(userNotEnoughChars);
+        });
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(userWithSpaces);
+        });
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(userOkButSpaces);
+        });
+    }
+
+    @Test
+    void wrongUserPassword_NotOk() {
+        User userOnlySpaces = createUser(DEFAULT_LOGIN, "              ", DEFAULT_AGE);
+        User userOkButSpaces = createUser(DEFAULT_LOGIN, "giantk    123", DEFAULT_AGE);
+        User userNotEnoughChars = createUser(DEFAULT_LOGIN, "giant1234124", DEFAULT_AGE);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(userOkButSpaces);
+        });
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(userOnlySpaces);
+        });
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(userNotEnoughChars);
+        });
+    }
+
+    private User createUser(String login, String password, int age) {
+        User user = new User();
+        user.setLogin(login);
+        user.setPassword(password);
+        user.setAge(age);
+        return user;
+    }
+}
