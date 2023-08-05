97d9bb46a77c0edba5f25509d2603b7ea10971ba
Kovaev <eugenkoval1010@gmail.com>
2023-08-05 16:00:49 +0300

    created JUnit tests

---
+++ src/main/java/core/basesyntax/exception/DataValidationException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exception;
+
+public class DataValidationException extends RuntimeException {
+    public DataValidationException(String message) {
+        super(message);
+    }
+}
