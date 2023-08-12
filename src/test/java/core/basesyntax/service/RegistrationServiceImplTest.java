250c3288a0e08dc9de5c4a54c598168e60f6e9d4
silchenko-arsen <silchenkoarsen@gmail.com>
2023-08-12 17:55:45 +0300

    add setUp

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import core.basesyntax.exception.NotValidUserException;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
@@ -23,8 +24,15 @@ class RegistrationServiceImplTest {
 
     @BeforeAll
     static void beforeAll() {
-        user = new User();
         registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setPassword(VALID_PASSWORD);
+        user.setAge(VALID_AGE);
+        user.setLogin(VALID_LOGIN);
     }
 
     @Test
@@ -34,8 +42,6 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_loginNull_notOk() {
-        user.setPassword(VALID_PASSWORD);
-        user.setAge(VALID_AGE);
         user.setLogin(INVALID_LOGIN_NULL);
         assertThrows(NotValidUserException.class, () -> registrationService.register(user));
     }
@@ -43,23 +49,17 @@ class RegistrationServiceImplTest {
     @Test
     void register_passwordNull_notOk() {
         user.setPassword(INVALID_PASSWORD_NULL);
-        user.setAge(VALID_AGE);
-        user.setLogin(VALID_LOGIN);
         assertThrows(NotValidUserException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_ageNull_notOk() {
-        user.setPassword(VALID_PASSWORD);
         user.setAge(INVALID_AGE_NULL);
-        user.setLogin(VALID_LOGIN);
         assertThrows(NotValidUserException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_shortLoginLength_notOk() {
-        user.setPassword(VALID_PASSWORD);
-        user.setAge(VALID_AGE);
         user.setLogin(INVALID_LOGIN);
         assertThrows(NotValidUserException.class, () -> registrationService.register(user));
     }
@@ -67,24 +67,17 @@ class RegistrationServiceImplTest {
     @Test
     void register_shortPasswordLength_notOk() {
         user.setPassword(INVALID_PASSWORD);
-        user.setAge(VALID_AGE);
-        user.setLogin(VALID_LOGIN);
         assertThrows(NotValidUserException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_invalidAge_notOk() {
         user.setAge(INVALID_AGE);
-        user.setPassword(VALID_PASSWORD);
-        user.setLogin(VALID_LOGIN);
         assertThrows(NotValidUserException.class, () -> registrationService.register(user));
     }
 
     @Test
     void register_suchLoginAlreadyExists_notOk() {
-        user.setAge(VALID_AGE);
-        user.setLogin(VALID_LOGIN);
-        user.setPassword(VALID_PASSWORD);
         assertThrows(NotValidUserException.class, () -> registrationService.register(user));
     }
 
