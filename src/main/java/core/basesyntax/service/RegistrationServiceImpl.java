8c5d3afe6d74b015e773c7ed1fb6e198bbafa570
Dmytro Zarudnyi <dmytro.zarudnyi@gmail.com>
2023-08-23 14:23:14 +0300

    maven errors changes

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -14,11 +14,13 @@ public class RegistrationServiceImpl implements RegistrationService {
     public User register(User user) {
         if (user == null) {
             throw new InvalidUserDataException("User should not be null!");
-        } else if (user.getLogin() == null || user.getLogin().length() < MIN_LOGIN_LENGTH) {
+        } else if (user.getLogin() == null
+                || user.getLogin().length() < MIN_LOGIN_LENGTH) {
             throw new InvalidUserDataException("Login length should be at least 6 characters!");
         } else if (storageDao.get(user.getLogin()) != null) {
             throw new InvalidUserDataException("User with this login already exists!");
-        } else if (user.getPassword() == null || user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+        } else if (user.getPassword() == null
+                || user.getPassword().length() < MIN_PASSWORD_LENGTH) {
             throw new InvalidUserDataException("Password length should be at least 6 characters!");
         } else if (user.getAge() == null || user.getAge() < MIN_AGE) {
             throw new InvalidUserDataException("User should not be younger than 18 years old!");
