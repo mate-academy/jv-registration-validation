35b4b27cb468537e01d5bcd16ba45a23b46513e6
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2023-08-23 16:51:00 -0500

    Fixed common mistakes: added testing of adge cases

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -12,7 +12,8 @@ import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
     private static RegistrationService registrationService;
-    private User testUser;
+    private User testUser1;
+    private User testUser2;
 
     @BeforeAll
     static void init() {
@@ -21,10 +22,15 @@ class RegistrationServiceImplTest {
 
     @BeforeEach
     void initUser() {
-        testUser = new User();
-        testUser.setAge(23);
-        testUser.setLogin("Login1");
-        testUser.setPassword("123456");
+        testUser1 = new User();
+        testUser1.setAge(23);
+        testUser1.setLogin("Login1");
+        testUser1.setPassword("123456");
+
+        testUser2 = new User();
+        testUser2.setAge(25);
+        testUser2.setLogin("Login2");
+        testUser2.setPassword("123458");
 
         Storage.people.clear();
         User user1 = new User();
@@ -62,83 +68,114 @@ class RegistrationServiceImplTest {
     @Test
     void register_nullPassword_notOK() {
         assertThrows(RegistrationException.class, () -> {
-            testUser.setPassword(null);
-            registrationService.register(testUser);
+            testUser1.setPassword(null);
+            registrationService.register(testUser1);
         });
     }
 
     @Test
     void register_nullLogin_notOK() {
         assertThrows(RegistrationException.class, () -> {
-            testUser.setLogin(null);
-            registrationService.register(testUser);
+            testUser1.setLogin(null);
+            registrationService.register(testUser1);
         });
     }
 
     @Test
-    void register_minAge_OK() {
-        testUser.setAge(18);
-        assertEquals(testUser, registrationService.register(testUser));
+    void register_validAge_OK() {
+        testUser1.setAge(18);
+        assertEquals(testUser1, registrationService.register(testUser1));
+
+        testUser2.setAge(31);
+        assertEquals(testUser2, registrationService.register(testUser2));
     }
 
     @Test
     void register_lowAge_notOK() {
         assertThrows(RegistrationException.class, () -> {
-            testUser.setAge(5);
-            registrationService.register(testUser);
+            testUser1.setAge(17);
+            registrationService.register(testUser1);
+        });
+
+        testUser2.setAge(-1);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(testUser2);
         });
     }
 
     @Test
-    void register_minLoginAndPasswordLength_OK() {
-        assertEquals(testUser, registrationService.register(testUser));
+    void register_validLogin_OK() {
+        testUser1.setLogin("sixLet");
+        assertEquals(testUser1, registrationService.register(testUser1));
+
+        testUser2.setLogin("tenLetters");
+        assertEquals(testUser2, registrationService.register(testUser2));
+    }
+
+    @Test
+    void register_validPassword_OK() {
+        testUser1.setPassword("123456");
+        assertEquals(testUser1, registrationService.register(testUser1));
+
+        testUser2.setPassword("12345678987654321");
+        assertEquals(testUser2, registrationService.register(testUser2));
     }
 
     @Test
     void register_shortLogin_notOK() {
         assertThrows(RegistrationException.class, () -> {
-            testUser.setLogin("Login");
-            registrationService.register(testUser);
+            testUser1.setLogin("Login");
+            registrationService.register(testUser1);
+        });
+
+        testUser2.setLogin("Log");
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(testUser2);
         });
     }
 
     @Test
     void register_shortPassword_notOK() {
         assertThrows(RegistrationException.class, () -> {
-            testUser.setPassword("12345");
-            registrationService.register(testUser);
+            testUser1.setPassword("12345");
+            registrationService.register(testUser1);
+        });
+
+        testUser2.setPassword("13");
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(testUser2);
         });
     }
 
     @Test
     void register_emptyLogin_notOK() {
         assertThrows(RegistrationException.class, () -> {
-            testUser.setLogin("");
-            registrationService.register(testUser);
+            testUser1.setLogin("");
+            registrationService.register(testUser1);
         });
     }
 
     @Test
     void register_emptyPassword_notOK() {
         assertThrows(RegistrationException.class, () -> {
-            testUser.setPassword("");
-            registrationService.register(testUser);
+            testUser1.setPassword("");
+            registrationService.register(testUser1);
         });
     }
 
     @Test
     void register_registerUserWithSameLogin_notOK() {
         assertThrows(RegistrationException.class, () -> {
-            testUser.setLogin(Storage.people.get(1).getLogin());
-            registrationService.register(testUser);
+            testUser1.setLogin(Storage.people.get(1).getLogin());
+            registrationService.register(testUser1);
         });
     }
 
     @Test
     void register_validCriteria_OK() {
-        testUser.setAge(41);
-        testUser.setLogin("BestUserEver");
-        testUser.setPassword("qwerty1234ytrewq");
-        assertEquals(testUser, registrationService.register(testUser));
+        testUser1.setAge(41);
+        testUser1.setLogin("BestUserEver");
+        testUser1.setPassword("qwerty1234ytrewq");
+        assertEquals(testUser1, registrationService.register(testUser1));
     }
 }
