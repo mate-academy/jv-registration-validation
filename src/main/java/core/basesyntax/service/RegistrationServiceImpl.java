fd53896068358b13dbf95635f029dcb35293a4a1
Volodymyr Skvira <deruty13061999@gmail.com>
2023-09-04 15:52:51 +0300

    Implemented "register" method and added tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,53 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.InvalidInputException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int REQUIRED_AGE = 18;
+    private static final int MINIMUM_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
+    private final User user = new User();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new InvalidInputException("No data provided");
+        }
+        loginValidation();
+        passwordValidation();
+        ageValidation();
+        return storageDao.add(user);
+    }
+
+    private void loginValidation() {
+        if (user.getLogin() == null) {
+            throw new InvalidInputException("Please, fill all the required fields");
+        }
+        if (user.getLogin().length() < MINIMUM_LENGTH) {
+            throw new InvalidInputException("Your login should be at least 6 characters long");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new InvalidInputException("User already exists");
+        }
+    }
+
+    private void passwordValidation() {
+        if (user.getPassword() == null) {
+            throw new InvalidInputException("Please, fill all the required fields");
+        }
+        if (user.getPassword().length() < MINIMUM_LENGTH) {
+            throw new InvalidInputException("Your password should be at least 6 characters long");
+        }
+    }
+
+    private void ageValidation() {
+        if (user.getAge() == null) {
+            throw new InvalidInputException("Please, fill every required field");
+        }
+        if (user.getAge() < REQUIRED_AGE) {
+            throw new InvalidInputException("You should be at least 18 years old to register");
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,78 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.InvalidInputException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private RegistrationServiceImpl registator = new RegistrationServiceImpl();
+    private User user = new User();
+
+    @BeforeEach
+    void setUpDefaultUser() {
+        user.setLogin("GhostVoice");
+        user.setPassword("123456");
+        user.setAge(19);
+        Storage.PEOPLE.add(user);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.PEOPLE.clear();
+    }
+
+    @Test
+    void register_NullUser_NotOk() {
+        assertThrows(InvalidInputException.class, () -> registator.register(null));
+    }
+
+    @Test
+    void register_NullAge_NotOk() {
+        user.setAge(null);
+        assertThrows(InvalidInputException.class, () -> registator.register(user));
+    }
+
+    @Test
+    void register_NullLogin_NotOk() {
+        user.setLogin(null);
+        assertThrows(InvalidInputException.class, () -> registator.register(user));
+    }
+
+    @Test
+    void register_NullPassword_NotOk() {
+        user.setPassword(null);
+        assertThrows(InvalidInputException.class, () -> registator.register(user));
+    }
+
+    @Test
+    void register_ShortLogin_NotOk() {
+        user.setLogin("Wally");
+        assertThrows(InvalidInputException.class, () -> registator.register(user));
+    }
+
+    @Test
+    void register_ShortPassword_NotOk() {
+        user.setPassword("Milky");
+        assertThrows(InvalidInputException.class, () -> registator.register(user));
+    }
+
+    @Test
+    void register_YoungAge_NotOk() {
+        user.setAge(12);
+        assertThrows(InvalidInputException.class, () -> registator.register(user));
+    }
+
+    @Test
+    void register_LoginAlreadyExists_NotOk() {
+        User existingUser = new User();
+        user.setLogin("GhostVoice");
+        user.setPassword("123456");
+        user.setAge(19);
+        assertThrows(InvalidInputException.class, () -> registator.register(user));
+    }
+}
