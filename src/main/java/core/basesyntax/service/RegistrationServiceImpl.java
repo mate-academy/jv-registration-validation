c1ff179886c2a8c5db005a8c889f438e651f7edd
Teract10s <trs.leonov@gmail.com>
2023-09-04 12:28:13 +0300

    added userNull check

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -19,20 +19,35 @@ public class RegistrationServiceImpl implements RegistrationService {
     }
 
     private void userCheck(User user) {
+        if (user == null) {
+            throw new RegistrationException("User can't be null");
+        }
+        loginCheck(user);
+        passwordCheck(user);
+        ageCheck(user);
+    }
+
+    private void loginCheck(User user) {
         if (user.getLogin() == null) {
             throw new RegistrationException("Login can't be null");
-        }
-        if (user.getPassword() == null) {
-            throw new RegistrationException("Password can't be null");
-        }
-        if (user.getAge() == null) {
-            throw new RegistrationException("Age can't be null");
         }
         if (user.getLogin().length() < MIN_LENGTH_FOR_LOGIN_PASSWORD) {
             throw new RegistrationException("Login must to have at least 6 characters");
         }
+    }
+
+    private void passwordCheck(User user) {
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Password can't be null");
+        }
         if (user.getPassword().length() < MIN_LENGTH_FOR_LOGIN_PASSWORD) {
             throw new RegistrationException("Password must to have at least 6 characters");
+        }
+    }
+
+    private void ageCheck(User user) {
+        if (user.getAge() == null) {
+            throw new RegistrationException("Age can't be null");
         }
         if (user.getAge() < 18) {
             throw new RegistrationException("User must to be adult");
