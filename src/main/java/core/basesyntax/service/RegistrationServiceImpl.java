68cf2d68358893a01ca0307a2158effc00cab857
Bohdan Maksymenko <bogdan.maksimenko.2002@gmail.com>
2023-09-04 12:35:04 +0300

    implement user registration service with tests.87%

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,66 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.exceptions.ValidationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MINIMAL_AGE = 18;
+    private static final int MINIMAL_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        try {
+            if (user == null) {
+                throw new ValidationException("User can`t be null");
+            }
+            checkUserDataNull(user);
+            checkLengthOfData(user);
+            checkIsUserAdult(user);
+            isLoginInStorage(user);
+        } catch (ValidationException e) {
+            throw new RuntimeException(e);
+        }
+        storageDao.add(user);
+        return user;
+    }
+
+    private User isLoginInStorage(User user) throws ValidationException {
+        for (User userInStorage:Storage.PEOPLE) {
+            if (userInStorage.getLogin().equals(user.getLogin())) {
+                throw new ValidationException("User with this login name already exist");
+            }
+        }
+        return user;
+    }
+
+    private User checkIsUserAdult(User user) throws ValidationException {
+        if (user.getAge() < MINIMAL_AGE) {
+            throw new ValidationException("Age must equals or more than "
+                    + MINIMAL_AGE);
+        }
+        return user;
+    }
+
+    private User checkLengthOfData(User user) throws ValidationException {
+        if (user.getLogin().length() < MINIMAL_LENGTH) {
+            throw new ValidationException("Login length must be more than 6");
+        }
+        if (user.getPassword().length() < MINIMAL_LENGTH) {
+            throw new ValidationException("Password length must be more than 6");
+        }
+        return user;
+    }
+
+    private User checkUserDataNull(User user) throws ValidationException {
+        if (user.getLogin() == null) {
+            throw new ValidationException("No login found");
+        }
+        if (user.getPassword() == null) {
+            throw new ValidationException("No password found");
+        }
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -0,0 +1,107 @@
+package core.basesyntax;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationServiceImplTest {
+    private static final String VALID_LOGIN = "Username";
+    private static final String ANOTHER_VALID_LOGIN = "Username1";
+    private static final String ONE_MORE_LOGIN = "Username2";
+    private static final String VALID_PASSWORD = "Password";
+    private static final String UNVALID_LOGIN = "12345";
+    private static final String UNVALID_PASSWORD = "12345";
+    private static final int VALID_AGE = 18;
+    private static RegistrationService registrationService;
+    private static Storage storage;
+    private static User validUser;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        storage = new Storage();
+        validUser = new User();
+        validUser.setAge(VALID_AGE);
+        validUser.setLogin(VALID_LOGIN);
+        validUser.setPassword(VALID_PASSWORD);
+        validUser.setId((long) 12);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.PEOPLE.clear();
+    }
+
+    @Test
+    void userRegistration_Ok() {
+        registrationService.register(validUser);
+    }
+
+    @Test
+    void sizeStorageWithTwoCorrectUsers_Ok() {
+        User validUser2 = new User();
+        validUser2.setAge(VALID_AGE);
+        validUser2.setLogin(ONE_MORE_LOGIN);
+        validUser2.setPassword(VALID_PASSWORD);
+        validUser2.setId((long) 13);
+        registrationService.register(validUser2);
+        registrationService.register(validUser);
+        assertEquals(2, Storage.PEOPLE.size());
+    }
+
+    @Test
+    void checkIsUserLoginLength_notOk() {
+        User unvalidUserLogin = new User();
+        unvalidUserLogin.setAge(19);
+        unvalidUserLogin.setLogin(UNVALID_LOGIN);
+        unvalidUserLogin.setPassword(VALID_PASSWORD);
+        unvalidUserLogin.setId((long) 1);
+        assertThrows(RuntimeException.class,() -> registrationService
+                .register(unvalidUserLogin));
+    }
+
+    @Test
+    void checkIsUserPasswordLength_NotOk() {
+        User unvalidUserPassword = new User();
+        unvalidUserPassword.setAge(19);
+        unvalidUserPassword.setLogin(VALID_LOGIN);
+        unvalidUserPassword.setPassword(UNVALID_PASSWORD);
+        unvalidUserPassword.setId((long) 1);
+        assertThrows(RuntimeException.class,() -> registrationService
+                .register(unvalidUserPassword));
+    }
+
+    @Test
+    void checkIsUserAdult_NotOk() {
+        User unvalidUser1 = new User();
+        unvalidUser1.setAge(17);
+        unvalidUser1.setLogin(ONE_MORE_LOGIN);
+        unvalidUser1.setPassword(VALID_PASSWORD);
+        unvalidUser1.setId((long) 17);
+        assertThrows(RuntimeException.class,() -> registrationService.register(unvalidUser1));
+    }
+
+    @Test
+    void checkUserIsNull_NotOk() {
+        assertThrows(RuntimeException.class, () -> registrationService.register(null));
+    }
+
+    @Test
+    void userWithTheSameLoginInStorage_NotOk() {
+        registrationService.register(validUser);
+        User validUser2 = new User();
+        validUser2.setAge(VALID_AGE);
+        validUser2.setLogin(VALID_LOGIN);
+        validUser2.setPassword(VALID_PASSWORD);
+        validUser2.setId((long) 13);
+        assertThrows(RuntimeException.class,() -> registrationService.register(validUser2));
+    }
+}
