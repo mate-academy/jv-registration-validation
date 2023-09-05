4498b1bf7f8a746106c878a07f44e594c8c3059c
evgenkolner <evgen.kolner@gmail.com>
2023-09-05 17:28:02 +0300

    added solution1

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,43 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LENGTH = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        checkLogin(user);
+        checkPassword(user);
+        checkAge(user);
+        return storageDao.add(user);
+    }
+
+    public void checkLogin(User user) {
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Login can`t be null");
+        }
+        if (user.getLogin().isBlank() || user.getLogin().length() < MIN_LENGTH) {
+            throw new RegistrationException("Login must have " + MIN_LENGTH + "or more symbols");
+        }
+    }
+
+    public void checkPassword(User user) {
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Password can`t be null");
+        }
+        if (user.getPassword().isBlank() || user.getPassword().length() < MIN_LENGTH) {
+            throw new RegistrationException("Password must have " + MIN_LENGTH + "or more symbols");
+        }
+    }
+
+    public void checkAge(User user) {
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("Age can`t be less than " + MIN_AGE);
+        }
     }
 }
