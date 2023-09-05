19cd14340fa78309823992b5a3995d55929f194e
Parashchuk <Andry.parashchuk@gmail.com>
2023-09-05 14:22:29 +0200

    Implement register and test coverage

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -4,11 +4,43 @@ import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
+import java.util.Objects;
+
 public class RegistrationServiceImpl implements RegistrationService {
+    public static final int VALID_LOGIN_LENGTH = 6;
+    public static final int VALID_PASSWORD_LENGTH = 6;
+    public static final int VALID_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        Objects.requireNonNull(user);
+
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegisterException("User with " + user.getLogin() + " as a login already exist");
+        }
+
+        if (user.getLogin().length() < VALID_LOGIN_LENGTH) {
+            throw new RegisterException(
+                    "The login must have at least " + VALID_LOGIN_LENGTH
+                            + " symbols, but you have: "
+                            + user.getLogin().length());
+        }
+
+        if (user.getPassword().length() < VALID_PASSWORD_LENGTH) {
+            throw new RegisterException(
+                    "The password must have at least " + VALID_PASSWORD_LENGTH
+                            + " symbols, but you have: "
+                            + user.getPassword().length());
+        }
+
+        if(user.getAge().compareTo(VALID_AGE) < 0) {
+            throw new RegisterException(
+                    "The age must have at least " + VALID_AGE
+                            + " years old, but yours is : "
+                            + user.getAge());
+        }
+
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,89 @@
+package core.basesyntax.service;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private static final String VALID_LOGIN = "ValidLogin";
+    public static final int VALID_AGE = 20;
+    public static final String VALID_PASSWORD = "12345678";
+
+
+    private static RegistrationServiceImpl regService;
+    private User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        regService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setLogin(VALID_LOGIN);
+        user.setAge(VALID_AGE);
+        user.setPassword(VALID_PASSWORD);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void register_allParamsValid_ok() {
+        User actual = regService.register(user);
+        assertEquals(user, actual);
+    }
+
+    @Test
+    void register_nllUser_throwsException() {
+        user = null;
+        assertThrows(NullPointerException.class, () -> regService.register(user));
+    }
+
+    @Test
+    void register_nllLogin_throwsException() {
+        user.setLogin(null);
+        assertThrows(NullPointerException.class, () -> regService.register(user));
+    }
+
+    @Test
+    void register_nllAge_throwsException() {
+        user.setAge(null);
+        assertThrows(NullPointerException.class, () -> regService.register(user));
+    }
+
+    @Test
+    void register_nllPassword_throwsException() {
+        user.setPassword(null);
+        assertThrows(NullPointerException.class, () -> regService.register(user));
+    }
+
+    @Test
+    void register_invalidLogin_throwsException() {
+        user.setLogin("a");
+        assertThrows(RegisterException.class, () -> regService.register(user));
+    }
+
+    @Test
+    void register_invalidAge_throwsException() {
+        user.setAge(12);
+        assertThrows(RegisterException.class, () -> regService.register(user));
+    }
+
+    @Test
+    void register_invalidPassword_throwsException() {
+        user.setPassword("1234");
+        assertThrows(RegisterException.class, () -> regService.register(user));
+    }
+
+    @Test
+    void register_userAlreadyExist_throwsException() {
+        regService.register(user);
+        assertThrows(RegisterException.class, () -> regService.register(user));
+    }
+}
