bdaea5b6dc025f16e72975f2f3bc8ec5e89620ef
Mykyta Pavlov <pavlov2021mf12@student.karazin.ua>
2023-09-05 15:45:08 +0300

    implemented register method with TDD

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,67 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exceptions.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    public static final int MIN_LENGTH = 6;
+    public static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RegistrationException("User can't be null");
+        }
+        validateLogin(user.getLogin());
+        validatePassword(user.getPassword());
+        validateAge(user.getAge());
+        return storageDao.add(user);
+    }
+
+    private void validateAge(Integer age) {
+        if (age == null) {
+            throw new RegistrationException("age must be here");
+        }
+        if (age < MIN_AGE) {
+            throw new RegistrationException(
+                    "age should be at least %d, but was: %d"
+                            .formatted(MIN_AGE, age));
+        }
+    }
+
+    private void validatePassword(String password) {
+        if (password == null) {
+            throw new RegistrationException("password must be here");
+        }
+        if (password.length() < MIN_LENGTH) {
+            throw new RegistrationException(
+                    "password expected longer than: %d, but was: %d"
+                            .formatted(MIN_LENGTH, password.length()));
+        }
+        if (!password.trim().equals(password)) {
+            throw new RegistrationException(
+                    "password should be without whitespaces arround");
+        }
+    }
+
+    private void validateLogin(String login) {
+        if (login == null) {
+            throw new RegistrationException("login must be here");
+        }
+        if (login.length() < MIN_LENGTH) {
+            throw new RegistrationException(
+                    "login expected longer than: %d, but was: %d"
+                            .formatted(MIN_LENGTH, login.length()));
+        }
+        if (!login.trim().equals(login)) {
+            throw new RegistrationException("login should be without whitespaces arround");
+        }
+        if (storageDao.get(login) != null) {
+            throw new RegistrationException(
+                    "user with such login already exists: %s"
+                            .formatted(login));
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -0,0 +1,106 @@
+package core.basesyntax;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.exceptions.RegistrationException;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationServiceImplTest {
+    private RegistrationService registrationService;
+    //private Storage storage;
+
+    @BeforeEach
+    void setUp() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @AfterEach
+    void trimDown() {
+        Storage.PEOPLE.clear();
+    }
+
+    @Test
+    void register_nullUser_notOk() {
+        assertThrows(RegistrationException.class, () -> registrationService.register(null));
+        assertEquals(0, Storage.PEOPLE.size());
+    }
+
+    @Test
+    void register_nullLogin_notOk() {
+        User user = new User(null, "goodPassword", 20);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+        assertEquals(0, Storage.PEOPLE.size());
+    }
+
+    @Test
+    void register_invalidLoginLength_notOk() {
+        User user1 = new User("bad", "goodPassword", 20);
+        User user2 = new User("", "goodPassword", 20);
+        User user3 = new User("    NotCorrectLogin     ", "goodPassword", 20);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user1));
+        assertThrows(RegistrationException.class, () -> registrationService.register(user2));
+        assertThrows(RegistrationException.class, () -> registrationService.register(user3));
+        assertEquals(0, Storage.PEOPLE.size());
+    }
+
+    @Test
+    void register_nullPassword_notOk() {
+        User user = new User("goodLogin", null, 20);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+        assertEquals(0, Storage.PEOPLE.size());
+    }
+
+    @Test
+    void register_invalidPasswordLength_notOk() {
+        User user1 = new User("goodLogin", "passw", 20);
+        User user2 = new User("goodLogin", "", 20);
+        User user3 = new User("goodLogin", "    NotCorrectPassword     ", 20);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user1));
+        assertThrows(RegistrationException.class, () -> registrationService.register(user2));
+        assertThrows(RegistrationException.class, () -> registrationService.register(user3));
+        assertEquals(0, Storage.PEOPLE.size());
+    }
+
+    @Test
+    void register_nullAge_notOk() {
+        User user = new User("goodLogin", "goodPassword", null);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+        assertEquals(0, Storage.PEOPLE.size());
+    }
+
+    @Test
+    void register_invalidAge_notOk() {
+        User user1 = new User("goodLogin", "goodPassword", 0);
+        User user2 = new User("goodLogin", "goodPassword", -20);
+        User user3 = new User("goodLogin", "goodPassword", 17);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user1));
+        assertThrows(RegistrationException.class, () -> registrationService.register(user2));
+        assertThrows(RegistrationException.class, () -> registrationService.register(user3));
+        assertEquals(0, Storage.PEOPLE.size());
+    }
+
+    @Test
+    void register_existingUsersLogin_notOk() {
+        User user = new User("goodLogin", "goodPassword", 20);
+        Storage.PEOPLE.add(user);
+        User userToRegistrate = new User("goodLogin", "123456", 21);
+        assertThrows(RegistrationException.class,
+                () -> registrationService.register(userToRegistrate));
+        assertEquals(1, Storage.PEOPLE.size(), "User shouldn't be added to storage");
+    }
+
+    @Test
+    void register_validUser_ok() {
+        User userToRegistrate = new User("goodLogin", "goodPassword", 20);
+        User registeredUser = registrationService.register(userToRegistrate);
+        assertEquals(1, Storage.PEOPLE.size(), "User should be added to storage");
+        assertEquals(userToRegistrate, registeredUser);
+    }
+}
