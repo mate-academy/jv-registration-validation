c71ddc72f1620409a7fbc522dfa73a37f85fa4a6
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-09-06 17:20:57 +0300

    fixes added

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,8 +6,8 @@ import core.basesyntax.exception.ValidationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private static int MIN_LENGTH = 6;
-    private static int MIN_AGE_REQUIRED = 18;
+    private static final int MIN_LENGTH = 6;
+    private static final int MIN_AGE_REQUIRED = 18;
 
     private final StorageDao storageDao = new StorageDaoImpl();
 
