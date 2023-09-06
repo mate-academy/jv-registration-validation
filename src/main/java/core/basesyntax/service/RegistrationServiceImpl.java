a92c6c9c47a66f0a08164ae380eae9e26f71319d
Oleksandr Mykhailov <ososkdkc@gmai.com>
2023-09-06 17:47:33 +0300

    Added solution JUnit

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,32 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null) {
+            throw new InvalidUserException("Login can't be null");
+        }
+        if (user.getPassword() == null) {
+            throw new InvalidUserException("Password can't be null");
+        }
+
+        if (user.getAge() < MIN_AGE) {
+            throw new InvalidUserException("Not valid age: "
+                    + user.getAge()
+                    + ". Min allowed age is " + MIN_AGE);
+        }
+
+        if (user.getLogin().length() < 6 || user.getPassword().length() < 6) {
+            throw new InvalidUserException("Login and password must be at least 6 characters long");
+        }
+
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new InvalidUserException("User with this login already exists");
+        }
+
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -0,0 +1,133 @@
+package core.basesyntax;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import core.basesyntax.model.User;
+import core.basesyntax.service.InvalidUserException;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationServiceTest {
+    private static final RegistrationServiceImpl REG_SERVICE = new RegistrationServiceImpl();
+    private static final int MIN_AGE = 18;
+
+    @Test
+    public void register_validUser_successfulRegistration() {
+        User user = new User();
+        user.setLogin("username");
+        user.setPassword("password123");
+        user.setAge(20);
+
+        User registeredUser = REG_SERVICE.register(user);
+        assertNotNull(registeredUser);
+        assertEquals(user.getLogin(), registeredUser.getLogin());
+    }
+
+    @Test
+    public void register_existingUser_throwException() {
+        User user1 = new User();
+        user1.setLogin("username");
+        user1.setPassword("password123");
+        user1.setAge(20);
+
+        User user2 = new User();
+        user2.setLogin("username");
+        user2.setPassword("anotherpassword");
+        user2.setAge(25);
+
+        Exception exception = assertThrows(InvalidUserException.class, () -> {
+            REG_SERVICE.register(user1);
+            REG_SERVICE.register(user2);
+        });
+
+        String expectedMessage = "User with this login already exists";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void register_shortLogin_throwException() {
+        User user = new User();
+        user.setLogin("short");
+        user.setPassword("password123");
+        user.setAge(20);
+
+        Exception exception = assertThrows(InvalidUserException.class,
+                () -> REG_SERVICE.register(user));
+
+        String expectedMessage = "Login and password must be at least 6 characters long";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void register_shortPassword_throwException() {
+        User user = new User();
+        user.setLogin("username");
+        user.setPassword("short");
+        user.setAge(20);
+
+        Exception exception = assertThrows(InvalidUserException.class,
+                () -> REG_SERVICE.register(user));
+
+        String expectedMessage = "Login and password must be at least 6 characters long";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void register_incorrectAge_throwException() {
+        User user = new User();
+        user.setLogin("username");
+        user.setPassword("password123");
+        user.setAge(17);
+
+        Exception exception = assertThrows(InvalidUserException.class,
+                () -> REG_SERVICE.register(user));
+
+        String expectedMessage = "Not valid age: "
+                + user.getAge()
+                + ". Min allowed age is " + MIN_AGE;
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void register_nullLogin_throwException() {
+        User user = new User();
+        user.setLogin(null);
+        user.setPassword("password123");
+        user.setAge(18);
+
+        Exception exception = assertThrows(InvalidUserException.class,
+                () -> REG_SERVICE.register(user));
+
+        String expectedMessage = "Login can't be null";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void register_nullPassword_throwException() {
+        User user = new User();
+        user.setLogin("I am twinkerbell");
+        user.setPassword(null);
+        user.setAge(18);
+
+        Exception exception = assertThrows(InvalidUserException.class,
+                () -> REG_SERVICE.register(user));
+
+        String expectedMessage = "Password can't be null";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+}
