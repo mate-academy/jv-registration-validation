a5bdd81c7866a2218aba2d104b5739138be71e63
katym <katy.mozharovska@gmail.com>
2023-09-09 17:13:44 +0300

    tests implemented

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,14 +1,54 @@
 package core.basesyntax.service;
 
+import core.basesyntax.InvalidUserException;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LOGIN_LENGTH = 6;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        validateUser(user);
+        return storageDao.add(user);
+    }
+
+    private void validateUser(User user) {
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new InvalidUserException("User with login '" + user.getLogin() + "' already exists.");
+        }
+
+        if (user.getLogin() == null) {
+            throw new InvalidUserException("Login is required");
+        }
+
+        if (user.getLogin().length() < 6) {
+            throw new InvalidUserException("Invalid login '" + user.getLogin()
+                    +"'. Login must be at least '"
+                    + MIN_LOGIN_LENGTH + "' characters long.");
+        }
+
+        if (user.getPassword() == null) {
+            throw new InvalidUserException("Password is required");
+        }
+
+        if (user.getPassword().length() < 6) {
+            throw new InvalidUserException("Invalid password '" + user.getPassword()
+                    + "'. Password must be at least '"
+                    + MIN_PASSWORD_LENGTH + "' characters long.");
+        }
+
+        if (user.getAge() == null) {
+            throw new InvalidUserException("Age is required");
+        }
+
+        if (user.getAge() < 18 || user.getAge() > 2147483647) {
+            throw new InvalidUserException("Invalid age '" + user.getAge()
+                    + "'. User must be at least '" + MIN_AGE + "' years old.");
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceTest.java
@@ -0,0 +1,134 @@
+package core.basesyntax.service;
+
+import core.basesyntax.InvalidUserException;
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class RegistrationServiceTest {
+    private RegistrationService registrationService;
+    private StorageDao storageDao;
+
+    @BeforeEach
+    public void setUp() {
+        storageDao = new StorageDaoImpl();
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @AfterEach
+    void clearStorage() {
+        Storage.people.clear();
+    }
+
+    @Test
+    public void register_ValidUserWithBorderValues_SuccessfullyRegistered() {
+        User user = createUser("Lawson", "f1newb", 18);
+
+        User registeredUser = registrationService.register(user);
+
+        assertNotNull(registeredUser.getId());
+        Assertions.assertEquals(user.getLogin(), registeredUser.getLogin());
+        Assertions.assertEquals(user.getPassword(), registeredUser.getPassword());
+        Assertions.assertEquals(user.getAge(), registeredUser.getAge());
+    }
+
+    @Test
+    public void register_ValidUserWithEquivalentClass_SuccessfullyRegistered() {
+        User user = createUser("MaxVerstappen", "RedBullRacerNumber1", 25);
+
+        User registeredUser = registrationService.register(user);
+
+        assertNotNull(registeredUser.getId());
+        Assertions.assertEquals(user.getLogin(), registeredUser.getLogin());
+        Assertions.assertEquals(user.getPassword(), registeredUser.getPassword());
+        Assertions.assertEquals(user.getAge(), registeredUser.getAge());
+    }
+
+    @Test
+    public void register_MultipleUsers_SuccessfullyRegistered() {
+        User user1 = createUser("first user", "password", 18);
+        User user2 = createUser("second user", "password", 30);
+        
+        User registeredUser1 = registrationService.register(user1);
+        User registeredUser2 = registrationService.register(user2);
+        
+        assertNotNull(registeredUser1.getId());
+        assertNotNull(registeredUser2.getId());
+        Assertions.assertNotEquals(registeredUser1.getLogin(), registeredUser2.getLogin());
+    }
+
+    @Test
+    public void register_UserAlreadyExists_ThrowsException() {
+        User existingUser = createUser("existingUser", "validPassword", 30);
+        storageDao.add(existingUser);
+
+        User newUser = createUser("existingUser", "newPassword", 40);
+
+        Assertions.assertThrows(
+                InvalidUserException.class,
+                () -> registrationService.register(newUser)
+        );
+    }
+
+    @Test
+    public void register_UserWithoutLogin_ThrowsException() {
+        User user = createUser(null, "p@ssw0rd", 18);
+
+        Assertions.assertThrows(
+                InvalidUserException.class,
+                () -> registrationService.register(user)
+        );
+    }
+
+    @Test
+    public void register_UserWithShortLogin_ThrowsException() {
+        User user = createUser("Bob", "p@ssw0rd", 18);
+
+        Assertions.assertThrows(
+                InvalidUserException.class,
+                () -> registrationService.register(user)
+        );
+    }
+
+    @Test
+    public void register_UserWithoutPassword_ThrowsException() {
+        User user = createUser("Valid User", null, 18);
+
+        Assertions.assertThrows(
+                InvalidUserException.class,
+                () -> registrationService.register(user)
+        );
+    }
+
+    @Test
+    public void register_UserWithShortPassword_ThrowsException() {
+        User user = createUser("George", "123", 18);
+
+        Assertions.assertThrows(
+                InvalidUserException.class,
+                () -> registrationService.register(user)
+        );
+    }
+
+    @Test
+    public void register_TooYoungUser_ThrowsException() {
+        User user = createUser("Charles", "p@ssw0rd", 17);
+
+        Assertions.assertThrows(
+                InvalidUserException.class,
+                () -> registrationService.register(user)
+        );
+    }
+
+    private User createUser(String login, String password, int age) {
+        User user = new User();
+        user.setLogin(login);
+        user.setPassword(password);
+        user.setAge(age);
+        return user;
+    }
+}
