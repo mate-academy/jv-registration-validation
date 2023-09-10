8fdff11117471d8b20254c80d5dae1629b68a45c
Petro Smoliar <smp22pl@gmail.com>
2023-09-10 16:23:16 +0200

    implemented method register implemented tests for class RegistrationServiceImpl

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,14 +1,50 @@
 package core.basesyntax.service;
 
+import core.basesyntax.dao.AuthenticationException;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_NUMBER_OF_CHARACTERS = 6;
+    private static final int MINIMUM_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        checkNullPointerException(user);
+        checkAuthenticationException(user);
+        storageDao.add(user);
+        return user;
+    }
+
+    private void checkAuthenticationException(User user) {
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new AuthenticationException("A user with this login already exists");
+        }
+        if (user.getLogin().length() < MIN_NUMBER_OF_CHARACTERS) {
+            throw new AuthenticationException("Login less than 6 characters");
+        }
+        if (user.getPassword().length() < MIN_NUMBER_OF_CHARACTERS) {
+            throw new AuthenticationException("Password less than 6 characters");
+        }
+        if (user.getAge() < MINIMUM_AGE) {
+            throw new AuthenticationException("Age less than 18 years");
+        }
+    }
+
+    private void checkNullPointerException(User user) {
+        if (user == null) {
+            throw new NullPointerException("User == null");
+        }
+        if (user.getAge() == null) {
+            throw new NullPointerException("Age == null");
+        }
+        if (user.getLogin() == null) {
+            throw new NullPointerException("Login == null");
+        }
+        if (user.getPassword() == null) {
+            throw new NullPointerException("Password == null");
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,175 @@
+
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.dao.AuthenticationException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private final RegistrationService registrationService = new RegistrationServiceImpl();
+
+    @Test
+    void userContent_ok() {
+        User user1 = new User();
+        user1.setLogin("User11");
+        user1.setPassword("password");
+        user1.setAge(18);
+        User user2 = new User();
+        user2.setLogin("User22");
+        user2.setPassword("password");
+        user2.setAge(18);
+        User actual = registrationService.register(user1);
+        User expectation = user1;
+        assertEquals(expectation, actual, "expectation user" + expectation.getLogin()
+                + "but received by the user" + actual.getLogin());
+        actual = registrationService.register(user2);
+        expectation = user2;
+        assertEquals(expectation, actual, "expectation " + expectation
+                + "but received by the " + actual);
+
+    }
+
+    @Test
+    void userLogin_notOk() {
+        User user1 = new User();
+        user1.setLogin("1234");
+        user1.setPassword("password");
+        user1.setAge(18);
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        }, "Login less than 6 characters");
+        user1.setLogin("$$$$$");
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        }, "Login less than 6 characters");
+        user1.setLogin("///");
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        }, "Login less than 6 characters");
+        user1.setLogin(".");
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        });
+        user1.setLogin(" ");
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        }, "Login less than 6 characters");
+    }
+
+    @Test
+    void userExistingLogin_notOk() {
+        User user1 = new User();
+        user1.setLogin("SuperUser");
+        user1.setPassword("password");
+        user1.setAge(18);
+        registrationService.register(user1);
+        User user2 = new User();
+        user2.setLogin("SuperUser");
+        user2.setPassword("password2");
+        user2.setAge(22);
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user2);
+        }, "A user with this login already exists");
+    }
+
+    @Test
+    void userPassword_notOk() {
+        User user1 = new User();
+        user1.setLogin("SuperUser");
+        user1.setPassword(" ");
+        user1.setAge(18);
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        }, "Password less than 6 characters");
+        user1.setPassword("$$$$");
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        }, "Password less than 6 characters");
+        user1.setPassword("///");
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        }, "Password less than 6 characters");
+        user1.setPassword(".");
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        }, "Password less than 6 characters");
+        user1.setPassword(" ");
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        }, "Password less than 6 characters");
+    }
+
+    @Test
+    void userAge_notOk() {
+        User user1 = new User();
+        user1.setLogin("SuperUser");
+        user1.setPassword("111111");
+        user1.setAge(17);
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        }, "Age less than 18 years");
+        user1.setAge(0);
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        }, "Age less than 18 years");
+        user1.setAge(-20);
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        }, "Age less than 18 years");
+    }
+
+    @Test
+    void nullLogin_notOk() {
+        User user1 = new User();
+        user1.setLogin(null);
+        user1.setPassword("111111");
+        user1.setAge(17);
+        assertThrows(NullPointerException.class, () -> {
+            registrationService.register(user1);
+        });
+    }
+
+    @Test
+    void nullPassword_notOk() {
+        User user1 = new User();
+        user1.setLogin("UserSup");
+        user1.setPassword(null);
+        user1.setAge(17);
+        assertThrows(NullPointerException.class, () -> {
+            registrationService.register(user1);
+        });
+    }
+
+    @Test
+    void nullAge_notOk() {
+        User user1 = new User();
+        user1.setLogin("UserSup");
+        user1.setPassword("123456");
+        user1.setAge(null);
+        assertThrows(NullPointerException.class, () -> {
+            registrationService.register(user1);
+        });
+    }
+
+    @Test
+    void nullUser_notOk() {
+        User user1 = null;
+        assertThrows(NullPointerException.class, () -> {
+            registrationService.register(user1);
+        });
+    }
+
+    @Test
+    void maxValueIntAge_notOk() {
+        User user1 = new User();
+        user1.setLogin("UserSup");
+        user1.setPassword("123456");
+        user1.setAge(Integer.MAX_VALUE + 1);
+        assertThrows(AuthenticationException.class, () -> {
+            registrationService.register(user1);
+        });
+    }
+}
