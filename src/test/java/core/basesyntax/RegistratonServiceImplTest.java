1e6ffc9864c8abaf80ce3f05311712ef39e019a7
Oleh <popovich158@gmail.com>
2023-09-15 18:27:38 +0300

    updated test methods. Created few small test methods.

--- src/test/java/core/basesyntax/RegistratonServiceImplTest.java
+++ src/test/java/core/basesyntax/RegistratonServiceImplTest.java
@@ -26,15 +26,19 @@ public class RegistratonServiceImplTest {
     }
 
     @Test
-    void register_NotOk() {
+    void register_invalidLogin_NotOk() {
+        assertThrows(RegistrationException.class, () -> registrationService
+                .register(new User("user01", "password", 20)));
         assertThrows(RegistrationException.class, () -> registrationService
                 .register(new User(null, "password", 19)));
         assertThrows(RegistrationException.class, () -> registrationService
                 .register(new User("", "password", 19)));
         assertThrows(RegistrationException.class, () -> registrationService
                 .register(new User("short", "password", 20)));
-        assertThrows(RegistrationException.class, () -> registrationService
-                .register(new User("user01", "password", 20)));
+    }
+
+    @Test
+    void register_invalidPassword_NotOk() {
         assertThrows(RegistrationException.class, () -> registrationService
                 .register(new User("user06", null, 19)));
         assertThrows(RegistrationException.class, () -> registrationService
@@ -43,6 +47,10 @@ public class RegistratonServiceImplTest {
                 .register(new User("user678", "123", 20)));
         assertThrows(RegistrationException.class, () -> registrationService
                 .register(new User("user678", "12345", 20)));
+    }
+
+    @Test
+    void register_invalidAge_NotOk() {
         assertThrows(RegistrationException.class, () -> registrationService
                 .register(new User("user07", "password", null)));
         assertThrows(RegistrationException.class, () -> registrationService
@@ -52,23 +60,31 @@ public class RegistratonServiceImplTest {
     }
 
     @Test
-    void register_ok() {
+    void register_validLogin_Ok() {
         User userLoginLength6 = new User("user00", "password123", 30);
         User userLoginLength8 = new User("user0123", "password123", 30);
-        User userPasswordLength6 = new User("user12", "123456", 30);
-        User userpasswordLength8 = new User("user012345", "12345678", 30);
-        User userAge18 = new User("user33", "password", 18);
-        User userAgeGreater18 = new User("user22", "password", 80);
         registrationService.register(userLoginLength6);
         registrationService.register(userLoginLength8);
-        registrationService.register(userPasswordLength6);
-        registrationService.register(userpasswordLength8);
-        registrationService.register(userAge18);
-        registrationService.register(userAgeGreater18);
         assertTrue(Storage.PEOPLE.contains(userLoginLength6));
         assertTrue(Storage.PEOPLE.contains(userLoginLength8));
+    }
+
+    @Test
+    void register_validPassword_Ok() {
+        User userPasswordLength6 = new User("user12", "123456", 30);
+        User userPasswordLength8 = new User("user012345", "12345678", 30);
+        registrationService.register(userPasswordLength6);
+        registrationService.register(userPasswordLength8);
         assertTrue(Storage.PEOPLE.contains(userPasswordLength6));
-        assertTrue(Storage.PEOPLE.contains(userpasswordLength8));
+        assertTrue(Storage.PEOPLE.contains(userPasswordLength8));
+    }
+
+    @Test
+    void register_validAge_Ok() {
+        User userAge18 = new User("user33", "password", 18);
+        User userAgeGreater18 = new User("user22", "password", 80);
+        registrationService.register(userAge18);
+        registrationService.register(userAgeGreater18);
         assertTrue(Storage.PEOPLE.contains(userAge18));
         assertTrue(Storage.PEOPLE.contains(userAgeGreater18));
     }
