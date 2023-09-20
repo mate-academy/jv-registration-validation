9bb1881bcfca477bd3d2ec64a68ad69d2dde6385
Vlad Kucher <kuchervlad49@gmail.com>
2023-09-20 12:27:13 +0300

    fixed problems

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,13 +6,15 @@ import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_AGE = 18;
+    private static final int MIN_LOGIN_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
     private String login;
 
     @Override
     public User register(User user) {
-        if (user.getLogin() == null || user.getLogin().length() < 6) {
-            throw new RegistrationException("Login can't be null");
+        if (user.getLogin() == null || user.getLogin().length() < MIN_LOGIN_LENGTH) {
+            throw new RegistrationException("Login can't be null" +
+                    "And must be at least 6 characters");
         }
         if (user.getPassword() == null) {
             throw new RegistrationException("Password can't be null");
