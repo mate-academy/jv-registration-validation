3fce8ce0b9f831a47f50e26429979f89f7fffad3
Vlad Kucher <kuchervlad49@gmail.com>
2023-09-20 12:29:15 +0300

    mvn clean package done

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -7,16 +7,17 @@ import core.basesyntax.model.User;
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_AGE = 18;
     private static final int MIN_LOGIN_LENGTH = 6;
+    private static final int MIN_PASSWORD_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
     private String login;
 
     @Override
     public User register(User user) {
         if (user.getLogin() == null || user.getLogin().length() < MIN_LOGIN_LENGTH) {
-            throw new RegistrationException("Login can't be null" +
-                    "And must be at least 6 characters");
+            throw new RegistrationException("Login can't be null"
+                    + "And must be at least 6 characters");
         }
-        if (user.getPassword() == null) {
+        if (user.getPassword() == null || user.getPassword().length() < MIN_PASSWORD_LENGTH) {
             throw new RegistrationException("Password can't be null");
         }
         if (user.getAge() < MIN_AGE) {
