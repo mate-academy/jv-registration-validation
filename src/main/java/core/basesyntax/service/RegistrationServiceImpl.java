c88432c36241f658f9b178a98f1dfb3318d7e1c3
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-09-22 01:02:16 +0300

    added changes to the class RegistrationServiceImpl

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -7,20 +7,45 @@ import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
+    private static final int MIN_LOGIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_AGE = 18;
 
     @Override
     public User register(User user) {
-        // Check if a user with the same login already exists
-        if (storageDao.get(user.getLogin()) != null) {
+        validateUser(user);
+        checkExistingUser(user.getLogin());
+
+        return storageDao.add(user);
+    }
+
+    private void validateUser(User user) {
+        validateLogin(user.getLogin());
+        validatePassword(user.getPassword());
+        validateAge(user.getAge());
+    }
+
+    private void validateLogin(String login) {
+        if (login == null || login.length() < MIN_LOGIN_PASSWORD_LENGTH) {
+            throw new RegistrationException("Login must be at least 6 characters long.");
+        }
+    }
+
+    private void validatePassword(String password) {
+        if (password == null || password.length() < MIN_LOGIN_PASSWORD_LENGTH) {
+            throw new RegistrationException("Password must be at least 6 characters long.");
+        }
+    }
+
+    private void validateAge(Integer age) {
+        if (age == null || age < MIN_AGE) {
+            throw new RegistrationException("Age must be at least 18.");
+        }
+    }
+
+    private void checkExistingUser(String login) {
+        User existingUser = storageDao.get(login);
+        if (existingUser != null) {
             throw new RegistrationException("User with this login already exists.");
         }
-
-        // Check other registration criteria
-        if (user.getLogin().length() < 6 || user.getPassword().length() < 6 || user.getAge() < 18) {
-            throw new RegistrationException("Invalid user data.");
-        }
-
-        // Add the user to the storage if all criteria are met
-        return storageDao.add(user);
     }
 }
