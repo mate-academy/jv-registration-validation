a51a22ef1aeca51ade2322cf26dd5ba3057e2a37
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-09-22 00:29:12 +0300

    added changes to the class RegistrationServiceImpl

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,14 +1,27 @@
 package core.basesyntax.service;
 
+import core.basesyntax.RegistrationException;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        // Check if a user with the same login already exists
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("User with this login already exists.");
+        }
+
+        // Check other registration criteria
+        if (user.getLogin().length() < 6 || user.getPassword().length() < 6 || user.getAge() < 18) {
+            throw new RegistrationException("Invalid user data.");
+        }
+
+        // Add the user to the storage if all criteria are met
+        return storageDao.add(user);
     }
 }
