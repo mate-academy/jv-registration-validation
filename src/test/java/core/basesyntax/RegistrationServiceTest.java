176c1c6c71d3e444d458ea59a778b6752ec4b4be
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-09-24 13:40:55 +0300

    added changes to the class RegistrationServiceTest

--- src/test/java/core/basesyntax/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -1,12 +1,14 @@
 package core.basesyntax;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationService;
 import core.basesyntax.service.RegistrationServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class RegistrationServiceTest {
     private RegistrationService registrationService;
@@ -56,6 +58,7 @@ public class RegistrationServiceTest {
         registrationService.register(existingUser);
 
         User userWithSameLogin = new User("existinguser", "newpassword", 35);
-        assertThrows(RegistrationException.class, () -> registrationService.register(userWithSameLogin));
+        assertThrows(RegistrationException.class, () -> registrationService
+                .register(userWithSameLogin));
     }
 }
