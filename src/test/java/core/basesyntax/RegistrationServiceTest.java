b85255fd52503d27009fce50360095c7d6f9cc46
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-09-24 19:39:55 +0300

    added changes to the class RegistrationServiceTest

--- src/test/java/core/basesyntax/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
