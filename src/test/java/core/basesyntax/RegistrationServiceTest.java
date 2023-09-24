2819433f14b95714d32bfd8ac22a5c88804ff080
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-09-24 20:43:46 +0300

    added changes to the class RegistrationServiceTest

--- src/test/java/core/basesyntax/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -19,34 +19,75 @@ public class RegistrationServiceTest {
     }
 
     @Test
-    public void register_EmptyPassword_ThrowsException() {
-        User user = new User("validuser", "", MIN_AGE);
+    public void register_NullLogin_ThrowsException() {
+        User user = new User(null, "validpassword", MIN_AGE);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    public void register_ShortPassword_ThrowsException() {
-        User user = new User("validuser", "abc", MIN_AGE);
+    public void register_NullPassword_ThrowsException() {
+        User user = new User("validuser", null, MIN_AGE);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    public void register_FiveCharacterPassword_ThrowsException() {
-        User user = new User("validuser", "abcdf", MIN_AGE);
+    public void register_NullAge_ThrowsException() {
+        User user = new User("validuser", "validpassword", null);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    public void register_SixCharacterPassword_Success() throws RegistrationException {
-        User newUser = new User("validuser", "abcdef", MIN_AGE);
-        assertThrows(RegistrationException.class, () -> registrationService.register(newUser));
-
+    public void register_UserUnder18_ThrowsException() {
+        User user = new User("validuser", "validpassword", 17);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    public void register_EightCharacterPassword_Success() {
-        User user = new User("validuser", "abcdefgh", MIN_AGE);
+    public void register_UserOver18_Success() {
+        User user = new User("validuser", "validpassword", 19);
         User registeredUser = registrationService.register(user);
         assertNotNull(registeredUser.getId());
+    }
+
+    @Test
+    public void register_NegativeAge_ThrowsException() {
+        User user = new User("validuser", "validpassword", -5);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_EmptyLogin_ThrowsException() {
+        User user = new User("", "validpassword", MIN_AGE);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_ShortLogin_ThrowsException() {
+        User user = new User("u", "validpassword", MIN_AGE);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void register_NotValidPassword_NotOk() {
+        User userWithEmptyPassword = new User("validuser", "", MIN_AGE);
+        User userWithShortPassword = new User("validuser", "abc", MIN_AGE);
+        User userWithFiveCharacterPassword = new User("validuser", "abcdf", MIN_AGE);
+        assertThrows(RegistrationException.class, () -> registrationService
+                .register(userWithEmptyPassword));
+        assertThrows(RegistrationException.class, () -> registrationService
+                .register(userWithShortPassword));
+        assertThrows(RegistrationException.class, () -> registrationService
+                .register(userWithFiveCharacterPassword));
+    }
+
+    @Test
+    public void registerPassword_Ok() throws RegistrationException {
+        User registerPasswordWithSixCharacter = new User("validuser", "abcdef", MIN_AGE);
+        User registerPasswordWithEightCharacter = new User("validuser", "abcdefgh", MIN_AGE);
+        assertThrows(RegistrationException.class, () -> registrationService
+                .register(registerPasswordWithSixCharacter));
+        assertThrows(RegistrationException.class, () -> registrationService
+                .register(registerPasswordWithEightCharacter));
+
     }
 }
