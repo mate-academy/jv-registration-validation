c1f57b96a0428051d118bd4144202c8f4378a70b
Andrey Polezhaiev <polezhaievandrey28@gmail.com>
2023-09-24 12:32:44 +0300

    changing code according to mentor

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -9,139 +9,61 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
     private static StorageDao storageDao = new StorageDaoImpl();
     private final RegistrationService registrationService = new RegistrationServiceImpl();
-    private List<User> users = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
         Storage.people.clear();
-
-        User user1 = new User();
-        user1.setAge(17);
-        user1.setLogin("12345");
-        user1.setPassword("12345");
-
-        User user2 = new User();
-        user2.setAge(18);
-        user2.setLogin("123456");
-        user2.setPassword("123456");
 
         User user3 = new User();
         user3.setAge(19);
         user3.setLogin("1234567");
         user3.setPassword("1234567");
 
-        User user4 = new User();
-        user4.setAge(20);
-        user4.setLogin("12345678");
-        user4.setPassword("12345678");
-
-        storageDao.add(user1);
-        storageDao.add(user2);
         storageDao.add(user3);
-        storageDao.add(user4);
-
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-        users.add(user4);
     }
 
     @Test
-    void register_user_OK() {
+    void register_OK() {
         User user = new User();
-        user.setId(5L);
-        user.setAge(19);
-        user.setLogin("123456789");
-        user.setPassword("123456789");
-
-        User expected = user;
-        User actual = registrationService.register(user);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void register_login_OK() {
-        User user = new User();
-        user.setId(5L);
-        user.setAge(19);
-        user.setLogin("012345");
-        user.setPassword("012345");
-
-        Integer expected = user.getLogin().length();
-        Integer actual = registrationService.register(user).getLogin().length();
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void register_password_OK() {
-        User user = new User();
-        user.setId(5L);
-        user.setAge(19);
-        user.setLogin("012345");
-        user.setPassword("012345");
-
-        Integer expected = user.getPassword().length();
-        Integer actual = registrationService.register(user).getPassword().length();
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void register_age_18_OK() {
-        User user = new User();
-        user.setId(5L);
         user.setAge(18);
         user.setLogin("012345");
         user.setPassword("012345");
 
-        Integer expected = user.getAge();
-        Integer actual = registrationService.register(user).getAge();
+        User expectedUser = user;
+        User actualUser = registrationService.register(user);
+        assertEquals(expectedUser, actualUser);
+        Storage.people.clear();
 
-        assertEquals(expected, actual);
-    }
+        Integer expectedLoginLength = user.getLogin().length();
+        Integer actualLoginLength = registrationService.register(user).getLogin().length();
+        assertEquals(expectedLoginLength, actualLoginLength);
+        Storage.people.clear();
 
-    @Test
-    void register_age_25_OK() {
-        User user = new User();
-        user.setId(5L);
+        Integer expectedPasswordLength = user.getPassword().length();
+        Integer actualPasswordLength = registrationService.register(user).getPassword().length();
+        assertEquals(expectedPasswordLength, actualPasswordLength);
+        Storage.people.clear();
+
+        Integer expectedAge = user.getAge();
+        Integer actualAge = registrationService.register(user).getAge();
+        assertEquals(expectedAge, actualAge);
+        Storage.people.clear();
+
         user.setAge(25);
-        user.setLogin("012345");
-        user.setPassword("012345");
-
-        Integer expected = user.getAge();
-        Integer actual = registrationService.register(user).getAge();
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void register_null_OK() {
-        User user = new User();
-        user.setId(5L);
-        user.setAge(18);
-        user.setLogin("012345");
-        user.setPassword("012345");
-
-        User expected = user;
-        User actual = registrationService.register(user);
-
-        assertEquals(expected, actual);
+        expectedAge = user.getAge();
+        actualAge = registrationService.register(user).getAge();
+        assertEquals(expectedAge, actualAge);
     }
 
     @Test
     void register_user_notOK() {
         User user = new User();
-        user.setId(5L);
         user.setAge(19);
         user.setLogin("1234567");
         user.setPassword("1234567");
@@ -152,9 +74,8 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void register_login_notOK() {
+    void register_shortLogin_notOK() {
         User user = new User();
-        user.setId(5L);
         user.setAge(19);
         user.setLogin("12345");
         user.setPassword("1234567");
@@ -167,7 +88,6 @@ class RegistrationServiceImplTest {
     @Test
     void register_nullLogin_notOK() {
         User user = new User();
-        user.setId(5L);
         user.setAge(19);
         user.setLogin(null);
         user.setPassword("1234567");
@@ -180,7 +100,6 @@ class RegistrationServiceImplTest {
     @Test
     void register_password_notOK() {
         User user = new User();
-        user.setId(5L);
         user.setAge(19);
         user.setLogin("1234567");
         user.setPassword("12345");
@@ -193,7 +112,6 @@ class RegistrationServiceImplTest {
     @Test
     void register_nullPassword_notOK() {
         User user = new User();
-        user.setId(5L);
         user.setAge(19);
         user.setLogin("1234567");
         user.setPassword(null);
@@ -206,7 +124,6 @@ class RegistrationServiceImplTest {
     @Test
     void register_age_17_notOK() {
         User user = new User();
-        user.setId(5L);
         user.setAge(17);
         user.setLogin("1234567");
         user.setPassword("1234567");
@@ -219,7 +136,6 @@ class RegistrationServiceImplTest {
     @Test
     void register_negative_age_17_notOK() {
         User user = new User();
-        user.setId(5L);
         user.setAge(-17);
         user.setLogin("1234567");
         user.setPassword("1234567");
@@ -232,7 +148,6 @@ class RegistrationServiceImplTest {
     @Test
     void register_age_0_notOK() {
         User user = new User();
-        user.setId(5L);
         user.setAge(0);
         user.setLogin("1234567");
         user.setPassword("1234567");
@@ -245,7 +160,6 @@ class RegistrationServiceImplTest {
     @Test
     void register_nullAge_notOK() {
         User user = new User();
-        user.setId(5L);
         user.setAge(null);
         user.setLogin("1234567");
         user.setPassword("1234567");
