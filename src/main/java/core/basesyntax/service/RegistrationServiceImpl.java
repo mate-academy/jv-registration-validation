8647001f2a958b023362b7bf5dd3305faaf5e956
Chorniypistolet <volkivskiy95@gmail.com>
2023-09-27 10:02:18 +0300

    fixed uppercase

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,8 +5,8 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private static final int minAge = 18;
-    private static final int minLength = 6;
+    private static final int MIN_AGE = 18;
+    private static final int MIN_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -17,14 +17,14 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user.getPassword() == null) {
             throw new InvalidUserDataException("Password can't be null");
         }
-        if (user.getAge() < minAge) {
+        if (user.getAge() < MIN_AGE) {
             throw new InvalidUserDataException("Not valid age: "
-                    + user.getAge() + ". Min allowed age is " + minAge);
+                    + user.getAge() + ". Min allowed age is " + MIN_AGE);
         }
-        if (user.getPassword().length() < minLength) {
+        if (user.getPassword().length() < MIN_LENGTH) {
             throw new InvalidUserDataException("Password cant be less than 6");
         }
-        if (user.getLogin().length() < minLength) {
+        if (user.getLogin().length() < MIN_LENGTH) {
             throw new InvalidUserDataException("Login cant be less than 6");
         }
         return storageDao.add(user);
