65e2f50c7ea93153964a7e5095f94e9182eed6a8
Danial <danialbest55@gmail.com>
2023-09-27 10:52:22 +0300

    Third try Fixed all the errors listed above

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -3,44 +3,50 @@ package core.basesyntax.service;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    private RegistrationService registrationService = new RegistrationServiceImpl();
+    private RegistrationService registrationService;
+
+    @BeforeEach
+    void setUp() {
+        registrationService = new RegistrationServiceImpl();
+    }
 
     @Test
-    public void notEnoughAge_Exception_OK() {
+    void registerUserWithInsufficientAge_ThrowsRegistrationException() {
         User user = new User("Loginnir", "Password", 5);
         assertThrows(RegistrationException.class, () -> {
-            User actual = registrationService.register(user);
-        },"That massage mean that age is correct");
+            registrationService.register(user);
+        }, "Expected RegistrationException for user with insufficient age.");
     }
 
     @Test
-    void smallPassword_Exception_OK() {
+    void registerUserWithShortPassword_ThrowsRegistrationException() {
         User user = new User(null, "Pass", 19);
-        assertThrows(RegistrationException.class,() -> {
-            User actual = registrationService.register(user);
-        },"That massage mean that password large is correct");
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        }, "Expected RegistrationException for user with short password.");
     }
 
     @Test
-    void smallLogin_Exception_OK() {
+    void registerUserWithShortLogin_ThrowsRegistrationException() {
         User user = new User("Log", "Passwordius", 35);
-        assertThrows(RegistrationException.class,() -> {
-            User actual = registrationService.register(user);
-        },"That massage mean that Login large is correct");
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        }, "Expected RegistrationException for user with short login.");
     }
 
     @Test
-    void userAlreadyExist_Exception_OK() {
+    void registerUserWithDuplicateLogin_ThrowsRegistrationException() {
         User user = new User("Loginius", "Passwordius", 35);
         registrationService.register(user);
 
         User userWithSameLogin = new User("Loginius", "Passw1231", 25);
 
-        assertThrows(RegistrationException.class,() -> {
+        assertThrows(RegistrationException.class, () -> {
             registrationService.register(userWithSameLogin);
-        },"That massage mean that we cant find same person in List and that is good");
+        }, "Expected RegistrationException for user with duplicate login.");
     }
 }
