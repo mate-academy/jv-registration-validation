cedf39b52b3b62f9f5b1e7b99b43d87b31333092
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-10-12 14:39:37 +0300

    created a few methods for checking

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -9,6 +9,28 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RegistrationServiceException("User cannot be null");
+        }
+        if (user.getLogin() == null) {
+            throw new RegistrationServiceException("Login cannot be null");
+        }
+        if (user.getPassword() == null) {
+            throw new RegistrationServiceException("Password cannot be null");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationServiceException("The user is registered");
+        }
+        if (user.getLogin().length() < 6) {
+            throw new RegistrationServiceException("The login is too short");
+        }
+        if (user.getPassword().length() < 6) {
+            throw new RegistrationServiceException("The password is too short");
+        }
+        if (user.getAge() < 18) {
+            throw new RegistrationServiceException("User must be adult");
+        }
+        storageDao.add(user);
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,78 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService service;
+    private List<User> usersTest;
+    @BeforeAll
+    static void beforeAll() {
+        service = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        usersTest = new ArrayList<>();
+        usersTest.add(new User("mylogin", "ilovecats", 20));
+        usersTest.add(new User("maria", "Kuzmych", 23));
+        usersTest.add(new User("Lyubov", "Kos", 40));
+        usersTest.add(null);
+        usersTest.add(new User("Valentyna", "Dyachuk", 17));
+        usersTest.add(new User(null, "18081996", 26));
+        usersTest.add(new User("Rostyslav", null, 29));
+    }
+
+    @Test
+    void userIsNull_notOk() {
+         assertThrows(RegistrationServiceException.class, () -> service.register(usersTest.get(3)));
+    }
+
+    @Test
+    void userLoginIsNull_notOk() {
+        assertThrows(RegistrationServiceException.class, () -> service.register(usersTest.get(5)));
+    }
+
+    @Test
+    void userPasswordIsNull_notOk() {
+        assertThrows(RegistrationServiceException.class, () -> service.register(usersTest.get(6)));
+    }
+
+    @Test
+    void userLoginLessSix_notOk() {
+        assertThrows(RegistrationServiceException.class, () -> service.register(usersTest.get(1)));
+    }
+
+    @Test
+    void userPasswordLessSix_notOk() {
+        assertThrows(RegistrationServiceException.class, () -> service.register(usersTest.get(2)));
+    }
+
+    @Test
+    void userAgeLessEighteen_notOk() {
+        assertThrows(RegistrationServiceException.class, () -> service.register(usersTest.get(4)));
+    }
+
+    @Test
+    void userValid_isOk() {
+        User userLogin = service.register(usersTest.get(0));
+        assertNotNull(userLogin);
+    }
+
+    @Test
+    void userLogin() {
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Testing is over");
+    }
+}
