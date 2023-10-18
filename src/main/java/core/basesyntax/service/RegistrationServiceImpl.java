52295e0186eeb213b7c06df0bf7921b9e504f4b0
Alex-Solod <solodyhin_2@icloud.com>
2023-10-19 02:03:04 +0300

    Implemented method 'register', in class 'RegistrationServiceImpl' Created test methods: beforeAll null_User_NotOk null_Empty_Login_NotOk null_Empty_Password_NotOk null_Empty_Age_NotOk loginLessThen6Characters_NotOk passwordLessThen6Characters_NotOk register_DuplicateUser_NotOk userAgeIsAtLeast18YearsOld that check if there is no user with such login in the Storage yet, user's login is at least 6 characters, user's password is at least 6 characters, user's age is at least 18 years old.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,44 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LOGIN_LENGTH = 6;
+    private static final int MAX_PASSWORD_LENGTH = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+
+        if (user.getLogin() == null || user.getLogin().equals("")) {
+            throw new RegistrationException("The user is existed. Enter a valid login");
+        }
+
+        if (user.getPassword() == null || user.getPassword().equals("")) {
+            throw new RegistrationException("Enter valid password");
+        }
+
+        if (user.getAge() == null || user.getAge().equals("")) {
+            throw new RegistrationException("Enter valid age");
+        }
+
+        if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
+            throw new RegistrationException("Login cant be less then 6 characters");
+        }
+
+        if (user.getPassword().length() < MAX_PASSWORD_LENGTH) {
+            throw new RegistrationException("The password can't be less then 6 characters");
+        }
+
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("Your age can not be les then "
+                    + MIN_AGE);
+        }
+
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("User with the same login already exists");
+        }
+
+        storageDao.add(user);
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -0,0 +1,71 @@
+package core.basesyntax;
+
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationException;
+import core.basesyntax.service.RegistrationService;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @Test
+    void null_User_NotOk() {
+        User user = new User(null, null, null);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void null_Empty_Login_NotOk() {
+        User user = new User(null, "199jjdat", 19);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void null_Empty_Password_NotOk() {
+        User user = new User("bob1234", null, 19);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void null_Empty_Age_NotOk() {
+        User user = new User("bob1234", "199jjdat", null);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void loginLessThen6Characters_NotOk() {
+        User user = new User("b34", "199jjdat", 19);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void passwordLessThen6Characters_NotOk() {
+        User user = new User("bob1234", "199j", 19);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_DuplicateUser_NotOk() {
+        User user1 = new User("bob1234", "199jjdat", 19);
+        registrationService.register(user1);
+        User user2 = new User("bob1234", "134jj4at", 25);
+
+        assertThrows(RegistrationException.class, () -> registrationService.register(user2));
+    }
+
+    @Test
+    void userAgeIsAtLeast18YearsOld() {
+        User user = new User("bob1234", "199jjdat", 17);
+
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+}
