d9e3206858b133e30daa6386258c04cc7a820cd0
Oleksandr Voitenko <129169030+OlexVoit@users.noreply.github.com>
2023-10-23 13:15:42 +0200

    fixed the magic numbers

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -9,24 +9,28 @@ import core.basesyntax.model.User;
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_YEAR = 18;
     private static final int MIN_LENGTH = 6;
+    private static final String USER_NULL = "The user can't be Null";
+    private static final String USER_EXISTS = "User exists";
+    private static final String AGE_UNDER_18 = "Age - over 18 years old";
+    private static final String INCORRECT_LOGIN_OR_PASSWORD = "Login and password must contain "
+            + "at least 6 characters";
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
         if (user == null) {
-            throw new NullExceptionDuringRegistration("The user can't be Null");
+            throw new NullExceptionDuringRegistration(USER_NULL);
         }
         if (user.equals(storageDao.get(user.getLogin()))) {
-            throw new ExceptionDuringRegistration("User exists");
+            throw new ExceptionDuringRegistration(USER_EXISTS);
         }
 
         if (user.getAge() < MIN_YEAR) {
-            throw new ExceptionDuringRegistration("Age - over 18 years old");
+            throw new ExceptionDuringRegistration(AGE_UNDER_18);
         }
         if (user.getLogin() == null || user.getLogin().length() < MIN_LENGTH
                 || user.getPassword() == null || user.getPassword().length() < MIN_LENGTH) {
-            throw new ExceptionDuringRegistration("Login and password must contain "
-                    + "at least 6 characters");
+            throw new ExceptionDuringRegistration(INCORRECT_LOGIN_OR_PASSWORD);
         }
         return storageDao.add(user);
     }
