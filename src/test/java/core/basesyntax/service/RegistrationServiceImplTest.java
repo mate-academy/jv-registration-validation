db515741c8cf64d93081476ee72e49c77f1d47b5
Taras <tazik1232@gmail.com>
2023-10-23 10:32:40 +0300

    added constants jv-registration-validation

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -15,6 +15,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
+    private static final User NULL_USER = null;
+    private static final String VALID_LOGIN = "tazbog123";
+    private static final String LENGTH_FOUR_INVALID_LOGIN = "1234";
+    private static final String LENGTH_EMPTY_INVALID_LOGIN = "";
+    private static final String LENGTH_SIX_VALID_LOGIN = "123456";
+    private static final String NULL_LOGIN = null;
+    private static final String VALID_PASSWORD = "tazbog123";
+    private static final String LENGTH_FOUR_INVALID_PASSWORD = "abcd";
+    private static final String LENGTH_EMPTY_INVALID_PASSWORD = "";
+    private static final String LENGTH_SIX_VALID_PASSWORD = "abcdef";
+    private static final String NULL_PASSWORD = null;
+    private static final Integer VALID_AGE = 25;
+    private static final Integer YOUNG_INVALID_AGE = 17;
+    private static final Integer NEGATIVE_INVALID_AGE = -59;
+    private static final Integer MIN_VALID_AGE = 18;
+    private static final Integer NULL_AGE = null;
     private static RegistrationService registrationService;
     private static StorageDao storageDao;
     private static User user;
@@ -28,9 +44,9 @@ class RegistrationServiceImplTest {
 
     @BeforeEach
     void setUp() {
-        user.setLogin("tazbog123");
-        user.setPassword("1234512345");
-        user.setAge(25);
+        user.setLogin(VALID_LOGIN);
+        user.setPassword(VALID_PASSWORD);
+        user.setAge(VALID_AGE);
     }
 
     @AfterEach
@@ -54,33 +70,33 @@ class RegistrationServiceImplTest {
     @Test
     void register_nullUser_notOk() {
         assertThrows(InvalidValidationException.class,
-                () -> registrationService.register(null));
+                () -> registrationService.register(NULL_USER));
     }
 
     @Test
     void register_userAge_notOk() {
-        user.setAge(17);
+        user.setAge(YOUNG_INVALID_AGE);
         assertThrows(InvalidValidationException.class,
                 () -> registrationService.register(user));
     }
 
     @Test
     void register_userAgeNull_notOk() {
-        user.setAge(null);
+        user.setAge(NULL_AGE);
         assertThrows(InvalidValidationException.class,
                 () -> registrationService.register(user));
     }
 
     @Test
     void register_userAgeNegative_notOk() {
-        user.setAge(-59);
+        user.setAge(NEGATIVE_INVALID_AGE);
         assertThrows(InvalidValidationException.class,
                 () -> registrationService.register(user));
     }
 
     @Test
     void register_userMinAge_Ok() {
-        user.setAge(18);
+        user.setAge(MIN_VALID_AGE);
         assertDoesNotThrow(() -> registrationService.register(user));
     }
 
@@ -91,28 +107,28 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_userLoginNull_notOk() {
-        user.setLogin(null);
+        user.setLogin(NULL_LOGIN);
         assertThrows(InvalidValidationException.class,
                 () -> registrationService.register(user));
     }
 
     @Test
     void register_userLoginLengthFour_notOk() {
-        user.setLogin("1234");
+        user.setLogin(LENGTH_FOUR_INVALID_LOGIN);
         assertThrows(InvalidValidationException.class,
                 () -> registrationService.register(user));
     }
 
     @Test
     void register_userEmptyLogin_notOk() {
-        user.setLogin("");
+        user.setLogin(LENGTH_EMPTY_INVALID_LOGIN);
         assertThrows(InvalidValidationException.class,
                 () -> registrationService.register(user));
     }
 
     @Test
     void register_userLoginLengthSix_Ok() {
-        user.setLogin("123456");
+        user.setLogin(LENGTH_SIX_VALID_LOGIN);
         assertDoesNotThrow(() -> registrationService.register(user));
     }
 
@@ -123,28 +139,28 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_userEmptyPassword_notOk() {
-        user.setPassword("");
+        user.setPassword(LENGTH_EMPTY_INVALID_PASSWORD);
         assertThrows(InvalidValidationException.class,
                 () -> registrationService.register(user));
     }
 
     @Test
     void register_userPasswordNull_notOk() {
-        user.setPassword(null);
+        user.setPassword(NULL_PASSWORD);
         assertThrows(InvalidValidationException.class,
                 () -> registrationService.register(user));
     }
 
     @Test
     void register_userPasswordLengthFour_notOk() {
-        user.setPassword("1234");
+        user.setPassword(LENGTH_FOUR_INVALID_PASSWORD);
         assertThrows(InvalidValidationException.class,
                 () -> registrationService.register(user));
     }
 
     @Test
     void register_userPasswordLengthSix_Ok() {
-        user.setPassword("123456");
+        user.setPassword(LENGTH_SIX_VALID_PASSWORD);
         assertDoesNotThrow(() -> registrationService.register(user));
     }
 
