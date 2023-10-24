78b575e4623aa5d47c8bd36b6b50e8ceb254914f
barbequeue <p99kab00@gmail.com>
2023-10-24 16:47:39 +0300

    Refactor unit-test for RegistrationServiceImpl service

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,29 +1,35 @@
 package core.basesyntax.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class RegistrationServiceImplTest {
     private static final RegistrationService registrationService = new RegistrationServiceImpl();
+    private static final User existingUser = new User();
+    private static final User user = new User();
 
     @BeforeAll
     public static void setupUsers() {
-        Storage.people.add(new User());
-        Storage.people.get(0).setLogin("administrator");
+        existingUser.setLogin("administrator");
+        Storage.people.add(existingUser);
+    }
+
+    @AfterAll
+    public static void teardown() {
+        Storage.people.clear();
     }
 
     @Test
-    public void register_validUser_returnsSameUser() {
-        User user = new User();
-        user.setLogin("0".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH));
+    public void register_validUser_ok() {
+        user.setLogin("l".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH));
         user.setPassword("p".repeat(RegistrationServiceImpl.MIN_PASSWORD_LENGTH));
         user.setAge(RegistrationServiceImpl.MIN_AGE);
         int initialStorageSize = Storage.people.size();
@@ -37,9 +43,8 @@ public class RegistrationServiceImplTest {
     }
 
     @Test
-    public void register_invalidUser_throwsException() {
-        User user = new User();
-        user.setLogin("1".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH - 1));
+    public void register_invalidUser_notOk() {
+        user.setLogin("l".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH - 1));
         user.setPassword("p".repeat(RegistrationServiceImpl.MIN_PASSWORD_LENGTH - 1));
         user.setAge(RegistrationServiceImpl.MIN_AGE - 1);
         int initialStorageSize = Storage.people.size();
@@ -52,57 +57,55 @@ public class RegistrationServiceImplTest {
     }
 
     @Test
-    public void register_nullLoginUser_throwsException() {
-        User user = new User();
+    public void register_nullLogin_notOk() {
         user.setLogin(null);
+        user.setPassword("p".repeat(RegistrationServiceImpl.MIN_PASSWORD_LENGTH));
+        user.setAge(RegistrationServiceImpl.MIN_AGE);
         assertThrows(RegistrationException.class,
                 () -> registrationService.register(user),
                 "Method should throw an Exception");
     }
 
     @Test
-    public void register_invalidLoginUser_throwsException() {
-        User user1 = new User();
-        user1.setLogin("");
+    public void register_invalidLogin_notOk() {
+        user.setLogin("");
+        user.setPassword("p".repeat(RegistrationServiceImpl.MIN_PASSWORD_LENGTH));
+        user.setAge(RegistrationServiceImpl.MIN_AGE);
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(user1),
+                () -> registrationService.register(user),
                 "Method should throw an Exception");
-        User user2 = new User();
-        user2.setLogin("l".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH - 1));
+        user.setLogin("l".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH - 1));
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(user2),
+                () -> registrationService.register(user),
                 "Method should throw an Exception");
     }
 
     @Test
-    public void register_nullPasswordUser_throwsException() {
-        User user = new User();
+    public void register_nullPassword_notOk() {
         user.setLogin("l".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH));
         user.setPassword(null);
+        user.setAge(RegistrationServiceImpl.MIN_AGE);
         assertThrows(RegistrationException.class,
                 () -> registrationService.register(user),
                 "Method should throw an Exception");
     }
 
     @Test
-    public void register_invalidPasswordUser_throwsException() {
-        User user1 = new User();
-        user1.setLogin("l".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH));
-        user1.setPassword("");
+    public void register_invalidPassword_notOk() {
+        user.setLogin("l".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH));
+        user.setPassword("");
+        user.setAge(RegistrationServiceImpl.MIN_AGE);
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(user1),
+                () -> registrationService.register(user),
                 "Method should throw an Exception");
-        User user2 = new User();
-        user2.setLogin("l".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH));
-        user2.setPassword("p".repeat(RegistrationServiceImpl.MIN_PASSWORD_LENGTH - 1));
+        user.setPassword("p".repeat(RegistrationServiceImpl.MIN_PASSWORD_LENGTH - 1));
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(user2),
+                () -> registrationService.register(user),
                 "Method should throw an Exception");
     }
 
     @Test
-    public void register_nullAgeUser_throwsException() {
-        User user = new User();
+    public void register_nullAge_notOk() {
         user.setLogin("l".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH));
         user.setPassword("p".repeat(RegistrationServiceImpl.MIN_PASSWORD_LENGTH));
         user.setAge(null);
@@ -112,42 +115,33 @@ public class RegistrationServiceImplTest {
     }
 
     @Test
-    public void register_invalidAgeUser_throwsException() {
-        User user1 = new User();
-        user1.setLogin("l".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH));
-        user1.setPassword("p".repeat(RegistrationServiceImpl.MIN_PASSWORD_LENGTH));
-        user1.setAge(0);
+    public void register_invalidAge_notOk() {
+        user.setLogin("l".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH));
+        user.setPassword("p".repeat(RegistrationServiceImpl.MIN_PASSWORD_LENGTH));
+        user.setAge(0);
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(user1),
+                () -> registrationService.register(user),
                 "Method should throw an Exception");
-        User user2 = new User();
-        user2.setLogin("l".repeat(RegistrationServiceImpl.MIN_LOGIN_LENGTH));
-        user2.setPassword("p".repeat(RegistrationServiceImpl.MIN_PASSWORD_LENGTH));
-        user2.setAge(RegistrationServiceImpl.MIN_AGE - 1);
-        assertThrows(RegistrationException.class,
-                () -> registrationService.register(user2),
-                "Method should throw an Exception");
-    }
-
-    @Test
-    public void register_nonUniqueLoginUser_throwsException() {
-        User user = new User();
-        user.setLogin("administrator");
-        user.setPassword("i_am_the_law");
-        user.setAge(42);
+        user.setAge(RegistrationServiceImpl.MIN_AGE - 1);
         assertThrows(RegistrationException.class,
                 () -> registrationService.register(user),
                 "Method should throw an Exception");
     }
 
     @Test
-    public void register_nullUser_returnsNull() {
-        User user = null;
-        int initialStorageSize = Storage.people.size();
-        User addedUser = registrationService.register(user);
-        assertNull(addedUser, "Method should return a null");
-        assertEquals(Storage.people.size(),
-                initialStorageSize,
+    public void register_nonUniqueLogin_notOk() {
+        user.setLogin(existingUser.getLogin());
+        user.setPassword("p".repeat(RegistrationServiceImpl.MIN_PASSWORD_LENGTH));
+        user.setAge(RegistrationServiceImpl.MIN_AGE);
+        assertThrows(RegistrationException.class,
+                () -> registrationService.register(user),
+                "Method should throw an Exception");
+    }
+
+    @Test
+    public void register_null_notOk() {
+        assertThrows(RegistrationException.class,
+                () -> registrationService.register(null),
                 "Method should not add null to the Storage");
     }
 }
