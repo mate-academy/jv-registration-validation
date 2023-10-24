8865cd3a7c325d47bba8d2e8f78f8682dcf7c532
TymurPanchenk0 <tymur.panchenko@gmail.com>
2023-10-24 17:42:01 +0300

    Registration validation student changes

--- src/test/java/core/basesyntax/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -8,55 +8,62 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RegistrationServiceTest {
 
-    private RegistrationServiceImpl registrationService;
-    private StorageDaoImpl storageDao;
+    private static StorageDaoImpl storageDao;
+    private static RegistrationServiceImpl registrationService;
+    private User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        storageDao = new StorageDaoImpl();
+    }
 
     @BeforeEach
     public void setUp() {
-        storageDao = new StorageDaoImpl();
-        registrationService = new RegistrationServiceImpl();
+        user = new User("validuser", "password123", 18);
     }
 
     @Test
     public void register_ValidUser_Success() {
-        User user = new User("validuser", "password123", 18);
-        User registeredUser = registrationService.register(user);
-        assertNotNull(registeredUser);
-        assertEquals(user.getLogin(), registeredUser.getLogin());
+        User expected = user;
+        User actual = registrationService.register(expected);
+        assertNotNull(actual);
+        assertEquals(expected.getLogin(), actual.getLogin());
     }
 
     @Test
     public void register_NullLogin_ExceptionThrown() {
-        User user = new User(null, "password123", 18);
+        user.setLogin(null);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
     public void register_ShortLogin_ExceptionThrown() {
-        User user = new User("short", "password123", 18);
+        user.setLogin("short");
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
     public void register_NullPassword_ExceptionThrown() {
-        User user = new User("validuser", null, 18);
+        user.setPassword(null);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
     public void register_ShortPassword_ExceptionThrown() {
-        User user = new User("validuser", "123", 18);
+        user.setPassword("123");
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
     public void register_UnderageUser_ExceptionThrown() {
-        User user = new User("validuser", "password123", 17);
+        user.setAge(17);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
@@ -67,19 +74,5 @@ public class RegistrationServiceTest {
 
         User user2 = new User("user1", "anotherPassword", 19);
         assertThrows(RegistrationException.class, () -> registrationService.register(user2));
-    }
-
-    @Test
-    public void register_AllValidButBorderlineLengthLogin_Success() {
-        User user = new User("123456", "password123", 18);
-        User registeredUser = registrationService.register(user);
-        assertNotNull(registeredUser);
-        assertEquals(user.getLogin(), registeredUser.getLogin());
-    }
-
-    @Test
-    public void register_AllValidButBorderlineLengthPassword_ExceptionThrown() {
-        User user = new User("validuser", "12345", 18);
-        assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 }
