9347f442f2589c41ef5f95c8d895d1c7d907ba25
barbequeue <p99kab00@gmail.com>
2023-10-24 16:46:39 +0300

    Refactor unit-test for User model

--- src/test/java/core/basesyntax/model/UserTest.java
+++ src/test/java/core/basesyntax/model/UserTest.java
@@ -7,119 +7,143 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class UserTest {
-    private static final User userWithId = new User();
     private static final User userWithLogin = new User();
     private static final User userWithPassword = new User();
     private static final User userWithAge = new User();
+    private static final User user = new User();
 
     @BeforeAll
     public static void setupTestUsers() {
-        userWithId.setId(0L);
         userWithLogin.setLogin("admin");
+        userWithLogin.setPassword(null);
+        userWithLogin.setAge(null);
+        userWithPassword.setLogin(null);
         userWithPassword.setPassword("password");
+        userWithPassword.setAge(null);
+        userWithAge.setLogin(null);
+        userWithAge.setPassword(null);
         userWithAge.setAge(20);
     }
 
     @Test
-    public void equals_sameIdUser_true() {
-        User user = new User();
-        user.setId(0L);
-        assertEquals(userWithId,
-                user,
-                "Users with same ID should be equal");
-    }
-
-    @Test
-    public void equals_differentIdUser_false() {
-        User user = new User();
-        user.setId(1L);
-        assertNotEquals(userWithId,
-                user,
-                "Users with different IDs should not be equal");
-    }
-
-    @Test
-    public void equals_sameLoginUser_true() {
-        User user = new User();
-        user.setLogin("admin");
+    public void equals_sameLogin_ok() {
+        user.setLogin(userWithLogin.getLogin());
+        user.setPassword(null);
+        user.setAge(null);
         assertEquals(userWithLogin,
                 user,
                 "Users with same login should be equal");
     }
 
     @Test
-    public void equals_differentLoginUser_false() {
-        User user = new User();
-        user.setLogin("employee");
+    public void equals_differentLogin_ok() {
+        user.setLogin(userWithLogin.getLogin() + "1");
+        user.setPassword(null);
+        user.setAge(null);
         assertNotEquals(userWithLogin,
                 user,
                 "Users with different logins should not be equal");
     }
 
     @Test
-    public void equals_samePasswordUser_true() {
-        User user = new User();
-        user.setPassword("password");
+    public void equals_nullLogin_ok() {
+        user.setLogin(null);
+        user.setPassword(null);
+        user.setAge(null);
+        assertNotEquals(userWithLogin,
+                user,
+                "User with empty login should not be equal to a User with present login");
+    }
+
+    @Test
+    public void equals_samePassword_ok() {
+        user.setLogin(null);
+        user.setPassword(userWithPassword.getPassword());
+        user.setAge(null);
         assertEquals(userWithPassword,
                 user,
                 "Users with same password should be equal");
     }
 
     @Test
-    public void equals_differentPasswordUser_false() {
-        User user = new User();
-        user.setPassword("drowssap");
+    public void equals_differentPassword_ok() {
+        user.setLogin(null);
+        user.setPassword(userWithPassword.getPassword() + "1");
+        user.setAge(null);
         assertNotEquals(userWithPassword,
                 user,
                 "Users with different passwords should not be equal");
     }
 
     @Test
-    public void equals_sameAgeUser_true() {
-        User user = new User();
-        user.setAge(20);
+    public void equals_nullPassword_ok() {
+        user.setLogin(null);
+        user.setPassword(null);
+        user.setAge(null);
+        assertNotEquals(userWithPassword,
+                user,
+                "User with empty password should not be equal to a User with present password");
+    }
+
+    @Test
+    public void equals_sameAge_ok() {
+        user.setLogin(null);
+        user.setPassword(null);
+        user.setAge(userWithAge.getAge());
         assertEquals(userWithAge,
                 user,
                 "Users with same age should be equal");
     }
 
     @Test
-    public void equals_differentAgeUser_false() {
-        User user = new User();
-        user.setAge(35);
+    public void equals_differentAge_ok() {
+        user.setLogin(null);
+        user.setPassword(null);
+        user.setAge(userWithAge.getAge() + 1);
         assertNotEquals(userWithAge,
                 user,
                 "Users with different ages should not be equal");
     }
 
     @Test
-    public void equals_sameUser_true() {
-        User user = userWithId;
-        assertEquals(userWithId,
+    public void equals_nullAge_ok() {
+        user.setLogin(null);
+        user.setPassword(null);
+        user.setAge(null);
+        assertNotEquals(userWithAge,
+                user,
+                "User with empty age should not be equal to a User with present age");
+    }
+
+    @Test
+    public void equals_itself_ok() {
+        User user = userWithLogin;
+        assertEquals(userWithLogin,
                 user,
                 "Same user should be equal to itself");
     }
 
     @Test
-    public void equals_null_false() {
-        assertNotEquals(userWithId,
+    public void equals_null_ok() {
+        assertNotEquals(userWithLogin,
                 null,
                 "User should not be equal to null");
     }
 
     @Test
-    public void equals_otherClass_false() {
+    public void equals_differentClass_ok() {
         Long id = 0L;
-        assertNotEquals(userWithId,
+        assertNotEquals(userWithLogin,
                 id,
                 "User should not be equal to an other class object");
     }
 
     @Test
-    public void hashCode_equalUser_equal() {
-        User user = new User();
-        user.setId(0L);
-        assertEquals(userWithId.hashCode(),
+    public void hashCode_equal_ok() {
+        user.setLogin(userWithLogin.getLogin());
+        user.setPassword(null);
+        user.setAge(null);
+        assertEquals(userWithLogin.hashCode(),
                 user.hashCode(),
                 "Equal users should have the same hash code");
     }
