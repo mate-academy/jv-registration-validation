7b573f07dbe89fc955730ec2dc14a5fff54c0605
barbequeue <p99kab00@gmail.com>
2023-10-24 15:24:03 +0300

    Add improved constructors to RegistrationException

--- src/main/java/core/basesyntax/exception/RegistrationException.java
+++ src/main/java/core/basesyntax/exception/RegistrationException.java
@@ -1,7 +1,19 @@
 package core.basesyntax.exception;
 
 public class RegistrationException extends RuntimeException {
+    public RegistrationException() {
+        super();
+    }
+
     public RegistrationException(String message) {
         super(message);
+    }
+
+    public RegistrationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RegistrationException(Throwable cause) {
+        super(cause);
     }
 }
