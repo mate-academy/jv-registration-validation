28146e3d7bcaae3780ae11aff0766a7ebadbb045
TymurPanchenk0 <tymur.panchenko@gmail.com>
2023-10-27 15:59:00 +0300

    Registration validation last fix

--- src/test/java/core/basesyntax/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -4,9 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
 import core.basesyntax.service.RegistrationServiceImpl;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,8 +16,8 @@ import org.junit.jupiter.api.Test;
 
 public class RegistrationServiceTest {
     private static final String VALID_USER_NAME = "validUser";
-    private static StorageDaoImpl storageDao;
-    private static RegistrationServiceImpl registrationService;
+    private static StorageDao storageDao;
+    private static RegistrationService registrationService;
     private User user;
 
     @BeforeAll
@@ -25,55 +27,55 @@ public class RegistrationServiceTest {
     }
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         user = new User("userOne", "password123", 18);
     }
 
     @Test
-    public void register_ValidUser_Success() {
+    void register_ValidUser_Success() {
         User actual = registrationService.register(user);
         assertNotNull(actual);
         assertEquals(user.getLogin(), actual.getLogin());
     }
 
     @Test
-    public void register_NullLogin_ExceptionThrown() {
+    void register_NullLogin_ExceptionThrown() {
         user.setLogin(null);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    public void register_ShortLogin_ExceptionThrown() {
+    void register_ShortLogin_ExceptionThrown() {
         user.setLogin("short");
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    public void register_NullPassword_ExceptionThrown() {
+    void register_NullPassword_ExceptionThrown() {
         user.setPassword(null);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    public void register_AgeNull_ExceptionThrown() {
+    void register_AgeNull_ExceptionThrown() {
         user.setAge(null);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    public void register_PasswordLessThanSix_ExceptionThrown() {
+    void register_PasswordLessThanSix_ExceptionThrown() {
         user.setPassword("12345");
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    public void register_UnderageUser_ExceptionThrown() {
+    void register_UnderageUser_ExceptionThrown() {
         user.setAge(17);
         assertThrows(RegistrationException.class, () -> registrationService.register(user));
     }
 
     @Test
-    public void register_ExistingUser_ExceptionThrown() {
+    void register_ExistingUser_ExceptionThrown() {
         User user1 = new User(VALID_USER_NAME, "password123", 18);
         storageDao.add(user1);
         User duplicateUser = new User(VALID_USER_NAME, "anotherPassword", 19);
