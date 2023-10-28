3ba59ab69bccaba5578febd51e35229c2887b240
Anton Babych <antonbabi13@gmail.com>
2023-10-28 09:21:55 +0300

    Improve code, move conts value to constants fields, replace big if() statement to separate function with logic name

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,16 +6,41 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_LOGIN_LENGTH = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        if (user.getLogin() == null || user.getLogin().length() < 6
-                || user.getPassword() == null || user.getPassword().length() < 6
-                || user.getAge() == null || user.getAge() < 18
-                || storageDao.get(user.getLogin()) != null) {
-            throw new IllegalUserDataExeption("User data not valid");
-        }
+        validateLogin(user.getLogin());
+        validatePassword(user.getPassword());
+        validateAge(user.getAge());
+        checkUserNotExists(user.getLogin());
         return storageDao.add(user);
+    }
+
+    private void validateLogin(String login) {
+        if (login == null || login.length() < MIN_LOGIN_LENGTH) {
+            throw new IllegalUserDataExeption("Login " + login + " not valid");
+        }
+    }
+
+    private void validatePassword(String password) {
+        if (password == null || password.length() < MIN_PASSWORD_LENGTH) {
+            throw new IllegalUserDataExeption("This password not valid");
+        }
+    }
+
+    private void validateAge(Integer age) {
+        if (age == null || age < MIN_AGE) {
+            throw new IllegalUserDataExeption("Age " + age + " not valid");
+        }
+    }
+
+    private void checkUserNotExists(String login) {
+        if (storageDao.get(login) != null) {
+            throw new IllegalUserDataExeption("User with login " + login + " already exists");
+        }
     }
 }
