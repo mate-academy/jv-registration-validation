ee1c2b016e0458aaf3278016adc0210f47b83344
Denys Okhrymovych <denys.ohrymovych.2015@gmail.com>
2023-10-30 21:17:42 +0200

    make constants end empty lines, remove public modifier in tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -14,26 +14,44 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        checkNulls(user);
+        checkIfUserIsNotNull(user);
+        checkIfIdIsNotNull(user);
+        checkIfLoginIsNotNull(user);
+        checkIfPasswordIsNotNull(user);
+        checkIfAgeIsNotNull(user);
         checkIfUserExists(user);
-        checkData(user);
+        checkLogin(user);
+        checkPassword(user);
+        checkAge(user);
         Storage.people.add(user);
         return user;
     }
 
-    private void checkNulls(User user) {
+    private void checkIfUserIsNotNull(User user) {
         if (user == null) {
             throw new InvalidDataException("The user is null");
         }
+    }
+
+    private void checkIfIdIsNotNull(User user) {
         if (user.getId() == null) {
             throw new InvalidDataException("The user's ID is null");
         }
+    }
+
+    private void checkIfLoginIsNotNull(User user) {
         if (user.getLogin() == null) {
             throw new InvalidDataException("The user's login is null");
         }
+    }
+
+    private void checkIfPasswordIsNotNull(User user) {
         if (user.getPassword() == null) {
             throw new InvalidDataException("The user's password is null");
         }
+    }
+
+    private void checkIfAgeIsNotNull(User user) {
         if (user.getAge() == null) {
             throw new InvalidDataException("The user's age is null");
         }
@@ -45,13 +63,19 @@ public class RegistrationServiceImpl implements RegistrationService {
         }
     }
 
-    private void checkData(User user) {
+    private void checkLogin(User user) {
         if (user.getLogin().length() < MINIMUM_LOGIN_LENGTH) {
             throw new InvalidDataException("The login is shorter than 6 characters");
         }
+    }
+
+    private void checkPassword(User user) {
         if (user.getPassword().length() < MINIMUM_PASSWORD_LENGTH) {
             throw new InvalidDataException("The password is shorter than 6 characters");
         }
+    }
+
+    private void checkAge(User user) {
         if (user.getAge() < MINIMUM_AGE) {
             throw new InvalidDataException("The age is less than 18");
         }
--- src/test/java/core/basesyntax/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -10,183 +10,246 @@ import core.basesyntax.service.RegistrationService;
 import core.basesyntax.service.RegistrationServiceImpl;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class RegistrationServiceImplTest {
+class RegistrationServiceImplTest {
     private static final int INDEX_OF_FIRST_USER = 0;
     private static final int INDEX_OF_SECOND_USER = 1;
-    private RegistrationService registrationService = new RegistrationServiceImpl();
+    private static final long ID_FOR_FIRST_USER = 38452693L;
+    private static final String LOGIN_FOR_FIRST_USER = "newboblogin";
+    private static final String PASSWORD_FOR_FIRST_USER = "u48V8j207";
+    private static final int AGE_FOR_FIRST_USER = 20;
+    private static final long ID_FOR_SECOND_USER = 503684295L;
+    private static final String LOGIN_FOR_SECOND_USER = "alicelogin";
+    private static final String PASSWORD_FOR_SECOND_USER = "i5y8QJ4vc";
+    private static final int AGE_FOR_SECOND_USER = 25;
+    private static final String EMPTY_LOGIN = "";
+    private static final String THREE_LETTER_LOGIN = "log";
+    private static final String FIVE_LETTER_LOGIN = "short";
+    private static final String SIX_LETTER_LOGIN = "abcdef";
+    private static final String TEN_LETTER_LOGIN = "qwertyuiop";
+    private static final String EMPTY_PASSWORD = "";
+    private static final String THREE_LETTER_PASSWORD = "log";
+    private static final String FIVE_LETTER_PASSWORD = "short";
+    private static final String SIX_LETTER_PASSWORD = "abcdef";
+    private static final String TEN_LETTER_PASSWORD = "qwertyuiop";
+    private static final int FIRST_NEGATIVE_AGE = -23;
+    private static final int SECOND_NEGATIVE_AGE = -1;
+    private static final int ZERO_AGE = 0;
+    private static final int FIRST_INSUFFICIENT_AGE = 5;
+    private static final int SECOND_INSUFFICIENT_AGE = 17;
+    private static final int FIRST_SUFFICIENT_AGE = 18;
+    private static final int SECOND_SUFFICIENT_AGE = 26;
+    private static RegistrationService registrationService;
     private List<User> users;
+
+    @BeforeAll
+    static void initializeRegistrationService() {
+        registrationService = new RegistrationServiceImpl();
+    }
 
     @BeforeEach
     void createUserList() {
         User user1 = new User();
-        user1.setId(38452693L);
-        user1.setLogin("newboblogin");
-        user1.setPassword("u48V8j207");
-        user1.setAge(20);
+        user1.setId(ID_FOR_FIRST_USER);
+        user1.setLogin(LOGIN_FOR_FIRST_USER);
+        user1.setPassword(PASSWORD_FOR_FIRST_USER);
+        user1.setAge(AGE_FOR_FIRST_USER);
+
         User user2 = new User();
-        user2.setId(503684295L);
-        user2.setLogin("alicelogin");
-        user2.setPassword("i5y8QJ4vc");
-        user2.setAge(25);
+        user2.setId(ID_FOR_SECOND_USER);
+        user2.setLogin(LOGIN_FOR_SECOND_USER);
+        user2.setPassword(PASSWORD_FOR_SECOND_USER);
+        user2.setAge(AGE_FOR_SECOND_USER);
+
         users = new ArrayList<>();
         users.add(user1);
         users.add(user2);
     }
 
     @Test
-    public void register_nullUser_notOk() {
+    void register_nullUser_notOk() {
         User user = null;
+
         assertThrows(InvalidDataException.class, () -> {
             registrationService.register(user);
         });
     }
 
     @Test
-    public void register_nullId_notOk() {
+    void register_nullId_notOk() {
         users.get(INDEX_OF_FIRST_USER).setId(null);
+
         assertThrows(InvalidDataException.class, () -> {
             registrationService.register(users.get(INDEX_OF_FIRST_USER));
         });
     }
 
     @Test
-    public void register_nullLogin_notOk() {
+    void register_nullLogin_notOk() {
         users.get(INDEX_OF_FIRST_USER).setLogin(null);
+
         assertThrows(InvalidDataException.class, () -> {
             registrationService.register(users.get(INDEX_OF_FIRST_USER));
         });
     }
 
     @Test
-    public void register_nullPassword_notOk() {
+    void register_nullPassword_notOk() {
         users.get(INDEX_OF_FIRST_USER).setPassword(null);
+
         assertThrows(InvalidDataException.class, () -> {
             registrationService.register(users.get(INDEX_OF_FIRST_USER));
         });
     }
 
     @Test
-    public void register_nullAge_notOk() {
+    void register_nullAge_notOk() {
         users.get(INDEX_OF_FIRST_USER).setAge(null);
+
         assertThrows(InvalidDataException.class, () -> {
             registrationService.register(users.get(INDEX_OF_FIRST_USER));
         });
     }
 
     @Test
-    public void register_existingUser_notOk() {
+    void register_existingUser_notOk() {
         Storage.people.add(users.get(INDEX_OF_SECOND_USER));
+
         assertThrows(InvalidDataException.class, () -> {
             registrationService.register(users.get(INDEX_OF_SECOND_USER));
         });
     }
 
     @Test
-    public void register_emptyLogin_notOk() {
-        users.get(INDEX_OF_SECOND_USER).setLogin("");
+    void register_emptyLogin_notOk() {
+        users.get(INDEX_OF_SECOND_USER).setLogin(EMPTY_LOGIN);
+
         assertThrows(InvalidDataException.class, () -> {
             registrationService.register(users.get(INDEX_OF_SECOND_USER));
         });
     }
 
     @Test
-    public void register_tooShortLogin_notOk() {
-        users.get(INDEX_OF_SECOND_USER).setLogin("log");
+    void register_tooShortLogin_notOk() {
+        users.get(INDEX_OF_SECOND_USER).setLogin(THREE_LETTER_LOGIN);
+
         assertThrows(InvalidDataException.class, () -> {
             registrationService.register(users.get(INDEX_OF_SECOND_USER));
         });
-        users.get(INDEX_OF_SECOND_USER).setLogin("short");
+
+        users.get(INDEX_OF_SECOND_USER).setLogin(FIVE_LETTER_LOGIN);
+
         assertThrows(InvalidDataException.class, () -> {
             registrationService.register(users.get(INDEX_OF_SECOND_USER));
         });
     }
 
     @Test
-    public void register_sufficientLoginLength_ok() {
-        users.get(INDEX_OF_SECOND_USER).setLogin("abcdef");
+    void register_sufficientLoginLength_ok() {
+        users.get(INDEX_OF_SECOND_USER).setLogin(SIX_LETTER_LOGIN);
+
         assertDoesNotThrow(() -> {
             registrationService.register(users.get(INDEX_OF_SECOND_USER));
         });
-        users.get(INDEX_OF_FIRST_USER).setLogin("qwertyuiop");
-        assertDoesNotThrow(() -> {
-            registrationService.register(users.get(INDEX_OF_FIRST_USER));
-        });
-    }
 
-    @Test
-    public void register_emptyPassword_notOk() {
-        users.get(INDEX_OF_SECOND_USER).setPassword("");
-        assertThrows(InvalidDataException.class, () -> {
-            registrationService.register(users.get(INDEX_OF_SECOND_USER));
-        });
-    }
+        users.get(INDEX_OF_FIRST_USER).setLogin(TEN_LETTER_LOGIN);
 
-    @Test
-    public void register_tooShortPassword_notOk() {
-        users.get(INDEX_OF_SECOND_USER).setPassword("log");
-        assertThrows(InvalidDataException.class, () -> {
-            registrationService.register(users.get(INDEX_OF_SECOND_USER));
-        });
-        users.get(INDEX_OF_SECOND_USER).setPassword("short");
-        assertThrows(InvalidDataException.class, () -> {
-            registrationService.register(users.get(INDEX_OF_SECOND_USER));
-        });
-    }
-
-    @Test
-    public void register_sufficientPasswordLength_ok() {
-        users.get(INDEX_OF_SECOND_USER).setPassword("abcdef");
-        assertDoesNotThrow(() -> {
-            registrationService.register(users.get(INDEX_OF_SECOND_USER));
-        });
-        users.get(INDEX_OF_FIRST_USER).setPassword("qwertyuiop");
         assertDoesNotThrow(() -> {
             registrationService.register(users.get(INDEX_OF_FIRST_USER));
         });
     }
 
     @Test
-    public void register_negativeAge_notOk() {
-        users.get(INDEX_OF_FIRST_USER).setAge(-23);
+    void register_emptyPassword_notOk() {
+        users.get(INDEX_OF_SECOND_USER).setPassword(EMPTY_PASSWORD);
+
         assertThrows(InvalidDataException.class, () -> {
-            registrationService.register(users.get(INDEX_OF_FIRST_USER));
-        });
-        users.get(INDEX_OF_FIRST_USER).setAge(-1);
-        assertThrows(InvalidDataException.class, () -> {
-            registrationService.register(users.get(INDEX_OF_FIRST_USER));
+            registrationService.register(users.get(INDEX_OF_SECOND_USER));
         });
     }
 
     @Test
-    public void register_zeroAge_notOk() {
-        users.get(INDEX_OF_FIRST_USER).setAge(0);
+    void register_tooShortPassword_notOk() {
+        users.get(INDEX_OF_SECOND_USER).setPassword(THREE_LETTER_PASSWORD);
+
         assertThrows(InvalidDataException.class, () -> {
-            registrationService.register(users.get(INDEX_OF_FIRST_USER));
+            registrationService.register(users.get(INDEX_OF_SECOND_USER));
+        });
+
+        users.get(INDEX_OF_SECOND_USER).setPassword(FIVE_LETTER_PASSWORD);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(users.get(INDEX_OF_SECOND_USER));
         });
     }
 
     @Test
-    public void register_insufficientAge_notOk() {
-        users.get(INDEX_OF_FIRST_USER).setAge(5);
-        assertThrows(InvalidDataException.class, () -> {
-            registrationService.register(users.get(INDEX_OF_FIRST_USER));
-        });
-        users.get(INDEX_OF_FIRST_USER).setAge(17);
-        assertThrows(InvalidDataException.class, () -> {
-            registrationService.register(users.get(INDEX_OF_FIRST_USER));
-        });
-    }
+    void register_sufficientPasswordLength_ok() {
+        users.get(INDEX_OF_SECOND_USER).setPassword(SIX_LETTER_PASSWORD);
 
-    @Test
-    public void register_sufficientAge_ok() {
-        users.get(INDEX_OF_FIRST_USER).setAge(18);
+        assertDoesNotThrow(() -> {
+            registrationService.register(users.get(INDEX_OF_SECOND_USER));
+        });
+
+        users.get(INDEX_OF_FIRST_USER).setPassword(TEN_LETTER_PASSWORD);
+
         assertDoesNotThrow(() -> {
             registrationService.register(users.get(INDEX_OF_FIRST_USER));
         });
-        users.get(INDEX_OF_SECOND_USER).setAge(26);
+    }
+
+    @Test
+    void register_negativeAge_notOk() {
+        users.get(INDEX_OF_FIRST_USER).setAge(FIRST_NEGATIVE_AGE);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(users.get(INDEX_OF_FIRST_USER));
+        });
+
+        users.get(INDEX_OF_FIRST_USER).setAge(SECOND_NEGATIVE_AGE);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(users.get(INDEX_OF_FIRST_USER));
+        });
+    }
+
+    @Test
+    void register_zeroAge_notOk() {
+        users.get(INDEX_OF_FIRST_USER).setAge(ZERO_AGE);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(users.get(INDEX_OF_FIRST_USER));
+        });
+    }
+
+    @Test
+    void register_insufficientAge_notOk() {
+        users.get(INDEX_OF_FIRST_USER).setAge(FIRST_INSUFFICIENT_AGE);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(users.get(INDEX_OF_FIRST_USER));
+        });
+
+        users.get(INDEX_OF_FIRST_USER).setAge(SECOND_INSUFFICIENT_AGE);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(users.get(INDEX_OF_FIRST_USER));
+        });
+    }
+
+    @Test
+    void register_sufficientAge_ok() {
+        users.get(INDEX_OF_FIRST_USER).setAge(FIRST_SUFFICIENT_AGE);
+
+        assertDoesNotThrow(() -> {
+            registrationService.register(users.get(INDEX_OF_FIRST_USER));
+        });
+
+        users.get(INDEX_OF_SECOND_USER).setAge(SECOND_SUFFICIENT_AGE);
+
         assertDoesNotThrow(() -> {
             registrationService.register(users.get(INDEX_OF_SECOND_USER));
         });
