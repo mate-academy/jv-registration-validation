21c2624e4cadc2bd1cd272bbc7307216edf3aec2
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-10-31 04:40:04 +0200

    Implement `RegistrationException`

---
+++ src/main/java/core/basesyntax/exeption/RegistrationException.java
@@ -0,0 +1,8 @@
+package core.basesyntax.exeption;
+
+public class RegistrationException extends RuntimeException {
+    public RegistrationException(String message) {
+        super(message);
+    }
+
+}
