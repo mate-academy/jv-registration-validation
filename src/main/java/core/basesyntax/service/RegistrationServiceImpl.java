c6fee46c6b12f09507d80873897e0801e8556c99
Yevhenii Liashenko <paradoxrial@gmail.com>
2023-10-31 04:41:13 +0200

    Implement `RegistrationServiceImpl`

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,38 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exeption.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private final static int MIN_LOGIN_LENGTH = 6;
+    private final static int MIN_PASSWORD_LENGTH = 6;
+    private final static int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Login can't be null");
+        }
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Password can't be null");
+        }
+        if (user.getAge() == null) {
+            throw new RegistrationException("Age can't be null");
+        }
+        if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
+            throw new RegistrationException("Not valid login: " + user.getLogin() + ". Min length login is " + MIN_LOGIN_LENGTH);
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RegistrationException("Not valid password: " + user.getPassword() + ". Min length password age is " + MIN_PASSWORD_LENGTH);
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("Not valid age: " + user.getAge() + ". Min allowed age is " + MIN_AGE);
+        }
+        if (user.equals(storageDao.get(user.getLogin()))) {
+            throw new RegistrationException("Not valid login: " + user.getLogin() + ". This login is already taken");
+        }
+        return storageDao.add(user);
     }
 }
