6e90519c057241f618b1429ae7530d2893c32811
Andrii Laver <laver.andriy@gmail.com>
2023-11-02 12:01:31 +0200

    divided registration method logic

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -9,8 +9,12 @@ public class RegistrationServiceImpl implements RegistrationService {
     private static final int LOGIN_MIN_SIZE = 6;
     private static final int PASSWORD_MIN_SIZE = 6;
     private static final int MIN_AGE = 18;
-    private static long id_count = 1;
-    private final StorageDao storageDao = new StorageDaoImpl();
+    private static long id_count;
+    private final StorageDao storageDao;
+
+    public RegistrationServiceImpl() {
+        storageDao = new StorageDaoImpl();
+    }
 
     @Override
     public User register(User user) {
@@ -18,6 +22,16 @@ public class RegistrationServiceImpl implements RegistrationService {
             throw new InvalidDataException("User is null");
         }
 
+        checkLogin(user);
+        checkPassword(user);
+        checkAge(user);
+
+        user.setId(++id_count);
+        System.out.println(user.getId());
+        return storageDao.add(user);
+    }
+
+    private void checkLogin(User user) {
         if (user.getLogin() == null) {
             throw new InvalidDataException("Login is null");
         }
@@ -27,22 +41,23 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (storageDao.get(user.getLogin()) != null) {
             throw new InvalidDataException("Such login is already exist");
         }
+    }
 
+    private void checkPassword(User user) {
         if (user.getPassword() == null) {
             throw new InvalidDataException("Password is null");
         }
         if (user.getPassword().length() < PASSWORD_MIN_SIZE) {
             throw new InvalidDataException("Password is too short");
         }
+    }
 
+    private void checkAge(User user) {
         if (user.getAge() == null) {
             throw new InvalidDataException("Age is null");
         }
         if (user.getAge() < MIN_AGE) {
             throw new InvalidDataException("You are underage");
         }
-
-        user.setId(++id_count);
-        return storageDao.add(user);
     }
 }
