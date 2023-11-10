faaa8def9e7c8231b7e033f1663be0f72e478ec7
Roman Globa <globaroman@gmail.com>
2023-11-10 18:27:25 +0200

    Added constants to tests

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,35 +1,38 @@
 package core.basesyntax.service;
 
-import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    private static StorageDaoImpl storageDao;
+    private static final String EMPTY_STRING = "";
+    private static final String ONE_CARECTER_STRING = "A";
+    private static final String THRE_CARACTER_STRING = "pAs";
+    private static final String FIVE_CARACTER_STRING = "passW";
+    private static final int NEGATIVE_AGE = -10;
+    private static final int AGE_IS_ZERO = 0;
+    private static final int AGE_IS_SEVENTEEN = 17;
+    private static final int OVER_AGE_USER = 150;
+
     private static RegistrationServiceImpl registrationService;
+
+    private User bob;
+    private User steven;
 
     @BeforeAll
     static void beforeAll() {
-        storageDao = new StorageDaoImpl();
         registrationService = new RegistrationServiceImpl();
     }
 
     @BeforeEach
     void setUp() {
+        bob = new User("bob123", "password1", 25);
+        steven = new User("stiven123", "password4", 21);
 
-        User bob = new User("bob123", "password1", 25);
-        User alice = new User("alice123", "password2", 23);
-        User john = new User("john123", "password3", 27);
-        User steven = new User("stiven123", "password4", 21);
-
-        Storage.people.add(bob);
-        Storage.people.add(alice);
-        Storage.people.add(john);
-        Storage.people.add(steven);
     }
 
     @Test
@@ -41,10 +44,10 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_LoginOfUserIfInvalid_NotOk() {
-        User mike = new User("mike3", "password5", 19);
+        User mike = new User(FIVE_CARACTER_STRING, "password5", 19);
         User mike1 = new User(null, "password5", 19);
-        User mike2 = new User("", "password5", 19);
-        User mike3 = new User("m", "password5", 19);
+        User mike2 = new User(EMPTY_STRING, "password5", 19);
+        User mike3 = new User(ONE_CARECTER_STRING, "password5", 19);
         Assert.assertThrows(RegistrationException.class, () -> {
             registrationService.register(mike);
         });
@@ -61,10 +64,10 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_PasswordOfUserIfInvalid_NotOk() {
-        User mike = new User("mike366", "pass", 19);
+        User mike = new User("mike366", THRE_CARACTER_STRING, 19);
         User mike1 = new User("mike366", null, 19);
-        User mike2 = new User("mike366", "", 19);
-        User mike3 = new User("mike366", "p", 19);
+        User mike2 = new User("mike366", EMPTY_STRING, 19);
+        User mike3 = new User("mike366", ONE_CARECTER_STRING, 19);
 
         Assert.assertThrows(RegistrationException.class, () -> {
             registrationService.register(mike);
@@ -82,10 +85,10 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_AgeOfUserIfInvalid_NotOk() {
-        User mike2 = new User("mike366", "password1", 17);
-        User mike3 = new User("mike366", "password1", -17);
-        User mike4 = new User("mike366", "password1", 0);
-        User mike5 = new User("mike366", "password1", 150);
+        User mike2 = new User("mike366", "password1", AGE_IS_SEVENTEEN);
+        User mike3 = new User("mike366", "password1", NEGATIVE_AGE);
+        User mike4 = new User("mike366", "password1", AGE_IS_ZERO);
+        User mike5 = new User("mike366", "password1", OVER_AGE_USER);
         Assert.assertThrows(RegistrationException.class, () -> {
             registrationService.register(mike2);
         });
@@ -102,17 +105,17 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_UserExist_NotOk() {
-        User steven = new User("stiven123", "password4", 21);
-        Assert.assertThrows(RuntimeException.class, () -> {
+        Storage.people.add(steven);
+        Assert.assertThrows(RegistrationException.class, () -> {
             registrationService.register(steven);
         });
     }
 
     @Test
-    void register_RegisterUser_Ok() {
-        User user = new User("user123", "password466", 30);
-        User actual = registrationService.register(user);
-        Assert.assertEquals(actual, user);
+    void register_RegisterValidUser_Ok() {
+        User alice = new User("alice123", "password2", 23);
+        User userExp1 = registrationService.register(alice);
+        Assertions.assertEquals(userExp1, alice);
 
     }
 }
