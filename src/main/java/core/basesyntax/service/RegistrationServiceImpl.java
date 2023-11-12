dabd6c18ff9dd5927a799217dc3136e101544bc4
VKhmil <vovahmil@gmail.com>
2023-11-12 19:39:27 +0200

    Registration Service Impl Test

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,6 +1,7 @@
 package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
+import core.basesyntax.exaption.RegistrationException;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
@@ -9,6 +10,42 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        checkNull(user);
+        existLoginValid(user);
+        passwordLengthValid(user);
+        loginLengthValid(user);
+        ageValid(user);
+        storageDao.add(user);
+        return user;
+    }
+
+    private void existLoginValid(User user) {
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("User with this login is register");
+        }
+    }
+
+    private void passwordLengthValid(User user) {
+        if (user.getPassword().length() < 6) {
+            throw new RegistrationException("User with this password is less than 6");
+        }
+    }
+
+    private void loginLengthValid(User user) {
+        if (user.getLogin().length() < 6) {
+            throw new RegistrationException("User with this login is less than 6");
+        }
+    }
+
+    private void ageValid(User user) {
+        if (user.getAge() < 18) {
+            throw new RegistrationException("User age less than 18");
+        }
+    }
+
+    private void checkNull(User user) {
+        if (user.getLogin() == null || user.getPassword() == null || user.getAge() == null) {
+            throw new RegistrationException("User info can't be null");
+        }
     }
 }
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,8 +1,78 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorldTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import core.basesyntax.db.Storage;
+import core.basesyntax.exaption.RegistrationException;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
+    private User testUser;
+
+    @BeforeEach
+    void setUp() {
+        testUser = new User();
+        testUser.setLogin("QWERTY123");
+        testUser.setAge(21);
+        testUser.setPassword("start33Password_007");
+    }
+
+    @Test
+    void addTrueUser_Ok() {
+        assertEquals(registrationService.register(testUser), testUser);
+    }
+
+    @Test
+    void addUserIsRegister_exception() {
+        registrationService.register(testUser);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(testUser);
+        });
+    }
+
+    @Test
+    void addUserPasswordLessThen6_exception() {
+        testUser.setPassword("12345");
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(testUser);
+        });
+    }
+
+    @Test
+    void addUserInfoIsNull_exception() {
+        testUser.setPassword(null);
+        testUser.setAge(null);
+        testUser.setAge(null);
+        testUser.setId(null);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(testUser);
+        });
+    }
+
+    @Test
+    void addUserLoginLessThen6_exception() {
+        testUser.setLogin("12345");
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(testUser);
+        });
+    }
+
+    @Test
+    void addUserAgeLessThen18_exception() {
+        testUser.setAge(17);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(testUser);
+        });
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
 }
