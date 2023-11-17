e7ce3207c898cfcdd01776f11034f67da3a88f7b
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2023-11-17 14:20:50 +0200

    remove hardcode msg; delete RegistrationServiceImpl from BeforeEach; move all ok-case to one method; combine all notOk-case for invalidLogin/Password/Age;

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -13,23 +13,23 @@ public class RegistrationServiceImpl implements RegistrationService {
     @Override
     public User register(User user) {
         if (user == null) {
-            throw new RegistrationException("Invalid user date. User fields cannot be null");
+            throw new RegistrationException();
         }
 
         if (user.getLogin() == null || user.getLogin().length() < MIN_LENGTH_LOGIN) {
-            throw new RegistrationException("Login must be at least 6 characters long");
+            throw new RegistrationException();
         }
 
         if (user.getPassword() == null || user.getPassword().length() < MIN_LENGTH_PASS) {
-            throw new RegistrationException("Password must be at least 6 characters long");
+            throw new RegistrationException();
         }
 
         if (user.getAge() == null || user.getAge() < MIN_AGE) {
-            throw new RegistrationException("User must be at least 18 years old");
+            throw new RegistrationException();
         }
 
         if (storageDao.get(user.getLogin()) != null) {
-            throw new RegistrationException("User with this login already exists");
+            throw new RegistrationException();
         }
 
         return storageDao.add(user);
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -12,18 +12,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RegistrationServiceImplTest {
-    private RegistrationServiceImpl registrationServiceImpl;
+    private final RegistrationServiceImpl registrationServiceImpl = new RegistrationServiceImpl();
     private StorageDao storageDao;
 
     @BeforeEach
     public void set_Up() {
-        registrationServiceImpl = new RegistrationServiceImpl();
         storageDao = new StorageDaoImpl();
     }
 
     @Test
-    public void register_invalidUserData_notOk() {
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(null));
+    public void register_userNull_notOk() {
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(null),
+                "Invalid user date. User fields cannot be null");
     }
 
     @Test
@@ -34,12 +34,8 @@ public class RegistrationServiceImplTest {
         validUser.setAge(20);
 
         registrationServiceImpl.register(validUser);
-
         assertNotNull(storageDao.get("ValidUser"));
-    }
 
-    @Test
-    public void register_differentLogin_ok() {
         User firstUser = new User();
         firstUser.setLogin("FirstUser");
         firstUser.setPassword("firstPassword");
@@ -55,10 +51,43 @@ public class RegistrationServiceImplTest {
 
         assertNotNull(storageDao.get("FirstUser"));
         assertNotNull(storageDao.get("SecondUser"));
+
+        validUser.setLogin("UserLogin6");
+        assertTrue(Storage.people.add(validUser));
+
+        validUser.setLogin("UserLogin7");
+        assertTrue(Storage.people.add(validUser));
+
+        validUser.setLogin("UserLogin10");
+        assertTrue(Storage.people.add(validUser));
+
+        validUser.setPassword("abcdef");
+        assertTrue(Storage.people.add(validUser));
+
+        validUser.setPassword("abcdefg");
+        assertTrue(Storage.people.add(validUser));
+
+        validUser.setPassword("abcdefghij");
+        assertTrue(Storage.people.add(validUser));
+
+        validUser.setAge(18);
+        assertTrue(Storage.people.add(validUser));
+
+        validUser.setAge(19);
+        assertTrue(Storage.people.add(validUser));
+
+        validUser.setAge(25);
+        assertTrue(Storage.people.add(validUser));
+
+        validUser.setAge(60);
+        assertTrue(Storage.people.add(validUser));
+
+        validUser.setAge(Integer.MAX_VALUE);
+        assertTrue(Storage.people.add(validUser));
     }
 
     @Test
-    public void register_duplicateLogin_notOk() {
+    public void register_invalidLogin_notOk() {
         User firstUser = new User();
         firstUser.setLogin("NewUser");
         firstUser.setPassword("secondPassword");
@@ -70,280 +99,87 @@ public class RegistrationServiceImplTest {
         secondUser.setLogin("NewUser");
         secondUser.setPassword("secondPassword");
         secondUser.setAge(30);
-
         assertThrows(RegistrationException.class,
-                () -> registrationServiceImpl.register(secondUser));
-    }
+                () -> registrationServiceImpl.register(secondUser),
+                "User with this login already exists");
 
-    @Test
-    public void register_nullLogin_notOk() {
         User user = new User();
         user.setLogin(null);
         user.setPassword("password");
         user.setAge(20);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Username length null. Login must be at least 6 characters long");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_lengthLoginZero_notOk() {
-        User user = new User();
         user.setLogin("");
-        user.setPassword("password");
-        user.setAge(20);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Username length 0. Login must be at least 6 characters long");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_lengthLoginThree_notOk() {
-        User user = new User();
         user.setLogin("abc");
-        user.setPassword("password");
-        user.setAge(20);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Username length 3. Login must be at least 6 characters long");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_lengthLoginFive_notOk() {
-        User user = new User();
         user.setLogin("abcde");
-        user.setPassword("password");
-        user.setAge(20);
-
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Username length 5. Login must be at least 6 characters long");
     }
 
     @Test
-    public void register_lengthLoginSix_ok() {
-        User validUser = new User();
-        validUser.setLogin("UserLogin6");
-        validUser.setPassword("validP");
-        validUser.setAge(20);
-
-        assertTrue(Storage.people.add(validUser));
-    }
-
-    @Test
-    public void register_lengthLoginSeven_ok() {
-        User validUser = new User();
-        validUser.setLogin("UserLogin7");
-        validUser.setPassword("validPs");
-        validUser.setAge(20);
-
-        assertTrue(Storage.people.add(validUser));
-    }
-
-    @Test
-    public void register_lengthLoginTen_ok() {
-        User validUser = new User();
-        validUser.setLogin("UserLogin10");
-        validUser.setPassword("validPassw");
-        validUser.setAge(20);
-
-        assertTrue(Storage.people.add(validUser));
-    }
-
-    @Test
-    public void register_nullPassword_notOk() {
+    public void register_invalidPassword_notOk() {
         User user = new User();
-        user.setLogin("UserNullPassword");
+        user.setLogin("UserInvalidPassword");
         user.setPassword(null);
         user.setAge(20);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Keywords length null. Password must be at least 6 characters long");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_lengthPasswordZero_notOk() {
-        User user = new User();
-        user.setLogin("UserZeroPassword");
         user.setPassword("");
-        user.setAge(20);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Keywords length 0. Password must be at least 6 characters long");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_lengthPasswordThree_notOk() {
-        User user = new User();
-        user.setLogin("UserThreePassword");
         user.setPassword("abc");
-        user.setAge(20);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Keywords length 3. Password must be at least 6 characters long");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_lengthPasswordFive_notOk() {
-        User user = new User();
-        user.setLogin("UserFivePassword");
         user.setPassword("abcde");
-        user.setAge(20);
-
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Keywords length 5. Password must be at least 6 characters long");
     }
 
     @Test
-    public void register_lengthPasswordSix_ok() {
-        User validUser = new User();
-        validUser.setLogin("UserLengthPassSix");
-        validUser.setPassword("abcdef");
-        validUser.setAge(20);
-
-        assertTrue(Storage.people.add(validUser));
-    }
-
-    @Test
-    public void register_lengthPasswordSeven_ok() {
-        User validUser = new User();
-        validUser.setLogin("UserLengthPassSeven");
-        validUser.setPassword("abcdefg");
-        validUser.setAge(20);
-
-        assertTrue(Storage.people.add(validUser));
-    }
-
-    @Test
-    public void register_LengthPasswordTen_ok() {
-        User validUser = new User();
-        validUser.setLogin("UserLengthPassTen");
-        validUser.setPassword("abcdefghij");
-        validUser.setAge(20);
-
-        assertTrue(Storage.people.add(validUser));
-    }
-
-    @Test
-    public void register_nullAge_notOk() {
+    public void register_invalidAge_notOk() {
         User user = new User();
         user.setLogin("UserAgeNull");
         user.setPassword("password");
         user.setAge(null);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Age: null. User must be at least 18 years old");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_negativeAgeSix_notOk() {
-        User user = new User();
-        user.setLogin("UserAgeNegative6");
-        user.setPassword("password");
         user.setAge(-6);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Age: -6. User must be at least 18 years old");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_negativeAgeEighteen_notOk() {
-        User user = new User();
-        user.setLogin("UserAgeNegative18");
-        user.setPassword("password");
         user.setAge(-18);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Age: -18. User must be at least 18 years old");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_negativeAgeSixty_notOk() {
-        User user = new User();
-        user.setLogin("UserAgeNegative60");
-        user.setPassword("password");
         user.setAge(-60);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Age: -60. User must be at least 18 years old");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_userAgeZero_notOk() {
-        User user = new User();
-        user.setLogin("UserAge0");
-        user.setPassword("password");
         user.setAge(0);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Age: 0. User must be at least 18 years old");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_userAgeSix_notOk() {
-        User user = new User();
-        user.setLogin("UserAge6");
-        user.setPassword("password");
         user.setAge(6);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Age: 6. User must be at least 18 years old");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_userAgeSeventeen_notOk() {
-        User user = new User();
-        user.setLogin("UserAge17");
-        user.setPassword("password");
         user.setAge(17);
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "Age: 17. User must be at least 18 years old");
 
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
-    }
-
-    @Test
-    public void register_userAgeEighteen_ok() {
-        User validUser = new User();
-        validUser.setLogin("UserAge18");
-        validUser.setPassword("validPassword");
-        validUser.setAge(18);
-
-        assertTrue(Storage.people.add(validUser));
-    }
-
-    @Test
-    public void register_userAgeNineteen_ok() {
-        User validUser = new User();
-        validUser.setLogin("UserAge19");
-        validUser.setPassword("validPassword");
-        validUser.setAge(19);
-
-        assertTrue(Storage.people.add(validUser));
-    }
-
-    @Test
-    public void register_userAgeTwentyFive_ok() {
-        User validUser = new User();
-        validUser.setLogin("UserAge25");
-        validUser.setPassword("validPassword");
-        validUser.setAge(25);
-
-        assertTrue(Storage.people.add(validUser));
-    }
-
-    @Test
-    public void register_userAgeSixty_ok() {
-        User validUser = new User();
-        validUser.setLogin("UserAge60");
-        validUser.setPassword("validPassword");
-        validUser.setAge(60);
-
-        assertTrue(Storage.people.add(validUser));
-    }
-
-    @Test
-    void register_userWithMaxAge_ok() {
-        User validUser = new User();
-        validUser.setLogin("UserMaxAge");
-        validUser.setPassword("validPassword");
-        validUser.setAge(Integer.MAX_VALUE);
-
-        registrationServiceImpl.register(validUser);
-
-        assertNotNull(storageDao.get("UserMaxAge"));
-    }
-
-    @Test
-    void register_userWithMinAge_notOk() {
-        User user = new User();
-        user.setLogin("UserMinAge");
-        user.setPassword("validPassword");
         user.setAge(Integer.MIN_VALUE);
-
-        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user));
+        assertThrows(RegistrationException.class, () -> registrationServiceImpl.register(user),
+                "User must be at least 18 years old");
     }
 }
