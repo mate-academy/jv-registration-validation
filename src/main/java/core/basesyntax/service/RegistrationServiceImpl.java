69d072dd1ed195850af45a4cff5ef8fe0d6b7f1d
Mykola Horianin <anyavic2379@gmail.com>
2023-11-21 19:15:27 +0200

    implemented the registration service and module tests.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,27 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MAX_AGE = 18;
+    private static final int MIN_NUMBER_CHARACTERS = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new InvalidDataException("User is invalid. Please enter the data");
+        }
+        if (user.getAge() == null || user.getAge() < MAX_AGE) {
+            throw new InvalidDataException("The user must be at least 18 years old");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new InvalidDataException("A user with this login already exists");
+        }
+        if (user.getLogin() == null || user.getLogin().length() < MIN_NUMBER_CHARACTERS) {
+            throw new InvalidDataException("The login is invalid");
+        }
+        if (user.getPassword() == null || user.getPassword().length() < MIN_NUMBER_CHARACTERS) {
+            throw new InvalidDataException("The password is invalid");
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,140 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private RegistrationService registrationService = new RegistrationServiceImpl();
+    private StorageDao storageDao = new StorageDaoImpl();
+
+    @Test
+    void registerUserNull_NotOk() {
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(null);
+        }, "An exception should be thrown");
+
+        User bobAgeNull = new User();
+        bobAgeNull.setAge(null);
+        bobAgeNull.setLogin("bobist");
+        bobAgeNull.setPassword("hhjfbchbv");
+        bobAgeNull.setId(52698L);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(bobAgeNull);
+        }, "An exception should be thrown");
+
+        User bobLoginNull = new User();
+        bobLoginNull.setAge(19);
+        bobLoginNull.setLogin(null);
+        bobLoginNull.setPassword("hhjfbchbv");
+        bobLoginNull.setId(52698L);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(bobLoginNull);
+        }, "An exception should be thrown");
+
+        User bobPasswordNull = new User();
+        bobPasswordNull.setAge(19);
+        bobPasswordNull.setLogin("bobist");
+        bobPasswordNull.setPassword("");
+        bobPasswordNull.setId(52698L);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(bobPasswordNull);
+        }, "An exception should be thrown");
+
+    }
+
+    @Test
+    void registerInvalidAge_NotOk() {
+        User bob = new User();
+        bob.setAge(10);
+        bob.setLogin("bobist");
+        bob.setPassword("hhjfbchbv");
+        bob.setId(52698L);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(bob);
+        }, "An exception should be thrown");
+
+        User alice = new User();
+        alice.setAge(15);
+        alice.setLogin("aliceFox");
+        alice.setPassword("hhjf562bv");
+        alice.setId(36898L);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(alice);
+        }, "An exception should be thrown");
+    }
+
+    @Test
+    void registerInvalidLogin_NotOk() {
+        User john = new User();
+        john.setAge(19);
+        john.setLogin("bobist");
+        john.setPassword("hhfjbcdyb");
+        john.setId(1259L);
+        registrationService.register(john);
+
+        User bob = new User();
+        bob.setAge(19);
+        bob.setLogin("bobist");
+        bob.setPassword("hiokfbchbv");
+        bob.setId(52698L);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(bob);
+        }, "An exception should be thrown");
+    }
+
+    @Test
+    void registerLengthPassword_NotOk() {
+        User bob = new User();
+        bob.setAge(19);
+        bob.setLogin("bobistik");
+        bob.setPassword("hhjf");
+        bob.setId(52698L);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(bob);
+        }, "An exception should be thrown");
+
+        User john = new User();
+        john.setAge(19);
+        john.setLogin("johnidd");
+        john.setPassword("jo");
+        john.setId(1259L);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(john);
+        }, "An exception should be thrown");
+    }
+
+    @Test
+    void registerLengthLogin_NotOk() {
+        User bob = new User();
+        bob.setAge(19);
+        bob.setLogin("bob");
+        bob.setPassword("hhjfklk");
+        bob.setId(52698L);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(bob);
+        }, "An exception should be thrown");
+
+        User john = new User();
+        john.setAge(19);
+        john.setLogin("john");
+        john.setPassword("jolop5963");
+        john.setId(1259L);
+
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(john);
+        }, "An exception should be thrown");
+    }
+}
