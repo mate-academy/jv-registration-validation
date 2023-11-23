5ad952e6678c2ff735c4ce4fdd6bba002eb42597
gulini32 <gulini32@gmail.com>
2023-11-23 13:42:58 +0200

    HW

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,22 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final Integer MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null || user.getLogin() == null || user.getPassword() == null
+                || user.getLogin().isEmpty() || user.getPassword().isEmpty()) {
+            return null;
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("You are too young to register");
+        }
+        return storageDao.add(user);
+
+
     }
 }
+
+
