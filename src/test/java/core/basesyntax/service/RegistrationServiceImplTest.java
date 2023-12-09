62718f7629fda9e537c8301e1358b5673c06d5ce
Maksym Holubitskyi <golub1718@gmail.com>
2023-12-09 20:42:22 +0200

    refactored class RegistrationServiceImplTest.java: - removed methods : register_nullAge_notOk(), register_nullLogin_notOk(), register_nullPass_notOk(), register_negativeAge_notOk(), register_wrongAge_notOk() and and transferred their logic to the appropriate methods. - renamed variables and methods names.

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -13,11 +13,12 @@ import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
     private static final int DEF_PEOPLE_AMOUNT = 3;
-    private static final String WRONG_PASS = "weDfD";
-    private static final String WRITE_PASS = "weDfD1fgr#4";
-    private static final String WRONG_LOGIN = "David";
-    private static final String WRITE_LOGIN = "DavidDanis";
-    private static final int WRONG_AGE = 17;
+    private static final String WRONG_EDGE_PASS = "1eDf5";
+    private static final String WRITE_EDGE_PASS = "1e3f56";
+    private static final String WRONG_EDGE_LOGIN = "David";
+    private static final String WRITE_EDGE_LOGIN = "David6";
+    private static final int WRONG_EDGE_AGE = 17;
+    private static final int WRITE_EDGE_AGE = 18;
     private static RegistrationService registrationService;
     private User testUser;
 
@@ -40,75 +41,83 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void register_lengthAfterRegistration_ok() {
-        int additionalUsers = 50;
-        for (int i = 0; i < additionalUsers; i++) {
-            registrationService.register(new User("BobSalimon" + i, "asd$%DF#$234fgg", 28));
-        }
-        assertEquals(DEF_PEOPLE_AMOUNT + additionalUsers, Storage.people.size());
-    }
-
-    @Test
     void register_addUserWithCorrectLog_ok() {
-        testUser.setLogin(WRITE_LOGIN);
+        testUser.setLogin(WRITE_EDGE_LOGIN);
         User registerUser = registrationService.register(testUser);
         assertEquals(testUser, registerUser);
         assertEquals(DEF_PEOPLE_AMOUNT + 1, Storage.people.size());
+
+        User newTestUser = new User(WRITE_EDGE_LOGIN + "7", WRITE_EDGE_PASS, WRITE_EDGE_AGE);
+        registerUser = registrationService.register(newTestUser);
+        assertEquals(newTestUser, registerUser);
+        assertEquals(DEF_PEOPLE_AMOUNT + 2, Storage.people.size());
     }
 
     @Test
     void register_addUserWithCorrectPass_ok() {
-        testUser.setLogin(WRITE_PASS);
+        testUser.setLogin(WRITE_EDGE_PASS);
         User registerUser = registrationService.register(testUser);
         assertEquals(testUser, registerUser);
         assertEquals(DEF_PEOPLE_AMOUNT + 1, Storage.people.size());
+
+        User newTestUser = new User(WRITE_EDGE_LOGIN, WRITE_EDGE_PASS + "7", WRITE_EDGE_AGE);
+        registerUser = registrationService.register(newTestUser);
+        assertEquals(newTestUser, registerUser);
+        assertEquals(DEF_PEOPLE_AMOUNT + 2, Storage.people.size());
     }
 
     @Test
-    void register_SameUserName_notOk() {
-        User sameUser = Storage.people.get(DEF_PEOPLE_AMOUNT - 1);
+    void register_addUserWithCorrectAge_ok() {
+        testUser.setAge(WRITE_EDGE_AGE);
+        User registerUser = registrationService.register(testUser);
+        assertEquals(testUser, registerUser);
+        assertEquals(DEF_PEOPLE_AMOUNT + 1, Storage.people.size());
+
+        User newTestUser = new User(WRITE_EDGE_LOGIN, WRITE_EDGE_PASS, WRITE_EDGE_AGE + 1);
+        registerUser = registrationService.register(newTestUser);
+        assertEquals(newTestUser, registerUser);
+        assertEquals(DEF_PEOPLE_AMOUNT + 2, Storage.people.size());
+    }
+
+    @Test
+    void register_SameUserLogin_notOk() {
+        User sameUser = new User("Cristal", "12gGfe18gS", 25);
         assertThrows(RegisterDataException.class, () -> registrationService.register(sameUser));
     }
 
     @Test
-    void register_passLessThanSixChars_notOK() {
-        testUser.setPassword(WRONG_PASS);
+    void register_invalidPassword_notOK() {
+        testUser.setPassword(null);
+        assertThrows(RegisterDataException.class, () -> registrationService.register(testUser));
+
+        testUser.setPassword("1234");
+        assertThrows(RegisterDataException.class, () -> registrationService.register(testUser));
+
+        testUser.setPassword(WRONG_EDGE_PASS);
         assertThrows(RegisterDataException.class, () -> registrationService.register(testUser));
     }
 
     @Test
-    void register_loginLessThanSixChars_notOK() {
-        testUser.setLogin(WRONG_LOGIN);
+    void register_invalidLogin_notOK() {
+        testUser.setLogin(null);
+        assertThrows(RegisterDataException.class, () -> registrationService.register(testUser));
+
+        testUser.setLogin("ABBY");
+        assertThrows(RegisterDataException.class, () -> registrationService.register(testUser));
+
+        testUser.setLogin(WRONG_EDGE_LOGIN);
         assertThrows(RegisterDataException.class, () -> registrationService.register(testUser));
     }
 
     @Test
     void register_wrongAge_notOk() {
-        testUser.setAge(WRONG_AGE);
-        assertThrows(RegisterDataException.class, () -> registrationService.register(testUser));
-    }
-
-    @Test
-    void register_negativeAge_notOk() {
-        testUser.setAge(-WRONG_AGE);
-        assertThrows(RegisterDataException.class, () -> registrationService.register(testUser));
-    }
-
-    @Test
-    void register_nullAge_notOk() {
         testUser.setAge(null);
         assertThrows(RegisterDataException.class, () -> registrationService.register(testUser));
-    }
 
-    @Test
-    void register_nullLogin_notOk() {
-        testUser.setLogin(null);
+        testUser.setAge(WRONG_EDGE_AGE);
         assertThrows(RegisterDataException.class, () -> registrationService.register(testUser));
-    }
 
-    @Test
-    void register_nullPass_notOk() {
-        testUser.setPassword(null);
+        testUser.setAge(-WRONG_EDGE_AGE);
         assertThrows(RegisterDataException.class, () -> registrationService.register(testUser));
     }
 }
