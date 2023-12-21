c1560e4f1fd3bab4a08c584c6d5f1337cd761a99
AdamMudrak <mudrets2000@gmail.com>
2023-12-22 00:13:52 +0200

    Added class with register method according to the requirements + added exception to register method signature in the interface

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,29 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.RegistrationServiceImplException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LOGIN_AND_PASSWORD_LEN = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
-    public User register(User user) {
-        return null;
+    public User register(User user) throws RegistrationServiceImplException {
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationServiceImplException("User with this login already exists");
+        }
+        if (user.getLogin().length() < MIN_LOGIN_AND_PASSWORD_LEN) {
+            throw new RegistrationServiceImplException("Login should consist of at least 6 characters");
+        }
+        if (user.getPassword().length() < MIN_LOGIN_AND_PASSWORD_LEN) {
+            throw new RegistrationServiceImplException("Password should consist of at least 6 characters");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationServiceImplException("Password should consist of at least 6 characters");
+        }
+        storageDao.add(user);
+        return user;
     }
 }
