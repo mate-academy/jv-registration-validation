7e7133c22ef2ed13a83cede6b7b5253be94046f1
Adam Mudrak <113807446+AdamMudrak@users.noreply.github.com>
2023-12-22 23:23:33 +0200

    Update src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
    Co-authored-by: Ivan Kosovych <41166578+Ivan95kos@users.noreply.github.com>

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -11,7 +11,7 @@ public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
-    public User register(User user) throws RegistrationServiceImplException {
+    public User register(User user) {
         if (user == null) {
             throw new RegistrationServiceImplException("User can't be null");
         }
