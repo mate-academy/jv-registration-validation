372cd25fcf3058ffa94888db7aed7b02fa88214c
MilianCode <milian.code@gmail.com>
2023-12-25 00:25:43 +0100

    Added solution to jv-registration-validation

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,6 +2,7 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.InvalidDataException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
@@ -9,6 +10,22 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new InvalidDataException("User cannot be null");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new InvalidDataException("User with this login already exist");
+        }
+        if (user.getLogin().length() < 6) {
+            throw new InvalidDataException("Login must be at least 6 characters long");
+        }
+        if (user.getPassword().length() < 6) {
+            throw new InvalidDataException("Password must be at least 6 characters long");
+        }
+        if (user.getAge() == null || user.getAge() < 18) {
+            throw new InvalidDataException("You must be at least 18 years old to register account");
+        }
+        storageDao.add(user);
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,96 @@
+package core.basesyntax.service;
+
+import core.basesyntax.exception.InvalidDataException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+    private static User defaultUser;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        defaultUser = new User();
+        defaultUser.setId(1234L);
+        defaultUser.setAge(20);
+        defaultUser.setLogin("milian");
+        defaultUser.setPassword("maksym");
+    }
+
+    private void checkThrows(User user) {
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    public void addNewUser_Ok() {
+        User newUser = new User();
+        newUser.setId(1234L);
+        newUser.setAge(20);
+        newUser.setLogin("login6");
+        newUser.setPassword("maksym");
+        User actual = registrationService.register(newUser);
+        assertEquals(newUser, actual);
+    }
+
+    @Test
+    public void addUserWithExistedLogin_NotOk() {
+        User newUser = new User();
+        newUser.setId(3456L);
+        newUser.setAge(22);
+        newUser.setLogin("milian");
+        newUser.setPassword("asdasd");
+        registrationService.register(defaultUser);
+        checkThrows(newUser);
+    }
+
+    @Test
+    public void addUserWithShortLogin_NotOk() {
+        User newUser = new User();
+        newUser.setId(3456L);
+        newUser.setAge(22);
+        newUser.setLogin("mili");
+        newUser.setPassword("asdasd");
+        checkThrows(newUser);
+    }
+
+    @Test
+    public void addUserWithShortPassword_NotOk() {
+        User newUser = new User();
+        newUser.setId(3456L);
+        newUser.setAge(22);
+        newUser.setLogin("shortPassword");
+        newUser.setPassword("asd");
+        checkThrows(newUser);
+    }
+
+    @Test
+    public void addUserUnderaged_NotOk() {
+        User newUser = new User();
+        newUser.setId(3456L);
+        newUser.setAge(17);
+        newUser.setLogin("underaged");
+        newUser.setPassword("asdasd");
+        checkThrows(newUser);
+    }
+
+    @Test
+    public void addUserWithNullAge_NotOk() {
+        User newUser = new User();
+        newUser.setId(3456L);
+        newUser.setAge(null);
+        newUser.setLogin("underaged");
+        newUser.setPassword("asdasd");
+        checkThrows(newUser);
+    }
+
+    @Test
+    public void nullUserAdding_NotOk() {
+        checkThrows(null);
+    }
+}
