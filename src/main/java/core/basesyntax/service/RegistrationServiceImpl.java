bc9cbe0a6bf32e34185784d8e2cfcd9e980fa145
Oleh <mihailyakyb45@gmail.com>
2023-12-26 12:26:20 +0200

    done hw

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,33 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_FIELDS_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Login can't be null");
+        }
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Password can't be null");
+        }
+        if (user.getAge() == null) {
+            throw new RegistrationException("Age can't be null");
+        }
+        if (user.getLogin().length() < MIN_FIELDS_LENGTH) {
+            throw new RegistrationException("Login's length should be longer that " + MIN_FIELDS_LENGTH);
+        }
+        if (user.getPassword().length() < MIN_FIELDS_LENGTH) {
+            throw new RegistrationException("Password's length should be longer that " + MIN_FIELDS_LENGTH);
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("Not valid age: " + user.getAge() + ". Min allowed age is " + MIN_AGE);
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("User already exists");
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,78 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private RegistrationService registrationService;
+
+    @BeforeEach
+    void setUp() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @Test
+    void userAllFieldsNulls_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User(null, null, null));
+        });
+    }
+
+    @Test
+    void userLoginNull_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User(null, "123456789", 19));
+        });
+    }
+
+    @Test
+    void userPasswordNull_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("vadim1337", null, 19));
+        });
+    }
+
+    @Test
+    void userAgeNull_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("vadim1337", "123456789", null));
+        });
+    }
+
+    @Test
+    void userLoginInvalid_notOk() {
+        User user = new User("v1337", "123456789", 19);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void userPasswordInvalid_notOk() {
+        User user = new User("vadim1337", "1234", 19);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void userAgeInvalid_notOk() {
+        User user = new User("vadim1337", "123456789", 16);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void userExistsInStorage_notOk() {
+        User user1 = new User("vadim1337", "123456789", 19);
+        registrationService.register(user1);
+        User user2 = new User("vadim1337", "123456789", 19);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user2);
+        });
+    }
+}
