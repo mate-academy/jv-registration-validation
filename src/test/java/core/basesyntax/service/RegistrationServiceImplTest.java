af05b8595ecff3b52b586f65fc1721eb3d16b7f9
Nazar Kolesnikov <nazarkolesnikov536@gmail.com>
2024-01-11 15:40:12 +0200

    Initial commit

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,4 +1,4 @@
-package core.basesyntax;
+package core.basesyntax.service;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
