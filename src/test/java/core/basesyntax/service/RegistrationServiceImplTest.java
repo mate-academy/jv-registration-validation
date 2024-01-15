7ff6e7a8c078148ea6ab966de0dde7b580b03e5d
Misha Kazarian <misha.kazaryan2@gmail.com>
2024-01-16 00:56:41 +0200

    Jv-registration-validation task solution (fix 1)

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -11,72 +11,90 @@ import core.basesyntax.exceptions.LoginDuplicateException;
 import core.basesyntax.exceptions.NullUserDataException;
 import core.basesyntax.exceptions.NullUserException;
 import core.basesyntax.model.User;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
+    private static final List<User> storageUsersList = new ArrayList<>();
+    private static final List<User> correctUsersList = new ArrayList<>();
     private final RegistrationService registrationService = new RegistrationServiceImpl();
     private final StorageDao storageDao = new StorageDaoImpl();
 
+    @BeforeAll
+    static void beforeAll() {
+        User storageUser1 = new User();
+        storageUser1.setId(1L);
+        storageUser1.setLogin("LordFar");
+        storageUser1.setPassword("34gsge&ws");
+        storageUser1.setAge(23);
+
+        User storageUser2 = new User();
+        storageUser2.setId(2L);
+        storageUser2.setLogin("Cramliren");
+        storageUser2.setPassword("233Fwaefs");
+        storageUser2.setAge(20);
+
+        User storageUser3 = new User();
+        storageUser3.setId(3L);
+        storageUser3.setLogin("Ignesco");
+        storageUser3.setPassword("123455fef");
+        storageUser3.setAge(99);
+
+        storageUsersList.add(storageUser1);
+        storageUsersList.add(storageUser2);
+        storageUsersList.add(storageUser3);
+
+        User correctUser1 = new User();
+        correctUser1.setId(4L);
+        correctUser1.setLogin("Dsterr");
+        correctUser1.setPassword("34gsge&ws");
+        correctUser1.setAge(23);
+
+        User correctUser2 = new User();
+        correctUser2.setId(5L);
+        correctUser2.setLogin("Roparked");
+        correctUser2.setPassword("waefs5");
+        correctUser2.setAge(23);
+
+        User correctUser3 = new User();
+        correctUser3.setId(6L);
+        correctUser3.setLogin("Limilon");
+        correctUser3.setPassword("123455fef");
+        correctUser3.setAge(18);
+
+        User correctUser4 = new User();
+        correctUser4.setId(7L);
+        correctUser4.setLogin("Astaros");
+        correctUser4.setPassword("f455fef");
+        correctUser4.setAge(39);
+
+        correctUsersList.add(correctUser1);
+        correctUsersList.add(correctUser2);
+        correctUsersList.add(correctUser3);
+        correctUsersList.add(correctUser4);
+    }
+
     @BeforeEach
     void setUp() {
-        User user1 = new User();
-        user1.setId(1L);
-        user1.setLogin("LordFar");
-        user1.setPassword("34gsge&ws");
-        user1.setAge(23);
+        Storage.people.addAll(storageUsersList);
+    }
 
-        User user2 = new User();
-        user2.setId(2L);
-        user2.setLogin("Cramliren");
-        user2.setPassword("233Fwaefs");
-        user2.setAge(23);
-
-        User user3 = new User();
-        user3.setId(3L);
-        user3.setLogin("Ignesco");
-        user3.setPassword("123455fef");
-        user3.setAge(23);
-
-        Storage.people.add(user1);
-        Storage.people.add(user2);
-        Storage.people.add(user3);
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
     }
 
     @Test
     void register_correctUser_ok() {
+        for (User user : correctUsersList) {
+            registrationService.register(user);
+            assertEquals(user, storageDao.get(user.getLogin()));
+        }
 
-        User actualUserFirst = new User();
-        actualUserFirst.setLogin("Valdelkron");
-        actualUserFirst.setPassword("233Fwa");
-        actualUserFirst.setAge(23);
-
-        registrationService.register(actualUserFirst);
-        assertEquals(actualUserFirst, storageDao.get(actualUserFirst.getLogin()));
-
-        User actualUserSecond = new User();
-        actualUserSecond.setLogin("Kalibr");
-        actualUserSecond.setPassword("564gfass");
-        actualUserSecond.setAge(19);
-
-        registrationService.register(actualUserSecond);
-        assertEquals(actualUserSecond, storageDao.get(actualUserSecond.getLogin()));
-
-        User actualUserThird = new User();
-        actualUserThird.setLogin("Faradei");
-        actualUserThird.setPassword("564gfass");
-        actualUserThird.setAge(18);
-
-        registrationService.register(actualUserThird);
-        assertEquals(actualUserThird, storageDao.get(actualUserThird.getLogin()));
-
-        User actualUserForth = new User();
-        actualUserForth.setLogin("Ragnorer");
-        actualUserForth.setPassword("564gfass");
-        actualUserForth.setAge(21);
-
-        registrationService.register(actualUserForth);
-        assertEquals(actualUserForth, storageDao.get(actualUserForth.getLogin()));
     }
 
     @Test
@@ -86,158 +104,97 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_nullUserData_notOk() {
-        User actualUser1 = new User();
-        actualUser1.setLogin(null);
-        actualUser1.setPassword("344gs&ws");
-        actualUser1.setAge(39);
+        User user1 = copeUser(correctUsersList.get(0));
+        user1.setLogin(null);
+        assertThrows(NullUserDataException.class, () -> registrationService.register(user1));
 
-        assertThrows(NullUserDataException.class, () -> registrationService.register(actualUser1));
+        User user2 = copeUser(correctUsersList.get(1));
+        user2.setPassword(null);
+        assertThrows(NullUserDataException.class, () -> registrationService.register(user2));
 
-        User actualUser2 = new User();
-        actualUser2.setLogin("Aster");
-        actualUser2.setPassword(null);
-        actualUser2.setAge(39);
-
-        assertThrows(NullUserDataException.class, () -> registrationService.register(actualUser2));
-
-        User actualUser4 = new User();
-        actualUser4.setLogin("");
-        actualUser4.setPassword("344gs&ws");
-        actualUser4.setAge(null);
-
-        assertThrows(NullUserDataException.class, () -> registrationService.register(actualUser4));
+        User user3 = copeUser(correctUsersList.get(2));
+        user3.setAge(null);
+        assertThrows(NullUserDataException.class, () -> registrationService.register(user3));
     }
 
     @Test
     void register_loginDuplicateSameUsers_notOk() {
-        User actualUser1 = new User();
-        actualUser1.setLogin("LordFar");
-        actualUser1.setPassword("34gsge&ws");
-        actualUser1.setAge(23);
+        User user1 = copeUser(storageUsersList.get(0));
+        assertThrows(LoginDuplicateException.class, () -> registrationService.register(user1));
 
-        assertThrows(LoginDuplicateException.class,
-                () -> registrationService.register(actualUser1));
+        User user2 = copeUser(storageUsersList.get(1));
+        user2.setPassword("1234566wwf4");
+        assertThrows(LoginDuplicateException.class, () -> registrationService.register(user2));
 
-        User actualUser2 = new User();
-        actualUser2.setLogin("Cramliren");
-        actualUser2.setPassword("hkl4gs&ws");
-        actualUser2.setAge(39);
-
-        assertThrows(LoginDuplicateException.class,
-                () -> registrationService.register(actualUser2));
-
-        User actualUser3 = new User();
-        actualUser3.setLogin("Ignesco");
-        actualUser3.setPassword("hkl4gs&ws");
-        actualUser3.setAge(39);
-
-        assertThrows(LoginDuplicateException.class,
-                () -> registrationService.register(actualUser3));
+        User user3 = copeUser(storageUsersList.get(2));
+        user3.setAge(23);
+        assertThrows(LoginDuplicateException.class, () -> registrationService.register(user3));
     }
 
     @Test
-    void register_loginAllowableSize_notOk() {
-        User actualUser1 = new User();
-        actualUser1.setLogin("Asa");
-        actualUser1.setPassword("344gs&ws");
-        actualUser1.setAge(39);
+    void register_loginWrongSize_notOk() {
+        User user1 = copeUser(correctUsersList.get(0));
+        user1.setLogin("Aster");
+        assertThrows(DataCorrectnessException.class, () -> registrationService.register(user1));
 
-        assertThrows(DataCorrectnessException.class,
-                () -> registrationService.register(actualUser1));
+        User user2 = copeUser(correctUsersList.get(1));
+        user2.setLogin("Asa");
+        assertThrows(DataCorrectnessException.class, () -> registrationService.register(user2));
 
-        User actualUser2 = new User();
-        actualUser2.setLogin("Aster");
-        actualUser2.setPassword("344gs&ws");
-        actualUser2.setAge(39);
+        User user3 = copeUser(correctUsersList.get(2));
+        user3.setLogin("A");
+        assertThrows(DataCorrectnessException.class, () -> registrationService.register(user3));
 
-        assertThrows(DataCorrectnessException.class,
-                () -> registrationService.register(actualUser2));
-
-        User actualUser3 = new User();
-        actualUser3.setLogin("A");
-        actualUser3.setPassword("344gs&ws");
-        actualUser3.setAge(39);
-
-        assertThrows(DataCorrectnessException.class,
-                () -> registrationService.register(actualUser3));
-
-        User actualUser4 = new User();
-        actualUser4.setLogin("");
-        actualUser4.setPassword("344gs&ws");
-        actualUser4.setAge(39);
-
-        assertThrows(DataCorrectnessException.class,
-                () -> registrationService.register(actualUser4));
+        User user4 = copeUser(correctUsersList.get(3));
+        user4.setLogin("");
+        assertThrows(DataCorrectnessException.class, () -> registrationService.register(user4));
     }
 
     @Test
-    void register_passwordAllowableSize_notOk() {
-        User actualUser1 = new User();
-        actualUser1.setLogin("Asallre");
-        actualUser1.setPassword("344gs");
-        actualUser1.setAge(39);
+    void register_passwordWrongSize_notOk() {
+        User user1 = copeUser(correctUsersList.get(0));
+        user1.setPassword("1ster");
+        assertThrows(DataCorrectnessException.class, () -> registrationService.register(user1));
 
-        assertThrows(DataCorrectnessException.class,
-                () -> registrationService.register(actualUser1));
+        User user2 = copeUser(correctUsersList.get(1));
+        user2.setPassword("f23");
+        assertThrows(DataCorrectnessException.class, () -> registrationService.register(user2));
 
-        User actualUser2 = new User();
-        actualUser2.setLogin("Dsterres");
-        actualUser2.setPassword("34");
-        actualUser2.setAge(39);
+        User user3 = copeUser(correctUsersList.get(2));
+        user3.setPassword("f");
+        assertThrows(DataCorrectnessException.class, () -> registrationService.register(user3));
 
-        assertThrows(DataCorrectnessException.class,
-                () -> registrationService.register(actualUser2));
-
-        User actualUser3 = new User();
-        actualUser3.setLogin("Roparked");
-        actualUser3.setPassword("3");
-        actualUser3.setAge(39);
-
-        assertThrows(DataCorrectnessException.class,
-                () -> registrationService.register(actualUser3));
-
-        User actualUser4 = new User();
-        actualUser4.setLogin("Wlrensar");
-        actualUser4.setPassword("");
-        actualUser4.setAge(39);
-
-        assertThrows(DataCorrectnessException.class,
-                () -> registrationService.register(actualUser4));
+        User user4 = copeUser(correctUsersList.get(3));
+        user4.setPassword("");
+        assertThrows(DataCorrectnessException.class, () -> registrationService.register(user4));
     }
 
     @Test
-    void register_AllowableAge_notOk() {
-        User actualUser1 = new User();
-        actualUser1.setLogin("Asallre");
-        actualUser1.setPassword("3gd4h8@44gs");
-        actualUser1.setAge(17);
+    void register_underage_notOk() {
+        User user1 = copeUser(correctUsersList.get(0));
+        user1.setAge(17);
+        assertThrows(DataCorrectnessException.class, () -> registrationService.register(user1));
 
-        assertThrows(DataCorrectnessException.class,
-                () -> registrationService.register(actualUser1));
+        User user2 = copeUser(correctUsersList.get(1));
+        user2.setAge(6);
+        assertThrows(DataCorrectnessException.class, () -> registrationService.register(user2));
 
-        User actualUser2 = new User();
-        actualUser2.setLogin("Dsterres");
-        actualUser2.setPassword("3gd4h8@44gs");
-        actualUser2.setAge(4);
+        User user3 = copeUser(correctUsersList.get(2));
+        user3.setAge(0);
+        assertThrows(DataCorrectnessException.class, () -> registrationService.register(user3));
 
-        assertThrows(DataCorrectnessException.class,
-                () -> registrationService.register(actualUser2));
+        User user4 = copeUser(correctUsersList.get(3));
+        user4.setAge(-10);
+        assertThrows(DataCorrectnessException.class, () -> registrationService.register(user4));
 
-        User actualUser3 = new User();
-        actualUser3.setLogin("Roparked");
-        actualUser3.setPassword("3gd4h8@44gs");
-        actualUser3.setAge(0);
+    }
 
-        assertThrows(DataCorrectnessException.class,
-                () -> registrationService.register(actualUser3));
-
-        User actualUser4 = new User();
-        actualUser4.setLogin("Wlrensar");
-        actualUser4.setPassword("3gd4h8@44gs");
-        actualUser4.setAge(-5);
-
-        assertThrows(DataCorrectnessException.class,
-                () -> registrationService.register(actualUser4));
-
+    private User copeUser(User user) {
+        User newUser = new User();
+        newUser.setId(user.getId());
+        newUser.setLogin(user.getLogin());
+        newUser.setPassword(user.getPassword());
+        newUser.setAge(user.getAge());
+        return newUser;
     }
 }
