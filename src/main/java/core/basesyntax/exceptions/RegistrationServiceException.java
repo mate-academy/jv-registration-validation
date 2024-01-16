662b3c9c54396f10f67a7a4f2ab45b2ea5142dbf
petro feniak <petro.fenyak@gmail.com>
2024-01-17 01:44:38 +0200

    implemented test class  RegistrationServiceImplTest

---
+++ src/main/java/core/basesyntax/exceptions/RegistrationServiceException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exceptions;
+
+public class RegistrationServiceException extends Exception {
+    public RegistrationServiceException(String message) {
+        super(message);
+    }
+}
