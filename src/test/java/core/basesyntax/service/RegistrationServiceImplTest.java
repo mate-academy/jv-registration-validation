71d128de4fdff8e44e247fce5cb18628bd3a45d6
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2024-01-19 21:57:49 +0200

    1-st commit after 1-st cod review: added BeforEach, AfterEach. Renamed tests

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -5,17 +5,29 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
     private RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
-    private User user = new User();
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setPassword("123456");
+        user.setLogin("123456");
+        user.setAge(18);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
 
     @Test
-    void registrationUser_PasswordLengthEdge_isOk() {
-        user.setPassword("123456");
-        user.setLogin("academy");
-        user.setAge(18);
+    void register_PasswordLengthEdge_isOk() {
         registrationService.register(user);
         int expected = 1;
         int actual = Storage.people.size();
@@ -23,51 +35,39 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void registrationUser_PasswordLength_notOk() {
+    void register_PasswordLength_notOk() {
         user.setPassword("12345");
-        user.setLogin("123456");
-        user.setAge(18);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         });
     }
 
     @Test
-    void registrationUser_LoginLengthEdge_isOk() {
-        user.setPassword("123456");
-        user.setLogin("Developer");
-        user.setAge(18);
+    void register_LoginLengthEdge_isOk() {
         registrationService.register(user);
-        int expected = 2;
+        int expected = 1;
         int actual = Storage.people.size();
         assertEquals(expected, actual);
     }
 
     @Test
-    void registrationUser_LoginLength_notOk() {
-        user.setPassword("123456");
+    void register_LoginLength_notOk() {
         user.setLogin("12345");
-        user.setAge(18);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         });
     }
 
     @Test
-    void registrationUser_Age_isOk() {
-        user.setPassword("123456");
-        user.setLogin("Mate2024");
-        user.setAge(18);
+    void register_Age_isOk() {
         registrationService.register(user);
-        int expected = 3;
+        int expected = 1;
         int actual = Storage.people.size();
         assertEquals(expected, actual);
     }
 
     @Test
-    void registrationUser_Age_notOk() {
-        user.setPassword("123456");
-        user.setLogin("Mate2024");
+    void register_underAge_notOk() {
         user.setAge(17);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
@@ -75,28 +75,24 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void registrationUser_PasswordIsNull_notOk() {
-        user.setAge(18);
-        user.setLogin("academy");
+    void register_nullPassword_notOk() {
+        user.setPassword(null);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         });
     }
 
     @Test
-    void registrationUser_LoginIsNull_notOk() {
-        user.setPassword("123456");
-        user.setAge(18);
+    void register_nullLogin_notOk() {
+        user.setLogin(null);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         });
     }
 
     @Test
-    void registrationUser_LoginAlreadyExist_notOk() {
-        user.setPassword("123456");
-        user.setLogin("academy");
-        user.setAge(18);
+    void register_LoginAlreadyExist_notOk() {
+        registrationService.register(user);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(user);
         });
