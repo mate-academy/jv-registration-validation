52456fbf3c20dc84a4299431819e26e58411fc57
Orest 40th <student2528@bmet.org.ua>
2024-01-30 11:46:21 +0200

    implemented registration service methods;  added tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,58 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LOGIN_LENGTH = 6;
+    private static final int MIN_PASS_LENGTH = 6;
+    private static final int MIN_AGE_LENGTH = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null || user.equals(new User())) {
+            throw new RegistrationException("Fill in the data fields");
+        }
+
+        if (user.getLogin() == null) {
+            throw new RegistrationException(("Login missing"));
+        }
+
+        if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
+            throw new RegistrationException("Login must be at least "
+                    + MIN_LOGIN_LENGTH + " characters");
+        }
+
+        if (user.getLogin().matches(".*\\s.*")) {
+            throw new RegistrationException("Password must contain no spacing");
+        }
+
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Password must contain no spacing");
+        }
+
+        if (user.getPassword().matches(".*\\s.*")) {
+            throw new RegistrationException("Password must contain no spacing");
+        }
+
+        if (user.getPassword().length() < MIN_PASS_LENGTH) {
+            throw new RegistrationException("Password must be at least "
+                    + MIN_PASS_LENGTH + " characters");
+        }
+
+        if (user.getAge() < MIN_AGE_LENGTH) {
+            throw new RegistrationException("You must be at least "
+                    + MIN_AGE_LENGTH + " years old to register");
+        }
+
+        for (User storageUser : Storage.people) {
+            if (storageUser.getLogin().equals(user.getLogin())) {
+                throw new RegistrationException("User with current login already exists");
+            }
+        }
+
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,139 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static RegistrationServiceImpl registrationService;
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        Storage.people.clear();
+    }
+
+    @Test
+    public void register_validUser_ok() {
+        User registeredUser0 = registrationService.register(
+                new User("boroda4436", "2Gillette_Pro_glide", 30));
+        User registeredUser1 = registrationService.register(new User("Rommelua", "ulemoR", 18));
+        User registeredUser2 = registrationService.register(new User("sokima", "a!aa3sokim1", 100));
+
+        assertTrue(Storage.people.contains(registeredUser0));
+        assertTrue(Storage.people.contains(registeredUser1));
+        assertTrue(Storage.people.contains(registeredUser2));
+        assertEquals(3, Storage.people.size());
+    }
+
+    @Test
+    public void register_nullUser_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(null);
+        });
+    }
+
+    @Test
+    public void register_emptyUser_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User());
+        });
+    }
+
+    @Test
+    public void register_existingUser_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("Kerrusha", "metallica4ever", 25));
+            registrationService.register(new User("Kerrushaa", "i_love_KPOP", 24));
+            registrationService.register(new User("Kerrushaa", "keyPOP4EVER", 24));
+        });
+    }
+
+    @Test
+    public void register_youngUser_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("okuzan", "bigGUY3000", 4));
+            registrationService.register(new User("notOkuzan", "ImOldEnoguh17", 17));
+        });
+    }
+
+    @Test
+    public void register_negativeAge_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("okuzan3", "thirdTry3", -18));
+        });
+    }
+
+    @Test
+    public void register_emptyAge_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("Orest40th3", "thirdTry3", 0));
+        });
+    }
+
+    @Test
+    public void register_emptyLogin_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("            ", "123123asdasd", 19));
+        });
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("", "123123asdasd", 40));
+        });
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("O r Est", "123123asd", 40));
+        });
+    }
+
+    @Test
+    public void register_shortLogin_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("leksk", "123123asdasd", 80));
+        });
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("Rom", "123123asdasd", 66));
+        });
+    }
+
+    @Test
+    public void register_nullPassword_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("Orest40th", null, 23));
+        });
+    }
+
+    @Test
+    public void register_emptyPassword_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("Orest40th", "             ", 23));
+        });
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("RommelUA", "     ", 69));
+        });
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("MENTOR_MA", "", 43));
+        });
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("MentorMaa", "1df 1fghj", 40));
+        });
+    }
+
+    @Test
+    public void register_shortPassword_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("olekskov", "12a", 18));
+        });
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(new User("olekskov2", "c2345", 18));
+        });
+    }
+}
