cafe5405b11880a72812e79749084c4a11afab6a
Kacper <kacperkukula97@gmail.com>
2024-02-13 19:40:14 +0100

    Revised version with implemented suggestions.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -14,28 +14,32 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
+        if (user == null) {
+            throw new RegistrationException("User can't be null");
+        }
         if (user.getLogin() == null) {
             throw new RegistrationException("Login can't be null");
         }
         if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
-            throw new RegistrationException("Login must be at least " + MIN_LOGIN_LENGTH
-                    + " characters");
+            throw new RegistrationException("Login must be at least "
+                    + MIN_LOGIN_LENGTH + " characters");
         }
         if (storageDao.get(user.getLogin()) != null) {
-            throw new RegistrationException("User with that login already exists");
+            throw new RegistrationException("Login already taken");
         }
         if (user.getPassword() == null) {
             throw new RegistrationException("Password can't be null");
         }
         if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
-            throw new RegistrationException("Password must be at least " + MIN_PASSWORD_LENGTH
-                    + " characters");
+            throw new RegistrationException("Password must be at least "
+                    + MIN_PASSWORD_LENGTH + " characters");
         }
         if (user.getAge() == null) {
             throw new RegistrationException("Age can't be null");
         }
         if (user.getAge() < MIN_AGE) {
-            throw new RegistrationException("Users age must be at least " + MIN_AGE + " years old");
+            throw new RegistrationException("Age must be at least "
+                    + MIN_AGE + " years old");
         }
         return storageDao.add(user);
     }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -13,10 +13,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
+    private static final String TWO_USERS_ERROR =
+            "The list should contain 2 users.";
+    private static final String ONLY_ONE_USER_ERROR =
+            "There should be only 1 user in list.";
+    private static final String CUSTOM_EXCEPTION_ERROR =
+            "Custom Exception class should be thrown.";
+    private static final String SIX_OR_MORE_CHARS_ERROR =
+            "with 6 or more characters should be registered.";
+
     private static RegistrationService registrationService;
-    private User user1;
-    private User user2;
-    private User user3;
+    private User user;
 
     @BeforeAll
     static void setUpAll() {
@@ -25,202 +32,254 @@ class RegistrationServiceImplTest {
 
     @BeforeEach
     void setUp() {
-        // Technically valid users.
-        // Three of them for testing edge cases, because duplicates are not allowed to store.
-        // Clearing static storage list before each test.
-        user1 = new User();
-        user1.setLogin("uniqueLogin1");
-        user1.setPassword("password");
-        user1.setAge(20);
+        user = new User();
 
-        user2 = new User();
-        user2.setLogin("uniqueLogin2");
-        user2.setPassword("password");
-        user2.setAge(20);
-
-        user3 = new User();
-        user3.setLogin("uniqueLogin3");
-        user3.setPassword("password");
-        user3.setAge(20);
+        user.setLogin("uniqueLogin1");
+        user.setPassword("validPassword1");
+        user.setAge(25);
 
         Storage.people.clear();
     }
 
     @Test
-    void register_nullLogin_notOk() {
-        user1.setLogin(null);
-        assertThrows(RegistrationException.class, () -> registrationService.register(user1));
+    void register_nullableUser_notOk() {
+        user = null;
 
+        assertThrows(RegistrationException.class, () -> registrationService.register(user),
+                CUSTOM_EXCEPTION_ERROR);
         boolean containsNone = Storage.people.isEmpty();
-        assertTrue(containsNone, "Error. The User was added to the list despite NULL login.");
+
+        assertTrue(containsNone, "User was added despite being null.");
     }
 
     @Test
-    void register_nullPassword_notOk() {
-        user1.setPassword(null);
-        assertThrows(RegistrationException.class, () -> registrationService.register(user1));
+    void register_nullableLogin_notOk() {
+        user.setLogin(null);
 
+        assertThrows(RegistrationException.class, () -> registrationService.register(user),
+                CUSTOM_EXCEPTION_ERROR);
         boolean containsNone = Storage.people.isEmpty();
-        assertTrue(containsNone, "Error. The User was added to the list despite NULL password.");
+
+        assertTrue(containsNone, "User was added despite null login.");
     }
 
     @Test
-    void register_nullAge_notOk() {
-        user1.setAge(null);
-        assertThrows(RegistrationException.class, () -> registrationService.register(user1));
+    void register_nullablePassword_notOk() {
+        user.setPassword(null);
 
+        assertThrows(RegistrationException.class, () -> registrationService.register(user),
+                CUSTOM_EXCEPTION_ERROR);
         boolean containsNone = Storage.people.isEmpty();
-        assertTrue(containsNone, "Error. The User was added to the list despite NULL age.");
+
+        assertTrue(containsNone, "User was added despite null password.");
+    }
+
+    @Test
+    void register_nullableAge_notOk() {
+        user.setAge(null);
+
+        assertThrows(RegistrationException.class, () -> registrationService.register(user),
+                CUSTOM_EXCEPTION_ERROR);
+        boolean containsNone = Storage.people.isEmpty();
+
+        assertTrue(containsNone, "User was added despite null age.");
+    }
+
+    @Test
+    void register_emptyLogin_notOk() {
+        user.setLogin("");
+
+        assertThrows(RegistrationException.class, () -> registrationService.register(user),
+                CUSTOM_EXCEPTION_ERROR);
+        user.setLogin("validLogin");
+        registrationService.register(user);
+        boolean containsOne = Storage.people.size() == 1;
+
+        assertTrue(containsOne, ONLY_ONE_USER_ERROR);
+        assertEquals(Storage.people.get(0).getLogin(), user.getLogin(),
+                "User login in list should be: " + user.getLogin());
+    }
+
+    @Test
+    void register_emptyPassword_notOk() {
+        user.setPassword("");
+
+        assertThrows(RegistrationException.class, () -> registrationService.register(user),
+                CUSTOM_EXCEPTION_ERROR);
+        user.setPassword("validPassword");
+        registrationService.register(user);
+        boolean containsOne = Storage.people.size() == 1;
+
+        assertTrue(containsOne, ONLY_ONE_USER_ERROR);
+        assertEquals(Storage.people.get(0).getPassword(), user.getPassword(),
+                "User password in list should be: " + user.getPassword());
     }
 
     @Test
     void register_tooShortLogin_notOk() {
-        user1.setLogin("");
-        assertThrows(RegistrationException.class, () -> registrationService.register(user1));
+        user.setLogin("abc");
+        User edgeCaseUser = new User();
+        edgeCaseUser.setLogin("distinctLogin");
+        edgeCaseUser.setAge(60);
+        edgeCaseUser.setLogin("abcde"); // edge case
 
-        user2.setLogin("abc");
-        assertThrows(RegistrationException.class, () -> registrationService.register(user2));
+        assertThrows(RegistrationException.class, () -> registrationService.register(user),
+                CUSTOM_EXCEPTION_ERROR);
+        assertThrows(RegistrationException.class, () -> registrationService.register(edgeCaseUser),
+                CUSTOM_EXCEPTION_ERROR);
+        user.setLogin("validLogin");
+        registrationService.register(user);
+        boolean containsOne = Storage.people.size() == 1;
 
-        user3.setLogin("abcde");
-        assertThrows(RegistrationException.class, () -> registrationService.register(user3));
-
-        boolean containsNone = Storage.people.isEmpty();
-        assertTrue(containsNone, "Error. At least one User was added despite "
-                + "login being too short");
+        assertTrue(containsOne, ONLY_ONE_USER_ERROR);
+        assertEquals(Storage.people.get(0).getLogin(), user.getLogin(),
+                "User login in list should be: " + user.getLogin());
     }
 
     @Test
     void register_tooShortPassword_notOk() {
-        user1.setPassword("");
-        assertThrows(RegistrationException.class, () -> registrationService.register(user1));
+        user.setPassword("abc");
+        User edgeCaseUser = new User();
+        edgeCaseUser.setLogin("distinctLogin");
+        edgeCaseUser.setAge(60);
+        edgeCaseUser.setPassword("abcde"); // edge case
 
-        user2.setLogin("abc");
-        assertThrows(RegistrationException.class, () -> registrationService.register(user2));
+        assertThrows(RegistrationException.class, () -> registrationService.register(user),
+                CUSTOM_EXCEPTION_ERROR);
+        assertThrows(RegistrationException.class, () -> registrationService.register(edgeCaseUser),
+                CUSTOM_EXCEPTION_ERROR);
+        user.setPassword("validPassword");
+        registrationService.register(user);
+        boolean containsOne = Storage.people.size() == 1;
 
-        user3.setLogin("abcde");
-        assertThrows(RegistrationException.class, () -> registrationService.register(user3));
-
-        boolean containsNone = Storage.people.isEmpty();
-        assertTrue(containsNone, "Error. At least one User was added despite "
-                + "password being too short");
+        assertTrue(containsOne, ONLY_ONE_USER_ERROR);
+        assertEquals(Storage.people.get(0).getPassword(), user.getPassword(),
+                "User password in list should be: " + user.getPassword());
     }
 
     @Test
-    void register_negativeAge_notOk() {
-        user1.setAge(-134);
-        assertThrows(RegistrationException.class, () -> registrationService.register(user1));
+    void register_negativeOrZeroAge_notOk() {
+        user.setAge(-11);
+        User zeroAgeUser = new User();
+        zeroAgeUser.setLogin("distinctLogin");
+        zeroAgeUser.setPassword("distinctPassword");
+        zeroAgeUser.setAge(0);
 
-        user2.setAge(-1);
-        assertThrows(RegistrationException.class, () -> registrationService.register(user2));
+        assertThrows(RegistrationException.class, () -> registrationService.register(user),
+                CUSTOM_EXCEPTION_ERROR);
+        assertThrows(RegistrationException.class, () -> registrationService.register(zeroAgeUser),
+                CUSTOM_EXCEPTION_ERROR);
+        user.setAge(44);
+        registrationService.register(user);
+        boolean containsOne = Storage.people.size() == 1;
 
-        boolean containsNone = Storage.people.isEmpty();
-        assertTrue(containsNone, "Error. At least one User was added despite "
-                + "the negative input age");
+        assertTrue(containsOne, ONLY_ONE_USER_ERROR);
+        assertEquals(Storage.people.get(0).getAge(), user.getAge(),
+                "User password in list should be: " + user.getAge());
     }
 
     @Test
     void register_tooSmallAge_notOk() {
-        user1.setAge(0);
-        assertThrows(RegistrationException.class, () -> registrationService.register(user1));
+        user.setAge(1);
+        User edgeCaseUser = new User();
+        edgeCaseUser.setLogin("distinctLogin");
+        edgeCaseUser.setPassword("distinctPassword");
+        edgeCaseUser.setAge(17);
 
-        user2.setAge(10);
-        assertThrows(RegistrationException.class, () -> registrationService.register(user2));
+        assertThrows(RegistrationException.class, () -> registrationService.register(user),
+                CUSTOM_EXCEPTION_ERROR);
+        assertThrows(RegistrationException.class, () -> registrationService.register(edgeCaseUser),
+                CUSTOM_EXCEPTION_ERROR);
+        user.setAge(44);
+        registrationService.register(user);
+        boolean containsOne = Storage.people.size() == 1;
 
-        user3.setAge(17);
-        assertThrows(RegistrationException.class, () -> registrationService.register(user3));
-
-        boolean containsNone = Storage.people.isEmpty();
-        assertTrue(containsNone, "Error. At least one User was added despite "
-                + "age being below 18");
+        assertTrue(containsOne, ONLY_ONE_USER_ERROR);
+        assertEquals(Storage.people.get(0).getAge(), user.getAge(),
+                "User password in list should be: " + user.getAge());
     }
 
     @Test
-    void register_sameUserLogin_notOk() {
-        Storage.people.add(user1);
+    void register_loginTaken_notOk() {
+        Storage.people.add(user);
 
-        assertThrows(RegistrationException.class, () -> registrationService.register(user1));
+        assertThrows(RegistrationException.class, () -> registrationService.register(user),
+                CUSTOM_EXCEPTION_ERROR);
+        boolean containsOne = Storage.people.size() == 1;
 
-        boolean containsOnlyOne = Storage.people.size() == 1;
-        assertTrue(containsOnlyOne, "Error. User with same login was added");
+        assertTrue(containsOne, "Added user despite having duplicate login.");
     }
 
     @Test
     void register_differentUserLogin_Ok() {
-        Storage.people.add(user1);
-        registrationService.register(user2);
-        registrationService.register(user3);
+        Storage.people.add(user);
+        User anotherUser = new User();
+        anotherUser.setLogin("differentLogin");
+        anotherUser.setPassword("differentPassword");
+        anotherUser.setAge(25);
 
-        boolean containsThree = Storage.people.size() == 3;
-        assertTrue(containsThree, "Error. The list should contain 3 users.");
+        registrationService.register(anotherUser);
+        boolean containsTwo = Storage.people.size() == 2;
 
-        assertNotEquals(Storage.people.get(0), Storage.people.get(1), "Test failed! "
-                + "There are at least 2 identical Users in the list.");
-        assertNotEquals(Storage.people.get(0), Storage.people.get(2), "Test failed! "
-                + "There are at least 2 identical Users in the list.");
-        assertNotEquals(Storage.people.get(1), Storage.people.get(2), "Test failed! "
-                + "There are at least 2 identical Users in the list.");
+        assertTrue(containsTwo, TWO_USERS_ERROR);
+        assertNotEquals(Storage.people.get(0), Storage.people.get(1),
+                "There are at 2 identical Users in the list.");
     }
 
     @Test
     void register_loginLengthRequired_Ok() {
-        user1.setLogin("abcdef");
-        User actual = registrationService.register(user1);
-        assertEquals(user1.getLogin(), actual.getLogin(), "Test failed! "
-                + "The user with login that is >= 6 characters should be registered.");
+        user.setLogin("abcdef"); // edge case
+        User anotherUser = new User();
+        anotherUser.setLogin("abcdefabcdefabcdef");
+        anotherUser.setPassword("someValidPassword");
+        anotherUser.setAge(25);
 
-        user2.setLogin("abcdefghijk");
-        actual = registrationService.register(user2);
-        assertEquals(user2.getLogin(), actual.getLogin(), "Test failed! "
-                + "The user with login that is >= 6 characters should be registered.");
+        User actual = registrationService.register(user);
+        User actual2 = registrationService.register(anotherUser);
+        boolean containsTwo = Storage.people.size() == 2;
 
-        user3.setLogin("abcdefghijkabcdefghijkabcdefghijk");
-        actual = registrationService.register(user3);
-        assertEquals(user3.getLogin(), actual.getLogin(), "Test failed! "
-                + "The user with login that is >= 6 characters should be registered.");
-
-        boolean containsThree = Storage.people.size() == 3;
-        assertTrue(containsThree, "Error. The list should contain 3 users.");
+        assertEquals(user.getLogin(), actual.getLogin(), "Login "
+                + SIX_OR_MORE_CHARS_ERROR);
+        assertEquals(anotherUser.getLogin(), actual2.getLogin(), "Login "
+                + SIX_OR_MORE_CHARS_ERROR);
+        assertTrue(containsTwo, TWO_USERS_ERROR);
     }
 
     @Test
     void register_passwordLengthRequired_Ok() {
-        user1.setPassword("abcdef");
-        User actual = registrationService.register(user1);
-        assertEquals(user1.getPassword(), actual.getPassword(), "Test failed! "
-                + "The user with password that is >= 6 characters should be registered.");
+        user.setPassword("abcdef"); // edge case
+        User anotherUser = new User();
+        anotherUser.setLogin("someValidLogin");
+        anotherUser.setPassword("abcdefabcdefabcdef");
+        anotherUser.setAge(25);
 
-        user2.setPassword("abcdefghijk");
-        actual = registrationService.register(user2);
-        assertEquals(user2.getPassword(), actual.getPassword(), "Test failed! "
-                + "The user with password that is >= 6 characters should be registered.");
+        User actual = registrationService.register(user);
+        User actual2 = registrationService.register(anotherUser);
+        boolean containsTwo = Storage.people.size() == 2;
 
-        user3.setPassword("abcdefghijkabcdefghijkabcdefghijk");
-        actual = registrationService.register(user3);
-        assertEquals(user3.getPassword(), actual.getPassword(), "Test failed! "
-                + "The user with password that is >= 6 characters should be registered.");
-
-        boolean containsThree = Storage.people.size() == 3;
-        assertTrue(containsThree, "Error. The list should contain 3 users.");
+        assertEquals(user.getPassword(), actual.getPassword(), "Password "
+                + SIX_OR_MORE_CHARS_ERROR);
+        assertEquals(anotherUser.getPassword(), actual2.getPassword(), "Password "
+                + SIX_OR_MORE_CHARS_ERROR);
+        assertTrue(containsTwo, TWO_USERS_ERROR);
     }
 
     @Test
     void register_atLeast18Age_Ok() {
-        user1.setAge(18);
-        User actual = registrationService.register(user1);
-        assertEquals(user1.getAge(), actual.getAge(), "Test failed! "
-                + "The user which is 18 years or more should be registered.");
+        user.setAge(18); // edge case
+        User anotherUser = new User();
+        anotherUser.setLogin("someValidLogin");
+        anotherUser.setPassword("someValidPassword");
+        anotherUser.setAge(100);
 
-        user2.setAge(58);
-        actual = registrationService.register(user2);
-        assertEquals(user2.getAge(), actual.getAge(), "Test failed! "
-                + "The user which is 18 years or more should be registered.");
+        User actual = registrationService.register(user);
+        User actual2 = registrationService.register(anotherUser);
+        boolean containsTwo = Storage.people.size() == 2;
 
-        user3.setAge(101);
-        actual = registrationService.register(user3);
-        assertEquals(user3.getAge(), actual.getAge(), "Test failed! "
-                + "The user which is 18 years or more should be registered.");
-
-        boolean containsThree = Storage.people.size() == 3;
-        assertTrue(containsThree, "Error. The list should contain 3 users.");
+        assertEquals(user.getAge(), actual.getAge(),
+                "Age 18 or above should be on the list.");
+        assertEquals(anotherUser.getAge(), actual2.getAge(),
+                "Age 18 or above should be on the list.");
+        assertTrue(containsTwo, TWO_USERS_ERROR);
     }
 }
