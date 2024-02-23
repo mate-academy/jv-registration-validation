7983e77a28e8c3c7585ab9de440cd14be093f7bf
vladislavdzadevic <vladdzyadevich@gmail.com>
2024-02-23 17:07:15 +0100

    Initial commit

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,28 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.InvalidDataException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MINIMUM_LENGTH_OF_LOGIN_AND_PASSWORD = 6;
+    private static final int MINIMUM_USER_AGE_TO_LOGIN = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new InvalidDataException("User should be not null");
+        }
+        if (storageDao.get(user.getLogin()) == null
+                && user.getLogin().length() >= MINIMUM_LENGTH_OF_LOGIN_AND_PASSWORD
+                && user.getPassword().length() >= MINIMUM_LENGTH_OF_LOGIN_AND_PASSWORD
+                && user.getAge() >= MINIMUM_USER_AGE_TO_LOGIN) {
+            storageDao.add(user);
+        } else {
+            throw new InvalidDataException("Can't add the user");
+        }
+
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -0,0 +1,112 @@
+package core.basesyntax;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.InvalidDataException;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationServiceTest {
+    public static final int MINIMUM_LENGTH_OF_LOGIN_AND_PASSWORD = 6;
+    public static final int MINIMUM_USER_AGE_TO_LOGIN = 18;
+    private static StorageDao storageDao;
+    private static RegistrationService registrationService;
+    private static User user;
+    private static Storage storage;
+
+    @BeforeAll
+    public static void initializeResources() {
+        storageDao = new StorageDaoImpl();
+        registrationService = new RegistrationServiceImpl();
+        user = new User();
+        storage = new Storage();
+    }
+
+    @Test
+    void userShouldBeNoNull() {
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(null);
+        });
+    }
+
+    @Test
+    void validLogin_OK() {
+        user.setLogin("Vova_Vovin");
+        int actual = user.getLogin().length();
+        boolean expected = actual >= MINIMUM_LENGTH_OF_LOGIN_AND_PASSWORD;
+        assertTrue(expected);
+    }
+
+    @Test
+    void validPassword_OK() {
+        user.setPassword("Vova_Vovin");
+        int actual = user.getPassword().length();
+        boolean expected = actual >= MINIMUM_LENGTH_OF_LOGIN_AND_PASSWORD;
+        assertTrue(expected);
+    }
+
+    @Test
+    void notValidPassword() {
+        user.setPassword("Vova1");
+        int actual = user.getPassword().length();
+        boolean expected = actual >= MINIMUM_LENGTH_OF_LOGIN_AND_PASSWORD;
+        assertFalse(expected);
+    }
+
+    @Test
+    void notValidLogin() {
+        user.setPassword("Vova");
+        int actual = user.getPassword().length();
+        boolean expected = actual >= MINIMUM_LENGTH_OF_LOGIN_AND_PASSWORD;
+        assertFalse(expected);
+    }
+
+    @Test
+    void validUserAge_OK() {
+        user.setAge(18);
+        int actual = user.getAge();
+        boolean expected = actual >= MINIMUM_USER_AGE_TO_LOGIN;
+        assertTrue(expected);
+    }
+
+    @Test
+    void notValidUserAge() {
+        user.setAge(17);
+        int actual = user.getAge();
+        boolean expected = actual >= MINIMUM_USER_AGE_TO_LOGIN;
+        assertFalse(expected);
+    }
+
+    @Test
+    void registrationWasOk() {
+        user = new User();
+        user.setAge(18);
+        user.setLogin("1234567");
+        user.setPassword("zxcqwe123");
+        User actual;
+        actual = registrationService.register(user);
+        assertEquals(user, actual);
+
+    }
+
+    @Test
+    void registrationWasNotOk() {
+        User actual = new User();
+        user = new User();
+        user.setAge(18);
+        user.setLogin("1234567");
+        user.setPassword("qwert");
+        assertThrows(InvalidDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+}
