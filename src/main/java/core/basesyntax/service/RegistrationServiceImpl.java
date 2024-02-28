cc71abe680b4315aa2a0f9961faa5dea32376981
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-02-28 12:28:49 +0200

    added hw_solution

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,75 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.UserAddException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MINIMUM_DEMANDING_LENGTH = 6;
+    private static final int MINIMUM_DEMANDING_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user != null) {
+            validationUserData(user);
+            checkIfUserExist(user.getLogin());
+            return storageDao.add(user);
+        }
+        throw new UserAddException("There is no user to add , because user is null");
+    }
+
+    private boolean checkIfUserExist(String login) {
+        User foundedUser = storageDao.get(login);
+        if (foundedUser != null) {
+            if (foundedUser.getLogin()
+                    .equals(login)) {
+                throw new UserAddException("User "
+                        + login + " has already been added");
+            }
+        }
+        return false;
+    }
+
+    private void validationUserData(User user) {
+        chekId(user.getId());
+        checkLoginLength(user.getLogin());
+        checkPasswordLength(user.getPassword());
+        checkUserAge(user.getAge());
+    }
+
+    private boolean chekId(long id) {
+        if (id <= 0) {
+            throw new UserAddException("User id should be positive number");
+        }
+        return true;
+    }
+
+    private boolean checkUserAge(Integer age) {
+        if (age == null || age < 0
+                || age < MINIMUM_DEMANDING_AGE) {
+            throw new UserAddException("User age  should be more or equals "
+                    + MINIMUM_DEMANDING_AGE
+                    + " years");
+        }
+        return true;
+    }
+
+    private boolean checkLoginLength(String login) {
+        if (login == null || login.length() < MINIMUM_DEMANDING_LENGTH) {
+            throw new UserAddException("Length of user login should be more or equals "
+                    + MINIMUM_DEMANDING_LENGTH
+                    + "symbols");
+        }
+        return true;
+    }
+
+    private boolean checkPasswordLength(String password) {
+        if (password == null || password.length() < MINIMUM_DEMANDING_LENGTH) {
+            throw new UserAddException("Length of user password should be more or equals "
+                    + MINIMUM_DEMANDING_LENGTH
+                    + "symbols");
+        }
+        return true;
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,167 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.UserAddException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registrationService;
+    private static StorageDao storageDao;
+    private User newUser;
+
+    @BeforeEach
+    void setUp() {
+        newUser = new User();
+        newUser.setId(1L);
+        newUser.setLogin("Markus");
+        newUser.setPassword("MarkPass");
+        newUser.setAge(29);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        registrationService = new RegistrationServiceImpl();
+        storageDao = new StorageDaoImpl();
+    }
+
+    @Test
+    void register_existingUser_notOk() {
+        storageDao.add(newUser);
+        User existingUser = storageDao.get(newUser.getLogin());
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(existingUser));
+    }
+
+    @Test
+    void register_nullUser_notOk() {
+        newUser = null;
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_nonExistingUser_Ok() {
+        User actualUser = registrationService.register(newUser);
+        User expectedUser = storageDao.get(newUser.getLogin());
+        assertEquals(expectedUser, actualUser);
+    }
+
+    @Test
+    void register_userLoginShortThen6Symbols_notOk() {
+        newUser.setLogin("Mark");
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userLoginEmpty_notOk() {
+        newUser.setLogin("");
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userLogin3Symbols_notOk() {
+        newUser.setLogin("Mar");
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userLogin5Symbols_notOk() {
+        newUser.setLogin("Marku");
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userPasswordShortThen6Symbols_notOk() {
+        newUser.setPassword("Mark");
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userPasswordEmpty_notOk() {
+        newUser.setPassword("");
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userPassword3Symbols_notOk() {
+        newUser.setPassword("Mar");
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userPassword5Symbols_notOk() {
+        newUser.setPassword("Marku");
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userUnderage18_notOk() {
+        newUser.setAge(17);
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userNegativeAge_notOk() {
+        newUser.setAge(-5);
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userCorrectAge_Ok() {
+        newUser.setAge(18);
+        assertTrue(newUser.getAge() >= 18);
+    }
+
+    @Test
+    void register_userLoginNull_notOk() {
+        newUser.setLogin(null);
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userPasswordNull_notOk() {
+        newUser.setPassword(null);
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void register_userAgeNull_notOk() {
+        newUser.setAge(null);
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+
+    @Test
+    void checkUserId_positive_Ok() {
+        newUser.setId(5L);
+        assertTrue(newUser.getId() > 0, "User id should be positive number "
+                + "but id = " + newUser.getId());
+    }
+
+    @Test
+    void checkUserId_negative_notOk() {
+        newUser.setId(-5L);
+        assertThrows(UserAddException.class, () ->
+                registrationService.register(newUser));
+    }
+}
