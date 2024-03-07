1faebf7e4729f7ced9e9e2bcca900a000c44ead1
VolandevlodD <vladradko2000@gmail.com>
2024-03-07 19:41:13 +0200

    Some improvements were made based on a colleague's review.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,13 +5,13 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    public static final String NULL_MSG = "%s can`t be null";
-    public static final String LESS_MSG = "%s less than minimum: expected %d but was %d!";
-    public static final String EXIST_MSG = "A user with this email already exists!";
+    public static final String NULL_MESSAGE = "%s can`t be null";
+    public static final String LESS_MESSAGE = "%s less than minimum: expected %d but was %d!";
+    public static final String EXIST_MESSAGE = "A user with this email already exists!";
     public static final String LOGIN = "Login";
     public static final String PASSWORD = "Password";
     public static final String AGE = "Age";
-    public static final int USER_DATA_MIN_LENGTH = 6;
+    public static final int LOGIN_PASSWORD_MIN_LENGTH = 6;
     public static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
@@ -20,38 +20,51 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user == null) {
             throw new RegistrationException("User can`t be null!");
         }
-        if (user.getLogin() == null) {
-            throw new RegistrationException(String.format(NULL_MSG, LOGIN));
-        }
-        if (user.getPassword() == null) {
-            throw new RegistrationException(String.format(NULL_MSG, PASSWORD));
-        }
-        if (user.getAge() == null) {
-            throw new RegistrationException(String.format(NULL_MSG, AGE));
-        }
-        if (user.getLogin().length() < USER_DATA_MIN_LENGTH) {
-            throw new RegistrationException(String.format(
-                    LESS_MSG,
-                    LOGIN,
-                    USER_DATA_MIN_LENGTH,
-                    user.getLogin().length()));
-        }
-        if (user.getPassword().length() < USER_DATA_MIN_LENGTH) {
-            throw new RegistrationException(String.format(
-                    LESS_MSG,
-                    PASSWORD,
-                    USER_DATA_MIN_LENGTH,
-                    user.getPassword().length()));
-        }
-        if (user.getAge() < MIN_AGE) {
-            throw new RegistrationException(String.format(LESS_MSG, AGE, MIN_AGE, user.getAge()));
-        }
-        String currentUserLogin = user.getLogin();
-        if (storageDao.get(currentUserLogin) != null) {
-            throw new RegistrationException(EXIST_MSG);
-        }
+        validateLogin(user);
+        validatePassword(user);
+        validateAge(user);
         storageDao.add(user);
         return user;
+    }
+
+    private void validateLogin(User user) {
+        String login = user.getLogin();
+        if (login == null) {
+            throw new RegistrationException(String.format(NULL_MESSAGE, LOGIN));
+        }
+        if (login.length() < LOGIN_PASSWORD_MIN_LENGTH) {
+            throw new RegistrationException(String.format(
+                    LESS_MESSAGE,
+                    LOGIN,
+                    LOGIN_PASSWORD_MIN_LENGTH,
+                    user.getLogin().length()));
+        }
+        if (storageDao.get(login) != null) {
+            throw new RegistrationException(EXIST_MESSAGE);
+        }
+    }
+
+    private void validatePassword(User user) {
+        if (user.getPassword() == null) {
+            throw new RegistrationException(String.format(NULL_MESSAGE, PASSWORD));
+        }
+        if (user.getPassword().length() < LOGIN_PASSWORD_MIN_LENGTH) {
+            throw new RegistrationException(String.format(
+                    LESS_MESSAGE,
+                    PASSWORD,
+                    LOGIN_PASSWORD_MIN_LENGTH,
+                    user.getPassword().length()));
+        }
+    }
+
+    private void validateAge(User user) {
+        if (user.getAge() == null) {
+            throw new RegistrationException(String.format(NULL_MESSAGE, AGE));
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException(
+                    String.format(LESS_MESSAGE, AGE, MIN_AGE, user.getAge()));
+        }
     }
 
     public static class RegistrationException extends RuntimeException {
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,14 +1,14 @@
 package core.basesyntax.service;
 
 import static core.basesyntax.service.RegistrationServiceImpl.AGE;
-import static core.basesyntax.service.RegistrationServiceImpl.EXIST_MSG;
-import static core.basesyntax.service.RegistrationServiceImpl.LESS_MSG;
+import static core.basesyntax.service.RegistrationServiceImpl.EXIST_MESSAGE;
+import static core.basesyntax.service.RegistrationServiceImpl.LESS_MESSAGE;
 import static core.basesyntax.service.RegistrationServiceImpl.LOGIN;
+import static core.basesyntax.service.RegistrationServiceImpl.LOGIN_PASSWORD_MIN_LENGTH;
 import static core.basesyntax.service.RegistrationServiceImpl.MIN_AGE;
-import static core.basesyntax.service.RegistrationServiceImpl.NULL_MSG;
+import static core.basesyntax.service.RegistrationServiceImpl.NULL_MESSAGE;
 import static core.basesyntax.service.RegistrationServiceImpl.PASSWORD;
 import static core.basesyntax.service.RegistrationServiceImpl.RegistrationException;
-import static core.basesyntax.service.RegistrationServiceImpl.USER_DATA_MIN_LENGTH;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -21,6 +21,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
+    private static final String LOGIN_OK = "userLogin";
+    private static final String LOGIN_NOT_OK = "login";
+    private static final String PASSWORD_OK = "userPassword";
+    private static final String PASSWORD_NOT_OK = "pass";
+    private static final int AGE_OK = 18;
+    private static final int AGE_NOT_OK = 17;
+    private static final int AGE_ZERO = 0;
+    private static final String EMPTY_STRING = "pass";
     private static RegistrationServiceImpl registrationService;
     private User user;
 
@@ -31,7 +39,7 @@ class RegistrationServiceImplTest {
 
     @BeforeEach
     void setUp() {
-        user = new User("userLogin", "userPassword", 18);
+        user = new User(LOGIN_OK, PASSWORD_OK, AGE_OK);
     }
 
     @AfterEach
@@ -53,70 +61,77 @@ class RegistrationServiceImplTest {
     @Test
     void registerSameLoginNotOk() {
         Storage.people.add(user);
-        User second = new User(user.getLogin(), "321321", 25);
-        assertException(second, EXIST_MSG);
+        assertException(user, EXIST_MESSAGE);
     }
 
     @Test
     void registerLoginNullNotOk() {
         user.setLogin(null);
-        assertException(user, String.format(NULL_MSG, LOGIN));
+        assertException(user, String.format(NULL_MESSAGE, LOGIN));
     }
 
     @Test
     void registerPasswordNullNotOk() {
         user.setPassword(null);
-        assertException(user, String.format(NULL_MSG, PASSWORD));
+        assertException(user, String.format(NULL_MESSAGE, PASSWORD));
     }
 
     @Test
     void registerAgeNullNotOk() {
         user.setAge(null);
-        assertException(user, String.format(NULL_MSG, AGE));
+        assertException(user, String.format(NULL_MESSAGE, AGE));
     }
 
     @Test
     void registerLoginShortNotOk() {
-        user.setLogin("0123");
+        user.setLogin(LOGIN_NOT_OK);
         assertException(
                 user,
-                String.format(LESS_MSG, LOGIN, USER_DATA_MIN_LENGTH, user.getLogin().length()));
-        user.setLogin("");
+                String.format(
+                        LESS_MESSAGE,
+                        LOGIN,
+                        LOGIN_PASSWORD_MIN_LENGTH,
+                        user.getLogin().length()));
+        user.setLogin(EMPTY_STRING);
         assertException(
                 user,
-                String.format(LESS_MSG, LOGIN, USER_DATA_MIN_LENGTH, user.getLogin().length()));
+                String.format(
+                        LESS_MESSAGE,
+                        LOGIN,
+                        LOGIN_PASSWORD_MIN_LENGTH,
+                        user.getLogin().length()));
     }
 
     @Test
     void registerPasswordShortNotOk() {
-        user.setPassword("0123");
+        user.setPassword(PASSWORD_NOT_OK);
         assertException(
                 user,
                 String.format(
-                        LESS_MSG,
+                        LESS_MESSAGE,
                         PASSWORD,
-                        USER_DATA_MIN_LENGTH,
+                        LOGIN_PASSWORD_MIN_LENGTH,
                         user.getPassword().length()));
-        user.setPassword("");
+        user.setPassword(EMPTY_STRING);
         assertException(
                 user,
                 String.format(
-                        LESS_MSG,
+                        LESS_MESSAGE,
                         PASSWORD,
-                        USER_DATA_MIN_LENGTH,
+                        LOGIN_PASSWORD_MIN_LENGTH,
                         user.getPassword().length()));
     }
 
     @Test
     void registerAgeLessNotOk() {
-        user.setAge(17);
+        user.setAge(AGE_NOT_OK);
         assertException(
                 user,
-                String.format(LESS_MSG, AGE, MIN_AGE, user.getAge()));
-        user.setAge(0);
+                String.format(LESS_MESSAGE, AGE, MIN_AGE, user.getAge()));
+        user.setAge(AGE_ZERO);
         assertException(
                 user,
-                String.format(LESS_MSG, AGE, MIN_AGE, user.getAge()));
+                String.format(LESS_MESSAGE, AGE, MIN_AGE, user.getAge()));
     }
 
     private void assertException(User user, String expected) {
