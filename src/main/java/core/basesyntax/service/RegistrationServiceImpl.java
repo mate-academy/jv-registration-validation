a1614b7b71e6cc5a21fefacff9f8334376f8cf49
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-03-07 13:46:43 +0200

    update version

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -14,21 +14,44 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        if (user == null || user.getLogin() == null || user.getPassword() == null) {
-            throw new ValidationException("User data can't be null!");
-        }
+        validateUser(user);
+        return storageDao.add(user);
+    }
+
+    private void validateUser(User user) {
+        validateNull(user);
+        validatePassword(user);
+        validateAge(user);
+        validateLogin(user);
+    }
+
+    private void validateLogin(User user) {
         if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
             throw new ValidationException("User login can't be less than 6 symbols!");
-        }
-        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
-            throw new ValidationException("User password can't be less than 6 symbols!");
-        }
-        if (user.getAge() < MIN_AGE) {
-            throw new ValidationException("User age can't be less than 18 years old!");
         }
         if (storageDao.get(user.getLogin()) != null) {
             throw new ValidationException("User with such login is already exist!");
         }
-        return storageDao.add(user);
+    }
+
+    private void validateAge(User user) {
+        if (user.getAge() < MIN_AGE) {
+            throw new ValidationException("User age can't be less than 18 years old!");
+        }
+    }
+
+    private void validatePassword(User user) {
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new ValidationException("User password can't be less than 6 symbols!");
+        }
+    }
+
+    private void validateNull(User user) {
+        if (user == null
+                || user.getLogin() == null
+                || user.getPassword() == null
+                || user.getAge() == null) {
+            throw new ValidationException("User data can't be null!");
+        }
     }
 }
--- src/test/java/core/basesyntax/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -8,33 +8,35 @@ import core.basesyntax.exception.ValidationException;
 import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationService;
 import core.basesyntax.service.RegistrationServiceImpl;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RegistrationServiceTest {
-    private static RegistrationService registrationService;
-    private static StorageDao storageDao;
+    private static final int INCORRECT_AGE = 8;
+    private static final int CORRECT_AGE = 20;
+    private static final long ID = 2;
+    private RegistrationService registrationService;
+    private StorageDao storageDao;
 
-    @BeforeAll
-    static void beforeAll() {
+    @BeforeEach
+    void setUp() {
         registrationService = new RegistrationServiceImpl();
         storageDao = new StorageDaoImpl();
-        setUp();
+        addTestUsers();
     }
 
-    // It this method we add two users in storageDao
-    private static void setUp() {
+    private void addTestUsers() {
         User userBob = new User();
         userBob.setId(1L);
         userBob.setLogin("111111");
         userBob.setPassword("aaaaaa");
-        userBob.setAge(20);
+        userBob.setAge(CORRECT_AGE);
 
         User userJohn = new User();
         userJohn.setId(2L);
         userJohn.setLogin("222222");
         userJohn.setPassword("bbbbbb");
-        userJohn.setAge(22);
+        userJohn.setAge(CORRECT_AGE);
 
         storageDao.add(userBob);
         storageDao.add(userJohn);
@@ -44,16 +46,17 @@ public class RegistrationServiceTest {
     void register_userIncorrectRegistration_NotOk() {
         User user = null;
 
-        assertThrows(ValidationException.class, () -> registrationService.register(user));
+        assertThrows(ValidationException.class, () -> registrationService.register(user),
+                "User can't be null");
     }
 
     @Test
     void register_userCorrectRegistration_Ok() {
         User user = new User();
-        user.setId(3L);
+        user.setId(ID);
         user.setLogin("333333");
         user.setPassword("cccccc");
-        user.setAge(25);
+        user.setAge(CORRECT_AGE);
 
         registrationService.register(user);
     }
@@ -61,66 +64,72 @@ public class RegistrationServiceTest {
     @Test
     void register_userExistLogin_throwException() {
         User user = new User();
-        user.setId(4L);
-        user.setLogin("111111"); // this login is already exist
+        user.setId(ID);
+        user.setLogin("111111");
         user.setPassword("dddddd");
-        user.setAge(85);
+        user.setAge(CORRECT_AGE);
 
-        assertThrows(ValidationException.class, () -> registrationService.register(user));
+        assertThrows(ValidationException.class, () -> registrationService.register(user),
+                "Such user is already exist");
     }
 
     @Test
     void register_userIncorrectLogin_throwException() {
         User user = new User();
-        user.setId(4L);
-        user.setLogin("1111"); // this login is incorrect
-        user.setPassword("dddddd");
-        user.setAge(55);
+        user.setId(ID);
+        user.setLogin("1111");
+        user.setPassword("ddd1dd");
+        user.setAge(CORRECT_AGE);
 
-        assertThrows(ValidationException.class, () -> registrationService.register(user));
+        assertThrows(ValidationException.class, () -> registrationService.register(user),
+                "Login can't be less than 6 symbols");
     }
 
     @Test
     void register_userIncorrectPassword_throwException() {
         User user = new User();
-        user.setId(4L);
-        user.setLogin("1111");
-        user.setPassword("dddd"); // this password is incorrect
-        user.setAge(85);
+        user.setId(ID);
+        user.setLogin("111122");
+        user.setPassword("dddd");
+        user.setAge(CORRECT_AGE);
 
-        assertThrows(ValidationException.class, () -> registrationService.register(user));
+        assertThrows(ValidationException.class, () -> registrationService.register(user),
+                "Password can't be less than 6 symbols");
     }
 
     @Test
     void register_userIncorrectAge_throwException() {
         User user = new User();
-        user.setId(4L);
+        user.setId(ID);
         user.setLogin("555555");
         user.setPassword("dddddd");
-        user.setAge(5); // age can't be less than 18
+        user.setAge(INCORRECT_AGE);
 
-        assertThrows(ValidationException.class, () -> registrationService.register(user));
+        assertThrows(ValidationException.class, () -> registrationService.register(user),
+                "Age can't be less than 18");
     }
 
     @Test
     void register_userNullLogin_throwException() {
         User user = new User();
-        user.setId(4L);
-        user.setLogin(null); // login can't be null
+        user.setId(ID);
+        user.setLogin(null);
         user.setPassword("dddddd");
-        user.setAge(5);
+        user.setAge(CORRECT_AGE);
 
-        assertThrows(ValidationException.class, () -> registrationService.register(user));
+        assertThrows(ValidationException.class, () -> registrationService.register(user),
+                "Can't be null login");
     }
 
     @Test
     void register_userNullPassword_throwException() {
         User user = new User();
-        user.setId(4L);
+        user.setId(ID);
         user.setLogin("555555");
-        user.setPassword(null); // password can't be null
-        user.setAge(5); // age can't be less than 18
+        user.setPassword(null);
+        user.setAge(CORRECT_AGE);
 
-        assertThrows(ValidationException.class, () -> registrationService.register(user));
+        assertThrows(ValidationException.class, () -> registrationService.register(user),
+                "Can't be null password");
     }
 }
