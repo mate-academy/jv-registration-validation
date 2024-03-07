0d5920155ad7932dd6429d1dabaf8ca6826c9a9e
Mykola Skrypalov <skripalov1@gmail.com>
2024-03-07 23:35:51 +0200

    *minor change*

--- src/main/java/core/basesyntax/exception/InvalidInputDataException.java
+++ src/main/java/core/basesyntax/exception/InvalidInputDataException.java
@@ -2,9 +2,6 @@ package core.basesyntax.exception;
 
 public class InvalidInputDataException extends RuntimeException {
 
-    public InvalidInputDataException() {
-    }
-
     public InvalidInputDataException(String message) {
         super(message);
     }
