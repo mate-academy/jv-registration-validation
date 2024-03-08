e5c586ba6571baa612af8b192fb143724f051eef
Valentyn Sharshon <woosay333@gmail.com>
2024-03-08 11:56:22 +0200

    changed USER_PASSWORD_IS_NULL constant message

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -12,7 +12,7 @@ public class RegistrationServiceImpl implements RegistrationService {
     private static final String USER_LOGIN_IS_NULL = "User login can`t be null";
     private static final String USER_LOGIN_ALREADY_EXISTS_ERROR_MESSAGE = "User with login %s already exists";
     private static final String USER_LOGIN_LENGTH_LESS_THAN_ERROR_MESSAGE = "User login %s length less then 6 characters";
-    private static final String USER_PASSWORD_IS_NULL = "User password %s can`t be null";
+    private static final String USER_PASSWORD_IS_NULL = "User password can`t be null";
     private static final String USER_PASSWORD_LENGTH_LESS_THAN_ERROR_MESSAGE = "User password %s length less then 6 characters";
     private static final String USER_AGE_LESS_THAN_ERROR_MESSAGE = "User age %d less then 18 years";
     private static final int LOGIN_PASSWORD_MIN_LENGTH = 6;
@@ -40,7 +40,7 @@ public class RegistrationServiceImpl implements RegistrationService {
             throw new UserRegistrationException(String.format(USER_LOGIN_LENGTH_LESS_THAN_ERROR_MESSAGE, user.getLogin()));
         }
         if (Objects.isNull(user.getPassword())) {
-            throw new UserRegistrationException(String.format(USER_PASSWORD_IS_NULL, user.getPassword()));
+            throw new UserRegistrationException(USER_PASSWORD_IS_NULL);
         }
         if (user.getPassword().length() < LOGIN_PASSWORD_MIN_LENGTH) {
             throw new UserRegistrationException(String.format(USER_PASSWORD_LENGTH_LESS_THAN_ERROR_MESSAGE, user.getPassword()));
