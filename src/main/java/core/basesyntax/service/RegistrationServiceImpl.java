f6a65b6193b6d872c8a3827687e25cda7d47e6e9
John <pandastars535@gmail.com>
2024-03-08 17:14:04 +0200

    initial commit

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,5 +1,7 @@
 package core.basesyntax.service;
 
+import core.basesyntax.LoginValidateException;
+import core.basesyntax.PasswordValidate;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
@@ -7,8 +9,25 @@ import core.basesyntax.model.User;
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
 
+    private boolean loginValidation(String login) {
+        if (login.length() <= 4 || login == null) {
+            throw new LoginValidateException("your login must be more than 4 characters");
+        } else if (!Character.isLetter(login.charAt(0))) {
+            throw new LoginValidateException("your password should start from the letter");
+        }
+        return true;
+    }
+
+    private boolean passwordValidation(String password) {
+        if (password == null || password.length() < 6) {
+            throw new PasswordValidate("password must be longer than six characters");
+        }
+        return true;
+    }
+
     @Override
     public User register(User user) {
+        storageDao.get(user.getLogin());
         return null;
     }
 }
