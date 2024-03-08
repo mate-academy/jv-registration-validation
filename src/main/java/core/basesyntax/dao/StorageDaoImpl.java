3c5f396d1e916a4266fd0a0f6cbd5c711a0b1eed
MarynaBodina <bananafox2002@gmail.com>
2024-03-08 17:09:36 +0200

    "registration service is done "

--- src/main/java/core/basesyntax/dao/StorageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/StorageDaoImpl.java
@@ -2,22 +2,28 @@ package core.basesyntax.dao;
 
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class StorageDaoImpl implements StorageDao {
-    private static Long index = 0L;
+    private static final AtomicLong index = new AtomicLong(0);
 
     @Override
     public User add(User user) {
-        user.setId(++index);
-        Storage.people.add(user);
+        long id = index.incrementAndGet();
+        user.setId(id);
+        synchronized (Storage.people) {
+            Storage.people.add(user);
+        }
         return user;
     }
 
     @Override
     public User get(String login) {
-        for (User user : Storage.people) {
-            if (user.getLogin().equals(login)) {
-                return user;
+        synchronized (Storage.people) {
+            for (User user : Storage.people) {
+                if (user.getLogin().equals(login)) {
+                    return user;
+                }
             }
         }
         return null;
--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,14 +1,36 @@
 package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
-import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private final StorageDao storageDao = new StorageDaoImpl();
+    private final StorageDao storageDao;
+
+    public RegistrationServiceImpl(StorageDao storageDao) {
+        this.storageDao = storageDao;
+    }
 
     @Override
-    public User register(User user) {
-        return null;
+    public User register(User user) throws RegistrationException {
+        if (user == null || user.getLogin() == null || user.getPassword()
+                == null || user.getAge() == null) {
+            throw new IllegalArgumentException("User or its properties cannot be null");
+        }
+
+        if (user.getLogin().length() < 6 || user.getPassword().length() < 6) {
+            throw new RegistrationException("Login and password must contain "
+                    + "at least 6 characters");
+        }
+
+        if (user.getAge() < 18) {
+            throw new RegistrationException("User must be at least 18 years old");
+        }
+
+        User existingUser = storageDao.get(user.getLogin());
+        if (existingUser != null) {
+            throw new RegistrationException("User with this login already exists");
+        }
+
+        return storageDao.add(user);
     }
 }
