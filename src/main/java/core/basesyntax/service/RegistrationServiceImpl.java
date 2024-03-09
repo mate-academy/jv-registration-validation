e2b5a7958a53f194ae9e1bd34e35c0d267223931
John <pandastars535@gmail.com>
2024-03-09 17:31:14 +0200

    added tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -4,10 +4,14 @@ import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.exception.AgeValidationException;
 import core.basesyntax.exception.LoginValidateException;
-import core.basesyntax.exception.PasswordValidate;
+import core.basesyntax.exception.PasswordValidateException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MINIMAL_AGE = 18;
+    private static final int MINIMAL_PASSWORD_LENGTH = 6;
+    private static final int MINIMAL_LOGIN_LENGTH = 4;
+    private static final int FIRST_LOGIN_CHARACTER_INDEX = 0;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
@@ -19,9 +23,9 @@ public class RegistrationServiceImpl implements RegistrationService {
     }
 
     private void loginValidation(String login) {
-        if (login == null || login.length() <=4) {
+        if (login == null || login.length() <= MINIMAL_LOGIN_LENGTH) {
             throw new LoginValidateException("your login must be more than 4 characters");
-        } else if (!Character.isLetter(login.charAt(0))) {
+        } else if (!Character.isLetter(login.charAt(FIRST_LOGIN_CHARACTER_INDEX))) {
             throw new LoginValidateException("your login should start from the letter");
         }
     }
@@ -29,21 +33,21 @@ public class RegistrationServiceImpl implements RegistrationService {
     private void checkExistUserInDataBase(String login) {
         loginValidation(login);
         if (storageDao.get(login) != null) {
-            throw new LoginValidateException("consumer with that name is already " +
-                    "exist please try another one login or input password for that one");
+            throw new LoginValidateException("consumer with " + login + "login is already "
+                    + "exist please try another one login try to login with password");
         }
     }
 
     private void passwordValidation(String password) {
-        if (password == null || password.length() < 6) {
-            throw new PasswordValidate("password must be longer than six characters");
+        if (password == null || password.length() <= MINIMAL_PASSWORD_LENGTH) {
+            throw new PasswordValidateException("password must be longer than six characters");
         }
     }
 
     private void ageValidation(int age) {
-        if (age < 18) {
+        if (age < MINIMAL_AGE) {
             throw new AgeValidationException(
-                    "User age must be at least " + 18);
+                    "User age must be at least " + MINIMAL_AGE);
         }
     }
 }
