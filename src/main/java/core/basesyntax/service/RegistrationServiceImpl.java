53e5d423302e42f5351b59b317a255fc3f4d96cb
HAIOVIY <gaevoywork@gmail.com>
2024-03-10 21:28:53 +0200

    created unchecked exception class, implemented tests, the register method is implemented

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,60 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exceptions.RegistrationServiceException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LENGTH = 6;
+    private static final int MIN_AGE = 18;
+
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        checkValidLogin(user);
+        checkPassword(user);
+        checkAge(user);
+        return storageDao.add(user);
+    }
+
+    private void checkValidLogin(User user) {
+        if (user.getLogin() == null
+                || user.getLogin().length() < MIN_LENGTH) {
+            throw new RegistrationServiceException(
+                    "User login can not be least then "
+                            + MIN_LENGTH
+                            + " chars!"
+            );
+        } else {
+            checkEmailDuplicate(user);
+        }
+    }
+
+    private void checkEmailDuplicate(User user) {
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationServiceException(
+                    "User with login: "
+                            + user.getLogin()
+                            + " has already registered!"
+            );
+        }
+    }
+
+    private void checkPassword(User user) {
+        if (user.getPassword() == null
+                || user.getPassword().length() < MIN_LENGTH) {
+            throw new RegistrationServiceException(
+                    "User password can not be least then "
+                            + MIN_LENGTH
+                            + " chars!"
+            );
+        }
+    }
+
+    private void checkAge(User user) {
+        if (user.getAge() == null || user.getAge() < MIN_AGE) {
+            throw new RegistrationServiceException("User must be of legal age!");
+        }
     }
 }
