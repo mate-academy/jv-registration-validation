0d42dac768317268be425afa3af3765301e4a75b
Milosz <szwajkowski.milosz@gmail.com>
2024-03-10 17:10:15 +0100

    Solved jv-registration-validation task:
    Implemented registration service and its tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,46 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MINIMUM_AGE = 18;
+    private static final int REQ_PASS_LENGTH = 6;
+    private static final int REQ_LOGIN_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        validateUser(user);
+        storageDao.add(user);
+        return user;
+    }
+
+    private void validateUser(User user) {
+        if (user.getLogin() == null) {
+            throw new InvalidDataException("Login can't be empty");
+        }
+        if (user.getPassword() == null) {
+            throw new InvalidDataException("Password can't be empty");
+        }
+        if (user.getAge() == null) {
+            throw new InvalidDataException("Age can't be empty");
+        }
+        if (user.getAge() < MINIMUM_AGE) {
+            throw new InvalidDataException("Minimum age is 18 years old. User too young: "
+                    + user.getAge());
+        }
+        if (user.getLogin().length() < REQ_LOGIN_LENGTH) {
+            throw new InvalidDataException("Login too short. Minimum length: "
+                    + REQ_LOGIN_LENGTH);
+        }
+        if (user.getPassword().length() < REQ_LOGIN_LENGTH) {
+            throw new InvalidDataException("Password too short. Minimum length: "
+                    + REQ_PASS_LENGTH);
+        }
+        if (loginAlreadyExist(user.getLogin())) {
+            throw new InvalidDataException("User: " + user.getLogin() + " already exist in DB");
+        }
+    }
+
+    private boolean loginAlreadyExist(String login) {
+        return storageDao.get(login) != null;
     }
 }
