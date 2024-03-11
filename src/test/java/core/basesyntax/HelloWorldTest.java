2dda833ac5f578083b741d9052b76817b6074c72
Chopyk Mykola <record7654@gmail.com>
2024-03-11 14:24:11 +0200

    Resolved

--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -8,6 +8,7 @@ import core.basesyntax.exceptions.RegistrationFailureException;
 import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationService;
 import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,8 +18,15 @@ import org.junit.jupiter.api.Test;
  */
 public class HelloWorldTest {
     private static final int VALID_AGE = 20;
+    private static final String VALID_LOGIN = "ValidLogin";
+    private static final String VALID_PASSWORD_FIRST_TYPE = "password";
+    private static final String VALID_PASSWORD_SECOND_TYPE = "123456";
+    private static final String INVALID_LOGIN = "login";
+    private static final String INVALID_PASSWORD = "1234";
+    private static final int ZERO_AGE = 0;
+    private static final int INVALID_AGE = 17;
     private static RegistrationService registrationService;
-    private static User validUser;
+    private static User user;
 
     @BeforeAll
     static void beforeAll() {
@@ -27,102 +35,100 @@ public class HelloWorldTest {
 
     @BeforeEach
     void beforeEach() {
-        validUser = new User();
-        validUser.setLogin("ValidLogin");
-        validUser.setPassword("password");
-        validUser.setAge(VALID_AGE);
+        user = new User();
+        user.setLogin(VALID_LOGIN);
+        user.setPassword(VALID_PASSWORD_FIRST_TYPE);
+        user.setAge(VALID_AGE);
+    }
+
+    @AfterEach
+    void afterEach() {
+        Storage.people.clear();
     }
 
     @Test
     void register_addUser_Ok() {
-        validUser.setLogin("newLogin");
-        User result = registrationService.register(validUser);
-        assertEquals(validUser, result, "The valid user and result aren't the same.");
+        User addedUser = registrationService.register(user);
+        assertEquals(user, addedUser, "The valid user and result aren't the same.");
     }
 
     @Test
     void register_passwordEdges_Ok() {
-        validUser.setLogin("Marco1");
-        validUser.setPassword("123456");
-        User result = registrationService.register(validUser);
-        assertEquals(result, validUser, "User and added result aren't the same.");
+        User addedUser = registrationService.register(user);
+        assertEquals(user, addedUser, "User and dded User aren't the same.");
 
-        validUser = new User();
-        validUser.setLogin("Parmezan");
-        validUser.setPassword("12345678910");
-        validUser.setAge(VALID_AGE);
-
-        result = registrationService.register(validUser);
-        assertEquals(result, validUser, "User and added result aren't the same.");
+        Storage.people.clear();
+        user.setPassword(VALID_PASSWORD_SECOND_TYPE);
+        addedUser = registrationService.register(user);
+        assertEquals(user, addedUser, "User and added User aren't the same.");
     }
 
     @Test
     void register_nullUser_notOk() {
-        validUser = null;
+        user = null;
         assertThrows(RegistrationFailureException.class,
-                () -> registrationService.register(validUser),
+                () -> registrationService.register(user),
                 "Invalid result for null user.");
     }
 
     @Test
     void register_addIdenticalUsers_notOk() {
-        validUser.setLogin("Antonio");
-        Storage.people.add(validUser);
+        Storage.people.add(user);
         assertThrows(RegistrationFailureException.class,
-                () -> registrationService.register(validUser),
+                () -> registrationService.register(user),
                 "Added 2 identical users.");
     }
 
     @Test
     void register_addYoungUser_notOk() {
-        validUser.setAge(15);
+        user.setAge(INVALID_AGE);
         assertThrows(RegistrationFailureException.class,
-                () -> registrationService.register(validUser),
+                () -> registrationService.register(user),
                 "Added user that younger then 18 y.o.");
 
-        validUser.setAge(0);
+        user.setAge(ZERO_AGE);
         assertThrows(RegistrationFailureException.class,
-                () -> registrationService.register(validUser),
+                () -> registrationService.register(user),
                 "Added user that younger then 18 y.o.");
     }
 
     @Test
     void register_nullLogin_notOk() {
-        validUser.setLogin(null);
+        user.setLogin(null);
         assertThrows(RegistrationFailureException.class,
-                () -> registrationService.register(validUser),
+                () -> registrationService.register(user),
                 "Added user with null login.");
     }
 
     @Test
     void register_nullPassword_notOk() {
-        validUser.setPassword(null);
+        user.setPassword(null);
         assertThrows(RegistrationFailureException.class,
-                () -> registrationService.register(validUser),
+                () -> registrationService.register(user),
                 "Added user with null password.");
     }
 
     @Test
     void register_nullAge_notOk() {
-        validUser.setAge(null);
+        user.setAge(null);
         assertThrows(RegistrationFailureException.class,
-                () -> registrationService.register(validUser),
+                () -> registrationService.register(user),
                 "Added user with null age.");
     }
 
     @Test
     void register_shortLogin_notOk() {
-        validUser.setLogin("Login");
+        user.setLogin(INVALID_LOGIN);
         assertThrows(RegistrationFailureException.class,
-                () -> registrationService.register(validUser),
+                () -> registrationService.register(user),
                 "Added user with short login.");
     }
 
     @Test
     void register_shortPassword_notOk() {
-        validUser.setPassword("1234");
+        user.setPassword(INVALID_PASSWORD);
         assertThrows(RegistrationFailureException.class,
-                () -> registrationService.register(validUser),
+                () -> registrationService.register(user),
                 "Added user with short password");
     }
 }
