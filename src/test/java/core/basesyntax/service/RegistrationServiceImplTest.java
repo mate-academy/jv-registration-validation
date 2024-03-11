5da586d7ece6e8b72e217e207ff87e0d2bf67578
Valentyn Sharshon <woosay333@gmail.com>
2024-03-11 16:14:01 +0200

    changed RegistrationServiceImplTest test class based on mentor's comments

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -8,154 +8,152 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.exception.UserRegistrationException;
 import core.basesyntax.model.User;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    private static StorageDaoImpl storageDao;
-    private static RegistrationServiceImpl registrationService;
-
-    @BeforeAll
-    static void setUp() {
-        storageDao = new StorageDaoImpl();
-        registrationService = new RegistrationServiceImpl();
-    }
+    private final StorageDaoImpl storageDao = new StorageDaoImpl();
+    private final RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
+    private String expectedMessage;
+    private User newUser;
 
     @BeforeEach
-    void beforeEach() {
-        storageDao = new StorageDaoImpl();
+    void init() {
+        newUser = new User();
+        Storage.people.clear();
     }
 
     @AfterEach
-    void afterEach() {
-        storageDao = null;
+    void teardown() {
+        newUser = null;
     }
 
     @Test
     @DisplayName("Method register(User user) "
             + "throws UserRegistrationException if user parameter is null")
-    void registerMethodThrowsExceptionIfUserParameterIsNull() {
-        UserRegistrationException exception = assertThrows(
+    void register_nullUser_notOk() {
+        expectedMessage = "User can`t be null";
+        UserRegistrationException expectedException = assertThrows(
                 UserRegistrationException.class,
-                () -> registrationService.register(null));
-        assertAll(() -> assertThrows(UserRegistrationException.class,
-                        () -> registrationService.register(null)),
-                () -> assertEquals("User can`t be null",
-                        exception.getMessage())
+                () -> registrationService.register(null)
+        );
+        assertEquals(
+                expectedMessage,
+                expectedException.getMessage()
         );
     }
 
     @Test
     @DisplayName("Method register(User user) "
             + "throws UserRegistrationException if user login is null")
-    void registerMethodThrowsExceptionIfUserLoginIsNull() {
-        User user = new User(1L, null, null, 17);
-        UserRegistrationException exception = assertThrows(
+    void register_nullUserLogin_notOk() {
+        expectedMessage = "User login can`t be null";
+        UserRegistrationException expectedException = assertThrows(
                 UserRegistrationException.class,
-                () -> registrationService.register(user));
-        assertAll(() -> assertThrows(UserRegistrationException.class,
-                        () -> registrationService.register(user)),
-                () -> assertEquals("User login can`t be null",
-                        exception.getMessage())
+                () -> registrationService.register(newUser)
+        );
+        assertEquals(
+                expectedMessage,
+                expectedException.getMessage()
         );
     }
 
     @Test
     @DisplayName("Method register(User user) "
             + "throws UserRegistrationException if user password is null")
-    void registerMethodThrowsExceptionIfUserPasswordIsNull() {
-        User user = new User(1L, "testLogin", null, 17);
-        UserRegistrationException exception = assertThrows(
+    void register_nullUserPassword_notOk() {
+        newUser.setLogin("valid login");
+        expectedMessage = "User password can`t be null";
+        UserRegistrationException expectedException = assertThrows(
                 UserRegistrationException.class,
-                () -> registrationService.register(user));
-        assertAll(() -> assertThrows(UserRegistrationException.class,
-                        () -> registrationService.register(user)),
-                () -> assertEquals("User password can`t be null",
-                        exception.getMessage())
+                () -> registrationService.register(newUser)
+        );
+        assertEquals(
+                expectedMessage,
+                expectedException.getMessage()
         );
     }
 
     @Test
     @DisplayName("Method register(User user) "
-            + "throws UserRegistrationException if user login length less then 6")
-    void registerMethodThrowsExceptionIfUserLoginLengthLessThenSixCharacters() {
-        User user = new User(1L, "login", null, 17);
-        UserRegistrationException exception = assertThrows(
+            + "throws UserRegistrationException if user login length less then 6 characters")
+    void register_UserLoginLengthLessThenSixCharacters_notOk() {
+        newUser.setLogin("test");
+        newUser.setPassword("valid password");
+        expectedMessage = "User login %s length less then %d characters";
+        UserRegistrationException expectedException = assertThrows(
                 UserRegistrationException.class,
-                () -> registrationService.register(user));
-        assertAll(() -> assertThrows(UserRegistrationException.class,
-                        () -> registrationService.register(user)),
-                () -> assertEquals(String.format("User login %s length less then 6 characters",
-                        user.getLogin()),
-                        exception.getMessage())
+                () -> registrationService.register(newUser)
+        );
+        assertEquals(
+                String.format(expectedMessage, newUser.getLogin(), 6),
+                expectedException.getMessage()
         );
     }
 
     @Test
     @DisplayName("Method register(User user) "
-            + "throws UserRegistrationException if user password length less then 6")
-    void registerMethodThrowsExceptionIfUserPasswordLengthLessThenSixCharacters() {
-        User user = new User(1L, "testLogin", "12345", 17);
-        assertThrows(
+            + "throws UserRegistrationException if user password length less then 6 characters")
+    void register_UserPasswordLengthLessThenSixCharacters_notOk() {
+        newUser.setLogin("valid login");
+        newUser.setPassword("test");
+        expectedMessage = "User password %s length less then %d characters";
+        UserRegistrationException expectedException = assertThrows(
                 UserRegistrationException.class,
-                () -> registrationService.register(user)
+                () -> registrationService.register(newUser)
         );
-    }
-
-    @Test
-    @DisplayName("Method register(User user) "
-            + "throws UserRegistrationException if user password is null")
-    void registerMethodThrowsExceptionIfUserAlreadyExistsInStorage() {
-        User existsUser = new User(1L, "testLogin", "testPassword", 17);
-        storageDao.add(existsUser);
-        User newUser = new User(1L, "testLogin", null, 17);
-        UserRegistrationException exception = assertThrows(
-                UserRegistrationException.class,
-                () -> registrationService.register(newUser));
-        assertAll(() -> assertThrows(UserRegistrationException.class,
-                        () -> registrationService.register(newUser)),
-                () -> assertEquals(String.format("User with login %s already exists",
-                        newUser.getLogin()),
-                        exception.getMessage())
+        assertEquals(
+                String.format(expectedMessage, newUser.getPassword(), 6),
+                expectedException.getMessage()
         );
     }
 
     @Test
     @DisplayName("Method register(User user) "
             + "throws UserRegistrationException if user age less then 18")
-    void registerMethodThrowsExceptionIfUserAgeLessThanEighteen() {
-        User user = new User(1L, "testLogin", "testPassword", 17);
-        UserRegistrationException exception = assertThrows(
+    void register_UserAgeLessThenEighteen_notOk() {
+        newUser.setLogin("valid login");
+        newUser.setPassword("valid password");
+        newUser.setAge(17);
+        expectedMessage = "User age %d less then %d years";
+        UserRegistrationException expectedException = assertThrows(
                 UserRegistrationException.class,
-                () -> registrationService.register(user));
-        assertAll(() -> assertThrows(
-                UserRegistrationException.class,
-                        () -> registrationService.register(user)),
-                () -> assertEquals(String.format(String.format("User age %s less then 18 years",
-                        user.getAge())),
-                        exception.getMessage())
+                () -> registrationService.register(newUser)
+        );
+        assertEquals(
+                String.format(expectedMessage, newUser.getAge(), 18),
+                expectedException.getMessage()
         );
     }
 
     @Test
-    void registerMethodReturnsRegisteredUser() {
-        User expectedUser = new User(1L, "testLogin", "testPassword", 18);
-        User actualUser = new User(1L, "testLogin", "testPassword", 18);
-        int expectedSize = Storage.people.size() + 1;
-        assertAll(() -> assertEquals(expectedUser,
-                        registrationService.register(actualUser)),
-                () -> assertEquals(expectedSize,
-                        Storage.people.size())
+    @DisplayName("Method register(User user) "
+            + "throws UserRegistrationException if user with such login already exists")
+    void register_UserAlreadyExists_notOk() {
+        User oldUser = new User(1L, "valid login", "valid password", 18);
+        storageDao.add(oldUser);
+        newUser = new User(2L, "valid login", "other valid password", 18);
+        expectedMessage = "User with login %s already exists";
+        UserRegistrationException expectedException = assertThrows(
+                UserRegistrationException.class,
+                () -> registrationService.register(newUser)
+        );
+        assertEquals(
+                String.format(expectedMessage, newUser.getLogin()),
+                expectedException.getMessage()
         );
     }
 
-    @AfterAll
-    static void tearDown() {
-        storageDao = null;
-        registrationService = null;
+    @Test
+    void register_registerNewUser_ok() {
+        User expectedUser = new User(1L, "test login", "test password", 18);
+        User actualUser = new User(1L, "test login", "test password", 18);
+        int expectedSize = Storage.people.size() + 1;
+        assertAll(
+                () -> assertEquals(expectedUser, registrationService.register(actualUser)),
+                () -> assertEquals(expectedSize, Storage.people.size())
+        );
     }
 }
