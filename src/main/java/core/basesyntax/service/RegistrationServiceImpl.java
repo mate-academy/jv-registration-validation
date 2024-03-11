0829cd7864fdcfbb14ed45d03261fb6e900836fd
Valentyn Sharshon <woosay333@gmail.com>
2024-03-11 15:21:33 +0200

    changed RegistrationServiceImpl class based on mentor's comments

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -7,20 +7,6 @@ import core.basesyntax.model.User;
 import java.util.Objects;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private static final String USER_IS_NULL_ERROR_MESSAGE
-            = "User can`t be null";
-    private static final String USER_LOGIN_IS_NULL =
-            "User login can`t be null";
-    private static final String USER_LOGIN_ALREADY_EXISTS_ERROR_MESSAGE
-            = "User with login %s already exists";
-    private static final String USER_LOGIN_LENGTH_LESS_THAN_ERROR_MESSAGE
-            = "User login %s length less then 6 characters";
-    private static final String USER_PASSWORD_IS_NULL
-            = "User password can`t be null";
-    private static final String USER_PASSWORD_LENGTH_LESS_THAN_ERROR_MESSAGE
-            = "User password %s length less then 6 characters";
-    private static final String USER_AGE_LESS_THAN_ERROR_MESSAGE
-            = "User age %d less then 18 years";
     private static final int LOGIN_PASSWORD_MIN_LENGTH = 6;
     private static final int USER_MIN_AGE = 18;
 
@@ -33,37 +19,68 @@ public class RegistrationServiceImpl implements RegistrationService {
     }
 
     private void checkIsUserValid(User user) {
+        checkUserIsNull(user);
+        checkUserLoginIsNull(user);
+        checkUserPasswordIsNull(user);
+        checkUserLoginLength(user);
+        checkUserPasswordLength(user);
+        checkUserAge(user);
+        checkIsUserAlreadyExists(user);
+    }
+
+    private static void checkUserIsNull(User user) {
         if (user == null) {
-            throw new UserRegistrationException(USER_IS_NULL_ERROR_MESSAGE);
+            throw new UserRegistrationException("User can`t be null");
         }
+    }
+
+    private static void checkUserLoginIsNull(User user) {
         if (user.getLogin() == null) {
-            throw new UserRegistrationException(USER_LOGIN_IS_NULL);
+            throw new UserRegistrationException("User login can`t be null");
         }
+    }
+
+    private static void checkUserPasswordIsNull(User user) {
+        if (Objects.isNull(user.getPassword())) {
+            throw new UserRegistrationException("User password can`t be null");
+        }
+    }
+
+    private static void checkUserLoginLength(User user) {
+        if (user.getLogin().length() < LOGIN_PASSWORD_MIN_LENGTH) {
+            throw new UserRegistrationException(
+                    String.format("User login %s length less then %d characters",
+                            user.getLogin(),
+                            LOGIN_PASSWORD_MIN_LENGTH)
+            );
+        }
+    }
+
+    private static void checkUserPasswordLength(User user) {
+        if (user.getPassword().length() < LOGIN_PASSWORD_MIN_LENGTH) {
+            throw new UserRegistrationException(
+                    String.format("User password %s length less then %d characters",
+                            user.getPassword(),
+                            LOGIN_PASSWORD_MIN_LENGTH)
+            );
+        }
+    }
+
+    private static void checkUserAge(User user) {
+        if (user.getAge() < USER_MIN_AGE) {
+            throw new UserRegistrationException(
+                    String.format("User age %d less then %d years",
+                            user.getAge(),
+                            USER_MIN_AGE)
+            );
+        }
+    }
+
+    private void checkIsUserAlreadyExists(User user) {
         if (storageDao.get(user.getLogin()) != null) {
             throw new UserRegistrationException(
-                    String.format(USER_LOGIN_ALREADY_EXISTS_ERROR_MESSAGE,
+                    String.format("User with login %s already exists",
                             user.getLogin())
-            );
-        }
-        if (user.getLogin().length() < LOGIN_PASSWORD_MIN_LENGTH) {
-            throw new UserRegistrationException(String.format(
-                    USER_LOGIN_LENGTH_LESS_THAN_ERROR_MESSAGE,
-                    user.getLogin())
-            );
-        }
-        if (Objects.isNull(user.getPassword())) {
-            throw new UserRegistrationException(USER_PASSWORD_IS_NULL);
-        }
-        if (user.getPassword().length() < LOGIN_PASSWORD_MIN_LENGTH) {
-            throw new UserRegistrationException(String.format(
-                    USER_PASSWORD_LENGTH_LESS_THAN_ERROR_MESSAGE,
-                    user.getPassword())
-            );
-        }
-        if (user.getAge() < USER_MIN_AGE) {
-            throw new UserRegistrationException(String.format(
-                    USER_AGE_LESS_THAN_ERROR_MESSAGE,
-                    user.getAge())
             );
         }
     }
