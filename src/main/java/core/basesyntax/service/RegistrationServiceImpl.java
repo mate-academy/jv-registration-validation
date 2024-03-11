f05360b0e6f7a1d4d58d3653d43059e0e196c555
Hanna Ratushniak <ratushnyakhanna@gmail.com>
2024-03-11 15:19:11 +0200

    added nessesary changes

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -11,32 +11,56 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
+        checkUser(user);
+        checkLogin(user.getLogin());
+        checkUserIsNotExist(user.getLogin());
+        checkUserAge(user.getAge());
+        checkUserPassword(user.getPassword());
+        return storageDao.add(user);
+    }
+
+    private boolean checkLogin(String login) {
+        if (login == null) {
+            throw new RegistrationException("Login can't be null");
+        }
+        if (login.length() < MIN_LENGTH) {
+            throw new RegistrationException("Login can't be  less than 6 characters");
+        }
+        return true;
+    }
+
+    private boolean checkUser(User user) {
         if (user == null) {
             throw new RegistrationException("User can't be null");
         }
-        if (user.getLogin() == null) {
-            throw new RegistrationException("Login can't be null");
-        }
-        if (user.getLogin().length() < MIN_LENGTH) {
-            throw new RegistrationException("Login can't be  less than 6 characters");
-        }
+        return true;
+    }
 
-        if (storageDao.get(user.getLogin()) != null) {
-            throw new RegistrationException("User with login " + user.getLogin()
+    private boolean checkUserIsNotExist(String login) {
+        if (storageDao.get(login) != null) {
+            throw new RegistrationException("User with login " + login
                     + " already exists");
         }
-        if (user.getAge() == null) {
+        return true;
+    }
+
+    private boolean checkUserAge(Integer age) {
+        if (age == null) {
             throw new RegistrationException("Age can't be null");
         }
-        if (user.getAge() < 18) {
+        if (age < 18) {
             throw new RegistrationException("Age can't be less than 18");
         }
-        if (user.getPassword() == null) {
+        return true;
+    }
+
+    private boolean checkUserPassword(String password) {
+        if (password == null) {
             throw new RegistrationException("Password can't be null");
         }
-        if (user.getPassword().length() < MIN_LENGTH) {
+        if (password.length() < MIN_LENGTH) {
             throw new RegistrationException("Password's length can't be less than 6 characters");
         }
-        return storageDao.add(user);
+        return true;
     }
 }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,20 +1,18 @@
 package core.basesyntax.service;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.exceptions.RegistrationException;
 import core.basesyntax.model.User;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    private static RegistrationService registrationService;
-    private static StorageDao storageDao;
     private static final String INVALID_LOGIN = "user";
     private static final String VALID_LOGIN = "user1986";
     private static final String INVALID_PASSWORD = "pass";
@@ -22,30 +20,40 @@ class RegistrationServiceImplTest {
     private static final String EXIST_LOGIN = "bob1989";
     private static final int INVALID_AGE = 15;
     private static final int VALID_AGE = 25;
+    private StorageDao storageDao = new StorageDaoImpl();
+    private RegistrationService registrationService = new RegistrationServiceImpl();
     private User user;
-
-    @BeforeAll
-    static void beforeAll() {
-        registrationService = new RegistrationServiceImpl();
-        storageDao = new StorageDaoImpl();
-        User bob = new User();
-        bob.setLogin(EXIST_LOGIN);
-        storageDao.add(bob);
-    }
 
     @BeforeEach
     void setUp() {
+        Storage.people.clear();
         user = new User();
+        user.setLogin(EXIST_LOGIN);
+        user.setPassword(VALID_PASSWORD);
+        user.setAge(VALID_AGE);
+    }
+
+    @Test
+    void register_UserIsValid() {
+        user.setLogin(VALID_LOGIN);
+        user.setAge(VALID_AGE);
+        user.setPassword(VALID_PASSWORD);
+        assertDoesNotThrow(() -> registrationService.register(user),
+                "Registering user with valid data should not throw an exception");
+
+    }
+
+    @Test
+    void register_OutputUserIsEqualtoInputUser_IsOk() {
+        User expected = user;
+        User actual = registrationService.register(user);
+        assertEquals(expected, actual, "Registered user should be equal to the original user");
+
     }
 
     @Test
     void register_nullUser_notOk() {
-        try {
-            registrationService.register(null);
-        } catch (RegistrationException e) {
-            return;
-        }
-        fail("If user is null RegistrationException should be.");
+        assertThrows(RegistrationException.class, () -> registrationService.register(null));
     }
 
     @Test
@@ -90,17 +98,8 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_ExistUser_notOk() {
-        user.setLogin(EXIST_LOGIN);
-        assertThrows(RegistrationException.class, () -> registrationService.register(user));
-    }
-
-    @Test
-    void register_UserIsValid() {
-        user.setLogin(VALID_LOGIN);
-        user.setAge(VALID_AGE);
-        user.setPassword(VALID_PASSWORD);
-        assertDoesNotThrow(() -> registrationService.register(user),
-                "Registering user with valid data should not throw an exception");
-
+        User bob = new User();
+        bob.setLogin(EXIST_LOGIN);
+        assertThrows(RegistrationException.class, () -> registrationService.register(bob));
     }
 }
