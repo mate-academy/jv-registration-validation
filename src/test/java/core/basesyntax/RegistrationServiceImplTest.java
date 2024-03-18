f7dad3bae3b1fb82403251171d63835e10b37958
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-03-18 16:31:17 +0200

    finished task

--- src/test/java/core/basesyntax/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -1,8 +1,8 @@
 package core.basesyntax;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.exeptionforservice.RegistrationException;
 import core.basesyntax.model.User;
