6a9b64e86b2e1526250ec4871455a27ee401e65e
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-03-19 12:36:57 +0200

    Fixed task

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -7,7 +7,8 @@ public class User {
     private String login;
     private String password;
     private Integer age;
-    public User (String login, String password, int age) {
+
+    public User(String login, String password, int age) {
         this.login = login;
         this.password = password;
         this.age = age;
