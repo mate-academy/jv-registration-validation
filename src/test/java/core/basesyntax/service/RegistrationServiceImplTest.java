c0d75f0665de1b77b7ccca69f08edaab4557ebcd
Zorii Bohdan <bodkazorii@gmail.com>
2024-03-20 10:37:18 +0200

    removed user id initialization from constructor, added storage size check after registration

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -17,7 +17,6 @@ import org.junit.jupiter.api.Test;
 class RegistrationServiceImplTest {
     public static final String DEFAULT_LOGIN = "someLogin";
     public static final String DEFAULT_PASSWORD = "somePassword";
-    public static final Long DEFAULT_ID = 0L;
 
     private static StorageDao storageDao;
     private static RegistrationService service;
@@ -35,9 +34,12 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_notNull_ok() {
-        User user = new User(DEFAULT_ID, DEFAULT_LOGIN, DEFAULT_PASSWORD, MINIMAL_USER_AGE);
+        User user = new User(DEFAULT_LOGIN, DEFAULT_PASSWORD, MINIMAL_USER_AGE);
+        int storageSize = Storage.people.size();
         assertDoesNotThrow(() -> service.register(user),
                 "Exception while registering user " + user);
+        assertEquals(storageSize + 1, Storage.people.size(),
+                "Storage size is " + Storage.people.size() + " but expected" + storageSize + 1);
     }
 
     @Test
@@ -47,21 +49,21 @@ class RegistrationServiceImplTest {
 
     @Test
     void register_nullLogin_notOk() {
-        User user = new User(DEFAULT_ID, null, DEFAULT_PASSWORD, MINIMAL_USER_AGE);
+        User user = new User(null, DEFAULT_PASSWORD, MINIMAL_USER_AGE);
         assertThrows(RegistrationException.class, () -> service.register(user));
     }
 
     @Test
     void register_tooShortLogin_notOk() {
-        User user1 = new User(DEFAULT_ID, "lgn28", DEFAULT_PASSWORD, MINIMAL_USER_AGE);
+        User user1 = new User("lgn28", DEFAULT_PASSWORD, MINIMAL_USER_AGE);
         assertThrows(RegistrationException.class, () -> service.register(user1));
-        User user2 = new User(DEFAULT_ID + 1, "login", DEFAULT_PASSWORD, MINIMAL_USER_AGE);
+        User user2 = new User("login", DEFAULT_PASSWORD, MINIMAL_USER_AGE);
         assertThrows(RegistrationException.class, () -> service.register(user2));
     }
 
     @Test
     void register_loginRepeated_notOk() {
-        User user = new User(DEFAULT_ID, DEFAULT_LOGIN, DEFAULT_PASSWORD, MINIMAL_USER_AGE);
+        User user = new User(DEFAULT_LOGIN, DEFAULT_PASSWORD, MINIMAL_USER_AGE);
         service.register(user);
         assertThrows(RegistrationException.class, () -> service.register(user));
     }
@@ -70,30 +72,33 @@ class RegistrationServiceImplTest {
     void register_login_ok() {
         User actual;
         User expected;
-        long idCounter = DEFAULT_ID;
+        int storageSize;
         String[] loginValues = new String[]{"login1", "loginX", "123456", "Nagibator_228"};
 
         for (String login: loginValues) {
-            actual = new User(idCounter++, login, DEFAULT_PASSWORD, MINIMAL_USER_AGE);
+            storageSize = Storage.people.size();
+            actual = new User(login, DEFAULT_PASSWORD, MINIMAL_USER_AGE);
             expected = service.register(actual);
             assertEquals(actual, expected,
                     "User with login " + login + " not returned correctly");
             assertEquals(storageDao.get(actual.getLogin()), actual,
                     "User with login " + login + " not registered correctly");
+            assertEquals(storageSize + 1, Storage.people.size(),
+                    "Storage size is " + Storage.people.size() + " but expected" + storageSize + 1);
         }
     }
 
     @Test
     void register_nullPassword_notOk() {
-        User user = new User(DEFAULT_ID, DEFAULT_LOGIN,null, MINIMAL_USER_AGE);
+        User user = new User(DEFAULT_LOGIN,null, MINIMAL_USER_AGE);
         assertThrows(RegistrationException.class, () -> service.register(user));
     }
 
     @Test
     void register_tooShortPassword_notOk() {
-        User user1 = new User(DEFAULT_ID, DEFAULT_LOGIN + 1, "pass1", MINIMAL_USER_AGE);
+        User user1 = new User(DEFAULT_LOGIN + 1, "pass1", MINIMAL_USER_AGE);
         assertThrows(RegistrationException.class, () -> service.register(user1));
-        User user2 = new User(DEFAULT_ID + 1, DEFAULT_LOGIN + 2, "pswrd", MINIMAL_USER_AGE);
+        User user2 = new User(DEFAULT_LOGIN + 2, "pswrd", MINIMAL_USER_AGE);
         assertThrows(RegistrationException.class, () -> service.register(user2));
     }
 
@@ -101,27 +106,31 @@ class RegistrationServiceImplTest {
     void register_password_ok() {
         User actual;
         User expected;
-        long idCounter = DEFAULT_ID;
+        int storageSize;
+        long counter = 0;
         String[] passwordValues = new String[]
                 {"password", "12345678", "xXx_xXx123456789", "jkdH_das23$^c5hj"};
 
         for (String password: passwordValues) {
-            actual = new User(idCounter++, DEFAULT_LOGIN + idCounter, password, MINIMAL_USER_AGE);
+            storageSize = Storage.people.size();
+            actual = new User(DEFAULT_LOGIN + counter++, password, MINIMAL_USER_AGE);
             expected = service.register(actual);
             assertEquals(actual, expected,
                     "User with password " + password + " not returned correctly");
             assertEquals(storageDao.get(actual.getLogin()), actual,
                     "User with password " + password + " not registered correctly");
+            assertEquals(storageSize + 1, Storage.people.size(),
+                    "Storage size is " + Storage.people.size() + " but expected" + storageSize + 1);
         }
     }
 
     @Test
     void register_invalidAge_notOk() {
-        long idCounter = DEFAULT_ID;
+        long counter = 0;
         int[] ageValues = new int[]{-1, 0, MINIMAL_USER_AGE - 5, MINIMAL_USER_AGE - 1};
 
         for (int age: ageValues) {
-            User user = new User(idCounter++, DEFAULT_LOGIN + idCounter, DEFAULT_PASSWORD, age);
+            User user = new User(DEFAULT_LOGIN + counter, DEFAULT_PASSWORD, age);
             assertThrows(RegistrationException.class, () -> service.register(user),
                     "User with age " + age + " can't be registered");
         }
@@ -131,17 +140,21 @@ class RegistrationServiceImplTest {
     void register_age_ok() {
         User actual;
         User expected;
-        long idCounter = DEFAULT_ID;
+        int storageSize;
+        long counter = 0;
         int[] ageValues = new int[]{MINIMAL_USER_AGE, MINIMAL_USER_AGE + 1,
                 MINIMAL_USER_AGE + 5, MINIMAL_USER_AGE + 50};
 
         for (int age : ageValues) {
-            actual = new User(idCounter++, DEFAULT_LOGIN + idCounter, DEFAULT_PASSWORD, age);
+            storageSize = Storage.people.size();
+            actual = new User(DEFAULT_LOGIN + counter++, DEFAULT_PASSWORD, age);
             expected = service.register(actual);
             assertEquals(actual, expected,
                     "User with age " + age + " not returned correctly");
             assertEquals(storageDao.get(actual.getLogin()), actual,
                     "User with age " + age + " not registered correctly");
+            assertEquals(storageSize + 1, Storage.people.size(),
+                    "Storage size is " + Storage.people.size() + " but expected" + storageSize + 1);
         }
     }
 }
