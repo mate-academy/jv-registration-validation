104d75360d54ccf7b8210b6c1a8d48b6c1276e04
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-03-22 16:06:30 +0200

    fixed the method names and added new method

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,6 +5,8 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.exeptionforservice.RegistrationException;
 import core.basesyntax.model.User;
 
+import javax.swing.*;
+
 public class RegistrationServiceImpl implements RegistrationService {
     private static final int MIN_AGE = 18;
     private static final int MINIMUM_PASSWORD_LENGTH = 6;
@@ -13,35 +15,51 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        userVarification(user);
+        isUserNull(user);
+        isValidAge(user.getAge());
+        isValidPassword(user.getPassword());
+        isValidLogin(user.getLogin());
+        isUserInStorage(user);
         return storageDao.add(user);
     }
 
-    private void userVarification(User user) {
-        if (user.getLogin() == null) {
-            throw new RegistrationException("Login null input.Try again!");
+    private void isUserNull(User user) {
+        if (user == null) {
+            throw new RegistrationException("User can not be null!");
         }
+    }
 
-        if (user.getPassword() == null) {
+    private void isValidAge(int age) {
+        if (age < MIN_AGE) {
+            throw new RegistrationException("Invalid login format "
+                    + age + ". Age was less than should be " + MIN_AGE);
+        }
+    }
+
+    private void isValidPassword (String password) {
+        if (password == null) {
             throw new RegistrationException("Password null input. Try again!");
         }
-
-        if (user.getLogin().length() < MINIMUM_LOGIN_LENGTH) {
+        if (password.length() < MINIMUM_PASSWORD_LENGTH) {
             throw new RegistrationException("Invalid login format "
-                    + user.getLogin() + ". Min length login : " + MINIMUM_LOGIN_LENGTH);
+                    + password + ". Min length password: " + MINIMUM_PASSWORD_LENGTH);
         }
 
-        if (user.getPassword().length() < MINIMUM_PASSWORD_LENGTH) {
-            throw new RegistrationException("Invalid login format "
-                    + user.getLogin() + ". Min length password: " + MINIMUM_PASSWORD_LENGTH);
-        }
-        if (user.getAge() < MIN_AGE) {
-            throw new RegistrationException("Invalid login format "
-                    + user.getLogin() + ". Age was less than should be " + MIN_AGE);
-        }
+    }
 
+    private void isValidLogin(String login) {
+        if (login == null) {
+            throw new RegistrationException("Login null input.Try again!");
+        }
+        if (login.length() < MINIMUM_LOGIN_LENGTH) {
+            throw new RegistrationException("Invalid login format "
+                    + login + ". Min length login : " + MINIMUM_LOGIN_LENGTH);
+        }
+    }
+
+    private void isUserInStorage(User user) {
         if (storageDao.get(user.getLogin()) != null) {
-            throw new RegistrationException("User already exists");
+            throw new RegistrationException("This user already exists");
         }
     }
 }
--- src/test/java/core/basesyntax/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -21,9 +21,9 @@ public class RegistrationServiceImplTest {
     private static User correct_user2;
     private static User null_password;
     private static User null_login;
-    private static User invalid_password;
-    private static User invalid_login;
-    private static User invalid_age;
+    private static User incorrect_password;
+    private static User incorrect_login;
+    private static User incorrect_age;
     private static User same_correct_user;
 
     @BeforeAll
@@ -32,9 +32,9 @@ public class RegistrationServiceImplTest {
         correct_user2 = new User("alicelogin", "alicepassword", 20);
         null_password = new User("boblogin", null, 25);
         null_login = new User(null, "password355",40);
-        invalid_password = new User("login24","2324", 45);
-        invalid_login = new User("ksf", "validpassword", 30);
-        invalid_age = new User("boblogin", "bobpassword", 13);
+        incorrect_password = new User("login24","2324", 45);
+        incorrect_login = new User("ksf", "validpassword", 30);
+        incorrect_age = new User("boblogin", "bobpassword", 13);
         same_correct_user = new User(correct_user.getLogin(),
                 correct_user.getPassword(), correct_user.getAge());
     }
@@ -76,23 +76,23 @@ public class RegistrationServiceImplTest {
     }
 
     @Test
-    public void checkInvalidPassword() {
+    public void checkIncorrectidPassword() {
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(invalid_password),
+                () -> registrationService.register(incorrect_password),
                 "Password was correct");
     }
 
     @Test
     public void checkIncorrectLogin() {
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(invalid_login),
+                () -> registrationService.register(incorrect_login),
                 "Login was correct");
     }
 
     @Test
     public void checkIncorrectAge() {
         assertThrows(RegistrationException.class,
-                () -> registrationService.register(invalid_age),
+                () -> registrationService.register(incorrect_age),
                 "Age was correct");
     }
 }
