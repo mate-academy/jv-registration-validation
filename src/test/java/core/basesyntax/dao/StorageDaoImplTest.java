d35737ef2d2527170811f884bea228a4e1fe48e2
Yarolsav <kiritok712@gmail.com>
2024-04-16 12:18:47 +0200

    Testing_modules

---
+++ src/test/java/core/basesyntax/dao/StorageDaoImplTest.java
@@ -0,0 +1,45 @@
+package core.basesyntax.dao;
+
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StorageDaoImplTest {
+    private StorageDao storageDao;
+
+    @BeforeEach
+    public void setUp() {
+        storageDao = new StorageDaoImpl();
+    }
+
+    @Test
+    public void testAddUser_Ok() {
+        User user = new User();
+        user.setLogin("testUser");
+        user.setPassword("password");
+        user.setAge(20);
+
+        User addedUser = storageDao.add(user);
+        assertNotNull(addedUser.getId());
+        assertEquals(user, addedUser);
+    }
+
+    @Test
+    public void testGetUserByLogin_Ok() {
+        User user = new User();
+        user.setLogin("testUser");
+        user.setPassword("password");
+        user.setAge(20);
+
+        storageDao.add(user);
+        User retrievedUser = storageDao.get("testUser");
+        assertEquals(user, retrievedUser);
+    }
+
+    @Test
+    public void testGetExistingUser_NotOk() {
+        assertNull(storageDao.get("nonExistingUser"));
+    }
+}
