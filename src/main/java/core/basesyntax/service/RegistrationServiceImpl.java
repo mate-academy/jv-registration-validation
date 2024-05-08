c723094c67a923505f0494da195ea8d7df98c8a3
Banderan <bartoszwojcik2004@gmail.com>
2024-05-08 12:02:17 +0200

    First solution

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -9,6 +9,30 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        String login = user.getLogin();
+        String password = user.getPassword();
+        int age = user.getAge();
+
+        if (login == null) {
+            throw new RegisterException("This isnt login");
+        }
+        if (password == null) {
+            throw new RegisterException("this isnt password");
+        }
+        if (age < 18) {
+            throw new RegisterException("Ur age is too low");
+        }
+        if (password.length() < 6) {
+            throw new RegisterException("Password is too short");
+        }
+        if (login.length() < 6) {
+            throw new RegisterException("Login is too short");
+        }
+        if (user.getLogin()
+                .equals(storageDao.get(login).getLogin())) {
+            throw new RegisterException("We have already this login");
+        }
+
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,98 @@
+package core.basesyntax.service;
+
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService registration = new RegistrationServiceImpl();
+
+    @BeforeEach
+    void setUp() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void register_NullLogin_NotOk() {
+        User user1 = new User();
+        user1.setPassword("password");
+        user1.setAge(18);
+
+        assertThrows(RegisterException.class, () -> registration.register(user1), "Login cant be null");
+    }
+
+    @Test
+    void register_NullPassword_NotOk() {
+        User user1 = new User();
+        user1.setLogin("login123");
+        user1.setAge(18);
+
+        assertThrows(RegisterException.class, () -> registration.register(user1), "Password cant be null");
+    }
+
+    @Test
+    void register_BelowOrZeroAge_NotOk() {
+        User user1 = new User();
+        user1.setLogin("login123");
+        user1.setPassword("password");
+        user1.setAge(-16);
+
+        User user2 = new User();
+        user2.setLogin("newlogin123");
+        user2.setPassword("nwepassword");
+        user2.setAge(0);
+
+        assertThrows(RegisterException.class, () -> registration.register(user1), "Age cant be below 0");
+        assertThrows(RegisterException.class, () -> registration.register(user2), "Age cant be 0");
+    }
+
+    @Test
+    void register_DuplicateLogin_NotOk() {
+        User user1 = new User();
+        user1.setLogin("login123");
+        user1.setPassword("password1");
+        user1.setAge(19);
+        StorageDaoImpl storageDao = new StorageDaoImpl();
+        storageDao.add(user1);
+
+        User user2 = new User();
+        user2.setLogin("login123");
+        user2.setPassword("password2");
+        user2.setAge(20);
+        assertThrows(RegisterException.class, () -> registration.register(user2), "In storage we cant have 2 equals logins");
+    }
+
+    @Test
+    void register_TooShortLogin_NotOk() {
+        User user1 = new User();
+        user1.setLogin("12345");
+        user1.setPassword("password1");
+        user1.setAge(18);
+
+        assertThrows(RegisterException.class, () -> registration.register(user1), "minimum length for login is 6");
+    }
+
+    @Test
+    void register_TooShortPassword_NotOk() {
+        User user1 = new User();
+        user1.setLogin("login123");
+        user1.setPassword("12345");
+        user1.setAge(18);
+
+        assertThrows(RegisterException.class, () -> registration.register(user1), "minimum length for password is 6");
+    }
+
+    @Test
+    void register_UnderAge_NotOk() {
+        User user1 = new User();
+        user1.setLogin("login123");
+        user1.setPassword("password1");
+        user1.setAge(17);
+
+        assertThrows(RegisterException.class, () -> registration.register(user1), "U have to accept only persons with 18 y.o.");
+    }
+}
