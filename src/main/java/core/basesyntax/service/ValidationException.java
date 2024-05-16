6a55fe916cd7ff261a710435cf1c29922bb465e3
flamehost <flamehost@syneforge.com>
2024-05-16 22:47:41 +0300

    display edits

--- src/main/java/core/basesyntax/service/ValidationException.java
+++ src/main/java/core/basesyntax/service/ValidationException.java
@@ -1,7 +1,7 @@
 package core.basesyntax.service;
 
 public class ValidationException extends RuntimeException {
-    public ValidationException (String message) {
+    public ValidationException(String message) {
         super(message);
     }
 }
