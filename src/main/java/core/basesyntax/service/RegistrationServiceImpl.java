9f2ffa8366d8526dd7d5237184ee30173b09dd60
flamehost <flamehost@syneforge.com>
2024-05-16 22:13:28 +0300

    Implement register + test

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -6,9 +6,26 @@ import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
+    private final int MIN_AGE = 18;
+    private final int MIN_LOGIN_LENGTH = 6;
+    private final int MIN_PASSWORD_LENGTH = 6;
 
     @Override
     public User register(User user) {
-        return null;
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new ValidationException("LOGIN " + user.getLogin() + "is used");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new ValidationException("PASSWORD " + user.getPassword() + "is to short");
+        }
+        if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
+            throw new ValidationException("Login " + user.getLogin() + "is to short");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new ValidationException("Age " + user.getAge() + " is too young");
+        }
+        storageDao.add(user);
+        return user;
+
     }
 }
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -1,8 +1,108 @@
 package core.basesyntax;
 
-/**
- * Feel free to remove this class and create your own.
- */
-public class HelloWorldTest {
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationServiceImpl;
 
+import core.basesyntax.service.ValidationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloWorldTest {
+    RegistrationServiceImpl registrationService;
+    User user = new User();
+    String login = "admin" + new Random().nextInt(6, 99999);
+    String password = "admin" + new Random().nextInt(6, 99999);
+    int userAge = new Random().nextInt(18, 99);
+
+    @BeforeEach
+    public void setUp() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @Test
+    public void check_correct_login() {
+        user.setLogin(login);
+        user.setPassword(password);
+        user.setAge(userAge);
+
+        assertEquals(registrationService.register(user), user);
+    }
+
+    @Test
+    public void check_setid_after_registration_ok() {
+        user.setLogin(login);
+        user.setPassword(password);
+        user.setAge(userAge);
+        registrationService.register(user);
+
+        assertNotNull(user.getId());
+    }
+
+    @Test
+    public void check_toshort_password_notOk() {
+        String toShortPassword = "1";
+        user.setLogin(login);
+        user.setPassword(toShortPassword);
+
+        assertThrows(ValidationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void check_not_valid_age_ok() {
+        int failAge = 17;
+        user.setLogin(login);
+        user.setPassword(password);
+        user.setAge(failAge);
+
+        assertThrows(ValidationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    public void check_two_users_notequals() {
+        User user1 = new User();
+        user1.setLogin("user1");
+        user1.setPassword("password1");
+        user1.setAge(25);
+
+        User user2 = new User();
+        user2.setLogin("user2"); // Изменили login для второго пользователя
+        user2.setPassword("password1");
+        user2.setAge(25);
+
+        assertNotEquals(user1, user2);
+    }
+
+    @Test
+    public void check_user_hashcode() {
+        User user1 = new User();
+        user1.setLogin("user1");
+        user1.setPassword("password1");
+        user1.setAge(25);
+
+        User user2 = new User();
+        user2.setLogin("user1");
+        user2.setPassword("password1");
+        user2.setAge(25);
+
+        assertEquals(user1.hashCode(), user2.hashCode());
+    }
+
+    @Test
+    public void check_user_hashcode_with_notequal_objects() {
+        User user1 = new User();
+        user1.setLogin("user1");
+        user1.setPassword("password1");
+        user1.setAge(25);
+
+        User user2 = new User();
+        user2.setLogin("user1");
+        user2.setPassword("password1");
+        user2.setAge(26);
+
+        assertNotEquals(user1.hashCode(), user2.hashCode());
+    }
 }
