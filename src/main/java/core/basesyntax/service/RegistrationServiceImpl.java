bc750649640a3eccd285c5dd8498ae564630d367
trokhim03 <trokhimchuk.003@gmail.com>
2024-05-18 15:58:33 +0300

    made your own test

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,49 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exeption.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_AGE = 18;
+    private static final int MIN_LENGTH_LOGIN = 6;
+    private static final int MIN_LENGTH_PASSWORD = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
-    public User register(User user) {
-        return null;
+    public User register(User user) throws RegistrationException {
+        if (user == null) {
+            return null;
+        }
+        isNullElementsUser(user);
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("The user with this login already exists");
+        }
+        if (user.getLogin().length() < MIN_LENGTH_LOGIN) {
+            throw new RegistrationException("User's login cannot be less than "
+                    + MIN_LENGTH_LOGIN + " characters");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("User's age cannot be less than "
+                    + MIN_AGE + " years old");
+        }
+        if (user.getPassword().length() < MIN_LENGTH_PASSWORD) {
+            throw new RegistrationException("User's password cannot be less than "
+                    + MIN_LENGTH_PASSWORD + " characters");
+        }
+        storageDao.add(user);
+        return user;
+    }
+
+    private void isNullElementsUser(User user) throws RegistrationException {
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Login cannot be null");
+        }
+        if (user.getAge() == null) {
+            throw new RegistrationException("Age cannot be null");
+        }
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Password cannot be null");
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/dao/StorageDaoImplTest.java
@@ -0,0 +1,63 @@
+package core.basesyntax.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StorageDaoImplTest {
+    private static final StorageDaoImpl storageDao = new StorageDaoImpl();
+    private static final Integer DEFAULT_AGE = 18;
+    private static final String DEFAULT_PASSWORD = "123456";
+    private static final String DEFAULT_LOGIN = "Ukraine";
+    private User defaultUser;
+    
+    @BeforeEach
+    void setUp() {
+        defaultUser = new User();
+        defaultUser.setAge(DEFAULT_AGE);
+        defaultUser.setPassword(DEFAULT_PASSWORD);
+        defaultUser.setLogin(DEFAULT_LOGIN);
+    }
+
+    @AfterEach
+    void clearAfterTest() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void addUser_OK() {
+        User expected = defaultUser;
+        User actual = storageDao.add(defaultUser);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getUser_OK() {
+        User expected = defaultUser;
+        storageDao.add(defaultUser);
+        assertEquals(expected, storageDao.get(defaultUser.getLogin()));
+    }
+
+    @Test
+    void getUserNotExist_OK() {
+        User user1 = new User();
+        user1.setLogin("Den76746");
+        user1.setPassword("12345677");
+        user1.setAge(34);
+        User user2 = new User();
+        user2.setLogin("Alex76746");
+        user2.setPassword("76543211");
+        user2.setAge(21);
+        User user3 = new User();
+        user3.setLogin("Tom76746");
+        user3.setPassword("1029384756");
+        user3.setAge(45);
+        String testLogin = "Dimon6969";
+        assertNull(storageDao.get(testLogin));
+    }
+}
