d76fcb7f292234c0027e040cb4d598b0849f7331
Oleksandr Bondarenko <bondarenkoov.dev@gmail.com>
2024-05-20 21:12:48 +0200

    Implemented RegistrationServiceImpl

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,26 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private final StorageDao storageDao = new StorageDaoImpl();
+    private static final String USER_EXIST = "User already has been registered!";
+    private final StorageDao storageDao;
+    private final UserValidator userValidator;
+
+    public RegistrationServiceImpl() {
+        storageDao = new StorageDaoImpl();
+        userValidator = new UserValidatorImpl();
+    }
 
     @Override
     public User register(User user) {
-        return null;
+        userValidator.validate(user);
+        if (isUserExist(user)) {
+            throw new RuntimeException(USER_EXIST);
+        }
+        storageDao.add(user);
+        return user;
+    }
+
+    private boolean isUserExist(User user) {
+        return storageDao.get(user.getLogin()) != null;
     }
 }
