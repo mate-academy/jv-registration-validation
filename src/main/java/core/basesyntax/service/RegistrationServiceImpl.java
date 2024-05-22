6cdc7bfd2dd3a855ac7424aafae50c8f99e814c5
Bohdan Chupika <boroda4436@users.noreply.github.com>
2024-05-22 06:08:49 +0300

    create TDD

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,38 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LOGIN_LENGTH = 6;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RuntimeException("User cannot be null");
+        }
+        if (user.getLogin() == null) {
+            throw new RuntimeException("Login cannot be null");
+        }
+        if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
+            throw new RuntimeException("Login must be at least 6 element");
+        }
+        if (user.getPassword() == null) {
+            throw new RuntimeException("Password cannot be null");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password must be at least 6 characters");
+        }
+        if (user.getAge() == null) {
+            throw new RuntimeException("Age cannot be null");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("Age must be at least 18 years old");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User with this login already  exists");
+        }
+        storageDao.add(user);
+        return user;
     }
 }
