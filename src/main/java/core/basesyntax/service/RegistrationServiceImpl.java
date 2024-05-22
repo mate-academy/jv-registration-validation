15b3ecf1ea5574b56261feddf79c25463f0c191a
Bohdan Chupika <boroda4436@users.noreply.github.com>
2024-05-22 06:08:49 +0300

    improve checklist.md

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -9,6 +9,53 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RegistrationException("User can`t be null!");
+        }
+
+        if (user.getLogin() == null || user.getLogin().length() < 6) {
+            throw new RegistrationException("Login must contain at least 6 symbols!");
+        }
+
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("User with this login already exists!");
+        }
+
+        if (user.getPassword() == null || user.getPassword().length() < 6) {
+            throw new RegistrationException("Password must contain at least 6 symbols!");
+        }
+
+        if (user.getAge() == null || user.getAge() < 18) {
+            throw new RegistrationException("User`s age must be at least 18 years old!");
+        }
+        return storageDao.add(user);
+    }
+
+    private boolean isPasswordComplex(String password) {
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasDigit = false;
+        boolean hasSpecialSymbol = false;
+
+        for (char a : password.toCharArray()) {
+            if (Character.isUpperCase(a)) {
+                hasUpperCase = true;
+            }
+
+            if (Character.isLowerCase(a)) {
+                hasLowerCase = true;
+            }
+
+            if (Character.isDigit(a)) {
+                hasDigit = true;
+            }
+
+            if ((!Character.isLetterOrDigit(a))) {
+                hasSpecialSymbol = true;
+            }
+        }
+
+        return hasUpperCase && hasLowerCase & hasDigit & hasSpecialSymbol;
     }
 }
+
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,101 @@
+package core.basesyntax.service;
+
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+
+    private RegistrationServiceImpl registrationService;
+
+    @BeforeEach
+    public void setUp() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @Test
+    void register_nullUser_notOk() {
+        registrationService.register(null);
+    }
+
+    @Test
+    void register_shortLogin_notOk() {
+        User user = new User();
+        user.setLogin("abc");
+        user.setPassword("password");
+        user.setAge(20);
+        registrationService.register(user);
+    }
+
+    @Test
+    void register_duplicateLogin_notOk() {
+        User user1 = new User();
+        user1.setLogin("uniqueLogin");
+        user1.setPassword("password");
+        user1.setAge(20);
+        registrationService.register(user1);
+
+        User user2 = new User();
+        user2.setLogin("uniqueLogin");
+        user2.setPassword("password123");
+        user2.setAge(21);
+        registrationService.register(user2);
+    }
+
+    @Test
+    void register_shortPassword_notOk() {
+        User user = new User();
+        user.setLogin("validLogin");
+        user.setPassword("123");
+        user.setAge(20);
+        registrationService.register(user);
+    }
+
+    @Test
+    void register_simplePassword_notOk() {
+        User user = new User();
+        user.setLogin("validLogin");
+        user.setPassword("simple");
+        user.setAge(20);
+        RegistrationException registrationException = Assertions.assertThrows(RegistrationException.class, () -> registrationService.register(user));
+        Assertions.assertEquals("The password must be complex!", registrationException.getMessage());
+    }
+
+    @Test
+    void register_ComplexPassword_Ok() {
+        User user = new User();
+        user.setLogin("validLogin");
+        user.setPassword("password@/123");
+        user.setAge(20);
+        User registeredUser = registrationService.register(user);
+        assertNotNull(registeredUser);
+    }
+
+    @Test
+    void register_underageUser_notOk() {
+        User user = new User();
+        user.setLogin("validLogin");
+        user.setPassword("validPassport");
+        user.setAge(17);
+        registrationService.register(user);
+    }
+
+    @Test
+    void register_validUser_Ok() {
+        User user = new User();
+        user.setLogin("validLogin");
+        user.setPassword("validPassport");
+        user.setAge(20);
+
+        User registeredUser = registrationService.register(user);
+
+        assertNotNull(registeredUser);
+        assertEquals(user.getLogin(), registeredUser.getLogin());
+        assertEquals(user.getPassword(), registeredUser.getPassword());
+        assertEquals(user.getAge(), registeredUser.getAge());
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+}
