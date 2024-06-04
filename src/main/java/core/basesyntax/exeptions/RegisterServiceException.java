e0359f744d945a25661f0d4607a7d8771a5b8af4
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-06-04 20:21:20 +0300

    Created exceptions for UserValidator and RegisterService

---
+++ src/main/java/core/basesyntax/exeptions/RegisterServiceException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exeptions;
+
+public class RegisterServiceException extends Exception {
+    public RegisterServiceException(String message) {
+        super(message);
+    }
+}
