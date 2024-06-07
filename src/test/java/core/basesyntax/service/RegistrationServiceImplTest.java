c94fd58268c8cb05e6e5851d635364c38c2db32a
Greshch <greshchook@gmail.com>
2024-06-07 19:45:50 +0300

    8 fix style errors

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -2,8 +2,8 @@ package core.basesyntax.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import core.basesyntax.model.User;
 
+import core.basesyntax.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
