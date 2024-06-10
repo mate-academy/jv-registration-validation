3bb9d39b4e33994eff975a2bd277fd106fa46e98
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-10 08:13:56 +0300

    RegistrationServiceImplTest.java

--- src/test/java/core/basesyntax/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -5,6 +5,8 @@ import core.basesyntax.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class RegistrationServiceImplTest {
     private RegistrationService registrationService;
 
