d0739fe3440fc23a3f3dcfac253d1932c6d61935
Igor Glebov <igor.glebov0312@gmail.com>
2024-06-11 16:25:32 +0300

    implemented register method in RegistrationServiceImpl class, added test cases for this class

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,11 +2,10 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
-import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private StorageDao storageDao = new StorageDaoImpl();
+    private static final StorageDao storageDao = new StorageDaoImpl();
     private static final int MIN_AGE = 18;
     private static final int MIN_PASSWORD_LENGTH = 6;
     private static final int MIN_LOGIN_LENGTH = 6;
