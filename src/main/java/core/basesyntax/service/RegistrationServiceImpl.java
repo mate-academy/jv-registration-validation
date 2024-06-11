9635309bd66f4468ba9ab1a5e16ad747b2734447
Igor Glebov <igor.glebov0312@gmail.com>
2024-06-11 16:22:49 +0300

    implemented register method in RegistrationServiceImpl class, added test cases for this class

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,42 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
-    private final StorageDao storageDao = new StorageDaoImpl();
+    private StorageDao storageDao = new StorageDaoImpl();
+    private static final int MIN_AGE = 18;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_LOGIN_LENGTH = 6;
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Login cannot be null");
+        }
+        if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
+            throw new RegistrationException("Login length cannot be less then "
+                    + MIN_LOGIN_LENGTH);
+        }
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Password cannot be null");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RegistrationException("Password length cannot be less then "
+                    + MIN_PASSWORD_LENGTH);
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("Not valid age: "
+                    + user.getAge() + ". Min allowed age is " + MIN_AGE);
+        }
+        if (user.getAge() > Integer.MAX_VALUE) {
+            throw new RegistrationException("Incorrect age");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("User already exist");
+        }
+        storageDao.add(user);
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,208 @@
+package core.basesyntax.service;
+
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+    private static final RegistrationService REGISTRATION_SERVICE = new RegistrationServiceImpl();
+    private static final StorageDaoImpl STORAGE_DAO = new StorageDaoImpl();
+
+    @BeforeEach
+    void setUp() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void addUser_LoginLessThanMinLength_NotOk() {
+        User user = new User();
+        user.setLogin("Alice");
+        user.setPassword("qwerty1234");
+        user.setAge(19);
+        assertThrows(RegistrationException.class, () -> {
+            REGISTRATION_SERVICE.register(user);
+        });
+    }
+
+    @Test
+    void addUser_WithAllInvalidFields_NotOk() {
+        User user = new User();
+        user.setLogin("Alice");
+        user.setPassword("qwert");
+        user.setAge(17);
+        assertThrows(RegistrationException.class, () -> {
+            REGISTRATION_SERVICE.register(user);
+        });
+    }
+
+    @Test
+    void addUser_WithTheSameFields_NotOk() {
+        User user = new User();
+        user.setLogin("Alice123");
+        user.setPassword("qwerty1234");
+        user.setAge(19);
+        REGISTRATION_SERVICE.register(user);
+        User user1 = new User();
+        user1.setLogin("Alice123");
+        user1.setPassword("qwerty1234");
+        user1.setAge(19);
+        assertThrows(RegistrationException.class, () -> {
+            REGISTRATION_SERVICE.register(user1);
+        });
+    }
+
+    @Test
+    void addUser_CheckSizeOfArrayAfterAdd_Ok() {
+        int sizeBefore = Storage.people.size();
+        User user = new User();
+        user.setLogin("Alice123");
+        user.setPassword("qwerty1234");
+        user.setAge(19);
+        REGISTRATION_SERVICE.register(user);
+        int sizeAfter = Storage.people.size();
+        assertEquals(sizeAfter, sizeBefore + 1);
+    }
+
+    @Test
+    void checkSize_AfterAdd10Users_Ok() {
+        String login = "Alice123";
+        String password = "qwerty123";
+        int age = 19;
+        StringBuilder passwordBuilder = new StringBuilder().append(password);
+        StringBuilder loginBuilder = new StringBuilder().append(login);
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setLogin(loginBuilder.append(i).toString());
+            user.setPassword(passwordBuilder.append(i).toString());
+            user.setAge(age + i);
+            REGISTRATION_SERVICE.register(user);
+        }
+        int expectedSize = 10;
+        int actualSize = Storage.people.size();
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    void addUser_WithAgeLessThanMin_NotOk() {
+        User user = new User();
+        user.setLogin("Alice");
+        user.setPassword("qwerty1234");
+        user.setAge(17);
+        assertThrows(RegistrationException.class, () -> {
+            REGISTRATION_SERVICE.register(user);
+        });
+    }
+
+    @Test
+    void addUser_WithLoginNull_NotOk() {
+        User user = new User();
+        user.setLogin(null);
+        user.setPassword("qwerty1234");
+        user.setAge(17);
+        assertThrows(RegistrationException.class, () -> {
+            REGISTRATION_SERVICE.register(user);
+        });
+    }
+
+    @Test
+    void addUser_WithPasswordNull_NotOk() {
+        User user = new User();
+        user.setLogin("Bob1256");
+        user.setPassword(null);
+        user.setAge(25);
+        assertThrows(RegistrationException.class, () -> {
+            REGISTRATION_SERVICE.register(user);
+        });
+    }
+
+    @Test
+    void addUser_WithAgeHigherThanMaxInteger_NotOk() {
+        User user = new User();
+        user.setLogin("Bob1256");
+        user.setPassword(null);
+        user.setAge(Integer.MAX_VALUE + 1);
+        assertThrows(RegistrationException.class, () -> {
+            REGISTRATION_SERVICE.register(user);
+        });
+    }
+
+    @Test
+    void addUser_PasswordLessThanMinLength_NotOk() {
+        User user = new User();
+        user.setPassword("qwert");
+        user.setLogin("Bob1256");
+        user.setAge(26);
+        assertThrows(RegistrationException.class, () -> {
+            REGISTRATION_SERVICE.register(user);
+        });
+    }
+
+    @Test
+    void addUser_WithAllNormalFields_Ok() {
+        User user = new User();
+        user.setPassword("qwerty123");
+        user.setLogin("Alex1976");
+        user.setAge(27);
+        User returnedUser = REGISTRATION_SERVICE.register(user);
+        assertEquals(user, returnedUser);
+    }
+
+    @Test
+    void addUser_WithNegativeValueOfAge_NotOk() {
+        User user = new User();
+        user.setLogin("abcdr22");
+        user.setPassword("jjjjjjj");
+        user.setAge(-1);
+        assertThrows(RegistrationException.class, () -> {
+            REGISTRATION_SERVICE.register(user);
+        });
+    }
+
+    @Test
+    void getUserByLogin_Ok() {
+        User user = new User();
+        user.setLogin("abcdr22");
+        user.setPassword("jjjjjjj");
+        user.setAge(19);
+        REGISTRATION_SERVICE.register(user);
+        User getUser = STORAGE_DAO.get(user.getLogin());
+        assertEquals(user, getUser);
+    }
+
+    @Test
+    void getUser_ByNoExistLogin_NotOk() {
+        User user = new User();
+        user.setLogin("Alice123");
+        user.setPassword("qwerty1234");
+        user.setAge(19);
+        REGISTRATION_SERVICE.register(user);
+        User getUSer = STORAGE_DAO.get("Alice1234");
+        assertTrue(getUSer == null);
+    }
+
+    @Test
+    void getUser_WithoutUserGetterMethod_Ok() {
+        User user = new User();
+        user.setLogin("Alice123");
+        user.setPassword("qwerty1234");
+        user.setAge(19);
+        REGISTRATION_SERVICE.register(user);
+        User getUSer = STORAGE_DAO.get("Alice123");
+        assertEquals(user, getUSer);
+    }
+
+    @Test
+    void getUser_AfterAddFromStorageClass_Ok() {
+        User user = new User();
+        user.setLogin("Alice123");
+        user.setPassword("qwerty1234");
+        user.setAge(19);
+        Storage.people.add(user);
+        User getUser = STORAGE_DAO.get(user.getLogin());
+        assertEquals(user, getUser);
+    }
+}
