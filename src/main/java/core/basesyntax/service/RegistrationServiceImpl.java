502f5b09e208927182d0f229e889e8c6ae5fe82b
Tomasz Baran <MarcusOfficial@tuta.io>
2024-06-21 16:00:47 +0200

    initial commit

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,44 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int minAge = 18;
+    private static final int minCredentialLength = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RegistrationException("User can not be null");
+        }
+        Integer userAge = user.getAge();
+        if (userAge == null || userAge < minAge) {
+            String exceptionMessage =
+                    userAge == null ? "Age mustn't be null"
+                            : "You must be 18 years old in order to register";
+            throw new RegistrationException(exceptionMessage);
+        }
+        String userLogin = user.getLogin();
+        if (userLogin == null || userLogin.length() < minCredentialLength) {
+            String exceptionMessage =
+                    userLogin == null ? "Login mustn't be null"
+                            : "Your login must be at least 6 characters long";
+            throw new RegistrationException(exceptionMessage);
+        }
+        if (storageDao.get(userLogin) != null) {
+            throw new RegistrationException(
+                    "Such login already exists. Please choose a different one");
+        }
+        String userPassword = user.getPassword();
+        if (userPassword == null || userPassword.length() < minCredentialLength) {
+            String exceptionMessage =
+                    userPassword == null ? "Password mustn't be null"
+                            : "Your password must be at least 6 characters long";
+            throw new RegistrationException(exceptionMessage);
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/RegistrationValidationTest.java
@@ -0,0 +1,148 @@
+package core.basesyntax;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.RegistrationException;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationService;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationValidationTest {
+    private static final StorageDao storageDao = new StorageDaoImpl();
+    private RegistrationService registrationService;
+
+    //region Repeated Tasks
+    @BeforeEach
+    void setUp() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+        Storage.people.clear();
+    }
+    //endregion
+
+    //region assertEquals Tests
+    @Test
+    void register_validUser_Ok() {
+        User validUser = new User();
+        validUser.setAge(18);
+        validUser.setLogin("validLogin");
+        validUser.setPassword("validPassword");
+        User actual = registrationService.register(validUser);
+        assertEquals(actual, validUser);
+    }
+
+    @Test
+    void getUser_AfterAddFromStorageClass_Ok() {
+        User user = new User();
+        user.setAge(19);
+        user.setLogin("test007");
+        user.setPassword("qwerty1234");
+        Storage.people.add(user);
+        User getUser = storageDao.get(user.getLogin());
+        assertEquals(user, getUser);
+    }
+    //endregion
+
+    //region assertThrows Tests
+    @Test
+    void register_invalidUser_notOk() {
+        User invalidAgeUser = new User();
+        invalidAgeUser.setAge(17);
+        invalidAgeUser.setLogin("validName");
+        invalidAgeUser.setPassword("validPassword");
+        assertThrows(RegistrationException.class,
+                () -> registrationService.register(invalidAgeUser));
+
+        User invalidLoginUser = new User();
+        invalidLoginUser.setAge(18);
+        invalidLoginUser.setLogin("");
+        invalidLoginUser.setPassword("validPassword");
+        assertThrows(RegistrationException.class,
+                () -> registrationService.register(invalidLoginUser));
+
+        User invalidPasswordUser = new User();
+        invalidPasswordUser.setAge(19);
+        invalidPasswordUser.setLogin("validName");
+        invalidPasswordUser.setPassword("abc");
+        assertThrows(RegistrationException.class,
+                () -> registrationService.register(invalidPasswordUser));
+    }
+
+    @Test
+    void register_existingUser_notOk() {
+        User user = new User();
+        user.setLogin("userTest");
+        Storage.people.add(user);
+        User userLoginCopy = new User();
+        userLoginCopy.setAge(Integer.MAX_VALUE);
+        userLoginCopy.setLogin(user.getLogin());
+        userLoginCopy.setPassword("validPassword");
+        assertThrows(RegistrationException.class,
+                () -> registrationService.register(userLoginCopy));
+    }
+
+    @Test
+    void register_ageNotEligible_notOk() {
+        User user = new User();
+        user.setAge(0);
+        user.setLogin("userValid");
+        user.setPassword("password");
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_loginShorterThan6_notOk() {
+        User user = new User();
+        user.setAge(55);
+        user.setLogin("user1");
+        user.setPassword("password");
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_passwordShorterThan6_notOk() {
+        User user = new User();
+        user.setAge(50);
+        user.setLogin("userValid");
+        user.setPassword("abc");
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_nullAge_notOk() {
+        User user = new User();
+        user.setAge(null);
+        user.setLogin("userValid");
+        user.setPassword("abc123");
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_nullLogin_notOk() {
+        User user = new User();
+        user.setAge(99);
+        user.setLogin(null);
+        user.setPassword("abc123");
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+
+    @Test
+    void register_nullPassword_notOk() {
+        User user = new User();
+        user.setAge(22);
+        user.setLogin("user123");
+        user.setPassword(null);
+        assertThrows(RegistrationException.class, () -> registrationService.register(user));
+    }
+    //endregion
+}
