ea9b1ead35d990a376d71fad21e047a3115e516b
Nikita Boyko <nikitabojko57@gmail.com>
2024-06-26 12:58:28 +0300

    change method register and tests after AI mentor review

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -12,22 +12,27 @@ public class RegistrationServiceImpl implements RegistrationService {
     public User register(User user) throws CustomException {
         if (user == null) {
             throw new CustomException("User is null.");
-        } else if (user.getLogin() == null
-                || user.getPassword() == null
-                || user.getAge() == null) {
-            throw new CustomException("The parameters of user are null.");
+        } else if (user.getLogin() == null) {
+            throw new CustomException("The login is null.");
+        } else if (user.getPassword() == null) {
+            throw new CustomException("The password is null.");
+        } else if (user.getAge() == null) {
+            throw new CustomException("The age is null.");
         }
         User existingUser = storageDao.get(user.getLogin());
-        if (existingUser != null && user.getLogin().equals(storageDao.get(user.getLogin())
-                .getLogin())) {
+        if (existingUser != null && user.getLogin().equals(existingUser.getLogin())) {
             throw new CustomException("There is an user with the same login.");
-        } else if (user.getLogin().length() >= 6
-                && user.getPassword().length() >= 6
-                && user.getAge() >= 18) {
-            storageDao.add(user);
-            return user;
-        } else {
-            throw new CustomException("User doesn't have a valid data.");
         }
+        if (user.getLogin().length() < 6) {
+            throw new CustomException("The length of login is less than 6.");
+        } else if (user.getPassword().length() < 6) {
+            throw new CustomException("The length of password is less than 6.");
+        } else if (user.getAge() < 0) {
+            throw new CustomException("The age of user must be greater than 0.");
+        } else if (user.getAge() < 18) {
+            throw new CustomException("The age of user is less than 18.");
+        }
+        storageDao.add(user);
+        return user;
     }
 }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -18,10 +18,10 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void userIsNull_notOk() {
+    void register_nullUser_notOk() {
         User user = null;
         try {
-            User register = service.register(user);
+            service.register(user);
         } catch (CustomException e) {
             System.out.println("Custom exception was catched.");
             return;
@@ -30,23 +30,75 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void userWithNullFields_notOk() {
+    void register_nullFields_notOk() {
         User user = new User();
         assertThrows(CustomException.class, () -> service.register(user));
     }
 
     @Test
-    void userWithNoValidData_notOk() {
+    void register_noValidLogin_notOk() {
         User user = new User();
         user.setId(1L);
-        user.setAge(14);
+        user.setAge(19);
         user.setLogin("login");
         user.setPassword("password");
         assertThrows(CustomException.class, () -> service.register(user));
     }
 
     @Test
-    void userWithValidData_ok() {
+    void register_loginMinimumLength_Ok() {
+        User user = new User();
+        user.setId(1L);
+        user.setAge(19);
+        user.setLogin("login1");
+        user.setPassword("pass123");
+        User register = service.register(user);
+        assertEquals(user, register);
+    }
+
+    @Test
+    void register_noValidPassword_notOk() {
+        User user = new User();
+        user.setId(1L);
+        user.setAge(19);
+        user.setLogin("goodlogin");
+        user.setPassword("pass");
+        assertThrows(CustomException.class, () -> service.register(user));
+    }
+
+    @Test
+    void register_passwordMinimumLength_Ok() {
+        User user = new User();
+        user.setId(1L);
+        user.setAge(19);
+        user.setLogin("goodlogin");
+        user.setPassword("pass12");
+        User register = service.register(user);
+        assertEquals(user, register);
+    }
+
+    @Test
+    void register_negativeAge_notOk() {
+        User user = new User();
+        user.setId(1L);
+        user.setAge(-8);
+        user.setLogin("goodlogin");
+        user.setPassword("password");
+        assertThrows(CustomException.class, () -> service.register(user));
+    }
+
+    @Test
+    void register_ageLessThan18_notOk() {
+        User user = new User();
+        user.setId(1L);
+        user.setAge(14);
+        user.setLogin("goodlogin");
+        user.setPassword("password");
+        assertThrows(CustomException.class, () -> service.register(user));
+    }
+
+    @Test
+    void register_ValidData_ok() {
         User user = new User();
         user.setId(1L);
         user.setAge(19);
@@ -57,7 +109,7 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void registerExistingUser_notOk() {
+    void register_ExistingUser_notOk() {
         User user = new User();
         user.setId(1L);
         user.setAge(28);
@@ -69,6 +121,6 @@ class RegistrationServiceImplTest {
         anotherUser.setAge(19);
         anotherUser.setLogin("greatuser12");
         anotherUser.setPassword("password12");
-        assertThrows(CustomException.class, () -> service.register(user));
+        assertThrows(CustomException.class, () -> service.register(anotherUser));
     }
 }
