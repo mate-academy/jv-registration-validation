e4316efdff68b6e18ff2bba3f0b6f5cc5292617b
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-12 18:49:26 +0300

    The registry method is implemented. The validate User method has been created to fulfill all registration rules.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,39 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.ValidationFailedException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int DEFAULT_LENGTH = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new ValidationFailedException("User is null");
+        }
+        validateUser(user);
+        return storageDao.add(user);
+    }
+
+    private void validateUser(User user) {
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new ValidationFailedException("This login is already taken: " + user.getLogin());
+        }
+        if (user.getLogin() == null || user.getPassword() == null) {
+            throw new ValidationFailedException("Login and password can't be null");
+        }
+        if (user.getLogin().length() < DEFAULT_LENGTH
+                || user.getPassword().length() < DEFAULT_LENGTH) {
+            throw new ValidationFailedException("Login and password must be at least 6 characters");
+        }
+        if (user.getAge() == null) {
+            throw new ValidationFailedException("Age can't be null");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new ValidationFailedException("Can't register your age is below 18");
+        }
     }
 }
