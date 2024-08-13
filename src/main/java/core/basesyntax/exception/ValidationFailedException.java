338b543a7a40f926fb1ada4a441572bcb8f6a1d1
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-13 15:40:04 +0300

    The custom class exception has been renamed.

--- src/main/java/core/basesyntax/exception/ValidationFailedException.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax.exception;
-
-public class ValidationFailedException extends RuntimeException {
-    public ValidationFailedException(String message) {
-        super(message);
-    }
-}
