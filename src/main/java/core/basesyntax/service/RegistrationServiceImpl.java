7d820d8f1455b025854cc506b1c3cf79cc67c235
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-08-13 15:41:36 +0300

    Fixed custom exception issues.

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,7 +2,7 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
-import core.basesyntax.exception.ValidationFailedException;
+import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
@@ -13,7 +13,7 @@ public class RegistrationServiceImpl implements RegistrationService {
     @Override
     public User register(User user) {
         if (user == null) {
-            throw new ValidationFailedException("User is null");
+            throw new RegistrationException("User is null");
         }
         validateUser(user);
         return storageDao.add(user);
@@ -21,25 +21,25 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     private void validateUser(User user) {
         if (storageDao.get(user.getLogin()) != null) {
-            throw new ValidationFailedException("This login is already taken: " + user.getLogin());
+            throw new RegistrationException("This login is already taken: " + user.getLogin());
         }
         if (user.getLogin() == null) {
-            throw new ValidationFailedException("Login can't be null");
+            throw new RegistrationException("Login can't be null");
         }
         if (user.getPassword() == null) {
-            throw new ValidationFailedException("Password can't be null");
+            throw new RegistrationException("Password can't be null");
         }
         if (user.getLogin().length() < DEFAULT_LENGTH) {
-            throw new ValidationFailedException("Login must be at least 6 characters");
+            throw new RegistrationException("Login must be at least 6 characters");
         }
         if (user.getPassword().length() < DEFAULT_LENGTH) {
-            throw new ValidationFailedException("Password must be at least 6 characters");
+            throw new RegistrationException("Password must be at least 6 characters");
         }
         if (user.getAge() == null) {
-            throw new ValidationFailedException("Age can't be null");
+            throw new RegistrationException("Age can't be null");
         }
         if (user.getAge() < MIN_AGE) {
-            throw new ValidationFailedException("Can't register your age: "
+            throw new RegistrationException("Can't register your age: "
                     + user.getAge() + " is below 18");
         }
     }
