a2fed03a37bbd17aa4c67741ee5a59ab7688e9a5
Nikita Lafinskiy <niklafinskiy@gmail.com>
2024-08-21 11:58:14 +0200

    made changes

--- src/test/java/core/basesyntax/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -3,6 +3,7 @@ package core.basesyntax;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationException;
 import core.basesyntax.service.RegistrationServiceImpl;
@@ -16,11 +17,9 @@ public class RegistrationServiceTest {
     private static final int INVALID_USER_AGE = 2;
     private static final String INVALID_USER_LOGIN = "login";
     private static final String INVALID_USER_PASSWORD = "pass";
-    private static final long USER_ID = 123L;
     private static final int DUPLICATE_VALID_USER_AGE = 20;
     private static final String DUPLICATE_VALID_USER_LOGIN = "LongLogin";
     private static final String DUPLICATE_VALID_USER_PASSWORD = "LongPassword";
-    private static final long DUPLICATE_USER_ID = 321L;
     private static final int NEGATIVE_USER_AGE = -10;
     private static RegistrationServiceImpl registrationService;
 
@@ -31,17 +30,17 @@ public class RegistrationServiceTest {
 
     @Test
     void register_userValid_Ok() {
-        User validUser = new User(USER_ID,
+        User expected = new User(
                 VALID_USER_LOGIN,
                 VALID_USER_PASSWORD,
                 VALID_USER_AGE);
-        User actual = registrationService.register(validUser);
-        assertEquals(validUser, actual);
+        User actual = registrationService.register(expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     void register_userAgeInvalid_notOk() {
-        User invalidUser = new User(USER_ID,
+        User invalidUser = new User(
                 VALID_USER_LOGIN,
                 VALID_USER_PASSWORD,
                 INVALID_USER_AGE);
@@ -52,7 +51,7 @@ public class RegistrationServiceTest {
 
     @Test
     void register_userLoginInvalid_notOk() {
-        User invalidUser = new User(USER_ID,
+        User invalidUser = new User(
                 INVALID_USER_LOGIN,
                 VALID_USER_PASSWORD,
                 VALID_USER_AGE);
@@ -63,7 +62,7 @@ public class RegistrationServiceTest {
 
     @Test
     void register_userPasswordInvalid_notOk() {
-        User invalidUser = new User(USER_ID,
+        User invalidUser = new User(
                 VALID_USER_LOGIN,
                 INVALID_USER_PASSWORD,
                 VALID_USER_AGE);
@@ -74,11 +73,11 @@ public class RegistrationServiceTest {
 
     @Test
     void register_userAlreadyExists_notOk() {
-        User duplicateUser = new User(DUPLICATE_USER_ID,
+        User duplicateUser = new User(
                 DUPLICATE_VALID_USER_LOGIN,
                 DUPLICATE_VALID_USER_PASSWORD,
                 DUPLICATE_VALID_USER_AGE);
-        registrationService.register(duplicateUser);
+        Storage.people.add(duplicateUser);
         assertThrows(RegistrationException.class, () -> {
             registrationService.register(duplicateUser);
         });
@@ -86,15 +85,14 @@ public class RegistrationServiceTest {
 
     @Test
     void register_userIsNull_notOk() {
-        User nullUser = null;
         assertThrows(RegistrationException.class, () -> {
-            registrationService.register(nullUser);
+            registrationService.register(null);
         });
     }
 
     @Test
     void register_userLoginIsNull_notOk() {
-        User nullLoginUser = new User(USER_ID,
+        User nullLoginUser = new User(
                 null,
                 VALID_USER_PASSWORD,
                 VALID_USER_AGE);
@@ -105,7 +103,7 @@ public class RegistrationServiceTest {
 
     @Test
     void register_userPasswordIsNull_noOk() {
-        User nullPasswordUser = new User(USER_ID,
+        User nullPasswordUser = new User(
                 VALID_USER_LOGIN,
                 null,
                 VALID_USER_AGE);
@@ -116,7 +114,7 @@ public class RegistrationServiceTest {
 
     @Test
     void register_userAgeIsNegative() {
-        User negativeAgeUser = new User(USER_ID,
+        User negativeAgeUser = new User(
                 VALID_USER_LOGIN,
                 VALID_USER_PASSWORD,
                 NEGATIVE_USER_AGE);
