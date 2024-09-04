78a7a787b63076167acef292226f54c93e4e4b83
homeworkfirst <zookotelif@gmail.com>
2024-09-04 22:22:37 +0300

    I don t why it doesn t work I need help

--- src/test/java/core/basesyntax/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -1,15 +1,15 @@
 package core.basesyntax;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import core.basesyntax.exception.InvalidUserException;
 import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationService;
 import core.basesyntax.service.RegistrationServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RegistrationServiceTest {
 
