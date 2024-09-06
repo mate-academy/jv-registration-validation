f61be4ae83441a249eaafa74375f830390ae7cc4
Oleksii <obabych1@stu.vistula.edu.pl>
2024-09-06 16:32:56 +0200

    fix checkstyle

--- src/test/java/core/basesyntax/validation/UserValidatorImplTest.java
+++ src/test/java/core/basesyntax/validation/UserValidatorImplTest.java
@@ -2,10 +2,8 @@ package core.basesyntax.validation;
 
 import core.basesyntax.exception.UserValidationException;
 import core.basesyntax.model.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UserValidatorImplTest {
     private final UserValidator validator = new UserValidatorImpl();
@@ -16,54 +14,54 @@ class UserValidatorImplTest {
         user.setAge(18);
         user.setLogin("valid1");
         user.setPassword("valid1");
-        assertDoesNotThrow(() -> validator.validate(user));
+        Assertions.assertDoesNotThrow(() -> validator.validate(user));
     }
 
     @Test
     public void validate_UserIsNull_notOk() {
         User user = null;
-        assertThrows(UserValidationException.class, () -> validator.validate(user));
+        Assertions.assertThrows(UserValidationException.class, () -> validator.validate(user));
     }
 
     @Test
     public void validate_LoginIsNull_notOk() {
         User user = new User();
         user.setLogin(null);
-        assertThrows(UserValidationException.class, () -> validator.validate(user));
+        Assertions.assertThrows(UserValidationException.class, () -> validator.validate(user));
     }
 
     @Test
     public void validate_LoginIsLessThan6Characters_notOk() {
         User user = new User();
         user.setLogin("null1");
-        assertThrows(UserValidationException.class, () -> validator.validate(user));
+        Assertions.assertThrows(UserValidationException.class, () -> validator.validate(user));
     }
 
     @Test
     public void validate_PasswordIsNull_notOk() {
         User user = new User();
         user.setPassword(null);
-        assertThrows(UserValidationException.class, () -> validator.validate(user));
+        Assertions.assertThrows(UserValidationException.class, () -> validator.validate(user));
     }
 
     @Test
     public void validate_PasswordIsLessThan6Characters_notOk() {
         User user = new User();
         user.setPassword("null1");
-        assertThrows(UserValidationException.class, () -> validator.validate(user));
+        Assertions.assertThrows(UserValidationException.class, () -> validator.validate(user));
     }
 
     @Test
     public void validate_AgeIsNull_notOk() {
         User user = new User();
         user.setAge(null);
-        assertThrows(UserValidationException.class, () -> validator.validate(user));
+        Assertions.assertThrows(UserValidationException.class, () -> validator.validate(user));
     }
 
     @Test
     public void validate_AgeIsLessThan18YearsOld_notOk() {
         User user = new User();
         user.setAge(17);
-        assertThrows(UserValidationException.class, () -> validator.validate(user));
+        Assertions.assertThrows(UserValidationException.class, () -> validator.validate(user));
     }
 }
