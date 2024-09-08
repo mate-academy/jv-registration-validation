2d690486a0a7df1f0ee36b1cfa1b9644ed964186
homeworkfirst <zookotelif@gmail.com>
2024-09-08 20:38:00 +0300

    I don t why it doesn t work I need help

--- src/test/java/core/basesyntax/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceTest.java
@@ -5,8 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.exception.InvalidUserException;
-import core.basesyntax.model.User;
 import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationService;
 import core.basesyntax.service.RegistrationServiceImpl;
 import org.junit.jupiter.api.BeforeAll;
