39ad2ed90e9836ca2a07608ade67802488185afb
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-09-12 10:02:59 +0300

    checkstyling

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -4,18 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.model.User;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RegistrationServiceImplTest {
     private static RegistrationService registrationService;
-    private final User correctUser = new User("sixChr", "sixChr", 18);
-    private final User moreThenEnough = new User("moreThenSix", "moreThenSix",23);
-    private final User loginCopy = new User("sixChr", "blaBla", 1000);
+    private final User edgeUser = new User("sixChr", "sixChr", 18);
+    private final User correctUser = new User("moreThenSix", "moreThenSix",23);
+    private final User userWithSameLogin = new User("moreThenSix", "blaBla", 1000);
     private final User nullUser = new User();
-    private final User wrongLogin = new User("Short", "notShort", 19);
-    private final User wrongPassword = new User("notShort", "Short", 19);
-    private final User wrongAge = new User("notShort", "notShort", 17);
+    private final User shortLogin = new User("Short", "notShort", 19);
+    private final User shortPassword = new User("notShort", "Short", 19);
+    private final User shortAge = new User("notShort", "notShort", 17);
     private final User emptyLogin = new User(null, "notShort", 19);
     private final User emptyPassword = new User("notShort", null, 19);
     private final User emptyAge = new User("notShort", "notShort", null);
@@ -33,42 +33,45 @@ class RegistrationServiceImplTest {
     }
 
     @Test
+    void register_edgeUser_ok() {
+        User actual = registrationService.register(edgeUser);
+        assertEquals(actual, edgeUser);
+    }
+
+    @Test
+    @Order(1)
     void register_correctUser_ok() {
         User actual = registrationService.register(correctUser);
         assertEquals(actual, correctUser);
     }
 
     @Test
-    void register_moreThenEnough_ok() {
-        User actual = registrationService.register(moreThenEnough);
-        assertEquals(actual, moreThenEnough);
+    @Order(2)
+    void register_userWithSameLogin_notOk() {
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(userWithSameLogin);
+        });
     }
 
     @Test
     void register_underAge_notOk() {
         assertThrows(RegistrationException.class, () -> {
-            registrationService.register(wrongAge);
+            registrationService.register(shortAge);
         });
     }
 
-    @Test
-    void register_loginCopy_notOk() {
-        assertThrows(RegistrationException.class, () -> {
-            registrationService.register(loginCopy);
-        });
-    }
 
     @Test
     void register_shortLogin_notOk() {
         assertThrows(RegistrationException.class, () -> {
-            registrationService.register(wrongLogin);
+            registrationService.register(shortLogin);
         });
     }
 
     @Test
     void register_shortPassword_notOk() {
         assertThrows(RegistrationException.class, () -> {
-            registrationService.register(wrongPassword);
+            registrationService.register(shortPassword);
         });
     }
 
