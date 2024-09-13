b034938f38619da24fd6b3233d87664476921ca7
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-09-14 00:10:29 +0300

    Implemented method register

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,14 +2,34 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.RegistrationFailedException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int ACCEPTABLE_AGE = 18;
+    private static final String UPPER_CASE_SYMBOLS = ".*[A-Z].*";
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-
-        return null;
+        if (user.getLogin() == null) {
+            throw new RegistrationFailedException("Login can't be null" + user.getLogin());
+        }
+        if (user.getPassword() == null) {
+            throw new RegistrationFailedException("Password can't be null" + user.getPassword());
+        }
+        if (user.getAge() == null) {
+            throw new RegistrationFailedException("Age can't be null" + user.getAge());
+        }
+        if (storageDao.get(user.getLogin()).getLogin().equals(user.getLogin())) {
+            throw new RegistrationFailedException("Login is already exist " + user.getLogin());
+        }
+        if (user.getLogin().matches(UPPER_CASE_SYMBOLS)) {
+            throw new RegistrationFailedException("Enter the login in lower case " + user.getLogin());
+        }
+        if (user.getAge() < ACCEPTABLE_AGE) {
+            throw new RegistrationFailedException("You are not of legal age " + user.getAge());
+        }
+        return storageDao.add(user);
     }
 }
