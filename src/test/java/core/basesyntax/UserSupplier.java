7c9d46c113b3d64e076e754a08c526defdc65ec6
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-09-13 22:44:35 +0300

    Created user supplier for fast creating users

---
+++ src/test/java/core/basesyntax/UserSupplier.java
@@ -0,0 +1,13 @@
+package core.basesyntax;
+
+import core.basesyntax.model.User;
+
+public class UserSupplier {
+    public User of(String login, String password, int age) {
+        User newUser = new User();
+        newUser.setLogin(login);
+        newUser.setPassword(password);
+        newUser.setAge(age);
+        return newUser;
+    }
+}
