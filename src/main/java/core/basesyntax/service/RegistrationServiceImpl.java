68e70f376f597ceaaa67eb1a1a33c9527cdf2354
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-09-14 22:47:35 +0300

    deleted registrationFailedException

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,7 +2,6 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
-import core.basesyntax.exception.RegistrationFailedException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
