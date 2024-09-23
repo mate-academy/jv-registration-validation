c8363239a29b7e9bf1cc161f570eca64198127cf
Ivan Demchenko <experywow@gmail.com>
2024-09-23 21:38:06 +0100

    Implemented user registration validation logic
    - Added new exceptions for validation:
      - Check for too short login (LoginTooShortException)
      - Check for too short password (PasswordTooShortException)
      - Check for user age restriction (AgeRestrictionException)
      - Check for existing user with the same login (UserAlreadyExistException)
    
    - Added tests for:
      - Registering a user with valid data
      - Attempting to register with an already existing login
      - Validating minimum login and password length
      - Validating user age and null age cases
      - Attempting to register null users and missing data
    
    - Using `@BeforeEach` to reset the storage state between tests
    - Adjustments made to ensure proper functioning of registration methods and tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,37 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exceptions.AgeRestrictionException;
+import core.basesyntax.exceptions.LoginTooShortException;
+import core.basesyntax.exceptions.PasswordTooShortException;
+import core.basesyntax.exceptions.UserAlreadyExistException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int LOGIN_MIN_LENGTH = 6;
+    private static final int PASSWORD_MIN_LENGTH = 6;
+    private static final int AGE_MINIMAL_LIMIT = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new IllegalArgumentException("User can't be null!");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new UserAlreadyExistException("User with same login already exist!");
+        }
+        if (user.getLogin() == null || user.getLogin().length() < LOGIN_MIN_LENGTH) {
+            throw new LoginTooShortException("Login should be more then six characters!");
+        }
+        if (user.getPassword() == null || user.getPassword().length() < PASSWORD_MIN_LENGTH) {
+            throw new PasswordTooShortException("Login should be more then six characters!");
+        }
+        if (user.getAge() == null || user.getAge() < AGE_MINIMAL_LIMIT) {
+            throw new AgeRestrictionException(
+                    "User with age less then 18 or null, can't be created!"
+            );
+        }
+        return storageDao.add(user);
     }
 }
