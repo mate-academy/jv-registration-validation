21863daf2f6fae614d4ac694df5f6e83a19d74aa
Volodymyr Nyshta <volodymyr.nyshta@gmail.com>
2024-09-30 22:55:07 +0300

    added new tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -12,10 +12,12 @@ public class RegistrationServiceImpl implements RegistrationService {
     private static final int MINIMAL_LOGIN_LENGTH = 6;
     private static final int MINIMAL_USER_AGE = 18;
     private static final int MINIMAL_PASSWORD_LENGTH = 6;
-    private final StorageDao storageDao = new StorageDaoImpl();
-    private Set<String> loginSet = new HashSet<>();
+    private final StorageDao storageDao;
+    private Set<String> loginSet;
 
-    private void loginSetInit() {
+    public RegistrationServiceImpl() {
+        storageDao = new StorageDaoImpl();
+        loginSet = new HashSet<>();
         for (User user : Storage.people) {
             loginSet.add(user.getLogin());
         }
@@ -23,27 +25,29 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) throws UserNotAllowed {
-        if (user != null) {
-            loginSetInit();
-            if (loginSet != null && loginSet.contains(user.getLogin())) {
-                throw new UserNotAllowed("User with login "
-                        + user.getLogin() + " already exist");
-            }
-            if (user.getLogin() == null || user.getLogin().length()
-                    < MINIMAL_LOGIN_LENGTH) {
-                throw new UserNotAllowed("Login must be at least "
-                        + MINIMAL_LOGIN_LENGTH + " characters long");
-            } else if (user.getPassword() == null || user.getPassword().length()
-                    < MINIMAL_PASSWORD_LENGTH) {
-                throw new UserNotAllowed("Password must be at least "
-                        + MINIMAL_PASSWORD_LENGTH + " characters long");
-            } else if (user.getAge() == null || user.getAge() < MINIMAL_USER_AGE) {
-                throw new UserNotAllowed("User must be at least "
-                        + MINIMAL_PASSWORD_LENGTH + " years old");
-            }
-            Storage.people.add(user);
-            return user;
+        validateUser(user);
+        storageDao.add(user);
+        loginSet.add(user.getLogin());
+        return user;
+    }
+
+    private void validateUser(User user) throws UserNotAllowed {
+        if (user == null) {
+            throw new UserNotAllowed("User can not be null");
         }
-        throw new UserNotAllowed("User can not be null");
+        if (loginSet.contains(user.getLogin())) {
+            throw new UserNotAllowed("User with login " + user.getLogin() + " already exists");
+        }
+        if (user.getLogin() == null || user.getLogin().length() < MINIMAL_LOGIN_LENGTH) {
+            throw new UserNotAllowed("Login cannot be null or less than "
+                    + MINIMAL_LOGIN_LENGTH + " characters long");
+        }
+        if (user.getPassword() == null || user.getPassword().length() < MINIMAL_PASSWORD_LENGTH) {
+            throw new UserNotAllowed("Password must be at least "
+                    + MINIMAL_PASSWORD_LENGTH + " characters long");
+        }
+        if (user.getAge() == null || user.getAge() < MINIMAL_USER_AGE) {
+            throw new UserNotAllowed("User must be at least " + MINIMAL_USER_AGE + " years old");
+        }
     }
 }
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,5 +1,7 @@
 package core.basesyntax.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.exception.UserNotAllowed;
@@ -8,11 +10,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
-    private RegistrationService registrationService = new RegistrationServiceImpl();
+    private RegistrationService registrationService;
     private User user;
 
     @BeforeEach
-    void setup() {
+     void setup() {
+        registrationService = new RegistrationServiceImpl();
         user = new User();
         user.setLogin("volodymyr.nyshta@gmail.com");
         user.setAge(21);
@@ -20,10 +23,25 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void userWithTheSameLoginAlreadyExists_NotOk() {
+    void register_validUser_Ok() {
+        User actual = new User();
+        actual.setAge(20);
+        actual.setPassword("asdfasdf");
+        actual.setLogin("testtesttest@gmail.com");
+        User registeredUser = registrationService.register(actual);
+        assertEquals(registeredUser, actual);
+    }
+
+    @Test
+    void register_userWithTheSameLoginAlreadyExists_NotOk() {
+        User user1 = new User();
+        user1.setAge(18);
+        user1.setLogin("volodymyrtest.nyshta@gmail.com");
+        user1.setPassword("1234567");
+        registrationService.register(user1);
         User user2 = new User();
         user2.setAge(18);
-        user2.setLogin("volodymyr.nyshta@gmail.com");
+        user2.setLogin("volodymyrtest.nyshta@gmail.com");
         user2.setPassword("1234567");
         registrationService.register(user);
         assertThrows(UserNotAllowed.class, () -> registrationService.register(user2));
@@ -35,37 +53,77 @@ class RegistrationServiceImplTest {
     }
 
     @Test
-    void loginLengthLessThen_6_CharactersIsNotOk() {
+    void register_loginLengthLessThan6Characters_notOk() {
         User actual = user;
         actual.setLogin("test");
         assertThrows(UserNotAllowed.class, () -> registrationService.register(actual));
     }
 
     @Test
-    void ageLessThan_18_NotOk() {
+    void register_loginNull_notOk() {
         User actual = user;
-        actual.setAge(12);
+        actual.setLogin(null);
         assertThrows(UserNotAllowed.class, () -> registrationService.register(actual));
     }
 
     @Test
-    void passwordLengthLessThan_6_characters_NotOk() {
+    void register_emptylogin_notOk() {
+        User actual = user;
+        actual.setLogin("");
+        assertThrows(UserNotAllowed.class, () -> registrationService.register(actual));
+    }
+
+    @Test
+    void register_passwordLengthLessThan6Characters_notOk() {
         User actual = user;
         actual.setPassword("test");
         assertThrows(UserNotAllowed.class, () -> registrationService.register(actual));
     }
 
     @Test
-    void passwordIsNull_NotOk() {
+    void register_passwordIsNull_notOk() {
         User actual = user;
         actual.setPassword(null);
         assertThrows(UserNotAllowed.class, () -> registrationService.register(actual));
     }
 
     @Test
-    void ageIsNull_NotOk() {
+    void register_emptyPassword_notOk() {
+        User actual = user;
+        actual.setPassword("");
+        assertThrows(UserNotAllowed.class, () -> registrationService.register(actual));
+    }
+
+    @Test
+    void register_ageIsNull_notOk() {
         User actual = user;
         actual.setAge(null);
         assertThrows(UserNotAllowed.class, () -> registrationService.register(actual));
+    }
+
+    @Test
+    void register_negativeAge_notOk() {
+        User actual = user;
+        actual.setAge(-1);
+        assertThrows(UserNotAllowed.class, () -> registrationService.register(actual));
+    }
+
+    @Test
+    void register_ageLessThan_18_notOk() {
+        User actual = user;
+        actual.setAge(12);
+        assertThrows(UserNotAllowed.class, () -> registrationService.register(actual));
+    }
+
+    @Test
+    void register_FildShuldNotBeNullAfterRegister() {
+        User actual = new User();
+        actual.setAge(19);
+        actual.setLogin("volodymyrTest");
+        actual.setPassword("123456qwe");
+        User registeredUser = registrationService.register(actual);
+        assertNotNull(registeredUser.getAge());
+        assertNotNull(registeredUser.getLogin());
+        assertNotNull(registeredUser.getPassword());
     }
 }
