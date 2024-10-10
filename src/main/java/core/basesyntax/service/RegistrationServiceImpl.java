4aec378e55ec52a3217a87786b50fdd83d2da8cb
Kate Kraska <kate2002.kk42@gmail.com>
2024-10-10 19:29:54 +0300

    add test

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -7,8 +7,32 @@ import core.basesyntax.model.User;
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
 
-    @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null) {
+            throw new RegistrationException(
+                    "Login cannot be null");
+        }
+
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException(
+                    "User with this login already exists");
+        }
+
+        if (user.getLogin().length() < 6) {
+            throw new RegistrationException(
+                    "Login must be at least 6 characters long");
+        }
+        if (user.getPassword() == null || user.getPassword().length() < 6) {
+            throw new RegistrationException(
+                    "Password must be at least 6 characters long");
+        }
+        if (user.getAge() < 18) {
+            throw new RegistrationException(
+                    "User must be at least 18 years old");
+        }
+
+        storageDao.add(user);
+
+        return user;
     }
 }
