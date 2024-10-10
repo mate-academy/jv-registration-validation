ec87e68207d5fbed1be2b3f2d27afade3b74d54d
Alinka Lyn <alinka.lyn@ukr.net>
2024-10-10 21:21:37 +0300

    fix

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -3,7 +3,6 @@ package core.basesyntax.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import core.basesyntax.exception.InvalidUserDataException;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.BeforeEach;
