40addd083c32e5d8471271964c7b5eb4acdb0345
iurii <kenu21@gmail.com>
2024-10-11 20:30:58 +0200

    JUNIT: refactored

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -7,6 +7,7 @@ import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
 
+    public static final int MIN_LENGTH_PASSWORD_PARAMETER = 6;
     public static final int MIN_LENGTH_LOGIN_PARAMETER = 6;
 
     private final StorageDao storageDao = new StorageDaoImpl();
@@ -22,25 +23,31 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     private void checkUserExist(User user) {
         if (storageDao.get(user.getLogin()) != null) {
-            throw new UserRegisterException("User exists!");
+            throw new UserRegisterException(
+                "User with login " + user.getLogin() + " already exists!");
         }
     }
 
     private void checkUserLoginLength(User user) {
-        if (user.getLogin().length() < MIN_LENGTH_LOGIN_PARAMETER) {
-            throw new UserRegisterException("Login is short!");
+        if (user.getLogin() != null && user.getLogin().length() < MIN_LENGTH_LOGIN_PARAMETER) {
+            throw new UserRegisterException(
+                "User login should be at least " + MIN_LENGTH_LOGIN_PARAMETER
+                    + " characters long!");
         }
     }
 
     private void checkUserPassLength(User user) {
-        if (user.getPassword().length() < MIN_LENGTH_LOGIN_PARAMETER) {
-            throw new UserRegisterException("Pass is short!");
+        if (user.getPassword() != null
+                && user.getPassword().length() < MIN_LENGTH_PASSWORD_PARAMETER) {
+            throw new UserRegisterException(
+                "User password should be at least " + MIN_LENGTH_PASSWORD_PARAMETER
+                    + " characters long!");
         }
     }
 
     private void checkUserAge(User user) {
-        if (user.getAge() < 18) {
-            throw new UserRegisterException("Age is bellow 18!");
+        if (user.getAge() != null && user.getAge() < 18) {
+            throw new UserRegisterException("User should be at least 18 years old!");
         }
     }
 }
