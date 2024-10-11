0ca3149251a495c9c7e9872a76cbcd0f17de24ec
clo3eX <pansydaisy83@gmail.com>
2024-10-11 15:22:01 +0200

    fixed errors

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,16 +1,18 @@
 package core.basesyntax.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.exceptions.RegistrationException;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RegistrationServiceImplTest {
     private static RegistrationService registrationService;
     private static StorageDaoImpl storageDao;
+
     @BeforeAll
     static void beforeAll() {
         registrationService = new RegistrationServiceImpl();
@@ -27,57 +29,43 @@ class RegistrationServiceImplTest {
     @Test
     void register_userPasswordIsShort_isNotOK() {
         User expected = new User("BobWithShortPassword", "12", 18);
-        assertThrows(RegistrationException.class, () -> {
-            registrationService.register(expected);
-        });
+        assertThrows(RegistrationException.class, () -> registrationService.register(expected));
     }
 
     @Test
     void register_userLoginIsShort_isNotOK() {
         User expected = new User("Bob", "1234567", 18);
-        assertThrows(RegistrationException.class, () -> {
-            registrationService.register(expected);
-        });
+        assertThrows(RegistrationException.class, () -> registrationService.register(expected));
     }
 
     @Test
     void register_userUnderage_isNotOK() {
         User expected = new User("BobChild", "1234567", 15);
-        assertThrows(RegistrationException.class, () -> {
-            registrationService.register(expected);
-        });
+        assertThrows(RegistrationException.class, () -> registrationService.register(expected));
     }
 
     @Test
     void register_userLoginIsNull_isNotOK() {
         User expected = new User(null, "1234567", 18);
-        assertThrows(RegistrationException.class, () -> {
-            registrationService.register(expected);
-        });
+        assertThrows(RegistrationException.class, () -> registrationService.register(expected));
     }
 
     @Test
     void register_userPasswordIsNull_isNotOk() {
         User expected = new User("BobWithoutPassword", null, 18);
-        assertThrows(RegistrationException.class, () -> {
-            registrationService.register(expected);
-        });
+        assertThrows(RegistrationException.class, () -> registrationService.register(expected));
     }
 
     @Test
     void register_userAgeIsNull_isNotOK() {
         User expected = new User("BobWithoutAge", "1234567", null);
-        assertThrows(RegistrationException.class, () -> {
-            registrationService.register(expected);
-        });
+        assertThrows(RegistrationException.class, () -> registrationService.register(expected));
     }
 
     @Test
     void register_userAlreadyExists_isNotOK() {
-        User user = new User("BobAlredyExists", "john123", 28);
-        storageDao.add(user);
-        assertThrows(RegistrationException.class, () -> {
-            registrationService.register(user);
-        });
+        User expected = new User("BobAlreadyExists", "john123", 28);
+        storageDao.add(expected);
+        assertThrows(RegistrationException.class, () -> registrationService.register(expected));
     }
 }
