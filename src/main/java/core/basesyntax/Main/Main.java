cf1f1c54a223cb03fb37c91b2e039d00e522c6cd
Mykola Kovbasiuk <nikolya.cr@gmail.com>
2024-10-17 10:49:29 +0200

    first try

---
+++ src/main/java/core/basesyntax/Main/Main.java
@@ -0,0 +1,14 @@
+package core.basesyntax.Main;
+
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationServiceImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        RegistrationServiceImpl registrationService = new RegistrationServiceImpl();
+        User user1 = new User(10l, "Mykola", "1234567", 21);
+        registrationService.register(user1);
+        User user2 = new User(10l, "Mykol", "1234567", 17);
+        registrationService.register(user2);
+    }
+}
--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,7 +1,9 @@
 package core.basesyntax.service;
 
+import core.basesyntax.Exception.NoValidDataException;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
@@ -9,6 +11,23 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        char []loginLength = user.getLogin().toCharArray();
+        char []passwordLength = user.getPassword().toCharArray();
+        for (User user1 : Storage.people) {
+            if (user.getLogin().equals(user1.getLogin())) {
+                throw new NoValidDataException("User with such login already exist");
+            }
+        }
+        if (loginLength.length < 6) {
+            throw new NoValidDataException("Login should have at least 6 characters");
+        }
+        if (passwordLength.length < 6) {
+            throw new NoValidDataException("Password should have at least 6 characters");
+        }
+        if (user.getAge() < 18) {
+            throw new NoValidDataException("User need to be at least 18 years old");
+        }
+        storageDao.add(user);
+        return user;
     }
 }
