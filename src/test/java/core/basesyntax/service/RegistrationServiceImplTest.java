b28ad8d0a023501c7cea97de9eebbe961f3d2494
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-10-30 00:14:17 +0200

    Fix

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -4,10 +4,10 @@ import core.basesyntax.db.Storage;
 import core.basesyntax.exceptions.InvalidDataException;
 import core.basesyntax.exceptions.UserExistException;
 import core.basesyntax.model.User;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 class RegistrationServiceImplTest {
     private static RegistrationService registrationService;
