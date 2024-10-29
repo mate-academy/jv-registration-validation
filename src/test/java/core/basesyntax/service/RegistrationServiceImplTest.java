f23181535234ec90da7f55b39164d5dbdc328801
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-10-30 00:15:06 +0200

    Fix

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -6,8 +6,8 @@ import core.basesyntax.exceptions.UserExistException;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class RegistrationServiceImplTest {
     private static RegistrationService registrationService;
