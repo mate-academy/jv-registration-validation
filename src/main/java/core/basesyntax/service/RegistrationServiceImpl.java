594ec2b50691546728dede51502e24351a24719d
kupnovytskyi <kupnovytskyinazar@gmail.com>
2024-10-31 10:49:26 +0200

    fixed problem issues

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,7 +2,6 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
-import core.basesyntax.db.Storage;
 import core.basesyntax.exception.InvalidDataException;
 import core.basesyntax.model.User;
 
@@ -17,35 +16,43 @@ public class RegistrationServiceImpl implements RegistrationService {
         if (user == null) {
             throw new InvalidDataException("User cannot be null");
         }
+
         if (user.getLogin() == null) {
             throw new InvalidDataException("User login cannot be null");
         }
-        if (Storage.people.contains(storageDao.get(user.getLogin()))) {
+
+        if (storageDao.get(user.getLogin()) != null) {
             throw new InvalidDataException("User already exists "
                     + user.getLogin()
                     + " with such login");
         }
+
         if (user.getLogin().length() < MIN_LENGTH) {
             throw new InvalidDataException("Login must be at least "
                     + MIN_LENGTH
                     + " current long "
                     + user.getLogin().length());
         }
+
         if (user.getPassword() == null) {
             throw new InvalidDataException("User password cannot be null");
         }
+
         if (user.getPassword().length() < MIN_LENGTH) {
             throw new InvalidDataException("Password must be at least"
                     + MIN_LENGTH
                     + " current length"
                     + user.getPassword().length());
         }
+
         if (user.getAge() == null) {
             throw new InvalidDataException("User age cannot be null");
         }
+
         if (user.getAge() < 0) {
             throw new InvalidDataException("Age cannot be negative");
         }
+
         if (user.getAge() < 18) {
             throw new InvalidDataException("User must be at least "
                     + MIN_AGE
--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -16,15 +16,14 @@ class RegistrationServiceImplTest {
     private static final int MIN_AGE = 18;
     private static StorageDao storageDao;
     private static RegistrationService registrationService;
-    private User firstUser;
-    private User secondUser;
-    private User thirdUser;
-    private User fourthUser;
-    private User fifthUser;
-    private User sixthUser;
-    private User seventhUser;
-    private User eighthUser;
-    private User nineUser;
+    private User validUser;
+    private User loginIsNull;
+    private User shortName;
+    private User shortPassword;
+    private User passwordIsNull;
+    private User ageIsNotEnough;
+    private User ageIsNegative;
+    private User ageIsNull;
 
     @BeforeAll
     static void beforeAll() {
@@ -39,176 +38,170 @@ class RegistrationServiceImplTest {
 
     @BeforeEach
     void setUp() {
-        firstUser = new User();
-        firstUser.setId(1L);
-        firstUser.setLogin("tarsan@Gmail.com");
-        firstUser.setPassword("123456");
-        firstUser.setAge(30);
+        validUser = new User();
+        validUser.setId(1L);
+        validUser.setLogin("tarsan@Gmail.com");
+        validUser.setPassword("123456");
+        validUser.setAge(30);
 
-        secondUser = new User();
-        secondUser.setId(2L);
-        secondUser.setLogin("nazar@gmail.com");
-        secondUser.setPassword("123456");
-        secondUser.setAge(25);
+        loginIsNull = new User();
+        loginIsNull.setId(3L);
+        loginIsNull.setLogin(null);
+        loginIsNull.setPassword("11223344");
+        loginIsNull.setAge(18);
 
-        thirdUser = new User();
-        thirdUser.setId(3L);
-        thirdUser.setLogin(null);
-        thirdUser.setPassword("11223344");
-        thirdUser.setAge(18);
+        shortName = new User();
+        shortName.setId(4L);
+        shortName.setLogin("nazar"); // don't valid name
+        shortName.setPassword("11223344");
+        shortName.setAge(23);
 
-        fourthUser = new User();
-        fourthUser.setId(4L);
-        fourthUser.setLogin("nazar");
-        fourthUser.setPassword("11223344");
-        fourthUser.setAge(23);
+        shortPassword = new User();
+        shortPassword.setId(5L);
+        shortPassword.setLogin("nazar@gmail.com");
+        shortPassword.setPassword("123"); // don't valid password
+        shortPassword.setAge(23);
 
-        fifthUser = new User();
-        fifthUser.setId(5L);
-        fifthUser.setLogin("nazar@gmail.com");
-        fifthUser.setPassword("123");
-        fifthUser.setAge(23);
+        passwordIsNull = new User();
+        passwordIsNull.setId(6L);
+        passwordIsNull.setLogin("nazar@gmail.com");
+        passwordIsNull.setPassword(null);
+        passwordIsNull.setAge(23);
 
-        sixthUser = new User();
-        sixthUser.setId(6L);
-        sixthUser.setLogin("nazar@gmail.com");
-        sixthUser.setPassword(null);
-        sixthUser.setAge(23);
+        ageIsNotEnough = new User();
+        ageIsNotEnough.setId(7L);
+        ageIsNotEnough.setLogin("Olexiy@gmail.com");
+        ageIsNotEnough.setPassword("1234567");
+        ageIsNotEnough.setAge(15); // don't valid age
 
-        seventhUser = new User();
-        seventhUser.setId(7L);
-        seventhUser.setLogin("Olexiy@gmail.com");
-        seventhUser.setPassword("1234567");
-        seventhUser.setAge(15);
+        ageIsNegative = new User();
+        ageIsNegative.setId(8L);
+        ageIsNegative.setLogin("Mariya@gmail.com");
+        ageIsNegative.setPassword("1234567");
+        ageIsNegative.setAge(-5);
 
-        eighthUser = new User();
-        eighthUser.setId(8L);
-        eighthUser.setLogin("Mariya@gmail.com");
-        eighthUser.setPassword("1234567");
-        eighthUser.setAge(-5);
-
-        nineUser = new User();
-        nineUser.setId(9L);
-        nineUser.setLogin("Mark@gmail.com");
-        nineUser.setPassword("1234567");
-        nineUser.setAge(null);
+        ageIsNull = new User();
+        ageIsNull.setId(9L);
+        ageIsNull.setLogin("Mark@gmail.com");
+        ageIsNull.setPassword("1234567");
+        ageIsNull.setAge(null);
     }
 
     @Test
     void register_loginNoExistingUser_addedToStorage() {
-        Assertions.assertNull(storageDao.get(firstUser.getLogin()),
+        Assertions.assertNull(storageDao.get(validUser.getLogin()),
                 "User isn't exist with such login yet");
-        registrationService.register(firstUser);
-        Assertions.assertTrue(Storage.people.contains(firstUser),
+        registrationService.register(validUser);
+        Assertions.assertTrue(Storage.people.contains(validUser),
                 "User added in storage with such login");
     }
 
     @Test
     void register_loginExistingUser_throwsException() {
-        Storage.people.add(secondUser);
+        Storage.people.add(validUser);
         var exception =
                 Assertions.assertThrows(InvalidDataException.class,
-                        () -> registrationService.register(secondUser));
+                        () -> registrationService.register(validUser));
 
         String expected = "User already exists "
-                + secondUser.getLogin()
+                + validUser.getLogin()
                 + " with such login";
         String actual = exception.getMessage();
 
         Assertions.assertEquals(expected, actual);
-        Assertions.assertNotNull(storageDao.get(secondUser.getLogin()),
+        Assertions.assertNotNull(storageDao.get(validUser.getLogin()),
                 "User with such login exist in storage.");
     }
 
     @Test
     void register_nullLogin_throwsException() {
         var exception = Assertions.assertThrows(InvalidDataException.class,
-                () -> registrationService.register(thirdUser));
+                () -> registrationService.register(loginIsNull));
 
         String expected = "User login cannot be null";
         String actual = exception.getMessage();
 
         Assertions.assertEquals(expected, actual);
-        Assertions.assertNull(storageDao.get(thirdUser.getLogin()),
+        Assertions.assertNull(storageDao.get(loginIsNull.getLogin()),
                 "User with null login should not be added to storage.");
     }
 
     @Test
     void userLengthLogin_isOk() {
-        var actual = registrationService.register(firstUser).getLogin().length();
+        var actual = registrationService.register(validUser).getLogin().length();
 
         Assertions.assertTrue(MIN_LENGTH <= actual);
-        Assertions.assertTrue(Storage.people.contains(firstUser),
+        Assertions.assertTrue(Storage.people.contains(validUser),
                 "User should be added to storage.");
     }
 
     @Test
     void userLengthLogin_isNotOk() {
         var exception = Assertions.assertThrows(InvalidDataException.class,
-                () -> registrationService.register(fourthUser));
+                () -> registrationService.register(shortName));
 
         var expected = exception.getMessage();
         var actual = "Login must be at least "
                 + MIN_LENGTH
                 + " current long "
-                + fourthUser.getLogin().length();
+                + shortName.getLogin().length();
 
         Assertions.assertEquals(actual, expected);
-        Assertions.assertFalse(Storage.people.contains(fourthUser),
+        Assertions.assertFalse(Storage.people.contains(shortName),
                 "User shouldn't be added to storage.");
     }
 
     @Test
     void userPasswordLength_isOk() {
-        var actual = registrationService.register(firstUser).getLogin().length();
+        var actual = registrationService.register(validUser).getPassword().length();
 
-        Assertions.assertTrue(MIN_LENGTH <= actual);
-        Assertions.assertTrue(Storage.people.contains(firstUser),
+        Assertions.assertTrue(actual >= MIN_LENGTH);
+        Assertions.assertTrue(Storage.people.contains(validUser),
                 "User should be added to storage.");
     }
 
     @Test
     void userPasswordLength_isNotOk() {
         var exception = Assertions.assertThrows(InvalidDataException.class,
-                () -> registrationService.register(fifthUser));
+                () -> registrationService.register(shortPassword));
 
         var expected = "Password must be at least"
                 + MIN_LENGTH
                 + " current length"
-                + fifthUser.getPassword().length();
+                + shortPassword.getPassword().length();
         var actual = exception.getMessage();
 
         Assertions.assertEquals(expected, actual);
-        Assertions.assertFalse(Storage.people.contains(fifthUser),
+        Assertions.assertFalse(Storage.people.contains(shortPassword),
                 "User with short password should not be added to storage.");
     }
 
     @Test
     void userPassword_isNull() {
         var exception = Assertions.assertThrows(InvalidDataException.class,
-                () -> registrationService.register(sixthUser));
+                () -> registrationService.register(passwordIsNull));
 
         var expected = "User password cannot be null";
         var actual = exception.getMessage();
 
         Assertions.assertEquals(expected, actual);
-        Assertions.assertFalse(Storage.people.contains(sixthUser),
+        Assertions.assertFalse(Storage.people.contains(passwordIsNull),
                 "User with null password should not be added to storage.");
     }
 
     @Test
     void userAges_isOk() {
-        var actual = registrationService.register(firstUser).getAge();
+        var actual = registrationService.register(validUser).getAge();
 
         Assertions.assertTrue(MIN_AGE <= actual);
-        Assertions.assertTrue(Storage.people.contains(firstUser),
+        Assertions.assertTrue(Storage.people.contains(validUser),
                 "User should be added to storage.");
     }
 
     @Test
     void userAge_isNotOk() {
         var exception = Assertions.assertThrows(InvalidDataException.class,
-                () -> registrationService.register(seventhUser));
+                () -> registrationService.register(ageIsNotEnough));
 
         var expected = "User must be at least "
                 + MIN_AGE
@@ -216,33 +209,33 @@ class RegistrationServiceImplTest {
         var actual = exception.getMessage();
 
         Assertions.assertEquals(expected, actual);
-        Assertions.assertFalse(Storage.people.contains(seventhUser),
+        Assertions.assertFalse(Storage.people.contains(ageIsNotEnough),
                 "User with incorrect age should not be added to storage.");
     }
 
     @Test
     void userAge_isNegative() {
         var exception = Assertions.assertThrows(InvalidDataException.class,
-                () -> registrationService.register(eighthUser));
+                () -> registrationService.register(ageIsNegative));
 
         var expected = "Age cannot be negative";
         var actual = exception.getMessage();
 
         Assertions.assertEquals(expected, actual);
-        Assertions.assertFalse(Storage.people.contains(eighthUser),
+        Assertions.assertFalse(Storage.people.contains(ageIsNegative),
                 "User with incorrect age should not be added to storage.");
     }
 
     @Test
-    void userAge_isNull() {
+    void registerUser_whenAgeIsNull_throwsInvalidDataException() {
         var exception = Assertions.assertThrows(InvalidDataException.class,
-                () -> registrationService.register(nineUser));
+                () -> registrationService.register(ageIsNull));
 
         var expected = "User age cannot be null";
         var actual = exception.getMessage();
 
         Assertions.assertEquals(expected, actual);
-        Assertions.assertFalse(Storage.people.contains(nineUser),
+        Assertions.assertFalse(Storage.people.contains(ageIsNull),
                 "User with null age should not be added to storage.");
     }
 }
