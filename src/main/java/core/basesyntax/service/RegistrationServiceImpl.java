a0d5a2080635115199ab010824eab5f8061e47c8
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2024-11-05 22:48:27 +0200

    user task

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -10,17 +10,20 @@ public class RegistrationServiceImpl implements RegistrationService {
     @Override
     public User register(User user) throws CustomException {
         if (user.getAge() == null || user.getPassword() == null || user.getLogin() == null) {
-            throw new CustomException("User login,password or age is null!" + user.getAge() + user.getPassword() + user.getLogin());
+            throw new CustomException("User login,password or age is null!"
+                    + user.getAge() + user.getPassword() + user.getLogin());
         }
         if (user.getLogin().isEmpty() || (user.getPassword().isEmpty())) {
-            throw new CustomException("User login or password is empty!" + user.getLogin() + user.getPassword());
+            throw new CustomException("User login or password is empty!"
+                    + user.getLogin() + user.getPassword());
         }
         if ((user.getLogin().length()) < 6) {
             throw new CustomException("User login must be at least 6 characters long! - "
                     + user.getLogin());
         }
         if (user.getAge() < 18) {
-            throw new CustomException("The user must not be less than 18 years old! " + user.getAge());
+            throw new CustomException("The user must not be less than 18 years old! "
+                    + user.getAge());
         }
         if ((user.getPassword().length() < 6)) {
             throw new CustomException("User password is need to be at least 6 letters! - "
