a1388c825bd14a3e89cc1200a224e526609232e3
Vadym Dmytriv <vadymdmytriv@gmail.com>
2024-11-11 17:51:10 +0100

    Added RegistrationServiceImplTest class

--- src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -1,6 +1,5 @@
 package core.basesyntax.service;
 
-
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
