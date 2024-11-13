69b56070b73e53e53ac5b24b118dea7ccfb7693d
Vasyl <vasiliy.azarov.vl1@gmail.com>
2024-11-13 12:11:08 +0100

    implemented base task logic

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -9,6 +9,45 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
+        if(userValidation(user)){
+            return storageDao.add(user);
+        }
         return null;
+    }
+
+
+    private boolean userValidation(User user) {
+        return userNullCheck(user)
+                && userValidateLoginExist(user.getLogin())
+                && userValidateLoginLength(user.getLogin())
+                && userValidatePasswordLength(user.getPassword())
+                && userValidateAge(user.getAge());
+    }
+
+    private boolean userNullCheck(User user){
+        return user != null
+                && user.getLogin() != null
+                && user.getPassword() != null
+                && user.getAge() != null;
+    }
+
+    private boolean userValidateLoginExist(String userLogin) {
+        return storageDao.get(userLogin) == null;
+    }
+
+    private boolean userValidateLoginLength(String userLogin) {
+        return userLogin.length() >= 6;
+    }
+
+    private boolean userValidatePasswordLength(String userPassword) {
+        return userPassword.length() >= 6;
+    }
+
+    private boolean userValidateAge(int age) {
+        return age >= 18;
+    }
+
+    public void removeByLogin(String login) {
+        storageDao.deleteByLogin(login);
     }
 }
---
+++ src/test/java/core/basesyntax/UserRegistrationTest.java
@@ -0,0 +1,116 @@
+package core.basesyntax;
+
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.model.User;
+import core.basesyntax.service.RegistrationServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class UserRegistrationTest {
+    private static final String SUCCESS_LOGIN = "SUCCESS_LOGIN";
+    private static final String SUCCESS_PASSWORD = "SUCCESS_PASSWORD_123";
+    private static final Integer SUCCESS_AGE = 19;
+    private static final String NOT_SUCCESS_LOGIN = "short";
+    private static final String NOT_SUCCESS_PASSWORD = "short";
+    private static final Integer NOT_SUCCESS_AGE = 15;
+    private RegistrationServiceImpl registrationService;
+    private final static User user = new User();
+
+    @BeforeAll
+    static void setUserValidParams() {
+        user.setLogin(SUCCESS_LOGIN);
+        user.setPassword(SUCCESS_PASSWORD);
+        user.setAge(SUCCESS_AGE);
+    }
+
+    @AfterEach
+    void setValidUserParams() {
+        user.setLogin(SUCCESS_LOGIN);
+        user.setPassword(SUCCESS_PASSWORD);
+        user.setAge(SUCCESS_AGE);
+        registrationService.removeByLogin(SUCCESS_LOGIN);
+    }
+
+    @BeforeEach
+    void setStorageDao() {
+        registrationService = new RegistrationServiceImpl();
+    }
+
+    @Test
+    void allUserValuesValid_OK() {
+        assertEquals(user, registrationService.register(user));
+    }
+
+    @Test
+    void nullUser_NotOK() {
+        assertNull(null);
+    }
+
+    @Test
+    void addUserWithNullLogin_NotOK() {
+        user.setLogin(null);
+        assertNull(registrationService.register(user));
+    }
+
+    @Test
+    void addUserWithNullPassword_NotOK() {
+        user.setPassword(null);
+        assertNull(registrationService.register(user));
+    }
+
+    @Test
+    void addUserWithNullAge_NotOK() {
+        user.setAge(null);
+        assertNull(registrationService.register(user));
+    }
+
+    @Test
+    void addUserWithNewLogin_OK() {
+        assertEquals(user, registrationService.register(user));
+    }
+
+    @Test
+    void addUserWithExistingLogin_NotOK() {
+        registrationService.register(user);
+        assertNull(registrationService.register(user));
+    }
+
+    @Test
+    void addUserWithShortLogin_NotOK() {
+        user.setLogin(NOT_SUCCESS_LOGIN);
+        assertNull(registrationService.register(user));
+    }
+
+    @Test
+    void addUserWithLongLogin_OK() {
+        assertEquals(user, registrationService.register(user));
+    }
+
+    @Test
+    void addUserWithShortPassword_NotOK() {
+        user.setPassword(NOT_SUCCESS_PASSWORD);
+        assertNull(registrationService.register(user));
+    }
+
+    @Test
+    void addUserWithLongPassword_OK() {
+        assertEquals(user, registrationService.register(user));
+    }
+
+    @Test
+    void addUserWithInvalidAge_NotOK() {
+        user.setAge(NOT_SUCCESS_AGE);
+        assertNull(registrationService.register(user));
+    }
+
+    @Test
+    void addUserWithValidAge_OK() {
+        assertEquals(user, registrationService.register(user));
+    }
+
+}
