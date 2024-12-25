16cfd7f9e1b620b6c3c9f5285f2b992a55c41055
Dmytro Borysenko <borisand020774@gmail.com>
2024-12-25 16:55:33 +0200

    https://github.com/DmytroBorysenko2004/jv-registration-validation

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -3,42 +3,23 @@ package core.basesyntax.model;
 import java.util.Objects;
 
 public class User {
-    private Long id;
-    private String login;
+    final String login;
     private String password;
     private Integer age;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getLogin() {
         return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public Integer getAge() {
         return age;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 
     @Override
     public boolean equals(Object o) {
@@ -53,7 +34,14 @@ public class User {
                 && Objects.equals(password, user.password)
                 && Objects.equals(age, user.age);
     }
-
+public User(String login , String password , Integer age){
+        this.login = login;
+        this.password = password;
+        this.age = age;
+}
+public User(String login){
+        this.login = login;
+}
     @Override
     public int hashCode() {
         return Objects.hash(login, password, age);
