f68b8f2d003c3d70f1183b0722bb30324e802daa
Sasha Koval <kovalsana40@gmail.com>
2024-12-27 16:27:04 +0200

    Let`s try

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -10,16 +10,18 @@ import java.util.List;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
+    private final int MINIMAL_AGE = 18;
+    private final int MINIMAL_LENGTH = 6;
 
     @Override
     public User register(User user) throws ImpossibleRegisterUserException {
         if (user == null) {
             throw new ImpossibleRegisterUserException("The user can`t be empty");
-        } else if (user.getLogin() == null || user.getLogin().length() < 6) {
+        } else if (user.getLogin() == null || user.getLogin().length() < MINIMAL_LENGTH) {
             throw new ImpossibleRegisterUserException("This login is incorrect");
-        } else if (user.getPassword() == null || user.getPassword().length() < 6) {
+        } else if (user.getPassword() == null || user.getPassword().length() < MINIMAL_LENGTH) {
             throw new ImpossibleRegisterUserException("This password is incorrect");
-        } else if (user.getAge() == null || user.getAge() < 18) {
+        } else if (user.getAge() == null || user.getAge() < MINIMAL_AGE) {
             throw new ImpossibleRegisterUserException("Age of the user isn`t enough to show content");
         } else {
             List<User> copy = new ArrayList<>(Storage.people);
--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -18,15 +18,14 @@ public class HelloWorldTest {
 
     @BeforeEach
     void setUp() {
-        user = new User();
-    }
-
-    @Test
-    void loginOfUserAlreadyExists_NotOk() {
         user.setId(12l);
         user.setLogin("sanya67");
         user.setAge(18);
         user.setPassword("123456");
+    }
+
+    @Test
+    void loginOfUserAlreadyExists_NotOk() {
         try {
             Assert.assertEquals(user, registrationService.register(user));
         } catch (ImpossibleRegisterUserException e) {
@@ -46,9 +45,6 @@ public class HelloWorldTest {
     @Test
     void loginIsNull_NotOk() {
         user.setLogin(null);
-        user.setId(11l);
-        user.setPassword("890765");
-        user.setAge(20);
         Assertions.assertThrows(ImpossibleRegisterUserException.class,
                 ()-> {
             registrationService.register(user);
@@ -57,10 +53,7 @@ public class HelloWorldTest {
 
     @Test
     void loginNotExist_Ok() {
-        user.setId(8l);
         user.setLogin("Deadpool");
-        user.setAge(89);
-        user.setPassword("90809809");
         try {
             Assert.assertEquals(user, registrationService.register(user));
         } catch (ImpossibleRegisterUserException e) {
@@ -70,10 +63,7 @@ public class HelloWorldTest {
 
     @Test
     void loginHaveSizeLessThanAllowable_NotOk() {
-        user.setId(2l);
         user.setLogin("sanya");
-        user.setPassword("7907001");
-        user.setAge(21);
         Assertions.assertThrows(ImpossibleRegisterUserException.class,
                 ()-> {
             registrationService.register(user);
@@ -82,10 +72,7 @@ public class HelloWorldTest {
 
     @Test
     void loginHaveSizeEqualsAllowableSize_Ok() {
-        user.setId(4l);
         user.setLogin("sanya2");
-        user.setPassword("7907001767");
-        user.setAge(23);
         try {
             Assert.assertEquals(user, registrationService.register(user));
         } catch (ImpossibleRegisterUserException e) {
@@ -95,10 +82,7 @@ public class HelloWorldTest {
 
     @Test
     void loginIsBiggerThanAllowableSize_Ok() {
-        user.setId(8l);
         user.setLogin("sanya1256");
-        user.setPassword("7907001767");
-        user.setAge(23);
         try {
             Assert.assertEquals(user, registrationService.register(user));
         } catch (ImpossibleRegisterUserException e) {
@@ -108,10 +92,7 @@ public class HelloWorldTest {
 
     @Test
     void ageOfUserLessThanAllowableAge() {
-        user.setId(78l);
         user.setAge(15);
-        user.setLogin("VenoM!!!");
-        user.setPassword("yufgugfyufguy");
         Assertions.assertThrows(ImpossibleRegisterUserException.class,
                 ()-> {
             registrationService.register(user);
@@ -120,10 +101,7 @@ public class HelloWorldTest {
 
     @Test
     void ageOfUserEqualsAllowableAge_Ok() {
-        user.setId(12l);
         user.setAge(18);
-        user.setLogin("JaxTaller!");
-        user.setPassword("yufgugfyufguy1123");
         try {
             Assert.assertEquals(user, registrationService.register(user));
         } catch (ImpossibleRegisterUserException e) {
@@ -133,10 +111,7 @@ public class HelloWorldTest {
 
     @Test
     void ageOfUserBiggerThanAllowableAge_Ok() {
-        user.setId(17l);
         user.setAge(45);
-        user.setLogin("JaxTaller!isAlive");
-        user.setPassword("yufgu121123");
         try {
             Assert.assertEquals(user, registrationService.register(user));
         } catch (ImpossibleRegisterUserException e) {
@@ -146,8 +121,6 @@ public class HelloWorldTest {
 
     @Test
     void passwordLengthOfUserLessThanAllowableLength_NotOk() {
-        user.setId(77l);
-        user.setAge(24);
         user.setPassword("gyfyt");
         Assertions.assertThrows(ImpossibleRegisterUserException.class,
                 ()->{
@@ -157,9 +130,6 @@ public class HelloWorldTest {
 
     @Test
     void passwordLengthOfUserEqualsAllowableLength_Ok() {
-        user.setId(123l);
-        user.setAge(49);
-        user.setLogin("DyingLight");
         user.setPassword("121123");
         try {
             Assert.assertEquals(user, registrationService.register(user));
@@ -170,9 +140,6 @@ public class HelloWorldTest {
 
     @Test
     void passwordLengthOfUserBiggerThanAllowableLength_Ok() {
-        user.setId(177l);
-        user.setAge(36);
-        user.setLogin("DyingLightTheFollowing");
         user.setPassword("121123gghugugghg");
         try {
             Assert.assertEquals(user, registrationService.register(user));
