a416ea2bb2a856674d36e6ba00d931ca05bc6e0b
Danil Tarasiuk <vodafonenous9388@gmail.com>
2025-01-04 09:30:33 +0100

    Created IncorrectDataException.java, RegistrationServiceImplTest.java and modified RegistrationServiceImpl.java

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -3,12 +3,75 @@ package core.basesyntax.service;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
+import java.util.ArrayList;
 
 public class RegistrationServiceImpl implements RegistrationService {
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
+        if (storageDao.get(user.getLogin()) == null) {
+            if (user.getLogin() == null) {
+                throw new IncorrectDataException("Login can't be null");
+            } else if (user.getLogin().length() < 6) {
+                throw new IncorrectDataException("Login can't be null and length "
+                        + "must be more than 6 characters");
+            }
+
+            passwordVerification(user.getPassword());
+
+            if (user.getAge() == null) {
+                throw new IncorrectDataException("Age can't be null");
+            } else if (user.getAge() < 18) {
+                throw new IncorrectDataException("Age can't be null and must "
+                        + "be equal to or greater than 18");
+            }
+            storageDao.add(user);
+            return user;
+        }
         return null;
+    }
+
+    public void passwordVerification(String password) {
+        if (password == null) {
+            throw new IncorrectDataException("Password can't be null");
+        } else if (password.length() < 6) {
+            throw new IncorrectDataException("Password can't be null and length "
+                    + "must be more than 6 characters");
+        }
+
+        char[] passwordCharacters = password.toCharArray();
+        ArrayList<Character> list = new ArrayList<>();
+
+        for (Character element : passwordCharacters) {
+            list.add(element);
+        }
+
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasNumber = false;
+        boolean hasSpecialSymbol = false;
+
+        for (Character element : list) {
+            if (Character.isUpperCase(element)) {
+                hasUpperCase = true;
+            } else if (Character.isLowerCase(element)) {
+                hasLowerCase = true;
+            } else if (Character.isDigit(element)) {
+                hasNumber = true;
+            } else if (!Character.isLetterOrDigit(element)) {
+                hasSpecialSymbol = true;
+            }
+        }
+
+        if (!hasUpperCase) {
+            throw new IncorrectDataException("Password must contain at least one capital letter");
+        } else if (!hasLowerCase) {
+            throw new IncorrectDataException("Password must contain at least one lowercase letter");
+        } else if (!hasNumber) {
+            throw new IncorrectDataException("Password must contain at least one number");
+        } else if (!hasSpecialSymbol) {
+            throw new IncorrectDataException("Password must contain at least one special symbol");
+        }
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,196 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private RegistrationService registrationService = new RegistrationServiceImpl();
+
+    private User createValidUser() {
+        User user = new User();
+        //user.setLogin("superlogin");
+        user.setPassword("P$assword21");
+        user.setAge(18);
+        user.setId(1L);
+        return user;
+    }
+
+    @Test
+    void sizeOfLoginIsLongerThan6_Ok() {
+        User user = createValidUser();
+        user.setLogin("login24");
+
+        User actual = registrationService.register(user);
+
+        assertNotNull(actual);
+    }
+
+    @Test
+    void sizeOfLoginIsShorterThan6_NotOk() {
+        User user = createValidUser();
+        user.setLogin("log");
+
+        assertThrows(IncorrectDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void loginIsNull_NotOk() {
+        User user = createValidUser();
+        user.setLogin(null);
+
+        assertThrows(IncorrectDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void sizeOfPasswordIslongerThan6_Ok() {
+        User user = createValidUser();
+        user.setLogin("login54");
+
+        User actual = registrationService.register(user);
+
+        assertNotNull(actual);
+    }
+
+    @Test
+    void sizeOfPasswordIsShorterThan6_NotOk() {
+        User user = createValidUser();
+        user.setLogin("login64");
+        user.setPassword("pass");
+
+        assertThrows(IncorrectDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void passwordHasAtLeastOneCapitalLetter_Ok() {
+        User user = createValidUser();
+        user.setLogin("login74");
+
+        User actual = registrationService.register(user);
+
+        assertNotNull(actual);
+    }
+
+    @Test
+    void passwordDoesNotHaveAtLeastOneCapitalLetter_NotOk() {
+        User user = createValidUser();
+        user.setLogin("login84");
+        user.setPassword("p$assword21");
+
+        assertThrows(IncorrectDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void passwordHasAtLeastOneLowerCaseLetter_Ok() {
+        User user = createValidUser();
+        user.setLogin("login95");
+
+        User actual = registrationService.register(user);
+        assertNotNull(actual);
+    }
+
+    @Test
+    void passwordDoesNotHaveAtLeastOneLowerCaseLetter_NotOk() {
+        User user = createValidUser();
+        user.setLogin("login105");
+        user.setPassword("P$ASSWORD21");
+
+        assertThrows(IncorrectDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void passwordHasAtLeastOneNumber_Ok() {
+        User user = createValidUser();
+        user.setLogin("login115");
+
+        User actual = registrationService.register(user);
+
+        assertNotNull(actual);
+    }
+
+    @Test
+    void passwordDoesNotHaveAtLeastOneNumber_NotOk() {
+        User user = createValidUser();
+        user.setLogin("login125");
+        user.setPassword("P$assword");
+
+        assertThrows(IncorrectDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void passwordHasAtLeastOneSpecialSymbol_Ok() {
+        User user = createValidUser();
+        user.setLogin("login136");
+
+        User actual = registrationService.register(user);
+        assertNotNull(actual);
+    }
+
+    @Test
+    void passwordDoesNotHaveAtLeastOneSpecialSymbol_NotOk() {
+        User user = createValidUser();
+        user.setLogin("login145");
+        user.setPassword("Password21");
+
+        assertThrows(IncorrectDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void passwordIsNull_NotOk() {
+        User user = createValidUser();
+        user.setLogin("login156");
+        user.setPassword(null);
+
+        assertThrows(IncorrectDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void ageIsGreaterThan18_Ok() {
+        User user = createValidUser();
+        user.setLogin("login167");
+
+        User actual = registrationService.register(user);
+
+        assertNotNull(actual);
+    }
+
+    @Test
+    void ageIsLessThan18_NotOk() {
+        User user = createValidUser();
+        user.setLogin("login177");
+        user.setAge(15);
+
+        assertThrows(IncorrectDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void ageIsNull_NotOk() {
+        User user = createValidUser();
+        user.setLogin("login188");
+        user.setAge(null);
+
+        assertThrows(IncorrectDataException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+}
