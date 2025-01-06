3ca7c8b057f6ab7e6f203b5513f330d901637d67
Mich Hmk <gumeniukmich@gmail.com>
2025-01-06 18:13:41 +0200

    mentor feedback 2

--- src/test/java/core/basesyntax/RegistrationServiceImplTest.java
+++ src/test/java/core/basesyntax/RegistrationServiceImplTest.java
@@ -13,6 +13,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RegistrationServiceImplTest {
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_LOGIN_LENGTH = 6;
+    private static final int MIN_AGE = 18;
     private static final User user = new User("Bob_Super", "Bob12345", 18);
 
     private static RegistrationServiceImpl service;
@@ -32,101 +35,90 @@ public class RegistrationServiceImplTest {
     @Test
     void register_validUser_Ok() {
         User validNewUser = new User("_Emily_", "_emily_", 22);
-
         User validUserResult = service.register(validNewUser);
-
-        assertEquals(validUserResult, validNewUser);
-    }
-    
-    @Test
-    void register_validEdgeValuesUser_Ok() {
         User validEdgeNewUser = new User("_Emily", "emily_", 18);
-
         User validEdgeLoginResult = service.register(validEdgeNewUser);
 
+        assertEquals(validUserResult, validNewUser);
         assertEquals(validEdgeLoginResult, validEdgeNewUser);
     }
 
     @Test
-    void register_sameLoginUser_NotOk() {
+    void register_userLoginAlreadyExists_NotOk() {
         User sameLoginUser = new User("Bob_Super", "Bob2345", 20);
 
-        assertThrows(RegisterFailedException.class, () ->
-                service.register(sameLoginUser));
+        RegisterFailedException exception = assertThrows(RegisterFailedException.class,
+                () -> service.register(sameLoginUser));
+        assertEquals("User with login " + sameLoginUser.getLogin()
+                + " already exists", exception.getMessage());
     }
 
     @Test
-    void register_nullUser_NotOk() {
-        assertThrows(IllegalArgumentException.class, () ->
-                service.register(null));
+    void register_userIsNull_NotOk() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> service.register(null));
+        assertEquals("User cannot be null", exception.getMessage());
     }
 
     @Test
-    void register_wrongLogin_NotOk() {
+    void register_loginLengthLessThanMinimalRequired_NotOk() {
         User wrongLoginUser = new User("Jack", "jack255", 55);
-
-        assertThrows(RegisterFailedException.class, () ->
-                service.register(wrongLoginUser));
-    }
-
-    @Test
-    void register_edgeWrongLogin_NotOk() {
         User edgeWrongLoginUser = new User("JackJ", "jack255", 55);
-
-        assertThrows(RegisterFailedException.class, () ->
-                service.register(edgeWrongLoginUser));
-    }
-
-    @Test
-    void register_nullLogin_NotOk() {
         User nullLoginUser = new User(null, "jack255", 55);
 
-        assertThrows(RegisterFailedException.class, () ->
+        RegisterFailedException exception = assertThrows(RegisterFailedException.class, () ->
+                service.register(wrongLoginUser));
+        RegisterFailedException exceptionEdge = assertThrows(RegisterFailedException.class, () ->
+                service.register(edgeWrongLoginUser));
+        RegisterFailedException exceptionNull = assertThrows(RegisterFailedException.class, () ->
                 service.register(nullLoginUser));
+
+        String expectedMessage = "Login must be at least " + MIN_LOGIN_LENGTH
+                + " characters.";
+
+        assertEquals(expectedMessage, exception.getMessage());
+        assertEquals(expectedMessage, exceptionEdge.getMessage());
+        assertEquals(expectedMessage, exception.getMessage());
     }
 
     @Test
-    void register_wrongPassword_NotOk() {
+    void register_passwordLengthLessThanMinimalRequired_NotOk() {
         User wrongPasswordUser = new User("Jack123", "jack", 40);
-
-        assertThrows(RegisterFailedException.class, () ->
-                service.register(wrongPasswordUser));
-    }
-
-    @Test
-    void register_edgePassword_NotOk() {
         User edgePasswordUser = new User("Jack123", "jack1", 40);
-
-        assertThrows(RegisterFailedException.class, () ->
-                service.register(edgePasswordUser));
-    }
-
-    @Test
-    void register_nullPassword_NotOk() {
         User nullPasswordUser = new User("Jack123", null, 40);
 
-        assertThrows(RegisterFailedException.class, () ->
+        RegisterFailedException exception = assertThrows(RegisterFailedException.class, () ->
+                service.register(wrongPasswordUser));
+        RegisterFailedException exceptionEdge = assertThrows(RegisterFailedException.class, () ->
+                service.register(edgePasswordUser));
+        RegisterFailedException exceptionNull = assertThrows(RegisterFailedException.class, () ->
                 service.register(nullPasswordUser));
+
+        String expectedMessage = "Password must be at least " + MIN_PASSWORD_LENGTH
+                + " characters.";
+
+        assertEquals(expectedMessage, exception.getMessage());
+        assertEquals(expectedMessage, exceptionEdge.getMessage());
+        assertEquals(expectedMessage, exceptionNull.getMessage());
     }
 
     @Test
-    void register_wrongAge_NotOk() {
+    void register_ageValueLessThanMinimalRequired_NotOk() {
         User wrongAgeUser = new User("AlexGL", "alex_gl", 10);
-        assertThrows(RegisterFailedException.class, () ->
-                service.register(wrongAgeUser));
-    }
-
-    @Test
-    void register_edgeAge_NotOk() {
         User edgeAgeUser = new User("AlexGL", "alex_gl", 17);
-        assertThrows(RegisterFailedException.class, () ->
-                service.register(edgeAgeUser));
-    }
-
-    @Test
-    void register_nullAge_NotOk() {
         User nullAgeUser = new User("AlexGL", "alex_gl", null);
-        assertThrows(RegisterFailedException.class, () ->
-                service.register(nullAgeUser));
+
+        RegisterFailedException exception = assertThrows(RegisterFailedException.class, () ->
+                service.register(wrongAgeUser));
+        RegisterFailedException exceptionEdge = assertThrows(RegisterFailedException.class, () ->
+                service.register(edgeAgeUser));
+        RegisterFailedException exceptionNull = assertThrows(RegisterFailedException.class, () ->
+                service.register(edgeAgeUser));
+
+        String expectedMessage = "User must be at least " + MIN_AGE + " years old.";
+
+        assertEquals(expectedMessage, exception.getMessage());
+        assertEquals(expectedMessage, exceptionEdge.getMessage());
+        assertEquals(expectedMessage, exceptionNull.getMessage());
     }
 }
