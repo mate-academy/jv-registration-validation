7999f5f8b0a11610648922aa68627256134529e0
eulu <anegamega@gmail.com>
2025-01-20 15:53:36 +0200

    Add user login, password validation and tests

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,6 +2,8 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
@@ -9,6 +11,25 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        String login = user.getLogin();
+        String password = user.getPassword();
+        if (login == null) {
+            throw new RegistrationException("Username must not be empty");
+        }
+        if (password == null) {
+            throw new RegistrationException("Password must not be empty");
+        }
+        if (storageDao.get(login) != null) {
+            throw new RegistrationException("Username '"
+                    + login
+                    + "' is already taken");
+        }
+        if (login.trim().length() < 6) {
+            throw new RegistrationException("Username must be at least 6 characters long");
+        }
+        if (password.trim().length() < 6) {
+            throw new RegistrationException("Password must be at least 6 characters long");
+        }
+        return storageDao.add(user);
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,89 @@
+package core.basesyntax.service;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.RegistrationException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class RegistrationServiceImplTest {
+    public static RegistrationService service;
+
+    @BeforeAll
+    static void beforeAll() {
+        service = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        Storage.people.clear();
+    }
+
+    @Test
+    void register_loginAlreadyExist_notOk() {
+        User user1 = new User();
+        user1.setLogin("username");
+        user1.setPassword("password");
+        User user2 = new User();
+        user2.setLogin("username");
+        user2.setPassword("password");
+        service.register(user1);
+        Executable executable = () -> service.register(user2);
+        assertThrows(RegistrationException.class, executable);
+    }
+
+    @Test
+    void register_loginIsNull_notOk() {
+        User user = new User();
+        user.setPassword("password");
+        assertThrows(RegistrationException.class, () -> service.register(user));
+    }
+
+    @Test
+    void register_loginLengthGreaterThanSix_ok() {
+        User user = new User();
+        user.setLogin("username");
+        user.setPassword("password");
+        User registeredUser = service.register(user);
+        assertEquals(registeredUser.getLogin(), user.getLogin());
+    }
+
+    @Test
+    void register_loginLengthLessThanSix_notOk() {
+        User user = new User();
+        user.setLogin("name");
+        user.setPassword("password");
+        Executable executable = () -> service.register(user);
+        assertThrows(RegistrationException.class, executable);
+    }
+
+    @Test
+    void register_passwordIsNull_notOk() {
+        User user = new User();
+        user.setLogin("username");
+        assertThrows(RegistrationException.class, () -> service.register(user));
+    }
+
+    @Test
+    void register_passwordLengthGreaterThanSix_ok() {
+        User user = new User();
+        user.setLogin("username");
+        user.setPassword("password");
+        User registeredUser = service.register(user);
+        assertEquals(registeredUser.getPassword(), user.getPassword());
+    }
+
+    @Test
+    void register_passwordLengthLessThanSix_notOk() {
+        User user = new User();
+        user.setLogin("username");
+        user.setPassword("pass");
+        Executable executable = () -> service.register(user);
+        assertThrows(RegistrationException.class, executable);
+    }
+}
