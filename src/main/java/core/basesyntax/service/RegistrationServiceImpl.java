780e78e5a78eed801e7d4dd2cb7d0ea94bc58098
Tetiana Hrytsenko <shviridtanya107@gmail.com>
2025-01-30 15:30:32 +0100

    initial commit

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,35 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.CustomException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LOGIN_LENGTH = 6;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new CustomException("User login not entered. Please, enter login");
+        }
+        if (Storage.people.contains(user)) {
+            throw new CustomException("User with this login already exists");
+        }
+        if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
+            throw new CustomException("Login should be least " + MIN_LOGIN_LENGTH + " characters");
+        }
+        if (user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new CustomException("Password should be least " + MIN_PASSWORD_LENGTH
+                    + " characters");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new CustomException("Age should be at least then " + MIN_AGE + " years");
+        }
+        storageDao.add(user);
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,131 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.exception.CustomException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private static RegistrationService service;
+    private User user;
+
+    @BeforeAll
+    static void beforeAll() {
+        service = new RegistrationServiceImpl();
+    }
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+    }
+
+    @Test
+    void register_StorageHasUserWithSuchLogin_NotOk() {
+        User storedUser = new User();
+        storedUser.setLogin("existingUser");
+        Storage.people.add(storedUser);
+        user.setLogin("existingUser");
+        assertThrows(CustomException.class, () -> service.register(user),
+                "User with this login already exists");
+    }
+
+    @Test
+    void register_StorageDoNotHaveUser_Ok() {
+        user.setLogin("notExistingUser");
+        user.setPassword("validPassword");
+        user.setAge(25);
+        User expected = user;
+        User actual = service.register(user);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void register_NullUser_NotOk() {
+        assertThrows(CustomException.class, () -> service.register(null));
+    }
+
+    @Test
+    void register_LoginIsLessThen6Characters_NotOK() {
+        StringBuilder login = new StringBuilder();
+        for (char letter = 'a'; letter < 'f'; letter++) {
+            user.setLogin(login.toString());
+            login.append(letter);
+            assertThrows(CustomException.class, () -> service.register(user));
+        }
+    }
+
+    @Test
+    void register_LoginIsAtLeast6Characters_OK() {
+        StringBuilder login = new StringBuilder("6chars");
+        for (char ch = 'a'; ch < 'f'; ch++) {
+            user.setPassword("validPassword");
+            user.setAge(25);
+            user.setLogin(login.toString());
+            User expected = user;
+            User actual = service.register(user);
+            assertNotNull(actual);
+            assertEquals(expected, actual);
+            login.append(ch);
+            user = new User();
+        }
+    }
+
+    @Test
+    void register_PasswordIsLessThen6Characters_NotOK() {
+        user.setLogin("validLogin");
+        user.setAge(25);
+        StringBuilder password = new StringBuilder();
+        for (char letter = 'a'; letter < 'f'; letter++) {
+            user.setPassword(password.toString());
+            password.append(letter);
+            assertNotNull(user);
+            assertThrows(CustomException.class, () -> service.register(user));
+        }
+    }
+
+    @Test
+    void register_PasswordIsAtLeast6Characters_OK() {
+        StringBuilder login = new StringBuilder("anotherValidLogin");
+        StringBuilder password = new StringBuilder("sixLet");
+        for (char letter = 'a'; letter < 'f'; letter++) {
+            user.setLogin(login.toString());
+            user.setPassword(password.toString());
+            user.setAge(25);
+            login.append(letter);
+            password.append(letter);
+            assertNotNull(user);
+            assertEquals(service.register(user), user);
+            user = new User();
+        }
+    }
+
+    @Test
+    void register_AgeLessThen18_NotOk() {
+        user.setLogin("validLogin");
+        user.setPassword("validPassword");
+        for (int age = -4; age < 18; age += 4) {
+            user.setAge(age);
+            assertNotNull(user);
+            assertThrows(CustomException.class, () -> service.register(user));
+        }
+    }
+
+    @Test
+    void register_AgeIs18_Ok() {
+        int testAge = 50;
+        for (int age = 18; age < testAge; age += 10) {
+            user.setLogin("validLogin" + age);
+            user.setPassword("validPassword");
+            user.setAge(age);
+            assertNotNull(user);
+            assertEquals(service.register(user), user);
+            user = new User();
+        }
+    }
+}
