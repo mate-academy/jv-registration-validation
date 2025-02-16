f24a06ad8a8607dc0292efc9ec8ac7cc1406320e
Oleksii Petrychenko <apetrychenkoff@gmail.com>
2025-02-16 14:58:01 +0200

    completed the task by creating and testing service for all possible cases

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -5,10 +5,36 @@ import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LOGIN_LENGTH = 6;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_REGISTRATION_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Login cannot be empty");
+        }
+        if (user.getPassword() == null || user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RegistrationException("Please, enter password at least 6 characters long");
+        }
+        if (user.getAge() == null) {
+            throw new RegistrationException("Please, indicate your age");
+        }
+        if (user.getAge() < MIN_REGISTRATION_AGE) {
+            throw new RegistrationException("You must be "
+                    + MIN_REGISTRATION_AGE
+                    + " or older to register");
+        }
+        if (user.getAge() < 0) {
+            throw new RegistrationException("Please, enter valid age");
+        }
+        if (user.getLogin().length() < MIN_LOGIN_LENGTH) {
+            throw new RegistrationException("You login must be at least 6 character long");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("Such user already exists");
+        }
+        return storageDao.add(user);
     }
 }
