135d3abe09a754cf89962aff26b1c9abac1be6f7
Maks <lisichenkowork@gmail.com>
2025-02-17 10:51:06 +0100

    firts commit

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,6 +2,7 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exception.InvalidDataOfUserException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
@@ -9,6 +10,47 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        checkIfUserIsNull(user);
+        checkLoginIsCorrect(user);
+        checkPasswordIsCorrect(user);
+        checkUserAgeIsCorrect(user);
+        checkLoginExist(user);
+        storageDao.add(user);
+        return user;
+    }
+
+    private boolean checkIfUserIsNull(User user) {
+        if (user == null) {
+            throw new InvalidDataOfUserException("can`t add null user");
+        }
+        return true;
+    }
+
+    private boolean checkLoginExist(User user) {
+        if (storageDao.get(user.getLogin()) == null) {
+            return true;
+        }
+        throw new InvalidDataOfUserException("is already exist");
+    }
+
+    private boolean checkLoginIsCorrect(User user) {
+        if (user.getLogin().length() < 6) {
+            throw new InvalidDataOfUserException(" login length is under 6");
+        }
+        return true;
+    }
+
+    private boolean checkPasswordIsCorrect(User user) {
+        if (user.getPassword().length() < 6) {
+            throw new InvalidDataOfUserException(" password length is under 6");
+        }
+        return true;
+    }
+
+    private boolean checkUserAgeIsCorrect(User user) {
+        if (user.getAge() < 18) {
+            throw new InvalidDataOfUserException(" age is under 18");
+        }
+        return true;
     }
 }
