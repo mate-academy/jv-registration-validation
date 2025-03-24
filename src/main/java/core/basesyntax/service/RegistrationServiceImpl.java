1641d04f62ac31fc57a8f4ea8d4049137235e82d
Viktoriia Dragneva <viktoriiadragneva@gmail.com>
2025-03-24 15:36:31 +0200

    solution

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -1,5 +1,6 @@
 package core.basesyntax.service;
 
+import core.basesyntax.RegistrationException;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
@@ -9,6 +10,18 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        if (user.getLogin() == null || user.getPassword() == null) {
+            throw new RegistrationException("Login or password cannot be null!");
+        }
+        if (user.getLogin().length() < 6 || (user.getPassword().length() < 6)) {
+            throw new RegistrationException("Login or password must be at least 6 characters");
+        }
+        if (user.getAge() < 18){
+            throw new RegistrationException("User must be at least 18 years old");
+        }
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RegistrationException("User with this login already exists");
+        }
+        return storageDao.add(user);
     }
 }
