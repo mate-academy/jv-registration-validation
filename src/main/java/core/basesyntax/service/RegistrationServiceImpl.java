80365f06dcb9e3a739fd4af3ab00b8e2a449198a
Petro <petroipatiy@gmail.com>
2025-04-09 11:39:19 +0100

    registration-validation first update

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -2,13 +2,48 @@ package core.basesyntax.service;
 
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exceptions.RegistrationException;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_SYMBOLS = 6;
+    private static final int MIN_AGE = 18;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        boolean passwordIsValid = false;
+        boolean loginIsValid = false;
+        boolean ageIsValid = false;
+
+        if (user == null) {
+            throw new NullPointerException("User is null");
+        }
+
+        if (user.getLogin() != null && user.getLogin().length() >= MIN_SYMBOLS) {
+            if (storageDao.get(user.getLogin()) == null) {
+                loginIsValid = true;
+            }
+        } else {
+            throw new RegistrationException("Login is not valid "
+                    + "or different user uses this login!");
+        }
+
+        if (user.getPassword() != null && user.getPassword().length() >= MIN_SYMBOLS) {
+            passwordIsValid = true;
+        } else {
+            throw new RegistrationException("Password is not valid!");
+        }
+
+        if (user.getAge() != null && user.getAge() >= MIN_AGE) {
+            ageIsValid = true;
+        } else {
+            throw new RegistrationException("Age is not valid!");
+        }
+        if (loginIsValid && passwordIsValid && ageIsValid) {
+            storageDao.add(user);
+        }
+
+        return user;
     }
 }
---
+++ src/test/java/core/basesyntax/service/RegistrationServiceImplTest.java
@@ -0,0 +1,60 @@
+package core.basesyntax.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import core.basesyntax.dao.StorageDao;
+import core.basesyntax.dao.StorageDaoImpl;
+import core.basesyntax.exceptions.RegistrationException;
+import core.basesyntax.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RegistrationServiceImplTest {
+    private User user = new User();
+    private final RegistrationService registrationService = new RegistrationServiceImpl();
+    private final StorageDao storageDao = new StorageDaoImpl();
+
+    @BeforeEach
+     void beforeEach() {
+        user.setAge(19);
+        user.setLogin("ambasador");
+        user.setPassword("ambasador1");
+    }
+
+    @Test
+    void storage_checkingByEmail_emailExistsNotOk() {
+        boolean actual = storageDao.get(user.getLogin()) == null;
+        assertTrue(actual);
+    }
+
+    @Test
+    void register_Login_isNotValid() {
+        user.setLogin("qw2sq");
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_Password_isNotValid() {
+        user.setPassword("q1esq");
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_Age_isNotValid() {
+        user.setAge(17);
+        assertThrows(RegistrationException.class, () -> {
+            registrationService.register(user);
+        });
+    }
+
+    @Test
+    void register_User_isNull() {
+        user = null;
+        assertThrows(NullPointerException.class, () -> registrationService.register(user));
+    }
+}
